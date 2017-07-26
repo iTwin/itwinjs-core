459fbf3c8a58175f13e786874a10ba658b30f05d
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2017-07-26 11:50:17 -0400

    Add BisClass enum, utilize BisCore and BisClass enums

--- source/BisCore.ts
+++ source/BisCore.ts
@@ -2,6 +2,7 @@
 |  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 *--------------------------------------------------------------------------------------------*/
 
+/** BisCore.ClassName */
 export enum BisCore {
   Schema = "BisCore",
   Category = "BisCore.Category",
@@ -14,9 +15,32 @@ export enum BisCore {
   GeometricElement = "BisCore.GeometricElement",
   ModelSelector = "BisCore.ModelSelector",
   OrthographicViewDefinition = "BisCore.OrthographicViewDefinition",
+  RepositoryLink = "BisCore.RepositoryLink",
   SpatialCategory = "BisCore.SpatialCategory",
   SpatialViewDefinition = "BisCore.SpatialViewDefinition",
   SubCategory = "BisCore.SubCategory",
   ViewDefinition = "BisCore.ViewDefinition",
   ViewDefinition3d = "BisCore.ViewDefinition3d",
+  ViewDefinition3dUsesModelSelector = "BisCore.ViewDefinition3dUsesModelSelector",
+}
+
+/** Just ClassName, no "BisCore." prefix */
+export enum BisClass {
+  Category = "Category",
+  CategorySelector = "CategorySelector",
+  DisplayStyle = "DisplayStyle",
+  DisplayStyle2d = "DisplayStyle2d",
+  DisplayStyle3d = "DisplayStyle3d",
+  DrawingCategory = "DrawingCategory",
+  Element = "Element",
+  GeometricElement = "GeometricElement",
+  ModelSelector = "ModelSelector",
+  OrthographicViewDefinition = "OrthographicViewDefinition",
+  RepositoryLink = "RepositoryLink",
+  SpatialCategory = "SpatialCategory",
+  SpatialViewDefinition = "SpatialViewDefinition",
+  SubCategory = "SubCategory",
+  ViewDefinition = "ViewDefinition",
+  ViewDefinition3d = "ViewDefinition3d",
+  ViewDefinition3dUsesModelSelector = "ViewDefinition3dUsesModelSelector",
 }
--- source/test/ECSqlStatement.test.ts
+++ source/test/ECSqlStatement.test.ts
@@ -4,6 +4,7 @@
 
 import { assert } from "chai";
 import { ECSqlStatement } from "@bentley/imodeljs-dgnplatform/lib/DgnDb";
+import { BisCore } from "../Biscore";
 import { IModel } from "../IModel";
 import { IModelTestUtils } from "./IModelTestUtils";
 
@@ -20,13 +21,13 @@ describe("ECSqlStatement", () => {
 
   it("should prepare a SELECT ALL statement", async () => {
     const imodel: IModel = await IModelTestUtils.openIModel("test.bim", true);
-    ECSqlStatementTestUtils.getStatement (imodel, "select * from BIS.Element", true);
+    ECSqlStatementTestUtils.getStatement (imodel, "SELECT * FROM " + BisCore.Element, true);
   });
 
   it("should not prepare an UPDATE statement", async () => {
     try {
     const imodel: IModel = await IModelTestUtils.openIModel("test.bim", true, true);
-    await ECSqlStatementTestUtils.getStatement (imodel, "update BIS.Element set CodeValue='a'", false);
+    await ECSqlStatementTestUtils.getStatement (imodel, "UPDATE " + BisCore.Element + " SET CodeValue='a'", false);
     assert.fail(true, false, "An UPDATE statement should not prepare.");
     } catch (reason) {
       // this is what I expected
@@ -35,7 +36,7 @@ describe("ECSqlStatement", () => {
 
   it("should produce a single row from SELECT ALL using step_once", async () => {
     const imodel: IModel = await IModelTestUtils.openIModel("test.bim", true);
-    const stmt = await ECSqlStatementTestUtils.getStatement (imodel, "select * from BIS.Element", true);
+    const stmt = await ECSqlStatementTestUtils.getStatement(imodel, "SELECT * FROM " + BisCore.Element, true);
     const rowdata = await stmt.step_once();
     assert.isNotNull(rowdata);
     const obj: any = JSON.parse(rowdata);
@@ -45,7 +46,7 @@ describe("ECSqlStatement", () => {
 
   it("should produce an array of rows from SELECT ALL using step_all", async () => {
     const imodel: IModel = await IModelTestUtils.openIModel("test.bim", true);
-    const stmt = await ECSqlStatementTestUtils.getStatement (imodel, "select * from BIS.Element", true);
+    const stmt = await ECSqlStatementTestUtils.getStatement(imodel, "SELECT * FROM " + BisCore.Element, true);
     const allrowsdata = await stmt.step_all();
     assert.isNotNull(allrowsdata);
     const rows: any = JSON.parse(allrowsdata);
