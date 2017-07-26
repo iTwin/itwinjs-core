86244b00118fb2a21c902741926caf8ebd70e1b0
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2017-07-26 11:08:38 -0400

    Add BisCore enum values for classes related to ViewDefinition

--- source/BisCore.ts
+++ source/BisCore.ts
@@ -5,9 +5,18 @@
 export enum BisCore {
   Schema = "BisCore",
   Category = "BisCore.Category",
+  CategorySelector = "BisCore.CategorySelector",
+  DisplayStyle = "BisCore.DisplayStyle",
+  DisplayStyle2d = "BisCore.DisplayStyle2d",
+  DisplayStyle3d = "BisCore.DisplayStyle3d",
   DrawingCategory = "BisCore.DrawingCategory",
   Element = "BisCore.Element",
   GeometricElement = "BisCore.GeometricElement",
+  ModelSelector = "BisCore.ModelSelector",
+  OrthographicViewDefinition = "BisCore.OrthographicViewDefinition",
   SpatialCategory = "BisCore.SpatialCategory",
+  SpatialViewDefinition = "BisCore.SpatialViewDefinition",
   SubCategory = "BisCore.SubCategory",
+  ViewDefinition = "BisCore.ViewDefinition",
+  ViewDefinition3d = "BisCore.ViewDefinition3d",
 }
