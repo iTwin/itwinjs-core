a2069760e325a7821c4dbfd025dd159269d028ab
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2017-08-31 11:43:12 -0400

    API change: Elements.getElement now returns Promise<Element>

--- source/Elements.ts
+++ source/Elements.ts
@@ -5,10 +5,8 @@ import { Element, ElementProps } from "./Element";
 import { Code, IModel } from "./IModel";
 import { ClassRegistry } from "./ClassRegistry";
 import { LRUMap } from "@bentley/bentleyjs-core/lib/LRUMap";
-import { BentleyPromise } from "@bentley/bentleyjs-core/lib/Bentley";
-import { DgnDbStatus } from "@bentley/imodeljs-dgnplatform/lib/DgnDb";
 import { assert } from "@bentley/bentleyjs-core/lib/Assert";
-import { Id64 } from "@bentley/bentleyjs-core/lib/Id";
+import { Guid, Id64 } from "@bentley/bentleyjs-core/lib/Id";
 
 /** Parameters to specify what element to load. */
 export interface ElementLoadParams {
@@ -29,23 +27,19 @@ export class Elements {
 
   public constructor(iModel: IModel, maxElements: number = 2000) { this._iModel = iModel; this._loaded = new LRUMap<string, Element>(maxElements); }
 
-  /**
-   * Get an element by Id or Code.
-   * @param opts  Either the id or the code of the element
-   * @returns The Element or undefined if the Id is not found
-   */
-  public async getElement(opts: ElementLoadParams): BentleyPromise<DgnDbStatus, Element | undefined> {
+  /** Private implementation details of getElement */
+  private async doGetElement(opts: ElementLoadParams): Promise<Element> {
     // first see if the element is already in the local cache.
     if (opts.id) {
       const loaded = this._loaded.get(opts.id.toString());
       if (loaded)
-        return { result: loaded };
+        return loaded;
     }
 
     // Must go get the element from the iModel. Start by requesting the element's data.
     const getObj = await this._iModel.getElement(JSON.stringify(opts));
     if (getObj.error || !getObj.result) { // todo: Shouldn't getObj.result always be non-empty if there is no error?
-      return { result: undefined }; // we didn't find an element with the specified identity. That's not an error, just an empty result.
+      return Promise.reject(new Error("Didn't find an element with the specified identity"));
     }
     const json = getObj.result;
 
@@ -54,7 +48,7 @@ export class Elements {
 
     const elObj = await ClassRegistry.createInstance(props);
     if (elObj.error)
-      return { error: elObj.error };
+      return Promise.reject(new Error("Could not create element instance"));
 
     const el = elObj.result as Element;
     assert(el instanceof Element);
@@ -62,12 +56,21 @@ export class Elements {
     // We have created the element. Cache it before we return it.
     el.setPersistent(); // elements in the cache must be immutable and in their just-loaded state. Freeze it to enforce that
     this._loaded.set(el.id.toString(), el);
-    return { result: el };
+    return el;
+  }
+
+  /** Get an element by Id, FederationGuid, or Code */
+  public async getElement(elementId: Id64 | Guid | Code): Promise<Element> {
+    if (elementId instanceof Id64) return this.doGetElement({ id: elementId });
+    if (elementId instanceof Guid) return this.doGetElement({ federationGuid: elementId.toString() });
+    if (elementId instanceof Code) return this.doGetElement({ code: elementId });
+    assert(false);
+    return Promise.reject(new Error("Invalid parameter passed to getElement"));
   }
 
   /** The Id of the root subject element. */
   public get rootSubjectId(): Id64 { return new Id64("0x1"); }
 
   /** Get the root subject element. */
-  public async getRootSubject(): BentleyPromise<DgnDbStatus, Element | undefined> { return this.getElement({ id: this.rootSubjectId }); }
+  public async getRootSubject(): Promise<Element> { return this.getElement(this.rootSubjectId); }
 }
