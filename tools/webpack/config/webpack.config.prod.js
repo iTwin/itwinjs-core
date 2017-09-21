b2988beb739a53dd0c1eac4ad40debe36aabc19a
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2017-09-20 20:54:29 -0400

    WIP: Temporarily disabling CSS extraction in prod build.
    This is causing our stylesheets to be applied before those loaded by
    required modules, which is inconsistent with the dev build.

--- tools/webpack/config/webpack.config.prod.js
+++ tools/webpack/config/webpack.config.prod.js
@@ -359,7 +359,7 @@ module.exports = {
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
-      filename: cssFilename,
+      filename: cssFilename, disable: true // WIP: ExtractTextPlugin causes some of our CSS to be loaded too early... 
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
