6dc8d93ad5da478693112bd12ea7627c0f1897aa
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2017-10-09 08:56:21 -0400

    Add spec test files.
    The spec test assets are for testing the Json Schemas and an initial set of data to use for testing the core.

---
+++ spectests/utils.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+*--------------------------------------------------------------------------------------------*/
+
+export function getAssetsDir(): string {
+  return __dirname + "/../assets/";
+}
+
+export function getSpecDir(): string {
+  return "D:/dev/EC-Spec/specs/ECJson/jsonSchema/";
+}
