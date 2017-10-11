58cdc4db1794e41ef01d6d6ddb580be423601ec9
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2017-10-11 11:12:05 -0400

    Add "gulp grep" task to check for frontend/backend mismatches

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,27 @@
 
 const gulp = require("gulp");
 const bentleyTools = require("@bentley/bentleyjs-tools");
+const grepFail = require("gulp-grep-fail");
 
 // initialize and add the default Bentley gulp tasks
 bentleyTools.init(gulp);
+
+// defines "gulp grep" task
+gulp.task("grep", ["grepCommon", "grepFrontend", "grepMiddle", "grepBackend"], () => {
+});
+
+gulp.task("grepCommon", () => {
+  return gulp.src("source/*.ts").pipe(grepFail(["/frontend/", "/middle/", "/backend/"]));
+});
+
+gulp.task("grepFrontend", () => {
+  return gulp.src("source/frontend/**/*.ts").pipe(grepFail(["/backend/"]));
+});
+
+gulp.task("grepMiddle", () => {
+  return gulp.src("source/middle/**/*.ts").pipe(grepFail(["/frontend/"]));
+});
+
+gulp.task("grepBackend", () => {
+  return gulp.src("source/backend/**/*.ts").pipe(grepFail(["/frontend/"]));
+});
