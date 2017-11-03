3e02ad7e6cbf0c379181a9bd4c4df3396a7b7281
Steve Wilson <69857376+swbsi@users.noreply.github.com>
2017-11-03 15:34:53 -0400

    Initial Gateway refactor -- no http protocol yet.

--- source/backend/IModelGatewayImpl.ts
+++ source/backend/IModelGatewayImpl.ts
@@ -2,7 +2,6 @@
 |  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
  *--------------------------------------------------------------------------------------------*/
 import { OpenMode } from "@bentley/bentleyjs-core/lib/BeSQLite";
-import { MultiTierExecutionHost, RunsIn, Tier } from "@bentley/bentleyjs-core/lib/tiering";
 import { AccessToken } from "@bentley/imodeljs-clients";
 import { EntityQueryParams } from "../common/EntityProps";
 import { IModelError } from "../common/IModelError";
@@ -14,50 +13,36 @@ import { EntityMetaData } from "../backend/Entity";
 import { ECSqlStatement } from "../backend/ECSqlStatement";
 import { IModelDb } from "../backend/IModelDb";
 import { Model } from "../backend/Model";
+import { Gateway } from "../common/Gateway";
+import { IModelGateway } from "../gateway/IModelGateway";
 
-export type ConstructorType = new () => any;
-
-/** The interface that defines how the frontend remotely talks to the backend.
- * Note that only static methods can be remoted.
+/**
  * @hidden
  */
-@MultiTierExecutionHost("@bentley/imodeljs-core/IModelDbRemoting")
-export class IModelDbRemoting {
-  private constructor() { }
+export class IModelGatewayImpl extends IModelGateway {
+  public static register() {
+    Gateway.registerImplementation(IModelGateway, IModelGatewayImpl);
+  }
 
-  /** Opens an IModelDb on the backend to service frontend requests. */
-  @RunsIn(Tier.Services)
-  public static async open(accessToken: AccessToken, iModelId: string, openMode: OpenMode = OpenMode.ReadWrite, version: IModelVersion): Promise<IModelToken> {
+  public async open(accessToken: AccessToken, iModelId: string, openMode: OpenMode, version: IModelVersion): Promise<IModelToken> {
     const iModelDb: IModelDb = await IModelDb.open(AccessToken.clone(accessToken), iModelId, openMode, IModelVersion.clone(version));
     return iModelDb.iModelToken;
   }
 
-  /** Closes an IModelDb on the backend. */
-  @RunsIn(Tier.Services)
-  public static async close(accessToken: AccessToken, iModelToken: IModelToken): Promise<boolean> {
+  public async close(accessToken: AccessToken, iModelToken: IModelToken): Promise<boolean> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     await iModelDb.close(AccessToken.clone(accessToken));
     return true; // NEEDS_WORK: Promise<void> seems to crash the transport layer.
   }
 
-  /** Execute a query against the iModel.
-   * @param iModelToken The token which identifies the iModel.
-   * @param sql The ECSql to execute
-   * @param bindings Optional values to bind to placeholders in the statement.
-   * @returns All rows as an array or an empty array if nothing was selected
-   * @throws [[IModelError]] if the ECSql is invalid
-   */
-  @RunsIn(Tier.Services)
-  public static async executeQuery(iModelToken: IModelToken, sql: string, bindings?: any): Promise<any[]> {
+  public async executeQuery(iModelToken: IModelToken, sql: string, bindings?: any): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     const rows: any[] = await iModelDb.executeQuery(sql, bindings);
     Logger.logInfo("IModelDbRemoting.executeQuery", () => ({ sql, numRows: rows.length }));
     return rows;
   }
 
-  /** Return an array of model JSON strings given an array of stringified model ids. */
-  @RunsIn(Tier.Services)
-  public static async getModels(iModelToken: IModelToken, modelIds: string[]): Promise<any[]> {
+  public async getModels(iModelToken: IModelToken, modelIds: string[]): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     const models: Model[] = [];
     for (const modelId of modelIds) {
@@ -70,9 +55,7 @@ export class IModelDbRemoting {
     return models;
   }
 
-  /** Return an array of element JSON strings given an array of stringified element ids. */
-  @RunsIn(Tier.Services)
-  public static async getElements(iModelToken: IModelToken, elementIds: string[]): Promise<any[]> {
+  public async getElements(iModelToken: IModelToken, elementIds: string[]): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     const elements: Element[] = [];
     for (const elementId of elementIds) {
@@ -85,9 +68,7 @@ export class IModelDbRemoting {
     return elements;
   }
 
-  /** Return an array of element id strings from a query constructed from the specified parameters. */
-  @RunsIn(Tier.Services)
-  public static async queryElementIds(iModelToken: IModelToken, params: EntityQueryParams): Promise<string[]> {
+  public async queryElementIds(iModelToken: IModelToken, params: EntityQueryParams): Promise<string[]> {
     let sql: string = "SELECT ECInstanceId AS id FROM " + params.from;
     if (params.where) sql += " WHERE " + params.where;
     if (params.orderBy) sql += " ORDER BY " + params.orderBy;
@@ -105,9 +86,7 @@ export class IModelDbRemoting {
     return elementIds;
   }
 
-  /** Return an array of elements formatted for presentation given an array of stringified element ids. */
-  @RunsIn(Tier.Services)
-  public static async formatElements(iModelToken: IModelToken, elementIds: string[]): Promise<any[]> {
+  public async formatElements(iModelToken: IModelToken, elementIds: string[]): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     const formatArray: any[] = [];
     for (const elementId of elementIds) {
@@ -117,11 +96,7 @@ export class IModelDbRemoting {
     return formatArray;
   }
 
-  /** Returns an array of class entries given a starting class and walking up the inheritance chain.
-   * Each entry contains the class name and the class meta data.
-   */
-  @RunsIn(Tier.Services)
-  public static async loadMetaDataForClassHierarchy(iModelToken: IModelToken, startClassName: string): Promise<any[]> {
+  public async loadMetaDataForClassHierarchy(iModelToken: IModelToken, startClassName: string): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     let classFullName: string = startClassName;
     const classArray: any[] = [];
@@ -136,9 +111,7 @@ export class IModelDbRemoting {
     return classArray;
   }
 
-  /** Returns an array with an entry per CodeSpec in the iModel. */
-  @RunsIn(Tier.Services)
-  public static async getAllCodeSpecs(iModelToken: IModelToken): Promise<any[]> {
+  public async getAllCodeSpecs(iModelToken: IModelToken): Promise<any[]> {
     const iModelDb: IModelDb = IModelDb.find(iModelToken);
     const statement: ECSqlStatement = iModelDb.getPreparedStatement("SELECT ECInstanceId AS id, name, jsonProperties FROM BisCore.CodeSpec");
     const codeSpecs: any[] = [];
