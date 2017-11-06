c6db02845c72eed34306c913933fa7a6c612f9f2
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2017-11-06 11:18:14 -0500

    Throw IModelError instead of Error

--- source/backend/Schema.ts
+++ source/backend/Schema.ts
@@ -1,6 +1,8 @@
 /*---------------------------------------------------------------------------------------------
 |  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
 *--------------------------------------------------------------------------------------------*/
+import { IModelError, IModelStatus } from "../common/IModelError";
+import { Logger } from "../common/Logger";
 import { ClassRegistry } from "./ClassRegistry";
 import { EntityCtor } from "./Entity";
 import { IModelDb } from "./IModelDb";
@@ -24,17 +26,19 @@ export class Schema {
 export class Schemas {
   private static _registeredSchemas: { [key: string]: Schema; } = {};
 
-  /** Register a schema prior to using it.  */
+  /** Register a schema prior to using it.
+   * @throws [[IModelError]] if a schema of the same name is already registered.
+   */
   public static registerSchema(schema: Schema) {
     const key = schema.name.toLowerCase();
     if (Schemas.getRegisteredSchema(key))
-      throw new Error("schema " + key + " is already registered");
+      throw new IModelError(IModelStatus.DuplicateName, "Schema \"" + key + "\" is already registered", Logger.logWarning);
     Schemas._registeredSchemas[key] = schema;
   }
 
   /** Look up a previously registered schema
    * @param schemaName The name of the schema
-   * @returns the previously registered schema or undefined if not registered.
+   * @returns the previously registered schema or `undefined` if not registered.
    */
   public static getRegisteredSchema(schemaName: string): Schema | undefined {
     return Schemas._registeredSchemas[schemaName.toLowerCase()];
