1f1e2d57302377eba4b4ceaf3a9f67215ba00c07
Steve Wilson <69857376+swbsi@users.noreply.github.com>
2017-11-29 15:18:15 -0500

    WIP testbed

---
+++ source/testbed/frontend/index.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2017 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+// tslint:disable-next-line:no-var-requires
+require("electron").remote.require("../../../lib/backend/index");
+import { assert } from "chai";
+
+describe("Hello World", () => it("should be true", () => {
+  debugger;
+  assert(true);
+}));
