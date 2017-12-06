449d374d2dc8a25077171c44199a9031e85bf274
Steve Wilson <69857376+swbsi@users.noreply.github.com>
2017-12-06 16:57:43 -0500

    WIP Gateway...fixing instanceof bug

--- source/testbed/frontend/index.ts
+++ source/testbed/frontend/index.ts
@@ -5,6 +5,8 @@
 import { IModelGateway } from "@build/imodeljs-core/lib/gateway/IModelGateway";
 import { BentleyCloudGatewayConfiguration } from "@build/imodeljs-core/lib/gateway/BentleyCloudGatewayConfiguration";
 import { TestbedConfig } from "../common/TestbedConfig";
+import { assert } from "chai";
+import { TestData } from "./TestData";
 import { TestGateway } from "../common/TestGateway";
 
 const gatewaysConfig = BentleyCloudGatewayConfiguration.initialize(TestbedConfig.gatewayParams, [IModelGateway, TestGateway]);
@@ -13,6 +15,27 @@ gatewaysConfig.protocol.openAPIPathPrefix = () => `http://localhost:${TestbedCon
 const remote = require("electron").remote;
 remote.getCurrentWindow().setTitle(TestbedConfig.gatewayParams.info.title);
 remote.require("../../../backend/lib/backend/index");
+
+describe("Testbed", () => {
+  it("Server should be accessible", (done) => {
+    const info = TestbedConfig.gatewayParams.info;
+
+    const req = new XMLHttpRequest();
+    req.open("GET", `http://localhost:${TestbedConfig.serverPort}${TestbedConfig.swaggerURI}`);
+    req.addEventListener("load", () => {
+      assert.equal(200, req.status);
+      const desc = JSON.parse(req.responseText);
+      assert(desc.info.title === info.title && desc.info.version === info.version);
+      done();
+    });
+
+    req.send();
+  });
+
+  it("TestData should load", async () => {
+    await TestData.load();
+  }).timeout(99999);
+});
 
 const fs = remote.require("fs");
 for (const entry of fs.readdirSync(__dirname)) {
