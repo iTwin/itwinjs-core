0dbb658d012154e06e329cafb23b699e7125faba
Steve Wilson <69857376+swbsi@users.noreply.github.com>
2017-12-07 10:20:58 -0500

    Cleanup.

--- source/testbed/lib/config.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const gatewayParams: {
-    info: {
-        title: string;
-        version: string;
-    };
-};
-export declare const serverPort: any;
-export declare const swaggerURI = "/v3/swagger.json";
