f2c3bddc10e157311fb768286361b3eed36d7307
Steve Wilson <69857376+swbsi@users.noreply.github.com>
2017-12-12 15:38:39 -0500

    Added protocol declaration to BentleyCloudGatewayConfiguration. Demonstrate shared gateway config in testbed.

--- source/testbed/backend/index.ts
+++ source/testbed/backend/index.ts
@@ -4,16 +4,14 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { IModelDb } from "@build/imodeljs-core/lib/backend/IModelDb";
-import { IModelGateway } from "@build/imodeljs-core/lib/gateway/IModelGateway";
-import { BentleyCloudGatewayConfiguration } from "@build/imodeljs-core/lib/gateway/BentleyCloudGatewayConfiguration";
 import { NodeAddonRegistry, NodeAddonPackageName } from "@build/imodeljs-core/lib/backend/NodeAddonRegistry";
 import { TestbedConfig } from "../common/TestbedConfig";
-import { TestGateway } from "../common/TestGateway";
+
 import { TestGatewayImpl } from "./TestGatewayImpl";
 
 IModelDb; // Signal usage of IModelDb to tsc import logic
 TestGatewayImpl.register();
-const gatewaysConfig = BentleyCloudGatewayConfiguration.initialize(TestbedConfig.gatewayParams, [IModelGateway, TestGateway]);
+TestbedConfig.initializeGatewayConfig();
 
 // tslint:disable-next-line:no-var-requires
 const addon = require(NodeAddonPackageName.computeDefaultImodelNodeAddonName());
@@ -21,6 +19,6 @@ NodeAddonRegistry.registerAddon(addon);
 
 const app = express();
 app.use(bodyParser.text());
-app.get(TestbedConfig.swaggerURI, (req, res) => gatewaysConfig.protocol.handleOpenApiDescriptionRequest(req, res));
-app.post("*", async (req, res) => gatewaysConfig.protocol.handleOperationPostRequest(req, res));
+app.get(TestbedConfig.swaggerURI, (req, res) => TestbedConfig.gatewayConfig.protocol.handleOpenApiDescriptionRequest(req, res));
+app.post("*", async (req, res) => TestbedConfig.gatewayConfig.protocol.handleOperationPostRequest(req, res));
 app.listen(TestbedConfig.serverPort);
