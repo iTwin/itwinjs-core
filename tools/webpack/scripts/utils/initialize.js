cca349666543ce8698237680cfa4fe49917ba36b
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2017-12-15 10:42:02 -0500

    Improve support for the "TF_BUILD" environment variable.

--- tools/webpack/scripts/utils/initialize.js
+++ tools/webpack/scripts/utils/initialize.js
@@ -10,7 +10,12 @@ function init(NODE_ENV, MOCHA_ENV=undefined) {
   if (MOCHA_ENV)
     process.env.MOCHA_ENV = MOCHA_ENV;
 
-  global.CONTINUOUS_INTEGRATION = (process.env.CI || process.env.TF_BUILD);
+  // Set the "CI" environment variable if only "TF_BUILD" is set.
+  // We want these to behave exactly the same, but some tools are only looking for "CI".
+  if (process.env.TF_BUILD && !process.env.CI)
+    process.env.CI = "true";
+
+  global.CONTINUOUS_INTEGRATION = process.env.CI;
 
   // Makes the script crash on unhandled rejections instead of silently
   // ignoring them. In the future, promise rejections that are not handled will
