bc28ea110bf6af36dd68a2e45cb532ac502bec20
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2017-12-19 15:00:15 -0500

    Fix CORS proxy setup

--- tools/webpack/config/env.js
+++ tools/webpack/config/env.js
@@ -72,6 +72,7 @@ function getClientEnvironment(publicUrl) {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
+        CORS_PROXY_PORT: process.env.CORS_PROXY_PORT,
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
