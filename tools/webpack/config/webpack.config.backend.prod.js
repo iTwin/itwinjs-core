0222162f556df224474867feddcbc4f6920c084c
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2018-01-30 20:05:43 -0500

    Fix minification breaking gateway marshalling.

--- tools/webpack/config/webpack.config.backend.prod.js
+++ tools/webpack/config/webpack.config.backend.prod.js
@@ -46,6 +46,8 @@ module.exports = helpers.mergeWebpackConfigs(baseConfiguration, {
           keep_classnames: true,
         },
         compress: {
+          // Compressing classnames also breaks reflection
+          keep_classnames: true,
           warnings: false,
           // Disabled because of an issue with Uglify breaking seemingly valid code:
           // https://github.com/facebookincubator/create-react-app/issues/2376
