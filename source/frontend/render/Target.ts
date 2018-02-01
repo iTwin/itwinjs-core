21c073b89a96d2a9872c70407894c5c1e1c4b8ee
Marc Neely <36053767+MarcNeely@users.noreply.github.com>
2018-02-01 16:32:55 -0600

    Added const enums for all of GLES directory.

---
+++ source/frontend/render/Target.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+export const enum FrustumUniformType {
+  TwoDee,
+  Orthographic,
+  Perspective,
+}
+export const enum FrustumUniformPlane {
+  kTop,
+  kBottom,
+  kLeft,
+  kRight,
+  kNear,
+  kFar,
+  kType,
+  kCOUNT,
+}
