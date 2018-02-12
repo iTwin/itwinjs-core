b91b084560a2396c15ab699c38f4961a3cc6c026
Marc Neely <36053767+MarcNeely@users.noreply.github.com>
2018-02-12 12:16:58 -0600

    Moved FeatureINdexType to prepare for implementing FeatureIndex

---
+++ source/frontend/render/FeatureIndex.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+
+export const enum FeatureIndexType {
+  kEmpty,
+  kUniform,
+  kNonUniform,
+}
