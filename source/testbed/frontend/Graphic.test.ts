d155bacca45accba9c24bed78c07fc8a833be5f7
Marc Neely <36053767+MarcNeely@users.noreply.github.com>
2018-02-12 10:56:38 -0600

    Finished implementing tests for PolylineParamVertex

--- source/testbed/frontend/Graphic.test.ts
+++ source/testbed/frontend/Graphic.test.ts
@@ -3,7 +3,7 @@
  *--------------------------------------------------------------------------------------------*/
 import { assert } from "chai";
 import { Point3d } from "@bentley/geometry-core/lib/PointVector";
-import { PolylineParamVertex } from "../../frontend/render/Graphic";
+import { PolylineParamVertex, PolylineParam } from "../../frontend/render/Graphic";
 
 function withinTol(x: number, y: number): boolean {
   return Math.abs(x - y) < 0.0000000000001;
@@ -11,12 +11,177 @@ function withinTol(x: number, y: number): boolean {
 
 describe("Graphic", () => {
   it("PolilineParamVertex works as expected", () => {
-    const points: Point3d[] = [Point3d.create (0, 0, 0), Point3d.create (1, 0, 0), Point3d.create (1, 1, 0), Point3d.create (2, 2, 0)];
+    const points: Point3d[] = [Point3d.create (0, 0, 0), Point3d.create (1, 0, 0), Point3d.create (1, 1, 0), Point3d.create (1, 2, 0)];
     let ppv = new PolylineParamVertex (true, true, points[1], points[0], points[2], 0xffffff, 0, 1.0);
     let dot = ppv.DotProduct();
     assert.isTrue (0.0 === dot, "DotProduct of vertex at point 1 should be 0");
+
+    ppv = new PolylineParamVertex (true, true, points[2], points[1], points[0], 0xffffff, 0, 1.0);
+    dot = ppv.DotProduct();
+    assert.isTrue (withinTol(0.7071067811865475, dot), "DotProduct of vertex at point 2 should be 0.7071067811865475 but was " + dot);
+
     ppv = new PolylineParamVertex (true, true, points[2], points[1], points[3], 0xffffff, 0, 1.0);
     dot = ppv.DotProduct();
-    assert.isTrue (withinTol(-0.7071067811865475, dot), "DotProduct of vertex at point 2 should be -0.7071067811865475 but was " + dot);
+    assert.isTrue (withinTol(-1.0, dot), "DotProduct of vertex at point 2 should be -1.0 but was " + dot);
+
+    let p1: PolylineParam = ppv.GetParam (true, true, true, true);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (true, true, true, false);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (true, false, true, true);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (true, false, true, false);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (false, true, true, true);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (false, true, true, false);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (false, false, true, true);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+    p1 = ppv.GetParam (false, false, true, false);
+    assert.isTrue (PolylineParam.kJointBase === p1, "GetParam should always return kJointBase if joint is true");
+
+    p1 = ppv.GetParam (false, false, false, false);
+    assert.isTrue (PolylineParam.kSquare === p1,
+      "GetParam(false,false,false,false) should return kSquare if isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, false);
+    assert.isTrue (PolylineParam.kSquare + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,false) should return kSquare + PolylineParam.kNegatePerp if isSegmentStart and isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt if isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + PolylineParam.kNegatePerp if isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt if isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + PolylineParam.kNegatePerp if isSegmentStart and isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly if isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + PolylineParam.kNegatePerp if isSegmentStart and isPolylineStartOrEnd");
+
+    ppv = new PolylineParamVertex (true, false, points[2], points[1], points[3], 0xffffff, 0, 1.0);
+
+    p1 = ppv.GetParam (false, false, false, false);
+    assert.isTrue (PolylineParam.kMiter === p1,
+      "GetParam(false,false,false,false) should return kMiter if isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, false);
+    assert.isTrue (PolylineParam.kMiter + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,false) should return kMiter + PolylineParam.kNegatePerp if isSegmentStart and !isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt if isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + PolylineParam.kNegatePerp if isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt if isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + PolylineParam.kNegatePerp if isSegmentStart and !isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly if isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + PolylineParam.kNegatePerp if isSegmentStart and !isPolylineStartOrEnd");
+
+    ppv = new PolylineParamVertex (false, true, points[2], points[1], points[3], 0xffffff, 0, 1.0);
+
+    p1 = ppv.GetParam (false, false, false, false);
+    assert.isTrue (PolylineParam.kSquare + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,false) should return kSquare + kNegateAlong if !isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, false);
+    assert.isTrue (PolylineParam.kSquare + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,false) should return kSquare + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and isPolylineStartOrEnd");
+
+    p1 = ppv.GetParam (false, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong if !isSegmentStart and isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and isPolylineStartOrEnd");
+
+    ppv = new PolylineParamVertex (false, false, points[2], points[1], points[3], 0xffffff, 0, 1.0);
+
+    p1 = ppv.GetParam (false, false, false, false);
+    assert.isTrue (PolylineParam.kMiter + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,false) should return kMiter + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, false);
+    assert.isTrue (PolylineParam.kMiter + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,false) should return kMiter + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, false, false, true);
+
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, false);
+
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+
+    ppv = new PolylineParamVertex (false, false, points[2], points[1], points[3], 0xffffff, 0, 1.0);
+
+    p1 = ppv.GetParam (false, false, false, false);
+    assert.isTrue (PolylineParam.kMiter + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,false) should return kMiter + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, false);
+    assert.isTrue (PolylineParam.kMiter + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,false) should return kMiter + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, false, false, true);
+
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, false, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,false,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, true);
+    assert.isTrue (PolylineParam.kNoneAdjWt + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,true) should return kNoneAdjWt + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (false, true, false, false);
+    
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong if !isSegmentStart and !isPolylineStartOrEnd");
+    p1 = ppv.GetParam (true, true, false, false);
+    assert.isTrue (PolylineParam.kMiterInsideOnly + PolylineParam.kNegateAlong + PolylineParam.kNegatePerp === p1,
+      "GetParam(false,true,false,false) should return kMiterInsideOnly + kNegateAlong + PolylineParam.kNegatePerp if !isSegmentStart and !isPolylineStartOrEnd");
   });
 });
