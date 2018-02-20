ee709146a3cdf20f52a50747e7bab548b69dbb8e
Jason Crow <jason-crow@users.noreply.github.com>
2018-02-20 08:34:22 -0600

    remove .only clause in test

--- source/testbed/frontend/GraphicBuilder.test.ts
+++ source/testbed/frontend/GraphicBuilder.test.ts
@@ -5,7 +5,7 @@ import { assert } from "chai";
 import { GraphicBuilderTileCorners } from "../../frontend/render/GraphicBuilder";
 import { Point3d } from "@bentley/geometry-core/lib/PointVector";
 
-describe.only("GraphicBuilderTileCorners", () => {
+describe("GraphicBuilderTileCorners", () => {
   it("works as expected", () => {
     const corners = [ new Point3d(0, 1, 2), new Point3d(3, 4, 5), new Point3d(6, 7, 8), new Point3d(9, 10, 11) ];
     const tileCorners = new GraphicBuilderTileCorners([ new Point3d(0, 1, 2), new Point3d(3, 4, 5), new Point3d(6, 7, 8), new Point3d(9, 10, 11) ]);
