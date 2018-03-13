b3fd6c4b2e416760573b38e337ccf8c3bd3c6985
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-03-13 16:26:14 -0400

    we now support struct array properties. Add these to unit tests.

--- source/backend/test/IModel.test.ts
+++ source/backend/test/IModel.test.ts
@@ -398,21 +398,7 @@ describe("iModel", () => {
     assert.notEqual(rows[0].id, "");
   });
 
-  /* TBD
-  it("should load struct properties", () => {
-    const el1 = imodel3.elements.getElement(new Id64("0x14"));
-    assert.isDefined(el1);
-    // *** TODO: Check that struct property was loaded
-  });
-
-  it("should load array properties", () => {
-    const el1 = imodel3.elements.getElement(new Id64("0x14"));
-    assert.isDefined(el1);
-    // *** TODO: Check that array property was loaded
-  });
-  */
-
-  it("should insert and update auto-handled properties", () => {
+  it.only("should insert and update auto-handled properties", () => {
     const testElem = imodel4.elements.getElement(new Id64("0x14"));
     assert.isDefined(testElem);
     assert.equal(testElem.classFullName, "DgnPlatformTest:TestElementWithNoHandler");
@@ -425,10 +411,10 @@ describe("iModel", () => {
 
     const loc1 = { street: "Elm Street", city: { name: "Downingtown", state: "PA" } };
     const loc2 = { street: "Oak Street", city: { name: "Downingtown", state: "PA" } };
-    // TODO: struct arrays   const loc3 = {street: "Chestnut Street", city: {name: "Philadelphia", state: "PA"}};
-    // TODO: struct arrays    const arrayOfStructs = [loc2, loc3];
+    const loc3 = {street: "Chestnut Street", city: {name: "Philadelphia", state: "PA"}};
+    const arrayOfStructs = [loc2, loc3];
     newTestElem.location = loc1;
-    // TODO: struct arrays    newTestElem.arrayOfStructs = arrayOfStructs;
+    newTestElem.arrayOfStructs = arrayOfStructs;
     newTestElem.dtUtc = new Date("2015-03-25");
     newTestElem.p3d = new Point3d(1, 2, 3);
 
@@ -444,8 +430,9 @@ describe("iModel", () => {
     assert.equal(newTestElemFetched.integerProperty1, newTestElem.integerProperty1);
     assert.isTrue(newTestElemFetched.arrayOfPoint3d[0].isAlmostEqual(newTestElem.arrayOfPoint3d[0]));
     assert.deepEqual(newTestElemFetched.location, loc1);
-    // TODO: struct arrays   assert.deepEqual(newTestElem.arrayOfStructs, arrayOfStructs);
+    assert.deepEqual(newTestElem.arrayOfStructs, arrayOfStructs);
     // TODO: getElement must convert date ISO string to Date object    assert.deepEqual(newTestElemFetched.dtUtc, newTestElem.dtUtc);
+    assert.deepEqual(newTestElemFetched.dtUtc, newTestElem.dtUtc.toJSON());
     assert.isTrue(newTestElemFetched.p3d.isAlmostEqual(newTestElem.p3d));
 
     // ----------- updates ----------------
