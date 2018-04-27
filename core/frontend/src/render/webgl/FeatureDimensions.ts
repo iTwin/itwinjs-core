dc8356ab652424e02bf95291050e285e7644faf4
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2018-04-27 15:42:49 -0400

    TFS#885466: Port logic for building vertex data lookup tables. Update LUTDimensions for recent changes on native side.

--- core/frontend/src/render/webgl/FeatureDimensions.ts
+++ core/frontend/src/render/webgl/FeatureDimensions.ts
@@ -3,6 +3,7 @@
  *--------------------------------------------------------------------------------------------*/
 import { assert } from "@bentley/bentleyjs-core";
 import { FeatureIndexType } from "@bentley/imodeljs-common";
+import { System } from "./System";
 
 /** Describes the dimensionality of a texture used as a look-up table. */
 export const enum LUTDimension {
@@ -10,23 +11,14 @@ export const enum LUTDimension {
   NonUniform, // 1- or 2-dimensional lookup table
 }
 
-/**
- * Describes the dimensions of a texture used as a look-up table meeting the following constraints:
- *    - Dimensions < max texture size
- *        - Dimensions < max texture size
- *        - in practice expect at least 2048 and most tablets/phones at least 4096 (96.3% of all browsers according to webglstats.com)
- *    - Roughly square to reduce unused bytes at end of last row
- *    - No extra unused bytes at end of each row
- */
+/** Describes the dimensions of a texture used as a look-up table */
 export class LUTDimensions {
   public readonly width: number;
   public readonly height: number;
 
-  public constructor(nEntries: number, nRgbaPerEntry: number, maxSize: number) {
-    const smaxSize = 8192;
-    const nRgba = nEntries * nRgbaPerEntry;
-    if (maxSize > smaxSize)
-      maxSize = smaxSize;
+  public constructor(nEntries: number, nRgbaPerEntry: number, nExtraRgba: number = 0, nTables: number = 1) {
+    const maxSize = System.instance.capabilities.maxTextureSize;
+    const nRgba = nEntries * nRgbaPerEntry * nTables + nExtraRgba;
 
     if (nRgba < maxSize) {
       this.width = nRgba;
@@ -39,12 +31,15 @@ export class LUTDimensions {
 
     // Ensure a given entry's RGBA values all fit on the same row.
     const remainder = width % nRgbaPerEntry;
-    width += nRgbaPerEntry - remainder;
+    if (0 !== remainder) {
+      width += nRgbaPerEntry - remainder;
+    }
 
     // Compute height
     let height = nRgba / width;
-    if (width * height < nRgba)
+    if (width * height < nRgba) {
       ++height;
+    }
 
     assert(height <= maxSize);
     assert(width <= maxSize);
@@ -53,7 +48,7 @@ export class LUTDimensions {
     // Row padding should never be necessary...
     assert(0 === width % nRgbaPerEntry);
 
-    this.width  = width;
+    this.width = width;
     this.height = height;
   }
 }
