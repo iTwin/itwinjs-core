e67ccf2a708c870021849799828731b39dbb7063
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2018-04-28 09:19:45 -0400

    Tests for building vertex data tables. (TFS#885466)

--- testbed/frontend/VertexLUT.test.ts
+++ testbed/frontend/VertexLUT.test.ts
@@ -4,9 +4,10 @@
 import { expect } from "chai";
 import { WebGLTestContext } from "./WebGLTestContext";
 import { VertexLUT, MeshArgs } from "@bentley/imodeljs-frontend/lib/rendering";
-import { QPoint3dList, QParams3d, QPoint3d, ColorIndex } from "@bentley/imodeljs-common";
+import { QParams2d, QPoint3dList, QParams3d, QPoint3d, ColorIndex, FeatureIndexType } from "@bentley/imodeljs-common";
+import { Point2d } from "@bentley/geometry-core";
 
-function expectLUTParams(builder: VertexLUT.Builder, colorIndex: ColorIndex, vertexBytes: number[][]) {
+function expectLUTParams(builder: VertexLUT.Builder, colorIndex: ColorIndex, vertexBytes: number[][], expectedColors?: number[]) {
   const params = new VertexLUT.Params(builder, colorIndex);
   const data = params.data;
   let dataIndex = 0;
@@ -16,7 +17,15 @@ function expectLUTParams(builder: VertexLUT.Builder, colorIndex: ColorIndex, ver
     }
   }
 
-  // ###TODO compare appended color table bytes...
+  // Compare appended color table
+  expect(undefined === colorIndex.nonUniform).to.equal(undefined === expectedColors);
+  if (undefined !== expectedColors) {
+    expect(expectedColors.length).to.equal(colorIndex.nonUniform!.colors.length * 4);
+
+    for (const color of expectedColors) {
+      expect(data[dataIndex++]).to.equal(color);
+    }
+  }
 }
 
 function testVertexLUTParams() {
@@ -26,6 +35,7 @@ function testVertexLUTParams() {
   positions.push(QPoint3d.fromScalars(0x7fff, 0xf00d, 0xc001));
   positions.push(QPoint3d.fromScalars(0xbaad, 0, 0xffff));
 
+  // Test simple unlit vertices (no normals or uv params; uniform colors + features)
   const args = new MeshArgs();
   args.points = positions.toTypedArray();
   args.pointParams = positions.params;
@@ -49,6 +59,58 @@ function testVertexLUTParams() {
   ];
 
   expectLUTParams(new VertexLUT.MeshBuilder(args), args.colors, expected);
+
+  // Add uv params
+  args.textureUv.push(new Point2d(-1, 1));  // 0, 0xffff
+  args.textureUv.push(new Point2d(1, 0));   // 0xffff, 0x8000
+  args.textureUv.push(new Point2d(0, -1));  // 0x8000, 0
+
+  expected[0].push(0x00); expected[0].push(0x00); expected[0].push(0xff); expected[0].push(0xff);
+  expected[1].push(0xff); expected[1].push(0xff); expected[1].push(0x00); expected[1].push(0x80);
+  expected[2].push(0x00); expected[2].push(0x80); expected[2].push(0x00); expected[2].push(0x00);
+
+  const qUVParams = QParams2d.fromNormalizedRange();
+  expectLUTParams(new VertexLUT.TexturedMeshBuilder(args, qUVParams), args.colors, expected);
+
+  // Add feature IDs
+  args.features.type = FeatureIndexType.NonUniform;
+  args.features.featureIDs = new Uint32Array(3);
+  args.features.featureIDs[0] = 0xbaadf00d;
+  args.features.featureIDs[1] = 0xc001bead;
+  args.features.featureIDs[2] = 0;
+
+  expected[0][8] = 0x0d; expected[0][9] = 0xf0; expected[0][10] = 0xad; expected[0][11] = 0xba;
+  expected[1][8] = 0xad; expected[1][9] = 0xbe; expected[1][10] = 0x01; expected[1][11] = 0xc0;
+
+  expectLUTParams(new VertexLUT.TexturedMeshBuilder(args, qUVParams), args.colors, expected);
+
+  // Remove uv params
+  for (const arr of expected) {
+    arr.splice(12);
+  }
+
+  // Add non-uniform color.
+  const colors = new Uint32Array(3);
+  colors[0] = 0xffeeddcc;
+  colors[1] = 0x00010203;
+  colors[2] = 0x7f00ff00;
+  const colorIndices = new Uint16Array(3);
+  colorIndices[1] = 0x0001;
+  colorIndices[2] = 0xffee;
+  args.colors.initNonUniform(colors, colorIndices, true);
+
+  expected[0][6] = 0x00; expected[0][7] = 0x00;
+  expected[1][6] = 0x01; expected[1][7] = 0x00;
+  expected[2][6] = 0xee; expected[2][7] = 0xff;
+
+  // NB: The color values in VertexLUT.Params have premultiplied alpha, and alpha set to (255 - transparency)
+  const expectedColors = [
+    0x00, 0x00, 0x00, 0x00,
+    0x03, 0x02, 0x01, 0xff,
+    0x00, 0x80, 0x00, 0x80,
+  ];
+
+  expectLUTParams(new VertexLUT.MeshBuilder(args), args.colors, expected, expectedColors);
 }
 
 describe("VertexLUT", () => {
