5d8badf7e9ee27a8f43919b801cf0b5ffd48b49e
Will Bentley <38542573+williamkbentley@users.noreply.github.com>
2018-05-01 10:29:38 -0400

    Script for building doc site locally from imodeljs-core monorepo

---
+++ tools/bemetalsmith/buildDocSite.js
@@ -0,0 +1,92 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+const childProcess = require("child_process");
+const fs = require("fs-extra");
+const path = require("path");
+const argv = require("yargs").argv;
+
+//Three arguments: --noTypeDoc, --noBuild, and --mdOnly
+let runTypeDoc = argv.noTypeDoc === undefined ? true : false;
+let runRushBuild = argv.noBuild === undefined ? true : false;
+
+const runQuick = argv.mdOnly === undefined ? false : true;
+runTypeDoc = runQuick === true ? false : runTypeDoc;
+runRushBuild = runQuick === true ? false : runRushBuild;
+
+if (runTypeDoc) {
+  runDocs();
+}
+if (runRushBuild) {
+  runBuild();
+}
+runStagingCopy();
+runBemetalsmith();
+
+//Rush docs
+function runDocs() {
+  const docsProcess = childProcess.execSync("rush docs", { stdio: [0, 1, 2] });
+}
+
+//Rush rebuild. This ensures the code compiles and does the code snippet extraction.
+function runBuild() {
+  const rushBuildProcess = childProcess.execSync("rush rebuild", {
+    stdio: [0, 1, 2]
+  });
+}
+
+//Copy docs to staging area
+function runStagingCopy() {
+  //Copy md files.
+  const outputDir = path.resolve(process.cwd(), "generated-docs\\staging");
+  const docsDir = path.resolve(process.cwd(), "docs");
+  fs.copySync(docsDir, outputDir);
+
+  //Copy extract to staging area
+  const extractDir = path.resolve(process.cwd(), "generated-docs\\extract");
+  fs.copySync(extractDir, path.join(outputDir, "extract"));
+
+  //Copy reference to staging area
+  const refOutputDir = path.resolve(
+    process.cwd(),
+    "generated-docs\\staging\\reference"
+  );
+  if (runTypeDoc) {
+    const packages = getPackages();
+    const genDocsDir = path.resolve(process.cwd(), "generated-docs");
+
+    packages.forEach(function(pkg) {
+      let packageDir = pkg["projectFolder"].split("/")[1];
+      let packageName = pkg["packageName"].split("/")[1];
+      fs.copySync(
+        path.join(genDocsDir, packageDir, "json"),
+        path.join(refOutputDir, packageName)
+      );
+    });
+  }
+}
+
+function runBemetalsmith() {
+  const source = path.join(process.cwd(), "generated-docs\\staging");
+  const destination = path.join(
+    process.cwd(),
+    "generated-docs\\staging\\public"
+  );
+  const metalsmithProcess = childProcess.execSync(
+    `bmsWatch --source "${source}" --destination "${destination}"`,
+    { stdio: [0, 1, 2] }
+  );
+}
+
+//Get the packages from rush.json
+function getPackages() {
+  var obj = JSON.parse(fs.readFileSync("rush.json", "utf8"));
+  let retProjects = [];
+  var projs = obj["projects"];
+  projs.forEach(function(proj) {
+    if (proj["shouldPublish"] === true) {
+      retProjects.push(proj);
+    }
+  });
+  return retProjects;
+}
