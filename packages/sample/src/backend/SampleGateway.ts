95bddb906bc3328bd80320cbc1cfea19a6fcdda3
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2018-05-08 10:03:05 +0300

    Update imodeljs-core dependencies to 0.80

--- packages/sample/src/backend/SampleGateway.ts
+++ packages/sample/src/backend/SampleGateway.ts
@@ -3,15 +3,21 @@
  *--------------------------------------------------------------------------------------------*/
 import SampleGatewayDefinition from "../common/SampleGatewayDefinition";
 import { Gateway } from "@bentley/imodeljs-common";
-import { KnownLocations } from "@bentley/imodeljs-backend";
+import { IModelHost } from "@bentley/imodeljs-backend";
 import * as fs from "fs";
 import * as path from "path";
 
 /** The backend implementation of SampleGatewayDefinition. */
 export default class SampleGateway extends SampleGatewayDefinition {
 
+  private getAssetsDir(): string {
+    if (IModelHost.appAssetsDir)
+      return IModelHost.appAssetsDir;
+    return "assets";
+  }
+
   public async getSampleImodels(): Promise<string[]> {
-    const dir = path.join(KnownLocations.assetsDir, "sample_documents");
+    const dir = path.join(this.getAssetsDir(), "sample_documents");
     const files = fs.readdirSync(dir);
     return files.map((name: string) => (path.resolve(dir, name)));
   }
