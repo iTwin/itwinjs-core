a77b9c59e031eee8e5766e1dd266afd77220094b
Shaun Sewall <36768600+scsewall@users.noreply.github.com>
2018-05-14 09:59:49 -0400

    Add GeometricElement3d-related code/JSON examples to WireFormat article

--- docs/learning/WireFormat.md
+++ docs/learning/WireFormat.md
@@ -16,6 +16,8 @@ Produces the following result:
 [[include:WireFormat_RootSubject.json]]
 ```
 
+-------------------------------------------------
+
 ## RepositoryModel
 
 The following code:
@@ -30,30 +32,56 @@ Produces the following result:
 [[include:WireFormat_RepositoryModel.json]]
 ```
 
-## Arc3d
+-------------------------------------------------
 
-The following code:
+## GeometricElement3d
 
-```ts
-[[include:WireFormat_Arc3d.code]]
-```
-
-Produces the following result:
-
-```json
-[[include:WireFormat_Arc3d.json]]
-```
-
-## LineString3d
-
-The following code:
+The following code produces JSON at various stages on the way to creating a `GeometricElement3d`:
 
 ```ts
-[[include:WireFormat_LineString3d.code]]
+[[include:WireFormat_GeometricElement3d.code]]
 ```
 
-Produces the following result:
+### Arc3d JSON
+
+Below is the JSON from the `Arc3d`:
 
 ```json
-[[include:WireFormat_LineString3d.json]]
+[[include:WireFormat_GeometricElement3d_Arc.json]]
 ```
+
+### LineString3d JSON
+
+Below is the JSON from the `LineString3d`:
+
+```json
+[[include:WireFormat_GeometricElement3d_LineString.json]]
+```
+
+### GeometryStream JSON
+
+Below is the JSON from the `GeometryStream` that contains the `Arc3d` and `LineString3d` that were previously created.
+This shows that a `GeometryStream` is just an array of entries:
+
+```json
+[[include:WireFormat_GeometricElement3d_GeometryStream.json]]
+```
+
+### Placement3d JSON
+
+Below is the JSON from the `Placement3d` that will be used to place the `GeometryStream` in world coordinates:
+
+```json
+[[include:WireFormat_GeometricElement3d_Placement.json]]
+```
+
+### GeometricElement3d JSON
+
+Below is the JSON from the `GeometricElement3d`.
+This shows how the JSON from the `GeometricElement3d` contains the JSON from the objects used to create it:
+
+```json
+[[include:WireFormat_GeometricElement3d_Element.json]]
+```
+
+-------------------------------------------------
--- example-code/snippets/src/backend/WireFormat.test.ts
+++ example-code/snippets/src/backend/WireFormat.test.ts
@@ -4,9 +4,9 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import { assert } from "chai";
-import { OpenMode } from "@bentley/bentleyjs-core";
-import { Arc3d, Point3d, AngleSweep, Angle, LineString3d } from "@bentley/geometry-core";
-import { ElementProps, IModel, ModelProps } from "@bentley/imodeljs-common";
+import { Id64, OpenMode } from "@bentley/bentleyjs-core";
+import { Arc3d, Point3d, AngleSweep, Angle, LineString3d, YawPitchRollAngles } from "@bentley/geometry-core";
+import { ElementProps, GeometricElement3dProps, GeometryStreamBuilder, IModel, ModelProps, Placement3dProps } from "@bentley/imodeljs-common";
 import { IModelDb } from "@bentley/imodeljs-backend";
 import { IModelTestUtils } from "./IModelTestUtils";
 
@@ -51,34 +51,54 @@ describe("Wire Format Snippets", () => {
     assert.isTrue(fs.existsSync(jsonOutputFileName));
   });
 
-  it("Arc3d", () => {
-    const jsonOutputFileName: string = getJsonOutputFileName("WireFormat_Arc3d");
-    // __PUBLISH_EXTRACT_START__ WireFormat_Arc3d.code
+  it("GeometricElement3d", () => {
+    const modelId = new Id64();
+    const categoryId = new Id64();
+    // __PUBLISH_EXTRACT_START__ WireFormat_GeometricElement3d.code
+    // Construct an Arc3d (local coordinates)
     const center = new Point3d(0, 0, 0);
     const radius = 1;
     const sweep: AngleSweep = AngleSweep.createStartEnd(Angle.createDegrees(90), Angle.createDegrees(180));
     const arc: Arc3d = Arc3d.createXY(center, radius, sweep);
-    const json: string = JSON.stringify(arc, undefined, 2);
-    // __PUBLISH_EXTRACT_END__
-    assert.isDefined(arc);
-    fs.writeFileSync(jsonOutputFileName, json);
-    assert.isTrue(fs.existsSync(jsonOutputFileName));
-  });
+    const arcJson: string = JSON.stringify(arc, undefined, 2);
 
-  it("LineString3d", () => {
-    const jsonOutputFileName: string = getJsonOutputFileName("WireFormat_LineString3d");
-    // __PUBLISH_EXTRACT_START__ WireFormat_LineString3d.code
+    // Construct a LineString3d (local coordinates)
     const points: Point3d[] = [
       new Point3d(0, 0, 0),
       new Point3d(1, 2, 0),
       new Point3d(1, 2, 4),
     ];
     const lineString: LineString3d = LineString3d.createPoints(points);
-    const json: string = JSON.stringify(lineString, undefined, 2);
+    const lineStringJson: string = JSON.stringify(lineString, undefined, 2);
+
+    // Construct a GeometryStream containing the Arc3d and LineString3d created above (local coordinates)
+    const builder = new GeometryStreamBuilder();
+    builder.appendGeometryQuery(arc);
+    builder.appendGeometryQuery(lineString);
+    const geometryStreamJson: string = JSON.stringify(builder.geometryStream, undefined, 2);
+
+    // Construct a Placement (world coordinates)
+    const origin = new Point3d(100, 100, 0);
+    const angles = YawPitchRollAngles.createDegrees(0, 90, 0);
+    const placement: Placement3dProps = { origin, angles };
+    const placementJson: string = JSON.stringify(placement, undefined, 2);
+
+    // Construct a GeometricElement3d using the GeometryStream and Placement created above
+    const elementProps: GeometricElement3dProps = {
+      classFullName: "Generic:PhysicalObject",
+      model: modelId,
+      category: categoryId,
+      placement,
+      geom: builder.geometryStream,
+    };
+    const elementPropsJson: string = JSON.stringify(elementProps, undefined, 2);
+
     // __PUBLISH_EXTRACT_END__
-    assert.isDefined(lineString);
-    fs.writeFileSync(jsonOutputFileName, json);
-    assert.isTrue(fs.existsSync(jsonOutputFileName));
+    fs.writeFileSync(getJsonOutputFileName("WireFormat_GeometricElement3d_Arc"), arcJson);
+    fs.writeFileSync(getJsonOutputFileName("WireFormat_GeometricElement3d_LineString"), lineStringJson);
+    fs.writeFileSync(getJsonOutputFileName("WireFormat_GeometricElement3d_GeometryStream"), geometryStreamJson);
+    fs.writeFileSync(getJsonOutputFileName("WireFormat_GeometricElement3d_Placement"), placementJson);
+    fs.writeFileSync(getJsonOutputFileName("WireFormat_GeometricElement3d_Element"), elementPropsJson);
   });
 
 });
