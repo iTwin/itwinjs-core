15aa2a733f0c855913dcf921cac76b9381a7542f
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-05-30 08:53:37 -0400

    Adjust installnativeplatform.bat to work with tools/native-platform-installer

--- installnativeplatform.bat
+++ installnativeplatform.bat
@@ -2,20 +2,10 @@ REM Installs local builds of the platform-specific native platform packages for 
 
 set iModelJsCoreDir=%~dp0
 
-pushd %OutRoot%Winx64\packages\imodeljs-native-platform-node
-call npm install --no-save  %OutRoot%Winx64\packages\imodeljs-native-platform-api %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64
-
-cd %OutRoot%Winx64\packages\imodeljs-native-platform-electron
-call npm install --no-save  %OutRoot%Winx64\packages\imodeljs-native-platform-api %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64
-
 cd %ImodelJsRoot%
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api         %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-native-platform-api
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-node        %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-native-platform-node
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-node        %iModelJsCoreDir%nativePlatformForTests\node_modules\@bentley\imodeljs-native-platform-node
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-electron    %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-native-platform-electron
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-electron    %iModelJsCoreDir%nativePlatformForTests\node_modules\@bentley\imodeljs-native-platform-electron
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64          %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64          %iModelJsCoreDir%nativePlatformForTests\node_modules\@bentley\imodeljs-native-platform-node\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64       %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64       %iModelJsCoreDir%nativePlatformForTests\node_modules\@bentley\imodeljs-native-platform-electron\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-native-platform-api
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    %iModelJsCoreDir%tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-node\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    %iModelJsCoreDir%tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-electron\node_modules\@bentley\imodeljs-e_2-win32-x64
 popd
