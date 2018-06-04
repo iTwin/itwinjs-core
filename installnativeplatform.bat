6e0d3fa9ef563499227a547f06e7b33d50ce0b3c
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-06-04 11:21:50 -0400

    installnativeplatform.bat was copying to a bogus directory

--- installnativeplatform.bat
+++ installnativeplatform.bat
@@ -6,9 +6,9 @@ if not exist .\tools\native-platform-installer goto :baddir
 
 xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api common\temp\node_modules\@bentley\imodeljs-native-platform-api
 xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-node\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-n_8-win32-x64
 xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-electron\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-e_2-win32-x64
 
 goto :xit
 :baddir
