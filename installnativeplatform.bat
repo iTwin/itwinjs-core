14e0d6714f026be3fa0c0b5b4d890a48233c1325
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-06-04 11:24:25 -0400

    wip installnativeplatform.bat

--- installnativeplatform.bat
+++ installnativeplatform.bat
@@ -1,14 +1,13 @@
-@echo off
-echo Installing local build of the platform-specific native platform packages for use by examples and tests
+REM Install local build of the platform-specific native platform packages for use by examples and tests
 if not exist .\common goto :baddir
 if not exist .\tools goto :baddir
 if not exist .\tools\native-platform-installer goto :baddir
 
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api common\temp\node_modules\@bentley\imodeljs-native-platform-api
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Q /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api common\temp\node_modules\@bentley\imodeljs-native-platform-api
+xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-e_2-win32-x64
 
 goto :xit
 :baddir
