a0a23726b22a433209b7c2942872fd696f95f661
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-06-04 11:07:37 -0400

    Simplify installnativeplatform.bat - there's no need for an envvar or cd/popd.

--- installnativeplatform.bat
+++ installnativeplatform.bat
@@ -1,11 +1,16 @@
-REM Installs local builds of the platform-specific native platform packages for Windows.
+@echo off
+echo Installing local build of the platform-specific native platform packages for use by examples and tests
+if not exist .\common goto :baddir
+if not exist .\tools goto :baddir
+if not exist .\tools\native-platform-installer goto :baddir
 
-set iModelJsCoreDir=%~dp0
+xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api common\temp\node_modules\@bentley\imodeljs-native-platform-api
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-node\node_modules\@bentley\imodeljs-n_8-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
+xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-electron\node_modules\@bentley\imodeljs-e_2-win32-x64
 
-cd %ImodelJsRoot%
-xcopy /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-native-platform-api
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    %iModelJsCoreDir%tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-node\node_modules\@bentley\imodeljs-n_8-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    %iModelJsCoreDir%common\temp\node_modules\@bentley\imodeljs-e_2-win32-x64
-xcopy /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    %iModelJsCoreDir%tools\native-platform-installer\node_modules\@bentley\imodeljs-native-platform-electron\node_modules\@bentley\imodeljs-e_2-win32-x64
-popd
+goto :xit
+:baddir
+echo Change to an imodeljs-core directory before running this script.
+:xit
