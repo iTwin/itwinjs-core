4d00863938b8fd9db6359fcca4fa3072ae73a6c8
Tom Bertrand <38288322+imodeljs-admin@users.noreply.github.com>
2018-06-12 13:22:15 -0400

    Fixed comment that was preventing docSite from building

--- core/frontend/src/render/webgl/debugPrint.ts
+++ core/frontend/src/render/webgl/debugPrint.ts
@@ -3,8 +3,10 @@
  *--------------------------------------------------------------------------------------------*/
 /** @module WebGL */
 
-// This function is intended to be used only when explicit debugging options are enabled;
-// never in production builds. Avoids having to duplicate the tslint:disable-line all over the place.
+/**
+ * This function is intended to be used only when explicit debugging options are enabled;
+ * never in production builds. Avoids having to duplicate the tslint:disable-line all over the place.
+ */
 export function debugPrint(str: string): void {
   console.log(str); // tslint:disable-line:no-console
 }
