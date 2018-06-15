053944c250c7f2bbe709893325b9bc12394f3bc0
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-06-15 15:01:32 -0400

    bank-demo wip

--- bank-demo/src/cli.ts
+++ bank-demo/src/cli.ts
@@ -3,9 +3,8 @@
  *--------------------------------------------------------------------------------------------*/
 import { DemoFrontend } from "./DemoFrontend";
 import { DemoBackend } from "./DemoBackend";
-import { IModelAccessContext } from "@bentley/imodeljs-backend/lib/backend";
 
-const useIModelHub = true;
+const useIModelHub = (process.argv.length === 3) && (process.argv[2] === "1");
 
 // Pretend that we are spinning up the app's own backend
 const backend = new DemoBackend();
@@ -14,8 +13,12 @@ DemoBackend.initialize(useIModelHub);
 // Pretend that this is the app's frontend
 const frontend = new DemoFrontend(useIModelHub);
 
-// Simulate an app, where the user in the frontend logs in, picks a project, and then calls IModelConnection.open
-frontend.login()
-  .then(() => frontend.chooseIModel())
-  .then((iModelId: string) => frontend.getIModelAccessContext(iModelId))
-  .then((accessContext: IModelAccessContext) => backend.downloadBriefcase(accessContext, frontend.accessToken));
+async function runDemo() {
+  await frontend.login();
+  const iModelId = await frontend.chooseIModel();
+  const context = await frontend.getIModelAccessContext(iModelId);
+  await backend.downloadBriefcase(context, frontend.accessToken);
+  // await backend.logChangeSets(context, frontend.accessToken);
+}
+
+runDemo().then(() => process.exit(0));
