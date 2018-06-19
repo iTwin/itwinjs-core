5f48a05fc68f9d9b430cc3a3de8832847e3a2945
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2018-06-19 09:32:44 -0400

    Use TS 2.9 import types to simplify tsconfig and type definition setup.
    Now, rather than specifying an "include" with our index.d.ts,
    projects should just need to add the following to their tsconfigs:
      "compilerOptions": { "types": [ "@bentley/imodeljs-react-scripts" ] }

--- tools/webpack/index.d.ts
+++ tools/webpack/index.d.ts
@@ -1,5 +1,15 @@
+/// <reference types="node" />
 /// <reference types="mocha" />
-/// <reference path="./defaultImports.ts" />
+/// <reference types="chai" />
+/// <reference types="webdriverio" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare var React: typeof import("react");
+declare var sinon: typeof import("sinon");
+declare var expect: Chai.ExpectStatic;
+declare var shallow: typeof import("enzyme").shallow;
+declare var mount: typeof import("enzyme").mount;
 
 declare namespace Polymer {
   type Element = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {class: string, children: any};
