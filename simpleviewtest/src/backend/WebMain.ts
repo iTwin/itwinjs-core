f87f782acf5315aeef363b21779669f8ac7abaf4
Sam.Wilson <69692709+swwilson-bsi@users.noreply.github.com>
2018-07-13 15:31:25 -0400

    SampleViewTest - use https version of devCorsProxyServer

--- simpleviewtest/src/backend/WebMain.ts
+++ simpleviewtest/src/backend/WebMain.ts
@@ -2,10 +2,11 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cp from "child_process";
 
-import { BentleyCloudRpcManager } from "@bentley/imodeljs-common/lib/common";
-import { Config } from "@bentley/imodeljs-clients/lib";
+import { BentleyCloudRpcManager } from "@bentley/imodeljs-common";
+import { Config } from "@bentley/imodeljs-clients";
 import { IModelTileRpcInterface, StandaloneIModelRpcInterface, IModelReadRpcInterface } from "@bentley/imodeljs-common";
 import { IModelHost } from "@bentley/imodeljs-backend";
+import { Logger, LogLevel } from "@bentley/bentleyjs-core";
 
 // tslint:disable:no-console
 
@@ -14,7 +15,7 @@ export function getRpcInterfaces() {
 }
 
 // Start the dev-cors-proxy-server
-const proxyServer = cp.spawn("node", ["./node_modules/@bentley/dev-cors-proxy-server/server.js"]);
+const proxyServer = cp.spawn("node", ["./node_modules/@bentley/dev-cors-proxy-server/server.js", "--serve-over-https"]);
 proxyServer.stdout.on("data", (data) => {
   console.log(`proxy server: ${data}`);
 });
@@ -26,8 +27,11 @@ proxyServer.on("close", (code) => {
 });
 
 // Initialize backend functionality and logging
-Config.devCorsProxyServer = "http://localhost:3001";
+Config.devCorsProxyServer = "https://localhost:3001";
 IModelHost.startup();
+Logger.initializeToConsole();
+Logger.setLevelDefault(LogLevel.Error);
+Logger.setLevel("imodeljs-clients", LogLevel.Trace)
 
 // Set up the ability to serve the supported rpcInterfaces via web requests
 const cloudConfig = BentleyCloudRpcManager.initializeImpl({ info: { title: "SimpleViewApp", version: "v1.0" } }, getRpcInterfaces());
--- simpleviewtest/src/frontend/SimpleViewTest.ts
+++ simpleviewtest/src/frontend/SimpleViewTest.ts
@@ -630,7 +630,7 @@ async function main() {
     rpcConfiguration = ElectronRpcManager.initializeClient({}, [IModelTileRpcInterface, StandaloneIModelRpcInterface, IModelReadRpcInterface]);
   } else {
     rpcConfiguration = BentleyCloudRpcManager.initializeClient({ info: { title: "SimpleViewApp", version: "v1.0" } }, [IModelTileRpcInterface, StandaloneIModelRpcInterface, IModelReadRpcInterface]);
-    Config.devCorsProxyServer = "http://localhost:3001";
+    Config.devCorsProxyServer = "https://localhost:3001";
   }
 
   // WIP: WebAppRpcProtocol seems to require an IModelToken for every RPC request. ECPresentation initialization tries to set active locale using
