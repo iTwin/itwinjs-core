825a9dc6132e8a6d0b0f2a01536d2815772c5338
Ramanujam Raman <32458710+ramanujam-raman@users.noreply.github.com>
2018-07-15 14:06:41 -0400

    Setup a way to bypass native addon version checks when a local build of the native build is installed. This is to allow developers to update package_version.txt, and not update the dependency in imodels-core:backend.
    Note that updating the dependency in imodels-core:backend
    will cause "rush install" to fail, and so that's not
    a viable option. The dependency will be updated
    as part of the merge process from Bim0200Dev -> Bim0200.

--- installnativeplatform.bat
+++ installnativeplatform.bat
@@ -5,6 +5,12 @@ xcopy /Q /Y /I %OutRoot%Winx64\packages\imodeljs-native-platform-api common\temp
 xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-n_8-win32-x64    common\temp\node_modules\@bentley\imodeljs-native-platform-api\lib\@bentley\imodeljs-n_8-win32-x64
 xcopy /Q /Y /I /S %OutRoot%Winx64\packages\imodeljs-e_2-win32-x64    common\temp\node_modules\@bentley\imodeljs-native-platform-api\lib\@bentley\imodeljs-e_2-win32-x64
 
+REM Create a semaphore file that identifies that this is a dev build, and helps bypass version checks
+set libDir=%~dp0%core\backend\lib
+if not exist %libDir% mkdir %libDir%
+echo This is a Development build, and this semaphore helps bypass native version checks > %libDir%\DevBuild.txt
+set libDir=
+
 goto :xit
 :baddir
 echo Change to an imodeljs-core directory before running this script.
