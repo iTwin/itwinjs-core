6dc97ae3cbd35ed3b038cd1b98c3ce46b63b7fb0
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2018-07-16 09:26:04 -0400

    Enable old skybox.

--- core/frontend/src/ViewState.ts
+++ core/frontend/src/ViewState.ts
@@ -1331,7 +1331,7 @@ export abstract class ViewState3d extends ViewState {
 
   // ###TODO: Move this back to SpatialViewState...for some reason we always get OrthographicViewState, which we should rarely if ever encounter...
   public decorate(context: DecorateContext): void {
-    const useOldSkyBox = false;
+    const useOldSkyBox = true;
     if (useOldSkyBox)
       this.drawSkyBox(context);
     else
