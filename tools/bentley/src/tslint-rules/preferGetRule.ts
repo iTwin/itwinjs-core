057b35f49b190ad49f91deed1217b1f1c31157a4
Nate Rex <38288322+imodeljs-admin@users.noreply.github.com>
2018-08-15 11:04:18 -0400

    Add exception to prefer-get rule for type guard returns

--- tools/bentley/src/tslint-rules/preferGetRule.ts
+++ tools/bentley/src/tslint-rules/preferGetRule.ts
@@ -22,7 +22,7 @@ class PreferGetWalker extends Lint.RuleWalker {
   public visitMethodDeclaration(node: ts.MethodDeclaration) {
     if (node.parameters.length !== 0)
       return;
-    if (this.isPrivate(node))
+    if (this.isPrivate(node) || this.returnsTypeGuard(node))
       return;
 
     const name = node.name.getText();
@@ -39,6 +39,15 @@ class PreferGetWalker extends Lint.RuleWalker {
       if (modText === "private")
         return true;
     }
+    return false;
+  }
+
+  private returnsTypeGuard(node: ts.MethodDeclaration): boolean {
+    if (node.type === undefined)
+      return false;
+    const returnText = node.type.getText();
+    if (returnText.slice(0, 8) === "this is ")
+      return true;
     return false;
   }
 
