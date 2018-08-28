af1d6f2c6c2c7d7e0297bb4d0ed300dd396d2615
Allison Macdonald <38288322+imodeljs-admin@users.noreply.github.com>
2018-08-28 19:38:53 +0000

    Merged PR 1319: ActivityMessage added in SampleApp
    Added an ActivityMessage to the statusbar in SampleApp. Tool 3 will activate a sample activity.
    
    Related work items: #28833

---
+++ ui/framework/src/configurableui/statusbarfields/ActivityCenter.tsx
@@ -0,0 +1,92 @@
+/*---------------------------------------------------------------------------------------------
+| $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+/** @module Notification */
+
+import * as React from "react";
+
+import "../configurableui.scss";
+
+import { StatusBarFieldId, IStatusBar } from "../StatusBarWidgetControl";
+
+import Status from "@bentley/ui-ninezone/lib/footer/message/content/status/Status";
+import StatusLayout from "@bentley/ui-ninezone/lib/footer/message/content/status/Layout";
+import Progress from "@bentley/ui-ninezone/lib/footer/message/content/Progress";
+import { MessageManager, ActivityMessageEventArgs } from "../MessageManager";
+
+export interface ActivityCenterProps {
+  statusBar: IStatusBar;
+  isInFooterMode: boolean;
+  openWidget: StatusBarFieldId;
+}
+
+export interface ActivityCenterState {
+  title: string;
+  percentage: number;
+  isActivityMessageVisible: boolean;
+}
+
+/** Activity Center Field React component.
+ */
+export class ActivityCenterField extends React.Component<ActivityCenterProps, ActivityCenterState> {
+  private _element: any;
+
+  constructor(p: ActivityCenterProps) {
+    super(p);
+    this.state = {
+      title: "",
+      percentage: 0,
+      isActivityMessageVisible: true,
+    };
+  }
+
+  public componentDidMount() {
+    MessageManager.ActivityMessageAddedEvent.addListener(this._handleActivityMessageEvent);
+    MessageManager.ActivityMessageCanceledEvent.addListener(this._handleActivityMessageCanceledEvent);
+  }
+
+  public componentWillUnmount() {
+    MessageManager.ActivityMessageAddedEvent.removeListener(this._handleActivityMessageEvent);
+    MessageManager.ActivityMessageCanceledEvent.removeListener(this._handleActivityMessageCanceledEvent);
+  }
+
+  private _handleActivityMessageEvent = (args: ActivityMessageEventArgs) => {
+    this.setState((_prevState) => ({
+      title: args.title,
+      percentage: args.percentage,
+      isActivityMessageVisible: true,
+    }));
+  }
+
+  private _handleActivityMessageCanceledEvent = () => {
+    this.setState((_prevState) => ({
+      isActivityMessageVisible: false,
+    }));
+  }
+
+  private _openActivityMessage = () => {
+    MessageManager.setupActivityMessageValues(this.state.title, this.state.percentage, true);
+    this.setState((_prevState) => ({
+      isActivityMessageVisible: true,
+    }));
+  }
+
+  public render(): React.ReactNode {
+    const footerMessages = (
+      <div className="centered" onClick={this._openActivityMessage}>
+        <StatusLayout
+          progress={
+            <Progress
+              status={Status.Information}
+              progress={this.state.percentage}
+            />
+          }
+        />
+      </div>
+    );
+
+    this.props.statusBar.setFooterMessages(this._element);
+
+    return footerMessages;
+  }
+}
