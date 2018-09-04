7206aae295348a12ead9cf01618e60b7c96c21d9
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2018-09-04 06:24:07 -0400

    PolylineArgs => PolylineParams.

--- core/frontend/src/render/primitives/VertexTable.ts
+++ core/frontend/src/render/primitives/VertexTable.ts
@@ -4,12 +4,13 @@
 /** @module Rendering */
 
 import { assert } from "@bentley/bentleyjs-core";
-import { Range2d, Point2d } from "@bentley/geometry-core";
+import { Range2d, Point2d, Point3d, Vector3d } from "@bentley/geometry-core";
 import {
   ColorDef,
   ColorIndex,
   FeatureIndex,
   QPoint2d,
+  QPoint3dList,
   QParams2d,
   QParams3d,
   FeatureIndexType,
@@ -19,9 +20,10 @@ import {
   LinePixels,
   OctEncodedNormalPair,
   PolylineData,
+  PolylineTypeFlags,
   MeshEdge,
 } from "@bentley/imodeljs-common";
-import { PolylineArgs, MeshArgs, MeshArgsEdges } from "./mesh/MeshPrimitives";
+import { PolylineArgs, MeshArgs } from "./mesh/MeshPrimitives";
 import { IModelApp } from "../../IModelApp";
 
 /**
@@ -247,6 +249,204 @@ export class PointStringParams {
   }
 }
 
+/** Parameter associated with each vertex index of a tesselated polyline. */
+const enum PolylineParam {
+  kNone = 0,
+  kSquare = 1 * 3,
+  kMiter = 2 * 3,
+  kMiterInsideOnly = 3 * 3,
+  kJointBase = 4 * 3,
+  kNegatePerp = 8 * 3,
+  kNegateAlong = 16 * 3,
+  kNoneAdjustWeight = 32 * 3,
+}
+
+/**
+ * Represents a tesselated polyline.
+ * Given a polyline as a line string, each segment of the line string is triangulated into a quad.
+ * Based on the angle between two segments, additional joint triangles may be inserted in between to enable smoothly-rounded corners.
+ */
+export interface TesselatedPolyline {
+  /** 24-bit index of each vertex. */
+  readonly indices: VertexIndices;
+  /** 24-bit index of the previous vertex in the polyline. */
+  readonly prevIndices: VertexIndices;
+  /** 24-bit index of the next vertex in the polyline, plus 8-bit parameter describing the semantics of this vertex. */
+  readonly nextIndicesAndParams: Uint8Array;
+  /** Distance of this vertex along the polyline. */
+  readonly distances: Float32Array;
+}
+
+class PolylineVertex {
+  public isSegmentStart: boolean = false;
+  public isPolylineStartOrEnd: boolean = false;
+  public vertexIndex: number = 0;
+  public prevIndex: number = 0;
+  public nextIndex: number = 0;
+  public distance: number = 0;
+
+  public constructor() { }
+
+  public init(isSegmentStart: boolean, isPolylineStartOrEnd: boolean, vertexIndex: number, prevIndex: number, nextIndex: number, distance: number) {
+    this.isSegmentStart = isSegmentStart;
+    this.isPolylineStartOrEnd = isPolylineStartOrEnd;
+    this.vertexIndex = vertexIndex;
+    this.prevIndex = prevIndex;
+    this.nextIndex = nextIndex;
+    this.distance = distance;
+  }
+
+  public computeParam(negatePerp: boolean, adjacentToJoint: boolean = false, joint: boolean = false, noDisplacement: boolean = false): number {
+    if (joint)
+      return PolylineParam.kJointBase;
+
+    let param: PolylineParam = PolylineParam.kNone;
+    if (noDisplacement)
+      param = PolylineParam.kNoneAdjustWeight; // prevent getting tossed before width adjustment
+    else if (adjacentToJoint)
+      param = PolylineParam.kMiterInsideOnly;
+    else
+      param = this.isPolylineStartOrEnd ? PolylineParam.kSquare : PolylineParam.kMiter;
+
+    let adjust = 0;
+    if (negatePerp)
+      adjust = PolylineParam.kNegatePerp;
+    if (!this.isSegmentStart)
+      adjust += PolylineParam.kNegateAlong;
+
+    return param + adjust;
+  }
+}
+
+class PolylineTesselator {
+  private _polylines: PolylineData[];
+  private _points: QPoint3dList;
+  private _doJoints: boolean;
+  private _numIndices = 0;
+  private _vertIndex: number[] = [];
+  private _prevIndex: number[] = [];
+  private _nextIndex: number[] = [];
+  private _nextParam: number[] = [];
+  private _distance: number[] = [];
+  private _position: Point3d[] = [];
+
+  private constructor(polylines: PolylineData[], points: QPoint3dList, doJointTriangles: boolean) {
+    this._polylines = polylines;
+    this._points = points;
+    this._doJoints = doJointTriangles;
+  }
+
+  public static fromPolyline(args: PolylineArgs): PolylineTesselator {
+    return new PolylineTesselator(args.polylines, args.points, wantJointTriangles(args.width, args.flags.is2d));
+  }
+
+  public static fromMesh(args: MeshArgs): PolylineTesselator | undefined {
+    if (undefined !== args.edges.polylines.lines && undefined !== args.points)
+      return new PolylineTesselator(args.edges.polylines.lines, args.points, wantJointTriangles(args.edges.width, args.is2d));
+
+    return undefined;
+  }
+
+  public tesselate(): TesselatedPolyline {
+    for (const p of this._points.list)
+      this._position.push(p.unquantize(this._points.params));
+
+    this._tesselate();
+
+    const vertIndex = VertexIndices.fromArray(this._vertIndex);
+    const prevIndex = VertexIndices.fromArray(this._prevIndex);
+
+    const nextIndexAndParam = new Uint8Array(this._numIndices * 4);
+    for (let i = 0; i < this._numIndices; i++) {
+      const index = this._nextIndex[i];
+      const j = i * 4;
+      VertexIndices.encodeIndex(index, nextIndexAndParam, j);
+      nextIndexAndParam[j + 3] = this._nextParam[i] & 0x000000ff;
+    }
+
+    const distance = new Float32Array(this._numIndices);
+    for (let i = 0; i < this._numIndices; ++i)
+      distance[i] = this._distance[i];
+
+    return {
+      indices: vertIndex,
+      prevIndices: prevIndex,
+      nextIndicesAndParams: nextIndexAndParam,
+      distances: distance,
+    };
+  }
+
+  private _tesselate() {
+    const v0 = new PolylineVertex(), v1 = new PolylineVertex();
+    const maxJointDot = -0.7;
+
+    for (const line of this._polylines) {
+      if (line.numIndices < 2)
+        continue;
+
+      let cumulativeDist = line.startDistance;
+      const last = line.numIndices - 1;
+      const isClosed: boolean = line.vertIndices[0] === line.vertIndices[last];
+
+      for (let i = 0; i < last; ++i) {
+        const idx0 = line.vertIndices[i];
+        const idx1 = line.vertIndices[i + 1];
+        const pos0 = this._position[idx0];
+        const pos1 = this._position[idx1];
+        const dist = pos0.distance(pos1);
+        const isStart: boolean = (0 === i);
+        const isEnd: boolean = (last - 1 === i);
+        const prevIdx0 = isStart ? (isClosed ? line.vertIndices[last - 1] : idx0) : line.vertIndices[i - 1];
+        const nextIdx1 = isEnd ? (isClosed ? line.vertIndices[1] : idx1) : line.vertIndices[i + 2];
+
+        v0.init(true, isStart && !isClosed, idx0, prevIdx0, idx1, cumulativeDist);
+        v1.init(false, isEnd && !isClosed, idx1, nextIdx1, idx0, cumulativeDist += dist);
+
+        const jointAt0: boolean = this._doJoints && (isClosed || !isStart) && this._dotProduct(v0) > maxJointDot;
+        const jointAt1: boolean = this._doJoints && (isClosed || !isEnd) && this._dotProduct(v1) > maxJointDot;
+
+        if (jointAt0 || jointAt1) {
+          this._addVertex(v0, v0.computeParam(true, jointAt0, false, false));
+          this._addVertex(v1, v1.computeParam(false, jointAt1, false, false));
+          this._addVertex(v0, v0.computeParam(false, jointAt0, false, true));
+          this._addVertex(v0, v0.computeParam(false, jointAt0, false, true));
+          this._addVertex(v1, v1.computeParam(false, jointAt1, false, false));
+          this._addVertex(v1, v1.computeParam(false, jointAt1, false, true));
+          this._addVertex(v0, v0.computeParam(false, jointAt0, false, true));
+          this._addVertex(v1, v1.computeParam(false, jointAt1, false, true));
+          this._addVertex(v0, v0.computeParam(false, jointAt0, false, false));
+          this._addVertex(v0, v0.computeParam(false, jointAt0, false, false));
+          this._addVertex(v1, v1.computeParam(false, jointAt1, false, true));
+          this._addVertex(v1, v1.computeParam(true, jointAt1, false, false));
+        } else {
+          this._addVertex(v0, v0.computeParam(true));
+          this._addVertex(v1, v1.computeParam(false));
+          this._addVertex(v0, v0.computeParam(false));
+          this._addVertex(v0, v0.computeParam(false));
+          this._addVertex(v1, v1.computeParam(false));
+          this._addVertex(v1, v1.computeParam(true));
+        }
+      }
+    }
+  }
+
+  private _dotProduct(v: PolylineVertex): number {
+    const pos: Point3d = this._position[v.vertexIndex];
+    const prevDir: Vector3d = Vector3d.createStartEnd(this._position[v.prevIndex], pos);
+    const nextDir: Vector3d = Vector3d.createStartEnd(this._position[v.nextIndex], pos);
+    return prevDir.dotProduct(nextDir);
+  }
+
+  private _addVertex(vertex: PolylineVertex, param: number): void {
+    this._vertIndex[this._numIndices] = vertex.vertexIndex;
+    this._prevIndex[this._numIndices] = vertex.prevIndex;
+    this._nextIndex[this._numIndices] = vertex.nextIndex;
+    this._nextParam[this._numIndices] = param;
+    this._distance[this._numIndices] = vertex.distance;
+    this._numIndices++;
+  }
+}
+
 export const enum SurfaceType {
     Unlit,
     Lit,
@@ -376,17 +576,6 @@ function convertSilhouettes(edges: MeshEdge[], normalPairs: OctEncodedNormalPair
   };
 }
 
-/**
- * Describes chains of edges of a mesh.
- * Each chain consists of 2 or more vertices.
- * The chains are triangulated, with joint triangles injected between segments to support rounded corners.
- * Polyline edges are typically only used for 2d graphics, or for 3d shapes with explicitly wide edges.
- */
-export interface PolylineEdgeParams {
-  /* ###TODO */
-  indices: VertexIndices;
-}
-
 /** Describes the edges of a mesh. */
 export interface EdgeParams {
   /** The edge width in pixels. */
@@ -398,18 +587,30 @@ export interface EdgeParams {
   /** Single-segment edges of curved surfaces, displayed based on edge normal relative to eye. */
   readonly silhouettes?: SilhouetteParams;
   /** Polyline edges, always displayed when edge display is enabled. */
-  readonly polylines?: PolylineEdgeParams;
+  readonly polylines?: TesselatedPolyline;
 }
 
-function convertEdges(args?: MeshArgsEdges): EdgeParams | undefined {
+function wantJointTriangles(weight: number, is2d: boolean): boolean {
+  // Joints are incredibly expensive. In 3d, only generate them if the line is sufficiently wide for them to be noticeable.
+  const jointWidthThreshold = 5;
+  return is2d || weight > jointWidthThreshold;
+}
+
+function convertEdges(meshArgs: MeshArgs): EdgeParams | undefined {
+  const args = meshArgs.edges;
   if (undefined === args)
     return undefined;
 
-  // ###TODO: Determine if we wants polylines converted to segments or not...
-  // Also why the heck does PolylineEdgeArgs exist - its only member is a PolylineData[] ???
-  // Same deal with args.edges.edges like wtf?
-  const polylines = undefined;
-  const segments = convertPolylinesAndEdges(args.polylines.lines, args.edges.edges);
+  let polylines: TesselatedPolyline | undefined;
+  let segments: SegmentEdgeParams | undefined;
+  if (wantJointTriangles(args.width, meshArgs.is2d)) {
+    segments = convertPolylinesAndEdges(args.polylines.lines, args.edges.edges);
+  } else {
+    segments = convertPolylinesAndEdges(undefined, args.edges.edges);
+    const tesselator = PolylineTesselator.fromMesh(meshArgs);
+    if (undefined !== tesselator)
+      polylines = tesselator.tesselate();
+  }
 
   // ###TODO: why the heck are the edges and normals of SilhouetteEdgeArgs potentially undefined???
   const silhouettes = undefined !== args.silhouettes.edges && undefined !== args.silhouettes.normals ? convertSilhouettes(args.silhouettes.edges, args.silhouettes.normals) : undefined;
@@ -459,8 +660,47 @@ export class MeshParams {
       material: args.material,
     };
 
-    const edges = convertEdges(args.edges);
+    const edges = convertEdges(args);
     return new MeshParams(vertices, surface, edges, args.isPlanar);
+  }
+}
+
+/**
+ * Describes a set of tesselated polylines.
+ * Each segment of each polyline is triangulated into a quad. Additional triangles may be inserted
+ * between segments to enable rounded corners.
+ */
+export class PolylineParams {
+  public readonly vertices: VertexTable;
+  public readonly polyline: TesselatedPolyline;
+  public readonly isPlanar: boolean;
+  public readonly type: PolylineTypeFlags;
+
+  /** Directly construct a PolylineParams. The PolylineParams takes ownership of all input data. */
+  public constructor(vertices: VertexTable, polyline: TesselatedPolyline, isPlanar: boolean, type: PolylineTypeFlags = PolylineTypeFlags.Normal) {
+    this.vertices = vertices;
+    this.polyline = polyline;
+    this.isPlanar = isPlanar;
+    this.type = type;
+  }
+
+  /** Construct from an PolylineArgs. */
+  public static create(args: PolylineArgs): PolylineParams | undefined {
+    assert(!args.flags.isDisjoint);
+    const vertices = VertexTable.createForPolylines(args);
+    if (undefined === vertices)
+      return undefined;
+
+    const tesselator = PolylineTesselator.fromPolyline(args);
+    if (undefined === tesselator)
+      return undefined;
+
+    return {
+      vertices,
+      polyline: tesselator.tesselate(),
+      isPlanar: args.flags.isPlanar,
+      type: args.flags.type,
+    };
   }
 }
 
