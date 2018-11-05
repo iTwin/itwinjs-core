1df8ee53c7d67f197fb86b7ed44c629d80fa7662
Ray Bentley <20863871+RBBentley@users.noreply.github.com>
2018-11-05 10:59:38 -0500

    Initial version

---
+++ test-apps/analysis-importer/src/ImportAnalysis.ts
@@ -0,0 +1,149 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+ *--------------------------------------------------------------------------------------------*/
+import { IModelDb, IModelHost, IModelHostConfiguration } from "@bentley/imodeljs-backend";
+import { GeometryStreamBuilder, GeometryStreamProps, Gradient, Code, GeometricElement3dProps, ViewFlags, ColorDef, RenderMode } from "@bentley/imodeljs-common";
+import { Id64String } from "@bentley/bentleyjs-core";
+import { OpenMode } from "@bentley/bentleyjs-core";
+import { Polyface, IModelJson, AuxChannelDataType, AuxChannel, PolyfaceBuilder, Arc3d, Point3d, Vector3d, LineString3d, StrokeOptions, Transform, AuxChannelData, PolyfaceAuxData } from "@bentley/geometry-core";
+import { Utilities, KnownTestLocations, AnalysisStyleProps } from "./Utilities";
+import * as path from "path";
+import { readFileSync } from "fs";
+
+/** Create a geometry stream from a Polyface. */
+function generateGeometryStreamFromPolyface(polyface: Polyface): GeometryStreamProps {
+    const builder = new GeometryStreamBuilder();
+
+    builder.appendGeometry(polyface);
+
+    return builder.geometryStream;
+}
+
+/**  get [[AnalysisStyles] for a polyface.  This is just an example - it pairs displacement and scalar channels that have matching input names */
+function getPolyfaceAnalysisStyleProps(polyface: Polyface): AnalysisStyleProps[] {
+    const analysisStyleProps: AnalysisStyleProps[] = [];
+    if (undefined === polyface.data.auxData)
+        return analysisStyleProps;
+
+    /**  Create a mapping from input name to channel - this is used to pair the displacement and scalar channels. */
+    const displacementChannels: Map<string, AuxChannel> = new Map<string, AuxChannel>();
+
+    for (const channel of polyface.data.auxData.channels)
+        if (channel.dataType === AuxChannelDataType.Vector)
+            displacementChannels.set(channel.inputName!, channel);
+
+    for (const channel of polyface.data.auxData.channels) {
+        if (channel.isScalar) {
+            const thematicSettings = new Gradient.ThematicSettings();
+            const displacementChannel = displacementChannels.get(channel.inputName!);
+            /**  If this channel ends with "Height" assign it a "Sea to Mountain" Gradient rather than the default (green-red) gradient. */
+            if (channel.name && channel.name.endsWith("Height")) {
+                thematicSettings.colorScheme = Gradient.ThematicColorScheme.SeaMountain;
+                thematicSettings.mode = Gradient.ThematicMode.SteppedWithDelimiter;
+            }
+            /** create the [[AnalysisStyle]] and add to the array. */
+            analysisStyleProps.push({
+                displacementChannelName: displacementChannel ? displacementChannel.name : undefined,
+                scalarRange: channel.scalarRange,
+                scalarChannelName: channel.name,
+                scalarThematicSettings: thematicSettings,
+                inputName: channel.inputName,
+            });
+        }
+    }
+    return analysisStyleProps;
+}
+/** Create an analysis model for a [[Polyface]] with [[PolyfaceAuxData]] and [[DisplayStyles]] for viewing the [[AuxChannels]] */
+async function createAnalysisModel(polyface: any, categoryId: Id64String, modelName: string, iModel: IModelDb) {
+    if (!(polyface instanceof Polyface))
+        return;
+    const modelId = await Utilities.createModel(iModel, modelName);
+
+    const geometry = generateGeometryStreamFromPolyface(polyface);
+    const analysisStyleProps = getPolyfaceAnalysisStyleProps(polyface);
+    const viewFlags = new ViewFlags();
+    const backgroundColor = ColorDef.white;
+
+    viewFlags.renderMode = RenderMode.SolidFill;        // SolidFill rendering ... no lighting etc.
+
+    const props: GeometricElement3dProps = {
+        model: modelId,
+        code: Code.createEmpty(),
+        classFullName: "Generic: PhysicalObject",
+        category: categoryId,
+        geom: geometry,
+    };
+    await iModel.elements.insertElement(props);
+
+    let first = true;
+    for (const analysisStyleProp of analysisStyleProps) {
+        let name = analysisStyleProp.scalarChannelName!;
+        if (undefined !== analysisStyleProp.displacementChannelName)
+            name = name + " and " + analysisStyleProp.displacementChannelName;
+        const displayStyleId = Utilities.createAndInsertDisplayStyle3d(iModel, name + "Style", viewFlags, backgroundColor, analysisStyleProp);
+        const viewId = Utilities.createOrthographicView(iModel, name + " View", modelId, categoryId, polyface.range(), displayStyleId);
+        if (first) {
+            first = false;
+            Utilities.setDefaultViewId(iModel, viewId);
+        }
+    }
+}
+/** Import a polyface from the supplied json file. */
+async function importPolyfaceFromJson(jsonFileName: string) {
+    const jsonString = readFileSync(path.join(KnownTestLocations.assetsDir, jsonFileName), "utf8");
+    const json = JSON.parse(jsonString);
+    return IModelJson.Reader.parse(json);
+}
+/** Create a polyface representing a cantilever beam with [[PolyfaceAuxData]] representing the stress and deflection. */
+function createCantileverBeamPolyface() {
+    const beamRadius = 10.0;
+    const beamLength = 100.0;
+    const facetSize = 1.0;
+    const builder = PolyfaceBuilder.create();
+    const crossSectionArc = Arc3d.create(Point3d.createZero(), Vector3d.create(0.0, beamRadius, 0.0), Vector3d.create(0.0, 0.0, beamRadius));
+    const strokedCrossSection = LineString3d.create();
+    const strokeOptions = StrokeOptions.createForCurves();
+    strokeOptions.maxEdgeLength = facetSize;
+    crossSectionArc.emitStrokes(strokedCrossSection, strokeOptions);
+
+    for (let x = 0.0; x < beamLength; x += facetSize)
+        builder.addBetweenTransformedLineStrings(strokedCrossSection, Transform.createTranslationXYZ(x, 0.0, 0.0), Transform.createTranslationXYZ(x + facetSize, 0.0, 0.0), true);
+
+    const polyface = builder.claimPolyface();
+    const heightData: number[] = [];
+
+    const scratchPoint = Point3d.create();
+    for (let i = 0; i < polyface.data.point.length; i++) {
+        const point = polyface.data.point.atPoint3dIndex(i, scratchPoint) as Point3d;
+        heightData.push(point.z);
+    }
+    const heightChannel = new AuxChannel([new AuxChannelData(0.0, heightData)], AuxChannelDataType.Distance, "Height", "");
+
+    if (true) polyface.data.auxData = new PolyfaceAuxData([heightChannel], polyface.data.pointIndex);
+
+    return polyface;
+}
+
+async function doAnalysisExamples() {
+    const config = new IModelHostConfiguration();
+    IModelHost.startup(config);
+
+    const iModel: IModelDb = Utilities.openIModel("empty.bim", { copyFilename: "AnalysisExample.bim", deleteFirst: true, openMode: OpenMode.ReadWrite });
+    if (!iModel)
+        return;
+
+    const categoryId = await Utilities.createAndInsertSpatialCategory(iModel, "Analysis Category");
+    const importedPolyface = await importPolyfaceFromJson("RadialWave.json");
+
+    if (true)
+        await createAnalysisModel(importedPolyface, categoryId, "Imported Data", iModel);
+
+    const beamPolyface = createCantileverBeamPolyface();
+
+    await createAnalysisModel(beamPolyface, categoryId, "Cantiliever Beam", iModel);
+
+    iModel.saveChanges();
+    iModel.closeStandalone();
+}
+
+doAnalysisExamples();
