f044ec230b2e6cc18eb9c7244da9c5ee29669c8f
Affan Khan <29756942+khanaffan@users.noreply.github.com>
2018-12-17 16:39:33 +0000

    Merged PR 12480: Added config to allow build backend as bundle for mobile platform
    Added config to allow build backend as bundle for mobile platform

---
+++ tools/webpack/mobile/backend.config.js
@@ -0,0 +1,102 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) 2018 Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+
+const path = require("path");
+const webpack = require("webpack");
+module.exports = (env) => { return getConfig(env); };
+
+function getConfig(env) {
+  // set sourcedir if not specified in arguments.
+  if (!env.sourcedir)
+    env.sourcedir = "./";
+
+  if (!env.outdir)
+    env.outdir = "./lib/module" + (env.prod ? "/prod" : "/dev");
+
+  // get the directory for the bundle.
+  bundleDirectory = path.resolve(env.sourcedir, env.outdir);
+
+  // the context directory (for looking up imports, etc.) is the original module source directory.
+  const contextDirectory = path.resolve(env.sourcedir);
+
+  // unless specified with env.prod, create a development build.
+  const devMode = !(env.prod);
+
+  // this is the "barrel" file of the module, which imports all of the sources.
+  const bundleEntry = env.entry;
+
+  // name of the output bundle.
+  const bundleName = env.bundlename;
+
+  const webConfig = {
+    mode: devMode ? "development" : "production",
+    entry: bundleEntry,
+    output: {
+      libraryTarget: "commonjs2",
+      library: bundleName,
+      path: bundleDirectory,
+      pathinfo: true,
+    },
+    target: "webworker",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "source-map-loader",
+          enforce: "pre"
+        },
+        {
+          test: /growl\.js$/,
+          use: 'null-loader'
+        },
+        {
+          test: /xunit\.js$/,
+          use: 'null-loader'
+        },
+        {
+          test: /bunyan/,
+          use: 'null-loader'
+        }
+      ]
+    },
+    externals: {
+      "@bentley/imodeljs-electronaddon": "@bentley/imodeljs-electronaddon",
+      "@bentley/imodeljs-nodeaddon": "@bentley/imodeljs-nodeaddon",
+      "@bentley/imodeljs-nodeaddon/NodeAddonLoader": "@bentley/imodeljs-nodeaddon/NodeAddonLoader",
+      "@bentley/imodeljs-nodeaddonapi/package.json": "@bentley/imodeljs-nodeaddonapi/package.json",
+      "@bentley/imodeljs-native-platform-electron": "@bentley/imodeljs-native-platform-electron",
+      "@bentley/imodeljs-native-platform-node": "@bentley/imodeljs-native-platform-node",
+      "@bentley/imodeljs-native-platform-node/NodeAddonLoader": "@bentley/imodeljs-native-platform-node/NodeAddonLoader",
+      "@bentley/imodeljs-native-platform-api/package.json": "@bentley/imodeljs-native-platform-api/package.json,",
+      "electron": "electron",
+      "IModelJsFs": "IModelJsFs",
+      "./IModelJsFs": "IModelJsFs",
+      "../IModelJsFs": "IModelJsFs",
+      "fs": "fs",
+      "fs-extra": "IModelJsFs",
+      "express": "express",
+    },
+    stats: {
+      warnings: false
+    },
+    node: {
+      fs: false,
+      console: false,
+      process: false,
+      Buffer: true,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+      }),
+      new webpack.ProvidePlugin({
+      }),
+      new webpack.EnvironmentPlugin({
+      })
+    ],
+  };
+
+  return webConfig;
+}
