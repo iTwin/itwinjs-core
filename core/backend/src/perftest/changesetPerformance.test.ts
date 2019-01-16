0bd9fc90869c9d46d8cca756862bb4599ecf7a15
Taslim Murad <38288322+imodeljs-admin@users.noreply.github.com>
2019-01-16 15:31:47 +0500

    added cs perf test

--- core/backend/src/perftest/changesetPerformance.test.ts
+++ core/backend/src/perftest/changesetPerformance.test.ts
@@ -2,16 +2,17 @@
 * Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
 * Licensed under the MIT License. See LICENSE.md in the project root for license terms.
 *--------------------------------------------------------------------------------------------*/
-import { IModelDb, OpenParams, IModelJsFs, KeepBriefcase } from "../imodeljs-backend";
-import { Config, IModelHubClient, ImsActiveSecureTokenClient, AuthorizationToken, AccessToken, ChangeSet } from "@bentley/imodeljs-clients";
-import { ActivityLoggingContext, Guid } from "@bentley/bentleyjs-core";
-import { IModelVersion } from "@bentley/imodeljs-common";
+import { IModelDb, OpenParams, IModelJsFs, KeepBriefcase, ConcurrencyControl, DictionaryModel, SpatialCategory, BriefcaseManager } from "../imodeljs-backend";
+import { Config, IModelHubClient, ImsActiveSecureTokenClient, AuthorizationToken, AccessToken, ChangeSet, HubIModel, IModelQuery } from "@bentley/imodeljs-clients";
+import { ActivityLoggingContext, Guid, Id64String } from "@bentley/bentleyjs-core";
+import { IModelVersion, IModel, SubCategoryAppearance } from "@bentley/imodeljs-common";
 import { KnownTestLocations } from "../test/KnownTestLocations";
 import * as path from "path";
 import * as fs from "fs";
 import { assert } from "chai";
 import { Element } from "../Element";
 import { IModelHost } from "../IModelHost";
+import { IModelTestUtils } from "../test/IModelTestUtils";
 
 async function getImodelAfterApplyingCS(csvPath: string) {
   csvPath = csvPath;
@@ -37,7 +38,6 @@ async function getImodelAfterApplyingCS(csvPath: string) {
   const changeSets: ChangeSet[] = await client.changeSets.get(actLogCtx, accessToken, imodelId);
   const firstChangeSetId = changeSets[0].wsgId;
   const secondChangeSetId = changeSets[1].wsgId;
-  const thirdChangeSetId = changeSets[2].wsgId;
 
   // open imodel first time from imodel-hub with first revision
   const startTime = new Date().getTime();
@@ -58,16 +58,6 @@ async function getImodelAfterApplyingCS(csvPath: string) {
   assert.strictEqual<string>(imodeldb1.briefcase.currentChangeSetId, secondChangeSetId);
   imodeldb1.close(actLogCtx, accessToken).catch();
   fs.appendFileSync(csvPath, "Open, From Cache second cs," + elapsedTime1 + "\n");
-
-  // open imodel from local cache with third revision
-  const startTime2 = new Date().getTime();
-  const imodeldb2: IModelDb = await IModelDb.open(actLogCtx, accessToken, projectId, imodelId, OpenParams.pullOnly(), IModelVersion.asOfChangeSet(thirdChangeSetId));
-  const endTime2 = new Date().getTime();
-  assert.exists(imodeldb2);
-  const elapsedTime2 = (endTime2 - startTime2) / 1000.0;
-  assert.strictEqual<string>(imodeldb2.briefcase.currentChangeSetId, thirdChangeSetId);
-  imodeldb2.close(actLogCtx, accessToken).catch();
-  fs.appendFileSync(csvPath, "Open, From Cache third cs," + elapsedTime2 + "\n");
 }
 
 async function pushImodelAfterMetaChanges(csvPath: string) {
@@ -99,7 +89,7 @@ async function pushImodelAfterMetaChanges(csvPath: string) {
   const rootEl: Element = iModelPullAndPush.elements.getRootSubject();
   rootEl.userLabel = rootEl.userLabel + "changed";
   iModelPullAndPush.elements.updateElement(rootEl);
-  iModelPullAndPush.saveChanges();
+  iModelPullAndPush.saveChanges("user changes root subject of the imodel");
   const endTime = new Date().getTime();
   const elapsedTime = (endTime - startTime) / 1000.0;
   fs.appendFileSync(csvPath, "Update, Make Meta Changes," + elapsedTime + "\n");
@@ -113,6 +103,74 @@ async function pushImodelAfterMetaChanges(csvPath: string) {
     fs.appendFileSync(csvPath, "Push, Meta Changes to Hub," + elapsedTime1 + "\n");
   } catch (error) { }
   await iModelPullAndPush.close(actLogCtx, accessToken, KeepBriefcase.No);
+}
+
+export async function createNewModelAndCategory(rwIModel: IModelDb, accessToken: AccessToken, actx: ActivityLoggingContext) {
+  // Create a new physical model.
+  let modelId: Id64String;
+  [, modelId] = IModelTestUtils.createAndInsertPhysicalPartitionAndModel(rwIModel, IModelTestUtils.getUniqueModelCode(rwIModel, "newPhysicalModel"), true);
+
+  // Find or create a SpatialCategory.
+  const dictionary: DictionaryModel = rwIModel.models.getModel(IModel.dictionaryId) as DictionaryModel;
+  const newCategoryCode = IModelTestUtils.getUniqueSpatialCategoryCode(dictionary, "ThisTestSpatialCategory");
+  const spatialCategoryId: Id64String = SpatialCategory.insert(rwIModel, IModel.dictionaryId, newCategoryCode.value!, new SubCategoryAppearance({ color: 0xff0000 }));
+
+  // Reserve all of the codes that are required by the new model and category.
+  try {
+    await rwIModel.concurrencyControl.request(actx, accessToken);
+  } catch (err) {
+    if (err instanceof ConcurrencyControl.RequestError) {
+      assert.fail(JSON.stringify(err.unavailableCodes) + ", " + JSON.stringify(err.unavailableLocks));
+    }
+  }
+
+  return { modelId, spatialCategoryId };
+}
+
+async function pushImodelAfterDataChanges(csvPath: string) {
+  csvPath = csvPath;
+  const fs1 = require("fs");
+  const configData = JSON.parse(fs1.readFileSync("src/perftest/CSPerfConfig.json"));
+  const uname = configData.username;
+  const pass = configData.password;
+  const projectId = configData.projectId;
+  const myAppConfig = {
+    imjs_buddi_resolve_url_using_region: 102,
+    imjs_default_relying_party_uri: "https://connect-wsg20.bentley.com",
+  };
+  Config.App.merge(myAppConfig);
+  const client: IModelHubClient = new IModelHubClient();
+  IModelHost.loadNative(myAppConfig.imjs_buddi_resolve_url_using_region);
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  const actLogCtx = new ActivityLoggingContext(Guid.createValue());
+  const imsClient: ImsActiveSecureTokenClient = new ImsActiveSecureTokenClient();
+  const authToken: AuthorizationToken = await imsClient.getToken(actLogCtx, uname, pass);
+  const accessToken: AccessToken = await client.getAccessToken(actLogCtx, authToken);
+
+  const iModelName = "CodesPushTest";
+  // delete any existing imodel with given name
+  const iModels: HubIModel[] = await BriefcaseManager.imodelClient.iModels.get(actLogCtx, accessToken, projectId, new IModelQuery().byName(iModelName));
+  for (const iModelTemp of iModels) {
+    await BriefcaseManager.imodelClient.iModels.delete(actLogCtx, accessToken, projectId, iModelTemp.id!);
+  }
+  // create new imodel with given name
+  const rwIModel: IModelDb = await IModelDb.create(actLogCtx, accessToken, projectId, iModelName, { rootSubject: { name: "TestSubject" } });
+  const rwIModelId = rwIModel.iModelToken.iModelId;
+  assert.isNotEmpty(rwIModelId);
+
+  // create new model, category and physical element, and insert in imodel
+  rwIModel.concurrencyControl.setPolicy(new ConcurrencyControl.OptimisticPolicy());
+  const r: { modelId: Id64String, spatialCategoryId: Id64String } = await createNewModelAndCategory(rwIModel, accessToken, actLogCtx);
+  rwIModel.elements.insertElement(IModelTestUtils.createPhysicalObject(rwIModel, r.modelId, r.spatialCategoryId));
+  rwIModel.saveChanges("User created model, category and one physical element");
+
+  // get the time to push a data change of an imodel to imodel hub
+  const startTime1 = new Date().getTime();
+  await rwIModel.pushChanges(actLogCtx, accessToken).catch();
+  const endTime1 = new Date().getTime();
+  const elapsedTime1 = (endTime1 - startTime1) / 1000.0;
+  fs.appendFileSync(csvPath, "Push, Data(Element model category inserted) Changes to Hub," + elapsedTime1 + "\n");
+  await rwIModel.close(actLogCtx, accessToken, KeepBriefcase.No);
 }
 
 describe("ImodelChangesetPerformance", async () => {
@@ -133,5 +191,9 @@ describe("ImodelChangesetPerformance", async () => {
 
   it("PushImodelMetaChangeToImodelHUb", async () => {
     pushImodelAfterMetaChanges(csvPath).catch();
+  });
+
+  it("PushImodelDataChangeToImodelHUb", async () => {
+    pushImodelAfterDataChanges(csvPath).catch();
   });
 });
