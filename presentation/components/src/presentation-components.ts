39e676e4e4703818d71cd5be09335b3f55d30c16
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2019-01-23 14:26:11 +0000

    Merged PR 14523: Add additional packages to rush coverage script

--- presentation/components/src/presentation-components.ts
+++ presentation/components/src/presentation-components.ts
@@ -27,6 +27,7 @@ export { viewWithUnifiedSelection } from "./viewport/WithUnifiedSelection";
 
 // Set the version number so it can be found at runtime. BUILD_SEMVER is replaced at build time by the webpack DefinePlugin.
 declare var BUILD_SEMVER: string;
+/* istanbul ignore next */
 if ((typeof (BUILD_SEMVER) !== "undefined") && (typeof window !== "undefined") && window) {
   if (!(window as any).iModelJsVersions)
     (window as any).iModelJsVersions = new Map<string, string>();
--- presentation/frontend/src/presentation-frontend.ts
+++ presentation/frontend/src/presentation-frontend.ts
@@ -17,6 +17,7 @@ export { default as SelectionHandler } from "./selection/SelectionHandler";
 
 // Set the version number so it can be found at runtime. BUILD_SEMVER is replaced at build time by the webpack DefinePlugin.
 declare var BUILD_SEMVER: string;
+/* istanbul ignore next */
 if ((typeof (BUILD_SEMVER) !== "undefined") && (typeof window !== "undefined") && window) {
   if (!(window as any).iModelJsVersions)
     (window as any).iModelJsVersions = new Map<string, string>();
