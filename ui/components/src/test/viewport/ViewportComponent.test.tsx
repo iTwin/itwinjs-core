ed51ed6c98e804773a4f1524a2b1663fd9f05ce9
Dan East <51122937+DanEastBentley@users.noreply.github.com>
2019-01-29 19:57:30 +0000

    Merged PR 15321: Removed imodeljs-clients-backend dependency from ui-framework. WIP: ViewportComponent unit tests.

--- ui/components/src/test/viewport/ViewportComponent.test.tsx
+++ ui/components/src/test/viewport/ViewportComponent.test.tsx
@@ -5,34 +5,104 @@
 import * as React from "react";
 import { render, cleanup /*, RenderResult, waitForElement*/ } from "react-testing-library";
 import * as moq from "typemoq";
-import * as faker from "faker";
 
+import TestUtils from "../TestUtils";
 import { ViewportComponent } from "../../ui-components";
 
-import { IModelConnection, ViewState3d } from "@bentley/imodeljs-frontend";
-import { Id64String, Id64 } from "@bentley/bentleyjs-core";
+import { IModelConnection, ViewState3d, MockRender, SpatialViewState } from "@bentley/imodeljs-frontend";
+import { ViewStateProps, IModel, Code, RenderMode } from "@bentley/imodeljs-common";
+import { Id64String } from "@bentley/bentleyjs-core";
 
-// Most of this setup code is either from presentation/common or presentation/components
-const createRandomId = (): Id64String => {
-  return Id64.fromLocalAndBriefcaseIds(faker.random.number(), faker.random.number());
-};
+class SavedViewUtil {
+  /** Creates an empty view state props */
+  private static _createEmptyViewStateProps(_categories: Id64String[] | undefined, _models: Id64String[] | undefined): ViewStateProps {
+    // Use dictionary model in all props
+    const dictionaryId = IModel.dictionaryId;
 
-describe("ViewportComponent", () => {
+    // Category Selector Props
+    const categorySelectorProps = {
+      categories: _categories ? _categories : [],
+      code: Code.createEmpty(),
+      model: dictionaryId,
+      classFullName: "BisCore:CategorySelector",
+    };
+    // Model Selector Props
+    const modelSelectorProps = {
+      models: _models ? _models : [],
+      code: Code.createEmpty(),
+      model: dictionaryId,
+      classFullName: "BisCore:ModelSelector",
+    };
+    // View Definition Props
+    const viewDefinitionProps = {
+      categorySelectorId: "",
+      displayStyleId: "",
+      code: Code.createEmpty(),
+      model: dictionaryId,
+      classFullName: "BisCore:SpatialViewDefinition",
+    };
+    // Display Style Props
+    const displayStyleProps = {
+      code: Code.createEmpty(),
+      model: dictionaryId,
+      classFullName: "BisCore:DisplayStyle",
+      jsonProperties: {
+        styles: {
+          viewflags: {
+            renderMode: RenderMode.SmoothShade,
+            noSourceLights: false,
+            noCameraLights: false,
+            noSolarLight: false,
+            noConstruct: true,
+          },
+        },
+      },
+    };
+
+    return { displayStyleProps, categorySelectorProps, modelSelectorProps, viewDefinitionProps };
+  }
+
+  /** Create an empty view state */
+  public static async createEmptyViewState(iModelConnection: IModelConnection, categories: Id64String[] | undefined, models: Id64String[] | undefined): Promise<SpatialViewState> {
+    const props = SavedViewUtil._createEmptyViewStateProps(categories, models);
+    const viewState = SpatialViewState.createFromProps(props, iModelConnection) as SpatialViewState;
+    await viewState.load();
+    return viewState;
+  }
+}
+
+describe.skip("ViewportComponent", () => {
+
+  before(async () => {
+    TestUtils.initializeUiComponents(); // tslint:disable-line:no-floating-promises
+    MockRender.App.startup();
+  });
+
+  after(async () => {
+    MockRender.App.shutdown();
+  });
+
   const imodelMock = moq.Mock.ofType<IModelConnection>();
-  let viewDefinitionId: Id64String;
+  let viewState: ViewState3d;
 
-  beforeEach(() => {
-    viewDefinitionId = createRandomId();
+  beforeEach(async () => {
+    imodelMock.reset();
+
     const viewsMock = moq.Mock.ofInstance<IModelConnection.Views>(new IModelConnection.Views(imodelMock.object));
     viewsMock.setup(async (views) => views.load(moq.It.isAny())).returns(async () => moq.Mock.ofType<ViewState3d>().object);
-    imodelMock.reset();
     imodelMock.setup((imodel) => imodel.views).returns(() => viewsMock.object);
+
+    const modelsMock = moq.Mock.ofType<IModelConnection.Models>();
+    modelsMock.setup(async (models) => models.load(moq.It.isAny())).returns(async () => moq.Mock.ofType<void>().object);
+    imodelMock.setup((imodel) => imodel.models).returns(() => modelsMock.object);
+
+    viewState = await SavedViewUtil.createEmptyViewState(imodelMock.object, undefined, undefined);
   });
 
   afterEach(cleanup);
 
   it("should render", () => {
-    render(<ViewportComponent imodel={imodelMock.object} viewDefinitionId={viewDefinitionId} />);
+    render(<ViewportComponent imodel={imodelMock.object} viewState={viewState} />);
   });
 
 });
