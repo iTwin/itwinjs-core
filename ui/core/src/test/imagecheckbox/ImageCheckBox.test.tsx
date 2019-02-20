5782b8853e2e6d8a9aa827b253e972339834bad0
Dan East <51122937+DanEastBentley@users.noreply.github.com>
2019-02-20 17:46:12 +0000

    Merged PR 17590: Minor UI Color Theme changes

--- ui/core/src/test/imagecheckbox/ImageCheckBox.test.tsx
+++ ui/core/src/test/imagecheckbox/ImageCheckBox.test.tsx
@@ -4,13 +4,14 @@
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
 import { mount, shallow } from "enzyme";
+import * as sinon from "sinon";
 
 import { ImageCheckBox } from "../../ui-core";
 
 describe("<ImageCheckBox />", () => {
   it("should render", () => {
     const wrapper = mount(
-      <ImageCheckBox imageOn="icon-visibility" imageOff="icon-visibility-hide-2" />,
+      <ImageCheckBox imageOn="icon-visibility" imageOff="icon-visibility-hide-2" checked={true} />,
     );
     wrapper.unmount();
   });
@@ -19,6 +20,21 @@ describe("<ImageCheckBox />", () => {
     shallow(
       <ImageCheckBox imageOn="icon-visibility" imageOff="icon-visibility-hide-2" />,
     ).should.matchSnapshot();
+  });
+
+  it("disabled renders correctly", () => {
+    shallow(
+      <ImageCheckBox imageOn="icon-visibility" imageOff="icon-visibility-hide-2" disabled={true} />,
+    ).should.matchSnapshot();
+  });
+
+  it("onClick should be called on change", () => {
+    const handler = sinon.spy();
+    const wrapper = shallow(<ImageCheckBox imageOn="icon-visibility" imageOff="icon-visibility-hide-2" onClick={handler} checked={false} />);
+    const input = wrapper.find("input");
+    input.simulate("change", { target: { checked: true } });
+    handler.should.have.been.calledOnce;
+    handler.should.have.been.calledWithExactly(true);
   });
 
 });
