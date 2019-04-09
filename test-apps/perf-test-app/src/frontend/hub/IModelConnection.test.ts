38a165f562dae4eeab3cdf9413a7ff8b6cc1a07e
Taslim Murad <38288322+imodeljs-admin@users.noreply.github.com>
2019-04-09 07:40:21 +0000

    Merged PR 23252: Updated IModelConnection.test.ts
    Updated IModelConnection.test.ts

--- test-apps/perf-test-app/src/frontend/hub/IModelConnection.test.ts
+++ test-apps/perf-test-app/src/frontend/hub/IModelConnection.test.ts
@@ -19,7 +19,7 @@ async function executeQuery(iModel: IModelConnection, ecsql: string, bindings?: 
   return rows;
 }
 
-describe.skip("IModelConnection (#integration)", () => {
+describe("IModelConnection (#integration)", () => {
   let iModel: IModelConnection;
   let testProjectId: string;
   let testIModelId: string;
