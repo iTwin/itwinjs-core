8e3c0e5b6820d3b6fa9e1eddea638bb1d570a0cd
Evan Preslar <54330647+evanpreslar-bentley@users.noreply.github.com>
2019-04-10 22:09:34 +0000

    Merged PR 23575: Fix missing ULAS client request data

--- core/backend/src/IModelDb.ts
+++ core/backend/src/IModelDb.ts
@@ -331,7 +331,7 @@ export class IModelDb extends IModel implements PageableECSql {
       return defaultId;
     }
 
-    return parseInt(requestContext.applicationId, defaultId);
+    return parseInt(requestContext.applicationId, 10) || defaultId;
   }
 
   private async logUsage(requestContext: AuthorizedClientRequestContext, contextId: GuidString): Promise<void> {
@@ -343,6 +343,17 @@ export class IModelDb extends IModel implements PageableECSql {
       ulasEntry.projectId = contextId;
       ulasEntry.productId = this.getApplicationId(requestContext);
       ulasEntry.productVersion = this.getApplicationVersion(requestContext);
+
+      if (!requestContext.accessToken.isJwt) {
+        const userInfo = requestContext.accessToken.getUserInfo();
+        const featureTrackingInfo = userInfo ? userInfo.featureTracking : undefined;
+        ulasEntry.userInfo = {
+          imsId: !userInfo ? "" : userInfo.id,
+          ultimateSite: !featureTrackingInfo ? 0 : parseInt(featureTrackingInfo.ultimateSite, 10),
+          usageCountryIso: !featureTrackingInfo ? "" : featureTrackingInfo.usageCountryIso,
+        };
+      }
+
       const resp: LogPostingResponse = await client.logUsage(requestContext, ulasEntry);
       requestContext.enter();
       status = resp ? resp.status : BentleyStatus.ERROR;
