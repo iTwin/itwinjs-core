d001f0983dd41ff4185f65130379da0e87d7de99
Bill Goehrig <33036725+wgoehrig@users.noreply.github.com>
2019-05-20 19:09:41 +0000

    Merged PR 28189: Add boilerplate for browser-based frontend-only tests of the markup package.
    Add boilerplate for browser-based frontend-only tests of the markup package.

---
+++ core/markup/src/test/Markup.test.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+
+describe("Markup", () => {
+  it("tests should run in a browser", () => {
+    expect(window).to.exist;
+  });
+});
