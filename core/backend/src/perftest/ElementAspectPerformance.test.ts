52ecd329f821419b3fe96316111240567cea1fc5
Taslim Murad <38288322+imodeljs-admin@users.noreply.github.com>
2019-05-20 07:49:39 +0000

    Merged PR 28070: updated element aspect perf tests

--- core/backend/src/perftest/ElementAspectPerformance.test.ts
+++ core/backend/src/perftest/ElementAspectPerformance.test.ts
@@ -1,6 +1,6 @@
 import {
   IModelDb, IModelHost, OpenParams, ElementAspect, DictionaryModel, SpatialCategory,
-  ConcurrencyControl, IModelJsFs,
+  ConcurrencyControl,
 } from "../imodeljs-backend";
 import { Config, ImsUserCredentials, AuthorizedClientRequestContext } from "@bentley/imodeljs-clients";
 import { IModelTestUtils } from "../test/IModelTestUtils";
@@ -9,7 +9,7 @@ import { assert } from "chai";
 import { Id64String } from "@bentley/bentleyjs-core";
 import { KnownTestLocations } from "../test/KnownTestLocations";
 import * as path from "path";
-import * as fs from "fs";
+import { Reporter } from "@bentley/perf-tools/lib/Reporter";
 
 export async function createNewModelAndCategory(requestContext: AuthorizedClientRequestContext, rwIModel: IModelDb) {
   // Create a new physical model.
@@ -31,12 +31,7 @@ export async function createNewModelAndCategory(requestContext: AuthorizedClient
 }
 
 describe("ElementAspectPerfomance", () => {
-  if (!IModelJsFs.existsSync(KnownTestLocations.outputDir))
-    IModelJsFs.mkdirSync(KnownTestLocations.outputDir);
-  const csvPath = path.join(KnownTestLocations.outputDir, "ElementAspectPerfTests.csv");
-  if (!IModelJsFs.existsSync(csvPath)) {
-    fs.appendFileSync(csvPath, "Operation,Description,ExecutionTime,Count\n");
-  }
+  const reporter = new Reporter();
   let requestContext: AuthorizedClientRequestContext;
   let projectId: string;
   let imodelId: string;
@@ -44,7 +39,7 @@ describe("ElementAspectPerfomance", () => {
     const fs1 = require("fs");
     const configData = JSON.parse(fs1.readFileSync("src/perftest/CSPerfConfig.json"));
     projectId = configData.projectId;
-    imodelId = configData.aspectIModelId;
+    imodelId = configData.aspectIModelId;             // change imodel
     const myAppConfig = {
       imjs_buddi_resolve_url_using_region: 102,
       imjs_default_relying_party_uri: "https://connect-wsg20.bentley.com",
@@ -59,7 +54,65 @@ describe("ElementAspectPerfomance", () => {
     requestContext = await IModelTestUtils.getTestUserRequestContext(userCredentials);
   });
 
-  it("Element Unique Aspect Insertion, Updation, Deletion", async () => {
+  after(() => {
+    const csvPath = path.join(KnownTestLocations.outputDir, "ElementAspectPerfTests.csv");
+    reporter.exportCSV(csvPath);
+  });
+
+  it("SimpleElement-Insertion-Updation-Deletion-Read", async () => {
+    const imodeldb: IModelDb = await IModelDb.open(requestContext, projectId, imodelId, OpenParams.pullOnly(), IModelVersion.latest());
+    assert.exists(imodeldb);
+
+    let eleId: Id64String;
+    const count1 = 10;
+    let totalTimeReadSimpELeGet = 0;
+    let totalTimeInsertSimpELeGet = 0;
+    let totalTimeUpdateSimpELeGet = 0;
+    let totalTimeDeleteSimpELeGet = 0;
+
+    for (let m = 0; m < count1; ++m) {
+      const r: { modelId: Id64String, spatialCategoryId: Id64String } = await createNewModelAndCategory(requestContext, imodeldb);
+
+      // insert simple element with no aspect
+      const startTime1 = new Date().getTime();
+      eleId = imodeldb.elements.insertElement(IModelTestUtils.createPhysicalObject(imodeldb, r.modelId, r.spatialCategoryId));
+      const endTime1 = new Date().getTime();
+      const elapsedTime1 = (endTime1 - startTime1) / 1000.0;
+      totalTimeInsertSimpELeGet = totalTimeInsertSimpELeGet + elapsedTime1;
+
+      // read simple element with no aspect
+      const startTime = new Date().getTime();
+      const returnEle = imodeldb.elements.getElement(eleId);
+      const endTime = new Date().getTime();
+      const elapsedTime = (endTime - startTime) / 1000.0;
+      totalTimeReadSimpELeGet = totalTimeReadSimpELeGet + elapsedTime;
+      assert.exists(returnEle);
+
+      // update simple element with no aspect
+      const startTime2 = new Date().getTime();
+      const returnEle1 = imodeldb.elements.getElement(eleId);
+      returnEle1.userLabel = returnEle1.userLabel + "updated";
+      imodeldb.elements.updateElement(returnEle1);
+      const endTime2 = new Date().getTime();
+      const elapsedTime2 = (endTime2 - startTime2) / 1000.0;
+      totalTimeUpdateSimpELeGet = totalTimeUpdateSimpELeGet + elapsedTime2;
+
+      // delete simple element with no aspect
+      const startTime3 = new Date().getTime();
+      imodeldb.elements.deleteElement(eleId);
+      const endTime3 = new Date().getTime();
+      const elapsedTime3 = (endTime3 - startTime3) / 1000.0;
+      totalTimeDeleteSimpELeGet = totalTimeDeleteSimpELeGet + elapsedTime3;
+
+    }
+    imodeldb.close(requestContext).catch();
+    reporter.addEntry("ElementAspectPerfomance", "SimpleElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Insertion", totalTimeInsertSimpELeGet, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "SimpleElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Updation", totalTimeUpdateSimpELeGet, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "SimpleElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Deletion", totalTimeDeleteSimpELeGet, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "SimpleElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Read", totalTimeReadSimpELeGet, { "ElementCount ": count1 });
+  });
+
+  it("UniqueAspectElement-Insertion-Updation-Deletion-Read", async () => {
     const imodeldb: IModelDb = await IModelDb.open(requestContext, projectId, imodelId, OpenParams.pullOnly(), IModelVersion.latest());
     assert.exists(imodeldb);
 
@@ -69,59 +122,64 @@ describe("ElementAspectPerfomance", () => {
     let totalTimeInsert = 0;
     let totalTimeUpdate = 0;
     let totalTimeDelete = 0;
+    let totalTimeRead = 0;
 
     for (let m = 0; m < count1; ++m) {
       const r: { modelId: Id64String, spatialCategoryId: Id64String } = await createNewModelAndCategory(requestContext, imodeldb);
+
+      // insert element with unique aspect
+      const startTime1 = new Date().getTime();
       eleId = imodeldb.elements.insertElement(IModelTestUtils.createPhysicalObject(imodeldb, r.modelId, r.spatialCategoryId));
       aspectProps = {
         classFullName: "DgnPlatformTest:TestUniqueAspectNoHandler",
         element: { id: eleId },
         testUniqueAspectProperty: "UniqueAspectInsertTest1",
       };
-
-      // insert element unique aspect
-      const startTime1 = new Date().getTime();
       imodeldb.elements.insertAspect(aspectProps);
       const endTime1 = new Date().getTime();
       const elapsedTime1 = (endTime1 - startTime1) / 1000.0;
       totalTimeInsert = totalTimeInsert + elapsedTime1;
 
-      const aspects: ElementAspect[] = imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
-      assert.isTrue(aspects.length === 1);
-      assert.equal(aspects[0].testUniqueAspectProperty, aspectProps.testUniqueAspectProperty);
+      // read element with unique aspect
+      const startTime4 = new Date().getTime();
+      const returnEle1 = imodeldb.elements.getElement(eleId);
+      const aspects4: ElementAspect[] = imodeldb.elements.getAspects(returnEle1.id, aspectProps.classFullName);
+      const endTime4 = new Date().getTime();
+      const elapsedTime4 = (endTime4 - startTime4) / 1000.0;
+      assert.exists(returnEle1);
+      totalTimeRead = totalTimeRead + elapsedTime4;
+      assert.isTrue(aspects4.length === 1);
 
-      // update element unique aspect
-      aspects[0].testUniqueAspectProperty = "UniqueAspectInsertTest1-Updated";
+      // update element with unique aspect
       const startTime2 = new Date().getTime();
+      const returnEle2 = imodeldb.elements.getElement(eleId);
+      returnEle1.userLabel = returnEle1.userLabel + "updated";
+      imodeldb.elements.updateElement(returnEle1);
+      const aspects: ElementAspect[] = imodeldb.elements.getAspects(returnEle2.id, aspectProps.classFullName);
+      aspects[0].testUniqueAspectProperty = "UniqueAspectInsertTest1-Updated";
       imodeldb.elements.updateAspect(aspects[0]);
       const endTime2 = new Date().getTime();
       const elapsedTime2 = (endTime2 - startTime2) / 1000.0;
-      totalTimeUpdate = totalTimeUpdate + elapsedTime2;
-
       const aspectsUpdated: ElementAspect[] = imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
       assert.equal(aspectsUpdated.length, 1);
       assert.equal(aspectsUpdated[0].testUniqueAspectProperty, "UniqueAspectInsertTest1-Updated");
+      totalTimeUpdate = totalTimeUpdate + elapsedTime2;
 
-      // delete element unique aspect
+      // delete element with unique aspect
       const startTime3 = new Date().getTime();
-      imodeldb.elements.deleteAspect(aspects[0].id);
+      imodeldb.elements.deleteElement(eleId);
       const endTime3 = new Date().getTime();
       const elapsedTime3 = (endTime3 - startTime3) / 1000.0;
       totalTimeDelete = totalTimeDelete + elapsedTime3;
-      try {
-        imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
-        assert.isTrue(false, "Expected this line to be skipped");
-      } catch (error) {
-        assert.isTrue(error instanceof Error);
-      }
     }
-    fs.appendFileSync(csvPath, "Insert, Unique Aspect," + totalTimeInsert + "," + count1 + "\n");
-    fs.appendFileSync(csvPath, "Update, Unique Aspect," + totalTimeUpdate + "," + count1 + "\n");
-    fs.appendFileSync(csvPath, "Delete, Unique Aspect," + totalTimeDelete + "," + count1 + "\n");
     imodeldb.close(requestContext).catch();
+    reporter.addEntry("ElementAspectPerfomance", "UniqueAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Insertion", totalTimeInsert, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "UniqueAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Updation", totalTimeUpdate, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "UniqueAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Deletion", totalTimeDelete, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "UniqueAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Read", totalTimeRead, { "ElementCount ": count1 });
   });
 
-  it("Element Multi Aspect Insertion, Updation, Deletion", async () => {
+  it("MultiAspectElement-Insertion-Updation-Deletion-Read", async () => {
     const imodeldb: IModelDb = await IModelDb.open(requestContext, projectId, imodelId, OpenParams.pullOnly(), IModelVersion.latest());
     assert.exists(imodeldb);
 
@@ -130,27 +188,34 @@ describe("ElementAspectPerfomance", () => {
     let totalTimeInsert = 0;
     let totalTimeUpdate = 0;
     let totalTimeDelete = 0;
+    let totalTimeRead = 0;
 
     for (let m = 0; m < count1; ++m) {
       const r: { modelId: Id64String, spatialCategoryId: Id64String } = await createNewModelAndCategory(requestContext, imodeldb);
-      eleId = imodeldb.elements.insertElement(IModelTestUtils.createPhysicalObject(imodeldb, r.modelId, r.spatialCategoryId));
 
+      // insert element with multi aspect
+      const startTime1 = new Date().getTime();
+      eleId = imodeldb.elements.insertElement(IModelTestUtils.createPhysicalObject(imodeldb, r.modelId, r.spatialCategoryId));
+      assert.exists(eleId);
       const aspectProps: ElementAspectProps = {
         classFullName: "DgnPlatformTest:TestMultiAspectNoHandler",
         element: { id: eleId },
         testMultiAspectProperty: "MultiAspectInsertTest1",
       };
-
-      // insert element multi aspect
-      const startTime1 = new Date().getTime();
       imodeldb.elements.insertAspect(aspectProps);
       const endTime1 = new Date().getTime();
       const elapsedTime1 = (endTime1 - startTime1) / 1000.0;
       totalTimeInsert = totalTimeInsert + elapsedTime1;
 
-      let aspects: ElementAspect[] = imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
+      // read element with multi aspect
+      const startTime4 = new Date().getTime();
+      const returnEle1 = imodeldb.elements.getElement(eleId);
+      const aspects: ElementAspect[] = imodeldb.elements.getAspects(returnEle1.id, aspectProps.classFullName);
+      const endTime4 = new Date().getTime();
+      const elapsedTime4 = (endTime4 - startTime4) / 1000.0;
+      assert.exists(returnEle1);
+      totalTimeRead = totalTimeRead + elapsedTime4;
       assert.isAtLeast(aspects.length, 1);
-      const numAspects = aspects.length;
 
       let found: boolean = false;
       let foundIndex: number = -1;
@@ -163,31 +228,33 @@ describe("ElementAspectPerfomance", () => {
       }
       assert.isTrue(found);
 
-      // update element multi aspect
-      aspects[foundIndex].testMultiAspectProperty = "MultiAspectInsertTest1-Updated";
+      // update element with multi aspect
       const startTime2 = new Date().getTime();
-      imodeldb.elements.updateAspect(aspects[foundIndex]);
+      const returnEle2 = imodeldb.elements.getElement(eleId);
+      returnEle2.userLabel = returnEle2.userLabel + "updated";
+      imodeldb.elements.updateElement(returnEle2);
+      const aspects1: ElementAspect[] = imodeldb.elements.getAspects(returnEle2.id, aspectProps.classFullName);
+      aspects1[foundIndex].testMultiAspectProperty = "MultiAspectInsertTest1-Updated";
+      imodeldb.elements.updateAspect(aspects1[foundIndex]);
       const endTime2 = new Date().getTime();
       const elapsedTime2 = (endTime2 - startTime2) / 1000.0;
       totalTimeUpdate = totalTimeUpdate + elapsedTime2;
 
       const aspectsUpdated: ElementAspect[] = imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
-      assert.equal(aspectsUpdated.length, aspects.length);
+      assert.equal(aspectsUpdated.length, aspects1.length);
       assert.equal(aspectsUpdated[foundIndex].testMultiAspectProperty, "MultiAspectInsertTest1-Updated");
 
-      // delete element multi aspect
+      // delete element with multi aspect
       const startTime3 = new Date().getTime();
-      imodeldb.elements.deleteAspect(aspects[foundIndex].id);
+      imodeldb.elements.deleteElement(eleId);
       const endTime3 = new Date().getTime();
       const elapsedTime3 = (endTime3 - startTime3) / 1000.0;
       totalTimeDelete = totalTimeDelete + elapsedTime3;
-
-      aspects = imodeldb.elements.getAspects(eleId, aspectProps.classFullName);
-      assert.equal(numAspects, aspects.length + 1);
     }
-    fs.appendFileSync(csvPath, "Insert, Multi Aspect," + totalTimeInsert + "," + count1 + "\n");
-    fs.appendFileSync(csvPath, "Update, Multi Aspect," + totalTimeUpdate + "," + count1 + "\n");
-    fs.appendFileSync(csvPath, "Delete, Multi Aspect," + totalTimeDelete + "," + count1 + "\n");
-
+    imodeldb.close(requestContext).catch();
+    reporter.addEntry("ElementAspectPerfomance", "MultiAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Insertion", totalTimeInsert, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "MultiAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Updation", totalTimeUpdate, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "MultiAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Deletion", totalTimeDelete, { "ElementCount ": count1 });
+    reporter.addEntry("ElementAspectPerfomance", "MultiAspectElement-Insertion-Updation-Deletion-Read", "Execution time(s) of Read", totalTimeRead, { "ElementCount ": count1 });
   });
 });
