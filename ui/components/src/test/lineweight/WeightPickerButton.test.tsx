baba75cf66435c9ae7a1ebe8ecdf369a4edbbb7d
Bill Steinbock <65047615+bsteinbk@users.noreply.github.com>
2019-06-26 12:20:16 +0000

    Merged PR 33088: Allow weight selection via up/down arrow keys
    Provide better keyboard accessibility by allowing weight to be selected via up/down arrow keys.

--- ui/components/src/test/lineweight/WeightPickerButton.test.tsx
+++ ui/components/src/test/lineweight/WeightPickerButton.test.tsx
@@ -90,7 +90,7 @@ describe("<WeightPickerButton/>", () => {
       // wait for button to receive focus
       await new Promise((r) => { setTimeout(r, 80); });
 
-      renderedComponent.debug();
+      // renderedComponent.debug();
       // focus on weight 2 and press enter key which should close popup
       const node = popupDiv.querySelector(`#${buildIdForWeight(2)}`) as HTMLElement;
       if (node) {
@@ -100,6 +100,46 @@ describe("<WeightPickerButton/>", () => {
         // renderedComponent.debug();
         expect(spyOnWeightPick).to.be.calledOnce;
       }
+    }
+  });
+
+  it("button press should open popup and move selection via arrow (Enter to close)", async () => {
+    const spyOnWeightPick = sinon.spy();
+
+    function handleWeightPick(weight: number): void {
+      expect(weight).to.be.equal(2);
+      spyOnWeightPick();
+    }
+
+    const renderedComponent = render(<WeightPickerButton activeWeight={activeWeight} weights={weights} onLineWeightPick={handleWeightPick} dropDownTitle="test-title" />);
+    expect(renderedComponent.getByTestId("components-weightpicker-button")).to.exist;
+    const pickerButton = renderedComponent.getByTestId("components-weightpicker-button");
+    expect(pickerButton.tagName).to.be.equal("BUTTON");
+    let expandedAttribute = pickerButton.getAttribute("aria-expanded");
+    expect(expandedAttribute).to.be.eq("false");
+
+    fireEvent.click(pickerButton);
+    expandedAttribute = pickerButton.getAttribute("aria-expanded");
+    expect(expandedAttribute).to.be.eq("true");
+
+    // getByTestId will trigger failure if not found so need to add separate 'expect' to test
+    const popupDiv = renderedComponent.getByTestId("components-weightpicker-popup-lines");
+    if (popupDiv) {
+      const title = renderedComponent.getByText("test-title");
+      expect(title).not.to.be.undefined;
+
+      const firstColorButton = popupDiv.firstChild as HTMLElement;
+      expect(firstColorButton).not.to.be.undefined;
+
+      // wait for button to receive focus
+      await new Promise((r) => { setTimeout(r, 80); });
+
+      renderedComponent.debug();
+      fireEvent.keyDown(popupDiv, { key: "ArrowDown" });  // down to 4
+      fireEvent.keyDown(popupDiv, { key: "ArrowUp" });    // back up to 3
+      fireEvent.keyDown(popupDiv, { key: "ArrowUp" });    // up to 2
+      fireEvent.keyDown(popupDiv, { key: "Enter" });
+      expect(spyOnWeightPick).to.be.calledOnce;
     }
   });
 
