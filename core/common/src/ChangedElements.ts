b048b1fd3f7fd05219913546ee5764baa082ec24
Diego Pinate <4107657+diegopinate@users.noreply.github.com>
2019-08-08 11:47:50 -0400

    WIP: #154501: Add new interfaces for version compare changes.

--- core/common/src/ChangedElements.ts
+++ core/common/src/ChangedElements.ts
@@ -3,10 +3,22 @@
 * Licensed under the MIT License. See LICENSE.md in the project root for license terms.
 *--------------------------------------------------------------------------------------------*/
 import { Id64String } from "@bentley/bentleyjs-core";
+import { AxisAlignedBox3dProps } from "./geometry/Placement";
 
 /** @beta */
 export interface ChangedElements {
   elements: Id64String[];
   classIds: Id64String[];
   opcodes: number[];
+  modelIds?: Id64String[];
+}
+
+export interface ChangedModels {
+  modelIds: Id64String[];
+  bboxes: AxisAlignedBox3dProps[];
+}
+
+export interface ChangeData {
+  changedElements: ChangedElements;
+  changedModels: ChangedModels;
 }
