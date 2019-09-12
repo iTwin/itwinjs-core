aa0385cd9e147d994e79e7d34408cc7e320e5a1d
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2019-09-12 16:02:15 -0400

    Revert " #162342 - Element API should use UInt8Array as Binary Type properties"
    This reverts commit ecd478faa8e089edc938f1bc1bde5145c2bc5d3c.

--- core/backend/src/IModelDb.ts
+++ core/backend/src/IModelDb.ts
@@ -35,7 +35,6 @@ import { Relationship, RelationshipProps, Relationships } from "./Relationship";
 import { CachedSqliteStatement, SqliteStatement, SqliteStatementCache } from "./SqliteStatement";
 import { SheetViewDefinition, ViewDefinition } from "./ViewDefinition";
 import { IModelHost } from "./IModelHost";
-import { BinaryPropertyTypeConverter } from "./BinaryPropertyTypeConverter";
 const loggerCategory: string = BackendLoggerCategory.IModelDb;
 
 /** A string that identifies a Txn.
@@ -1423,7 +1422,7 @@ export namespace IModelDb {
       const val = this._iModel.nativeDb.getElement(elementIdArg);
       if (val.error)
         throw new IModelError(val.error.status, "reading element=" + elementIdArg, Logger.logWarning, loggerCategory);
-      return BinaryPropertyTypeConverter.decodeBinaryProps(val.result)! as T;
+      return val.result! as T;
     }
 
     /** Get properties of an Element by Id, FederationGuid, or Code
@@ -1509,8 +1508,8 @@ export namespace IModelDb {
       const iModel = this._iModel;
       const jsClass = iModel.getJsClass<typeof Element>(elProps.classFullName) as any; // "as any" so we can call the protected methods
       jsClass.onInsert(elProps, iModel);
-      const valJson = JSON.stringify(elProps, BinaryPropertyTypeConverter.createReplacerCallback(false));
-      const val = iModel.nativeDb.insertElement(valJson);
+
+      const val = iModel.nativeDb.insertElement(JSON.stringify(elProps));
       if (val.error)
         throw new IModelError(val.error.status, "Error inserting element", Logger.logWarning, loggerCategory, () => ({ classFullName: elProps.classFullName }));
 
@@ -1528,7 +1527,7 @@ export namespace IModelDb {
       const jsClass = iModel.getJsClass<typeof Element>(elProps.classFullName) as any; // "as any" so we can call the protected methods
       jsClass.onUpdate(elProps, iModel);
 
-      const stat = iModel.nativeDb.updateElement(JSON.stringify(elProps, BinaryPropertyTypeConverter.createReplacerCallback(false)));
+      const stat = iModel.nativeDb.updateElement(JSON.stringify(elProps));
       if (stat !== IModelStatus.Success)
         throw new IModelError(stat, "Error updating element", Logger.logWarning, loggerCategory, () => ({ elementId: elProps.id }));
 
