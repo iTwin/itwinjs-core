c8c5d3ff1263a3830b4eb0ae953624e24ff86ea1
Ramanujam Raman <32458710+ramanujam-raman@users.noreply.github.com>
2019-10-25 18:46:17 +0000

    Merged PR 51335: Allowed executing jQuery in electron window for ui-test-app.
    Allowed executing jQuery in electron window - this enables the SignIn button to work with a click - it currently only works by pressing 'enter'. (this was a fix done by Seamus Kirby in simple-viewer-app - I'm porting it over to ui-test-app)

---
+++ test-apps/ui-test-app/src/backend/electron/preload.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+if (window.location.href.indexOf("electron://") !== 0) {
+  if (process.env.NODE_ENV === "test") {
+    (window as any).electronRequire = require;
+  } else {
+    (window as any).nodeRequire = require;
+  }
+  delete window.require;
+  delete window.exports;
+  delete window.module;
+}
