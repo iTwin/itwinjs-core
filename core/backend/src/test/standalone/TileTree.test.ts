26dc682b62409de9c3678f1e154343464ab9314a
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2019-10-29 11:24:18 -0400

    tests.

--- core/backend/src/test/standalone/TileTree.test.ts
+++ core/backend/src/test/standalone/TileTree.test.ts
@@ -67,21 +67,86 @@ function createIModel(): IModelDb {
   return IModelDb.createSnapshot(IModelTestUtils.prepareOutputFile("TileTree", name), props);
 }
 
+function scaleProjectExtents(db: IModelDb, scale: number): Range3d {
+  const range = db.projectExtents.clone();
+  range.scaleAboutCenterInPlace(scale);
+  db.updateProjectExtents(range);
+  db.saveChanges();
+  return range.clone();
+}
+
 describe("purgeTileTrees", () => {
-  it("should update when project extents change", async () => {
+  it("should update after purge when project extents change", async () => {
     const db = createIModel();
     const modelId = insertPhysicalModel(db);
 
     // The "_1-" holds the flag saying to use the project extents as the range of the tile tree.
     const treeId = "5_1-" + modelId;
     const context = new BackendRequestContext();
-    const tree = await db.tiles.requestTileTreeProps(context, treeId);
+    let tree = await db.tiles.requestTileTreeProps(context, treeId);
     expect(tree).not.to.be.undefined;
     expect(tree.id).to.equal(treeId);
 
-    const range = Range3d.fromJSON(tree.rootTile.range);
+    let range = Range3d.fromJSON(tree.rootTile.range);
     expect(range.isNull).to.be.false;
     expect(range.isAlmostEqual(defaultExtents)).to.be.true;
+    expect(tree.rootTile.contentRange).to.be.undefined;
+    expect(tree.rootTile.isLeaf).to.be.true;
+
+    // Change the project extents - nothing should change - we haven't yet purged our model's tile tree.
+    let newExtents = scaleProjectExtents(db, 2.0);
+
+    tree = await db.tiles.requestTileTreeProps(context, treeId);
+    expect(tree).not.to.be.undefined;
+    expect(tree.id).to.equal(treeId);
+
+    range = Range3d.fromJSON(tree.rootTile.range);
+    expect(range.isNull).to.be.false;
+    expect(range.isAlmostEqual(defaultExtents)).to.be.true;
+    expect(range.isAlmostEqual(newExtents)).to.be.false;
+    expect(tree.rootTile.contentRange).to.be.undefined;
+    expect(tree.rootTile.isLeaf).to.be.true;
+
+    // Purge tile trees for a specific (non-existent) model - still nothing should change for our model.
+    db.nativeDb.purgeTileTrees(["0x123abc"]);
+
+    tree = await db.tiles.requestTileTreeProps(context, treeId);
+    expect(tree).not.to.be.undefined;
+    expect(tree.id).to.equal(treeId);
+
+    range = Range3d.fromJSON(tree.rootTile.range);
+    expect(range.isNull).to.be.false;
+    expect(range.isAlmostEqual(defaultExtents)).to.be.true;
+    expect(range.isAlmostEqual(newExtents)).to.be.false;
+    expect(tree.rootTile.contentRange).to.be.undefined;
+    expect(tree.rootTile.isLeaf).to.be.true;
+
+    // Purge tile trees for our model - now we should get updated tile tree props.
+    db.nativeDb.purgeTileTrees([modelId]);
+
+    tree = await db.tiles.requestTileTreeProps(context, treeId);
+    expect(tree).not.to.be.undefined;
+    expect(tree.id).to.equal(treeId);
+
+    range = Range3d.fromJSON(tree.rootTile.range);
+    expect(range.isNull).to.be.false;
+    expect(range.isAlmostEqual(defaultExtents)).to.be.false;
+    expect(range.isAlmostEqual(newExtents)).to.be.true;
+    expect(tree.rootTile.contentRange).to.be.undefined;
+    expect(tree.rootTile.isLeaf).to.be.true;
+
+    // Change extents again and purge tile trees for all loaded models (by passing `undefined` for model Ids).
+    newExtents = scaleProjectExtents(db, 0.75);
+    db.nativeDb.purgeTileTrees(undefined);
+
+    tree = await db.tiles.requestTileTreeProps(context, treeId);
+    expect(tree).not.to.be.undefined;
+    expect(tree.id).to.equal(treeId);
+
+    range = Range3d.fromJSON(tree.rootTile.range);
+    expect(range.isNull).to.be.false;
+    expect(range.isAlmostEqual(defaultExtents)).to.be.false;
+    expect(range.isAlmostEqual(newExtents)).to.be.true;
     expect(tree.rootTile.contentRange).to.be.undefined;
     expect(tree.rootTile.isLeaf).to.be.true;
   });
