475770b512936dc1b8c3cc71f7e2c64a79a55fb8
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2019-11-22 18:28:31 +0000

    Merged PR 55629: Fix for warning from static analysis
    I had the wrong line number for the previous fix.

--- core/bentley/src/Id.ts
+++ core/bentley/src/Id.ts
@@ -78,7 +78,6 @@ export namespace Id64 {
 
     const len = id.length;
     const start = (len > 12) ? (len - 10) : 2;
-
     return toHex(id.slice(start));
   }
 
@@ -87,14 +86,8 @@ export namespace Id64 {
     if (isInvalid(id))
       return 0;
 
-    const str = id.toString();
-    let start = 2;
-    const len = str.length;
-    if (len <= 12)
-      return 0;
-
-    start = (len - 10);
-    return toHex(str.slice(2, start));
+    const len = id.length;
+    return (len <= 12) ? 0 : toHex(id.slice(2, len - 10));
   }
 
   /** Create an Id64String from its JSON representation.
