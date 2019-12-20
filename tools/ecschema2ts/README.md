dc8d63eb45ec1b89a6dd213724c438f19f7da10f
Chris Lawson <32881725+christophermlawson@users.noreply.github.com>
2019-12-20 22:05:39 +0000

    Merged PR 60528: Merge ec2ts-move into master
    Related work items: #230536

---
+++ tools/ecschema2ts/README.md
@@ -0,0 +1,47 @@
+﻿# ecschema2ts
+
+ecschema2ts is a command-line tool that takes an EC3.1 BIS ECSchema xml file and outputs a valid Typescript module that uses iModelJs.
+
+## Quick Overview
+```sh
+$ npm install -g @bentley/ecschema2ts
+
+$ ecschema2ts -i C:\Path\To\Schema\Domain.ecschema.xml -o C:\Desired\Output\Path\
+```
+
+## Getting Started
+
+### Installation
+
+Install globally:
+
+```sh
+$ npm install -g @bentley/ecschema2ts
+```
+
+### Creating a Typescript module
+
+To create a Typescript file from the an ECSchema, run:
+
+```sh
+$ ecschema2ts -i C:\Path\To\Schema\Domain.ecschema.xml -o C:\Desired\Output\Path\
+```
+
+## Updating to new version
+
+Since the package is installed globally, updating has a different syntax than normal. To update the package globally, run:
+
+```sh
+$ npm update -g @bentley/ecschema2ts
+```
+
+## Known Issues
+
+- The ordering of the Typescript classes may be out of order preventing compilation of the typescript file. A workaround is to reorder the classes by hand.
+  - [TFS 793045](https://tfs.bentley.com/tfs/ProductLine/Platform%20Technology/ECTech/_workitems?id=793045&_a=edit&triage=true)
+
+## Troubleshooting
+
+- Are you have issues converting your ECSchema?
+  - Check to make sure your ECSchema version is EC3.1
+  - Check if the BIS ECSchema passes validation, [check ECSchema status](https://bentley.sharepoint.com/sites/BIS/Lists/Schema%20Development%20Status/AllItems.aspx?viewpath=%2Fsites%2FBIS%2FLists%2FSchema%20Development%20Status%2FAllItems.aspx).
---
+++ tools/ecschema2ts/src/test/ecschema2ts_cache.test.ts
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
+* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+*--------------------------------------------------------------------------------------------*/
+import { ECSchemaToTs } from "../ecschema2ts";
+import { assert } from "chai";
+import * as utils from "./utilities/utils";
+import { SchemaContext } from "@bentley/ecschema-metadata";
+import { SchemaXmlFileLocater } from "@bentley/ecschema-locaters";
+
+describe("BisCore Cache test", () => {
+  it("For ECEntity class with BaseClass in BisCore, find the correct BisCore props interface to extend", () => {
+    const schemaXml = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <ECSchema schemaName="MyDomain" alias="mydomain" version="01.00.00" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2">
+        <ECSchemaReference name="BisCore" version="01.00.00" alias="bis"/>
+        <ECSchemaReference name="ECDbMap" version="02.00.00" alias="ecdbmap"/>
+
+        <ECEntityClass typeName="Building" modifier="Sealed">
+          <BaseClass>bis:SpatialLocationElement</BaseClass>
+          <BaseClass>bis:IParentElement</BaseClass>
+        </ECEntityClass>
+      </ECSchema>`;
+
+    const expectedSchemaString =
+`import { ClassRegistry, Schema, Schemas } from "@bentley/imodeljs-backend";
+import * as elementsModule from "./MyDomainElements";
+
+export class MyDomain extends Schema {
+  public static get schemaName(): string { return "MyDomain"; }
+
+  public static registerSchema() {
+    if (!Schemas.getRegisteredSchema(MyDomain.name))
+      Schemas.registerSchema(MyDomain);
+  }
+
+  protected constructor() {
+    super();
+    ClassRegistry.registerModule(elementsModule, MyDomain);
+  }
+}\n\n`;
+
+    const expectedElementString =
+`import { SpatialLocationElement, IModelDb } from "@bentley/imodeljs-backend";
+import { GeometricElement3dProps } from "@bentley/imodeljs-common";
+
+export class Building extends SpatialLocationElement {
+  public static get className(): string { return "Building"; }
+
+  public constructor (props: GeometricElement3dProps, iModel: IModelDb) {
+    super(props, iModel);
+  }
+}\n\n`;
+
+    const schemaLocator = new SchemaXmlFileLocater();
+    schemaLocator.addSchemaSearchPath(utils.getAssetsDir() + "schema3.2");
+    const context = new SchemaContext();
+    context.addLocater(schemaLocator);
+
+    const schema = utils.deserializeXml(context, schemaXml);
+    const ecschema2ts = new ECSchemaToTs();
+    const { schemaTsString, elemTsString, propsTsString } = ecschema2ts.convertSchemaToTs(schema);
+    assert.equal(schemaTsString, expectedSchemaString);
+    assert.equal(propsTsString, `\n`);
+    assert.equal(elemTsString, expectedElementString);
+  });
+});
