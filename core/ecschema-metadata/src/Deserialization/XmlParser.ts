68ff8499dcd277792162fac3e7164109e679f818
Chris Lawson <32881725+christophermlawson@users.noreply.github.com>
2020-01-07 15:47:26 +0000

    Merged PR 61670: Merge isstrict-changes into master
    Setting ECEnumeration property 'isStrict' to true when parsing XML if the attribute is not set in schema XML.  Also includes BIS docs updates.
    
    Related work items: #256630

--- core/ecschema-metadata/src/Deserialization/XmlParser.ts
+++ core/ecschema-metadata/src/Deserialization/XmlParser.ts
@@ -303,8 +303,10 @@ export class XmlParser extends AbstractParser<Element> {
     else
       throw new ECObjectsError(ECObjectsStatus.InvalidECJson, `The Enumeration ${this._currentItemFullName} has an invalid 'backingTypeName' attribute. It should be either "int" or "string".`);
 
-    const _isStrict = this.getRequiredAttribute(xmlElement, "isStrict",
-      `The Enumeration ${this._currentItemFullName} is missing the required 'isStrict' attribute.`);
+    let _isStrict: string | undefined = this.getOptionalAttribute(xmlElement, "isStrict");
+    if (_isStrict === undefined)
+      _isStrict = "true";
+
     const isStrict = this.parseBoolean(_isStrict,
       `The Enumeration ${this._currentItemFullName} has an invalid 'isStrict' attribute. It should either be "true" or "false".`);
 
