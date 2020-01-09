9aba239170350268e139f96433846e3ff2a79c30
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2020-01-09 13:50:18 +0000

    Merged PR 62132: Presentation: Fix incorrect copyright headers
    Some new files where added with incorrect copyright headers in PR !60425

--- presentation/frontend/src/LocalizationHelper.ts
+++ presentation/frontend/src/LocalizationHelper.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import { I18NNamespace } from "@bentley/imodeljs-i18n";
 import { Node, LabelCompositeValue, LabelDefinition, Content, Item } from "@bentley/presentation-common";
 import { Presentation } from "./Presentation";
