8cb245000bab9f5bee27a6f1d4de55c8298451b3
Brien Bastings <65233531+bbastings@users.noreply.github.com>
2020-03-13 18:35:45 +0000

    Merged PR 73858: Tool settings to enter new lat/long, altitude, and north angle.
    Tool settings to enter new lat/long, altitude, and north angle.

--- extensions/projectlocation/src/ProjectGeolocation.ts
+++ extensions/projectlocation/src/ProjectGeolocation.ts
@@ -3,11 +3,12 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { IModelApp, PrimitiveTool, BeButtonEvent, EventHandled, Viewport, EditManipulator, AccuDrawHintBuilder, DecorateContext, CanvasDecoration, ToolAssistance, ToolAssistanceSection, ToolAssistanceInstruction, ToolAssistanceImage, ToolAssistanceInputMethod, CoreTools } from "@bentley/imodeljs-frontend";
+import { IModelApp, PrimitiveTool, BeButtonEvent, EventHandled, Viewport, EditManipulator, AccuDrawHintBuilder, DecorateContext, CanvasDecoration, ToolAssistance, ToolAssistanceSection, ToolAssistanceInstruction, ToolAssistanceImage, ToolAssistanceInputMethod, CoreTools, AngleDescription, LengthDescription } from "@bentley/imodeljs-frontend";
 import { Point3d, Vector3d, Matrix3d, Ray3d, Angle, XYAndZ } from "@bentley/geometry-core";
 import { Cartographic } from "@bentley/imodeljs-common";
 import { ProjectExtentsClipDecoration } from "./ProjectExtentsDecoration";
-import { translatePrompt } from "./ProjectLocation";
+import { translatePrompt, translateMessage } from "./ProjectLocation";
+import { DialogItemValue, PropertyDescription, DialogItem, DialogPropertySyncItem } from "@bentley/ui-abstract";
 
 /** @internal */
 class LabelDecoration implements CanvasDecoration {
@@ -73,10 +74,115 @@ export class ProjectGeolocationPointTool extends PrimitiveTool {
   public onRestartTool(): void { this.exitTool(); }
   public onUnsuspend(): void { this.provideToolAssistance(); }
 
+  private _latitudeValue: DialogItemValue = { value: 0.0 };
+  public get latitude(): number { return this._latitudeValue.value as number; }
+  public set latitude(value: number) { this._latitudeValue.value = value; }
+  private static _latitudeName = "latitude";
+  private static _latitudeDescription?: AngleDescription;
+  private _getLatitudeDescription = (): PropertyDescription => {
+    if (!ProjectGeolocationPointTool._latitudeDescription)
+      ProjectGeolocationPointTool._latitudeDescription = new AngleDescription(ProjectGeolocationPointTool._latitudeName, translateMessage("Latitude"));
+    return ProjectGeolocationPointTool._latitudeDescription;
+  }
+
+  private _longitudeValue: DialogItemValue = { value: 0.0 };
+  public get longitude(): number { return this._longitudeValue.value as number; }
+  public set longitude(value: number) { this._longitudeValue.value = value; }
+  private static _longitudeName = "longitude";
+  private static _longitudeDescription?: AngleDescription;
+  private _getLongitudeDescription = (): PropertyDescription => {
+    if (!ProjectGeolocationPointTool._longitudeDescription)
+      ProjectGeolocationPointTool._longitudeDescription = new AngleDescription(ProjectGeolocationPointTool._longitudeName, translateMessage("Longitude"));
+    return ProjectGeolocationPointTool._longitudeDescription;
+  }
+
+  private _altitudeValue: DialogItemValue = { value: 0.0 };
+  public get altitude(): number { return this._altitudeValue.value as number; }
+  public set altitude(value: number) { this._altitudeValue.value = value; }
+  private static _altitudeName = "altitude";
+  private static _altitudeDescription?: LengthDescription;
+  private _getAltitudeDescription = (): PropertyDescription => {
+    if (!ProjectGeolocationPointTool._altitudeDescription)
+      ProjectGeolocationPointTool._altitudeDescription = new LengthDescription(ProjectGeolocationPointTool._altitudeName, CoreTools.translate("Measure.Labels.Altitude"));
+    return ProjectGeolocationPointTool._altitudeDescription;
+  }
+
+  private _northValue: DialogItemValue = { value: 0.0 };
+  public get north(): number { return this._northValue.value as number; }
+  public set north(value: number) { this._northValue.value = value; }
+  private static _northName = "north";
+  private static _northDescription?: AngleDescription;
+  private _getNorthDescription = (): PropertyDescription => {
+    if (!ProjectGeolocationPointTool._northDescription)
+      ProjectGeolocationPointTool._northDescription = new AngleDescription(ProjectGeolocationPointTool._northName, translateMessage("North"));
+    return ProjectGeolocationPointTool._northDescription;
+  }
+
+  private syncToolSettingsCoordinates(): void {
+    const syncData: DialogPropertySyncItem[] = []; // NOTE: Check that formatted quantity descriptions are defined, i.e. abstract ui for tool settings is implemented...
+
+    if (ProjectGeolocationPointTool._latitudeDescription) {
+      const newLatitudeDisplayValue = ProjectGeolocationPointTool._latitudeDescription.format(this.latitude);
+      const latitudeValue: DialogItemValue = { value: this.latitude, displayValue: newLatitudeDisplayValue };
+      const syncLatitudeItem: DialogPropertySyncItem = { value: latitudeValue, propertyName: ProjectGeolocationPointTool._latitudeName, isDisabled: !this._accept };
+      syncData.push(syncLatitudeItem);
+    }
+
+    if (ProjectGeolocationPointTool._longitudeDescription) {
+      const newLongitudeDisplayValue = ProjectGeolocationPointTool._longitudeDescription.format(this.longitude);
+      const longitudeValue: DialogItemValue = { value: this.longitude, displayValue: newLongitudeDisplayValue };
+      const syncLongitudeItem: DialogPropertySyncItem = { value: longitudeValue, propertyName: ProjectGeolocationPointTool._longitudeName, isDisabled: !this._accept };
+      syncData.push(syncLongitudeItem);
+    }
+
+    if (ProjectGeolocationPointTool._altitudeDescription) {
+      const newAltitudeDisplayValue = ProjectGeolocationPointTool._altitudeDescription.format(this.altitude);
+      const altitudeValue: DialogItemValue = { value: this.altitude, displayValue: newAltitudeDisplayValue };
+      const syncAltitudeItem: DialogPropertySyncItem = { value: altitudeValue, propertyName: ProjectGeolocationPointTool._altitudeName, isDisabled: !this._accept };
+      syncData.push(syncAltitudeItem);
+    }
+
+    if (ProjectGeolocationPointTool._northDescription) {
+      const newNorthDisplayValue = ProjectGeolocationPointTool._northDescription.format(this.north);
+      const northValue: DialogItemValue = { value: this.north, displayValue: newNorthDisplayValue };
+      const syncNorthItem: DialogPropertySyncItem = { value: northValue, propertyName: ProjectGeolocationPointTool._northName, isDisabled: !this._accept };
+      syncData.push(syncNorthItem);
+    }
+
+    if (0 !== syncData.length)
+      this.syncToolSettingsProperties(syncData);
+  }
+
+  public applyToolSettingPropertyChange(updatedValue: DialogPropertySyncItem): boolean {
+    if (updatedValue.propertyName === ProjectGeolocationPointTool._latitudeName) {
+      this.latitude = updatedValue.value.value as number;
+      IModelApp.toolAdmin.toolSettingsState.saveToolSettingProperty(this.toolId, { propertyName: ProjectGeolocationPointTool._latitudeName, value: this._latitudeValue });
+    } else if (updatedValue.propertyName === ProjectGeolocationPointTool._longitudeName) {
+      this.longitude = updatedValue.value.value as number;
+      IModelApp.toolAdmin.toolSettingsState.saveToolSettingProperty(this.toolId, { propertyName: ProjectGeolocationPointTool._longitudeName, value: this._longitudeValue });
+    } else if (updatedValue.propertyName === ProjectGeolocationPointTool._altitudeName) {
+      this.altitude = updatedValue.value.value as number;
+      IModelApp.toolAdmin.toolSettingsState.saveToolSettingProperty(this.toolId, { propertyName: ProjectGeolocationPointTool._altitudeName, value: this._altitudeValue });
+    } else if (updatedValue.propertyName === ProjectGeolocationPointTool._northName) {
+      this.north = updatedValue.value.value as number;
+      IModelApp.toolAdmin.toolSettingsState.saveToolSettingProperty(this.toolId, { propertyName: ProjectGeolocationPointTool._northName, value: this._northValue });
+    }
+    return true;
+  }
+
+  public supplyToolSettingsProperties(): DialogItem[] | undefined {
+    const toolSettings = new Array<DialogItem>();
+    toolSettings.push({ value: this._latitudeValue, property: this._getLatitudeDescription(), isDisabled: true, editorPosition: { rowPriority: 1, columnIndex: 2 } });
+    toolSettings.push({ value: this._longitudeValue, property: this._getLongitudeDescription(), isDisabled: true, editorPosition: { rowPriority: 2, columnIndex: 2 } });
+    toolSettings.push({ value: this._altitudeValue, property: this._getAltitudeDescription(), isDisabled: true, editorPosition: { rowPriority: 3, columnIndex: 2 } });
+    toolSettings.push({ value: this._northValue, property: this._getNorthDescription(), isDisabled: true, editorPosition: { rowPriority: 4, columnIndex: 2 } });
+    return toolSettings;
+  }
+
   protected provideToolAssistance(): void {
     const acceptMsg = CoreTools.translate(undefined === this._origin ? "ElementSet.Inputs.AcceptPoint" : "ElementSet.Inputs.Accept");
     const rejectMsg = CoreTools.translate("ElementSet.Inputs.Cancel");
-    const mainInstruction = ToolAssistance.createInstruction(this.iconSpec, translatePrompt(undefined === this._origin ? "IdentifyRefPoint" : "ConfirmCoordinates"));
+    const mainInstruction = ToolAssistance.createInstruction(this.iconSpec, translatePrompt(undefined === this._origin ? "IdentifyKnownLocation" : "ConfirmCoordinates"));
     const sections: ToolAssistanceSection[] = [];
 
     const mouseInstructions: ToolAssistanceInstruction[] = [];
@@ -149,29 +255,49 @@ export class ProjectGeolocationPointTool extends PrimitiveTool {
     return EventHandled.No;
   }
 
+  public acceptKnownLocation(ev: BeButtonEvent): void {
+    const deco = ProjectExtentsClipDecoration.get();
+    if (undefined === deco)
+      return;
+
+    this._accept = true; // Require explict accept to give user a chance to change values...
+    this._origin = ev.point.clone();
+    this._labelDeco = new LabelDecoration(this._origin, translatePrompt("SpecifyCoordinates"));
+
+    if (this.iModel.isGeoLocated) {
+      const cartographic = this.iModel.spatialToCartographicFromEcef(this._origin);
+      this.longitude = cartographic.longitude;
+      this.latitude = cartographic.latitude;
+      this.altitude = cartographic.height;
+      this.north = deco.getClockwiseAngleToNorth().radians;
+    }
+
+    this.syncToolSettingsCoordinates();
+    this.pulseDecoration(); // Pulse to indicate that we are waiting for user input...
+    this.setupAndPromptForNextAction();
+  }
+
+  public acceptCoordinates(): void {
+    const deco = ProjectExtentsClipDecoration.get();
+    if (undefined === deco)
+      return;
+
+    const origin = new Cartographic(this.longitude, this.latitude, this.altitude);
+    if (!deco.updateEcefLocation(origin, this._origin, Angle.createRadians(this.north)))
+      return;
+
+    this.onReinitialize(); // Calls onRestartTool to exit...
+  }
+
   public async onDataButtonDown(ev: BeButtonEvent): Promise<EventHandled> {
     if (undefined === ev.viewport)
       return EventHandled.No; // Shouldn't really happen...
 
-    if (!this._accept) {
-      // ### TODO Synch tool settings w/current lat/long/alt/angle...
-      this._accept = true; // Require explict accept to give user a chance to change values...
-      this._origin = ev.point.clone();
-      this._labelDeco = new LabelDecoration(this._origin, translatePrompt("SpecifyCoordinates"));
-      this.setupAndPromptForNextAction();
-      this.pulseDecoration(); // Pulse to indicate that we are waiting for user input...
-      return EventHandled.No;
-    }
+    if (this._accept)
+      this.acceptCoordinates();
+    else
+      this.acceptKnownLocation(ev);
 
-    this._accept = false;
-    this._scale = 1.0;
-    const deco = ProjectExtentsClipDecoration.get();
-    if (undefined !== deco) {
-      const origin = new Cartographic(Angle.createDegrees(2.35).radians, Angle.createDegrees(48.85).radians); // ### TODO: Need UI for lat/long/alt/angle...
-      deco.updateEcefLocation(origin, this._origin);
-    }
-
-    this.onReinitialize(); // Calls onRestartTool to exit...
     return EventHandled.No;
   }
 
