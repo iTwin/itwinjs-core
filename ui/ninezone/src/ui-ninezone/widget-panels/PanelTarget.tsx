6a43ba5f1813192e284dcf96b8caab30b9841c01
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2020-03-20 13:09:41 +0000

    Merged PR 75146: Add missing packageDocumentation tags
    - Add missing packageDocumentation tags.
    - Rush change.

--- ui/ninezone/src/ui-ninezone/widget-panels/PanelTarget.tsx
+++ ui/ninezone/src/ui-ninezone/widget-panels/PanelTarget.tsx
@@ -2,7 +2,9 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-/** @module WidgetPanels */
+/** @packageDocumentation
+ * @module WidgetPanels
+ */
 
 import classnames from "classnames";
 import * as React from "react";
