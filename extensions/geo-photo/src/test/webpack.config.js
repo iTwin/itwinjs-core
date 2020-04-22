6e022a8a1b70f717b5832e7a2eca19432283431c
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-04-22 13:17:47 +0000

    Fix test webpack configs for electron
    undefined

--- extensions/geo-photo/src/test/webpack.config.js
+++ extensions/geo-photo/src/test/webpack.config.js
@@ -48,6 +48,9 @@ function createConfig(shouldInstrument) {
     optimization: {
       nodeEnv: "production"
     },
+    externals: {
+      electron: "commonjs electron",
+    },
     plugins: [
       // Makes some environment variables available to the JS code, for example:
       // if (process.env.NODE_ENV === "development") { ... }. See `./env.js`.
