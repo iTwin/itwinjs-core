22a32a97e95f1d2596a205dfa1c1cdff5d26e122
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-04-30 21:00:53 +0000

    Merged PR 84012: Add react-dom to the list of exceptions to include in the global scope

--- tools/webpack-core/src/plugins/IModeljsLibraryImportsPlugin.ts
+++ tools/webpack-core/src/plugins/IModeljsLibraryImportsPlugin.ts
@@ -29,7 +29,7 @@ export class IModeljsLibraryImportsPlugin {
     compiler.hooks.normalModuleFactory.tap("IModeljsLibraryImportsPlugin", (normalModuleFactory) => {
       normalModuleFactory.hooks.module.tap("IModeljsLibraryImportsPlugin", (mod, info) => {
         const pkgJson = info.resourceResolveData.descriptionFileData;
-        if ((pkgJson.name === info.rawRequest && pkgJson.imodeljsSharedLibrary) || pkgJson.name === "react") {
+        if ((pkgJson.name === info.rawRequest && pkgJson.imodeljsSharedLibrary) || pkgJson.name === "react" || pkgJson.name === "react-dom") {
           mod.___IS_BENTLEY = true;
           mod.___IMJS_VER = pkgJson.version;
           mod.___IMJS_NAME = pkgJson.name;
