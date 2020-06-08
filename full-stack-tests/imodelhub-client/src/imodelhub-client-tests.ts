cce7c287b8ace2b3384ab07c2f6cff1edae0256e
Jonathan Hamel <11891027+hamjo@users.noreply.github.com>
2020-06-08 15:55:37 +0000

    Merged PR 91310: imodelhub-client-tests - Exposed API to run tests
    Updated launch.json for imodelhub client tests
    
    Related work items: #202622

--- full-stack-tests/imodelhub-client/src/imodelhub-client-tests.ts
+++ full-stack-tests/imodelhub-client/src/imodelhub-client-tests.ts
@@ -3,5 +3,4 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-export * from "./ResponseBuilder";
-export * from "./imodelhub/TestUtils";
+export * from "./RunTests";
