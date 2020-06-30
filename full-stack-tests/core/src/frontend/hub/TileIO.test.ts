af1f55d9354a44323a3c690a63ce3a9312933ff2
Paul.Connelly <22944042+pmconne@users.noreply.github.com>
2020-06-30 19:20:17 +0000

    Merged PR 96600: Fix missing instanceable geometry
    If tile contained geometry too small to contribute, always sub-divide it rather than magnifying; otherwise the small geometry in child tiles may suffer from quantization error.
    
    Corresponding native PR: https://bentleycs.visualstudio.com/iModelTechnologies/_git/imodel02/pullrequest/96597

--- full-stack-tests/core/src/frontend/hub/TileIO.test.ts
+++ full-stack-tests/core/src/frontend/hub/TileIO.test.ts
@@ -949,7 +949,7 @@ describe("TileAdmin", () => {
       }
 
       public static async test(imodel: IModelConnection) {
-        await this.testPrimaryTree(imodel, "a_1-0x1c");
+        await this.testPrimaryTree(imodel, "b_1-0x1c");
 
         // ###TODO: The tree Id is validated on back-end and rejected if the animation source Id does not identify an existing DisplayStyle with an attached schedule script.
         // Our test iModel lacks any such styles so test will fail.
