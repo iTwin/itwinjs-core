1450ad1bae4f4dd241e43cb431a3b9f088a198f1
Mantas Boiko <xmontex@gmail.com>
2020-09-28 19:14:30 +0000

    Merged PR 116083: [ProjectShareClient] fix tests
    [ProjectShareClient] fix tests
    
    Related work items: #279516, #279517

--- clients/projectshare/src/test/ProjectShareClient.test.ts
+++ clients/projectshare/src/test/ProjectShareClient.test.ts
@@ -40,21 +40,21 @@ describe("ProjectShareClient (#integration)", () => {
     testFile.name = "sap.txt";
     testFile.size = 32;
     testFile.description = "test";
-    const file: ProjectShareFile = await projectShareClient.createFile(requestContext, projectId, folder2A.wsgId, testFile); // create new file with file exist property as false
+    const file: ProjectShareFile = await projectShareClient.createFile(requestContext, projectId, folder2B.wsgId, testFile); // create new file with file exist property as false
     chai.assert.strictEqual(file.description, testFile.description);
     chai.assert.equal(file.size, testFile.size);
     const data = { name: "John", age: 30, city: "New York" };
-    const  chagedFile = await projectShareClient.uploadContentInFile(requestContext, projectId, file, JSON.stringify(data)); // upload content into file
-    chai.assert.equal(chagedFile.fileExists, true);
+    const changedFile = await projectShareClient.uploadContentInFile(requestContext, projectId, file, JSON.stringify(data)); // upload content into file
+    chai.assert.equal(changedFile.fileExists, true);
     const res = await projectShareClient.deleteFile(requestContext, projectId, file.wsgId); // Permanent deleting File.
     chai.assert.isUndefined(res);
 
-    const file1: ProjectShareFile = await projectShareClient.createFile(requestContext, projectId, folder2A.wsgId, testFile);
+    const file1: ProjectShareFile = await projectShareClient.createFile(requestContext, projectId, folder2B.wsgId, testFile);
     chai.assert.strictEqual(file1.description, testFile.description);
     chai.assert.equal(file1.size, testFile.size);
     const data1 = { name: "xyz", age: 31, city: "aus" };
-    const chagedFile1 = await projectShareClient.uploadContentInFile(requestContext, projectId, file1, JSON.stringify(data1));
-    chai.assert.equal(chagedFile1.fileExists, true);
+    const changedFile1 = await projectShareClient.uploadContentInFile(requestContext, projectId, file1, JSON.stringify(data1));
+    chai.assert.equal(changedFile1.fileExists, true);
     const res1 = await projectShareClient.deleteFile(requestContext, projectId, file1.wsgId, RecycleOption.SendToRecycleBin); // file move to recycleBin.
     chai.assert.isNotNull(res1);
   });
@@ -109,7 +109,6 @@ describe("ProjectShareClient (#integration)", () => {
     const folder360Images = (await projectShareClient.getFolders(requestContext, projectId, new ProjectShareFolderQuery().inRootFolder(projectId)))[0];
     const subFolders = await projectShareClient.getFolders(requestContext, projectId, new ProjectShareFolderQuery().inFolder(folder360Images.wsgId));
     const folder2A = subFolders[0];
-    const folder2B = subFolders[1];
 
     // inRootFolder
     let files: ProjectShareFile[] = await projectShareClient.getFiles(requestContext, projectId, new ProjectShareFileQuery().inRootFolder(projectId));
@@ -120,9 +119,6 @@ describe("ProjectShareClient (#integration)", () => {
     files = await projectShareClient.getFiles(requestContext, projectId, new ProjectShareFileQuery().inFolder(folder2A.wsgId));
     chai.assert(files);
     chai.assert.strictEqual(files.length, 18);
-    files = await projectShareClient.getFiles(requestContext, projectId, new ProjectShareFileQuery().inFolder(folder2B.wsgId));
-    chai.assert(files);
-    chai.assert.strictEqual(files.length, 0);
 
     // startsWithPath
     files = await projectShareClient.getFiles(requestContext, projectId, new ProjectShareFileQuery().startsWithPath(projectId, "360-Images/2A"));
