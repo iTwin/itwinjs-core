d51c6e07a8899b3a11a09a75ff6e3e6d66aaf885
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-10-01 13:55:52 +0000

    Merged PR 117039: Add git commit hook for copyright header
    The git commit hook is put into the '.git/hooks' directory on a rush install and will fix the copyright header for any staged files.

--- full-stack-tests/core/src/common/IModelHubUserMgr.ts
+++ full-stack-tests/core/src/common/IModelHubUserMgr.ts
@@ -1,7 +1,8 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) 2019 Bentley Systems, Incorporated. All rights reserved.
-* Licensed under the MIT License. See LICENSE.md in the project root for license terms.
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+
 import { BeEvent, ClientRequestContext } from "@bentley/bentleyjs-core";
 import { FrontendAuthorizationClient } from "@bentley/frontend-authorization-client";
 import { AccessToken, UserInfo } from "@bentley/itwin-client";
