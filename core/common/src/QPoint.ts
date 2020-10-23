44d751051b7fbaf3c604173f3eb01cdfc77ee572
Ray Bentley <20863871+RBBentley@users.noreply.github.com>
2020-10-23 06:54:18 -0400

    Add 2D methods to match 3d.

--- core/common/src/QPoint.ts
+++ core/common/src/QPoint.ts
@@ -73,6 +73,10 @@ export class QParams2d {
 
   /** Creates parameters supporting quantization of values within the range [0.0, 1.0]. */
   public static fromZeroToOne(rangeScale = Quantization.rangeScale16) { return QParams2d.fromRange(Range2d.createArray([Point2d.create(0, 0), Point2d.create(1, 1)]), undefined, rangeScale); }
+
+  // Return the range diagonal.
+  public get rangeDiagonal(): Vector2d { return Vector2d.createFrom({ x: Quantization.rangeScale16 / this.scale.x, y: Quantization.rangeScale16 / this.scale.y }); }
+
 }
 
 /** Represents a quantized 2d point as an (x, y) pair in the integer range [0, 0xffff].
@@ -206,23 +210,19 @@ export class QPoint2dList {
       this._list[i] = QPoint2d.fromScalars(array[j++], array[j++]);
   }
 
-  // Return the range diagonal.
-  public get rangeDiagonal(): Vector2d { return Vector2d.createFrom({ x: Quantization.rangeScale16 / this.scale.x, y: Quantization.rangeScale16 / this.scale.y }); }
-}
-
   /** Construct a QPoint2dList containing all points in the supplied list, quantized to the range of those points. */
-  public static fromPoints(points: Point2d[], out ?: QPoint2dList) {
-  let qPoints;
-  const qParams = QParams2d.fromRange(Range2d.createArray(points));
-  if (out) {
-    qPoints = out;
-    qPoints.reset(qParams);
-  } else qPoints = new QPoint2dList(qParams);
-  for (const point of points)
-    qPoints.add(point);
+  public static fromPoints(points: Point2d[], out?: QPoint2dList) {
+    let qPoints;
+    const qParams = QParams2d.fromRange(Range2d.createArray(points));
+    if (out) {
+      qPoints = out;
+      qPoints.reset(qParams);
+    } else qPoints = new QPoint2dList(qParams);
+    for (const point of points)
+      qPoints.add(point);
 
-  return qPoints;
-}
+    return qPoints;
+  }
 }
 
 /** Parameters used for quantization of 3d points.
