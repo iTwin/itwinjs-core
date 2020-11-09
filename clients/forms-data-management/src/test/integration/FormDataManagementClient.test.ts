77f4c8f9d0756c3bf8b13fd3aa0160357f5f8d74
Mark.Uvari <mark.uvari@bentley.com>
2020-11-09 10:43:02 -0500

    Remove deprecated code

--- clients/forms-data-management/src/test/integration/FormDataManagementClient.test.ts
+++ clients/forms-data-management/src/test/integration/FormDataManagementClient.test.ts
@@ -5,11 +5,7 @@
 import * as chai from "chai";
 import { ContextRegistryClient, Project } from "@bentley/context-registry-client";
 import { AuthorizedClientRequestContext } from "@bentley/itwin-client";
-import {
-  FormDataManagementClient,
-  FormDefinition,
-  FormInstanceData,
-} from "../../FormDataManagementClient";
+import { FormDataManagementClient, FormDefinition } from "../../FormDataManagementClient";
 import { TestConfig } from "../TestConfig";
 
 /* eslint-disable @typescript-eslint/naming-convention */
@@ -42,70 +38,5 @@ describe("FormDataManagementClient", () => {
       projectId
     );
     chai.assert(formDefinitions);
-  });
-
-  it("should be able to retrieve Risk Issue Form Definitions (#integration)", async () => {
-    const formDefinitions: FormDefinition[] = await formDataManagementClient.getRiskIssueFormDefinitions(
-      requestContext,
-      projectId
-    );
-    chai.assert(formDefinitions);
-  });
-
-  it("should be able to create new Risk Issue Form Data (#integration)", async function () {
-    if (TestConfig.enableMocks) this.skip();
-
-    const definitions: FormDefinition[] = await formDataManagementClient.getRiskIssueFormDefinitions(
-      requestContext,
-      projectId
-    );
-    const formDef = definitions[0];
-
-    const formId = formDef.formId!;
-    const iModelId = "b702d44e-d8c0-4978-9a41-b6cdddcb5619";
-    const elementId = "f76bd999504568fd6b076977440d34859be4e1f8";
-    const properties: any = {
-      _CreatedDate: "2015-07-28T06:07:34Z",
-      _Description: "Test FeatureDescription",
-      _ModifiedDate: "2015-07-28T06:07:34Z",
-      _Subject: "Test Feature",
-      AssignedTo: "Test AssignedTo",
-      AssignedToId: "Test AssignedToId",
-    };
-
-    const newFromData: FormInstanceData = await formDataManagementClient.postRiskIssueFormData(
-      requestContext,
-      properties,
-      projectId,
-      iModelId,
-      elementId,
-      formId,
-      "Issue"
-    );
-    chai.assert(newFromData);
-  });
-
-  it("should be able to retrieve Form Data (#integration)", async function () {
-    if (TestConfig.enableMocks) this.skip();
-
-    const formData: FormInstanceData[] = await formDataManagementClient.getFormData(
-      requestContext,
-      projectId,
-      "Issue"
-    );
-    chai.assert(formData);
-  });
-
-  it("should be able to retrieve Risk Issue Form Data (#integration)", async function () {
-    if (TestConfig.enableMocks) this.skip();
-
-    const iModelId = "b702d44e-d8c0-4978-9a41-b6cdddcb5619";
-    const formData: FormInstanceData[] = await formDataManagementClient.getRiskIssueFormData(
-      requestContext,
-      projectId,
-      iModelId,
-      "Issue"
-    );
-    chai.assert(formData);
   });
 });
