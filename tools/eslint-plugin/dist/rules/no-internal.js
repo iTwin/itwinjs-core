6d787d7e46c24837b1497c26307df615957fe62a
Paulius.Valiunas <66480813+paulius-valiunas@users.noreply.github.com>
2020-11-16 11:21:46 +0200

    clean up

--- tools/eslint-plugin/dist/rules/no-internal.js
+++ tools/eslint-plugin/dist/rules/no-internal.js
@@ -73,55 +73,11 @@ module.exports = {
         checkJsDoc(declaration.parent, node);
     }
 
-    JSON.stringifyOnce = function (obj, replacer, indent) {
-      var printedObjects = [];
-      var printedObjectKeys = [];
-
-      function printOnceReplacer(key, value) {
-        if (printedObjects.length > 2000) { // browsers will not print more than 20K, I don't see the point to allow 2K.. algorithm will not be fast anyway if we have too many objects
-          return 'object too long';
-        }
-        var printedObjIndex = false;
-        printedObjects.forEach(function (obj, index) {
-          if (obj === value) {
-            printedObjIndex = index;
-          }
-        });
-
-        if (key == '') { //root element
-          printedObjects.push(obj);
-          printedObjectKeys.push("root");
-          return value;
-        }
-
-        else if (printedObjIndex + "" != "false" && typeof (value) == "object") {
-          if (printedObjectKeys[printedObjIndex] == "root") {
-            return "(pointer to root)";
-          } else {
-            return "(see " + ((!!value && !!value.constructor) ? value.constructor.name.toLowerCase() : typeof (value)) + " with key " + printedObjectKeys[printedObjIndex] + ")";
-          }
-        } else {
-
-          var qualifiedKey = key || "(empty key)";
-          printedObjects.push(value);
-          printedObjectKeys.push(qualifiedKey);
-          if (replacer) {
-            return replacer(key, value);
-          } else {
-            return value;
-          }
-        }
-      }
-      return JSON.stringify(obj, printOnceReplacer, indent);
-    };
-
     return {
       CallExpression(node) {
         const tsCall = parserServices.esTreeNodeToTSNodeMap.get(node);
         if (!tsCall)
           return;
-
-        // typeChecker.getTypeAtLocation(a.thisParameter.valueDeclaration).symbol
 
         const resolved = typeChecker.getResolvedSignature(tsCall);
         if (!resolved || !resolved.declaration)
@@ -162,7 +118,6 @@ module.exports = {
           return;
 
         const resolved = typeChecker.getResolvedSignature(tsCall);
-        // typeChecker.getResolvedSignature(tsCall).declaration.jsDoc[i].tags[j].tagName
         if (!resolved || !resolved.declaration)
           return;
         checkJsDoc(resolved.declaration, node);
