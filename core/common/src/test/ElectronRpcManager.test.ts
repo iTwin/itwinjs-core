e1e0e63e54b683956816991d80ef2f73b0645642
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-11-23 17:50:55 -0500

    clean up lint errors

--- core/common/src/test/ElectronRpcManager.test.ts
+++ core/common/src/test/ElectronRpcManager.test.ts
@@ -3,30 +3,30 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { getIModelElectronApi, IModelElectronApi } from "../rpc/electron/ElectronRpcManager";
-import { expect, assert } from "chai";
+import { getIModelElectronApi } from "../rpc/electron/ElectronRpcManager";
+import { assert } from "chai";
 
 describe("Electron Rpc Manager", () => {
   it("getIModelElectron API undefined in node by default", () => {
-    expect(getIModelElectronApi()).to.be.undefined;
+    assert.isUndefined(getIModelElectronApi());
   });
 
   it("getIModelElectron API handles missing window", () => {
     (global as any).window = () => { };
-    expect(getIModelElectronApi()).to.be.undefined;
+    assert.isUndefined(getIModelElectronApi());
     delete (global as any).window;
   });
 
   it("getIModelElectron API succeeds", () => {
     (global as any).window = {
-      imodeljs_api: {
+      imodeljs_api: { // eslint-disable-line @typescript-eslint/naming-convention
         invoke: async (_channel: string, ..._data: any[]): Promise<any> => { },
         // on: (_channel: string, _listener: any) => { },
         once: (_channel: string, _listener: any) => { },
         removeListener: (_channel: string, _listener: any) => { },
         send: (_channel: string, ..._data: any[]) => { },
         sendSync: (_channel: string, ..._args: any[]) => { },
-      }
+      },
     };
 
     assert.isDefined(getIModelElectronApi());
