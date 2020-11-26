e296963cf39fd166c77222fb6e5974f89710b4e6
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2020-11-26 15:33:19 -0500

    Added content and completed layer support.

--- core/frontend/src/test/tile/map/WmtsCapabilities.test.ts
+++ core/frontend/src/test/tile/map/WmtsCapabilities.test.ts
@@ -7,6 +7,8 @@ import { expect } from "chai";
 import { WmtsCapabilities } from "../../../tile/map/WmtsCapabilities";
 
 describe.only("WmtsCapabilities", () => {
+  const SMALL_DEGREES_DIFFERENCE = 1.0e-8;
+
   it("should parse USGS WMTS capabilities", async () => {
     const capabilities = await WmtsCapabilities.create("assets/wmts_capabilities/USGSHydroCached_capabilities.xml");
     expect(capabilities?.version).to.equal("1.0.0");
@@ -49,9 +51,48 @@ describe.only("WmtsCapabilities", () => {
     // Check that no GetFeatureInfo has been configured
     expect(capabilities?.operationsMetadata?.getFeatureInfo).to.undefined;
 
-    expect(capabilities?.layers.length).to.equal(1);
-    expect(capabilities?.layers[0].identifier).to.equal("USGSHydroCached");
-    expect(capabilities?.layers[0].format).to.equal("image/jpgpng");
+    //
+    // Content
+    //
+    expect(capabilities?.contents).to.not.undefined;
+    expect(capabilities?.contents?.layers.length).to.equal(1);
+
+    // Identifier
+    expect(capabilities?.contents?.layers[0].identifier).to.equal("USGSHydroCached");
+
+    // Format
+    expect(capabilities?.contents?.layers[0].format).to.equal("image/jpgpng");
+
+    // BoundingBox
+    expect(capabilities?.contents?.layers[0].boundingBox).to.not.undefined;
+    expect(capabilities?.contents?.layers[0].boundingBox?.crs).to.equal("urn:ogc:def:crs:EPSG::3857");
+    expect(capabilities?.contents?.layers[0].boundingBox?.range).to.not.undefined;
+    expect(capabilities?.contents?.layers[0].boundingBox?.range?.low.isAlmostEqualXY(-2.003750785759102E7, -3.024245526192411E7));
+    expect(capabilities?.contents?.layers[0].boundingBox?.range?.low.isAlmostEqualXY(2.003872561259901E7, 3.0240971955423884E7));
+
+
+    expect(capabilities?.contents?.layers[0].wsg84BoundingBox).to.not.undefined;
+
+    expect(capabilities?.contents?.layers[0].wsg84BoundingBox?.west).to.not.undefined;
+    if (capabilities?.contents?.layers[0].wsg84BoundingBox?.west) {
+      const area = capabilities.contents.layers[0].wsg84BoundingBox.globalLocationArea;
+
+      expect(Math.abs(area.southwest.longitudeDegrees - (-179.99999550841463))).to.be.lessThan(SMALL_DEGREES_DIFFERENCE);
+      expect(Math.abs(area.southwest.latitudeDegrees - (-88.99999992161116))).to.be.lessThan(SMALL_DEGREES_DIFFERENCE);
+      expect(Math.abs(area.northeast.longitudeDegrees - (179.99999550841463))).to.be.lessThan(SMALL_DEGREES_DIFFERENCE);
+      expect(Math.abs(area.northeast.latitudeDegrees - (88.99999992161116))).to.be.lessThan(SMALL_DEGREES_DIFFERENCE);
+    }
+
+    // Style
+    expect(capabilities?.contents?.layers[0].style).to.not.undefined;
+    expect(capabilities?.contents?.layers[0].style?.identifier).to.equal("default");
+    expect(capabilities?.contents?.layers[0].style?.title).to.equal("Default Style");
+    expect(capabilities?.contents?.layers[0].style?.isDefault).to.equal(true);
+
+    // TileMatrixSetLink
+    expect(capabilities?.contents?.layers[0].tileMatrixSetLinks.length).to.equal(2);
+    expect(capabilities?.contents?.layers[0].tileMatrixSetLinks[0].tileMatrixSet).to.equal("default028mm");
+    expect(capabilities?.contents?.layers[0].tileMatrixSetLinks[1].tileMatrixSet).to.equal("GoogleMapsCompatible");
 
   });
 
@@ -107,8 +148,14 @@ describe.only("WmtsCapabilities", () => {
 
 
     expect(capabilities?.version).to.equal("1.0.0");
-    expect(capabilities?.layers.length).to.equal(2);
-    expect(capabilities?.layers[0].identifier).to.equal("etopo2");
-    expect(capabilities?.layers[1].identifier).to.equal("AdminBoundaries");
+    expect(capabilities?.contents).to.not.undefined;
+    expect(capabilities?.contents?.layers.length).to.equal(2);
+    expect(capabilities?.contents?.layers[0].identifier).to.equal("etopo2");
+    expect(capabilities?.contents?.layers[0].tileMatrixSetLinks.length).to.equal(1);
+    expect(capabilities?.contents?.layers[0].tileMatrixSetLinks[0].tileMatrixSet).to.equal("WholeWorld_CRS_84");
+
+    expect(capabilities?.contents?.layers[1].identifier).to.equal("AdminBoundaries");
+    expect(capabilities?.contents?.layers[1].tileMatrixSetLinks.length).to.equal(1);
+    expect(capabilities?.contents?.layers[1].tileMatrixSetLinks[0].tileMatrixSet).to.equal("WholeWorld_CRS_84");
   });
 });
--- core/frontend/src/tile/map/WmtsCapabilities.ts
+++ core/frontend/src/tile/map/WmtsCapabilities.ts
@@ -3,8 +3,10 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { ClientRequestContext } from "@bentley/bentleyjs-core";
+import { Range2d } from "@bentley/geometry-core";
 import { request, RequestBasicCredentials, RequestOptions } from "@bentley/itwin-client";
 import { xml2json } from "xml-js";
+import { MapCartoRectangle } from "../../imodeljs-frontend";
 import { WmsUtilities } from "./WmsUtilities"; // needed for getBaseUrl
 
 /** @packageDocumentation
@@ -30,10 +32,38 @@ async function getXml(requestContext: ClientRequestContext, url: string, credent
 
 export namespace WmtsCapability {
 
-  const WMTS_OWS_ABSTRACT_XML_TAG = "ows:Abstract";
-  const WMTS_OWS_OPERATIONMETADATA_XML_TAG = "ows:OperationMetadata";
-  const WMTS_OWS_SERVICETYPE_XML_TAG = "ows:ServiceType";
-  const WMTS_OWS_SERVICETYPEVERSION_XML_TAG = "ows:ServiceTypeVersion";
+  // Operations names
+  export const OPS_GETCAPABILITIES = "GetCapabilities";
+  export const OPS_GETTILE = "GetTile";
+  export const OPS_GETFEATUREINFO = "GetFeatureInfo";
+
+  // OWS xml tag names
+  export const OWS_ABSTRACT_XMLTAG = "ows:Abstract";
+  export const OWS_ALLOWEDVALUES_XMLTAG = "ows:AllowedValues";
+  export const OWS_BOUNDINGBOX_XMLTAG = "ows:BoundingBox";
+  export const OWS_CONSTRAINT_XMLTAG = "ows:Constraint";
+  export const OWS_DCP_XMLTAG = "ows:DCP";
+  export const OWS_GET_XMLTAG = "ows:Get";
+  export const OWS_HTTP_XMLTAG = "ows:HTTP";
+  export const OWS_IDENTIFIER_XMLTAG = "ows:Identifier";
+  export const OWS_LOWERCORNER_XMLTAG = "ows:LowerCorner";
+  export const OWS_OPERATION_XMLTAG = "ows:Operation";
+  export const OWS_OPERATIONSMETADATA_XMLTAG = "ows:OperationsMetadata";
+  export const OWS_POST_XMLTAG = "ows:Post";
+  export const OWS_SERVICETYPE_XMLTAG = "ows:ServiceType";
+  export const OWS_SERVICETYPEVERSION_XMLTAG = "ows:ServiceTypeVersion";
+  export const OWS_TITLE_XMLTAG = "ows:Title";
+  export const OWS_UPPERCORNER_XMLTAG = "ows:UpperCorner";
+  export const OWS_VALUE_XMLTAG = "ows:Value";
+  export const OWS_WGS84BOUNDINGBOX_XMLTAG = "ows:WGS84BoundingBox";
+
+  // xml tag names
+  export const TILEMATRIXSETLINK_XMLTAG = "TileMatrixSetLink";
+
+  // other strings...
+  export const CONSTRAINT_NAME_FILTER = "Encoding";
+  export const STYLE_ISDEFAULT = "IsDefault";
+  export const XLINK_HREF = "xlink:href";
 
   export class ServiceIdentification {
     public readonly abstract?: string;
@@ -43,11 +73,10 @@ export namespace WmtsCapability {
     public readonly title?: string;
 
     constructor(json: any) {
-      this.abstract = json[WMTS_OWS_ABSTRACT_XML_TAG]?._text;
-      this.operationMetadata = json[WMTS_OWS_OPERATIONMETADATA_XML_TAG]?._text;;
-      this.serviceType = json[WMTS_OWS_SERVICETYPE_XML_TAG]?._text;
-      this.serviceTypeVersion = json[WMTS_OWS_SERVICETYPEVERSION_XML_TAG]?._text;
-      this.title = json["ows:Title"]?._text;
+      this.abstract = json[OWS_ABSTRACT_XMLTAG]?._text;
+      this.serviceType = json[OWS_SERVICETYPE_XMLTAG]?._text;
+      this.serviceTypeVersion = json[OWS_SERVICETYPEVERSION_XMLTAG]?._text;
+      this.title = json[OWS_TITLE_XMLTAG]?._text;
     }
   }
 
@@ -62,16 +91,16 @@ export namespace WmtsCapability {
     public get getTile(): Operation | undefined { return this._getTile; }
 
     private readOperation(op: any) {
-      if (op?._attributes?.name === "GetCapabilities")
+      if (op?._attributes?.name === OPS_GETCAPABILITIES)
         this._getCapabilities = new Operation(op);
-      else if (op?._attributes?.name === "GetTile")
+      else if (op?._attributes?.name === OPS_GETTILE)
         this._getTile = new Operation(op);
-      else if (op?._attributes?.name === "GetFeatureInfo")
+      else if (op?._attributes?.name === OPS_GETFEATUREINFO)
         this._getFeatureInfo = new Operation(op);
     }
 
     constructor(json: any) {
-      const operation = (json && json.hasOwnProperty("ows:Operation")) ? json["ows:Operation"] : undefined;
+      const operation = (json ? json[OWS_OPERATION_XMLTAG] : undefined);
       if (operation) {
         if (Array.isArray(operation)) {
           operation.forEach((op: any) => {
@@ -81,7 +110,6 @@ export namespace WmtsCapability {
           this.readOperation(operation);
         }
       }
-
     }
   }
 
@@ -90,14 +118,15 @@ export namespace WmtsCapability {
     public readonly constraintName?: string;
 
     // For simplicity of use we create a 'static' encoding property instead of having
-    // a generic constraint value.  We make sure the constraint name is 'encoding' related.
+    // a generic constraint data model.
+    // We make sure the constraint name is 'encoding' related.
     public readonly encoding?: string;
 
     constructor(json: any) {
-      this.url = json?._attributes["xlink:href"];
-      this.constraintName = (json ? json["ows:Constraint"]?._attributes?.name : undefined);
-      if (this.constraintName?.endsWith("Encoding"))
-        this.encoding = json["ows:Constraint"]?.["ows:AllowedValues"]?.["ows:Value"]?._text;
+      this.url = json?._attributes[XLINK_HREF];
+      this.constraintName = (json ? json[OWS_CONSTRAINT_XMLTAG]?._attributes?.name : undefined);
+      if (this.constraintName?.endsWith(CONSTRAINT_NAME_FILTER))
+        this.encoding = json[OWS_CONSTRAINT_XMLTAG]?.[OWS_ALLOWEDVALUES_XMLTAG]?.[OWS_VALUE_XMLTAG]?._text;
     }
   }
 
@@ -111,11 +140,11 @@ export namespace WmtsCapability {
     constructor(json: any) {
       this.name = json?._attributes?.name;
 
-      const dcpHttp = (json ? json["ows:DCP"]?.["ows:HTTP"] : undefined);
+      const dcpHttp = (json ? json[OWS_DCP_XMLTAG]?.[OWS_HTTP_XMLTAG] : undefined);
       if (!dcpHttp)
         return;
 
-      const get = dcpHttp["ows:Get"];
+      const get = dcpHttp[OWS_GET_XMLTAG];
       if (get) {
         this._getDcpHttp = [];
 
@@ -128,7 +157,7 @@ export namespace WmtsCapability {
         }
       }
 
-      const post = dcpHttp["ows:Post"];
+      const post = dcpHttp[OWS_POST_XMLTAG];
       if (post) {
         this._postDcpHttp = [];
 
@@ -143,16 +172,113 @@ export namespace WmtsCapability {
     }
   }
 
+  export class Contents {
+    public readonly layers: WmtsCapability.Layer[] = [];
+
+    constructor(private _json: any) {
+
+      const jsonLayer = _json?.Layer;
+      if (jsonLayer) {
+        if (Array.isArray(jsonLayer)) {
+          jsonLayer.forEach((layer: any) => {
+            this.layers.push(new WmtsCapability.Layer(layer));
+          });
+        } else {
+          this.layers.push(new WmtsCapability.Layer(jsonLayer));
+        }
+      }
+    }
+  }
+
+  export class Style {
+    public readonly isDefault: boolean = true;
+    public readonly title?: string;
+    public readonly identifier?: string;
+    // TODO: LegendURL
+
+    constructor(private _json: any) {
+      if (!_json)
+        return;
+
+      if (_json._attributes.hasOwnProperty(STYLE_ISDEFAULT))
+        this.isDefault = _json._attributes.isDefault;
+
+      this.title = _json[OWS_TITLE_XMLTAG]?._text;
+      this.identifier = _json[OWS_IDENTIFIER_XMLTAG]?._text;
+    }
+  }
+  export class BoundingBox {
+    public readonly crs?: string;
+    public readonly range?: Range2d;
+
+    constructor(_json: any) {
+      this.crs = _json._attributes?.crs;
+      const lowerCorner = _json[OWS_LOWERCORNER_XMLTAG]?._text?.split(" ").map((x: string) => +x);
+      const upperCorner = _json[OWS_UPPERCORNER_XMLTAG]?._text?.split(" ").map((x: string) => +x);
+      if (lowerCorner.length === 2 && upperCorner.length === 2)
+        this.range = Range2d.createXYXY(lowerCorner[0], lowerCorner[1], upperCorner[0], upperCorner[1]);
+    }
+  }
+
+  export class TileMatrixSetLink {
+    public readonly tileMatrixSet: string;
+    // TODO: TileMatrixSetLimits
+
+    constructor(json: any) {
+      this.tileMatrixSet = (json?.TileMatrixSet?._text ? json.TileMatrixSet._text : "");
+    }
+  }
+
   export class Layer {
     public readonly identifier: string;
     public readonly title?: string;
     public readonly abstract?: string;
     public readonly format?: string;
+    public readonly wsg84BoundingBox?: MapCartoRectangle;
+    public readonly boundingBox?: BoundingBox;
+    public readonly style?: Style;
+    public readonly tileMatrixSetLinks: TileMatrixSetLink[] = [];
 
-    constructor(json: any) {
-      this.identifier = json["ows:Identifier"]?._text;
-      this.title = json["ows:Title"]?._text;
-      this.format = json.Format?._text;
+
+    constructor(_json: any) {
+      if (!_json)
+        throw new Error("Invalid json data provided");
+
+      this.identifier = _json[OWS_IDENTIFIER_XMLTAG]?._text;
+      this.title = _json[OWS_TITLE_XMLTAG]?._text;
+      this.format = _json.Format?._text;
+
+      // BoundingBox
+      this.boundingBox = (_json[OWS_BOUNDINGBOX_XMLTAG] ? new BoundingBox(_json[OWS_BOUNDINGBOX_XMLTAG]) : undefined);
+
+      // WSG84 BoundingBox
+      const lowerCorner = _json[OWS_WGS84BOUNDINGBOX_XMLTAG]?.[OWS_LOWERCORNER_XMLTAG]?._text?.split(" ").map((x: string) => +x);
+      const upperCorner = _json[OWS_WGS84BOUNDINGBOX_XMLTAG]?.[OWS_UPPERCORNER_XMLTAG]?._text?.split(" ").map((x: string) => +x);
+      if (lowerCorner?.length === 2 && upperCorner?.length === 2)
+        this.wsg84BoundingBox = MapCartoRectangle.createFromDegrees(lowerCorner[0], lowerCorner[1], upperCorner[0], upperCorner[1]);
+
+      // If we could not initialized WSG84 bounding box, attempt to initialized it from Bounding Box
+      if (!this.wsg84BoundingBox && (this.boundingBox?.crs?.includes("EPSG:4326") || this.boundingBox?.crs?.includes("CRS:84"))) {
+        this.wsg84BoundingBox = MapCartoRectangle.createFromDegrees(this.boundingBox.range?.low.x, this.boundingBox.range?.low.y, this.boundingBox.range?.high.x, this.boundingBox.range?.high.y);
+      }
+
+      // Style
+      this.style = (_json.Style ? new Style(_json.Style) : undefined);
+
+      // TileMatrixSetLink
+      // TileMatrixSetLink is mandatory on Layer, if it doesn't exists, something is OFF with the capability.
+      const tileMatrixSetLink = _json[TILEMATRIXSETLINK_XMLTAG];
+
+      if (!tileMatrixSetLink)
+        throw new Error("No matrix set link found for WMTS layer");
+
+      if (Array.isArray(tileMatrixSetLink)) {
+        tileMatrixSetLink.forEach((tmsl: any) => {
+          this.tileMatrixSetLinks.push(new TileMatrixSetLink(tmsl));
+        });
+      } else {
+        this.tileMatrixSetLinks.push(new TileMatrixSetLink(tileMatrixSetLink));
+      }
     }
   }
 }
@@ -165,25 +291,21 @@ export class WmtsCapabilities {
 
   public readonly version?: string;
   public readonly serviceIdentification?: WmtsCapability.ServiceIdentification;
-  public readonly layers: WmtsCapability.Layer[] = [];
+  public readonly contents?: WmtsCapability.Contents;
+
   public readonly operationsMetadata?: WmtsCapability.OperationMetadata;
 
   constructor(private _json: any) {
+    // Capabilities version
     this.version = _json?.Capabilities?._attributes.version;
 
-    if (_json?.Capabilities?.["ows:OperationsMetadata"])
-      this.operationsMetadata = new WmtsCapability.OperationMetadata(_json?.Capabilities?.["ows:OperationsMetadata"]);
+    // Operations metadata
+    if (_json?.Capabilities?.[WmtsCapability.OWS_OPERATIONSMETADATA_XMLTAG])
+      this.operationsMetadata = new WmtsCapability.OperationMetadata(_json?.Capabilities?.[WmtsCapability.OWS_OPERATIONSMETADATA_XMLTAG]);
 
-    const jsonLayer = _json.Capabilities?.Contents?.Layer;
-    if (jsonLayer) {
-      if (Array.isArray(jsonLayer)) {
-        jsonLayer.forEach((layer: any) => {
-          this.layers.push(new WmtsCapability.Layer(layer));
-        });
-      } else {
-        this.layers.push(new WmtsCapability.Layer(jsonLayer));
-      }
-    }
+    // Contents
+    if (_json.Capabilities?.Contents)
+      this.contents = new WmtsCapability.Contents(_json.Capabilities?.Contents);
   }
 
   public static createFromXml(xmlCapabilities: string): WmtsCapabilities | undefined {
@@ -208,6 +330,4 @@ export class WmtsCapabilities {
 
     return capabilities;
   }
-
 }
-
