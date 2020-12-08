dff1faa2aaa38f019fb04a2b066d53e4d70d616d
kckst8 <6283674+kckst8@users.noreply.github.com>
2020-12-08 15:22:17 -0500

    update logic so as to not mutate existing modules

--- tools/webpack-core/src/plugins/IModeljsLibraryExportsPlugin.ts
+++ tools/webpack-core/src/plugins/IModeljsLibraryExportsPlugin.ts
@@ -28,22 +28,26 @@ export class IModeljsLibraryExportsPlugin {
     });
     compiler.hooks.compilation.tap("IModeljsLibraryExportsPlugin", (compilation) => {
       compilation.moduleTemplates.javascript.hooks.content.tap("IModeljsLibraryExportsPlugin", (source, module) => {
-        let currentModule = module;
-        // check for a rootModule (for concatenated and/or lazily loaded modules)
-        if (!currentModule.___IS_BENTLEY && currentModule.rootModule && currentModule.rootModule.___IS_BENTLEY) {
-            currentModule.rootModule.id = currentModule.id;
-            currentModule = currentModule.rootModule;
+        // check for a rootModule (for concatenated modules)
+        let rootModule;
+        if (!module.___IS_BENTLEY && module.rootModule && module.rootModule.___IS_BENTLEY) {
+          rootModule = module.rootModule;
         }
-        if (!currentModule.___IS_BENTLEY)
+
+        const isBentley = module.___IS_BENTLEY || (rootModule && rootModule.___IS_BENTLEY);
+        if (!isBentley)
           return source;
-        const pkgName = JSON.stringify(currentModule.___IMJS_NAME);
+
+        const pkgName = JSON.stringify(rootModule ? rootModule.___IMJS_NAME : module.___IMJS_NAME);
+        const pkgVersion = JSON.stringify(rootModule ? rootModule.___IMJS_VER : module.___IMJS_VER);
+
         return new ConcatSource(
           source,
           `\nif ((typeof window !== "undefined") && window && !window.${utils.IMJS_GLOBAL_OBJECT}) window.${utils.IMJS_GLOBAL_OBJECT} = {};`,
           `\nif ((typeof window !== "undefined") && window && !window.${utils.IMJS_GLOBAL_LIBS}) window.${utils.IMJS_GLOBAL_LIBS} = {};`,
           `\nif ((typeof window !== "undefined") && window && !window.${utils.IMJS_GLOBAL_LIBS_VERS}) window.${utils.IMJS_GLOBAL_LIBS_VERS} = {};`,
-          `\nwindow.${utils.IMJS_GLOBAL_LIBS}[${pkgName}] = __webpack_require__(${JSON.stringify(currentModule.id)});`,
-          `\nwindow.${utils.IMJS_GLOBAL_LIBS_VERS}[${pkgName}] = ${JSON.stringify(currentModule.___IMJS_VER)};\n`,
+          `\nwindow.${utils.IMJS_GLOBAL_LIBS}[${pkgName}] = __webpack_require__(${JSON.stringify(module.id)});`,
+          `\nwindow.${utils.IMJS_GLOBAL_LIBS_VERS}[${pkgName}] = ${JSON.stringify(pkgVersion)};\n`,
         );
       });
     });
