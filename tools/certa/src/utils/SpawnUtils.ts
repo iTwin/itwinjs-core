f2c39fbd055d518315cf5b306e899e71aba0947b
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-12-13 20:16:44 +0000

    Remove logging from certa

--- tools/certa/src/utils/SpawnUtils.ts
+++ tools/certa/src/utils/SpawnUtils.ts
@@ -57,7 +57,6 @@ async function onExitElectronApp(child: ChildProcess): Promise<number> {
  * Returns a promise that will be resolved with the exit code of the child process, once it terminates.
  */
 export async function relaunchInElectron(): Promise<number> {
-  // const child = spawnChildProcess(require("electron/index.js"), process.argv.slice(1), { ELECTRON_ENABLE_LOGGING: "true" }, true);
   const child = spawnChildProcess(require("electron/index.js"), process.argv.slice(1), undefined, true);
   return onExitElectronApp(child);
 }
