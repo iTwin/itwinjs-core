b27d15545a9140ab79e180215925006b459c3902
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2020-12-14 17:16:46 -0500

    Load section drawing info.

--- core/frontend/src/DrawingViewState.ts
+++ core/frontend/src/DrawingViewState.ts
@@ -6,12 +6,23 @@
  * @module Views
  */
 
-import { Constant, Range3d } from "@bentley/geometry-core";
+import { Id64, Id64String } from "@bentley/bentleyjs-core";
+import { Constant, Range3d, Transform } from "@bentley/geometry-core";
 import { AxisAlignedBox3d, ViewDefinition2dProps, ViewStateProps } from "@bentley/imodeljs-common";
 import { ExtentLimits, ViewState2d } from "./ViewState";
 import { IModelConnection } from "./IModelConnection";
 import { CategorySelectorState } from "./CategorySelectorState";
 import { DisplayStyle2dState } from "./DisplayStyleState";
+import { SectionDrawingModelState } from "./ModelState";
+
+/** Temporary, for testing.
+ * @internal
+ */
+export interface SectionDrawingInfo {
+  readonly spatialView: Id64String;
+  readonly drawingToSpatialTransform: Transform;
+  readonly displaySpatialView: boolean;
+}
 
 /** A view of a [DrawingModel]($backend)
  * @public
@@ -21,16 +32,55 @@ export class DrawingViewState extends ViewState2d {
   public static get className() { return "DrawingViewDefinition"; }
   private readonly _modelLimits: ExtentLimits;
   private readonly _viewedExtents: AxisAlignedBox3d;
+  private _sectionDrawingInfo?: SectionDrawingInfo;
+  /** Temporary, for testing. @internal */
+  public get sectionDrawingInfo() { return this._sectionDrawingInfo; }
 
   public constructor(props: ViewDefinition2dProps, iModel: IModelConnection, categories: CategorySelectorState, displayStyle: DisplayStyle2dState, extents: AxisAlignedBox3d) {
     super(props, iModel, categories, displayStyle);
     if (categories instanceof DrawingViewState) {
       this._viewedExtents = categories._viewedExtents.clone();
       this._modelLimits = { ...categories._modelLimits };
+      this._sectionDrawingInfo = categories._sectionDrawingInfo;
     } else {
       this._viewedExtents = extents;
       this._modelLimits = { min: Constant.oneMillimeter, max: 10 * extents.maxLength() };
     }
+  }
+
+  public async load(): Promise<void> {
+    this._sectionDrawingInfo = undefined;
+    await super.load();
+    const model = this.iModel.models.getLoaded(this.baseModelId);
+    if (!model || !(model instanceof SectionDrawingModelState))
+      return;
+
+    // Find out if we also need to display the spatial view.
+    let spatialView;
+    let drawingToSpatialTransform;
+    let displaySpatialView;
+    const ecsql = `
+      SELECT spatialView,
+        json_extract(jsonProperties, '$.drawingToSpatialTransform') as drawingToSpatialTransform,
+        json_extract(jsonProperties, '$.displaySpatialView') as displaySpatialView
+      FROM bis.SectionDrawing
+      WHERE ECInstanceId=${model.modeledElement.id}`;
+
+    for await (const row of this.iModel.query(ecsql)) {
+      spatialView = Id64.fromJSON(row.spatialView?.id);
+      displaySpatialView = true === row.displaySpatialView;
+      try {
+        drawingToSpatialTransform = Transform.fromJSON(JSON.parse(row.drawingToSpatialTransform));
+      } catch (_) {
+        //
+      }
+
+      break;
+    }
+
+    // ###TODO: Don't bother if displaySpatialView === false
+    if (spatialView && Id64.isValidId64(spatialView) && drawingToSpatialTransform && undefined !== displaySpatialView)
+      this._sectionDrawingInfo = { spatialView, drawingToSpatialTransform, displaySpatialView };
   }
 
   public static createFromProps(props: ViewStateProps, iModel: IModelConnection): DrawingViewState {
--- full-stack-tests/core/src/frontend/hub/SectionDrawing.test.ts
+++ full-stack-tests/core/src/frontend/hub/SectionDrawing.test.ts
@@ -4,6 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
 import {
+  DrawingViewState,
   IModelApp,
   IModelConnection,
   RemoteBriefcaseConnection,
@@ -11,8 +12,9 @@ import {
 } from "@bentley/imodeljs-frontend";
 import { TestUsers } from "@bentley/oidc-signin-tool/lib/TestUsers";
 import { TestUtility } from "./TestUtility";
+import { testOffScreenViewport } from "../TestViewport";
 
-describe("SectionDrawings (#integration)", () => {
+describe("Section Drawings (#integration)", () => {
   const projectName = "iModelJsIntegrationTest";
   let imodel: IModelConnection;
 
@@ -35,13 +37,76 @@ describe("SectionDrawings (#integration)", () => {
     await IModelApp.shutdown();
   });
 
+  const specs = [
+    { model: "0x70", views: ["0x99", "0x8a"], spatialView: "0x78" },
+    { model: "0x72", views: ["0x8f"], spatialView: "0x80" },
+    { model: "0xa1", views: ["0xa3"], spatialView: "0x7c" },
+  ];
+
   it("instantiates a SectionDrawingModelState", async () => {
-    const modelIds = [ "0x70", "0x72", "0xa1" ];
+    const modelIds = specs.map((x) => x.model);
     await imodel.models.load(modelIds);
     for (const modelId of modelIds) {
       const model = imodel.models.getLoaded(modelId)!;
       expect(model).not.to.be.undefined;
       expect(model).instanceof(SectionDrawingModelState);
     }
+  });
+
+  it("loads section drawing info for view", async () => {
+    for (const spec of specs) {
+      const first = await imodel.views.load(spec.views[0]) as DrawingViewState;
+      expect(first).instanceof(DrawingViewState);
+      expect(first.baseModelId).to.equal(spec.model);
+
+      const info = first.sectionDrawingInfo!;
+      expect(info).not.to.be.undefined;
+
+      expect(info.spatialView).to.equal(spec.spatialView);
+      expect(info.displaySpatialView).to.be.false;
+      expect(info.drawingToSpatialTransform.isIdentity).to.be.false;
+
+      if (spec.views.length > 1) {
+        const second = await imodel.views.load(spec.views[1]) as DrawingViewState;
+        expect(second).instanceof(DrawingViewState);
+        expect(second.baseModelId).to.equal(first.baseModelId);
+
+        const secondInfo = second.sectionDrawingInfo!;
+        expect(secondInfo).not.to.be.undefined;
+
+        expect(secondInfo.spatialView).to.equal(info.spatialView);
+        expect(secondInfo.drawingToSpatialTransform.isAlmostEqual(info.drawingToSpatialTransform)).to.be.true;
+        expect(secondInfo.displaySpatialView).to.be.false;
+      }
+    }
+  });
+
+  it("updates section drawing info when viewed model changes", async () => {
+    await testOffScreenViewport(specs[0].views[0], imodel, 40, 30, async (vp) => {
+      for (let i = 1; i < specs.length; i++) {
+        const view = vp.view as DrawingViewState;
+        const oldInfo = view.sectionDrawingInfo!;
+        expect(oldInfo).not.to.be.undefined;
+
+        const spec = specs[i];
+        await vp.changeViewedModel2d(spec.model);
+        expect(vp.view).not.to.equal(view);
+
+        const newInfo = (vp.view as DrawingViewState).sectionDrawingInfo!;
+        expect(newInfo).not.to.be.undefined;
+        expect(newInfo).not.to.equal(oldInfo);
+        expect(newInfo.spatialView).to.equal(spec.spatialView);
+      }
+    });
+  });
+
+  it("clones section drawing info", async () => {
+    const first = await imodel.views.load(specs[0].views[0]) as DrawingViewState;
+    const info = first.sectionDrawingInfo!;
+
+    const second = first.clone() as DrawingViewState;
+    expect(second).not.to.equal(first);
+    const secondInfo = second.sectionDrawingInfo!;
+    expect(secondInfo).to.deep.equal(info);
   });
 });
