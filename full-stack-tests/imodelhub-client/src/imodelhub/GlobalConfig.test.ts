32f4a0759c5ba0aa8cdee251841d7c8559979875
Austeja Kalpakovaite <70565417+austeja-bentley@users.noreply.github.com>
2020-12-16 15:45:22 +0200

    Added global setup for iModels

---
+++ full-stack-tests/imodelhub-client/src/imodelhub/GlobalConfig.test.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { AccessToken, AuthorizedClientRequestContext } from "@bentley/itwin-client";
+import { TestUsers } from "@bentley/oidc-signin-tool";
+import { TestConfig } from "../TestConfig";
+import * as utils from "./TestUtils";
+
+before(async () => {
+  const accessToken: AccessToken = TestConfig.enableMocks ? new utils.MockAccessToken() : await utils.login(TestUsers.super);
+  const requestContext = new AuthorizedClientRequestContext(accessToken);
+  const contextId = await utils.getProjectId(requestContext);
+  await utils.createIModel(requestContext, utils.sharedimodelName, contextId);
+});
+
+after(async () => {
+  const accessToken: AccessToken = TestConfig.enableMocks ? new utils.MockAccessToken() : await utils.login(TestUsers.super);
+  const requestContext = new AuthorizedClientRequestContext(accessToken);
+  const contextId = await utils.getProjectId(requestContext);
+  await utils.deleteIModelByName(requestContext, contextId, utils.sharedimodelName);
+})
