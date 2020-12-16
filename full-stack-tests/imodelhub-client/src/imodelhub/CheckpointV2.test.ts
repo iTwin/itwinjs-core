6561863b6cb297d349d678c7b3faa862a1c24405
Austeja Kalpakovaite <70565417+austeja-bentley@users.noreply.github.com>
2020-12-16 18:31:10 +0200

    Removed string duplication

--- full-stack-tests/imodelhub-client/src/imodelhub/CheckpointV2.test.ts
+++ full-stack-tests/imodelhub-client/src/imodelhub/CheckpointV2.test.ts
@@ -99,7 +99,6 @@ describe("iModelHub CheckpointV2Handler", () => {
   let iModelClient: IModelClient;
   let briefcase: Briefcase;
   let changeSets: ChangeSet[];
-  const imodelName = "imodeljs-clients Shared iModel";
   let requestContext: AuthorizedClientRequestContext;
 
   before(async function () {
@@ -108,8 +107,8 @@ describe("iModelHub CheckpointV2Handler", () => {
     requestContext = new AuthorizedClientRequestContext(accessToken);
 
     contextId = await utils.getProjectId(requestContext);
-    await utils.createIModel(requestContext, imodelName, contextId, true, true);
-    imodelId = await utils.getIModelId(requestContext, imodelName, contextId);
+    await utils.createIModel(requestContext, utils.sharedimodelName, contextId, true, true);
+    imodelId = await utils.getIModelId(requestContext, utils.sharedimodelName, contextId);
     iModelClient = utils.getDefaultClient();
     briefcase = (await utils.getBriefcases(requestContext, imodelId, 1))[0];
 
@@ -123,7 +122,7 @@ describe("iModelHub CheckpointV2Handler", () => {
 
   after(async () => {
     if (TestConfig.enableIModelBank)
-      await utils.deleteIModelByName(requestContext, contextId, imodelName);
+      await utils.deleteIModelByName(requestContext, contextId, utils.sharedimodelName);
   });
 
   afterEach(() => {
