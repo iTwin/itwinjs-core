ff87004f1cc4a269aaf6f44a552ee64a2292e531
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2020-12-21 15:38:56 -0500

    Move CartographicRange tests.

--- full-stack-tests/core/src/frontend/standalone/Viewport.test.ts
+++ full-stack-tests/core/src/frontend/standalone/Viewport.test.ts
@@ -253,31 +253,6 @@ describe("Viewport", () => {
   });
 });
 
-describe("Cartographic range tests", () => {
-
-  let imodel: IModelConnection;
-
-  before(async () => {
-    await IModelApp.startup();
-    imodel = await SnapshotConnection.openFile("mirukuru.ibim");
-  });
-
-  after(async () => {
-    if (imodel)
-      await imodel.close();
-
-    await IModelApp.shutdown();
-  });
-
-  it("Cartographic range should convert properly", () => {
-    const projectRange = new CartographicRange(imodel.projectExtents, imodel.ecefLocation!.getTransform());
-    const expected = Range2d.fromJSON({ low: { x: 2.316129378420503, y: 0.5995855439816498 }, high: { x: 2.316183773897448, y: 0.5996166857950551 } });
-    const longLatBox = projectRange.getLongitudeLatitudeBoundingBox();
-    assert.isTrue(longLatBox.isAlmostEqual(expected), "range matches correctly");
-  });
-});
-
-
 class ViewportChangedHandler {
   private readonly _vp: Viewport;
   private readonly _removals: Array<() => void> = [];
