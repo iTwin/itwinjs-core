bed997cab3ce2748babdd5edc8e5ea59892205f2
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2020-12-30 10:37:26 -0500

    Docs tweaks.

--- core/frontend/src/render/ScreenSpaceEffectBuilder.ts
+++ core/frontend/src/render/ScreenSpaceEffectBuilder.ts
@@ -82,7 +82,7 @@ export interface UniformParams {
   type: UniformType;
   /** The name of the variable. It must be unique among all uniforms used by the shader program. */
   name: string;
-  /** A function that computes the value of the variable and binds it to the shader program. */
+  /** A function that computes the value of the variable and binds it to the shader program each time the effect is rendered. */
   bind: (uniform: Uniform, context: UniformContext) => void;
 }
 
@@ -95,14 +95,14 @@ export interface UniformArrayParams extends UniformParams {
   length: number;
 }
 
-/** The GLSL implementation of the effect, to be integrated into a complete shader program. The effect shader code differs slightly from that of an ordinary shader:
+/** The GLSL implementation of the effect produced by a [[ScreenSpaceEffectBuilder]], to be integrated into a complete shader program. The effect shader code differs slightly from that of an ordinary shader:
  *  - Instead of `main`, it should implement `effectMain`.
+ *    - It can include other functions, variables, etc outside of `effectMain`.
  *  - It should omit declarations of uniform and varying variables - these will be generated from those supplied to [[ScreenSpaceEffectBuilder.addUniform]] and [[ScreenSpaceEffectBuilder.addVarying]].
- *  - It can include other functions, variables, etc outside of `effectMain`.
  * The program receives one pre-defined `uniform sampler2D u_diffuse` representing the viewport's rendered image.
  * Because the [[RenderSystem]] uses either WebGL1 or WebGL2 based on the capabilities of the client, the effect shader should be written to compile with either; or, [[ScreenSpaceEffectBuilder.isWebGL2]] should be tested.
  * The [[RenderSystem]] takes care of adjusting the source code for some of these differences, e.g., `varying` (WebGL1) vs `in` and `out` (WebGL2);
- * and `TEXTURE`, `TEXTURE_CUBE`, and `TEXTURE_PROJ` macros to replace `texture2D`, `textureCube`, and `texture2DProj` with their WebGL2 equivalents when applicable.
+ * and `TEXTURE`, `TEXTURE_CUBE`, and `TEXTURE_PROJ` macros are provided to replace `texture2D`, `textureCube`, and `texture2DProj` with their WebGL2 equivalents when applicable.
  * @beta
  */
 export interface ScreenSpaceEffectSource {
@@ -112,7 +112,8 @@ export interface ScreenSpaceEffectSource {
   vertex: string;
 
   /** The GLSL implementation of the fragment shader. Instead of `main`, it implements `vec4 effectMain()` returning the color to be output.
-   * `effectMain` should sample `u_diffuse` using `TEXTURE()` or `TEXTURE_PROJ()` instead of `texture2D()`, `texture2DProj()`, or `texture()`.
+   * `effectMain` should sample `u_diffuse` directly using `TEXTURE()` or `TEXTURE_PROJ()` instead of `texture2D()`, `texture2DProj()`, or `texture()`;
+   * or, if [[ScreenSpaceEffectSource.sampleSourcePixel]] is defined, it can use `sampleSourcePixel()` instead.
    * It should not assign to `gl_FragColor`.
    * The alpha component of the output color is ignored as there is nothing with which to blend.
    */
@@ -125,6 +126,7 @@ export interface ScreenSpaceEffectSource {
    * This function will automatically be included in the fragment shader, so it can also be used by `effectMain` when computing the output color.
    * @note `sampleSourcePixel` should not modify the sample in any way - that should be done only in `effectMain`.
    * @note `sampleSourcePixel` should **not** be supplied if the effect does **not** shift pixels as it can negatively impact performance of element locate.
+   * @see [FlipImageEffect]($frontend-devtools) or [LensDistortionEffect]($frontend-devtools) for examples of effects that implement this property.
    */
   sampleSourcePixel?: string;
 }
@@ -134,7 +136,7 @@ export interface ScreenSpaceEffectSource {
  * @beta
  */
 export interface ScreenSpaceEffectBuilderParams {
-  /** The name of the effect. Must be unique among all registered screen-space effects. */
+  /** The name of the effect. Must be unique among all registered screen-space effects. It is not displayed to the user. */
   name: string;
   /** If true, adds a `vec2 textureCoordFromPosition(vec4 position)` function to the vertex shader that computes a UV coordinate based on the vertex's position. */
   textureCoordFromPosition?: boolean;
@@ -155,17 +157,24 @@ export interface ScreenSpaceEffectContext {
 
 /** An interface used to construct and register with the [[IModelApp.renderSystem]] a custom screen-space effect.
  * Screen-space effects take as input the image rendered by a Viewport, as a WebGL texture, and execute a shader program to modify the image.
- * Any number of screen-space effects can be registered; they are processed in the order in which they were registered.
- * Each frame, the [[RenderSystem]] does the following:
- *  - Render the scene to a texture.
- *  - For each registered screen-space effect, in the order in which they were registered:
- *    - If `shouldApply` is undefined, or returns true, apply the effect
+ * Any number of screen-space effects can be registered, but each must have a unique name. Each Viewport has an ordered list of effects to be applied to it.
+ *
+ * Each time a Viewport's contents are rendered, the [[RenderSystem]] does the following:
+ *  - Render Viewport's contents to a texture.
+ *  - For each effect name in [[Viewport.screenSpaceEffects]]:
+ *    - Look up the corresponding registered effect.
+ *    - If `shouldApply is defined and returns false, skip the effect. Otherwise:
+ *    - For each [[Uniform]] defined by the effect, invoke its `bind` property to set its current value.
+ *    - Bind the Viewport's rendered image to the uniform `u_diffuse`.
+ *    - Execute the effect shader to alter the viewport's image.
+ * In this way, a series of multiple effects can be chained together, each consuming as input the image output by the previous effect.
+ *
  * A screen-space effect that **moves** pixels from their original locations rather than simply recoloring them may cause some tools to behave unexpectedly:
  *  - Element locate will only work correctly if [[ScreenSpaceEffectBuilderParams.sampleSourcePixel]] is properly defined.
  *  - Tools like the measurement tool that require snapping to element geometry will not snap correctly since the element geometry has been distorted by the shader.
  * @see [[RenderSystem.createScreenSpaceEffectBuilder]] to create and register a new effect.
  * @see [[ScreenSpaceEffectBuilderParams]] to define the initial state of the builder.
- * @see [[Viewport.screenSpaceEffects]] to apply effects to a Viewport.
+ * @see [[Viewport.screenSpaceEffects]], [[Viewport.addScreenSpaceEffect]], and [[Viewport.removeScreenSpaceEffects]] to change the effects applied to a viewport.
  * @see [ConvolutionEffect]($frontend-devtools) for examples of effects like blur, sharpen, and emboss.
  * @see [LensDistortionEffect]($frontend-devtools) for an simulation of the fish-eye distortion produced by real-world cameras with very wide fields of view.
  * @see [FlipImageEffect]($frontend-devtools) for a very simple example of an effect that shifts pixels from their original locations.
