66c87fe800193d1b57c0f84cc5799667a4565bb5
nick.tessier <22119573+nick4598@users.noreply.github.com>
2021-01-05 10:39:06 -0500

    Add semver function for use instead of just string comparison with >=

--- tools/webpack-core/src/plugins/IModeljsLibraryImportsPlugin.ts
+++ tools/webpack-core/src/plugins/IModeljsLibraryImportsPlugin.ts
@@ -6,12 +6,32 @@
 import { Compiler } from "webpack";
 import { ConcatSource } from "webpack-sources";
 import * as utils from "./IModeljsLibraryUtils";
-
 function getLoaderString(pkgName: string, pkgVersion: string) {
-  return `  module.exports = (() => {
+  return `  const semverGte = (appVersion, extensionVersion) => {
+    const dashIndexApp = appVersion.indexOf("-");
+    const dashIndexExtension = extensionVersion.indexOf("-");
+    if (dashIndexExtension >= 0 ) {
+      console.warn("This extension has a dependency on a dev version. Use in prod with caution.");
+      extensionVersion = extensionVersion.substring(0, dashIndexExtension);
+    }
+    if (dashIndexApp >= 0) appVersion = appVersion.substring(0, dashIndexApp);
+    const appVersionSplit = appVersion.split(".").map((value) => parseInt(value));
+    const extensionVersionSplit = extensionVersion.split(".").map((value) => parseInt(value));
+    if (appVersionSplit.length !== 3)
+      throw new Error("An iModel.js Shared Library is not versioned correctly. Expected 3 numbers separated by . for the version");
+    if (extensionVersionSplit.length !== 3) throw new Error("Extension is not versioned correctly.Expected 3 numbers separated by . for the version.");
+    for (const [index, value] of appVersionSplit.entries()) {
+      if (value > extensionVersionSplit[index])
+        return true;
+      else if (value < extensionVersionSplit[index])
+        return false;
+    }
+    return true; // If we don't return while in the loop, then we're in the case where the versions are equal.
+  };
+  module.exports = (() => {
     if (!window.${utils.IMJS_GLOBAL_OBJECT} || !window.${utils.IMJS_GLOBAL_LIBS_VERS} || !window.${utils.IMJS_GLOBAL_LIBS})
       throw new Error("Expected globals are missing!");
-    if (window.${utils.IMJS_GLOBAL_LIBS_VERS}[${pkgName}] >= ${pkgVersion})
+    if (semverGte(window.${utils.IMJS_GLOBAL_LIBS_VERS}[${pkgName}], ${pkgVersion}))
       return window.${utils.IMJS_GLOBAL_LIBS}[${pkgName}];
     if (window.${utils.IMJS_GLOBAL_LIBS}[${pkgName}])
       throw new Error("iModel.js Shared Library " + ${pkgName} + " is loaded, but is an incompatible version." )
