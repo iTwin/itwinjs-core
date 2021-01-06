bfafb1e26aa3a9c405a75966098dc0a843cf6570
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-01-06 15:34:34 -0500

    documentation, clarity.

--- core/frontend-devtools/src/effects/Snow.ts
+++ core/frontend-devtools/src/effects/Snow.ts
@@ -11,13 +11,21 @@ import { ColorDef, GraphicParams } from "@bentley/imodeljs-common";
 import { DecorateContext,GraphicType, IModelApp, Tool, Viewport } from "@bentley/imodeljs-frontend";
 import { parseToggle } from "../tools/parseToggle";
 
-// Generate integer in [min, max], inclusive.
+/** Generate integer in [min, max]. */
 function randomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/** Generate floating-point number in [min, max). */
 function randomNumber(min: number, max: number): number {
   return Math.random() * (max - min) + min;
+}
+
+function wrapAround(value: number, min: number, max: number): number {
+  if (value < min)
+    return max;
+
+  return value > max ? min : value;
 }
 
 interface SnowParticle {
@@ -28,14 +36,27 @@ interface SnowParticle {
 }
 
 class SnowDecorator {
+  /** The viewport being decorated. */
   public readonly viewport: Viewport;
+  /** Invoked when this decorator is to be destroyed. */
   public readonly dispose: VoidFunction;
+  /** The number of snow particles to produce. This could alternatively be expressed as a density so that small viewports would not be more crowded than larger ones. */
   public numParticles = 1200;
+  /** Range from which to randomly select each particle's size, in pixels. */
   public readonly sizeRange = Range1d.createXX(1, 15);
+  /** Range from which to randomly select each particle's transparency. */
   public readonly transparencyRange = Range1d.createXX(0, 200);
-  public readonly velocityRange = new Range2d(-20, 20, 20, 70);
+  /** Range from which to randomly select each particle's initial velocity, in pixels per second. */
+  public readonly velocityRange = new Range2d(-25, 30, 25, 90);
+  /** Range from which to randomly select an acceleration to apply to each particle's velocity each frame, in pixels per second squared, to simulate wind. */
+  public readonly accelerationRange = new Range2d(-1, -0.25, 1, 0.25);
+  /** The initial width and height of the viewport, from which we randomly select each particle's initial position.
+   * Note: This example does not react when the viewport's dimensions change.
+   */
   private readonly _dimensions: Point2d;
+  /** The list of particles being drawn. */
   private readonly _particles: SnowParticle[] = [];
+  /** The last time `updateParticles()` was invoked, in milliseconds. */
   private _lastUpdateTime: number;
 
   private constructor(viewport: Viewport) {
@@ -43,9 +64,10 @@ class SnowDecorator {
     this._dimensions = new Point2d(viewport.viewRect.width, viewport.viewRect.height);
     this._lastUpdateTime = Date.now();
 
-    const removeDecorator = IModelApp.viewManager.addDecorator(this);
+    // Tell the viewport to re-render the decorations every frame so that the snow particles animate smoothly.
     const removeOnRender = viewport.onRender.addListener(() => viewport.invalidateDecorations());
     const removeOnDispose = viewport.onDisposed.addListener(() => this.dispose());
+    const removeDecorator = IModelApp.viewManager.addDecorator(this);
 
     this.dispose = () => {
       removeDecorator();
@@ -56,6 +78,7 @@ class SnowDecorator {
 
     SnowDecorator._decorators.set(viewport, this);
 
+    // Initialize the particles.
     for (let i = 0; i < this.numParticles; i++) {
       this._particles.push({
         position: new Point2d(randomInteger(0, this._dimensions.x), randomInteger(0, this._dimensions.y)),
@@ -70,17 +93,23 @@ class SnowDecorator {
     if (context.viewport !== this.viewport)
       return;
 
+    // Update the particles.
     const now = Date.now();
     const deltaMillis = now - this._lastUpdateTime;
     this._lastUpdateTime = now;
     this.updateParticles(deltaMillis / 1000);
 
+    // Create one GraphicParams object to reuse for every particle, for better performance.
     const params = new GraphicParams();
     params.lineColor = ColorDef.white;
 
+    // Create a view overlay because our particles' positions are specified in pixels and we want them to draw in front of everything else.
     const builder = context.createGraphicBuilder(GraphicType.ViewOverlay);
+
+    // The order in which are particles doesn't matter - setting `preserveOrder` to `false` dramatically improves performance.
     builder.preserveOrder = false;
 
+    // Draw a point for each particle. Alternatively, this could draw a square for each and map a snowflake image to them.
     for (const particle of this._particles) {
       params.setLineTransparency(particle.transparency);
       params.rasterWidth = particle.size;
@@ -91,17 +120,24 @@ class SnowDecorator {
     context.addDecorationFromBuilder(builder);
   }
 
-  private updateParticles(timeDelta: number): void {
+  // Update the positions and velocities of all the particles based on the amount of time that has passed since the last update.
+  private updateParticles(elapsedSeconds: number): void {
+    const acceleration = new Vector2d();
+    const velocity = new Vector2d();
     for (const particle of this._particles) {
-      particle.position.y += particle.velocity.y * timeDelta;
-      if (particle.position.y > this._dimensions.y)
-        particle.position.y = 0;
+      // Apply some acceleration to produce random drift.
+      acceleration.set(randomNumber(this.accelerationRange.low.x, this.accelerationRange.high.x), randomNumber(this.accelerationRange.low.y, this.accelerationRange.high.y));
+      acceleration.scale(elapsedSeconds, acceleration);
+      particle.velocity.plus(acceleration, particle.velocity);
 
-      particle.position.x += particle.velocity.x * timeDelta;
-      if (particle.position.x > this._dimensions.x)
-        particle.position.x = 0;
-      else if (particle.position.x < 0)
-        particle.position.x = this._dimensions.x;
+      // Apply velocity.
+      particle.velocity.clone(velocity);
+      velocity.scale(elapsedSeconds, velocity);
+      particle.position.plus(velocity, particle.position);
+
+      // Particles that travel beyond the viewport's borders wrap around to the other side.
+      particle.position.x = wrapAround(particle.position.x, 0, this._dimensions.x);
+      particle.position.y = wrapAround(particle.position.y, 0, this._dimensions.y);
     }
   }
 
