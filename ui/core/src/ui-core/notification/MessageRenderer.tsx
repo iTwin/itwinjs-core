befbb11df7e700f7ae6c518074a6be338e0bd130
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-01-25 09:47:54 -0500

    changes to allow target=_blank if secure relationship is present

--- ui/core/src/ui-core/notification/MessageRenderer.tsx
+++ ui/core/src/ui-core/notification/MessageRenderer.tsx
@@ -34,8 +34,57 @@ export function MessageRenderer(props: MessageRendererProps) {
     messageNode = <OutElement className={props.className}>{props.message}</OutElement>;
   } else if (isHTMLElement(props.message)) {
     const sanitizer = DOMPurify.sanitize; // `sanitizer` is default function name for "jam3/no-sanitizer-with-danger" ESLint rule
+
+    let validAnchors = false;
+    let hasAnchors = false;
+
+    // verify that the tag has proper relationships
+    const isAnchorValid = (anchor: HTMLAnchorElement): boolean => {
+      return anchor.hasAttribute("rel") && (anchor.rel.includes("noopener") || anchor.rel.includes("noreferrer"));
+    };
+
+    // recursively check child elements for valid anchor tags
+    const checkChildAnchors = (parent: Element) => {
+      if (hasAnchors && !validAnchors) {
+        return;
+      }
+      const children = Array.from(parent.children);
+      for (let i = 0; i < children.length; i++){
+        let child = children[i];
+        if (child.hasAttribute("target") && (child as HTMLAnchorElement).target === "_blank") {
+          hasAnchors = true;
+          validAnchors = isAnchorValid(child as HTMLAnchorElement);
+          if (!validAnchors) {
+            return;
+          }
+        }
+        checkChildAnchors(child);
+      }
+    };
+
+    // check for anchor tags in the message that have target _blank also have a relationship that avoids security holes
+    // https://web.dev/external-anchors-use-rel-noopener/
+    // first check the message element
+    if (props.message.hasAttribute("target") && (props.message as HTMLAnchorElement).target === "_blank") {
+      hasAnchors = true;
+      validAnchors = isAnchorValid(props.message as HTMLAnchorElement);
+    } else {
+      // if the message element is not an anchor, recursively verify it's children
+      if (props.message.children) {
+        checkChildAnchors(props.message);
+      }
+    }
+
+    let sanitizedHtml;
+    if (hasAnchors && validAnchors) {
+      // all anchors are valid. do not remove the target attr
+      sanitizedHtml = sanitizer(props.message.outerHTML, {ADD_ATTR: ["target"]});
+    } else {
+      sanitizedHtml = sanitizer(props.message.outerHTML);
+    }
+
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    messageNode = <OutElement className={props.className} dangerouslySetInnerHTML={{ __html: sanitizer(props.message.outerHTML) }} />;
+    messageNode = <OutElement className={props.className} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
   } else {
     /* istanbul ignore else */
     if (isReactMessage(props.message))
