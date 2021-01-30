2569da90016cd2702853cba0e4672dcf4d5861de
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-01-30 16:04:28 -0500

    wip

--- core/mobile-manager/src/MobileBackend.ts
+++ core/mobile-manager/src/MobileBackend.ts
@@ -5,9 +5,9 @@
 
 // Barrel file for backend of a mobile application
 
-export * from "./backend/AndroidHost";
-export * from "./backend/iOSHost";
-export * from "./backend/MobileHost";
-export * from "./backend/AuthorizationBackend";
+export * from "./Backend-1/AndroidHost";
+export * from "./Backend-1/iOSHost";
+export * from "./Backend-1/MobileHost";
+export * from "./Backend-1/AuthorizationBackend";
 export * from "./common/MobileRpcManager";
 
