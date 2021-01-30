d7681171c6603dbf731fbb74ce6ed43fb80bf1c6
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-01-30 16:05:16 -0500

    wip

--- core/mobile-manager/src/MobileFrontend.ts
+++ core/mobile-manager/src/MobileFrontend.ts
@@ -3,9 +3,9 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-export * from "./Frontend-1/AndroidApp";
-export * from "./Frontend-1/AuthorizationFrontend";
-export * from "./Frontend-1/IOSApp";
-export * from "./Frontend-1/MobileApp";
+export * from "./frontend/AndroidApp";
+export * from "./frontend/AuthorizationFrontend";
+export * from "./frontend/IOSApp";
+export * from "./frontend/MobileApp";
 export * from "./common/MobileRpcManager";
 export * from "./common/MobileAuthorizationClientConfiguration";
