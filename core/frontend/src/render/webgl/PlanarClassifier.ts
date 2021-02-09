30100b393739d599e0d0ef9b654ee3d0ed9ddd61
MarcNeely <36053767+MarcNeely@users.noreply.github.com>
2021-02-09 09:26:15 -0600

    Correct bad test to limit texture size.

--- core/frontend/src/render/webgl/PlanarClassifier.ts
+++ core/frontend/src/render/webgl/PlanarClassifier.ts
@@ -346,7 +346,7 @@ export class PlanarClassifier extends RenderPlanarClassifier implements RenderMe
 
     // TBD - Refine resolution calculation -- increase height based on viewing angle.
     // But make sure that we don't make it larger than the hardware supports, keeping in mind that we will be doubling the height later.
-    const requiredHeight = Math.max(Math.max(context.target.viewRect.width, context.target.viewRect.height), System.instance.capabilities.maxTextureSize / 2);
+    const requiredHeight = Math.min(Math.max(context.target.viewRect.width, context.target.viewRect.height), System.instance.capabilities.maxTextureSize / 2);
     const requiredWidth = requiredHeight;
 
     if (requiredWidth !== this._width || requiredHeight !== this._height)
