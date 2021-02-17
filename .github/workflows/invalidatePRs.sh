d404ddf75287d86b36929901d9ca5307205b7254
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2021-02-17 07:29:22 -0500

    WIP

--- .github/workflows/invalidatePRs.sh
+++ .github/workflows/invalidatePRs.sh
@@ -5,50 +5,75 @@
 # It will only invalidate open non-draft pull requests and mark each statuses
 #
 
+oauth=''
+
 # Get all active PRs
 # https://docs.github.com/en/rest/reference/pulls#list-pull-requests
-prs=$(curl \
+prs=$(curl -s \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/imodeljs/itwin-viewer/pulls)
+  -H "Authorization: token $oauth" \
+  https://api.github.com/repos/imodeljs/imodeljs/pulls)
 
-# test=$(echo $prs | jq '.[]')
+_jq() {
+  echo ${row} | base64 --decode | jq -r ${1}
+}
 
-# echo $test
+_jq2() {
+  echo ${status} | base64 --decode | jq -r ${1}
+}
 
-# jq '.[] | .version'
+for row in $(echo "${prs}" | jq -r '.[] | @base64'); do
+  if [[ "open" != $(_jq '.state') ]] || [[ "true" == $(_jq '.draft') ]]; then
+    # echo Skipping $(_jq '.title') with state $(_jq '.state') and draft $(_jq '.draft')
+    continue
+  fi
 
-for pr in $(echo ${prs} | jq '.[]'); do
-    # _jq() {
-    #  echo ${row} | base64 --decode | jq -r ${1}
-    # }
+  if [[ "d13ea94c5dc0f0f35492de5ebe12981eb0e83372" != $(_jq '.head.sha') ]]; then
+    continue
+  fi
 
-    echo $pr
+  echo Checking $(_jq '.title') updated last at $(_jq '.updated_at')
 
-    break
+  ## TODO Need to use to test for how long ago it expired
+  # ['updated_at']
+  # now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  # echo Current time $(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-   # echo $(_jq '.name')
+  # Get all statuses of the PR
+  #   e.g. https://api.github.com/repos/imodeljs/imodeljs/statuses/c29a168b6a201052098ea6743041868bfbadaa4f
+  statuses=$(curl -s \
+              -H "Accept: application/vnd.github.v3+json" \
+              -H "Authorization: token $oauth" \
+              $(_jq '.statuses_url'))
+
+  for status in $(echo "${statuses}" | jq -r '.[] | @base64'); do
+    if [[ "success" != $(_jq2 '.state') ]] || [[ "license/cla" == $(_jq2 '.context') ]]; then
+      continue
+    fi
+
+    #echo $(_jq '.head.sha')
+
+    target_url=$(_jq2 '.target_url')
+    body=
+
+    #echo $body
+
+    # testing
+    if [[ "d13ea94c5dc0f0f35492de5ebe12981eb0e83372" != $(_jq '.head.sha') ]]; then
+      continue
+    fi
+
+    updateUrl=https://api.github.com/repos/imodeljs/imodeljs/statuses/$(_jq '.head.sha')
+    echo $updateUrl
+
+    curl \
+      -X POST \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token $oauth" \
+      $updateUrl \
+      -d '{"state":"failed","target_url":"'$(_jq2 '.target_url')'","description":"The build hit the 3 hour threshold. Please re-queue the build.","context":"'$(_jq2 '.context')'"}'
+  done
 done
-
-# If ['state'] === 'open' and ['draft'] === false
-
-## TODO Need to use to test for how long ago it expired
-# ['updated_at']
-
-# Get all statuses of the PR
-#   e.g. https://api.github.com/repos/imodeljs/imodeljs/statuses/c29a168b6a201052098ea6743041868bfbadaa4f
-# ['statuses_url']
-
-# loop over the statuses
-# Invalidate if ['state'] === 'success' && ['context'] !== 'license/cla'
-# ['status']
-
-# Re-create the state of status
-#   {
-#     "state":
-#     "target_url":
-#     "description": "The build has timed out"
-#     "context":
-#   }
 
 # Get the sha of the pr ['head']['sha']
 # curl \
