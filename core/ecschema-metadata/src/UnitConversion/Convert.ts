5e788184854544443f3b29698687c05d47eb9382
Ivan.Kok <ivan.kok@users.nopereply.github.com>
2021-02-18 11:19:23 -0500

    Removing graphlib and unused files in UnitConversion

--- core/ecschema-metadata/src/UnitConversion/Convert.ts
+++ core/ecschema-metadata/src/UnitConversion/Convert.ts
@@ -2,141 +2,74 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { BentleyError, BentleyStatus } from "@bentley/bentleyjs-core";
-import { SchemaContext } from "../Context";
-import { SchemaItemType } from "../ECObjects";
-import { Unit } from "../Metadata/Unit";
-import { SchemaItemKey } from "../SchemaKey";
-import { LinearMap } from "./LinearMap";
-import { UnitGraph } from "./UnitTree";
+import { Schema, SchemaItemKey, Unit } from "../ecschema-metadata";
+import { parseDefinition } from "./Parser";
 
-/**
- * Convertor function type. Given a unit magnitude in a source unit, return the converted magnitude of the target unit.
- */
-export type ConvertorFunction = (from: number) => number;
+let schema: Schema;
+let schemaMap = new Map();
 
-/**
- * Convertor context is used to process unit conversion
- */
-export class UnitConvertorContext {
-  static identity: (x: number) => number = (x) => x;
+// Map schema with unit names as keys
+export function setSchema(schemaInput: Schema) {
+  schema = schemaInput;
+  let iter = schema.getItems();
+  for (let elem of iter) {
+    let unit = elem as Unit;
+    schemaMap.set(elem.name, {
+      definition: unit.definition,
+      numerator: unit.numerator,
+      denominator: unit.denominator,
+      offset: unit.offset || 0,
+    });
+  }
+}
 
-  private _uGraph: UnitGraph;
+type Conversion = {
+  multiplier: number;
+  offset: number;
+};
 
-  /**
-   * Create convertor context
-   * @param context SchemaContext with contexts added to it.
-   */
-  constructor(private readonly context: SchemaContext) {
-    this._uGraph = new UnitGraph(this.context);
+export function getConversion(
+  fromUnit: SchemaItemKey,
+  toUnit: SchemaItemKey
+): Conversion {
+  let from = recursiveCalculate(fromUnit.name, true);
+  let to = recursiveCalculate(toUnit.name, false);
+  return {
+    multiplier: from.multiplier / to.multiplier,
+    offset: from.offset / to.multiplier - to.offset,
+  };
+}
+
+// Recursive function to traverse from unit to unit
+function recursiveCalculate(unitName: string, isFrom: boolean): Conversion {
+  let currentUnit = schemaMap.get(unitName);
+  // Definition returns a map of children needed to calculate
+  let unitChildren = parseDefinition(currentUnit.definition);
+
+  // Base case where unit name equals its definition, like M, KG, ONE
+  if (unitName === currentUnit.definition) {
+    return { multiplier: 1, offset: 0 };
   }
 
-  /**
-   * Process source and target units.
-   * @param from item key of source unit
-   * @param to item key of target unit
-   * @returns convertor function to convert source unit value to target unit
-   */
-  async processSchemaItem(
-    from: SchemaItemKey,
-    to: SchemaItemKey
-  ): Promise<LinearMap> {
-    const fromItem = await this.context.getSchemaItem(from);
-    if (fromItem?.schemaItemType !== SchemaItemType.Unit)
-      throw new BentleyError(
-        BentleyStatus.ERROR,
-        "Schema item is not a unit",
-        () => {
-          return { schemaItem: from };
-        }
-      );
-    const toItem = await this.context.getSchemaItem(to);
-    if (toItem?.schemaItemType !== SchemaItemType.Unit)
-      throw new BentleyError(
-        BentleyStatus.ERROR,
-        "Schema item is not a unit",
-        () => {
-          return { schemaItem: to };
-        }
-      );
+  let aggregate = { multiplier: 1, offset: 0 };
+  // Calculate each children
+  unitChildren.forEach((value, key) => {
+    let result = recursiveCalculate(key, isFrom);
 
-    return this.processUnits(fromItem as Unit, toItem as Unit);
+    aggregate.multiplier *= result.multiplier ** value.exponent;
+    aggregate.offset += result.offset;
+  });
+
+  let fraction = currentUnit.numerator / currentUnit.denominator
+  aggregate.multiplier *= fraction;
+
+  if (isFrom) {
+    aggregate.offset =
+      aggregate.offset + currentUnit.offset * fraction; // Multiply current offset by fraction then add previous offset
+  } else {
+    aggregate.offset =
+      aggregate.offset / fraction + currentUnit.offset; // Divide previous offset by fraction then add current offset
   }
 
-  async processUnits(from: Unit, to: Unit): Promise<LinearMap> {
-    if (from.key.matches(to.key)) return LinearMap.Identity;
-
-    const fromPhenomenon = await from.phenomenon;
-    const toPhenomenon = await to.phenomenon;
-
-    if (
-      !fromPhenomenon ||
-      !toPhenomenon ||
-      !fromPhenomenon.key.matches(toPhenomenon.key)
-    )
-      throw new BentleyError(
-        BentleyStatus.ERROR,
-        `Source and target units do not belong to same phenomenon`,
-        () => {
-          return { from, to };
-        }
-      );
-
-    await this._uGraph.addUnit(from);
-    await this._uGraph.addUnit(to);
-    // Collect all the units reachable from to unit
-    // const { fromUnits, toUnits } = ConvertorContext.commonDescendants(this._uGraph, to, from);
-
-    const fromMapStore = this._uGraph.reduce(from, new Set<string>());
-    const toMapStore = this._uGraph.reduce(to, new Set<string>());
-
-    const fromMap = fromMapStore.get(from.key.fullName) || LinearMap.Identity;
-    const toMap = toMapStore.get(to.key.fullName) || LinearMap.Identity;
-    const fromInverse = fromMap.inverse();
-    return fromInverse.compose(toMap);
-  }
-
-  // Find common descendants for both units in the graph
-  // public static commonDescendants(
-  //   uGraph: UnitGraph,
-  //   to: Unit,
-  //   from: Unit
-  // ): { fromUnits: Set<string>; toUnits: Set<string> } {
-  //   const toAllUnits = uGraph.bfsReduce(
-  //     to,
-  //     (p, c) => {
-  //       p.add(c.key.fullName);
-  //       return [p, true];
-  //     },
-  //     new Set<string>()
-  //   );
-
-  //   // Stop whenever we reach any unit reachable by to unit
-  //   const fromUnits = uGraph.bfsReduce(
-  //     from,
-  //     (p, c) => {
-  //       if (toAllUnits.has(c.key.fullName)) {
-  //         p.add(c.key.fullName);
-  //         return [p, false];
-  //       }
-
-  //       return [p, true];
-  //     },
-  //     new Set<string>()
-  //   );
-
-  //   // Now units in target that stops at the fromUnits
-  //   const toUnits = uGraph.bfsReduce(
-  //     from,
-  //     (p, c) => {
-  //       if (fromUnits.has(c.key.fullName)) {
-  //         p.add(c.key.fullName);
-  //         return [p, false];
-  //       }
-  //       return [p, true];
-  //     },
-  //     new Set<string>()
-  //   );
-  //   return { fromUnits, toUnits };
-  // }
+  return aggregate;
 }
