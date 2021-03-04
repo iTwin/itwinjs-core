b62eeffb73412dcc2194d5286f6822b333644c20
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-03-04 09:30:29 -0500

    full stack tests.

--- full-stack-tests/core/src/frontend/standalone/tile/TileRequest.test.ts
+++ full-stack-tests/core/src/frontend/standalone/tile/TileRequest.test.ts
@@ -3,24 +3,148 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
+import { BeDuration } from "@bentley/bentleyjs-core";
+import { RenderMode, ServerTimeoutError } from "@bentley/imodeljs-common";
+import {
+  IModelApp, IModelTile, IModelTileTree, IpcApp, SnapshotConnection, Tile, TileLoadStatus, TileRequestChannel, Viewport, ViewState,
+} from "@bentley/imodeljs-frontend";
+import { TILE_DATA_2_0 } from "./data/TileIO.data.2.0";
 
-describe("TileRequestChannel", () => {
-  it("observes limits on max active requests", async () => {
+describe("CloudStorageCacheChannel", () => {
+  let imodel: SnapshotConnection;
+
+  beforeEach(async () => {
+    await IModelApp.startup();
+    imodel = await SnapshotConnection.openFile("test.bim");
   });
 
-  it("changes max active requests", async () => {
+  afterEach(async () => {
+    await imodel.close();
+    await IModelApp.shutdown();
   });
 
-  it("processes requests", async () => {
+  function getChannel(): TileRequestChannel {
+    const channels = IModelApp.tileAdmin.channels;
+    if (!channels.cloudStorageCache) {
+      channels.enableCloudStorageCache();
+      expect(channels.cloudStorageCache).not.to.be.undefined;
+    }
+
+    return channels.cloudStorageCache!;
+  }
+
+  async function getTile(): Promise<IModelTile> {
+    await imodel.models.load("0x1c");
+    const model = imodel.models.getLoaded("0x1c")!.asGeometricModel!;
+    const view = {
+      iModel: imodel,
+      viewFlags: {
+        renderMode: RenderMode.SmoothShade,
+        visibleEdges: false,
+      },
+      is3d: () => true,
+    } as unknown as ViewState;
+
+    const ref = model.createTileTreeReference(view);
+    const tree = (await ref.treeOwner.loadTree()) as IModelTileTree;
+
+    // The root tile marks itself as "ready" immediately. Make it "not loaded" instead.
+    tree.staticBranch.disposeContents();
+    return tree.staticBranch;
+  }
+
+  async function waitUntil(condition: () => boolean): Promise<void> {
+    await BeDuration.wait(1);
+    if (!condition())
+      return waitUntil(condition);
+  }
+
+  async function loadContent(tile: Tile): Promise<void> {
+    const viewport = {
+      viewportId: 12345,
+      iModel: tile.tree.iModel,
+      invalidateScene: () => { },
+    } as Viewport;
+
+    IModelApp.tileAdmin.requestTiles(viewport, new Set<Tile>([tile]));
+    IModelApp.tileAdmin.process();
+    return waitUntil(() => tile.loadStatus !== TileLoadStatus.Queued && tile.loadStatus !== TileLoadStatus.Loading);
+  }
+
+  it("is not configured by default", async () => {
+    expect(IModelApp.tileAdmin.channels.cloudStorageCache).to.be.undefined;
+    const tile = await getTile();
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.iModelTileRpc);
   });
 
-  it("can override Tile.requestContent", () => {
+  it("uses http concurrency", async () => {
+    const channel = getChannel();
+    expect(channel.concurrency).to.equal(IModelApp.tileAdmin.channels.httpConcurrency);
   });
 
-  it("can accumulate cancellations", async () => {
+  it("is used first if configured", async () => {
+    IModelApp.tileAdmin.channels.enableCloudStorageCache();
+    expect(IModelApp.tileAdmin.channels.cloudStorageCache).not.to.be.undefined;
+    const tile = await getTile();
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.cloudStorageCache);
+
+    tile.channel.requestContent = () => Promise.resolve(TILE_DATA_2_0.rectangle.bytes);
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.Ready);
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.cloudStorageCache);
   });
 
-  it("produces statistics", async () => {
+  it("falls back to RPC if content is not found", async () => {
+    IModelApp.tileAdmin.channels.enableCloudStorageCache();
+    const tile = await getTile();
+    const channel = getChannel();
+    expect(tile.channel).to.equal(channel);
+
+    channel.requestContent = () => Promise.resolve(undefined);
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.NotLoaded);
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.iModelTileRpc);
+
+    tile.channel.requestContent = () => Promise.resolve(TILE_DATA_2_0.rectangle.bytes);
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.Ready);
+  });
+
+  it("is not used again after cache miss", async () => {
+    IModelApp.tileAdmin.channels.enableCloudStorageCache();
+    const tile = await getTile();
+    const channel = getChannel();
+
+    channel.requestContent = () => { throw new ServerTimeoutError("...") };
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.NotLoaded);
+    expect(tile.channel).to.equal(channel);
+    expect(tile.cacheMiss).to.be.false;
+
+    channel.requestContent = () => Promise.resolve(undefined);
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.NotLoaded);
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.iModelTileRpc);
+    expect(tile.cacheMiss).to.be.true;
+
+    tile.channel.requestContent = () => Promise.resolve(TILE_DATA_2_0.rectangle.bytes);
+    await loadContent(tile);
+    expect(tile.loadStatus).to.equal(TileLoadStatus.Ready);
+
+    tile.disposeContents();
+    expect(tile.loadStatus).to.equal(TileLoadStatus.NotLoaded);
+    expect(tile.cacheMiss).to.be.true;
+    expect(tile.channel).to.equal(IModelApp.tileAdmin.channels.iModelTileRpc);
   });
 });
 
+describe("RPC channels", () => {
+  before(async () => await IModelApp.startup());
+  after(async () => await IModelApp.shutdown());
+
+  it("use http or rpc concurrency based on type of app", async () => {
+    const channels = IModelApp.tileAdmin.channels;
+    for (const channel of [channels.iModelTileRpc, channels.elementGraphicsRpc])
+      expect(channel.concurrency).to.equal(IpcApp.isValid ? channels.rpcConcurrency : channels.httpConcurrency);
+  });
+});
