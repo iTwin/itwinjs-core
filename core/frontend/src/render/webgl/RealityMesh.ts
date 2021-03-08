9abb21691ac71222b51b996c45d9819649609f8b
Ray.Bentley <rbbentley@users.noreply.github.com>
2021-03-08 16:38:00 -0500

    Fix translucent reality mesh classificaiton

--- core/frontend/src/render/webgl/RealityMesh.ts
+++ core/frontend/src/render/webgl/RealityMesh.ts
@@ -242,8 +242,7 @@ export class RealityMeshGeometry extends IndexedGeometry implements IDisposable,
     if (target.nonLocatableTerrain && !target.drawNonLocatable)
       return RenderPass.None;
 
-    if (target.terrainTransparency > 0.0 || this._baseIsTransparent || target.currentPlanarClassifier?.anyTranslucent  ||
-      (target.wantThematicDisplay && target.uniforms.thematic.wantIsoLines))
+    if (target.terrainTransparency > 0.0 || this._baseIsTransparent || (target.wantThematicDisplay && target.uniforms.thematic.wantIsoLines))
       return RenderPass.Translucent;
 
     return RenderPass.OpaqueGeneral;
