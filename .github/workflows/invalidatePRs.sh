e554200954b5e2dd7e586187e2e583252588c7bd
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2021-03-15 16:59:52 -0400

    Add better logging and correctly handle timeout

--- .github/workflows/invalidatePRs.sh
+++ .github/workflows/invalidatePRs.sh
@@ -11,6 +11,12 @@ if [[ "" == $token ]]; then
 fi
 
 oauth=$token
+shouldDebug=false
+
+if [[ "" != $debug ]]; then
+  shouldDebug=true
+  echo Initializing debug log level
+fi
 
 # Get all active PRs
 # https://docs.github.com/en/rest/reference/pulls#list-pull-requests
@@ -20,44 +26,38 @@ prs=$(curl -s \
   https://api.github.com/repos/imodeljs/imodeljs/pulls)
 
 _jq() {
-  echo ${row} | base64 --decode | jq -r ${1}
+  echo ${pr} | base64 --decode | jq -r ${1}
 }
 
 _jq2() {
   echo ${status} | base64 --decode | jq -r ${1}
 }
 
-for row in $(echo "${prs}" | jq -r '.[] | @base64'); do
+function log() {
+  if [[ $shouldDebug == true  ]]; then
+    echo "${1}"
+  fi
+}
+
+for pr in $(echo "${prs}" | jq -r '.[] | @base64'); do
+  #echo ${pr} | base64 --decode
+  log "$(_jq '.title') (#$(_jq '.number'))"
+
   if [[ "open" != $(_jq '.state') ]] || [[ "true" == $(_jq '.draft') ]]; then
-    echo Skipping $(_jq '.title') with state $(_jq '.state') and draft $(_jq '.draft')
+    log "  Skipping due to state=$(_jq '.state') and draft=$(_jq '.draft')."
     continue
   fi
 
-  #echo Checking $(_jq '.title') updated last at $(_jq '.head.sha')
+  log "  Last updated at $(_jq '.updated_at')."
 
-  if [[ "fa0e9cb3a20dd8fa6206d72371b3663b9a90e02b" != $(_jq '.head.sha') ]]; then
-    continue
-  fi
-
-  echo Checking $(_jq '.title') updated last at $(_jq '.updated_at')
-
-  ## TODO Need to use to test for how long ago it expired
+  ## Check if the PR is even 3 hours old
   lastUpdatedTime=$(_jq '.updated_at')
   currentTime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
   tooOld=$(node -e "const lutPlus3=new Date('$lastUpdatedTime'); lutPlus3.setHours(lutPlus3.getHours()+3); console.log((new Date('$currentTime') - lutPlus3) > 0)")
-  if [[ tooOld == false ]]; then
-    echo $tooOld
+  if [[ $tooOld == false ]]; then
+    log "  Skipping since it has been updated within the past 3 hours."
     continue
   fi
-  echo Too Old $tooOld
-  break
-
-  # START=$(date -jf '%Y-%m-%dT%H:%M:%SZ' '$(_jq '.updated_at')' +%s)
-  # END=$(date -jf '%Y-%m-%dT%H:%M:%SZ' '$(date -u +"%Y-%m-%dT%H:%M:%SZ")' +%s)
-  # echo $START
-  # echo $END
-  # DIFF=$(( $END - $START ))
-  # echo "Difference is $DIFF seconds"
 
   # Get all statuses of the PR
   #   e.g. https://api.github.com/repos/imodeljs/imodeljs/statuses/c29a168b6a201052098ea6743041868bfbadaa4f
@@ -68,28 +68,30 @@ for row in $(echo "${prs}" | jq -r '.[] | @base64'); do
 
   for status in $(echo "${statuses}" | jq -r '.[] | @base64'); do
     if [[ "success" != $(_jq2 '.state') ]] || [[ "license/cla" == $(_jq2 '.context') ]]; then
+      log "  Skipping $(_jq2 '.context') with state $(_jq2 '.state')."
       continue
     fi
 
-    echo ${status} | base64 --decode
-
-    target_url=$(_jq2 '.target_url')
-
-    # testing
-    if [[ "d13ea94c5dc0f0f35492de5ebe12981eb0e83372" != $(_jq '.head.sha') ]]; then
+    prUpdatedTime=$(_jq2 '.updated_at')
+    tooOld=$(node -e "const lutPlus3=new Date('$prUpdatedTime'); lutPlus3.setHours(lutPlus3.getHours()+3); console.log((new Date('$currentTime') - lutPlus3) > 0)")
+    if [[ $tooOld == false ]]; then
+      log "  Skipping $(_jq2 '.context') it was updated within the past 3 hours."
       continue
     fi
+
+    # echo ${status} | base64 --decode
+
+    echo "  The PR $(_jq '.title') has build $(_jq2 '.context') that is older than 3 hours. Invalidating..."
 
     updateUrl=https://api.github.com/repos/imodeljs/imodeljs/statuses/$(_jq '.head.sha')
-    echo $updateUrl
+    target_url=$(_jq2 '.target_url')
 
-    curl \
-      -X POST \
-      -H "Accept: application/vnd.github.v3+json" \
-      -H "Authorization: token $oauth" \
-      $updateUrl \
-      -d '{"state":"failure","target_url":"'$(_jq2 '.target_url')'","description":"The build hit the 3 hour threshold. Please re-queue the build.","context":"'$(_jq2 '.context')'"}'
+    # curl \
+    #   -X POST \
+    #   -H "Accept: application/vnd.github.v3+json" \
+    #   -H "Authorization: token $oauth" \
+    #   $updateUrl \
+    #   -d '{"state":"failure","target_url":"'$(_jq2 '.target_url')'","description":"The build hit the 3 hour threshold. Please re-queue the build.","context":"'$(_jq2 '.context')'"}'
 
-    break
   done
 done
