3676b9d16fc08f14b035a46a0fbe8910e036007e
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2021-03-24 13:26:06 +0200

    Remove random test data generation

--- ui/components/src/test/tree/controlled/TreeModelSource.test.ts
+++ ui/components/src/test/tree/controlled/TreeModelSource.test.ts
@@ -3,7 +3,6 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
-import * as faker from "faker";
 import sinon from "sinon";
 import * as moq from "typemoq";
 import { BeEvent } from "@bentley/bentleyjs-core";
@@ -11,15 +10,19 @@ import { PropertyRecord } from "@bentley/ui-abstract";
 import { MutableTreeModel, TreeModelNodeInput, VisibleTreeNodes } from "../../../ui-components/tree/controlled/TreeModel";
 import { TreeModelSource } from "../../../ui-components/tree/controlled/TreeModelSource";
 import { ITreeDataProvider, TreeDataChangesListener } from "../../../ui-components/tree/TreeDataProvider";
+import TestUtils from "../../TestUtils";
 
 describe("TreeModelSource", () => {
-
   let modelSource: TreeModelSource;
   const dataProviderMock = moq.Mock.ofType<ITreeDataProvider>();
   const mutableTreeModelMock = moq.Mock.ofType<MutableTreeModel>();
   const visibleNodesMock = moq.Mock.ofType<VisibleTreeNodes>();
 
   let onTreeNodeChanged: BeEvent<TreeDataChangesListener>;
+
+  before(async () => {
+    await TestUtils.initializeUiComponents();
+  });
 
   beforeEach(() => {
     dataProviderMock.reset();
@@ -32,7 +35,6 @@ describe("TreeModelSource", () => {
   });
 
   describe("constructor", () => {
-
     it("listens for onModelChanged events", () => {
       (modelSource as any)._model = mutableTreeModelMock.object;
       mutableTreeModelMock.setup((x) => x.computeVisibleNodes()).returns(() => visibleNodesMock.object).verifiable(moq.Times.exactly(2));
@@ -41,23 +43,20 @@ describe("TreeModelSource", () => {
       modelSource.getVisibleNodes();
       mutableTreeModelMock.verifyAll();
     });
-
   });
 
   describe("modifyModel", () => {
-    let inputNode: TreeModelNodeInput;
-
     beforeEach(() => {
-      inputNode = {
-        id: faker.random.uuid(),
-        isExpanded: faker.random.boolean(),
-        item: { id: faker.random.uuid(), label: PropertyRecord.fromString(faker.random.word(), "label") },
-        label: PropertyRecord.fromString(faker.random.word(), "label"),
-        isLoading: faker.random.boolean(),
-        isSelected: faker.random.boolean(),
+      const root1Input: TreeModelNodeInput = {
+        id: "root1",
+        isExpanded: false,
+        item: { id: "root1_item", label: PropertyRecord.fromString("test_label", "label") },
+        label: PropertyRecord.fromString("test_label", "label"),
+        isLoading: false,
+        isSelected: false,
       };
 
-      modelSource.modifyModel((model) => { model.setChildren(undefined, [inputNode], 0); });
+      modelSource.modifyModel((model) => { model.setChildren(undefined, [root1Input], 0); });
     });
 
     it("does not emit onModelChanged event if model did not change", () => {
@@ -67,20 +66,20 @@ describe("TreeModelSource", () => {
     });
 
     it("emits onModelChanged event with added node id", () => {
-      const newInput: TreeModelNodeInput = {
-        id: faker.random.uuid(),
-        isExpanded: faker.random.boolean(),
-        item: { id: faker.random.uuid(), label: PropertyRecord.fromString(faker.random.word(), "label") },
-        label: PropertyRecord.fromString(faker.random.word(), "label"),
-        isLoading: faker.random.boolean(),
-        isSelected: faker.random.boolean(),
+      const root2Input: TreeModelNodeInput = {
+        id: "root2",
+        isExpanded: false,
+        item: { id: "root2_item", label: PropertyRecord.fromString("test_label", "label") },
+        label: PropertyRecord.fromString("test_label", "label"),
+        isLoading: false,
+        isSelected: false,
       };
       const spy = sinon.spy(modelSource.onModelChanged, "emit");
-      modelSource.modifyModel((model) => { model.setChildren(undefined, [newInput], 1); });
+      modelSource.modifyModel((model) => { model.setChildren(undefined, [root2Input], 1); });
       expect(spy).to.be.called;
       const changes = spy.args[0][0][1];
       expect(changes.addedNodeIds.length).to.be.eq(1);
-      expect(changes.addedNodeIds[0]).to.be.eq(newInput.id);
+      expect(changes.addedNodeIds[0]).to.be.eq(root2Input.id);
     });
 
     it("emits onModelChanged event with removed node id", () => {
@@ -89,19 +88,19 @@ describe("TreeModelSource", () => {
       expect(spy).to.be.called;
       const changes = spy.args[0][0][1];
       expect(changes.removedNodeIds.length).to.be.eq(1);
-      expect(changes.removedNodeIds[0]).to.be.eq(inputNode.id);
+      expect(changes.removedNodeIds[0]).to.be.eq("root1");
     });
 
     it("emits onModelChanged event with modified node id", () => {
       const spy = sinon.spy(modelSource.onModelChanged, "emit");
       modelSource.modifyModel((model) => {
-        const node = model.getNode(inputNode.id);
+        const node = model.getNode("root1");
         node!.isSelected = !node!.isSelected;
       });
       expect(spy).to.be.called;
       const changes = spy.args[0][0][1];
       expect(changes.modifiedNodeIds.length).to.be.eq(1);
-      expect(changes.modifiedNodeIds[0]).to.be.eq(inputNode.id);
+      expect(changes.modifiedNodeIds[0]).to.be.eq("root1");
     });
 
     it("clears model and adds new nodes", () => {
@@ -109,53 +108,49 @@ describe("TreeModelSource", () => {
         model.clearChildren(undefined);
       });
       expect(modelSource.getVisibleNodes().getNumNodes()).to.be.eq(0);
-      const newInput: TreeModelNodeInput = {
-        id: faker.random.uuid(),
-        isExpanded: faker.random.boolean(),
-        item: { id: faker.random.uuid(), label: PropertyRecord.fromString(faker.random.word(), "label") },
-        label: PropertyRecord.fromString(faker.random.word(), "label"),
-        isLoading: faker.random.boolean(),
-        isSelected: faker.random.boolean(),
+      const newRoot1Input: TreeModelNodeInput = {
+        id: "new_root1",
+        isExpanded: false,
+        item: { id: "root1_item", label: PropertyRecord.fromString("test_label", "label") },
+        label: PropertyRecord.fromString("test_label", "label"),
+        isLoading: false,
+        isSelected: false,
       };
       modelSource.modifyModel((model) => {
-        model.setChildren(undefined, [newInput], 0);
+        model.setChildren(undefined, [newRoot1Input], 0);
       });
       expect(modelSource.getVisibleNodes().getNumNodes()).to.be.eq(1);
     });
 
     it("overrides existing children multiple times", () => {
-      const newInput: TreeModelNodeInput = {
-        id: faker.random.uuid(),
-        isExpanded: faker.random.boolean(),
-        item: { id: faker.random.uuid(), label: PropertyRecord.fromString(faker.random.word(), "label") },
-        label: PropertyRecord.fromString(faker.random.word(), "label"),
-        isLoading: faker.random.boolean(),
-        isSelected: faker.random.boolean(),
-      };
+      const newRoot1Input: TreeModelNodeInput = {
+        id: "new_root1",
+        isExpanded: false,
+        item: { id: "root1_item", label: PropertyRecord.fromString("test_label", "label") },
+        label: PropertyRecord.fromString("test_label", "label"),
+        isLoading: false,
+        isSelected: false,
+      };;
       modelSource.modifyModel((model) => {
         model.setNumChildren(undefined, 1);
-        model.setChildren(undefined, [newInput], 0);
+        model.setChildren(undefined, [newRoot1Input], 0);
       });
       modelSource.modifyModel((model) => {
         model.setNumChildren(undefined, 1);
-        model.setChildren(undefined, [inputNode], 0);
+        model.setChildren(undefined, [{ ...newRoot1Input, id: "root1" }], 0);
       });
     });
-
   });
 
   describe("getModel", () => {
-
     it("returns model", () => {
       (modelSource as any)._model = mutableTreeModelMock.object;
       const model = modelSource.getModel();
       expect(model).to.be.eq(mutableTreeModelMock.object);
     });
-
   });
 
   describe("getVisibleNodes", () => {
-
     beforeEach(() => {
       (modelSource as any)._model = mutableTreeModelMock.object;
     });
@@ -172,7 +167,5 @@ describe("TreeModelSource", () => {
       modelSource.getVisibleNodes();
       mutableTreeModelMock.verifyAll();
     });
-
   });
-
 });
