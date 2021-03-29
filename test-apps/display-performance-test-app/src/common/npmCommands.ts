6efe86fdaa8dfb6bc3e6cb00d1b9189fe9d94026
Heather Neely <18196761+hnn0003@users.noreply.github.com>
2021-03-29 10:03:06 -0500

    Add support for Safari & Mac testing to dpta #584788

--- test-apps/display-performance-test-app/src/common/npmCommands.ts
+++ test-apps/display-performance-test-app/src/common/npmCommands.ts
@@ -18,7 +18,18 @@ for (let i = 2; i < process.argv.length; i++) {
 }
 execSync(`npm run start:web ${args}`, { stdio: [0, 1, 2] });
 
-if (browser === "edge")
-  execSync("taskkill /f /im MicrosoftEdge.exe /t >nul");
-else if (browser === "firefox")
-  execSync("taskkill /f /im firefox.exe /t >nul");
+switch (browser) {
+  case "edge":
+    execSync("taskkill /f /im msedge.exe /t >nul");
+    break;
+  case "safari":
+    execSync("killall Safari");
+    break;
+  case "firefox":
+    if (process.platform === "darwin") { // Ie, if running on Mac
+      execSync("killall firefox");
+    } else {
+      execSync("taskkill /f /im firefox.exe /t >nul");
+    }
+    break;
+}
