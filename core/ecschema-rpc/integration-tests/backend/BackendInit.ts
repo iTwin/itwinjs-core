e7ef656944d629a81208fc1f905798a5cfc47339
Hoang Nam Le <50554904+hl662@users.noreply.github.com>
2021-03-30 11:45:41 -0400

    replaced depracated use of iModelJsConfig to loadEnv

--- core/ecschema-rpc/integration-tests/backend/BackendInit.ts
+++ core/ecschema-rpc/integration-tests/backend/BackendInit.ts
@@ -4,15 +4,16 @@
 *--------------------------------------------------------------------------------------------*/
 // Sets up certa to allow a method on the frontend to get an access token
 import "@bentley/oidc-signin-tool/lib/certa/certaBackend";
+import * as path from "path";
 import { Config } from "@bentley/bentleyjs-core";
-import { IModelJsConfig } from "@bentley/config-loader/lib/IModelJsConfig";
+import { loadEnv } from "@bentley/config-loader";
 import { Settings } from "../common/Settings";
 import { exposeBackendCallbacks } from "../common/SideChannels";
 
 // tslint:disable:no-console
 
 module.exports = (async () => {
-  IModelJsConfig.init(true, true, Config.App);
+  loadEnv(path.join(__dirname, "..", "..", ".env"));
 
   // Need to create a new one on the backend to properly setup dotenv
   const settings = new Settings(process.env);
