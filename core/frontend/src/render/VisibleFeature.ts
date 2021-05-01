9ca2212d1355b8095e9ca23938856fb8388f2752
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-05-01 08:11:16 -0400

    Invalidate features after callback.

--- core/frontend/src/render/VisibleFeature.ts
+++ core/frontend/src/render/VisibleFeature.ts
@@ -34,12 +34,41 @@ export interface QueryTileFeaturesOptions {
 export type QueryVisibleFeaturesOptions = QueryScreenFeaturesOptions | QueryTileFeaturesOptions;
 export type QueryVisibleFeaturesCallback = (features: Iterable<VisibleFeature>) => void;
 
+class ExpiringIterable implements Iterable<VisibleFeature> {
+  private _features: Iterable<VisibleFeature>;
+  private _disposed = false;
+
+  public constructor(features: Iterable<VisibleFeature>) {
+    this._features = features;
+  }
+
+  public dispose(): void {
+    this._disposed = true;
+    this._features = [];
+  }
+
+  public [Symbol.iterator](): Iterator<VisibleFeature> {
+    assert(!this._disposed, "The iterable supplied to QueryVisibleFeaturesCallback is valid only for the duration of the callback.");
+    return this._features[Symbol.iterator]();
+  }
+}
+
 function queryVisibleScreenFeatures(_viewport: Viewport, callback: QueryVisibleFeaturesCallback, _options: QueryScreenFeaturesOptions): void {
-  callback([]);
+  const iterable = new ExpiringIterable([]);
+  try {
+    callback(iterable);
+  } catch (_) {
+    iterable.dispose();
+  }
 }
 
 function queryVisibleTileFeatures(_viewport: Viewport, callback: QueryVisibleFeaturesCallback, _options: QueryTileFeaturesOptions): void {
-  callback([]);
+  const iterable = new ExpiringIterable([]);
+  try {
+    callback(iterable);
+  } catch (_) {
+    iterable.dispose();
+  }
 }
 
 export function queryVisibleFeatures(viewport: Viewport, options: QueryVisibleFeaturesOptions, callback: QueryVisibleFeaturesCallback): void {
