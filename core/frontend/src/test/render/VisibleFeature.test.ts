17314a903253df5ae9b6caf485d2a900b278b3c0
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-05-02 07:19:31 -0400

    WIP tests.

---
+++ core/frontend/src/test/render/VisibleFeature.test.ts
@@ -0,0 +1,103 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+import { Point3d, Vector3d } from "@bentley/geometry-core";
+import { IModelApp } from "../../IModelApp";
+import { IModelConnection } from "../../IModelConnection";
+import { ViewRect } from "../../ViewRect";
+import { QueryVisibleFeaturesOptions } from "../../render/VisibleFeature";
+import { SpatialViewState } from "../../SpatialViewState";
+import { ScreenViewport } from "../../Viewport";
+import { TiledGraphicsProvider } from "../../tile/TiledGraphicsProvider";
+import { createBlankConnection } from "../createBlankConnection";
+
+/*
+interface ViewTilerOptions {
+  readonly viewport: ScreenViewport;
+  readonly treeSize: XAndY;
+  readonly iModel?: IModelConnection;
+  readonly numElementsPerTile: number;
+}
+*/
+
+/** Produces a view-space tile tree that sub-divides a view rect into a regular grid of differently-colored rectangles.
+ * A tile's color is determined by its position along the X and Y axes: red increases from 0 to 1 along X and green from 0 to 1 along Y.
+ * Each tile is sub-divided into the same number of elements, where each element is drawn as a vertical slice of the tile.
+ */
+class ViewTiler /* implements TiledGraphicsProvider */ {
+  /*
+  public readonly iModel: IModelConnection;
+  public readonly modelId: Id64String;
+
+  public constructor(options: ViewTilerOptions) {
+    this.iModel = options.iModel ?? options.viewport.iModel;
+    this.modelId = iModel.transientIds.next();
+  }
+  */
+}
+
+describe("Visible feature query", () => {
+  let imodel: IModelConnection;
+  let viewport: ScreenViewport | undefined;
+
+  before(async () => {
+    await IModelApp.startup();
+    imodel = createBlankConnection("visible-features");
+  });
+
+  afterEach(() => {
+    if (viewport) {
+      viewport.dispose();
+      viewport = undefined;
+    }
+  });
+
+  after(async () => {
+    await imodel.close();
+    await IModelApp.shutdown();
+  });
+
+  function testViewport(width: number, height: number, devicePixelRatio: number | undefined, callback: (vp: ScreenViewport) => void): void {
+    const div = document.createElement("div");
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.position = "absolute";
+    div.style.top = div.style.left = "0px";
+    document.body.appendChild(div);
+
+    const view = SpatialViewState.createBlank(imodel, new Point3d(), new Vector3d(1, 1, 1));
+    view.viewFlags.acsTriad = view.viewFlags.grid = false;
+
+    const vp = ScreenViewport.create(div, view);
+    IModelApp.viewManager.addViewport(vp);
+    expect(vp.target.debugControl).not.to.be.undefined;
+    vp.target.debugControl!.devicePixelRatioOverride = devicePixelRatio ?? 1;
+
+    try {
+      callback(vp);
+    } finally {
+      vp.dispose();
+      document.body.removeChild(div);
+    }
+  }
+
+  it("is usable only within callback", () => {
+    testViewport(20, 20, 1, (vp) => {
+      const isDisposed = (features: any) => true === features._disposed;
+      function test(options: QueryVisibleFeaturesOptions): void {
+        let features;
+        vp.queryVisibleFeatures(options, (f) => {
+          features = f;
+          expect(isDisposed(features)).to.be.false;
+        });
+
+        expect(isDisposed(features)).to.be.true;
+      }
+
+      test({ source: "tiles" });
+      test({ source: "screen" });
+    });
+  });
+});
