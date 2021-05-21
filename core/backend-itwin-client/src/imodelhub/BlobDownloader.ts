a466f228e1a597b84c7602d1f686a02d178b817b
Affan Khan <khanaffan@users.noreply.github.com>
2021-05-21 15:49:53 -0400

    docs

--- core/backend-itwin-client/src/imodelhub/BlobDownloader.ts
+++ core/backend-itwin-client/src/imodelhub/BlobDownloader.ts
@@ -2,6 +2,10 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module iModelHub
+ */
+
 import got from "got";
 import * as fs from "fs";
 import * as stream from "stream";
