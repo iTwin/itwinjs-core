5c766144af72ff567f1b25b6dbd288b4bb53cae8
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-06-04 13:33:40 -0400

    add restartTxnSession to nativeDb

--- core/backend/src/TxnManager.ts
+++ core/backend/src/TxnManager.ts
@@ -269,9 +269,9 @@ export class TxnManager {
   public readonly onAfterUndoRedo = new BeEvent<(isUndo: boolean) => void>();
 
   /** Determine whether undo is possible, optionally permitting undoing txns from previous sessions.
-   * @param allowCrossSessions if true, allow undoing from previous sessions.
+   * @@deprecated - use [[isUndoPossible]]
    */
-  public checkUndoPossible(allowCrossSessions?: boolean) { return this._nativeDb.isUndoPossible(allowCrossSessions); }
+  public checkUndoPossible() { return this._nativeDb.isUndoPossible(); }
 
   /** Determine if there are currently any reversible (undoable) changes from this editing session. */
   public get isUndoPossible(): boolean { return this._nativeDb.isUndoPossible(); }
@@ -281,9 +281,8 @@ export class TxnManager {
 
   /** Get the description of the operation that would be reversed by calling reverseTxns(1).
    * This is useful for showing the operation that would be undone, for example in a menu.
-   * @param allowCrossSessions if true, allow undo from previous sessions.
    */
-  public getUndoString(allowCrossSessions?: boolean): string { return this._nativeDb.getUndoString(allowCrossSessions); }
+  public getUndoString(): string { return this._nativeDb.getUndoString(); }
 
   /** Get a description of the operation that would be reinstated by calling reinstateTxn.
    * This is useful for showing the operation that would be redone, in a pull-down menu for example.
@@ -307,14 +306,13 @@ export class TxnManager {
   /** Reverse (undo) the most recent operation(s) to this IModelDb.
    * @param numOperations the number of operations to reverse. If this is greater than 1, the entire set of operations will
    *  be reinstated together when/if ReinstateTxn is called.
-   * @param allowCrossSessions if true, allow undo from previous sessions.
    * @note If there are any outstanding uncommitted changes, they are reversed.
    * @note The term "operation" is used rather than Txn, since multiple Txns can be grouped together via [[beginMultiTxnOperation]]. So,
    * even if numOperations is 1, multiple Txns may be reversed if they were grouped together when they were made.
    * @note If numOperations is too large only the operations are reversible are reversed.
    */
-  public reverseTxns(numOperations: number, allowCrossSessions?: boolean): IModelStatus {
-    return this._iModel.reverseTxns(numOperations, allowCrossSessions);
+  public reverseTxns(numOperations: number): IModelStatus {
+    return this._iModel.reverseTxns(numOperations);
   }
 
   /** Reverse the most recent operation. */
@@ -325,18 +323,16 @@ export class TxnManager {
 
   /** Reverse all changes back to a previously saved TxnId.
    * @param txnId a TxnId obtained from a previous call to GetCurrentTxnId.
-   * @param allowCrossSessions if true, allow undo from previous sessions.
    * @returns Success if the transactions were reversed, error status otherwise.
    * @see  [[getCurrentTxnId]] [[cancelTo]]
    */
-  public reverseTo(txnId: TxnIdString, allowCrossSessions?: boolean): IModelStatus { return this._nativeDb.reverseTo(txnId, allowCrossSessions); }
+  public reverseTo(txnId: TxnIdString): IModelStatus { return this._nativeDb.reverseTo(txnId); }
 
   /** Reverse and then cancel (make non-reinstatable) all changes back to a previous TxnId.
    * @param txnId a TxnId obtained from a previous call to [[getCurrentTxnId]]
-   * @param allowCrossSessions if true, allow undo from previous sessions.
    * @returns Success if the transactions were reversed and cleared, error status otherwise.
    */
-  public cancelTo(txnId: TxnIdString, allowCrossSessions?: boolean): IModelStatus { return this._nativeDb.cancelTo(txnId, allowCrossSessions); }
+  public cancelTo(txnId: TxnIdString): IModelStatus { return this._nativeDb.cancelTo(txnId); }
 
   /** Reinstate the most recently reversed transaction. Since at any time multiple transactions can be reversed, it
    * may take multiple calls to this method to reinstate all reversed operations.
@@ -346,9 +342,8 @@ export class TxnManager {
   public reinstateTxn(): IModelStatus { return this._iModel.reinstateTxn(); }
 
   /** Get the Id of the first transaction, if any.
-   * @param allowCrossSessions if true, allow undo from previous sessions.
    */
-  public queryFirstTxnId(allowCrossSessions?: boolean): TxnIdString { return this._nativeDb.queryFirstTxnId(allowCrossSessions); }
+  public queryFirstTxnId(): TxnIdString { return this._nativeDb.queryFirstTxnId(); }
 
   /** Get the successor of the specified TxnId */
   public queryNextTxnId(txnId: TxnIdString): TxnIdString { return this._nativeDb.queryNextTxnId(txnId); }
