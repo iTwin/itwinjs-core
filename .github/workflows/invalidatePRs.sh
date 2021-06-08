b56f0d54b544b9c1ac0c24ea4324838495711f76
saemjeon <38971167+saemjeon@users.noreply.github.com>
2021-06-08 15:29:58 -0400

    Add context

--- .github/workflows/invalidatePRs.sh
+++ .github/workflows/invalidatePRs.sh
@@ -151,7 +151,7 @@ for pr in $(echo "${prs}" | jq -r '.[] | @base64'); do
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: token $oauth" \
       $updateUrl \
-      -d '{"state": "failure", "target_url": "'${target_url}'", "description": "The build hit the 3 hour threshold. Please re-queue the build."}'
+      -d '{"state": "failure", "target_url": "'${target_url}'", "context": "'${context}'", "description": "The build hit the 3 hour threshold. Please re-queue the build."}'
 
     break
   done
