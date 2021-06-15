ba41cc15711801429adecb94c5c5e9e80a16668c
bsteinbk <65047615+bsteinbk@users.noreply.github.com>
2021-06-15 15:33:20 -0400

    Ensure redux stays in sync with FrameworkVersion context, init configuableUiManager when UiFramework is, and call initializeConnectionEvents when setting connection in redux

--- test-apps/ui-test-app/src/frontend/index.tsx
+++ test-apps/ui-test-app/src/frontend/index.tsx
@@ -337,8 +337,6 @@ export class SampleAppIModelApp {
     });
     Logger.logInfo(SampleAppIModelApp.loggerCategory(this), `openViews: ${viewIdsParam}`);
 
-    SyncUiEventDispatcher.initializeConnectionEvents(iModelConnection);
-
     // store the IModelConnection in the sample app store - this may trigger redux connected components
     UiFramework.setIModelConnection(iModelConnection, true);
     const viewStates: ViewState[] = [];
@@ -425,8 +423,6 @@ export class SampleAppIModelApp {
       }
 
       SampleAppIModelApp.setIsIModelLocal(false, true);
-
-      SyncUiEventDispatcher.initializeConnectionEvents(iModelConnection);
 
       // store the IModelConnection in the sample app store
       UiFramework.setIModelConnection(iModelConnection, true);
