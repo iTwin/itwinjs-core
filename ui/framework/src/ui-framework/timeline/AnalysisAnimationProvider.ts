e9f633e2b3ca57ca077cdde4a901b540d578e3e1
NancyMcCallB <45079789+NancyMcCallB@users.noreply.github.com>
2021-06-16 09:11:56 -0500

    Update component doc and NextVersion

--- ui/framework/src/ui-framework/timeline/AnalysisAnimationProvider.ts
+++ ui/framework/src/ui-framework/timeline/AnalysisAnimationProvider.ts
@@ -9,7 +9,7 @@
 import { ScreenViewport, ViewState } from "@bentley/imodeljs-frontend";
 import { BaseTimelineDataProvider, PlaybackSettings } from "@bentley/ui-components";
 
-/**  Analysis Timeline Data Provider - handles View that define 'analysisStyle' data.
+/**  Analysis Timeline Data Provider - Allows a TimelineComponent to animate the AnalysisStyle information stored in a ViewState.
  * @public
  */
 export class AnalysisAnimationTimelineDataProvider extends BaseTimelineDataProvider {
