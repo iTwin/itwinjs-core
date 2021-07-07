a8a0205185940c8561115ba4c5a070eaed83627a
hnn0003 <18196761+hnn0003@users.noreply.github.com>
2021-07-07 16:59:58 -0500

    Get rid of unnecessary firefox kill command

--- test-apps/display-performance-test-app/src/common/npmCommands.ts
+++ test-apps/display-performance-test-app/src/common/npmCommands.ts
@@ -9,17 +9,8 @@ import * as child_process from "child_process";
 
 const execSync = child_process.execSync;
 let args = "";
-let browser = "";
 for (let i = 2; i < process.argv.length; i++) {
   const curArg = process.argv[i];
   args += `${curArg} `;
-  if (curArg === "chrome" || curArg === "edge" || curArg === "firefox" || curArg === "safari")
-    browser = curArg;
 }
 execSync(`npm run start:web ${args}`, { stdio: [0, 1, 2] });
-
-switch (browser) {
-  case "firefox":
-    execSync("taskkill /f /im firefox.exe /t >nul");
-    break;
-}
