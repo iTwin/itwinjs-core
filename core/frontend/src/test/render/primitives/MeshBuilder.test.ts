e6a10558d43d06c06acff4097818f15b8012ad69
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2021-07-13 11:57:40 -0400

    fix merge mistake where an import was removed

--- core/frontend/src/test/render/primitives/MeshBuilder.test.ts
+++ core/frontend/src/test/render/primitives/MeshBuilder.test.ts
@@ -18,6 +18,7 @@ import { StrokesPrimitiveList, StrokesPrimitivePointLists } from "../../../rende
 import { ToleranceRatio, Triangle } from "../../../render/primitives/Primitives";
 import { MeshParams } from "../../../render/primitives/VertexTable";
 import { MeshBuilder, MeshEdgeCreationOptions } from "../../../render/primitives/mesh/MeshBuilder";
+import { openBlankViewport } from "../../openBlankViewport";
 
 class FakeDisplayParams extends DisplayParams {
   public constructor() {
