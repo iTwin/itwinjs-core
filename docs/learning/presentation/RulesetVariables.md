ff9661f8ff4309d594d76cf4720304772eab9fdd
Grigas <35135765+grigasp@users.noreply.github.com>
2021-07-20 14:32:02 +0300

    Improvements for ruleset variables documentation

--- docs/learning/presentation/RulesetVariables.md
+++ docs/learning/presentation/RulesetVariables.md
@@ -1,86 +1,97 @@
 # Ruleset Variables
 
-> *User Settings* is a deprecated name of the *Ruleset Variables* concept.
+> Ruleset Variables is a new name for now deprecated *User Settings* concept.
 
-Presentation rulesets have a concept called *ruleset variables* whose purpose is
-to allow modifying ruleset behavior during the session without having to change
-the ruleset itself. The values of ruleset variables can be accessed using
-[ECExpressions](./ECExpressions.md#ruleset-variables-user-settings).
+Presentation rulesets have a concept called *ruleset variables* whose purpose is to allow modifying ruleset behavior during the session without having to change
+the ruleset itself. The values of ruleset variables can be accessed using [ECExpressions](./ECExpressions.md#ruleset-variables-user-settings) in:
 
-## User-Controllable Variables
+- Rule conditions ([example](#using-variables-in-rule-condition)).
+- Instance filters ([example](#using-variables-in-instance-filter)).
+- Some ECExpressions-based values, like [LabelOverride.label]($presentation-common) ([example](#using-variables-in-ecexpression-based-value)).
 
-> This feature is not fully supported yet - there is no UI component for
-> editing user-defined ruleset variables.
+The advantages of using ruleset variables over values hardcoded into the ruleset are:
 
-There can be either internal variables or user-controllable ones. Internal
-variables are not visible to end users and can only be changed using API calls.
-User-controllable variables are defined in the ruleset with pre-defined default
-values and may be modified by end users through some UI component.
+- When used for creating a hierarchy, changing a ruleset variable causes only affected parts of the hierarchy to be re-created. Changing a rule in a ruleset causes the whole hierarchy to be re-created.
+- It's much more convenient and effective to pass a large number of IDs through a ruleset variable than it is to hardcode them into a ruleset. Ruleset variables are sent to the backend separately from the ruleset and that allows us to compress them then needed.
 
-## Examples
+## Using Variables in Rule Condition
 
-Specifying user-controllable ruleset variable in a ruleset:
+Using a ruleset variable in rule condition allows enabling and disabling specific rules based on something that can't be determined at the point of creating the ruleset, e.g. a user action.
 
-```JSON
-{
-  "id": "ruleset_id",
-  "rules": [],
-  "vars": [{
-    "label": "Variables group label",
-    "vars": [{
-      "id": "boolean_var_id",
-      "type": "ShowHide",
-      "label": "Show something?"
-    }, {
-      "id": "string_var_id",
-      "type": "StringValue",
-      "label": "Filtering value"
-    }, {
-      "id": "int_var_id",
-      "type": "IntValue",
-      "label": "Minimum value"
-    }],
-  }]
-}
+The below example creates either a models' or an elements' hierarchy based on the value of ruleset variable.
+
+By default the ruleset value is not set, which means neither of the two root node rules pass the condition. The result is empty hierarchy:
+
+```ts
+[[include:RulesetVariables.InRuleCondition.Ruleset]]
 ```
 
-Using ruleset variable in a rule condition:
+![Empty Hierarchy](./media/ruleset-variables/empty-hierarchy.png)
 
-```JSON
-{
-  "id": "ruleset_id",
-  "rules": [{
-    "ruleType": "LabelOverride",
-    "condition": "GetVariableBoolValue(\"should_prefix_hidden_items\") ANDALSO this.IsHidden",
-    "label": "\"Hidden: \" & this.MyLabel",
-  }]
-}
+Setting the ruleset value to `models` creates a models' hierarchy:
+
+```ts
+[[include:RulesetVariables.InRuleCondition.SetToModels]]
 ```
 
-Using ruleset variable in a customization rule value:
+![Models' Hierarchy](./media/ruleset-variables/models-hierarchy.png)
 
-```JSON
-{
-  "id": "ruleset_id",
-  "rules": [{
-    "ruleType": "LabelOverride",
-    "label": "GetVariableStringValue(\"custom_prefix\") & this.MyLabel",
-  }]
-}
+Setting the ruleset value to `elements` creates an elements' hierarchy:
+
+```ts
+[[include:RulesetVariables.InRuleCondition.SetToElements]]
 ```
 
-Using ruleset variable in specification instance filter
+![Elements' Hierarchy](./media/ruleset-variables/elements-hierarchy.png)
 
-```JSON
-{
-  "id": "ruleset_id",
-  "rules": [{
-    "ruleType": "Content",
-    "specifications": [{
-      "specType": "ContentInstancesOfSpecificClasses",
-      "classes": { "schemaName": "MySchema", "classNames": ["MyClass"] },
-      "instanceFilter": "this.MyValue >= GetVariableIntValue(\"minimum_value\")"
-    }]
-  }]
-}
+## Using Variables in Instance Filter
+
+Using a ruleset variable in instance filter allows filtering instances at query time. This is useful when the filtering criteria depends on user actions.
+
+The below example shows a hierarchy of elements grouped by class. When the `element-ids` variable is not set, the whole hierarchy is displayed. When the value is set to specific element IDs, the hierarchy is created only for those elements.
+
+By default the ruleset value is not set - a hierarchy for all elements is created:
+
+```ts
+[[include:RulesetVariables.InInstanceFilter.Ruleset]]
 ```
+
+![Empty Hierarchy](./media/ruleset-variables/elements-hierarchy.png)
+
+Setting the ruleset value to specific IDs creates the hierarchy only for specific elements:
+
+```ts
+[[include:RulesetVariables.InInstanceFilter.SetIds]]
+```
+
+![Filtered Elements' Hierarchy](./media/ruleset-variables/filtered-elements-hierarchy.png)
+
+Clearing the ruleset value brings us back to initial, unfiltered, view:
+
+```ts
+[[include:RulesetVariables.InInstanceFilter.Unset]]
+```
+
+![Elements' Hierarchy](./media/ruleset-variables/elements-hierarchy.png)
+
+## Using Variables in ECExpression-based Value
+
+Using a ruleset variable in ECExpression-based value makes the value customizable by users.
+
+The below example shows a hierarchy that loads *Spatial View Definition* elements as root nodes. By default, when `prefix` variable is not set, node labels are created from `CodeValue` property of the element. When the `prefix` variable is set, it's value is used as a prefix for node labels.
+
+By default the ruleset value is not set - *CodeValue* property value is used as the label:
+
+```ts
+[[include:RulesetVariables.InCustomizationRuleValueExpression.Ruleset]]
+```
+
+![Spatial View Definitions](./media/ruleset-variables/spatial-view-definitions.png)
+
+Setting the ruleset variable value makes it appear in front of every node's label:
+
+```ts
+[[include:RulesetVariables.InCustomizationRuleValueExpression.SetValue]]
+```
+
+![Spatial View Definitions - Prefixed Labels](./media/ruleset-variables/spatial-view-definitions-prefixed.png)
---
+++ full-stack-tests/presentation/src/learning-snippets/RulesetVariables.test.ts
@@ -0,0 +1,306 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+import { IModelConnection, SnapshotConnection } from "@bentley/imodeljs-frontend";
+import { ChildNodeSpecificationTypes, Ruleset, RuleTypes } from "@bentley/presentation-common";
+import { Presentation } from "@bentley/presentation-frontend";
+import { initialize, terminate } from "../IntegrationTests";
+
+describe.only("Learning Snippets", () => {
+
+  describe("Ruleset Variables", () => {
+
+    let imodel: IModelConnection;
+
+    beforeEach(async () => {
+      await initialize();
+      imodel = await SnapshotConnection.openFile("assets/datasets/Properties_60InstancesWithUrl2.ibim");
+    });
+
+    afterEach(async () => {
+      await imodel.close();
+      await terminate();
+    });
+
+    it("uses ruleset variable in rule condition", async () => {
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InRuleCondition.Ruleset
+      // The ruleset has two root node rules - one for models and one for elements. The one actually used
+      // depends on a ruleset variable value, which can be changed without modifying the ruleset itself.
+      const variableId = "root-nodes-type";
+      const ruleset: Ruleset = {
+        id: "test",
+        rules: [{
+          ruleType: RuleTypes.RootNodes,
+          condition: `GetVariableStringValue("${variableId}") = "models"`,
+          specifications: [{
+            specType: ChildNodeSpecificationTypes.InstanceNodesOfSpecificClasses,
+            classes: { schemaName: "BisCore", classNames: ["Model"] },
+            arePolymorphic: true,
+          }],
+        }, {
+          ruleType: RuleTypes.RootNodes,
+          condition: `GetVariableStringValue("${variableId}") = "elements"`,
+          specifications: [{
+            specType: ChildNodeSpecificationTypes.InstanceNodesOfSpecificClasses,
+            classes: { schemaName: "BisCore", classNames: ["Element"] },
+            arePolymorphic: true,
+          }],
+        }],
+      };
+      // __PUBLISH_EXTRACT_END__
+
+      // No variable set - the request should return 0 nodes
+      const nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes.length).to.eq(0);
+
+      // Set variable to "models" and ensure we get model grouping nodes
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InRuleCondition.SetToModels
+      await Presentation.presentation.vars(ruleset.id).setString(variableId, "models");
+      // __PUBLISH_EXTRACT_END__
+      const modelNodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(modelNodes).to.containSubset([{
+        label: { displayValue: "Definition Model" },
+      }, {
+        label: { displayValue: "Dictionary Model" },
+      }, {
+        label: { displayValue: "Document List" },
+      }, {
+        label: { displayValue: "Group Model" },
+      }, {
+        label: { displayValue: "Link Model" },
+      }, {
+        label: { displayValue: "Physical Model" },
+      }, {
+        label: { displayValue: "Repository Model" },
+      }]);
+
+      // Set variable to "elements" and ensure we get element grouping nodes
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InRuleCondition.SetToElements
+      await Presentation.presentation.vars(ruleset.id).setString(variableId, "elements");
+      // __PUBLISH_EXTRACT_END__
+      const elementNodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(elementNodes).to.containSubset([{
+        label: { displayValue: "3D Display Style" },
+      }, {
+        label: { displayValue: "Category Selector" },
+      }, {
+        label: { displayValue: "Definition Partition" },
+      }, {
+        label: { displayValue: "Document Partition" },
+      }, {
+        label: { displayValue: "Drawing Category" },
+      }, {
+        label: { displayValue: "Group Information Partition" },
+      }, {
+        label: { displayValue: "Line Style" },
+      }, {
+        label: { displayValue: "Link Partition" },
+      }, {
+        label: { displayValue: "Model Selector" },
+      }, {
+        label: { displayValue: "Physical Object" },
+      }, {
+        label: { displayValue: "Physical Partition" },
+      }, {
+        label: { displayValue: "Repository Link" },
+      }, {
+        label: { displayValue: "Spatial Auxiliary Coordinate System" },
+      }, {
+        label: { displayValue: "Spatial Category" },
+      }, {
+        label: { displayValue: "Spatial View Definition" },
+      }, {
+        label: { displayValue: "Sub-Category" },
+      }, {
+        label: { displayValue: "Subject" },
+      }, {
+        label: { displayValue: "TestClass" },
+      }]);
+    });
+
+    it("uses ruleset variable in instance filter", async () => {
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InInstanceFilter.Ruleset
+      // The ruleset has a root node rule which loads all bis.Element instances, optionally filtered
+      // by ECInstanceId.
+      const variableId = "element-ids";
+      const ruleset: Ruleset = {
+        id: "test",
+        rules: [{
+          ruleType: RuleTypes.RootNodes,
+          specifications: [{
+            specType: ChildNodeSpecificationTypes.InstanceNodesOfSpecificClasses,
+            classes: { schemaName: "BisCore", classNames: ["Element"] },
+            arePolymorphic: true,
+            instanceFilter: `NOT HasVariable("${variableId}") OR GetVariableIntValues("${variableId}").AnyMatch(id => id = this.ECInstanceId)`,
+          }],
+        }],
+      };
+      // __PUBLISH_EXTRACT_END__
+
+      // No variable set - the request should return grouping nodes of all elements
+      let nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "3D Display Style" },
+      }, {
+        label: { displayValue: "Category Selector" },
+      }, {
+        label: { displayValue: "Definition Partition" },
+      }, {
+        label: { displayValue: "Document Partition" },
+      }, {
+        label: { displayValue: "Drawing Category" },
+      }, {
+        label: { displayValue: "Group Information Partition" },
+      }, {
+        label: { displayValue: "Line Style" },
+      }, {
+        label: { displayValue: "Link Partition" },
+      }, {
+        label: { displayValue: "Model Selector" },
+      }, {
+        label: { displayValue: "Physical Object" },
+      }, {
+        label: { displayValue: "Physical Partition" },
+      }, {
+        label: { displayValue: "Repository Link" },
+      }, {
+        label: { displayValue: "Spatial Auxiliary Coordinate System" },
+      }, {
+        label: { displayValue: "Spatial Category" },
+      }, {
+        label: { displayValue: "Spatial View Definition" },
+      }, {
+        label: { displayValue: "Sub-Category" },
+      }, {
+        label: { displayValue: "Subject" },
+      }, {
+        label: { displayValue: "TestClass" },
+      }]);
+
+      // Set the value to several element IDs and ensure we get their class grouping nodes
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InInstanceFilter.SetIds
+      await Presentation.presentation.vars(ruleset.id).setId64s(variableId, ["0x1", "0x74", "0x40"]);
+      // __PUBLISH_EXTRACT_END__
+      nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "Physical Object" },
+      }, {
+        label: { displayValue: "Subject" },
+      }, {
+        label: { displayValue: "TestClass" },
+      }]);
+
+      // Set the value to different element IDs and ensure we get their class grouping nodes
+      await Presentation.presentation.vars(ruleset.id).setId64s(variableId, ["0x17", "0x16"]);
+      nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "Definition Partition" },
+      }, {
+        label: { displayValue: "Spatial Category" },
+      }]);
+
+      // Finally, unsetting the value should get us the initial view
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InInstanceFilter.Unset
+      await Presentation.presentation.vars(ruleset.id).unset(variableId);
+      // __PUBLISH_EXTRACT_END__
+      nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "3D Display Style" },
+      }, {
+        label: { displayValue: "Category Selector" },
+      }, {
+        label: { displayValue: "Definition Partition" },
+      }, {
+        label: { displayValue: "Document Partition" },
+      }, {
+        label: { displayValue: "Drawing Category" },
+      }, {
+        label: { displayValue: "Group Information Partition" },
+      }, {
+        label: { displayValue: "Line Style" },
+      }, {
+        label: { displayValue: "Link Partition" },
+      }, {
+        label: { displayValue: "Model Selector" },
+      }, {
+        label: { displayValue: "Physical Object" },
+      }, {
+        label: { displayValue: "Physical Partition" },
+      }, {
+        label: { displayValue: "Repository Link" },
+      }, {
+        label: { displayValue: "Spatial Auxiliary Coordinate System" },
+      }, {
+        label: { displayValue: "Spatial Category" },
+      }, {
+        label: { displayValue: "Spatial View Definition" },
+      }, {
+        label: { displayValue: "Sub-Category" },
+      }, {
+        label: { displayValue: "Subject" },
+      }, {
+        label: { displayValue: "TestClass" },
+      }]);
+    });
+
+    it("uses ruleset variable in customization rule value expression", async () => {
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InCustomizationRuleValueExpression.Ruleset
+      // The ruleset has a root node rule which loads all bis.SpatialViewDefinition instances. There's
+      // also a label customization rule which optionally prefixes node labels with a ruleset variable value and
+      // an instance label override rule to clear default BIS label override rules.
+      const variableId = "prefix";
+      const ruleset: Ruleset = {
+        id: "test",
+        rules: [{
+          ruleType: RuleTypes.RootNodes,
+          specifications: [{
+            specType: ChildNodeSpecificationTypes.InstanceNodesOfSpecificClasses,
+            classes: { schemaName: "BisCore", classNames: ["SpatialViewDefinition"] },
+            arePolymorphic: true,
+            groupByClass: false,
+            groupByLabel: false,
+          }],
+        }, {
+          ruleType: RuleTypes.LabelOverride,
+          label: `IIF(HasVariable("${variableId}"), GetVariableStringValue("${variableId}") & " " & this.CodeValue, this.CodeValue)`,
+        }, {
+          ruleType: RuleTypes.InstanceLabelOverride,
+          class: { schemaName: "BisCore", className: "SpatialViewDefinition" },
+          values: [],
+        }],
+      };
+      // __PUBLISH_EXTRACT_END__
+
+      // No variable set - the request should return nodes without any prefix
+      let nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "Default - View 1" },
+      }, {
+        label: { displayValue: "Default - View 2" },
+      }, {
+        label: { displayValue: "Default - View 3" },
+      }, {
+        label: { displayValue: "Default - View 4" },
+      }]);
+
+      // Set the prefix to some value and confirm node labels get prefixed
+      // __PUBLISH_EXTRACT_START__ RulesetVariables.InCustomizationRuleValueExpression.SetValue
+      await Presentation.presentation.vars(ruleset.id).setString(variableId, "test");
+      // __PUBLISH_EXTRACT_END__
+      nodes = await Presentation.presentation.getNodes({ imodel, rulesetOrId: ruleset });
+      expect(nodes).to.containSubset([{
+        label: { displayValue: "test Default - View 1" },
+      }, {
+        label: { displayValue: "test Default - View 2" },
+      }, {
+        label: { displayValue: "test Default - View 3" },
+      }, {
+        label: { displayValue: "test Default - View 4" },
+      }]);
+    });
+
+  });
+
+});
