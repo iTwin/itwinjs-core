b66926733c4b9cdb8863af0e3d84bb562e9b6f4d
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-07-21 15:39:09 -0400

    getTextureImage is async.

--- core/backend/src/test/standalone/GetTextureImage.test.ts
+++ core/backend/src/test/standalone/GetTextureImage.test.ts
@@ -4,12 +4,14 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { expect } from "chai";
+import { ClientRequestContext } from "@bentley/bentleyjs-core";
 import { TextureLoadProps } from "@bentley/imodeljs-common";
 import { SnapshotDb } from "../../IModelDb";
 import { IModelTestUtils } from "../IModelTestUtils";
 
 describe.only("IModelDb.getTextureImage", () => {
   let imodel: SnapshotDb;
+  const ctxt = new ClientRequestContext();
 
   before(() => {
     imodel = IModelTestUtils.createSnapshotFromSeed(IModelTestUtils.prepareOutputFile("ElementGraphics", "mirukuru.ibim"), IModelTestUtils.resolveAssetFile("mirukuru.ibim"));
@@ -17,21 +19,21 @@ describe.only("IModelDb.getTextureImage", () => {
 
   after(() => imodel.close());
 
-  it("returns undefined if texture not found", () => {
-    expect(imodel.getTextureImage({ name: "0x123" })).to.be.undefined;
+  it("returns undefined if texture not found", async () => {
+    expect(await imodel.getTextureImage(ctxt, { name: "0x123" })).to.be.undefined;
   });
 
   describe("throws", () => {
-    it("if name is not a valid Id", () => {
-      expect(() => imodel.getTextureImage({ } as unknown as TextureLoadProps)).to.throw("GetTextureImage name property must be a valid Id64String");
-      expect(() => imodel.getTextureImage({ name: "0" })).to.throw("GetTextureImage name property must be a valid Id64String");
-      expect(() => imodel.getTextureImage({ name: "NotAnId" })).to.throw("GetTextureImage name property must be a valid Id64String");
+    it("if name is not a valid Id", async () => {
+      await expect(imodel.getTextureImage(ctxt, { } as unknown as TextureLoadProps)).to.be.rejectedWith("GetTextureImage name property must be a valid Id64String");
+      await expect(imodel.getTextureImage(ctxt, { name: "0" })).to.be.rejectedWith("GetTextureImage name property must be a valid Id64String");
+      await expect(imodel.getTextureImage(ctxt, { name: "NotAnId" })).to.be.rejectedWith("GetTextureImage name property must be a valid Id64String");
     });
 
-    it("if max size is not a positive number", () => {
-      expect(() => imodel.getTextureImage({ name: "0x123", maxTextureSize: "25" } as unknown as TextureLoadProps)).to.throw("GetTextureImage maxTextureSize property must be a positive number");
-      expect(() => imodel.getTextureImage({ name: "0x123", maxTextureSize: 0 })).to.throw("GetTextureImage maxTextureSize property must be a positive number");
-      expect(() => imodel.getTextureImage({ name: "0x123", maxTextureSize: -1 })).to.throw("GetTextureImage maxTextureSize property must be a positive number");
+    it("if max size is not a positive number", async () => {
+      await expect(imodel.getTextureImage(ctxt, { name: "0x123", maxTextureSize: "25" } as unknown as TextureLoadProps)).to.be.rejectedWith("GetTextureImage maxTextureSize property must be a positive number");
+      await expect(imodel.getTextureImage(ctxt, { name: "0x123", maxTextureSize: 0 })).to.be.rejectedWith("GetTextureImage maxTextureSize property must be a positive number");
+      await expect(imodel.getTextureImage(ctxt, { name: "0x123", maxTextureSize: -1 })).to.be.rejectedWith("GetTextureImage maxTextureSize property must be a positive number");
     });
   })
 });
