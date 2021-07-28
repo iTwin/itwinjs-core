0e5fb4bf9a3dc016fac56ad859a0a4023c994dc1
Dan East <51122937+DanEastBentley@users.noreply.github.com>
2021-07-28 10:47:45 -0400

    Fixed scrolling in Table component (#1952)
    * Fixed scrolling in Table component
    
    * rush change
    
    (cherry picked from commit 793b44e84db9c9937a7367258a6f78cf392afeae)
    
    # Conflicts:
    #	test-apps/ui-test-app/src/frontend/appui/contentviews/TableExampleContent.tsx

--- ui/components/src/ui-components/table/component/Table.tsx
+++ ui/components/src/ui-components/table/component/Table.tsx
@@ -16,7 +16,7 @@ import { DisposableList, Guid, GuidString } from "@bentley/bentleyjs-core";
 import { PropertyValueFormat } from "@bentley/ui-abstract";
 import {
   CommonProps, Dialog, ElementResizeObserver, isNavigationKey, ItemKeyboardNavigator, LocalSettingsStorage,
-  Orientation, SortDirection, UiSettings, UiSettingsStatus, UiSettingsStorage,
+  Orientation, SortDirection, Timer, UiSettings, UiSettingsStatus, UiSettingsStorage,
 } from "@bentley/ui-core";
 import {
   MultiSelectionHandler, OnItemsDeselectedCallback, OnItemsSelectedCallback, SelectionHandler, SingleSelectionHandler,
@@ -307,6 +307,7 @@ export class Table extends React.Component<TableProps, TableState> {
   private _filterDescriptors?: TableFilterDescriptorCollection;
   private _filterRowShown = false;
   private _topRowIndex = 0;
+  private _pokeScrollTimer = new Timer(100);
 
   /** @internal */
   public readonly state = initialState;
@@ -452,6 +453,7 @@ export class Table extends React.Component<TableProps, TableState> {
   public componentWillUnmount() {
     this._isMounted = false;
     this._disposableListeners.dispose();
+    this._unsetPokeScrollTimeout();
   }
 
   private scrollToRow(rowIndex: number) {
@@ -626,9 +628,19 @@ export class Table extends React.Component<TableProps, TableState> {
     }
   };
 
+  private _queuePokeScroll = () => {
+    this._unsetPokeScrollTimeout();
+    this._pokeScrollTimer.setOnExecute(() => { this._pokeScrollAfterUpdate(); });
+    this._pokeScrollTimer.start();
+  };
+
+  private _unsetPokeScrollTimeout = (): void => {
+    this._pokeScrollTimer.stop();
+  };
+
   private _onRowsChanged = async () => {
     await this.updateRows();
-    this._pokeScrollAfterUpdate();
+    this._queuePokeScroll();
   };
 
   /** @internal */
@@ -1068,7 +1080,7 @@ export class Table extends React.Component<TableProps, TableState> {
     // Sort the column
     this.gridSortAsync(columnKey, directionEnum); // eslint-disable-line @typescript-eslint/no-floating-promises
 
-    this._pokeScrollAfterUpdate();
+    this._queuePokeScroll();
   };
 
   private getColumnIndexFromKey(columnKey: string): number {
@@ -1735,7 +1747,7 @@ export class Table extends React.Component<TableProps, TableState> {
           }
           <ElementResizeObserver watchedElement={this.state.gridContainer}
             render={({ width, height }) => {
-              setTimeout(() => this._pokeScrollAfterUpdate());
+              setTimeout(() => this._queuePokeScroll());
               return (
                 <ReactDataGrid
                   ref={this._gridRef}
