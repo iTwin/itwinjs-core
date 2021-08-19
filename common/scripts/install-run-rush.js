1a97f2e37d49bfe0e1de9a95791c4ff358bd269f
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2021-08-19 12:39:27 -0400

    Update Rush and pnpm versions

--- common/scripts/install-run-rush.js
+++ common/scripts/install-run-rush.js
@@ -1,5 +1,8 @@
 "use strict";
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 // See the @microsoft/rush package's LICENSE file for license information.
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
@@ -16,7 +19,7 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
 };
