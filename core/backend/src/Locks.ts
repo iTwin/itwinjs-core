c91f51ed1e6ac7aff0743840e2d9c4b1d14dba31
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-08-23 13:18:52 -0400

    getParentAndModel

--- core/backend/src/Locks.ts
+++ core/backend/src/Locks.ts
@@ -6,6 +6,8 @@
  * @module iModels
  */
 
+import { DbResult, Id64String } from "@bentley/bentleyjs-core";
+import { IModelError } from "@bentley/imodeljs-common";
 import { LockProps } from "./BackendHubAccess";
 import { BriefcaseDb } from "./IModelDb";
 import { IModelHost } from "./IModelHost";
@@ -13,6 +15,17 @@ import { IModelHost } from "./IModelHost";
 export class Locks {
 
   constructor(private _iModel: BriefcaseDb) {
+  }
+
+  private getParentAndModel(id: Id64String): { modelId: Id64String, parentId: Id64String } {
+    return this._iModel.withPreparedSqliteStatement("SELECT ModelId,ParentId FROM bis_Element WHERE id=?", (stmt) => {
+      stmt.bindId(1, id);
+      const rc = stmt.step();
+      if (DbResult.BE_SQLITE_ROW !== rc)
+        throw new IModelError(rc, `element ${id} not found`);
+
+      return { modelId: stmt.getValueId(0), parentId: stmt.getValueId(1) };
+    });
   }
 
   public hasLock(_lock: LockProps): boolean {
