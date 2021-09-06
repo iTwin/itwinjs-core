c8c7097d12994cd0ac34aeffb29226434de57a56
swbsi <69857376+swbsi@users.noreply.github.com>
2021-09-06 15:22:51 -0400

    LocalhostIpcHost needs to control the http.Server now, not the app

--- core/backend/src/LocalhostIpcHost.ts
+++ core/backend/src/LocalhostIpcHost.ts
@@ -7,6 +7,7 @@
  */
 
 import * as ws from "ws";
+import * as http from "http";
 import { IpcWebSocket, IpcWebSocketBackend, IpcWebSocketMessage, IpcWebSocketTransport } from "@bentley/imodeljs-common";
 import { IpcHost } from "./IpcHost";
 import { IModelHostConfiguration } from "./IModelHost";
@@ -14,11 +15,14 @@ import { IModelHostConfiguration } from "./IModelHost";
 class LocalTransport extends IpcWebSocketTransport {
   private _server: ws.Server;
   private _connection: ws | undefined;
+  private _ready: Promise<void>;
 
-  public constructor(port: number) {
+  public get ready() { return this._ready; }
+
+  public constructor(port: number, server: http.Server) {
     super();
 
-    this._server = new ws.Server({ port });
+    this._server = new ws.Server({ port, server });
 
     this._server.on("connection", (connection) => {
       this._connection = connection;
@@ -28,6 +32,14 @@ class LocalTransport extends IpcWebSocketTransport {
           listener({} as Event, JSON.parse(data as string));
       });
     });
+
+    this._ready = new Promise((resolve, reject) => {
+      try {
+        server.listen(port, resolve);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 
   public send(message: IpcWebSocketMessage): void {
@@ -36,9 +48,23 @@ class LocalTransport extends IpcWebSocketTransport {
 }
 
 /** @internal */
+export interface LocalhostIpcHostOpts {
+  socketPort?: number;
+  appRequestListener?: http.RequestListener;
+}
+
+/** @internal */
 export class LocalhostIpcHost {
-  public static async startup(opts?: { localhostIpcHost?: { socketPort?: number }, iModelHost?: IModelHostConfiguration }) {
-    IpcWebSocket.transport = new LocalTransport(opts?.localhostIpcHost?.socketPort ?? 3002);
+  public static async startup(opts?: { localhostIpcHost?: LocalhostIpcHostOpts, iModelHost?: IModelHostConfiguration }) {
+    const server = http.createServer();
+    if (opts?.localhostIpcHost?.appRequestListener) {
+      server.on("request", opts.localhostIpcHost.appRequestListener);
+    }
+
+    const transport = new LocalTransport(opts?.localhostIpcHost?.socketPort ?? 3002, server);
+    IpcWebSocket.transport;
+    await transport.ready;
+
     const socket = new IpcWebSocketBackend();
     await IpcHost.startup({ ipcHost: { socket }, iModelHost: opts?.iModelHost });
   }
