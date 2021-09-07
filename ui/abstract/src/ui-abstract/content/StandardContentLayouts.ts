be9b3b44317cff6739cefce22c6d802523cb04d5
NancyMcCallB <45079789+NancyMcCallB@users.noreply.github.com>
2021-09-07 15:05:58 -0500

    Rename standard layout, update table in NextVersion.md

--- ui/abstract/src/ui-abstract/content/StandardContentLayouts.ts
+++ ui/abstract/src/ui-abstract/content/StandardContentLayouts.ts
@@ -8,43 +8,43 @@ import { ContentLayoutProps } from "./ContentLayoutProps";
 /** @packageDocumentation
  * @module ContentView
  */
-export const uifwSingleView: ContentLayoutProps =
+export const singleView: ContentLayoutProps =
  {
-   id: "uifw:singleView",
+   id: "uia:singleView",
    description: "Single Content View",
  };
 
-export const uifwFourQuadrants: ContentLayoutProps =
+export const fourQuadrants: ContentLayoutProps =
  {
-   id: "uifw:fourQuadrants",
+   id: "uia:fourQuadrants",
    description: "Four Views, two stacked on the left, two stacked on the right",
    verticalSplit: {
-     id: "uifw:fourQuadrantVerticalSplit",
+     id: "uia:fourQuadrantVerticalSplit",
      percentage: 0.50,
      lock: true,
      minSizeLeft: 100,
      minSizeRight: 100,
-     left: { horizontalSplit: { id: "uifw:fourQuadrantsLeftHorizontal", percentage: 0.50, top: 0, bottom: 1, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
-     right: { horizontalSplit: { id: "uifw:fourQuadrantsRightHorizontal", percentage: 0.50, top: 2, bottom: 3, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
+     left: { horizontalSplit: { id: "uia:fourQuadrantsLeftHorizontal", percentage: 0.50, top: 0, bottom: 1, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
+     right: { horizontalSplit: { id: "uia:fourQuadrantsRightHorizontal", percentage: 0.50, top: 2, bottom: 3, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
    },
  };
-export const uifwTwoVerticalSplit: ContentLayoutProps =
+export const twoVerticalSplit: ContentLayoutProps =
  {
-   id: "uifw:twoVerticalSplit",
+   id: "uia:twoVerticalSplit",
    description: "Two Views, side by side",
    verticalSplit: {
-     id: "uifw:twoViewsVerticalSplit",
+     id: "uia:twoViewsVerticalSplit",
      percentage: 0.50,
      left: 0,
      right: 1,
    },
  };
-export const uifwTwoHorizontalSplit: ContentLayoutProps =
+export const twoHorizontalSplit: ContentLayoutProps =
  {
-   id: "uifw:twoHorizontalSplit",
+   id: "uia:twoHorizontalSplit",
    description: "Two views, stack one on top of the other",
    horizontalSplit: {
-     id: "uifw:twoViewsHorizontalSplit",
+     id: "uia:twoViewsHorizontalSplit",
      percentage: 0.50,
      lock: true,
      top: 0,
@@ -52,63 +52,63 @@ export const uifwTwoHorizontalSplit: ContentLayoutProps =
    },
  };
 
-export const uifwThreeViewsTwoOnLeft: ContentLayoutProps =
+export const threeViewsTwoOnLeft: ContentLayoutProps =
  {
-   id: "uifw:threeViewsTwoOnLeft",
+   id: "uia:threeViewsTwoOnLeft",
    description: "Three views, one on the right with the two on the left stacked one of top of the other",
    verticalSplit: {
-     id: "uifw:twoViewsOnLeftSplit",
+     id: "uia:twoViewsOnLeftSplit",
      percentage: 0.50,
-     left: { horizontalSplit: { id: "uifw:twoViewsOnLeftHorizontal", percentage: 0.50, top: 0, bottom: 1, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
+     left: { horizontalSplit: { id: "uia:twoViewsOnLeftHorizontal", percentage: 0.50, top: 0, bottom: 1, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
      right: 2,
    },
  };
 
-export const uifwThreeViewsTwoOnRight: ContentLayoutProps =
+export const threeViewsTwoOnRight: ContentLayoutProps =
  {
-   id: "uifw:threeViewsTwoOnRight",
+   id: "uia:threeViewsTwoOnRight",
    description: "Three views, one on the left with the two on the right stacked one of top of the other",
    verticalSplit: {
-     id: "uifw:twoViewsOnRightSplit",
+     id: "uia:twoViewsOnRightSplit",
      percentage: 0.50,
      left: 0,
-     right: { horizontalSplit: { id: "uifw:twoViewsOnRightHorizontal", percentage: 0.50, top: 1, bottom: 2, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
+     right: { horizontalSplit: { id: "uia:twoViewsOnRightHorizontal", percentage: 0.50, top: 1, bottom: 2, lock: true, minSizeTop: 50, minSizeBottom: 50 } },
    },
  };
 
-export const uifwThreeViewsTwoOnBottom: ContentLayoutProps =
+export const threeViewsTwoOnBottom: ContentLayoutProps =
  {
-   id: "uifw:threeViewsTwoOnBottom",
+   id: "uia:threeViewsTwoOnBottom",
    description: "Three Views, one on top and two side by side on the bottom",
    horizontalSplit: {
-     id: "uifw:threeViewsTwoOnBottomHorizontal",
+     id: "uia:threeViewsTwoOnBottomHorizontal",
      percentage: 0.50,
      lock: true,
      top: 0,
-     bottom: { verticalSplit: { id: "uifw:twoViewsOnBottomVertical", percentage: 0.50, left: 1, right: 2, lock: true, minSizeLeft: 50, minSizeRight: 50 } },
+     bottom: { verticalSplit: { id: "uia:twoViewsOnBottomVertical", percentage: 0.50, left: 1, right: 2, lock: true, minSizeLeft: 50, minSizeRight: 50 } },
    },
  };
 
-export const uifwThreeViewsTwoOnTop: ContentLayoutProps =
+export const threeViewsTwoOnTop: ContentLayoutProps =
  {
-   id: "uifw:threeViewsTwoOnTop",
+   id: "uia:threeViewsTwoOnTop",
    description: "Three Views, two side by side on top and one on the bottom",
    horizontalSplit: {
-     id: "uifw:twoViewsOnTopHorizontal",
+     id: "uia:twoViewsOnTopHorizontal",
      percentage: 0.50,
      lock: true,
-     top: { verticalSplit: { id: "uifw:twoViewsOnTopVertical", percentage: 0.50, left: 0, right: 1, lock: true, minSizeLeft: 50, minSizeRight: 50 } },
+     top: { verticalSplit: { id: "uia:twoViewsOnTopVertical", percentage: 0.50, left: 0, right: 1, lock: true, minSizeLeft: 50, minSizeRight: 50 } },
      bottom: 2,
    },
  };
 
 export const standardViewLayouts: ContentLayoutProps[] = [
-  uifwSingleView,
-  uifwFourQuadrants,
-  uifwTwoVerticalSplit,
-  uifwTwoHorizontalSplit,
-  uifwThreeViewsTwoOnLeft,
-  uifwThreeViewsTwoOnRight,
-  uifwThreeViewsTwoOnBottom,
-  uifwThreeViewsTwoOnTop,
+  singleView,
+  fourQuadrants,
+  twoVerticalSplit,
+  twoHorizontalSplit,
+  threeViewsTwoOnLeft,
+  threeViewsTwoOnRight,
+  threeViewsTwoOnBottom,
+  threeViewsTwoOnTop,
 ];
