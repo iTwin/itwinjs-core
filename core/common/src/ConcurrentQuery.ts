b35c3ae823ace1929e1f5e6ac9464095d670e47d
Affan Khan <khanaffan@users.noreply.github.com>
2021-09-30 15:46:10 -0400

    next version

--- core/common/src/ConcurrentQuery.ts
+++ core/common/src/ConcurrentQuery.ts
@@ -4,17 +4,34 @@
 *--------------------------------------------------------------------------------------------*/
 import { BentleyError, CompressedId64Set, DbResult, Id64Set, Id64String } from "@itwin/core-bentley";
 import { Point2d, Point3d } from "@itwin/core-geometry";
-/** @public */
+/**
+ * Specify row format for the row returned by query() and restartQuery() methods
+ * @public
+ * */
 export enum QueryRowFormat {
+  /** Return ecsql name as defined in query
+   * This format does not include null and undefined values.
+  */
   UseECSqlPropertyNames,
+  /** Backward compatible format. It use Js base name as 2.x
+   * This format does not include null and undefined values.
+  */
   UseJsPropertyNames,
-  Array, // Any trailing null will be ignored.
+  /** Return array of values and can be accessed using index. This is default and preferred way.
+   * This format include nulls. Trailing null are not present and return undefine
+  */
+  Array,
+  /** Default is set to be array. */
   Default = Array,
-
 }
-/** @beta */
+/**
+ * Specify limit or range of rows to return
+ * @beta
+ * */
 export interface Limit {
+  /** Number of rows to return */
   count?: number;
+  /** Offset from which to return rows */
   offset?: number;
 }
 /** @beta */
@@ -35,23 +52,50 @@ export interface Stats {
   memLimit: number;
   memUsed: number;
 }
-/** @beta */
+/**
+ * Quota hint for the query.
+ * @beta
+ * */
 export interface Quota {
+  /** Max time allowed in seconds. This is hint and may not be honoured but help in prioritize request */
   time?: number;
+  /** Max memory allowed in bytes. This is hint and may not be honoured but help in prioritize request */
   memory?: number;
 }
-/** @beta */
+/**
+ * Config for all request made to concurrent query engine.
+ * @beta
+ * */
 export interface BaseConfig {
+  /** Determine priority of this query default to 0, used as hint and can be overriden by backend. */
   priority?: number;
+  /** If specified cancel last query (if any) with same restart token and queue the new query */
   restartToken?: string;
+  /** For editing apps this can be set to true and all query will run on primary connection
+  *  his may cause slow queries execution but the most recent data changes will be visitable via query
+  */
   usePrimaryConn?: boolean;
+  /** Restrict time or memory for query but use as hint and may be changed base on backend settings */
   quota?: Quota;
 }
-/** @beta */
+/**
+ * ECSql query config
+ * @beta
+ * */
 export interface QueryConfig extends BaseConfig {
+  /**
+   * default to false. It abbreviate blobs to single bytes. This help cases where wildcard is
+   * used in select clause. Use BlobReader api to read individual blob specially if its of large size.
+   * */
   abbreviateBlobs?: boolean;
+  /**
+   * default to false. It will suppress error and will not log it. Useful in cases where we expect query
+   * can fail.
+   */
   suppressLogErrors?: boolean;
+  /** This is used internally. If true it query will return meta data about query. */
   includeMetaData?: boolean;
+  /** Limit range of rows returned by query*/
   limit?: Limit;
 }
 /** @beta */
