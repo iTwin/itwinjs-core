098498f9ed2a52177b48ebe043ce25915cd5941a
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-10-05 08:21:21 -0400

    MaskTreeReference must set isPlanProjection appropriately (#2418)

--- core/frontend/src/tile/PrimaryTileTree.ts
+++ core/frontend/src/tile/PrimaryTileTree.ts
@@ -386,10 +386,17 @@ class MaskTreeReference extends TileTreeReference {
   private _owner: TileTreeOwner;
   public readonly model: GeometricModelState;
   public override get castsShadows() { return false; }
-  public constructor(model: GeometricModelState) {
+  public constructor(view: ViewState, model: GeometricModelState) {
     super();
     this.model = model;
-    this._id = { modelId: model.id, is3d: model.is3d, treeId: this.createTreeId(), isPlanProjection: false, forceNoInstancing: false };
+    this._id = {
+      modelId: model.id,
+      is3d: model.is3d,
+      treeId: this.createTreeId(),
+      isPlanProjection: isPlanProjection(view, model),
+      forceNoInstancing: false,
+    };
+
     this._owner = primaryTreeSupplier.getOwner(this._id, model.iModel);
   }
 
@@ -408,8 +415,8 @@ class MaskTreeReference extends TileTreeReference {
 }
 
 /** @internal */
-export function createMaskTreeReference(model: GeometricModelState): TileTreeReference {
-  return new MaskTreeReference(model);
+export function createMaskTreeReference(view: ViewState, model: GeometricModelState): TileTreeReference {
+  return new MaskTreeReference(view, model);
 }
 
 /** Provides [[TileTreeReference]]s for the loaded models present in a [[SpatialViewState]]'s [[ModelSelectorState]].
