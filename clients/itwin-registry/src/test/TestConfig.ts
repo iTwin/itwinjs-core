4ed640a3333640acdd93ed98a9857a928fa67585
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2021-10-05 19:23:23 -0400

    Minor tweaks to x-correlation-id

--- clients/itwin-registry/src/test/TestConfig.ts
+++ clients/itwin-registry/src/test/TestConfig.ts
@@ -11,9 +11,6 @@ export class TestConfig {
   /** Name of the iTwin used by some tests */
   public static readonly iTwinName: string = "iModelJsIntegrationTest";
 
-  /** Id of the iTwin used by some tests */
-  public static readonly iTwinId: string = "ec002f93-f0c1-4ab3-a407-351848eba233";
-
   /** Login the specified user and return the AuthorizationToken */
   public static async getAccessToken(user: TestUserCredentials = TestUsers.regular): Promise<AccessToken> {
     return getAccessTokenFromBackend(user);
--- clients/itwin/src/Request.ts
+++ clients/itwin/src/Request.ts
@@ -282,9 +282,8 @@ export async function request(url: string, options: RequestOptions): Promise<Res
   if (options.headers)
     sareq = sareq.set(options.headers);
 
-  (await sareq).headers;
   // Add an x-correlation-id header with a new GUID if one doesn't already exist.
-  if (!options.headers || !options.headers.hasOwnProperty("x-correlation-id"))
+  if (!options.headers || !options.headers.hasOwnProperty(requestIdHeaderName))
     sareq = sareq.set(requestIdHeaderName, Guid.createValue());
 
   let queryStr: string = "";
