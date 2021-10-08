8ccbb371f4db502fd64ec209b0888b8f41901a48
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-10-08 11:06:24 -0400

    add documentation for Localization

--- core/common/src/Localization.ts
+++ core/common/src/Localization.ts
@@ -38,22 +38,63 @@ interface LocalizationOptions {
  * @public
  */
 export interface Localization {
-  /** Returns the localized string. If an array is provided, localizes and returns the first recognized key in the array. */
+  /** Return the translated value of a key.
+   * @param key - the key that matches a property in the JSON localization file.
+   * @note The key includes the namespace, which identifies the particular localization file that contains the property,
+   * followed by a colon, followed by the property in the JSON file.
+   * For example:
+   * ``` ts
+   * const dataString: string = IModelApp.localization.getLocalizedString("iModelJs:BackgroundMap.BingDataAttribution");
+   *  ```
+   * assigns to dataString the string with property BackgroundMap.BingDataAttribution from the iModelJs.json localization file.
+   * @returns The string corresponding to the first key that resolves.
+   * @throws Error if no keys resolve to a string.
+   */
   getLocalizedString(key: string | string[], options?: LocalizationOptions): string;
-  /** Returns the localized string under the provided namespace. If an array is provided, localizes and returns the first recognized key in the array. */
+  /** Similar to 'translate()' but the namespace is a separate param and the key does not include the namespace.
+   * @param namespace - the namespace that identifies the particular localization file that contains the property.
+   * @param key - the key that matches a property in the JSON localization file.
+   * @returns The string corresponding to the first key that resolves.
+   * @throws Error if no keys resolve to a string.
+   */
   getLocalizedStringWithNamespace(namespace: string, key: string | string[], options?: LocalizationOptions): string;
+  /** get the English string for a key. */
   getEnglishString(namespace: string, key: string | string[], options?: LocalizationOptions): string;
+  /** Replace all instances of `%{key}` within a string with the translations of those keys.
+   * For example:
+   * ``` ts
+   * "MyKeys": {
+   *   "Key1": "First value",
+   *   "Key2": "Second value"
+   *  }
+   * ```
+   *
+   * ``` ts
+   * getLocalizedKeys("string with %{MyKeys.Key1} followed by %{MyKeys.Key2}!"") // returns "string with First Value followed by Second Value!"
+   * ```
+   */
   getLocalizedKeys(inputString: string): string;
+
+  /** Register a new Namespace and return a Promise that is fulfilled when the content is loaded.
+   * If the namespace is already registered, its Promise will be returned.
+   * @param name - the name of the namespace.
+   * @note - The registerNamespace method starts fetching the appropriate version of the JSON localization file from the server,
+   * based on the current locale. To make sure that fetch is complete before performing translations from this namespace, await
+   * fulfillment of returned Promise.
+   * @see [Localization in iTwin.js]($docs/learning/frontend/Localization.md)
+   */
   registerNamespace(namespace: string, setDefault?: true): Promise<void>;
+  /** @internal */
   unregisterNamespace(namespace: string): void;
+  /** @internal */
   getNamespacePromise(name: string): Promise<void> | undefined;
   /** Get the list of available languages for translations */
   getLanguageList(): string[];
-  /** change the language for translations. This overrides the language from the browser. */
+  /** Change the language for translations. This overrides the language from the browser. */
   changeLanguage(language: string): void;
 }
 
-/** The default [[Localization]] used in the event that an implementation is not provided to [IModelApp]($frontend). Does not perform localizations.
+/** An empty [[Localization]] used if one is not provided to [IModelApp]($frontend). Does not perform localizations (merely returns the key.)
  * @public
  */
 export class EmptyLocalization implements Localization {
@@ -61,9 +102,9 @@ export class EmptyLocalization implements Localization {
   public getLocalizedStringWithNamespace(_namespace: string, key: string | string[]): string { return this.getLocalizedString(key); }
   public getEnglishString(_namespace: string, key: string | string[]): string { return this.getLocalizedString(key); }
   public getLocalizedKeys(inputString: string): string { return inputString; }
-  public async registerNamespace(): Promise<void> { return; }
+  public async registerNamespace(): Promise<void> { }
   public unregisterNamespace(): void { }
-  public getNamespacePromise(): Promise<void> | undefined { return; }
+  public getNamespacePromise(): Promise<void> | undefined { return undefined; }
   public getLanguageList(): string[] { return []; }
   public changeLanguage(): void { }
 }
