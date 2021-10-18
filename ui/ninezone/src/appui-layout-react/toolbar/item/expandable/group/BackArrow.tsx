5621ecca4d289742aa183b58f5cd6277bf7eccfd
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2021-10-18 12:15:50 +0300

    Internal BackArrow used in NestedGroup.

--- ui/ninezone/src/appui-layout-react/toolbar/item/expandable/group/BackArrow.tsx
+++ ui/ninezone/src/appui-layout-react/toolbar/item/expandable/group/BackArrow.tsx
@@ -12,7 +12,8 @@ import * as React from "react";
 import { CommonProps, NoChildrenProps, useTargeted } from "@itwin/core-react";
 
 /** Properties of [[BackArrow]] component.
- * @alpha
+ * @deprecated
+ * @internal
  */
 export interface BackArrowProps extends CommonProps, NoChildrenProps {
   /** Function called when arrow is clicked. */
@@ -43,7 +44,8 @@ function BackArrowComponent(props: BackArrowProps) {
 }
 
 /** Back arrow used in [[NestedGroup]] component.
- * @alpha
+ * @deprecated
+ * @internal
  */
 export class BackArrow extends React.PureComponent<BackArrowProps> {
   public override render() {
