f27ca76a9ea1ff0ff2d88b8c8de7a32737d08931
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-11-01 08:38:08 -0400

    Two-way viewport sync that only syncs frusta.

--- core/frontend/src/TwoWayViewportSync.ts
+++ core/frontend/src/TwoWayViewportSync.ts
@@ -15,33 +15,55 @@ import { Viewport } from "./Viewport";
  * @public
  */
 export class TwoWayViewportSync {
-  private readonly _disconnect: VoidFunction[] = [];
+  protected readonly _disconnect: VoidFunction[] = [];
   private _isEcho = false;
 
   private syncView(source: Viewport, target: Viewport) {
-    if (this._isEcho) return;
+    if (this._isEcho)
+      return;
+
     this._isEcho = true; // so we don't react to the echo of this sync
-    target.applyViewState(source.view.clone(target.iModel));
+    this.syncViewports(source, target);
     this._isEcho = false;
+  }
+
+  protected syncViewports(source: Viewport, target: Viewport): void {
+    target.applyViewState(source.view.clone(target.iModel));
+  }
+
+  protected connectViewports(source: Viewport, target: Viewport): void {
+    const viewState = source.view.clone(target.iModel);
+    target.applyViewState(viewState);
   }
 
   /** Establish the connection between two Viewports. When this method is called, `view2` is initialized with the state of `view1`.
    * Thereafter, any change to the frustum of either view will be reflected in the frustum of the other view.
    */
-  public connect(view1: Viewport, view2: Viewport) {
+  public connect(source: Viewport, target: Viewport) {
     this.disconnect();
 
-    const viewState2 = view1.view.clone(view2.iModel); // use view1 as the starting point
-    view2.applyViewState(viewState2);
+    this.connectViewports(source, target);
 
     // listen to the onViewChanged events from both views
-    this._disconnect.push(view1.onViewChanged.addListener(() => this.syncView(view1, view2)));
-    this._disconnect.push(view2.onViewChanged.addListener(() => this.syncView(view2, view1)));
+    this._disconnect.push(source.onViewChanged.addListener(() => this.syncView(source, target)));
+    this._disconnect.push(target.onViewChanged.addListener(() => this.syncView(target, source)));
   }
 
   /** Remove the connection between the two views. */
   public disconnect() {
     this._disconnect.forEach((f) => f());
     this._disconnect.length = 0;
+  }
+}
+
+export class TwoWayViewportFrustumSync extends TwoWayViewportSync {
+  protected override syncViewports(source: Viewport, target: Viewport): void {
+    const pose = source.view.savePose();
+    const view = target.view.applyPose(pose);
+    target.applyViewState(view);
+  }
+
+  protected override connectViewports(source: Viewport, target: Viewport): void {
+    this.syncViewports(source, target);
   }
 }
