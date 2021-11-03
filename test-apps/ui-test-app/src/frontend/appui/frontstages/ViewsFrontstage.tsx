5a5d0c3c1d85540c184032ff3ea5c5de5dae54c1
Bill Steinbock <65047615+bsteinbk@users.noreply.github.com>
2021-11-03 09:33:04 -0400

    Update IconComponent SCSS to ensure color for inline svg is set (#2640)
    * fix svg styling of in-line svg icons
    
    * rush change

--- test-apps/ui-test-app/src/frontend/appui/frontstages/ViewsFrontstage.tsx
+++ test-apps/ui-test-app/src/frontend/appui/frontstages/ViewsFrontstage.tsx
@@ -50,6 +50,14 @@ import { VisibilityWidgetControl } from "../widgets/VisibilityWidget";
 import { NestedAnimationStage } from "./NestedAnimationStage";
 import { ViewSelectorPanel } from "../../tools/ViewSelectorPanel";
 
+function SvgApple(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' {...props}>
+      <path d='m14.38732 12.46864a8.67507 8.67507 0 0 1 -.85133 1.54667 7.83909 7.83909 0 0 1 -1.096 1.33933 2.11842 2.11842 0 0 1 -1.40933.62733 3.50824 3.50824 0 0 1 -1.30133-.314 3.7014 3.7014 0 0 0 -1.40133-.31333 3.82728 3.82728 0 0 0 -1.44066.31333 3.84425 3.84425 0 0 1 -1.24467.33067 1.98968 1.98968 0 0 1 -1.44066-.644 8.203 8.203 0 0 1 -1.14667-1.38664 9.61729 9.61729 0 0 1 -1.21266-2.43466 8.99338 8.99338 0 0 1 -.50933-2.90134 5.34287 5.34287 0 0 1 .68865-2.772 4.05969 4.05969 0 0 1 1.44134-1.474 3.84792 3.84792 0 0 1 1.94933-.556 4.55944 4.55944 0 0 1 1.50733.35466 4.79788 4.79788 0 0 0 1.196.35534 7.06478 7.06478 0 0 0 1.326-.41866 4.34039 4.34039 0 0 1 1.802-.32334 3.8146 3.8146 0 0 1 2.99733 1.59533 3.37671 3.37671 0 0 0 -1.768 3.062 3.3911 3.3911 0 0 0 1.09733 2.54467 3.59839 3.59839 0 0 0 1.096.72733q-.132.386-.27933.74133zm-3.05466-12.14864a3.43565 3.43565 0 0 1 -.86533 2.23866 2.93869 2.93869 0 0 1 -2.45 1.22267 2.58687 2.58687 0 0 1 -.018-.30334 3.63848 3.63848 0 0 1 2.03667-3.11132 3.30968 3.30968 0 0 1 1.28-.36667 2.86658 2.86658 0 0 1 .01667.32z' />
+    </svg>
+  );
+}
+
 function MyLoremIpsumPanel() {
   React.useEffect(() => {
     // eslint-disable-next-line no-console
@@ -776,7 +784,7 @@ class AdditionalTools {
     ToolbarHelper.createToolbarItemFromItemDef(140, AppTools.toggleHideShowItemsCommand, { groupPriority: 30 }),
     ToolbarHelper.createToolbarItemFromItemDef(145, new CommandItemDef({
       commandId: "Show widget",
-      iconSpec: "icon-placeholder",
+      iconSpec: <SvgApple />,
       label: "Show widget",
       execute: () => {
         const frontstageDef = FrontstageManager.activeFrontstageDef;
