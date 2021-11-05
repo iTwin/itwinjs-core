4f2a097471bb4d9321a3e77e616ebbcb1d7f0b15
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-11-05 06:35:58 -0400

    doc

--- core/bentley/src/Assert.ts
+++ core/bentley/src/Assert.ts
@@ -21,13 +21,15 @@ let assertionsEnabled = process.env.NODE_ENV === "development";
  */
 
 /** Asserts that a condition is `true` and - in development builds - throws an error if it is not.
- * Assertions serve two purposes:
+ * Assertions exist solely to assist programmers during development, in the following ways:
  *  1 They allow the programmer to declare conditions that they believe cannot possibly occur. If such conditions occur, they indicate
  *    a serious flaw in the programmer's logic.
  *  2 They allow the programmer to assure the TypeScript compiler of the truth of some condition that the compiler cannot itself infer.
+ *  3 They allow the author of an API to indicate to consumers of the API a serious misuse that should be corrected during development.
  *
- * Both purposes are intended for **programmers only**, to verify their assumptions during development. Assertions should **never** be used to test
- * for conditions - however unlikely - that could be expected to occur at run-time, such as failing to write to a file or load a resource over the network.
+ * Assertions should **never** be used to test for conditions - however unlikely - that could be expected to occur at run-time,
+ * such as failing to write to a file or load a resource over the network. If the condition asserted ever fails in a production environment,
+ * the programmer has made a serious mistake.
  *
  * By default, `assert` does nothing unless the environment variable `NODE_ENV` exists and is set to `development`. Use [[setAssertionsEnabled]] to override this behavior.
  *
