cfe5c4a412963f61681bb5b5c1a24a1fc8f28372
Ellord207 <30239292+Ellord207@users.noreply.github.com>
2021-11-11 14:50:14 -0600

    tests WIP: 'Error: RPC interface 'IModelReadRpcInterface' is not initialized.'

---
+++ core/frontend/src/test/ViewCreator.test.ts
@@ -0,0 +1,64 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+import { LightSettings } from "@itwin/core-common";
+import { IModelApp } from "../IModelApp";
+import { BlankConnection } from "../IModelConnection";
+import { ViewCreator3d, ViewCreator3dOptions } from "../ViewCreator3d";
+import { ViewState3d } from "../ViewState";
+import { createBlankConnection } from "./createBlankConnection";
+
+describe.only("ViewCreator", () => {
+  let iModel: BlankConnection;
+
+  before(async () => {
+    await IModelApp.startup();
+    iModel = createBlankConnection();
+  });
+  after(async () => {
+    await iModel.close();
+    await IModelApp.shutdown();
+  });
+
+  describe("3d option", () => {
+    let creator: ViewCreator3d;
+
+    async function test3dOption(options: ViewCreator3dOptions, testOptionAffect: (view: ViewState3d) => boolean) {
+      let view = await creator.createDefaultView();
+      expect(testOptionAffect(view as ViewState3d) === false, "Unexpected affect of option found.");
+
+      view = await creator.createDefaultView(options);
+      expect(testOptionAffect(view as ViewState3d), "Expected option's affect, but was not found.");
+    }
+
+    before(() => creator = new ViewCreator3d(iModel));
+
+    it("'cameraOn' should control when the camera is enabled in the created view.", async () => {
+      const test = (view: ViewState3d): boolean => view.isCameraOn;
+      const options: ViewCreator3dOptions = { cameraOn: true };
+      await test3dOption(options, test);
+    });
+    it("'skyboxOn' should control when the skybox is on in the created view.", async () => {
+      const test = (view: ViewState3d): boolean => view.displayStyle.environment.displaySky;
+      const options: ViewCreator3dOptions = { skyboxOn: true };
+      await test3dOption(options, test);
+    });
+    it("'useDefaultLighting' should control if the lighting is overriden in the created view.", async () => {
+      const test = (view: ViewState3d): boolean => {
+        // Test if the lighting is equivalent to the core default lighting.
+        const defaultLighting = LightSettings.fromJSON();
+        const viewLighting = view.displayStyle.lights;
+        return !viewLighting.equals(defaultLighting);
+      };
+      const options: ViewCreator3dOptions = { useDefaultLighting: true };
+      await test3dOption(options, test);
+    });
+
+  });
+  describe("2d options", () => {
+
+  });
+});
