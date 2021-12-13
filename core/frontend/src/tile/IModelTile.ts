iTwin/itwinjs-core

commit 9e8ec1c37c7fa052610e5d7431b03cfa61e47843

Fix regression in ImageTests animated test cases (#2784)
* Create a branch for the root node IFF the tile tree contains transform nodes.

* lint, api.

* please trigger my build github.

core/frontend/src/tile/IModelTile.ts
@@ -113,8 +113,15 @@ export class IModelTile extends Tile {
       return content;
 
     const tree = this.iModelTree;
-    const mult = this.hasSizeMultiplier ? this.sizeMultiplier : undefined;
-    const reader = ImdlReader.create(streamBuffer, tree.iModel, tree.modelId, tree.is3d, system, tree.batchType, tree.hasEdges, isCanceled, mult, { tileId: this.contentId });
+    const sizeMultiplier = this.hasSizeMultiplier ? this.sizeMultiplier : undefined;
+    const { iModel, modelId, is3d, containsTransformNodes } = tree;
+    const reader = ImdlReader.create({
+      stream: streamBuffer,
+      type: tree.batchType,
+      loadEdges: tree.hasEdges,
+      iModel, modelId, is3d, system, isCanceled, sizeMultiplier, containsTransformNodes,
+    });
+
     if (undefined !== reader) {
       try {
         content = await reader.read();

core/frontend/src/tile/DynamicIModelTile.ts
@@ -384,7 +384,13 @@ class GraphicsTile extends Tile {
 
     const tree = this.tree;
     assert(tree instanceof IModelTileTree);
-    const reader = ImdlReader.create(stream, tree.iModel, tree.modelId, tree.is3d, system, tree.batchType, tree.hasEdges, isCanceled, undefined, { tileId: this.contentId });
+    const { iModel, modelId, is3d, containsTransformNodes } = tree;
+    const reader = ImdlReader.create({
+      stream, iModel, modelId, is3d, system, isCanceled, containsTransformNodes,
+      type: tree.batchType,
+      loadEdges: tree.hasEdges,
+      options: { tileId: this.contentId },
+    });
 
     let content: TileContent = { isLeaf: true };
     if (reader) {

core/frontend/src/tile/IModelTileTree.ts
@@ -429,4 +429,8 @@ export class IModelTileTree extends TileTree {
   public getTransformNodeRange(nodeId: number): Range3d | undefined {
     return this._transformNodeRanges?.get(nodeId);
   }
+
+  public get containsTransformNodes(): boolean {
+    return undefined !== this._transformNodeRanges;
+  }
 }

core/frontend/src/tile/ImdlReader.ts
@@ -46,7 +46,11 @@ export interface ImdlReaderResult extends IModelTileContent {
  */
 export async function readElementGraphics(bytes: Uint8Array, iModel: IModelConnection, modelId: Id64String, is3d: boolean, options?: BatchOptions | false): Promise<RenderGraphic | undefined> {
   const stream = new ByteStream(bytes.buffer);
-  const reader = ImdlReader.create(stream, iModel, modelId, is3d, IModelApp.renderSystem, undefined, undefined, undefined, undefined, options);
+  const reader = ImdlReader.create({
+    stream, iModel, modelId, is3d, options,
+    system: IModelApp.renderSystem,
+  });
+
   if (!reader)
     return undefined;
 
@@ -185,6 +189,23 @@ interface ImdlAreaPatternSymbol {
   readonly primitives: AnyImdlPrimitive[];
 }
 
+/** Arguments supplied to [[ImdlReader.create]]
+ * @internal
+ */
+export interface ImdlReaderCreateArgs {
+  stream: ByteStream;
+  iModel: IModelConnection;
+  modelId: Id64String;
+  is3d: boolean;
+  system: RenderSystem;
+  type?: BatchType; // default Primary
+  loadEdges?: boolean; // default true
+  isCanceled?: ShouldAbortReadGltf;
+  sizeMultiplier?: number;
+  options?: BatchOptions | false;
+  containsTransformNodes?: boolean; // default false
+}
+
 /** Deserializes tile content in iMdl format. These tiles contain element geometry encoded into a format optimized for the imodeljs webgl renderer.
  * @internal
  */
@@ -194,22 +215,21 @@ export class ImdlReader extends GltfReader {
   private readonly _options: BatchOptions | false;
   private readonly _patternSymbols: { [key: string]: ImdlAreaPatternSymbol | undefined };
   private readonly _patternGeometry = new Map<string, RenderGeometry[]>();
+  private readonly _containsTransformNodes: boolean;
 
   /** Attempt to initialize an ImdlReader to deserialize iModel tile data beginning at the stream's current position. */
-  public static create(stream: ByteStream, iModel: IModelConnection, modelId: Id64String, is3d: boolean, system: RenderSystem,
-    type: BatchType = BatchType.Primary, loadEdges: boolean = true, isCanceled?: ShouldAbortReadGltf, sizeMultiplier?: number,
-    options?: BatchOptions | false): ImdlReader | undefined {
-    const header = new ImdlHeader(stream);
+  public static create(args: ImdlReaderCreateArgs): ImdlReader | undefined {
+    const header = new ImdlHeader(args.stream);
     if (!header.isValid || !header.isReadableVersion)
       return undefined;
 
     // The feature table follows the iMdl header
-    if (!this.skipFeatureTable(stream))
+    if (!this.skipFeatureTable(args.stream))
       return undefined;
 
     // A glTF header follows the feature table
-    const props = GltfReaderProps.create(stream, false);
-    return undefined !== props ? new ImdlReader(props, iModel, modelId, is3d, system, type, loadEdges, isCanceled, sizeMultiplier, options) : undefined;
+    const props = GltfReaderProps.create(args.stream, false);
+    return undefined !== props ? new ImdlReader(props, args) : undefined;
   }
 
   /** Attempt to deserialize the tile data */
@@ -461,12 +481,12 @@ export class ImdlReader extends GltfReader {
     return new PackedFeatureTable(packedFeatureArray, this._modelId, header.count, header.maxFeatures, this._type, animNodesArray);
   }
 
-  private constructor(props: GltfReaderProps, iModel: IModelConnection, modelId: Id64String, is3d: boolean, system: RenderSystem,
-    type: BatchType, loadEdges: boolean, isCanceled?: ShouldAbortReadGltf, sizeMultiplier?: number, options?: BatchOptions | false) {
-    super(props, iModel, modelId, is3d, system, type, isCanceled);
-    this._sizeMultiplier = sizeMultiplier;
-    this._loadEdges = loadEdges;
-    this._options = options ?? {};
+  private constructor(props: GltfReaderProps, args: ImdlReaderCreateArgs) {
+    super(props, args.iModel, args.modelId, args.is3d, args.system, args.type, args.isCanceled);
+    this._sizeMultiplier = args.sizeMultiplier;
+    this._loadEdges = args.loadEdges ?? true;
+    this._options = args.options ?? {};
+    this._containsTransformNodes = args.containsTransformNodes ?? false;
     this._patternSymbols = props.scene.patternSymbols ?? {};
   }
 
@@ -871,12 +891,7 @@ export class ImdlReader extends GltfReader {
           graphics.push(this._system.createBranch(branch, Transform.createIdentity()));
       };
 
-      // If more than one node exists, then we need to create a branch for Node_Root so that elements not associated with
-      // any node in the schedule script can be grouped together.
-      const nodeKeys = Object.keys(this._nodes);
-      const needBranchForRootNode = nodeKeys.length > 1;
-
-      for (const nodeKey of nodeKeys) {
+      for (const nodeKey of Object.keys(this._nodes)) {
         const meshValue = this._meshes[this._nodes[nodeKey]] as ImdlMesh | undefined;
         const primitives = meshValue?.primitives;
         if (!primitives || !meshValue)
@@ -884,7 +899,9 @@ export class ImdlReader extends GltfReader {
 
         const layerId = meshValue.layer;
         if ("Node_Root" === nodeKey) {
-          if (needBranchForRootNode) {
+          // If transform nodes exist in the tile tree, then we need to create a branch for Node_Root so that elements not associated with
+          // any node in the schedule script can be grouped together.
+          if (this._containsTransformNodes) {
             readBranch(primitives, AnimationNodeId.Untransformed, undefined);
           } else {
             for (const primitive of primitives) {

full-stack-tests/core/src/frontend/standalone/tile/Disposable.test.ts
@@ -208,7 +208,14 @@ describe("Disposal of WebGL Resources", () => {
     // Get a render graphic from tile reader
     const model = new FakeGMState(new FakeModelProps(new FakeREProps()), imodel0);
     const stream = new ByteStream(TILE_DATA_1_1.triangles.bytes.buffer);
-    const reader = ImdlReader.create(stream, model.iModel, model.id, model.is3d, system);
+    const reader = ImdlReader.create({
+      stream,
+      iModel: model.iModel,
+      modelId: model.id,
+      is3d: model.is3d,
+      system,
+    });
+
     expect(reader).not.to.be.undefined;
     const readerRes = await reader!.read();
     const tileGraphic = readerRes.graphic!;

full-stack-tests/core/src/frontend/standalone/tile/TileIO.test.ts
@@ -98,7 +98,14 @@ function processHeader(data: TileTestData, test: TileTestCase, numElements: numb
 function createReader(imodel: IModelConnection, data: TileTestData, test: TileTestCase): ImdlReader | undefined {
   const model = new FakeGMState(new FakeModelProps(new FakeREProps()), imodel);
   const stream = new ByteStream(test.bytes.buffer);
-  const reader = ImdlReader.create(stream, imodel, model.id, model.is3d, IModelApp.renderSystem);
+  const reader = ImdlReader.create({
+    stream,
+    iModel: imodel,
+    modelId: model.id,
+    is3d: model.is3d,
+    system: IModelApp.renderSystem,
+  });
+
   expect(undefined === reader).to.equal(!!data.unreadable);
   return reader;
 }
@@ -441,7 +448,17 @@ describe("TileIO (mock render)", () => {
     if (IModelApp.initialized) {
       const model = new FakeGMState(new FakeModelProps(new FakeREProps()), imodel);
       const stream = new ByteStream(currentTestCase.rectangle.bytes.buffer);
-      const reader = ImdlReader.create(stream, model.iModel, model.id, model.is3d, IModelApp.renderSystem, BatchType.Primary, true, (_) => true);
+      const reader = ImdlReader.create({
+        stream,
+        iModel: model.iModel,
+        modelId: model.id,
+        is3d: model.is3d,
+        system: IModelApp.renderSystem,
+        type: BatchType.Primary,
+        loadEdges: true,
+        isCanceled: (_) => true,
+      });
+
       expect(reader).not.to.be.undefined;
 
       const result = await reader!.read();
@@ -799,7 +816,14 @@ describe.skip("TileAdmin", () => {
         expect(response).instanceof(Uint8Array);
 
         const stream = new ByteStream(response.buffer);
-        const reader = ImdlReader.create(stream, imodel, "0x1c", true, IModelApp.renderSystem)!;
+        const reader = ImdlReader.create({
+          stream,
+          iModel: imodel,
+          modelId: "0x1c",
+          is3d: true,
+          system: IModelApp.renderSystem,
+        });
+
         expect(reader).not.to.be.undefined;
 
         const meshes = (reader as any)._meshes;

test-apps/display-test-app/src/frontend/TileContentTool.ts
@@ -22,7 +22,15 @@ export class GenerateTileContentTool extends Tool {
       const { tree, contentId } = args;
       const bytes = await IModelApp.tileAdmin.generateTileContent({ contentId, iModelTree: tree });
       const stream = new ByteStream(bytes.buffer);
-      const reader = ImdlReader.create(stream, tree.iModel, tree.modelId, tree.is3d, IModelApp.renderSystem, tree.batchType, tree.hasEdges, undefined, undefined, { tileId: contentId });
+      const { iModel, modelId, is3d, containsTransformNodes } = tree;
+      const reader = ImdlReader.create({
+        stream, iModel, modelId, is3d, containsTransformNodes,
+        system: IModelApp.renderSystem,
+        type: tree.batchType,
+        loadEdges: tree.hasEdges,
+        options: { tileId: contentId },
+      });
+
       assert(undefined !== reader);
       await reader.read();
       return true;

commit d3485304263d646eb68d37aebb073efbee6e530a

Fix failure to include tileId, regressed in #2784

core/frontend/src/tile/IModelTile.ts
@@ -119,6 +119,7 @@ export class IModelTile extends Tile {
       stream: streamBuffer,
       type: tree.batchType,
       loadEdges: tree.hasEdges,
+      options: { tileId: this.contentId },
       iModel, modelId, is3d, system, isCanceled, sizeMultiplier, containsTransformNodes,
     });
 