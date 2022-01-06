d0b82e3d8067b99ea3c2d4b49129811804fb8624
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2022-01-06 08:34:29 -0500

    Introduce a workaround to the peerDependency issue with using imodels-access-backend/frontend

--- common/config/rush/.pnpmfile.cjs
+++ common/config/rush/.pnpmfile.cjs
@@ -1,6 +1,21 @@
 function readPackage(pkg) {
   if ((pkg.name == "typedoc" || pkg.name == "@microsoft/api-extractor") && pkg.dependencies && pkg.dependencies["typescript"])
     pkg.dependencies["typescript"] = "~4.4.0";
+  else if (pkg.name == "@itwin/imodels-access-backend") {
+    // Hacky mess: Both @itwin/imodels-access-* packages have peer dependencies on the packages
+    // in this repo so need to do some magic in order to get the peerDeps to point to a correct
+    // version of the packages. Update the pkg.json real dependency list to
+    // Note that these dependencies are only ever allowed for testing purposes and should not be the
+    // dependency of any published packages.
+    pkg.dependencies["@itwin/core-bentley"] = "workspace:*";
+    pkg.dependencies["@itwin/core-backend"] = "workspace:*";
+    pkg.dependencies["@itwin/core-common"] = "workspace:*";
+  }
+  else if (pkg.name == "@itwin/imodels-access-frontend") {
+    pkg.dependencies["@itwin/core-bentley"] = "workspace:*";
+    pkg.dependencies["@itwin/core-frontend"] = "workspace:*";
+    pkg.dependencies["@itwin/core-common"] = "workspace:*";
+  }
   return pkg;
 }
 
