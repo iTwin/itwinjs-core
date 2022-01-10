7c79e0e08e6f1f605587ca82803155d8fffc90d0
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2022-01-10 16:36:35 -0500

    add tests for isProperSubclassOf

---
+++ core/bentley/src/test/ClassUtils.test.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+import { ClassUtils } from "../core-bentley";
+
+describe("ClassUtils", () => {
+  it("isProperSubclassOf", () => {
+    class A {}
+    class B extends A {}
+    class C extends B {}
+
+    expect(ClassUtils.isProperSubclassOf(A, A)).to.be.false;
+    expect(ClassUtils.isProperSubclassOf(A, B)).to.be.false;
+    expect(ClassUtils.isProperSubclassOf(A, C)).to.be.false;
+
+    expect(ClassUtils.isProperSubclassOf(B, A)).to.be.true;
+    expect(ClassUtils.isProperSubclassOf(B, B)).to.be.false;
+    expect(ClassUtils.isProperSubclassOf(B, C)).to.be.false;
+
+    expect(ClassUtils.isProperSubclassOf(C, A)).to.be.true;
+    expect(ClassUtils.isProperSubclassOf(C, B)).to.be.true;
+    expect(ClassUtils.isProperSubclassOf(C, C)).to.be.false;
+  });
+});
