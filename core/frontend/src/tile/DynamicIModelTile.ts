01b25a7b8939d3c332222a4754fc00f7ff093436
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-01-20 08:03:16 -0500

    Generalize the concept of a "tile user" (#3070)
    * TileUser
    
    * TileUser namespace.
    
    * Viewport implements TileUser.
    
    * LRUTileList
    
    * TileAdmin, TileUsageMarker
    
    * clean up usage of viewport where TileUser is indicated, and various non-uses.
    
    * Reinstate+deprecate TileRequest.viewports; extract-api
    
    * Reinstate
    
    * extract-api
    
    * remove TODO
    
    * lint
    
    * adjust for API changes.
    
    * no deprecation.
    
    * extract-api
    
    * build code plz
    
    * dpta
    
    * fix doc link.

--- core/frontend/src/tile/DynamicIModelTile.ts
+++ core/frontend/src/tile/DynamicIModelTile.ts
@@ -14,7 +14,6 @@ import {
   BatchType, ElementGeometryChange, ElementGraphicsRequestProps, FeatureAppearance, FeatureAppearanceProvider, FeatureAppearanceSource, GeometryClass, TileFormat,
 } from "@itwin/core-common";
 import { RenderSystem } from "../render/RenderSystem";
-import { Viewport } from "../Viewport";
 import { IModelApp } from "../IModelApp";
 import {
   ImdlReader, IModelTileTree, RootIModelTile, Tile, TileContent, TileDrawArgs, TileParams, TileRequest, TileRequestChannel, TileTree,
@@ -328,7 +327,7 @@ class GraphicsTile extends Tile {
     this.tolerance = 10 ** toleranceLog10;
   }
 
-  public override computeLoadPriority(_viewports: Iterable<Viewport>): number {
+  public override computeLoadPriority(): number {
     // We want the element's graphics to be updated as soon as possible
     return 0;
   }
