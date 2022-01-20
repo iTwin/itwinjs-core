02eafbee5124a2ba18ba8d1031aa69d61d34adc4
Arun George <aruniverse@users.noreply.github.com>
2022-01-19 21:19:16 -0500

    release as public

--- core/common/src/Request.ts
+++ core/common/src/Request.ts
@@ -2,10 +2,13 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+
+/** @@public */
 export interface ProgressInfo {
   percent?: number;
   total?: number;
   loaded: number;
 }
 
+/** @@public */
 export type ProgressCallback = (progress: ProgressInfo) => void;
