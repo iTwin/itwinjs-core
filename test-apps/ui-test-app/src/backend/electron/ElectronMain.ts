0f570670633352d95d0bf5444cea62890653cc17
Patrick Bell <20171969+pbell97@users.noreply.github.com>
2022-01-21 07:30:24 -0600

    Add silent sign in for electron (#2936)
    Added silent signin for electron

--- test-apps/ui-test-app/src/backend/electron/ElectronMain.ts
+++ test-apps/ui-test-app/src/backend/electron/ElectronMain.ts
@@ -28,7 +28,7 @@ export async function initializeElectron(opts?: IModelHostConfiguration) {
     iModelHost: opts,
   };
 
-  const authClient = await ElectronMainAuthorization.create({
+  const authClient = new ElectronMainAuthorization({
     clientId: process.env.IMJS_OIDC_ELECTRON_TEST_CLIENT_ID ?? "",
     redirectUri: process.env.IMJS_OIDC_ELECTRON_TEST_REDIRECT_URI ?? "",
     scope: process.env.IMJS_OIDC_ELECTRON_TEST_SCOPES ?? "",
@@ -37,6 +37,7 @@ export async function initializeElectron(opts?: IModelHostConfiguration) {
     opt.iModelHost.authorizationClient = authClient;
 
   await ElectronHost.startup(opt);
+  await authClient.signInSilent();
   EditCommandAdmin.registerModule(editorBuiltInCommands);
 
   // Handle custom keyboard shortcuts
