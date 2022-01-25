431aa40620f167099427baa7e50f6079eeaf7e83
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-01-25 12:51:08 -0500

    Revert changes to tsconfig and test webpack config.

--- core/frontend/src/test/utils/webpack.config.js
+++ core/frontend/src/test/utils/webpack.config.js
@@ -55,10 +55,6 @@ function createConfig(shouldInstrument) {
             env[key] = JSON.stringify(process.env[key]);
             return env;
           }, {}),
-      }),
-      // Disable code-splitting for tests - see dynamic import in ApproximateTerrainHeights.ts.
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1
       })
     ]
   };
