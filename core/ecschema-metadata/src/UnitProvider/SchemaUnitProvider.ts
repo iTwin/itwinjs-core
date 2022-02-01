8ccd6e491a1ada9ec964dc8f53e3e5143df7d9c1
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2022-02-01 15:54:49 -0500

    fix dependency errors

--- core/ecschema-metadata/src/UnitProvider/SchemaUnitProvider.ts
+++ core/ecschema-metadata/src/UnitProvider/SchemaUnitProvider.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { BentleyError, BentleyStatus } from "@bentley/bentleyjs-core";
+import { BentleyError, BentleyStatus } from "@itwin/core-bentley";
 import { UnitConversion, UnitExtraData, UnitProps, UnitsProvider } from "@itwin/core-quantity";
 import { SchemaContext } from "../Context";
 import { SchemaItem } from "../Metadata/SchemaItem";
