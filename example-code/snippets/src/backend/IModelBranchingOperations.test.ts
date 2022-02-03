ce85879e3e3b8fc4ed8dd784f117258008325bba
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2022-02-03 09:28:11 -0500

    add arbitrary editing to make synchronization non-trivial in IModelBranchingOperations

--- example-code/snippets/src/backend/IModelBranchingOperations.test.ts
+++ example-code/snippets/src/backend/IModelBranchingOperations.test.ts
@@ -3,13 +3,17 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { BriefcaseDb, BriefcaseManager, ExternalSource, ExternalSourceIsInRepository, IModelDb, IModelHost, RepositoryLink } from "@itwin/core-backend";
+import { BriefcaseDb, BriefcaseManager, ExternalSource, ExternalSourceIsInRepository, IModelDb, IModelHost, PhysicalModel, PhysicalObject, PhysicalPartition, RepositoryLink, SpatialCategory } from "@itwin/core-backend";
 import * as path from "path";
 import { AccessToken } from "@itwin/core-bentley";
-import { Code } from "@itwin/core-common";
+import { Code, IModel, PhysicalElementProps, SubCategoryAppearance } from "@itwin/core-common";
 import { IModelTransformer } from "@itwin/core-transformer";
 import { HubMock } from "@itwin/core-backend/lib/cjs/test/HubMock";
-import { HubWrappers, TestUserType } from "@itwin/core-backend/lib/cjs/test/IModelTestUtils";
+import { HubWrappers, IModelTestUtils, TestUserType } from "@itwin/core-backend/lib/cjs/test/IModelTestUtils";
+import { Point3d, YawPitchRollAngles } from "@itwin/core-geometry";
+
+// some json will be required later, but we don't want an eslint-disable line in the example code, so just disable for the file
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 async function initializeBranch(myITwinId: string, masterIModelId: string, myAccessToken: AccessToken) {
   // __PUBLISH_EXTRACT_START__ IModelBranchingOperations_initialize
@@ -128,8 +132,38 @@ async function reverseSyncBranchToMaster(branchDb: BriefcaseDb, masterDb: Briefc
   // __PUBLISH_EXTRACT_END__
 }
 
-async function arbitraryEdit(_db: BriefcaseDb) {
+async function arbitraryEdit(db: BriefcaseDb, myAccessToken: AccessToken, description: string) {
+  const spatialCategoryCode = SpatialCategory.createCode(db, IModel.dictionaryId, "SpatialCategory1");
+  const physicalModelCode = PhysicalPartition.createCode(db, IModel.rootSubjectId, "PhysicalModel1");
+  let spatialCategoryId = db.elements.queryElementIdByCode(spatialCategoryCode);
+  let physicalModelId = db.elements.queryElementIdByCode(physicalModelCode);
+  if (physicalModelId === undefined || spatialCategoryId === undefined) {
+    spatialCategoryId = SpatialCategory.insert(db, IModel.dictionaryId, "SpatialCategory1", new SubCategoryAppearance());
+    physicalModelId = PhysicalModel.insert(db, IModel.rootSubjectId, "PhysicalModel1");
+  }
+  const physicalObjectProps: PhysicalElementProps = {
+    classFullName: PhysicalObject.classFullName,
+    model: physicalModelId,
+    category: spatialCategoryId,
+    code: new Code({ spec: IModelDb.rootSubjectId, scope: IModelDb.rootSubjectId, value: `${arbitraryEdit.editCounter}` }),
+    userLabel: `${arbitraryEdit.editCounter}`,
+    geom: IModelTestUtils.createBox(Point3d.create(1, 1, 1)),
+    placement: {
+      origin: Point3d.create(arbitraryEdit.editCounter, arbitraryEdit.editCounter, 0),
+      angles: YawPitchRollAngles.createDegrees(0, 0, 0),
+    },
+  };
+  arbitraryEdit.editCounter++;
+  db.elements.insertElement(physicalObjectProps);
+  await db.pushChanges({
+    accessToken: myAccessToken,
+    description,
+  });
+}
 
+namespace arbitraryEdit {
+  // eslint-disable-next-line prefer-const
+  export let editCounter = 0;
 }
 
 describe("IModelBranchingOperations", () => {
@@ -146,9 +180,9 @@ describe("IModelBranchingOperations", () => {
       version0: path.join(__dirname, "assets", "test.bim"),
     });
     const { masterDb, branchDb } = await initializeBranch(myITwinId, masterIModelId, myAccessToken);
-    await arbitraryEdit(masterDb);
+    await arbitraryEdit(masterDb, myAccessToken, "edit master");
     await forwardSyncMasterToBranch(masterDb, branchDb, myAccessToken);
-    await arbitraryEdit(branchDb);
+    await arbitraryEdit(branchDb, myAccessToken, "edit branch");
     await reverseSyncBranchToMaster(branchDb, masterDb, myAccessToken);
   });
 });
