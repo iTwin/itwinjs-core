3837193e361bcfa7a9c38e660860cb2467da7f60
Bill Steinbock <65047615+bsteinbk@users.noreply.github.com>
2022-02-17 11:48:40 -0500

    Promote quantity formatter to public (#3206)
    * Promote QuantityFormatter to public
    
    * update to mention caching of specs
    
    * Update documentation
    
    * More doc updates and promoting QuantityFormatter to public.
    
    * Update Document
    
    * Suggested edits toi quantity docs. (#3207)
    
    * Add nextversion entry for SchemaUnitProvider
    
    * Update NextVersion
    
    * Add missing doc from public methods of quantityformatter.
    
    * Add doc for all protected entries in QuantityFormatter
    
    * rush change
    
    * Add documentation for each QuantityType enum entry.
    
    * Add documentation for a couple more interfaces used in quantity formatting.
    
    Co-authored-by: NancyMcCallB <45079789+NancyMcCallB@users.noreply.github.com>

---
+++ core/frontend/src/quantity-formatting/QuantityTypesEditorSpecs.ts
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module QuantityFormatting
+ */
+
+import {
+  FormatProps,
+} from "@itwin/core-quantity";
+
+/** Properties that define an EditorSpec for editing a custom formatting property that is stored in the "custom" property in the FormatProps.
+ * The editor controls will be automatically generated in the UI and are limited to a checkbox to set a boolean value, a text dropdown/select
+ * component to pick a string value from a list of options, and a text input component that returns a string value.
+ * @public
+ */
+export interface CustomFormatPropEditorSpec {
+  editorType: "checkbox" | "text" | "select";
+  label: string;
+}
+
+/** CheckboxFormatPropEditorSpec defines getter and setter method for a boolean property editor.
+ * @public
+ */
+export interface CheckboxFormatPropEditorSpec extends CustomFormatPropEditorSpec {
+  editorType: "checkbox";
+  getBool: (props: FormatProps) => boolean;
+  setBool: (props: FormatProps, isChecked: boolean) => FormatProps;
+}
+
+/** CheckboxFormatPropEditorSpec type guard.
+ * @public
+ */
+export const isCheckboxFormatPropEditorSpec = (item: CustomFormatPropEditorSpec): item is CheckboxFormatPropEditorSpec => {
+  return item.editorType === "checkbox";
+};
+
+/** TextInputFormatPropEditorSpec defines getter and setter method for a text input property editor.
+ * @public
+ */
+export interface TextInputFormatPropEditorSpec extends CustomFormatPropEditorSpec {
+  editorType: "text";
+  getString: (props: FormatProps) => string;
+  setString: (props: FormatProps, value: string) => FormatProps;
+}
+
+/** TextInputFormatPropEditorSpec type guard.
+ * @public
+ */
+export const isTextInputFormatPropEditorSpec = (item: CustomFormatPropEditorSpec): item is TextInputFormatPropEditorSpec => {
+  return item.editorType === "text";
+};
+
+/** TextSelectFormatPropEditorSpec defines getter and setter method for a Select/Dropdown property editor.
+ * @public
+ */
+export interface TextSelectFormatPropEditorSpec extends CustomFormatPropEditorSpec {
+  editorType: "select";
+  selectOptions: { label: string, value: string }[];
+  getString: (props: FormatProps) => string;
+  setString: (props: FormatProps, value: string) => FormatProps;
+}
+
+/** TextSelectFormatPropEditorSpec type guard.
+ * @public
+ */
+export const isTextSelectFormatPropEditorSpec = (item: CustomFormatPropEditorSpec): item is TextSelectFormatPropEditorSpec => {
+  return item.editorType === "select";
+};
