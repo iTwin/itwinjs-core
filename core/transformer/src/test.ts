0cf9afc5c07a3e71aceb4b141cf0d8bf3633a224
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2022-02-19 20:55:49 -0500

    separate getInstanceProps loop

--- core/transformer/src/test.ts
+++ core/transformer/src/test.ts
@@ -1,9 +1,9 @@
+/* eslint-disable prefer-template */
 /*---------------------------------------------------------------------------------------------
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { ElementRefersToElements, IModelDb, IModelHost, SnapshotDb } from "@itwin/core-backend";
-import * as fs from "fs";
 
 /* eslint-disable no-console */
 
@@ -12,17 +12,40 @@ const formatter = new Intl.NumberFormat("en-US", {
   maximumFractionDigits: 2,
 });
 
-const COUNT = 1_000_000;
+const COUNT = 100_000;
+
+function reportMemUsage(i: number) {
+  if (i % 10000 !== 0) return;
+  const memUsageGb = process.memoryUsage().rss / 1024 / 1024 / 1024;
+  const memUsageGbFmt = formatter.format(memUsageGb) + "GB";
+  console.log(String(i) + " iterations, process memory at " + memUsageGbFmt);
+}
 
 async function rels(sourceDb: IModelDb) {
   const sql = `SELECT ECInstanceId FROM ${ElementRefersToElements.classFullName}`;
   let i = 0;
-  for await (const [relInstanceId] of sourceDb.query(sql)) {
-    sourceDb.relationships.getInstanceProps(ElementRefersToElements.classFullName, relInstanceId);
-    const memUsageGb = process.memoryUsage().rss / 1024 / 1024 / 1024;
-    if (i % 10000 === 0) console.log(`${i} iterations, process memory at ${formatter.format(memUsageGb)}GB`);
+
+  const iter = sourceDb.query(sql);
+  let iterVal = await iter.next();
+  const ids = [];
+
+  // for await (const [relInstanceId] of sourceDb.query(sql)) {
+  while (!iterVal.done && i <= COUNT) {
+    ids.push(iterVal.value[0]);
+    reportMemUsage(i);
     ++i;
-    if (i >= COUNT) break;
+    iterVal = await iter.next();
+  }
+
+  console.log("finished id query loop, starting gets loop");
+
+  i = 0;
+  for (const relInstanceId of ids) {
+    sourceDb.relationships.getInstanceProps(ElementRefersToElements.classFullName, relInstanceId);
+    reportMemUsage(i);
+    ++i;
+    // it actually garbage collects with this:
+    // await new Promise(setImmediate);
   }
 }
 
