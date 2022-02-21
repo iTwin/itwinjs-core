49fed8947ddcc367c6f30d36f862c477090ffe20
Grigas <35135765+grigasp@users.noreply.github.com>
2022-02-21 08:46:56 +0200

    Remove unused `chai-string` dependency

--- full-stack-tests/presentation/scripts/setup-tests.js
+++ full-stack-tests/presentation/scripts/setup-tests.js
@@ -6,7 +6,6 @@ const chai = require("chai");
 const faker = require("faker");
 const chaiJestSnapshot = require("chai-jest-snapshot");
 const chaiAsPromised = require("chai-as-promised");
-const chaiString = require("chai-string");
 const jsdom = require("jsdom");
 
 console.log(`Backend PID: ${process.pid}`);
@@ -25,7 +24,6 @@ m._load = (request, parent, isMain) => {
 
 chai.use(chaiJestSnapshot);
 chai.use(chaiAsPromised);
-chai.use(chaiString);
 
 faker.seed(1);
 
