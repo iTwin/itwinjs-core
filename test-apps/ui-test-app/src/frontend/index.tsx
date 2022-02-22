85fc3359577c7f709d75751f39a53a5abae93b30
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2022-02-22 11:02:02 -0500

    simplify SchemaUnitProvider implementation (#3242)
    * simplify SchemaUnitProvider implementation
    
    * change log and api extraction
    
    * remove or since a context is a locater
    
    * add log message
    
    * update logger category
    
    * another api extraction attempt
    
    * lint fixes

--- test-apps/ui-test-app/src/frontend/index.tsx
+++ test-apps/ui-test-app/src/frontend/index.tsx
@@ -35,7 +35,7 @@ import { EditTools } from "@itwin/editor-frontend";
 import { FrontendDevTools } from "@itwin/frontend-devtools";
 import { HyperModeling } from "@itwin/hypermodeling-frontend";
 import { MapLayersUI } from "@itwin/map-layers";
-import { SchemaContext, SchemaUnitProvider } from "@itwin/ecschema-metadata";
+import { SchemaUnitProvider } from "@itwin/ecschema-metadata";
 import { createFavoritePropertiesStorage, DefaultFavoritePropertiesStorageTypes, Presentation } from "@itwin/presentation-frontend";
 import { IModelsClient } from "@itwin/imodels-client-management";
 import { AccessTokenAdapter, FrontendIModelsAccess } from "@itwin/imodels-access-frontend";
@@ -392,15 +392,9 @@ export class SampleAppIModelApp {
     let stageId: string;
     const defaultFrontstage = this.allowWrite ? EditFrontstage.stageId : ViewsFrontstage.stageId;
 
-    try{
     // Reset QuantityFormatter UnitsProvider with new iModelConnection
-      const schemaLocater = new ECSchemaRpcLocater(iModelConnection);
-      const context = new SchemaContext();
-      context.addLocater(schemaLocater);
-      await IModelApp.quantityFormatter.setUnitsProvider (new SchemaUnitProvider(context));
-    } catch (_) {
-      await IModelApp.quantityFormatter.resetToUseInternalUnitsProvider(); // this resets it to internal BasicUnitsProvider
-    }
+    const schemaLocater = new ECSchemaRpcLocater(iModelConnection);
+    await IModelApp.quantityFormatter.setUnitsProvider (new SchemaUnitProvider(schemaLocater));
 
     // store the IModelConnection in the sample app store - this may trigger redux connected components
     UiFramework.setIModelConnection(iModelConnection, true);
