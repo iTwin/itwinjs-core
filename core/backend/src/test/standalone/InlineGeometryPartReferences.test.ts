e5c1ca2bf05ec8a665b1fa38a3731a7e2d3ffe2d
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-02-23 12:38:35 -0500

    wip tests.

--- core/backend/src/test/standalone/InlineGeometryPartReferences.test.ts
+++ core/backend/src/test/standalone/InlineGeometryPartReferences.test.ts
@@ -4,9 +4,100 @@
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
 import { Guid, Id64 } from "@itwin/core-bentley";
-import { ColorDef, IModel } from "@itwin/core-common";
-import { GenericSchema, PhysicalModel, PhysicalPartition, SnapshotDb, SpatialCategory, SubCategory, SubjectOwnsPartitionElements } from "../../core-backend";
+import { CoordinateXYZ, Point3d, Range3dProps } from "@itwin/core-geometry";
+import {
+  ColorDef, GeometryParams, GeometryStreamBuilder, GeometryStreamIterator, IModel,
+} from "@itwin/core-common";
+import {
+  GenericSchema, PhysicalModel, PhysicalPartition, RenderMaterialElement, SnapshotDb, SpatialCategory, SubCategory, SubjectOwnsPartitionElements,
+} from "../../core-backend";
 import { IModelTestUtils } from "../IModelTestUtils";
+
+interface Point { x: number, y: number, z: number };
+
+interface PartRef {
+  partId: string;
+  origin?: Point;
+}
+
+interface Symbology {
+  categoryId?: string;
+  subCategoryId?: string;
+  color?: ColorDef;
+  materialId?: string;
+}
+
+function makeGeomParams(symb: Symbology): GeometryParams {
+  const params = new GeometryParams(symb.categoryId ?? Id64.invalid, symb.subCategoryId);
+  params.fillColor = params.lineColor = symb.color;
+  params.materialId = symb.materialId;
+  return params;
+}
+
+type UnionMember<T, U> = T & { [k in keyof U]?: never };
+
+type GeomWriterEntry =
+  UnionMember<PartRef, Symbology & Point> |
+  UnionMember<Symbology, PartRef & Point> |
+  UnionMember<Point, PartRef & Symbology>;
+
+class GeomWriter {
+  public readonly builder: GeometryStreamBuilder;
+
+  public constructor(symbology?: Symbology) {
+    this.builder = new GeometryStreamBuilder();
+    if (symbology)
+      this.append(symbology);
+  }
+
+  public append(entry: GeomWriterEntry): void {
+    if (entry.partId)
+      this.builder.appendGeometryPart3d(entry.partId, Point3d.fromJSON(entry.origin));
+    else if (entry.subCategoryId || entry.categoryId || entry.color || entry.materialId)
+      this.builder.appendGeometryParamsChange(makeGeomParams(entry));
+    else if (undefined !== entry.x)
+      this.builder.appendGeometry(CoordinateXYZ.createXYZ(entry.x, entry.y, entry.z));
+  }
+}
+
+interface SubRange { lo: Point, hi: Point };
+
+type GeomStreamEntry =
+  "header" |
+  UnionMember<PartRef, Symbology & SubRange & Point> |
+  UnionMember<Symbology, PartRef & SubRange & Point> |
+  UnionMember<SubRange, PartRef & Symbology & Point> |
+  UnionMember<Point, PartRef & Symbology & SubRange>;
+
+function readGeomStream(iter: GeometryStreamIterator): GeomStreamEntry[] {
+  const result: GeomStreamEntry[] = [];
+  for (const entry of iter) {
+    result.push({
+      categoryId: entry.geomParams.categoryId,
+      subCategoryId: entry.geomParams.subCategoryId,
+      color: entry.geomParams.fillColor,
+      materialId: entry.geomParams.materialId,
+    });
+
+    if (entry.localRange) {
+      result.push({
+        lo: { x: entry.localRange.low.x, y: entry.localRange.low.y, z: entry.localRange.low.z },
+        hi: { x: entry.localRange.high.x, y: entry.localRange.high.y, z: entry.localRange.high.z },
+      });
+    }
+
+    expect(entry.primitive.type).to.equal("geometryQuery");
+    if (entry.primitive.type === "geometryQuery") {
+      expect(entry.primitive.geometry.geometryCategory).to.equal("point");
+      if (entry.primitive.geometry.geometryCategory === "point") {
+        const pt = entry.primitive.geometry.point;
+        result.push({ x: pt.x, y: pt.y, z: pt.z });
+      }
+    }
+  }
+
+  return result;
+}
 
 describe.only("DgnDb.inlineGeometryPartReferences", () => {
   let imodel: SnapshotDb;
@@ -46,7 +137,7 @@ describe.only("DgnDb.inlineGeometryPartReferences", () => {
     redSubCategoryId = SubCategory.insert(imodel, categoryId, "red", { color: ColorDef.red.toJSON() });
     expect(Id64.isValidId64(redSubCategoryId)).to.be.true;
 
-    materialId = RenderMaterialElement.insert(imodel, IModel.dictionaryId, "mat", { paletteName: "pal", color: ColorDef.black });
+    materialId = RenderMaterialElement.insert(imodel, IModel.dictionaryId, "mat", { paletteName: "pal" });
     expect(Id64.isValidId64(materialId)).to.be.true;
   });
 
