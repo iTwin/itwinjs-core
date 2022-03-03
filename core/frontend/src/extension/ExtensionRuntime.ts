d93debbc59d95965625d880997e751e33d592fac
kckst8 <6283674+kckst8@users.noreply.github.com>
2022-03-03 11:07:41 -0500

    fix find/replace mishap

--- core/frontend/src/extension/ExtensionRuntime.ts
+++ core/frontend/src/extension/ExtensionRuntime.ts
@@ -59,7 +59,7 @@ const globalSymbol = Symbol.for("itwin.core.frontend.globals");
 if ((globalThis as any)[globalSymbol])
   throw new Error("Multiple @itwin/core-frontend imports detected!");
 
-const getextensions = (id: string) => {
+const getExtensionApi = (id: string) => {
   return {
     exports: {
       // Tools
@@ -99,5 +99,5 @@ const getextensions = (id: string) => {
 };
 
 (globalThis as any)[globalSymbol] = {
-  getextensions,
+  getExtensionApi,
 };
