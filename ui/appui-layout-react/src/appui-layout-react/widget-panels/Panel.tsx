235c645152a008996ed43767a9bd2f8fb92333ce
bsteinbk <65047615+bsteinbk@users.noreply.github.com>
2022-03-07 19:00:22 -0500

    remove unused hook

--- ui/appui-layout-react/src/appui-layout-react/widget-panels/Panel.tsx
+++ ui/appui-layout-react/src/appui-layout-react/widget-panels/Panel.tsx
@@ -21,38 +21,6 @@ import { RectangleProps, SizeProps, WebFontIcon } from "@itwin/core-react";
 import { assert } from "@itwin/core-bentley";
 import { WidgetComponent } from "../widget/Widget";
 
-function useLayoutEventListener(
-  eventName: string,
-  handler: (event: Event) => void,
-  element: HTMLElement | Document | undefined
-) {
-  // Based on published hook https://usehooks.com/useEventListener/.
-  const savedHandler = React.useRef<(event: Event) => void>();
-
-  // Update reference if handler changes. This allows our effect below to
-  // always use latest handler without us needing to pass it in effect deps array
-  // and potentially cause effect to re-run every render.
-  React.useLayoutEffect(() => {
-    savedHandler.current = handler;
-  }, [handler]);
-
-  React.useLayoutEffect(() => {
-    if (!element) {
-      return;
-    }
-
-    const eventListener = (event: Event) => {
-      // istanbul ignore else
-      if (savedHandler.current)
-        savedHandler.current(event);
-    };
-    element.addEventListener(eventName, eventListener);
-    return () => {
-      element.removeEventListener(eventName, eventListener);
-    };
-  }, [eventName, element]);
-}
-
 /** @internal */
 export type TopPanelSide = "top";
 
