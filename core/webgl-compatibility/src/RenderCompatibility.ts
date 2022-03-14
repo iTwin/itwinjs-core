27497d175eb540a5156d9f42ba11542344c4eb6c
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2022-03-14 08:18:13 -0400

    comment

--- core/webgl-compatibility/src/RenderCompatibility.ts
+++ core/webgl-compatibility/src/RenderCompatibility.ts
@@ -109,7 +109,7 @@ export interface WebGLRenderCompatibilityInfo {
   unmaskedRenderer?: string;
   /** The vendor string reported by this client's graphics driver. */
   unmaskedVendor?: string;
-  /** If true, there is a likelihood that integrated graphics are being used.
+  /** If true, there is a likelihood that integrated graphics are being used. This can be used to warn users on systems with both integrated graphics and dedicated graphics that they should try to switch to their dedicated graphics for better performance.
    * @note This property has the possibility of providing false positives and negatives. A user should use this property mainly as a hint and manually verify what graphics chip is being used.
    * @beta
    */
