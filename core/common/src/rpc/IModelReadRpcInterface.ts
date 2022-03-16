78bcbe75f7b2db5d5453b58cf5822e813e1f85c0
MindaugasButkus <87982008+MindaugasButkus@users.noreply.github.com>
2022-03-16 13:51:54 +0200

    IModelReadRpcInterface version bump

--- core/common/src/rpc/IModelReadRpcInterface.ts
+++ core/common/src/rpc/IModelReadRpcInterface.ts
@@ -53,7 +53,7 @@ export abstract class IModelReadRpcInterface extends RpcInterface {
   public static readonly interfaceName = "IModelReadRpcInterface";
 
   /** The semantic version of the interface. */
-  public static interfaceVersion = "3.0.0";
+  public static interfaceVersion = "3.1.0";
 
   /*===========================================================================================
     NOTE: Any add/remove/change to the methods below requires an update of the interface version.
