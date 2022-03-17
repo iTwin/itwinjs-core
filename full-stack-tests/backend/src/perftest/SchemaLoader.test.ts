d4e5a005efed79e68b032551990142ee6ab47ba2
AKSHAY.NANGARE <akshaynangare@users.noreply.github.com>
2022-03-17 15:38:29 +0530

    Add tests for BisSchemas

--- full-stack-tests/backend/src/perftest/SchemaLoader.test.ts
+++ full-stack-tests/backend/src/perftest/SchemaLoader.test.ts
@@ -4,21 +4,46 @@
 *--------------------------------------------------------------------------------------------*/
 import * as path from "path";
 import { Reporter } from "@itwin/perf-tools";
-import { IModelJsFs, KnownLocations, StandaloneDb } from "@itwin/core-backend";
+import { IModelJsFs, IModelJsNative, KnownLocations, StandaloneDb } from "@itwin/core-backend";
 import { IModelTestUtils, KnownTestLocations } from "@itwin/core-backend/lib/cjs/test";
+import * as fs from "fs";
 
 describe("SchemaLoaderPerformance", () => {
+  let iModelDb: StandaloneDb;
   const outDir: string = path.join(KnownTestLocations.outputDir, "SchemaLoaderPerformance");
+  const assetDir: string = path.join(__dirname, "..", "..", "..", "assets");
   const reporter = new Reporter();
 
-  before(() => {
+  before(async () => {
     if (!IModelJsFs.existsSync(KnownTestLocations.outputDir))
       IModelJsFs.mkdirSync(KnownTestLocations.outputDir);
     if (!IModelJsFs.existsSync(outDir))
       IModelJsFs.mkdirSync(outDir);
+
+    if (!IModelJsFs.existsSync(assetDir))
+      IModelJsFs.mkdirSync(assetDir);
+
+    copyBisSchemasToAssetsDir();
+
+    const rootSubject = { name: "Performance tests", description: "Performance tests" };
+    const snapshotFile: string = IModelTestUtils.prepareOutputFile("Performance", "Performance.bim");
+    iModelDb = StandaloneDb.createEmpty(snapshotFile, { rootSubject });
+
+    const bisSchemaPaths = getBisSchemaPaths();
+
+    try {
+      await iModelDb.importSchemas(bisSchemaPaths);   // will throw an exception if import fails
+    } catch (error) {
+      throw new Error(`Failed to import schemas`);
+    }
+
+    iModelDb.saveChanges();
+    iModelDb.clearCaches();
   });
 
   after(() => {
+    iModelDb.close();
+
     const csvPath = path.join(outDir, "SchemaLoaderPerfResults.csv");
     reporter.exportCSV(csvPath);
   });
@@ -32,21 +57,65 @@ describe("SchemaLoaderPerformance", () => {
     return schemaFile;
   }
 
-  async function timeBisSchemasLoading(schemaName: string, schemaPath: string): Promise<void> {
-    const rootSubject = { name: "Performance tests", description: "Performance tests" };
-    const snapshotFile: string = IModelTestUtils.prepareOutputFile("Performance", "Performance.bim");
-    const imodel: StandaloneDb = StandaloneDb.createEmpty(snapshotFile, { rootSubject });
+  function getBisSchemaPaths(): string[] {
+    const bisSchemaPaths: string[] = [];
 
-    try {
-      await imodel.importSchemas([schemaPath]);   // will throw an exception if import fails
-    } catch (error) {
-      throw new Error(`Failed to import schema ${schemaPath}`);
-    }
+    bisSchemaPaths.push(path.join(assetDir, "Units.01.00.07.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "Formats.01.00.00.ecschema.xml"));
 
-    imodel.saveChanges();
+    bisSchemaPaths.push(path.join(assetDir, "BisCore.ecschema.xml"));
 
+    bisSchemaPaths.push(path.join(assetDir, "ProcessFunctional.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "ProcessPhysical.ecschema.xml"));
+
+    bisSchemaPaths.push(path.join(assetDir, "CifBridge.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifCommon.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifGeometricRules.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifHydraulicAnalysis.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifHydraulicResults.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifQuantityTakeoffs.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifRail.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifRoads.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifSubsurface.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifSubsurfaceConflictAnalysis.ecschema.xml"));
+    bisSchemaPaths.push(path.join(assetDir, "CifUnits.ecschema.xml"));
+
+    return bisSchemaPaths;
+  }
+
+  function copyBisSchemasToAssetsDir() {
+    // Copy BisSchemas that we need for performance testing
+    fs.copyFileSync(getSchemaPath("Standard", "Units.01.00.07.ecschema.xml"), path.join(assetDir, "Units.01.00.07.ecschema.xml"));
+    fs.copyFileSync(getSchemaPath("Standard", "Formats.01.00.00.ecschema.xml"), path.join(assetDir, "Formats.01.00.00.ecschema.xml"));
+
+    fs.copyFileSync(getSchemaPath("Dgn", "BisCore.ecschema.xml"), path.join(assetDir, "BisCore.ecschema.xml"));
+
+    fs.copyFileSync(getSchemaPathFromPackage("process-functional-schema", "ProcessFunctional.ecschema.xml"), path.join(assetDir, "ProcessFunctional.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("process-physical-schema", "ProcessPhysical.ecschema.xml"), path.join(assetDir, "ProcessPhysical.ecschema.xml"));
+
+    fs.copyFileSync(getSchemaPathFromPackage("cif-bridge-schema", "CifBridge.ecschema.xml"), path.join(assetDir, "CifBridge.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-common-schema", "CifCommon.ecschema.xml"), path.join(assetDir, "CifCommon.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-geometric-rules-schema", "CifGeometricRules.ecschema.xml"), path.join(assetDir, "CifGeometricRules.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-hydraulic-analysis-schema", "CifHydraulicAnalysis.ecschema.xml"), path.join(assetDir, "CifHydraulicAnalysis.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-hydraulic-results-schema", "CifHydraulicResults.ecschema.xml"), path.join(assetDir, "CifHydraulicResults.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-quantity-takeoffs-schema", "CifQuantityTakeoffs.ecschema.xml"), path.join(assetDir, "CifQuantityTakeoffs.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-rail-schema", "CifRail.ecschema.xml"), path.join(assetDir, "CifRail.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-roads-schema", "CifRoads.ecschema.xml"), path.join(assetDir, "CifRoads.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-subsurface-schema", "CifSubsurface.ecschema.xml"), path.join(assetDir, "CifSubsurface.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-subsurface-conflict-analysis-schema", "CifSubsurfaceConflictAnalysis.ecschema.xml"), path.join(assetDir, "CifSubsurfaceConflictAnalysis.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("cif-units-schema", "CifUnits.ecschema.xml"), path.join(assetDir, "CifUnits.ecschema.xml"));
+
+    // Copy the required reference schemas
+    fs.copyFileSync(getSchemaPath("Domain", "Functional.ecschema.xml"), path.join(assetDir, "Functional.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("plant-custom-attributes-schema", "PlantCustomAttributes.ecschema.xml"), path.join(assetDir, "PlantCustomAttributes.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("quantity-takeoffs-aspects-schema", "QuantityTakeoffsAspects.ecschema.xml"), path.join(assetDir, "QuantityTakeoffsAspects.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("aec-units-schema", "AecUnits.ecschema.xml"), path.join(assetDir, "AecUnits.ecschema.xml"));
+    fs.copyFileSync(getSchemaPathFromPackage("road-rail-units-schema", "RoadRailUnits.ecschema.xml"), path.join(assetDir, "RoadRailUnits.ecschema.xml"));
+  }
+
+  function timeBisSchemasLoading(schemaName: string) {
     const startTime: number = new Date().getTime();
-    const schemaResult = imodel.nativeDb.getSchema(schemaName);
+    const schemaResult: IModelJsNative.ErrorStatusOrResult<any, any> = iModelDb.nativeDb.getSchema(schemaName);
     const endTime: number = new Date().getTime();
 
     if (schemaResult.error !== undefined) {
@@ -58,25 +127,13 @@ describe("SchemaLoaderPerformance", () => {
     }
 
     const elapsedTime = endTime - startTime;
-    reporter.addEntry("SchemaLoaderPerfTest", `Loading schema: ${schemaName}`, "Execution time(ms)", elapsedTime, {});
-    imodel.close();
+    reporter.addEntry("SchemaLoaderPerfTest", `Get schema from imodel: ${schemaName}`, "Execution time(ms)", elapsedTime, {});
+    iModelDb.clearCaches();
   }
 
-  async function memoryBisSchemasLoading(schemaName: string, schemaPath: string): Promise<void> {
-    const rootSubject = { name: "Performance tests", description: "Performance tests" };
-    const snapshotFile: string = IModelTestUtils.prepareOutputFile("Performance", "Performance.bim");
-    const imodel: StandaloneDb = StandaloneDb.createEmpty(snapshotFile, { rootSubject });
-
-    try {
-      await imodel.importSchemas([schemaPath]);   // will throw an exception if import fails
-    } catch (error) {
-      throw new Error(`Failed to import schema ${schemaPath}`);
-    }
-
-    imodel.saveChanges();
-
+  function memoryBisSchemasLoading(schemaName: string) {
     const beforeMemory: NodeJS.MemoryUsage = process.memoryUsage();
-    const schemaResult = imodel.nativeDb.getSchema(schemaName);
+    const schemaResult: IModelJsNative.ErrorStatusOrResult<any, any> = iModelDb.nativeDb.getSchema(schemaName);
     const afterMemory: NodeJS.MemoryUsage = process.memoryUsage();
 
     if (schemaResult.error !== undefined) {
@@ -88,51 +145,51 @@ describe("SchemaLoaderPerformance", () => {
     }
 
     const memoryUsed = afterMemory.heapUsed - beforeMemory.heapUsed;
-    reporter.addEntry("SchemaLoaderPerfTest", `Loading schema: ${schemaName}`, "Memory used(bytes)", memoryUsed, {});
-    imodel.close();
+    reporter.addEntry("SchemaLoaderPerfTest", `Get schema from imodel: ${schemaName}`, "Memory used(bytes)", memoryUsed, {});
+    iModelDb.clearCaches();
   }
 
   it("Time BisSchemas data read from imodel", async () => {
-    await timeBisSchemasLoading("Units", getSchemaPath("Standard", "Units.01.00.07.ecschema.xml"));
-    await timeBisSchemasLoading("Formats", getSchemaPath("Standard", "Formats.01.00.00.ecschema.xml"));
+    timeBisSchemasLoading("Units");
+    timeBisSchemasLoading("Formats");
 
-    await timeBisSchemasLoading("BisCore", getSchemaPath("Dgn", "BisCore.ecschema.xml"));
+    timeBisSchemasLoading("BisCore");
 
-    // await timeBisSchemasLoading("ProcessFunctional", getSchemaPathFromPackage("process-functional-schema", "ProcessFunctional.ecschema.xml"));
-    // await timeBisSchemasLoading("ProcessPhysical", getSchemaPathFromPackage("process-physical-schema", "ProcessPhysical.ecschema.xml"));
+    timeBisSchemasLoading("ProcessFunctional");
+    timeBisSchemasLoading("ProcessPhysical");
 
-    // await timeBisSchemasLoading("CifBridge", getSchemaPathFromPackage("cif-bridge-schema", "CifBridge.ecschema.xml"));
-    // await timeBisSchemasLoading("CifCmmon", getSchemaPathFromPackage("cif-common-schema", "CifCommon.ecschema.xml"));
-    // await timeBisSchemasLoading("CifGeometricRules", getSchemaPathFromPackage("cif-geometric-rules-schema", "CifGeometricRules.ecschema.xml"));
-    // await timeBisSchemasLoading("CifHydraulicAnalysis", getSchemaPathFromPackage("cif-hydraulic-analysis-schema", "CifHydraulicAnalysis.ecschema.xml"));
-    // await timeBisSchemasLoading("CifHydraulicResults", getSchemaPathFromPackage("cif-hydraulic-results-schema", "CifHydraulicResults.ecschema.xml"));
-    // await timeBisSchemasLoading("CifQuantityTakeoffs", getSchemaPathFromPackage("cif-quantity-takeoffs-schema", "CifQuantityTakeoffs.ecschema.xml"));
-    // await timeBisSchemasLoading("CifRail", getSchemaPathFromPackage("cif-rail-schema", "CifRail.ecschema.xml"));
-    // await timeBisSchemasLoading("CifRoads", getSchemaPathFromPackage("cif-roads-schema", "CifRoads.ecschema.xml"));
-    // await timeBisSchemasLoading("CifSubsurface", getSchemaPathFromPackage("cif-subsurface-schema", "CifSubsurface.ecschema.xml"));
-    // await timeBisSchemasLoading("CifSubsurfaceConflictAnalysis", getSchemaPathFromPackage("cif-subsurface-conflict-analysis-schema", "CifSubsurfaceConflictAnalysis.ecschema.xml"));
-    await timeBisSchemasLoading("CifUnits", getSchemaPathFromPackage("cif-units-schema", "CifUnits.ecschema.xml"));
+    timeBisSchemasLoading("CifBridge");
+    timeBisSchemasLoading("CifCommon");
+    timeBisSchemasLoading("CifGeometricRules");
+    timeBisSchemasLoading("CifHydraulicAnalysis");
+    timeBisSchemasLoading("CifHydraulicResults");
+    timeBisSchemasLoading("CifQuantityTakeoffs");
+    timeBisSchemasLoading("CifRail");
+    timeBisSchemasLoading("CifRoads");
+    timeBisSchemasLoading("CifSubsurface");
+    timeBisSchemasLoading("CifSubsurfaceConflictAnalysis");
+    timeBisSchemasLoading("CifUnits");
   });
 
   it("Memory used for BisSchemas data read from imodel", async () => {
-    await memoryBisSchemasLoading("Units", getSchemaPath("Standard", "Units.01.00.07.ecschema.xml"));
-    await memoryBisSchemasLoading("Formats", getSchemaPath("Standard", "Formats.01.00.00.ecschema.xml"));
+    memoryBisSchemasLoading("Units");
+    memoryBisSchemasLoading("Formats");
 
-    await memoryBisSchemasLoading("BisCore", getSchemaPath("Dgn", "BisCore.ecschema.xml"));
+    memoryBisSchemasLoading("BisCore");
 
-    // await memoryBisSchemasLoading("ProcessFunctional", getSchemaPathFromPackage("process-functional-schema", "ProcessFunctional.ecschema.xml"));
-    // await memoryBisSchemasLoading("ProcessPhysical", getSchemaPathFromPackage("process-physical-schema", "ProcessPhysical.ecschema.xml"));
+    memoryBisSchemasLoading("ProcessFunctional");
+    memoryBisSchemasLoading("ProcessPhysical");
 
-    // await memoryBisSchemasLoading("CifBridge", getSchemaPathFromPackage("cif-bridge-schema", "CifBridge.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifCmmon", getSchemaPathFromPackage("cif-common-schema", "CifCommon.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifGeometricRules", getSchemaPathFromPackage("cif-geometric-rules-schema", "CifGeometricRules.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifHydraulicAnalysis", getSchemaPathFromPackage("cif-hydraulic-analysis-schema", "CifHydraulicAnalysis.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifHydraulicResults", getSchemaPathFromPackage("cif-hydraulic-results-schema", "CifHydraulicResults.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifQuantityTakeoffs", getSchemaPathFromPackage("cif-quantity-takeoffs-schema", "CifQuantityTakeoffs.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifRail", getSchemaPathFromPackage("cif-rail-schema", "CifRail.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifRoads", getSchemaPathFromPackage("cif-roads-schema", "CifRoads.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifSubsurface", getSchemaPathFromPackage("cif-subsurface-schema", "CifSubsurface.ecschema.xml"));
-    // await memoryBisSchemasLoading("CifSubsurfaceConflictAnalysis", getSchemaPathFromPackage("cif-subsurface-conflict-analysis-schema", "CifSubsurfaceConflictAnalysis.ecschema.xml"));
-    await memoryBisSchemasLoading("CifUnits", getSchemaPathFromPackage("cif-units-schema", "CifUnits.ecschema.xml"));
+    memoryBisSchemasLoading("CifBridge");
+    memoryBisSchemasLoading("CifCommon");
+    memoryBisSchemasLoading("CifGeometricRules");
+    memoryBisSchemasLoading("CifHydraulicAnalysis");
+    memoryBisSchemasLoading("CifHydraulicResults");
+    memoryBisSchemasLoading("CifQuantityTakeoffs");
+    memoryBisSchemasLoading("CifRail");
+    memoryBisSchemasLoading("CifRoads");
+    memoryBisSchemasLoading("CifSubsurface");
+    memoryBisSchemasLoading("CifSubsurfaceConflictAnalysis");
+    memoryBisSchemasLoading("CifUnits");
   });
 });
