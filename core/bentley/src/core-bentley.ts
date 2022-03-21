fa9cec9a105d597d8c18eeee2c65365a070f9d51
swbsi <69857376+swbsi@users.noreply.github.com>
2022-03-21 16:37:00 -0400

    WIP

--- core/bentley/src/core-bentley.ts
+++ core/bentley/src/core-bentley.ts
@@ -8,7 +8,7 @@ export * from "./AsyncMutex";
 export * from "./BeEvent";
 export * from "./BentleyError";
 export * from "./BentleyLoggerCategory";
-export * from "./ErrorCategory";
+export * from "./StatusCategory";
 export * from "./BeSQLite";
 export * from "./ByteStream";
 export * from "./ClassUtils";
