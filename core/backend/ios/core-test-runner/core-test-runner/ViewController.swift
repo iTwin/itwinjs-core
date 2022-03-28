b2e7c444a247a221758157c8e4127f3f6c0e233f
Daniel Toby <41296254+DanielToby@users.noreply.github.com>
2022-03-28 12:56:10 -0400

    Remove encoding from Swift xml string instantiation, NSLog string as literal.

--- core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
+++ core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
@@ -4,7 +4,6 @@
 *--------------------------------------------------------------------------------------------*/
 
 import Foundation
-import os
 
 import IModelJsNative
 
@@ -25,9 +24,9 @@ class ViewController: ObservableObject {
         NSLog("(ios): Running tests.")
         host.loadBackend(main) { [self] (numFailed: UInt32) in
             do {
-                let text = try String(contentsOf: testResultsUrl, encoding: .utf8)
+                let text = try String(contentsOf: testResultsUrl)
                 for line in text.components(separatedBy: .newlines) {
-                    NSLog("[Mocha_Result_XML]: \(line)")
+                    NSLog("[Mocha_Result_XML]: %@", line)
                 }
             } catch {
                 NSLog("(ios): Failed to read mocha test results.")
