23b3122893fdc07177b8d47c4e90481c87985a73
Titouan Le Duigou <101629676+TitouanLeDuigou@users.noreply.github.com>
2022-04-06 16:45:30 +0200

    Add conditions to skip onMotionSnap events

--- core/frontend/src/tools/ToolAdmin.ts
+++ core/frontend/src/tools/ToolAdmin.ts
@@ -6,8 +6,8 @@
  * @module Tools
  */
 
-import { AbandonedError, assert, BeEvent, IModelStatus, Logger } from "@itwin/core-bentley";
-import { Matrix3d, Point2d, Point3d, Transform, Vector3d, XAndY } from "@itwin/core-geometry";
+import { AbandonedError, assert, BeEvent, BeTimePoint, IModelStatus, Logger } from "@itwin/core-bentley";
+import { Matrix3d, Point2d, Point3d, Transform, Vector2d, Vector3d, XAndY } from "@itwin/core-geometry";
 import { Easing, GeometryStreamProps, NpcCenter } from "@itwin/core-common";
 import { DialogItemValue, DialogPropertyItem, DialogPropertySyncItem } from "@itwin/appui-abstract";
 import { AccuSnap, TentativeOrAccuSnap } from "../AccuSnap";
@@ -322,6 +322,9 @@ export class ToolAdmin {
   private _saveLocateCircle = false;
   private _defaultToolId = "Select";
   private _defaultToolArgs?: any[];
+  private _lastHandledMotionTime?: BeTimePoint;
+  private _lastHandledMotionPoint?: Vector2d;
+  private _mouseMoveOverTimeout?: NodeJS.Timeout;
 
   /** The name of the [[PrimitiveTool]] to use as the default tool. Defaults to "Select", referring to [[SelectionTool]].
    * @see [[startDefaultTool]] to activate the default tool.
@@ -952,13 +955,51 @@ export class ToolAdmin {
 
   private async onMotionSnap(ev: BeButtonEvent): Promise<boolean> {
     try {
-      await IModelApp.accuSnap.onMotion(ev);
+      await this.onMotionSnapOrSkip(ev);
       return true;
     } catch (error) {
       if (error instanceof AbandonedError)
         return false; // expected, not a problem. Just ignore this motion and return.
       throw error; // unknown error
     }
+  }
+
+  // Call accuSnap.onMotion
+  private async onMotionSnapOrSkip(ev: BeButtonEvent): Promise<void> {
+    if (this.shouldSkipOnMotionSnap(ev))
+      return;
+
+    await IModelApp.accuSnap.onMotion(ev);
+
+    this._lastHandledMotionTime = BeTimePoint.now();
+    this._lastHandledMotionPoint = Vector2d.fromJSON(ev.viewPoint);
+  }
+
+  // Should the current onMotionSnap event be skipped to avoid unnecessary ReadPixel calls?
+  private shouldSkipOnMotionSnap(ev: BeButtonEvent ): boolean {
+    if (this._lastHandledMotionTime === undefined)
+      return false;
+
+    const now = BeTimePoint.now();
+    const msSinceLastCall = now.milliseconds - this._lastHandledMotionTime.milliseconds;
+
+    const maxOnMotionCallPerSecond = 15; // TODO: Make it configurable?
+    const delay = 1000 / maxOnMotionCallPerSecond;
+
+    if (msSinceLastCall < delay) // Call too close to the previous one, skip it
+      return true;
+    if (msSinceLastCall > 2 * delay) // Call too long after last one, keep it
+      return false;
+
+    const movement = this._lastHandledMotionPoint?.vectorTo(ev.viewPoint);
+
+    if (movement !== undefined) {
+      const distance = new Vector2d(movement.x / ev.viewport!.viewRect.width, movement.y / ev.viewport!.viewRect.height).magnitude();
+
+      return distance < 0.1 || distance > 0.6; // TODO: Make it configurable?
+    }
+
+    return false;
   }
 
   private async onStartDrag(ev: BeButtonEvent, tool?: InteractiveTool): Promise<EventHandled> {
@@ -1047,7 +1088,27 @@ export class ToolAdmin {
     if (!(buttonMask & 1))
       this.currentInputState.button[BeButton.Data].isDown = false;
 
+    if (this._mouseMoveOverTimeout !== undefined)
+      clearTimeout(this._mouseMoveOverTimeout);
+    this._mouseMoveOverTimeout = setTimeout(async () => {await this.onMouseMoveOver(event);}, 100);
+
     return this.onMotion(vp, pos, InputSource.Mouse, false, mov);
+  }
+
+  // Called with the last MouseMove event if the user stopped moving the mouse
+  private async onMouseMoveOver(event: ToolEvent): Promise<any> {
+    // Make sure that we fire the motion snap event correctly
+    this._lastHandledMotionTime = this._lastHandledMotionPoint = undefined;
+
+    const vp = event.vp!;
+    const pos = this.getMousePosition(event);
+    const current = this.currentInputState;
+
+    const ev = new BeButtonEvent();
+    current.fromPoint(vp, pos, InputSource.Mouse);
+    current.toEvent(ev, false);
+
+    await this.onMotionSnap(ev);
   }
 
   public adjustPointToACS(pointActive: Point3d, vp: Viewport, perpendicular: boolean): void {
@@ -1229,6 +1290,8 @@ export class ToolAdmin {
     current.onButtonDown(button);
     current.toEvent(ev, true);
     current.updateDownPoint(ev);
+
+    await this.onMotionSnap(ev);
 
     return this.sendButtonEvent(ev);
   }
