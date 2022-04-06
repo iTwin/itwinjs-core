a8e94c3d605f22470990053c3051ecd89d0bb51d
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2022-04-06 06:58:26 -0400

    Fix blank rendering when dropping and immediately re-adding the same viewport (#3472)
    * Do not update canvas dimensions if 0. Resolves an issue with a black rendering when a viewport is dropped and re-added with the same dimensions.
    
    * rush change

--- core/frontend/src/test/ViewManager.test.ts
+++ core/frontend/src/test/ViewManager.test.ts
@@ -8,6 +8,8 @@ import { IModelApp } from "../IModelApp";
 import { IModelConnection } from "../IModelConnection";
 import { createBlankConnection } from "./createBlankConnection";
 import { openBlankViewport } from "./openBlankViewport";
+import { expectColors } from "./ExpectColors";
+import { ColorDef } from "@itwin/core-common";
 
 describe("ViewManager", () => {
   let imodel: IModelConnection;
@@ -30,6 +32,31 @@ describe("ViewManager", () => {
     IModelApp.viewManager.dropViewport(vp, false);
     vp.renderFrame();
     expect((vp.target as OnScreenTarget).checkFboDimensions()).to.be.true;
+    vp.dispose();
+  });
+
+  /** Dropping and immediately re-adding an unresized viewport to the view manager would result in a black rendering
+   * until the viewport was manually resized. This happened because when the viewport was removed it would have a 0,0
+   * dimension, which was internally recorded (but not acted upon with regard to framebuffers). Once re-adding the viewport,
+   * it would be flagged as having a size change because its dimensions were no longer 0 (they became the original
+   * dimensions). Disposing and recreating the framebuffers with the same dimensions as the previous framebuffers caused the
+   * black rendering in the particular case of re-adding the viewport.
+   *
+   * We resolved this problem by adding a check to not record a dimension change if the new dimensions are 0 -- we really
+   * do not want to create framebuffers with those dimensions anyway, because that is invalid.
+   *
+   * This test verifies that this problem has been resolved.
+   */
+  it("should not render black when dropping and re-adding viewport with same dimensions", async () => {
+    const vp = openBlankViewport({ width: 32, height: 32 });
+    vp.displayStyle.backgroundColor = ColorDef.red;
+    IModelApp.viewManager.addViewport(vp);
+    vp.renderFrame();
+    expectColors(vp, [ColorDef.red]);
+    IModelApp.viewManager.dropViewport(vp, false);
+    IModelApp.viewManager.addViewport(vp);
+    vp.renderFrame();
+    expectColors(vp, [ColorDef.red]);
     vp.dispose();
   });
 });
