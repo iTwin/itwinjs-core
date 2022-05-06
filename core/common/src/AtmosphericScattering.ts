7b2288695384e837f372e2125070b3579dec0874
Raphaël <RaphaelMelancon@users.noreply.github.com>
2022-05-06 17:12:10 -0400

    WIP - debugging densityAtPoint

--- core/common/src/AtmosphericScattering.ts
+++ core/common/src/AtmosphericScattering.ts
@@ -6,7 +6,17 @@
  * @module DisplayStyles
  */
 
-import { Vector3d, XYZProps } from "@itwin/core-geometry";
+import { Point3d, Vector3d, XYZProps } from "@itwin/core-geometry";
+
+export const defaultAtmosphericScatteringProps: Required<AtmosphericScatteringProps> = {
+  sunDirection: {x: 0.0, y: -1.0, z: 0.0},
+  earthCenter: {x: 0.0, y: 0.0, z: -6_371_000.0},
+  atmosphereRadius: 6_371_100.0,
+  earthRadius: 6_371_000.0,
+  densityFalloff: 4.0,
+  scatteringStrength: 1.0,
+  wavelenghts: [700.0, 530.0, 440.0],
+};
 
 /**
  * @public
@@ -15,6 +25,10 @@ export interface AtmosphericScatteringProps {
   sunDirection?: XYZProps;
   earthCenter?: XYZProps;
   atmosphereRadius?: number;
+  earthRadius?: number;
+  densityFalloff?: number;
+  scatteringStrength?: number;
+  wavelenghts?: number[];
 }
 
 /**
@@ -22,8 +36,12 @@ export interface AtmosphericScatteringProps {
  */
 export class AtmosphericScattering {
   public readonly sunDirection: Vector3d;
-  public readonly earthCenter: Vector3d;
+  public readonly earthCenter: Point3d;
   public readonly atmosphereRadius: number;
+  public readonly earthRadius: number;
+  public readonly densityFalloff: number;
+  public readonly scatteringStrength: number;
+  public readonly wavelenghts: number[];
 
   public equals(other: AtmosphericScattering): boolean {
     if (this.sunDirection !== other.sunDirection)
@@ -32,19 +50,34 @@ export class AtmosphericScattering {
       return false;
     if (this.atmosphereRadius !== other.atmosphereRadius)
       return false;
+    if (this.earthCenter !== other.earthCenter)
+      return false;
+    if (this.densityFalloff !== other.densityFalloff)
+      return false;
+    if (this.scatteringStrength !== other.scatteringStrength)
+      return false;
+    if (this.wavelenghts !== other.wavelenghts)
+      return false;
     return true;
   }
 
   private constructor(json?: AtmosphericScatteringProps) {
     if (json === undefined) {
-      this.sunDirection = Vector3d.fromJSON();
-      this.earthCenter = Vector3d.fromJSON();
-      this.atmosphereRadius = 6371100.0;
+      this.sunDirection = Vector3d.fromJSON(defaultAtmosphericScatteringProps.sunDirection);
+      this.earthCenter = Point3d.fromJSON(defaultAtmosphericScatteringProps.earthCenter);
+      this.atmosphereRadius = defaultAtmosphericScatteringProps.atmosphereRadius;
+      this.earthRadius = defaultAtmosphericScatteringProps.earthRadius;
+      this.densityFalloff = defaultAtmosphericScatteringProps.densityFalloff;
+      this.scatteringStrength = defaultAtmosphericScatteringProps.scatteringStrength;
+      this.wavelenghts = defaultAtmosphericScatteringProps.wavelenghts;
     } else {
-      this.sunDirection = Vector3d.fromJSON(json.sunDirection);
-      this.earthCenter = Vector3d.fromJSON(json.earthCenter);
-      this.atmosphereRadius = json.atmosphereRadius === undefined ? 0.0 : json.atmosphereRadius;
-
+      this.sunDirection = json.sunDirection === undefined ? Vector3d.fromJSON(defaultAtmosphericScatteringProps.sunDirection) : Vector3d.fromJSON(json.sunDirection);
+      this.earthCenter = json.earthCenter === undefined ? Point3d.fromJSON(defaultAtmosphericScatteringProps.earthCenter) : Point3d.fromJSON(json.earthCenter);
+      this.atmosphereRadius = json.atmosphereRadius === undefined ? defaultAtmosphericScatteringProps.atmosphereRadius : json.atmosphereRadius;
+      this.earthRadius = json.earthRadius === undefined ? defaultAtmosphericScatteringProps.earthRadius : json.earthRadius;
+      this.densityFalloff = json.densityFalloff === undefined ? defaultAtmosphericScatteringProps.densityFalloff : json.densityFalloff;
+      this.scatteringStrength = json.scatteringStrength === undefined ? defaultAtmosphericScatteringProps.scatteringStrength : json.scatteringStrength;
+      this.wavelenghts = json.wavelenghts === undefined ? defaultAtmosphericScatteringProps.wavelenghts : json.wavelenghts;
     }
   }
 
@@ -57,6 +90,10 @@ export class AtmosphericScattering {
       sunDirection: this.sunDirection.toJSON(),
       earthCenter: this.earthCenter.toJSON(),
       atmosphereRadius: this.atmosphereRadius,
+      earthRadius: this.earthRadius,
+      densityFalloff: this.densityFalloff,
+      scatteringStrength: this.scatteringStrength,
+      wavelenghts: this.wavelenghts,
     };
     return json;
   }
--- core/frontend/src/render/webgl/AtmosphericScatteringUniforms.ts
+++ core/frontend/src/render/webgl/AtmosphericScatteringUniforms.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { Transform, Vector3d } from "@itwin/core-geometry";
+import { Point3d, Transform } from "@itwin/core-geometry";
 import { AtmosphericScattering } from "@itwin/core-common";
 import { WebGLDisposable } from "./Disposable";
 import { desync, sync } from "./Sync";
@@ -10,11 +10,13 @@ import { Target } from "./Target";
 import { UniformHandle } from "./UniformHandle";
 
 export class AtmosphericScatteringUniforms implements WebGLDisposable {
-  private readonly _sunDirection = new Float32Array(3);
   private readonly _earthCenter = new Float32Array(3);
   private _atmosphericScattering?: AtmosphericScattering;
-  private _scratchVector = new Vector3d();
+  private _scratchPoint3d = new Point3d();
   private _atmosphereRadius = 0.0;
+  private _earthRadius = 0.0;
+  private _scatteringCoefficients = new Float32Array(3);
+  private _densityFalloff = 0.0;
 
   public syncKey = 0;
 
@@ -32,36 +34,37 @@ export class AtmosphericScatteringUniforms implements WebGLDisposable {
     if (!this.atmosphericScattering) {
       return;
     }
-    this._updateSunDirection(this.atmosphericScattering.sunDirection, target.uniforms.frustum.viewMatrix);
     this._updateEarthCenter(this.atmosphericScattering.earthCenter, target.uniforms.frustum.viewMatrix);
     this._updateAtmosphereRadius(this.atmosphericScattering.atmosphereRadius);
+    this._updateEarthRadius(this.atmosphericScattering.earthRadius);
+    this._updateDensityFalloff(this.atmosphericScattering.densityFalloff);
+    this._updateScatteringCoefficients(this.atmosphericScattering.scatteringStrength, this.atmosphericScattering.wavelenghts);
+
   }
 
-  private _updateEarthCenter(earthCenter: Vector3d, viewMatrix: Transform) {
-    const scratchPoint = viewMatrix.multiplyPoint3d(earthCenter);
-    // eslint-disable-next-line no-console
-    console.log(this._earthCenter);
-    this._earthCenter[0] = scratchPoint.x;
-    this._earthCenter[1] = scratchPoint.y;
-    this._earthCenter[2] = scratchPoint.z;
-  }
-
-  private _updateSunDirection(sunDir: Vector3d, viewMatrix: Transform) {
-    viewMatrix.multiplyVector(sunDir, this._scratchVector);
-    this._scratchVector.negate(this._scratchVector);
-    this._scratchVector.normalizeInPlace();
-    this._sunDirection[0] = this._scratchVector.x;
-    this._sunDirection[1] = this._scratchVector.y;
-    this._sunDirection[2] = this._scratchVector.z;
+  private _updateEarthCenter(earthCenter: Point3d, viewMatrix: Transform) {
+    viewMatrix.multiplyPoint3d(earthCenter, this._scratchPoint3d);
+    this._earthCenter[0] = this._scratchPoint3d.x;
+    this._earthCenter[1] = this._scratchPoint3d.y;
+    this._earthCenter[2] = this._scratchPoint3d.z;
   }
 
   private _updateAtmosphereRadius(radius: number) {
     this._atmosphereRadius = radius;
   }
 
-  public bindSunDirection(uniform: UniformHandle): void {
-    if (!sync(this, uniform))
-      uniform.setUniform3fv(this._sunDirection);
+  private _updateEarthRadius(radius: number) {
+    this._earthRadius = radius;
+  }
+
+  private _updateScatteringCoefficients(scatteringStrength: number, wavelenghts: number[]) {
+    this._scatteringCoefficients[0] = ((400.0 / wavelenghts[0]) ** 4.0) * scatteringStrength;
+    this._scatteringCoefficients[1] = ((400.0 / wavelenghts[1]) ** 4.0) * scatteringStrength;
+    this._scatteringCoefficients[2] = ((400.0 / wavelenghts[2]) ** 4.0) * scatteringStrength;
+  }
+
+  private _updateDensityFalloff(dentityFalloff: number) {
+    this._densityFalloff = dentityFalloff;
   }
 
   public bindEarthCenter(uniform: UniformHandle): void {
@@ -72,6 +75,21 @@ export class AtmosphericScatteringUniforms implements WebGLDisposable {
   public bindAtmosphereRadius(uniform: UniformHandle): void {
     if (!sync(this, uniform))
       uniform.setUniform1f(this._atmosphereRadius);
+  }
+
+  public bindEarthRadius(uniform: UniformHandle): void {
+    if (!sync(this, uniform))
+      uniform.setUniform1f(this._earthRadius);
+  }
+
+  public bindDensityFalloff(uniform: UniformHandle): void {
+    if (!sync(this, uniform))
+      uniform.setUniform1f(this._densityFalloff);
+  }
+
+  public bindScatteringCoefficients(uniform: UniformHandle): void {
+    if (!sync(this, uniform))
+      uniform.setUniform3fv(this._scatteringCoefficients);
   }
 
   public get isDisposed(): boolean {
