40309b035688c28022b86ff339fa786558da0522
gdatbentley <99873235+gdatbentley@users.noreply.github.com>
2022-05-06 09:40:25 +0300

    auth client setup error handling

--- test-apps/display-performance-test-app/src/backend/backend.ts
+++ test-apps/display-performance-test-app/src/backend/backend.ts
@@ -10,9 +10,32 @@ import { IModelHost, IModelHostConfiguration } from "@itwin/core-backend";
 import { BackendIModelsAccess } from "@itwin/imodels-access-backend";
 import { IModelsClient } from "@itwin/imodels-client-authoring";
 import { IModelReadRpcInterface, IModelTileRpcInterface, SnapshotIModelRpcInterface } from "@itwin/core-common";
-import { TestBrowserAuthorizationClient } from "@itwin/oidc-signin-tool";
+import { TestBrowserAuthorizationClient, TestBrowserAuthorizationClientConfiguration, TestUserCredentials } from "@itwin/oidc-signin-tool";
 import DisplayPerfRpcInterface from "../common/DisplayPerfRpcInterface";
 import "./DisplayPerfRpcImpl"; // just to get the RPC implementation registered
+
+export async function initializeBackend() {
+  loadEnv(path.join(__dirname, "..", "..", ".env"));
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // (needed temporarily to use self-signed cert to communicate with iModelBank via https)
+
+  const iModelHost = new IModelHostConfiguration();
+  const iModelClient = new IModelsClient({ api: { baseUrl: `https://${process.env.IMJS_URL_PREFIX ?? ""}api.bentley.com/imodels`}});
+  iModelHost.hubAccess = new BackendIModelsAccess(iModelClient);
+
+  iModelHost.authorizationClient = setupAuthorizationClient();
+  
+  if (ProcessDetector.isElectronAppBackend) {
+    const rpcInterfaces = [DisplayPerfRpcInterface, IModelTileRpcInterface, SnapshotIModelRpcInterface, IModelReadRpcInterface];
+    await ElectronHost.startup({
+      electronHost: {
+        webResourcesPath: path.join(__dirname, "..", "..", "build"),
+        rpcInterfaces,
+      },
+      iModelHost,
+    });
+  } else
+    await IModelHost.startup(iModelHost);
+}
 
 /** Loads the provided `.env` file into process.env */
 function loadEnv(envFile: string) {
@@ -29,33 +52,26 @@ function loadEnv(envFile: string) {
   dotenvExpand(envResult);
 }
 
-export async function initializeBackend() {
-  loadEnv(path.join(__dirname, "..", "..", ".env"));
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // (needed temporarily to use self-signed cert to communicate with iModelBank via https)
-
-  const iModelHost = new IModelHostConfiguration();
-  const iModelClient = new IModelsClient({ api: { baseUrl: `https://${process.env.IMJS_URL_PREFIX ?? ""}api.bentley.com/imodels`}});
-  iModelHost.hubAccess = new BackendIModelsAccess(iModelClient);
-
-  iModelHost.authorizationClient = new TestBrowserAuthorizationClient({
+function setupAuthorizationClient(): TestBrowserAuthorizationClient | undefined {
+  const authConfig: TestBrowserAuthorizationClientConfiguration = {
     clientId: process.env.IMJS_OIDC_CLIENT_ID!,
     redirectUri: process.env.IMJS_OIDC_REDIRECT_URI!,
     scope: process.env.IMJS_OIDC_SCOPES!,
-    authority: process.env.IMJS_OIDC_AUTHORITY,
-  }, {
+    authority: process.env.IMJS_OIDC_AUTHORITY
+  };
+  const authCredentials: TestUserCredentials = {
     email: process.env.IMJS_OIDC_EMAIL!,
     password: process.env.IMJS_OIDC_PASSWORD!
-  });
-  
-  if (ProcessDetector.isElectronAppBackend) {
-    const rpcInterfaces = [DisplayPerfRpcInterface, IModelTileRpcInterface, SnapshotIModelRpcInterface, IModelReadRpcInterface];
-    await ElectronHost.startup({
-      electronHost: {
-        webResourcesPath: path.join(__dirname, "..", "..", "build"),
-        rpcInterfaces,
-      },
-      iModelHost,
-    });
-  } else
-    await IModelHost.startup(iModelHost);
+  };
+
+  const allArgs = Object.entries({...authConfig, ...authCredentials});
+  const missingKeys = allArgs.filter(([_key, val]) => !val).map(([key, _val]) => key);
+  if(missingKeys.length === 0)
+    return new TestBrowserAuthorizationClient(authConfig, authCredentials);
+  else {
+    if(missingKeys.length === allArgs.length)
+      return undefined;
+
+    throw new Error(`Missing env configuration for OIDC keys: ${ missingKeys.join(' ') }`);
+  }
 }
