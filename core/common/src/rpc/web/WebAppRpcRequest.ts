e915676876266ae603116df0258f152d2ce015d6
nick.tessier <22119573+nick4598@users.noreply.github.com>
2022-05-09 09:42:59 -0400

    Initial commit

--- core/common/src/rpc/web/WebAppRpcRequest.ts
+++ core/common/src/rpc/web/WebAppRpcRequest.ts
@@ -30,10 +30,10 @@ export class WebAppRpcRequest extends RpcRequest {
   private _pathSuffix: string = "";
   private get _headers() { return this._request.headers as { [key: string]: string }; }
 
-  /** The maximum size permitted for an encoded component in a URL.
+  /** The maximum size permitted for an encoded component in a URhow do i know which a cumulative request size of 16 kb. An authorization header may also add considerably to the request size.
    * @note This is used for features like encoding the payload of a cacheable request in the URL.
    */
-  public static maxUrlComponentSize = 4096;
+  public static maxUrlComponentSize = 1024 * 8;
 
   /** The HTTP method for this request. */
   public override method: HttpMethod_T;
@@ -284,9 +284,28 @@ export class WebAppRpcRequest extends RpcRequest {
 
   private static configureResponse(protocol: WebAppRpcProtocol, request: SerializedRpcRequest, fulfillment: RpcRequestFulfillment, res: HttpServerResponse) {
     const success = protocol.getStatus(fulfillment.status) === RpcRequestStatus.Resolved;
+    // TODO: Use stale-while-revalidate in cache headers. This needs to be tested, and does not currently have support in the router/caching-service.
+    // This will allow browsers to use stale cached responses while also revalidating with the router, allowing us to start up a backend if necessary.
 
+    // RPC Caching Service uses the s-maxage header to determine the TTL for the redis cache.
+    const oneHourInSeconds = 3600;
     if (success && request.caching === RpcResponseCacheControl.Immutable) {
-      res.set("Cache-Control", "private, max-age=31536000, immutable");
+      // If response size is > 50 MB, do not cache it.
+      if (fulfillment.result.objects.length > (50 * 10**7)) {
+        res.set("Cache-Control", "no-store");
+        break;
+      }
+      if (request.operation.operationName === "generateTileContent") {
+        res.set("Cache-Control", "no-store");
+      } else if (request.operation.operationName === "getConnectionProps") {
+        // GetConnectionprops can't be cached on the browser longer than the lifespan of the backend. The lifespan of backend may shrink too. Keep it at 1 second to be safe.
+        res.set("Cache-Control", "s-maxage=7200, max-age=1, immutable");
+      } else if (request.operation.operationName === "getTileCacheContainerUrl") {
+        // getTileCacheContainerUrl returns a SAS with an expiry of 23:59:59. We can't exceed that time when setting the max-age.
+        res.set("Cache-Control", `s-maxage=${oneHourInSeconds * 16}, max-age=${oneHourInSeconds * 16}, immutable`);
+      } else {
+        res.set("Cache-Control", `s-maxage=${oneHourInSeconds * 24}, max-age=${oneHourInSeconds * 48}, immutable`);
+      }
     }
 
     if (fulfillment.retry) {
