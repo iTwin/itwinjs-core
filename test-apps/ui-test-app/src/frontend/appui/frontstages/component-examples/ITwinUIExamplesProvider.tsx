7801164816ae1c3a25e10df3109aeee73d999291
bsteinbk <65047615+bsteinbk@users.noreply.github.com>
2022-05-10 14:30:26 -0400

    Use unload not beforeUnload event for UI clean up.

--- test-apps/ui-test-app/src/frontend/appui/frontstages/component-examples/ITwinUIExamplesProvider.tsx
+++ test-apps/ui-test-app/src/frontend/appui/frontstages/component-examples/ITwinUIExamplesProvider.tsx
@@ -9,6 +9,139 @@ import { ComponentExampleCategory } from "./ComponentExamples";
 import { createComponentExample } from "./ComponentExamplesProvider";
 import { IModelApp, NotifyMessageDetails, OutputMessagePriority } from "@itwin/core-frontend";
 
+// Hook
+function useDebounce(value: any, delay=300) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = React.useState(value);
+  React.useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return debouncedValue;
+}
+
+interface DateSliderProps {
+  onChangeHandler: (date: Date) => void;
+  timeStamp: Date;
+}
+
+interface TimeSliderProps {
+  timeStamp: Date;
+}
+
+const YEAR = 2019;
+const yearMin = new Date(YEAR, 0, 1).setHours(0, 0, 0, 0);
+const yearMax = new Date(YEAR, 11, 31).setHours(0, 0, 0, 0);
+
+const millisecPerMinute = 1000 * 60;
+const millisecPerHour = millisecPerMinute * 60;
+const millisecPerDay = millisecPerHour * 24;
+
+const calculateSunriseOrSunset = (timeStamp: Date, sunrise: boolean) => {
+  const year = timeStamp.getUTCFullYear();
+  const month = timeStamp.getUTCMonth();
+  const day = timeStamp.getUTCDay();
+  const newDate = new Date(year, month, day);
+  if (sunrise) {
+    newDate.setHours(7, 0, 0, 0);
+  } else {
+    newDate.setHours(17, 0, 0, 0);
+  }
+  return newDate;
+};
+
+function TimeSlider({ timeStamp }: TimeSliderProps) {
+  const sunrise = React.useMemo(
+    () => calculateSunriseOrSunset(timeStamp, true).getTime(),
+    [timeStamp],
+  );
+  const sunset = React.useMemo(
+    () => calculateSunriseOrSunset(timeStamp, false).getTime(),
+    [timeStamp],
+  );
+
+  const vals = React.useMemo(
+    () => [Math.max(sunrise, Math.min(timeStamp.getTime(), sunset))],
+    [timeStamp, sunrise, sunset],
+  );
+
+  if (vals[0] < sunrise || vals[0] > sunset) {
+    // eslint-disable-next-line no-console
+    console.log("timestamp is invalid", vals[0], sunrise, sunset);
+  }
+
+  return (
+    <Slider
+      min={sunrise}
+      max={sunset}
+      values={vals}
+      // onUpdate={(value: readonly number[]) => {
+      //   onChangeHandler(new Date(value[0]));
+      // }}
+      // onChange={(value: readonly number[]) => {
+      //   onChangeHandler(new Date(value[0]));
+      // }}
+    />
+  );
+}
+
+function DateSlider({ onChangeHandler, timeStamp }: DateSliderProps)  {
+  const currentDate = React.useMemo(() => new Date(timeStamp.getTime()), [
+    timeStamp,
+  ]);
+
+  const utcDate = React.useCallback(
+    (value: number) => {
+      // adjust the date passed in by the number of days passed in the slider
+      const days =
+          (new Date(value).getTime() - currentDate.getTime()) / millisecPerDay;
+      const newDate = new Date(timeStamp.valueOf());
+      newDate.setUTCDate(timeStamp.getUTCDate() + days);
+      return newDate;
+    },
+    [timeStamp, currentDate],
+  );
+
+  return (
+    <Slider
+      min={yearMin}
+      max={yearMax}
+      values={[currentDate.getTime()]}
+      onUpdate={(value: readonly number[]) => {
+        onChangeHandler(utcDate(value[0]));
+      }}
+      onChange={(value: readonly number[]) => {
+        onChangeHandler(utcDate(value[0]));
+      }}
+    />
+  );
+}
+
+function DualDateSlider() {
+  const [timeStamp, setTimeStamp] = React.useState(new Date(2019, 0, 1, 13));
+  const debouncedTimeStamp = useDebounce(timeStamp, 60);
+  return (
+    <div style={{display: "flex", flexDirection: "column", flex: "1"}}>
+      <TimeSlider timeStamp={debouncedTimeStamp} />
+      <DateSlider
+        onChangeHandler={(newDateTime) => setTimeStamp(newDateTime)}
+        timeStamp={timeStamp}
+      />
+    </div>
+  );
+}
+
 function WrappedSlider() {
   const [currentValues, setCurrentValues] = React.useState([50]);
   const handleValueChange = React.useCallback((values: readonly number[]) => {
@@ -107,6 +240,7 @@ export class ITwinUIExamplesProvider {
           <Slider style={style} min={0} max={100} values={[50]} step={1} disabled
             tickLabels={["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]} />),
         createComponentExample("Wrapped Slider", "Slider that reports changes", <WrappedSlider />),
+        createComponentExample("Dual Date Sliders", "using iTwinUi Slider", <DualDateSlider />),
       ],
     };
   }
