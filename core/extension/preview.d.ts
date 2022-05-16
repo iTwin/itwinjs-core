ca30b2faeb9967aa07dc30ceaf9a3afa716cc204
Arun George <aruniverse@users.noreply.github.com>
2022-05-16 17:12:49 -0400

    try to clean up and sort the generation script

--- core/extension/preview.d.ts
+++ core/extension/preview.d.ts
@@ -4,15 +4,19 @@
 *--------------------------------------------------------------------------------------------*/
 // BEGIN GENERATED CODE
 export {
+	
 } from "@itwin/core-frontend";
 
 export type {
+	
 } from "@itwin/core-frontend";
 
 export {
+	
 } from "@itwin/core-common";
 
 export type {
+	
 } from "@itwin/core-common";
 
 // END GENERATED CODE
