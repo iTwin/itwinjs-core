a2570549149159166fd8b166269dadf54b3106bb
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-05-25 09:02:53 -0400

    Fix.

--- core/frontend/src/test/render/primitives/PrimitiveBuilder.test.ts
+++ core/frontend/src/test/render/primitives/PrimitiveBuilder.test.ts
@@ -6,23 +6,25 @@ import { expect } from "chai";
 import { Point3d } from "@itwin/core-geometry";
 import { GraphicType, IModelApp, RenderGraphic } from "../../../core-frontend";
 import { PrimitiveBuilder } from "../../../render-primitives";
-import { Branch, GraphicsArray, MeshGraphic } from "../../../webgl";
+import { Batch, Branch, GraphicsArray, MeshGraphic } from "../../../webgl";
 
 describe("PrimitiveBuilder", () => {
   before(async () => IModelApp.startup());
   after(async () => IModelApp.shutdown());
 
-  function makeShape(chordTolerance: number): RenderGraphic {
+  function makeShape(chordTolerance: number, pickableId?: string): RenderGraphic {
+    const pickable = pickableId ? { id: pickableId } : undefined;
     const builder = new PrimitiveBuilder(IModelApp.renderSystem, {
       type: GraphicType.Scene,
       computeChordTolerance: () => chordTolerance,
+      pickable,
     });
 
     builder.addShape([new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(1, 1, 0), new Point3d(0, 0, 0)]);
     return builder.finish() as Branch;
   }
 
-  it.only("omits degenerate facets", () => {
+  it("omits degenerate facets", () => {
     const branch = makeShape(0.0001) as Branch;
     expect(branch).instanceof(Branch);
     expect(branch.branch.entries.length).to.equal(1);
@@ -33,6 +35,14 @@ describe("PrimitiveBuilder", () => {
     expect(array.graphics.length).to.equal(0);
   });
 
-  it.only("omits empty feature table", () => {
+  it("omits empty feature table", () => {
+    const batch = makeShape(0.0001, "0x123") as Batch;
+    expect(batch).instanceof(Batch);
+    expect(batch.featureTable.numFeatures).to.equal(1);
+
+    // Previously this would have produced a Batch with an empty FeatureTable, which induced an assertion in PackedFeatureTable.initFromMap.
+    const array = makeShape(10000.0, "0x123") as GraphicsArray;
+    expect(array).instanceof(GraphicsArray);
+    expect(array.graphics.length).to.equal(0);
   });
 });
