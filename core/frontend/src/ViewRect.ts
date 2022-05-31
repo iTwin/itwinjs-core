45affda56bb6bfda6e6a438e462c5b1abd87a7b1
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-05-31 14:24:58 -0400

    fix import

--- core/frontend/src/ViewRect.ts
+++ core/frontend/src/ViewRect.ts
@@ -6,7 +6,7 @@
  * @module Views
  */
 
-import { LowAndHighXY, XAndY } from "@bentley/core-geometry";
+import { LowAndHighXY, XAndY } from "@bentley/geometry-core";
 
 /** A rectangle in unsigned integer view coordinates with (0,0) corresponding to the top-left corner of the view.
  * Increasing **x** moves from left to right, and increasing **y** moves from top to bottom.
