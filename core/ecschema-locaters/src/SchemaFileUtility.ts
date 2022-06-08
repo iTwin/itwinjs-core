3beefa4cedc00d23a9ab5d6042837cc22dc35dd3
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2022-06-08 09:06:01 -0400

    wip

--- core/ecschema-locaters/src/SchemaFileUtility.ts
+++ core/ecschema-locaters/src/SchemaFileUtility.ts
@@ -4,6 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 import * as fs from "fs-extra";
 import * as path from "path";
+import { DOMParser, XMLSerializer } from "@xmldom/xmldom";
 import { Schema } from "@itwin/ecschema-metadata";
 
 export class SchemaFileUtility {
@@ -12,15 +13,10 @@ export class SchemaFileUtility {
     let xmlDoc = new DOMParser().parseFromString(`<?xml version="1.0" encoding="UTF-8"?>`, "application/xml");
     const baseFile = this.getSchemaPath(schema, outputPath);
 
-    try {
-      xmlDoc = await schema.toXml(xmlDoc);
-    } catch (err: any) {
-      const msg = `An error occurred serializing schema '${schema.fullName}': ${err.message}`;
-      throw new Error(msg);
-    }
-
+    xmlDoc = await schema.toXml(xmlDoc);
     const serializer = new XMLSerializer();
     const xml = serializer.serializeToString(xmlDoc);
+
     try {
       await fs.writeFile(baseFile, xml);
     } catch (err: any) {
@@ -30,7 +26,7 @@ export class SchemaFileUtility {
   }
 
   private static getSchemaPath(schema: Schema, outputPath: string): string {
-    const realDir = path.normalize(outputPath) + path.sep;
+    const realDir = path.normalize(outputPath);
     const test = fs.pathExistsSync(realDir);
     if (!test) {
       const msg = `The output directory '${realDir}' does not exist.`;
