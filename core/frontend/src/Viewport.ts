68a1f6df0ff4f2ec1bf48a30b493061d5cf79983
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2022-06-15 15:18:38 -0400

    Work around framebuffer resize issues that can cause WebGL errors.

--- core/frontend/src/Viewport.ts
+++ core/frontend/src/Viewport.ts
@@ -3294,7 +3294,11 @@ export class ScreenViewport extends Viewport {
         _clear2dCanvas(this.canvas);
     }
 
-    this.target.updateViewRect();
+    const resized = this.target.updateViewRect();
+    if (resized) {
+      this.target.onResized();
+      this.invalidateController();
+    }
     this.invalidateRenderPlan();
   }
 }
---
+++ core/frontend/src/test/ViewManager.test.ts
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+import { IModelApp } from "../IModelApp";
+import { IModelConnection } from "../IModelConnection";
+import { OnScreenTarget } from "../imodeljs-frontend";
+import { createBlankConnection } from "./createBlankConnection";
+import { openBlankViewport } from "./openBlankViewport";
+
+describe("ViewManager", () => {
+  let imodel: IModelConnection;
+
+  before(async () => {
+    await IModelApp.startup();
+    imodel = createBlankConnection("view-manager-test");
+  });
+
+  after(async () => {
+    await imodel.close();
+    await IModelApp.shutdown();
+  });
+
+  it("should resize fbo properly after dropping a recently-resized viewport", async () => {
+    const vp = openBlankViewport({ width: 32, height: 32 });
+    IModelApp.viewManager.addViewport(vp);
+    vp.renderFrame();
+    vp.vpDiv.style.width = vp.vpDiv.style.height = "3px";
+    IModelApp.viewManager.dropViewport(vp, false);
+    vp.renderFrame();
+    expect((vp.target as OnScreenTarget).checkFboDimensions()).to.be.true;
+    vp.dispose();
+  });
+});
