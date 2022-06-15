07a9eba9b9fa37cb56c366260f2f0e8ba42cc979
Seamus Kirby <32379572+skirby1996@users.noreply.github.com>
2022-06-15 13:00:09 -0400

    add release creation to version bump pipeline

--- common/scripts/generate_release.py
+++ common/scripts/generate_release.py
@@ -1,82 +1,91 @@
-import os, subprocess
-from platform import release
-
-# Validate arguments
-# command = "npm view " + packageName + " dist-tags.previous"
-# proc = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
-# previousVer = proc.communicate()[0]
-# if len(previousVer) == 0:
-#   print("No version found for dist-tag 'previous'")
+import os, re, subprocess, sys
 
 def getSHAFromTag(tag):
-  cmd = ["git", "rev-list", "-n", "1", tag]
-  proc = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+  cmd = ['git', 'rev-list', '-n', '1', tag]
+  proc = subprocess.Popen(" ".join(cmd), stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
   sha = proc.communicate()[0].decode("utf-8").strip()
   if len(sha) == 0:
-    print("Could not find commit for tag '{0}'".format(tag))
-    return ""
-
+    sys.exit("Could not find commit for tag '{0}'".format(tag))
   return sha
 
 def getCommitMessage(sha):
-  cmd = ["git", "log", "-1", "--format='%s'", sha]
-  proc = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+  cmd = ['git', 'log', '-1', '--format=%s', sha]
+  proc = subprocess.Popen(" ".join(cmd), stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
   commitMessage = proc.communicate()[0].decode("utf-8").strip()
   if len(commitMessage) == 0:
-    print("Could not find commit " + sha)
-    return ""
-  return commitMessage[1:-1]
+    sys.exit("Could not find info for commit " + sha)
 
-def createRelease(previousTag, currentTag):
+  # If commit message contains multiple PR links (in case of backports), remove the earlier one
+  pattern = " \(#\d+\)"
+  if len(re.findall(pattern, commitMessage)) > 1:
+    commitMessage = re.sub(pattern, "", commitMessage, 1)
+  return commitMessage
+
+# Tag assumed to be of format release/x.x.x
+def createRelease(tag):
   # Parse versions from tags
-  previousVer = previousTag.split("/")[1]
-  currentVer = currentTag.split("/")[1]
-
+  currentVer = tag.split("/")[1]
   parsedVer = [int(i) for i in currentVer.split(".")]
-  print(parsedVer)
 
+  # Write release to file to preview
+  fileName = currentVer + ".md"
+  if os.path.exists(fileName):
+    os.remove(fileName)
 
-  # Determine if this is a patch release
-  releaseType = ""
+  f = open(fileName, "w")
+  f.write("# Release notes\n\n")
+
+  # Determine release type
   if parsedVer[2] > 0:
     releaseType = "Patch"
   elif parsedVer[1] > 0:
     releaseType = "Minor"
   else:
     releaseType = "Major"
+  print("Generating {0} release notes".format(releaseType.lower()))
 
-  # If patch release, get previous tag
   if releaseType == "Patch":
-    print("Patch release")
+    # Determine previous tag and version
+    cmd = ['git', 'describe', '--abbrev=0', '--tags', tag + '~1']
+    proc = subprocess.Popen(" ".join(cmd), stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+    previousTag = proc.communicate()[0].decode("utf-8").strip()
+    if (len(previousTag) == 0):
+      sys.exit("Could not find previous tag for " + tag)
 
-  # If major/minor release
+    previousVer = previousTag.split("/")[1]
 
-  # Get SHAs for both tags
-  previousSHA = getSHAFromTag(previousTag)
-  currentSHA = getSHAFromTag(currentTag)
+    # Get SHAs for both tags
+    previousSHA = getSHAFromTag(previousTag)
+    currentSHA = getSHAFromTag(tag)
 
-  # Get all commit SHAs between tags
-  cmd = ["git", "rev-list", "--ancestry-path", previousSHA + ".." + currentSHA]
-  proc = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
-  commits = proc.communicate()[0].decode("utf-8").split()[1:]
+    # Get all commit SHAs between tags
+    cmd = ['git', 'rev-list', '--ancestry-path', previousSHA + '..' + currentSHA]
+    proc = subprocess.Popen(" ".join(cmd), stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+    # Remove first commit from this list as it will always be a version bump commit
+    commits = proc.communicate()[0].decode("utf-8").split()[1:]
 
-  # Write release to file to preview
-  f = open("release.md", "w")
-  f.write("# {0} {1} Release\n\n".format(currentVer, releaseType))
-  if releaseType != "Patch":
-    f.write("For detailed list of changes see the [detailed changelog.](./docs/changehistory/{0}.md)".format(currentVer))
-  f.write("## Changes\n\n")
-  for commit in commits[::-1]:
-    f.write("- {0}\n".format(getCommitMessage(commit)))
-  f.write("\n**Full changelog:** [{0}...{1}](https://github.com/iTwin/itwinjs-core/compare/{2}...{3})\n".format(previousVer, currentVer, previousTag, currentTag))
+    # Write commit messages to release notes
+    f.write("## Changes\n\n")
+    for commit in commits[::-1]:
+      f.write("- {0}\n".format(getCommitMessage(commit)))
+    f.write("\n")
+    f.write("**Full changelog:** [{0}...{1}](https://github.com/iTwin/itwinjs-core/compare/{2}...{3})\n".format(previousVer, currentVer, previousTag, tag))
+
+  else:
+    # If major/minor release, link to the changelog in ./docs/changehistory
+    f.write("For the full list of changes see the [detailed release notes.](./docs/changehistory/{0}.md)\n".format(currentVer))
+
   f.close()
 
+  print("Publishing GitHub release...")
+  cmd = ['gh', 'release', 'create', tag, '-F', './' + fileName, '-t', '"{0} {1} Release"'.format(currentVer, releaseType)]
+  proc = subprocess.Popen(" ".join(cmd), stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+  proc.wait()
 
-# Delete file if it exists
-if os.path.exists("release.md"):
-  os.remove("release.md")
+# Validate arguments
+if len(sys.argv) != 2:
+  sys.exit("Invalid number of arguments to script provided.\nExpected: 1\nReceived: {0}".format(len(sys.argv) - 1))
 
-createRelease("release/3.2.0", "release/3.2.1")
-# createRelease("release/3.2.1")
-
-# getCommitInfo("640a0436b5272253d1ad03ade8d845a898205c12")
+releaseTag = sys.argv[1]
+print("Creating release for " + releaseTag)
+createRelease(releaseTag)
