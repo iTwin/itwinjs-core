e5859e193d37147b86cdb6dd6a73ace5bb359e63
pmconne <22944042+pmconne@users.noreply.github.com>
2022-06-20 14:02:03 -0400

    String.replaceAll no existe.

--- full-stack-tests/backend/src/integration/BriefcaseManager.test.ts
+++ full-stack-tests/backend/src/integration/BriefcaseManager.test.ts
@@ -51,9 +51,9 @@ describe("BriefcaseManager", () => {
     assert.strictEqual(iModel.changeset.id, expectedChangeSet.id);
 
     // the v2 checkpoint should be opened directly
-    // Convert path separators to Windows for consistent results.
-    const expectedPathName = `\\imodelblocks-73c9d3f0-3a47-41d6-8d2a-c0b0e4099f6a\\BASELINE.bim`;
-    const actualPathName = iModel.pathName.replaceAll("/", "\\");
+    // Convert to UNIX path separators on Windows for consistent results.
+    const actualPathName = iModel.pathName.replace(/\\/g, "/");
+    const expectedPathName = `/imodelblocks-73c9d3f0-3a47-41d6-8d2a-c0b0e4099f6a/BASELINE.bim`;
     expect(actualPathName).equals(expectedPathName);
     iModel.close();
   });
