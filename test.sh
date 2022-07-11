6a1868e273199644a634cb718062987e326c76fb
kaung.zan <kaung.zan@users.noreply.github.com>
2022-07-11 16:43:55 -0400

    validate bump test

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
-getVer="3.3.0-dev.4.5"
-curVer=$(sed -e 's/^"//' -e 's/"$//' <<<"$getVer")
-echo "$curVer <- current version"
-
-if [[ $curVer =~ ^-?[0-9]+\.[0-9]+\.[0-9]+-dev\.[0-9]+ ]] && [[ a == b ]]
+branch="release/2.3.xsdaf"
+if [[ $branch =~ ^release\/[0-9]+\.[0-9]+\.x$ ]]
 then
-  echo "it is dev ver"
+  echo $branch
 fi
