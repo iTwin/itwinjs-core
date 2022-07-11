5dc5ede229d112115376f0f0342ca8fa503b8b17
kaung.zan <kaung.zan@users.noreply.github.com>
2022-07-11 08:27:59 -0400

    codeQL fix 2

--- core/geometry/src/test/GeometryCoreTestIO.ts
+++ core/geometry/src/test/GeometryCoreTestIO.ts
@@ -35,7 +35,7 @@ export class GeometryCoreTestIO {
         fs.mkdirSync(path);
     }
     let fullPath = `${path}/${fileName}`;
-    if (fileName.search(".imjs$") === 0)
+    if (fileName.search(".\imjs$") === -1)
         fullPath = `${fullPath}.imjs`;
 
     console.log(`saveGeometry:: ${fullPath}`);
