e296427e177cf8a4b297a77294af0c52d399efae
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2022-07-13 17:07:58 -0400

    enable gcs workspace from https://geocoord-workspace.itwinjs.org/

--- core/backend/src/GeoCoordConfig.ts
+++ core/backend/src/GeoCoordConfig.ts
@@ -46,7 +46,7 @@ export class GeoCoordConfig {
         return; // already had this db
 
       IModelHost.platform.enableLocalGcsFiles(false);
-      Logger.logInfo(loggerCat, `loaded gcsDb "${gcsDbName}", size=${gcsDbProps.totalBlocks}, local=${gcsDbProps.localBlocks}`);
+      Logger.logInfo(loggerCat, `loaded gcsDb "${gcsDbName}", from "${account.accessName}${containerProps.containerId}" size=${gcsDbProps.totalBlocks}, local=${gcsDbProps.localBlocks}`);
 
       if (true === dbProps.prefetch)
         new IModelHost.platform.CloudPrefetch(cloudContainer, gcsDbName);
@@ -80,9 +80,8 @@ export class GeoCoordConfig {
     }
   }
 
-  public static loadForImodel(_settings: Settings) {
-    // TODO: Enable when gcs workspaces exist
-    // this.loadDefaultDatabases();
-    // this.loadAll(settings, "gcs/databases");
+  public static loadForImodel(settings: Settings) {
+    this.loadDefaultDatabases();
+    this.loadAll(settings, "gcs/databases");
   }
 }
