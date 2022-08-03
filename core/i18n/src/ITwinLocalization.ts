fbce15756e623bfe3cf5a4d9971790093636140c
aidantran <76795558+AidanTran@users.noreply.github.com>
2022-08-03 12:39:55 -0400

    cleaned up old import

--- core/i18n/src/ITwinLocalization.ts
+++ core/i18n/src/ITwinLocalization.ts
@@ -9,7 +9,6 @@
 import i18next, { i18n, InitOptions, Module, TOptionsBase } from "i18next";
 import i18nextBrowserLanguageDetector, { DetectorOptions } from "i18next-browser-languagedetector";
 import { BackendOptions } from "i18next-http-backend";
-import XHR from "i18next-xhr-backend";
 import HTTP from "i18next-http-backend";
 import { Logger } from "@itwin/core-bentley";
 import type { Localization } from "@itwin/core-common";
