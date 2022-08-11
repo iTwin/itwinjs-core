11bd8426dcc41c4c6c6849d0e1d0c6adfc55c903
Matthew Jordan <14913576+mattbjordan@users.noreply.github.com>
2022-08-11 13:57:58 -0400

    add testing capability to core-i18n

---
+++ core/i18n/src/test/ITwinLocalization.test.ts
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { assert } from "chai";
+
+describe("ITwinLocalization", () => {
+
+  // before(async () => {
+  //   return;
+  // });
+
+  // after(async () => {
+  //   return;
+  // });
+
+  it("test 1", () => {
+    assert.isTrue(true);
+    return;
+  });
+
+});
