e592cd860307afbdc1b828995c74cfcada129f2d
aidantran <76795558+AidanTran@users.noreply.github.com>
2022-08-12 09:39:14 -0400

    moving test step higher up, adding more to setup.js to see if it will fix

--- full-stack-tests/node-imodelconnection/src/setup.js
+++ full-stack-tests/node-imodelconnection/src/setup.js
@@ -2,4 +2,11 @@ const fetch = require("node-fetch");
 global.fetch = fetch;
 global.Request = fetch.Request;
 
-console.log("completed setup.js");
+require('jsdom-global')();
+window.Date = Date;
+document.elementFromPoint = () => null;
+
+const {
+  JSDOM
+} = require('jsdom');
+global.DOMParser = new JSDOM().window.DOMParser;
