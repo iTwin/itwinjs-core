7176df44e88f4a848e21586d47ec94a6446377b1
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2022-08-18 11:10:53 -0400

    add function for creating profiles of code regions

--- core/transformer/src/test/IModelTransformerUtils.ts
+++ core/transformer/src/test/IModelTransformerUtils.ts
@@ -5,6 +5,8 @@
 
 import { assert, expect } from "chai";
 import * as path from "path";
+import * as fs from "fs";
+import * as inspector from "inspector";
 import { AccessToken, CompressedId64Set, DbResult, Guid, Id64, Id64Set, Id64String, Mutable } from "@itwin/core-bentley";
 import { Schema } from "@itwin/ecschema-metadata";
 import { Point3d, Transform, YawPitchRollAngles } from "@itwin/core-geometry";
@@ -1398,4 +1400,50 @@ export function copyDbPreserveId(sourceDb: IModelDb, pathForCopy: string) {
   // eslint-disable-next-line @typescript-eslint/dot-notation
   copy["_iModelId"] = sourceDb.iModelId;
   return copy;
+}
+
+/**
+ * Runs a function under the cpu profiler, by default creates cpu profiles in the working directory of
+ * the test runner process.
+ * You can override the default across all calls with the environment variable ITWIN_TESTS_CPUPROF_DIR,
+ * or per functoin just pass a specific `profileDir`
+ */
+export async function runWithCpuProfiler<F extends () => any>(
+  f: F,
+  {
+    profileDir = process.env.ITWIN_TESTS_CPUPROF_DIR ?? process.cwd(),
+    /** append an ISO timestamp to the name you provided */
+    timestamp = true,
+    profileName = "profile",
+    /** an extension to append to the profileName, including the ".". Defaults to ".js.cpuprofile" */
+    profileExtension = ".js.cpuprofile",
+  } = {}
+): Promise<ReturnType<F>> {
+  const maybeNameTimePortion = timestamp ? `_${new Date().toISOString()}` : "";
+  const profilePath = path.join(profileDir, `${profileName}${maybeNameTimePortion}${profileExtension}`);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // implementation influenced by https://github.com/wallet77/v8-inspector-api/blob/master/src/utils.js
+  const invokeFunc = async (thisSession: inspector.Session, funcName: string, args: any = {}) => {
+    return new Promise<void>((resolve, reject) => {
+      thisSession.post(funcName, args, (err) => err ? reject(err) : resolve());
+    });
+  };
+  const stopProfiler = async (thisSession: inspector.Session, funcName: "Profiler.stop", writePath: string) => {
+    return new Promise<void>((resolve, reject) => {
+      thisSession.post(funcName, async (err, res) => {
+        if (err) return reject(err);
+        await fs.promises.writeFile(writePath, JSON.stringify(res.profile));
+        resolve();
+      });
+    });
+  };
+  const session = new inspector.Session();
+  session.connect();
+  await invokeFunc(session, "Profiler.enable");
+  await invokeFunc(session, "Profiler.start", { profilePath });
+  const result = await f();
+  await stopProfiler(session, "Profiler.stop", profilePath);
+  await invokeFunc(session, "Profiler.disable");
+  session.disconnect();
+  return result;
 }
