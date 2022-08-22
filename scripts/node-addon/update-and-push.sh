2de82c300df4a481f02c5ba4b97fee0fad43c0fe
pmconne <22944042+pmconne@users.noreply.github.com>
2022-08-22 17:22:19 -0400

    WIP push-and-update

---
+++ scripts/node-addon/update-and-push.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+usage() {
+  >&2 echo "Usage: $0 -v NewNativePackageVersion" 1>&2
+}
+
+AddonVersion=""
+while getopts "v:" options; do
+  case "${options}" in
+    v) AddonVersion=${OPTARG} ;;
+    ?) usage ; exit 1 ;;
+  esac
+done
+
+if [ "$AddonVersion" = "" ]
+then
+  usage
+  exit 1
+fi
+
+RepoRoot=`git rev-parse --show-toplevel`
+if [ $? -ne 0 ]
+then
+  >&2 echo "This script must be executed from within itwinjs-core repo."
+  exit 1
+fi
+
+checkfail() {
+  if [ $? -ne 0 ]
+  then
+    >&2 echo "Error - aborting."
+    exit 1
+  fi
+}
+
+rm $RepoRoot/common/config/rush/browser-approved-packages.json
+checkfail
+rm $RepoRoot/common/config/rush/pnpm-lock.yaml
+checkfail
+rm -rf $RepoRoot/common/temp
+checkfail
+
+###TODO update package version
+
+rush update
+checkfail
+
+git commit -am"@bentley/imodeljs-native $AddonVersion"
+checkfail
+
+yes "" | rush change
+checkfail
+
+git add $RepoRoot/common/changes
+git commit -m"rush change"
+
+git push -u origin HEAD
+
