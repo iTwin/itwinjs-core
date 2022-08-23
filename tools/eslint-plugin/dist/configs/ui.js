4a211a0dbecc57696c78c9a0bdbd075dd6143c91
paulius.valiunas <66480813+paulius-valiunas@users.noreply.github.com>
2022-08-23 16:01:21 +0300

    move react rules out of itwinjs-recommended

--- tools/eslint-plugin/dist/configs/ui.js
+++ tools/eslint-plugin/dist/configs/ui.js
@@ -5,11 +5,14 @@
 module.exports = {
   "plugins": [
     "jam3",
-    "jsx-a11y"
+    "jsx-a11y",
+    "react-hooks",
+    "react"
   ],
   "extends": [
     "plugin:@itwin/itwinjs-recommended",
-    "plugin:jsx-a11y/recommended"
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended"
   ],
   "rules": {
     "jam3/no-sanitizer-with-danger": 2,
@@ -23,7 +26,11 @@ module.exports = {
         ]
       }
     ],
-    "@typescript-eslint/unbound-method": "off"
+    "@itwin/react-set-state-usage": ["error", { "updater-only": false, "allow-object": true }],
+    "@typescript-eslint/unbound-method": "off",
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
   },
   "settings": {
     "react": {
