be25874c044d0b28ed94ef9a9687f82415286008
kabentley <33296803+kabentley@users.noreply.github.com>
2022-08-24 14:10:30 -0400

    move `CloudSqlite` namespace to core-backend (#4156)
    * move CloudSqlite namespace to core-backend
    
    * extract-api
    
    * extract-api, again
    
    * make SQLiteDb open/create with containers @beta
    
    * create functions should be @beta
    
    Co-authored-by: Paul Connelly <22944042+pmconne@users.noreply.github.com>

--- utils/workspace-editor/src/WorkspaceEditor.ts
+++ utils/workspace-editor/src/WorkspaceEditor.ts
@@ -11,8 +11,8 @@ import { extname, join } from "path";
 import * as readline from "readline";
 import * as Yargs from "yargs";
 import {
-  CloudSqlite, EditableWorkspaceDb, IModelHost, IModelJsFs, ITwinWorkspaceContainer, ITwinWorkspaceDb, SQLiteDb,
-  SqliteStatement, WorkspaceAccount, WorkspaceContainer, WorkspaceDb, WorkspaceResource,
+  CloudSqlite, EditableWorkspaceDb, IModelHost, IModelJsFs, ITwinWorkspaceContainer, ITwinWorkspaceDb, SQLiteDb, SqliteStatement, WorkspaceAccount,
+  WorkspaceContainer, WorkspaceDb, WorkspaceResource,
 } from "@itwin/core-backend";
 import { BentleyError, DbResult, Logger, LogLevel, OpenMode, StopWatch } from "@itwin/core-bentley";
 import { IModelError, LocalDirName, LocalFileName } from "@itwin/core-common";
@@ -124,7 +124,7 @@ function showMessage(msg: string) {
 }
 
 /** perform a vacuum on a database, with a message while it's happening */
-function doVacuum(dbName: string, container?: SQLiteDb.CloudContainer) {
+function doVacuum(dbName: string, container?: CloudSqlite.CloudContainer) {
   process.stdout.write(`Vacuuming ${dbName} ... `);
   const db = new SQLiteDb();
   db.withOpenDb({ dbName, openMode: OpenMode.ReadWrite, container }, () => db.vacuum());
@@ -168,7 +168,7 @@ function getContainer(args: EditorOpts) {
 }
 
 /** get a WorkspaceContainer that is expected to be a cloud container, throw otherwise. */
-function getCloudContainer(args: EditorOpts): SQLiteDb.CloudContainer {
+function getCloudContainer(args: EditorOpts): CloudSqlite.CloudContainer {
   args.syncOnConnect = true;
   const container = getContainer(args);
   const cloudContainer = container.cloudContainer;
@@ -207,7 +207,7 @@ async function listWorkspaceDb(args: ListOptions) {
   await readWorkspace(args, async (file, args) => {
     const cloudContainer = file.container.cloudContainer;
     const timer = new StopWatch("list", true);
-    let prefetch: SQLiteDb.CloudPrefetch | undefined;
+    let prefetch: CloudSqlite.CloudPrefetch | undefined;
     if (args.prefetch && cloudContainer) {
       console.log(`start prefetch`);
       prefetch = file.prefetch({ nRequests: args.nRequests });
@@ -362,7 +362,7 @@ async function vacuumWorkspaceDb(args: WorkspaceDbOpt) {
 }
 
 /** Either upload or download a WorkspaceDb to/from a cloud WorkspaceContainer. Shows progress % during transfer */
-async function performTransfer(container: SQLiteDb.CloudContainer, direction: CloudSqlite.TransferDirection, args: TransferOptions) {
+async function performTransfer(container: CloudSqlite.CloudContainer, direction: CloudSqlite.TransferDirection, args: TransferOptions) {
   fixVersionArg(args);
   const localFileName = args.localFileName;
 
@@ -439,7 +439,7 @@ async function initializeWorkspace(args: InitializeOpts) {
     if (yesNo[0].toUpperCase() !== "Y")
       return;
   }
-  const container = SQLiteDb.createCloudContainer(args as CloudSqlite.ContainerAccessProps);
+  const container = CloudSqlite.createCloudContainer(args as CloudSqlite.ContainerAccessProps);
   container.initializeContainer({ checksumBlockNames: true });
   showMessage(`container "${args.containerId} initialized`);
 }
@@ -485,7 +485,7 @@ async function preFetchWorkspaceDb(args: WorkspaceDbOpt) {
   fixVersionArg(args);
   const container = getCloudContainer(args);
   const timer = new StopWatch("prefetch", true);
-  const prefetch = SQLiteDb.startCloudPrefetch(container, args.dbFileName);
+  const prefetch = CloudSqlite.startCloudPrefetch(container, args.dbFileName);
   await prefetch.promise;
   showMessage(`preFetched WorkspaceDb [${args.dbFileName}] in ${sayContainer(args)}, time=${timer.elapsedSeconds.toString()}`);
 }
