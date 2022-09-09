18f43ebd30c1b88705eb1711a222e6e6d37abb73
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2022-09-09 12:13:59 -0700

    Converted runIos.mjs to typescript (and renamed it).

--- test-apps/display-test-app/runIosSimulator.ts
+++ test-apps/display-test-app/runIosSimulator.ts
@@ -1,46 +1,34 @@
-import { Simctl } from "node-simctl";
-import { copyFile, readdir } from 'node:fs/promises';
-import { homedir } from 'node:os';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { copyFile, readdir } from 'fs/promises';
+import { homedir } from 'os';
+// Can't use import here otherwise Typescript complains: Could not find a declaration file for module 'node-simctl'.
+const Simctl = require("node-simctl").default;
 
 const appName = "imodeljs-test-app"
 const bundleId = `bentley.${appName}`;
 const bimFile = "mirukuru.ibim";
 
-/**
- * Copy of the launch function but it adds the --console option
- * and allows for args.
- * 
- * Execute the particular application package on Simulator.
- * It is required that Simulator is in _booted_ state and
- * the application with given bundle identifier is already installed.
- *
- * @param {string} bundleId - Bundle identifier of the application,
- *                            which is going to be removed.
- * @param {string[]} args - Other args to pass to the app.
- * @return {Promise<string>} the actual command output
- * @throws {Error} If the corresponding simctl subcommand command
- *                 returns non-zero return code.
- * @throws {Error} If the `udid` instance property is unset
- */
- Simctl.prototype.launchWithConsole = async function launchWithConsole(bundleId, ...args) {
+// Similar to the launchApp function but doesn't retry, adds the --console option, and allows for args.
+Simctl.prototype.launchAppWithConsole = async function (bundleId: string, ...args: [string]) {
   const {stdout} = await this.exec('launch', {
     args: ["--console", this.requireUdid('launch'), bundleId, ...args],
   });
   return stdout.trim();
 }
 
-async function go() {
+async function main() {
   const simctl = new Simctl();
   process.exitCode = 1;
 
   console.log("Getting iOS devices");
   const results = await simctl.getDevices(undefined, 'iOS');
-  var device = undefined;
+  var device: { name: string; sdk: string; udid: string; state: string; } | undefined;
   const keys = Object.keys(results).filter(key => key.startsWith("13"));
   if (!keys.length) {
-    console.log("No simulators for iOS 13.x found.");
+    console.log("No simulators for iOS 13 found.");
     return;
   }
 
@@ -50,14 +38,14 @@ async function go() {
   
   // Look for a booted simulator
   for (const key of keys) {
-    device = results[key].find(curr => curr.state === "Booted");
+    device = results[key].find((curr: { state: string; }) => curr.state === "Booted");
     if (device)
       break;
   }
 
   // If none are booted, use the iPad Pro (ii-inch) if available, otherwise the first one listed
   if (!device) {
-    device = results[keys[0]].find(device => device.name.startsWith("iPad Pro (11")) ?? results[keys[0]][0];
+    device = results[keys[0]].find((device: { name: string; }) => device.name.startsWith("iPad Pro (11")) ?? results[keys[0]][0];
   }
 
   if (!device) {
@@ -65,7 +53,7 @@ async function go() {
     return;
   }
 
-  // Select the simulator we're using
+  // Select the simulator we're using with simctl
   console.log(`Using simulator: ${device.name} iOS: ${device.sdk}`);
   simctl.udid = device.udid;
 
@@ -89,12 +77,12 @@ async function go() {
 
   // Copy the model to the simulator's Documents dir
   const container = await simctl.getAppContainer(bundleId, "data");
-  const assetsPath=`${dirname(fileURLToPath(import.meta.url))}/../../core/backend/src/test/assets`;
+  const assetsPath=`${__dirname}/../../core/backend/src/test/assets`;
   await copyFile(`${assetsPath}/${bimFile}`, `${container}/Documents/${bimFile}`);
 
   // Launch the app instructing it to open the model and exit
   console.log("Launching app");
-  const stdout = await simctl.launchWithConsole(bundleId, `IMJS_STANDALONE_FILENAME=${bimFile}`, "IMJS_EXIT_AFTER_MODEL_OPENED=1");
+  const stdout = await simctl.launchAppWithConsole(bundleId, `IMJS_STANDALONE_FILENAME=${bimFile}`, "IMJS_EXIT_AFTER_MODEL_OPENED=1");
   if (stdout.includes("iModel opened")) {
     process.exitCode = 0;
     console.log("Success!");
@@ -103,4 +91,4 @@ async function go() {
   }
 }
 
-go();
+main();
