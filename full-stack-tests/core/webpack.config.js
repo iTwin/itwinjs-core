6bea4000fe06549f94ee21b1ef0216ce2b987b54
paulius.valiunas <66480813+paulius-valiunas@users.noreply.github.com>
2022-09-14 15:25:39 +0300

    add override for sinon

--- full-stack-tests/core/webpack.config.js
+++ full-stack-tests/core/webpack.config.js
@@ -48,7 +48,8 @@ function createConfig(shouldInstrument) {
       alias: {
         "@azure/storage-blob$": "@azure/storage-blob/dist-esm/storage-blob/src/index.browser.js",
         "@azure/core-http$": "@azure/core-http/dist-esm/src/coreHttp.js",
-        "@azure/logger$": "@azure/logger/dist-esm/src/index.js"
+        "@azure/logger$": "@azure/logger/dist-esm/src/index.js",
+        "supports-color$": "supports-color/browser.js"
       }
     },
     module: {
