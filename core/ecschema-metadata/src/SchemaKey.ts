85af88c70c2b4c21404dff3e535d36305133a506
pmconne <22944042+pmconne@users.noreply.github.com>
2022-09-25 04:58:53 -0400

    ecschema-metadata

--- core/ecschema-metadata/src/SchemaKey.ts
+++ core/ecschema-metadata/src/SchemaKey.ts
@@ -27,9 +27,12 @@ export class ECVersion {
    *
    */
   constructor(read?: number, write?: number, minor?: number) {
-    if (undefined !== read) this._read = read;
-    if (undefined !== write) this._write = write;
-    if (undefined !== minor) this._minor = minor;
+    if (undefined !== read)
+      this._read = read;
+    if (undefined !== write)
+      this._write = write;
+    if (undefined !== minor)
+      this._minor = minor;
 
     if (this._read > 999 || this._read < 0 || this._write > 999 || this._write < 0 || this._minor > 9999999 || this._minor < 0)
       throw new ECObjectsError(ECObjectsStatus.InvalidECVersion);
