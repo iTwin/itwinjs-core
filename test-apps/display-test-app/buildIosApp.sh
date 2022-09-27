5d720be1867ba954556cccf62d4b6d09ab96bdd3
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2022-09-26 19:52:19 -0700

    Some formatting changes.

--- test-apps/display-test-app/buildIosApp.sh
+++ test-apps/display-test-app/buildIosApp.sh
@@ -2,16 +2,17 @@
 
 cd ios/imodeljs-test-app || { echo "cd failed"; exit 1; }
 echo "Getting Xcode build settings"
-BUILD_SETTINGS=$(xcodebuild -showBuildSettings 2>/dev/null) || { echo "xcodebuild -showBuildSettings failed"; exit 1; }
+BUILD_SETTINGS=$(xcrun xcodebuild -resolvePackageDependencies -showBuildSettings) || { echo "xcrun xcodebuild -resolvePackageDependencies -showBuildSettings failed"; exit 1; }
 echo "Getting BUILD_DIR"
 BUILD_DIR=$(grep -E '^[ ]+BUILD_DIR = ' <<< "${BUILD_SETTINGS}") || { echo "grep for BUILD_DIR failed, full output from xcodebuild:"; echo "$BUILD_SETTINGS"; exit 1; }
 SYMROOT=${BUILD_DIR# *BUILD_DIR = }
 SYMROOT=${SYMROOT%/Products}
-echo "SYMROOT: $SYMROOT"
-
 if [ -z "$SYMROOT" ]; then
     echo "ERROR: SYMROOT is empty"
     exit 1
 fi
+echo "SYMROOT: $SYMROOT"
 
-xcodebuild build SYMROOT="${SYMROOT}" CODE_SIGN_STYLE='Manual' CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO PROVISIONING_PROFILE_SPECIFIER='' -arch x86_64 -sdk iphonesimulator -configuration Debug
+CODE_SIGNING=("CODE_SIGN_STYLE='Manual'" "CODE_SIGN_IDENTITY=''" "CODE_SIGNING_REQUIRED=NO" "PROVISIONING_PROFILE_SPECIFIER=''")
+OPTS=("-arch" "x86_64" "-sdk" "iphonesimulator" "-configuration" "Debug")
+xcrun xcodebuild build SYMROOT="${SYMROOT}" "${CODE_SIGNING[@]}" "${OPTS[@]}"
