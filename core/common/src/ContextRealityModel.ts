f39acaa404890eee11ba8a57bcd81f68f456e75b
pmconne <22944042+pmconne@users.noreply.github.com>
2022-10-02 10:15:56 -0400

    ContextRealityModel.displaySettings

--- core/common/src/ContextRealityModel.ts
+++ core/common/src/ContextRealityModel.ts
@@ -10,6 +10,7 @@ import { assert, BeEvent } from "@itwin/core-bentley";
 import { FeatureAppearance, FeatureAppearanceProps } from "./FeatureSymbology";
 import { PlanarClipMaskMode, PlanarClipMaskProps, PlanarClipMaskSettings } from "./PlanarClipMask";
 import { SpatialClassifierProps, SpatialClassifiers } from "./SpatialClassification";
+import { RealityModelDisplayProps, RealityModelDisplaySettings } from "./RealityModelDisplaySettings";
 
 /** JSON representation of the blob properties for an OrbitGt property cloud.
  * @alpha
@@ -159,6 +160,8 @@ export interface ContextRealityModelProps {
   planarClipMask?: PlanarClipMaskProps;
   /** See [[ContextRealityModel.appearanceOverrides]]. */
   appearanceOverrides?: FeatureAppearanceProps;
+  /** See [[ContextRealityModel.displaySettings]]. */
+  displaySettings?: RealityModelDisplayProps;
 }
 
 /** @public */
@@ -188,6 +191,12 @@ export namespace ContextRealityModelProps {
       output.appearanceOverrides = { ...input.appearanceOverrides };
       if (input.appearanceOverrides.rgb)
         output.appearanceOverrides.rgb = { ...input.appearanceOverrides.rgb };
+    }
+
+    if (input.displaySettings) {
+      output.displaySettings = { ...input.displaySettings };
+      if (input.displaySettings.pointCloud)
+        output.displaySettings.pointCloud = { ...output.displaySettings.pointCloud };
     }
 
     if (input.planarClipMask)
@@ -228,11 +237,14 @@ export class ContextRealityModel {
   /** @alpha */
   public readonly orbitGtBlob?: Readonly<OrbitGtBlobProps>;
   protected _appearanceOverrides?: FeatureAppearance;
+  protected _displaySettings: RealityModelDisplaySettings;
   protected _planarClipMask?: PlanarClipMaskSettings;
   /** Event dispatched just before assignment to [[planarClipMaskSettings]]. */
   public readonly onPlanarClipMaskChanged = new BeEvent<(newSettings: PlanarClipMaskSettings | undefined, model: ContextRealityModel) => void>();
   /** Event dispatched just before assignment to [[appearanceOverrides]]. */
   public readonly onAppearanceOverridesChanged = new BeEvent<(newOverrides: FeatureAppearance | undefined, model: ContextRealityModel) => void>();
+  /** Event dispatched just before assignment to [[displaySettings]]. */
+  public readonly onDisplaySettingsChanged = new BeEvent<(newSettings: RealityModelDisplaySettings, model: ContextRealityModel) => void>();
 
   /** Construct a new context reality model.
    * @param props JSON representation of the reality model, which will be kept in sync with changes made via the ContextRealityModel's methods.
@@ -246,6 +258,8 @@ export class ContextRealityModel {
     this.realityDataId = props.realityDataId;
     this.description = props.description ?? "";
     this._appearanceOverrides = props.appearanceOverrides ? FeatureAppearance.fromJSON(props.appearanceOverrides) : undefined;
+    this._displaySettings = RealityModelDisplaySettings.fromJSON(props.displaySettings);
+
     if (props.planarClipMask && props.planarClipMask.mode !== PlanarClipMaskMode.None)
       this._planarClipMask = PlanarClipMaskSettings.fromJSON(props.planarClipMask);
 
@@ -279,6 +293,15 @@ export class ContextRealityModel {
       this._props.appearanceOverrides = overrides.toJSON();
 
     this._appearanceOverrides = overrides;
+  }
+
+  public get displaySettings(): RealityModelDisplaySettings {
+    return this._displaySettings;
+  }
+  public set displaySettings(settings: RealityModelDisplaySettings) {
+    this.onDisplaySettingsChanged.raiseEvent(settings, this);
+    this._props.displaySettings = settings.toJSON();
+    this._displaySettings = settings;
   }
 
   /** Convert this model to its JSON representation. */
@@ -317,6 +340,7 @@ export class ContextRealityModels {
   public readonly onPlanarClipMaskChanged = new BeEvent<(model: ContextRealityModel, newSettings: PlanarClipMaskSettings | undefined) => void>();
   /** Event dispatched just before [[ContextRealityModel.appearanceOverrides]] is modified for one of the reality models. */
   public readonly onAppearanceOverridesChanged = new BeEvent<(model: ContextRealityModel, newOverrides: FeatureAppearance | undefined) => void>();
+  public readonly onDisplaySettingsChanged = new BeEvent<(model: ContextRealityModel, newSettings: RealityModelDisplaySettings) => void>();
   /** Event dispatched when a model is [[add]]ed, [[delete]]d, [[replace]]d, or [[update]]d. */
   public readonly onChanged = new BeEvent<(previousModel: ContextRealityModel | undefined, newModel: ContextRealityModel | undefined) => void>();
 
@@ -447,6 +471,7 @@ export class ContextRealityModels {
     model.onPlanarClipMaskChanged.addListener(this.handlePlanarClipMaskChanged, this);
     // eslint-disable-next-line @typescript-eslint/unbound-method
     model.onAppearanceOverridesChanged.addListener(this.handleAppearanceOverridesChanged, this);
+    model.onDisplaySettingsChanged.addListener(this.handleDisplaySettingsChanged, this);
     return model;
   }
 
@@ -455,6 +480,7 @@ export class ContextRealityModels {
     model.onPlanarClipMaskChanged.removeListener(this.handlePlanarClipMaskChanged, this);
     // eslint-disable-next-line @typescript-eslint/unbound-method
     model.onAppearanceOverridesChanged.removeListener(this.handleAppearanceOverridesChanged, this);
+    model.onDisplaySettingsChanged.removeListener(this.handleDisplaySettingsChanged, this);
   }
 
   private handlePlanarClipMaskChanged(mask: PlanarClipMaskSettings | undefined, model: ContextRealityModel): void {
@@ -463,5 +489,9 @@ export class ContextRealityModels {
 
   private handleAppearanceOverridesChanged(app: FeatureAppearance | undefined, model: ContextRealityModel): void {
     this.onAppearanceOverridesChanged.raiseEvent(model, app);
+  }
+
+  private handleDisplaySettingsChanged(settings: RealityModelDisplaySettings, model: ContextRealityModel): void {
+    this.onDisplaySettingsChanged.raiseEvent(model, settings);
   }
 }
