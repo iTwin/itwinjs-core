0985c695ee95dd06b9f945e65a2fcd351123e2df
pmconne <22944042+pmconne@users.noreply.github.com>
2022-10-12 18:00:47 -0400

    wip test

--- full-stack-tests/core/src/frontend/standalone/tile/ElementGraphics.test.ts
+++ full-stack-tests/core/src/frontend/standalone/tile/ElementGraphics.test.ts
@@ -4,11 +4,12 @@
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
 import { Guid } from "@itwin/core-bentley";
+import { Transform } from "@itwin/core-geometry";
 import { PersistentGraphicsRequestProps } from "@itwin/core-common";
 import { IModelApp, MockRender, readElementGraphics, SnapshotConnection } from "@itwin/core-frontend";
 import { TestUtility } from "../../TestUtility";
 
-describe("requestElementGraphics", () => {
+describe.only("requestElementGraphics", () => {
   let imodel: SnapshotConnection;
 
   before(async () => {
@@ -23,40 +24,98 @@ describe("requestElementGraphics", () => {
     await TestUtility.shutdownFrontend();
   });
 
-  async function expectQuantized(requestQuantized: boolean | undefined, expected: boolean): Promise<void> {
-    const requestProps: PersistentGraphicsRequestProps = {
-      elementId: "0x29",
-      id: Guid.createValue(),
-      toleranceLog10: -3,
-    };
+  describe("quantization", () => {
+    async function expectQuantized(requestQuantized: boolean | undefined, expected: boolean): Promise<void> {
+      const requestProps: PersistentGraphicsRequestProps = {
+        elementId: "0x29",
+        id: Guid.createValue(),
+        toleranceLog10: -3,
+      };
 
-    if (undefined !== requestQuantized)
-      requestProps.quantizePositions = requestQuantized;
+      if (undefined !== requestQuantized)
+        requestProps.quantizePositions = requestQuantized;
 
-    const bytes = await IModelApp.tileAdmin.requestElementGraphics(imodel, requestProps);
-    expect(bytes).not.to.be.undefined;
+      const bytes = await IModelApp.tileAdmin.requestElementGraphics(imodel, requestProps);
+      expect(bytes).not.to.be.undefined;
 
-    let createdMesh = false;
-    IModelApp.renderSystem.createMeshGeometry = (params, _origin) => {
-      expect(params.vertices.usesUnquantizedPositions).to.equal(!expected);
-      createdMesh = true;
-      return new MockRender.Geometry();
-    };
+      let createdMesh = false;
+      IModelApp.renderSystem.createMeshGeometry = (params, _origin) => {
+        expect(params.vertices.usesUnquantizedPositions).to.equal(!expected);
+        createdMesh = true;
+        return new MockRender.Geometry();
+      };
 
-    const gfx = await readElementGraphics(bytes!, imodel, "0", true);
-    expect(gfx).not.to.be.undefined;
-    expect(createdMesh).to.be.true;
-  }
+      const gfx = await readElementGraphics(bytes!, imodel, "0", true);
+      expect(gfx).not.to.be.undefined;
+      expect(createdMesh).to.be.true;
+    }
 
-  it("does not quantize positions by default", async () => {
-    await expectQuantized(undefined, false);
+    it("is not applied by default", async () => {
+      await expectQuantized(undefined, false);
+    });
+
+    it("is applied if `quantizePositions` is true", async () => {
+      await expectQuantized(true, true);
+    });
+
+    it("is not applied if `quantizePositions` is false", async () => {
+      await expectQuantized(false, false);
+    });
   });
 
-  it("quantizes positions if explicitly requested", async () => {
-    await expectQuantized(true, true);
-  });
+  describe("relative-to-center transform", () => {
+    async function expectRtc(options: {
+      quantize?: boolean;
+      absolute?: boolean;
+      location?: Transform;
+    }, expectedRtc: number[] | undefined
+    ): Promise<void> {
+      const requestProps: PersistentGraphicsRequestProps = {
+        elementId: "0x29",
+        id: Guid.createValue(),
+        toleranceLog10: -3,
+        quantizePositions: options.quantize,
+        useAbsolutePositions: options.absolute,
+        location: options.location?.toJSON(),
+      };
 
-  it("produces unquantized positions if explicitly requested", async () => {
-    await expectQuantized(false, false);
+      const bytes = (await IModelApp.tileAdmin.requestElementGraphics(imodel, requestProps))!;
+      expect(bytes).not.to.be.undefined;
+
+      let createdMesh = false;
+      IModelApp.renderSystem.createMeshGeometry = (params) => {
+        expect(params.vertices.usesUnquantizedPositions).to.equal(true === options.quantize);
+        // ###TODO inspect mesh geometry
+        createdMesh = true;
+        return new MockRender.Geometry();
+      };
+
+      let actualRtc: number[] | undefined;
+      IModelApp.renderSystem.createGraphicBranch = (branch, transform, options) => {
+        actualRtc = transform.origin.toArray();
+        return new MockRender.Branch(branch, transform, options);
+      };
+
+      const gfx = await readElementGraphics(bytes, imodel, "0", true);
+      expect(gfx).not.to.be.undefined;
+      expect(createdMesh).to.be.true;
+      expect(actualRtc).to.deep.equal(expectedRtc);
+    }
+
+    it("is applied by default", async () => {
+      expectRtc({}, [12, 34, 56]);
+    });
+
+    it("is not applied to quantized positions", async () => {
+    });
+
+    it("is not applied if `useAbsolutePositions` is true", async () => {
+    });
+
+    it("is applied if `useAbsolutePositions` is false", async () => {
+    });
+
+    it("is adjusted based on location transform", async () => {
+    });
   });
 });
