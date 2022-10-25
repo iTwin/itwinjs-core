dffae90b50da9056d19ba442f002f39b2a659066
pmconne <22944042+pmconne@users.noreply.github.com>
2022-10-25 16:13:21 -0400

    wip test

--- core/frontend/src/test/LookAt.test.ts
+++ core/frontend/src/test/LookAt.test.ts
@@ -6,9 +6,11 @@ import { expect } from "chai";
 import { IModelApp } from "../IModelApp";
 import { IModelConnection } from "../IModelConnection";
 import { createBlankConnection } from "./createBlankConnection";
-import {  Cartographic, EcefLocation } from "@itwin/core-common";
+import { Cartographic, EcefLocation } from "@itwin/core-common";
 import { SpatialViewState } from "../SpatialViewState";
-import { Point3d, Vector3d } from "@itwin/core-geometry";
+import { Point3d, Range3d, Vector3d } from "@itwin/core-geometry";
+import { StandardViewId } from "../StandardView";
+import { MarginOptions } from "../ViewAnimation";
 
 describe("Look At", () => {
   let imodel: IModelConnection;
@@ -24,19 +26,75 @@ describe("Look At", () => {
     await IModelApp.shutdown();
   });
 
-  it("lookAtGlobalLocation should change camera", async () => {
-    const view3d = SpatialViewState.createBlank(imodel, new Point3d(), new Point3d());
+  describe("lookAtGlobalLocation", () => {
+    it("should change camera", async () => {
+      const view3d = SpatialViewState.createBlank(imodel, new Point3d(), new Point3d());
 
-    view3d.lookAt({
-      viewDirection: {x: 0.0, y: 0.0, z: 1.0},
-      eyePoint: {x: 0.0, y: 0.0, z: 0.0},
-      upVector: new Vector3d(0.0, 0.0, 1.0),
+      view3d.lookAt({
+        viewDirection: {x: 0.0, y: 0.0, z: 1.0},
+        eyePoint: {x: 0.0, y: 0.0, z: 0.0},
+        upVector: new Vector3d(0.0, 0.0, 1.0),
+      });
+
+      const oldCam = view3d.camera.clone();
+      view3d.lookAtGlobalLocation(1000.0, Math.PI / 4.0, {center: Cartographic.fromDegrees({latitude: 39.144703, longitude: -75.703054})});
+      const newCam = view3d.camera;
+
+      expect(oldCam.equals(newCam)).to.be.false;
+    });
+  });
+
+  describe.only("lookAtViewAlignedVolume", () => {
+    function createTopView(): SpatialViewState {
+      const view = SpatialViewState.createBlank(imodel, new Point3d(), new Point3d());
+      view.setStandardRotation(StandardViewId.Top);
+      expect(view.isCameraOn).to.be.false;
+      return view;
+    }
+
+    // [lowX, lowY, highX, highY
+    type Range = [number, number, number, number];
+
+    // volume is [lowX, lowY, highX, highY]
+    function lookAtVolume(view: SpatialViewState, volume: Range, options?: MarginOptions): void {
+      const range = new Range3d(volume[0], volume[1], -1, volume[2], volume[3], 1);
+      view.lookAtViewAlignedVolume(range, 1.0, options);
+    }
+
+    function expectNumber(actual: number, expected: number): void {
+      expect(Math.round(actual)).to.equal(Math.round(expected));
+    }
+
+    // volume is [lowX, lowY, highX, highY]
+    function expectExtents(volume: Range, extents: Range, options?: MarginOptions, view?: SpatialViewState): void {
+      view = view ?? createTopView();
+      lookAtVolume(view, volume, options);
+
+      const delta = view.getExtents();
+      console.log(JSON.stringify(view.origin));
+      console.log(JSON.stringify(delta));
+      expectNumber(view.origin.x, extents[0]);
+      expectNumber(view.origin.y, extents[1]);
+
+      expectNumber(delta.x, extents[2]);
+      expectNumber(delta.y, extents[3]);
+    }
+
+    it("ignores MarginOptions if camera is on", () => {
+      // ###TODO need to turn camera on...
     });
 
-    const oldCam = view3d.camera.clone();
-    view3d.lookAtGlobalLocation(1000.0, Math.PI / 4.0, {center: Cartographic.fromDegrees({latitude: 39.144703, longitude: -75.703054})});
-    const newCam = view3d.camera;
+    it("applies default dilation of 1.04", () => {
+      expectExtents([0, 0, 100, 100], [-2, -2, 104, 104]);
+    });
 
-    expect(oldCam.equals(newCam)).to.be.false;
+    it("applies MarginPercent", () => {
+    });
+
+    it("applies PaddingPercent", () => {
+    });
+
+    it("prioritizes PaddingPercent over MarginPercent", () => {
+    });
   });
 });
