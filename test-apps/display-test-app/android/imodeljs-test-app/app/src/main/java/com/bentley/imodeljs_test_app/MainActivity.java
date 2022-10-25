1cdf11b15cbe3c2f4fa0126887e4c7017173a5eb
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2022-10-25 12:38:46 -0700

    Created shouldInterceptRequest override to properly load dependent javascript files.

--- test-apps/display-test-app/android/imodeljs-test-app/app/src/main/java/com/bentley/imodeljs_test_app/MainActivity.java
+++ test-apps/display-test-app/android/imodeljs-test-app/app/src/main/java/com/bentley/imodeljs_test_app/MainActivity.java
@@ -1,10 +1,23 @@
 package com.bentley.imodeljs_test_app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.bentley.itwin.IModelJsHost;
 import com.bentley.itwin.MobileFrontend;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     IModelJsHost m_host;
@@ -22,18 +35,36 @@ public class MainActivity extends AppCompatActivity {
         String files = getFilesDir().getPath();
         MobileFrontend frontend = new MobileFrontend(m_host, "&standalone=true&iModelName=" + files + "/JoesHouse.bim") {
             @Override
+            protected void configure() {
+                setWebViewClient(new WebViewClient() {
+                    @Override
+                    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                        Uri url = request.getUrl();
+                        if (url.getScheme().equals("file") && url.getPath().endsWith(".js")) {
+                            try {
+                                InputStream is = getContext().getAssets().open("frontend" + url.getPath(), AssetManager.ACCESS_STREAMING);
+                                return new WebResourceResponse("text/plain", null, is);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return super.shouldInterceptRequest(view, request);
+                    }
+                });
+                super.configure();
+                getSettings().setAllowFileAccess(true);
+            }
+
+            @Override
             protected String supplyEntryPoint() {
-                return super.supplyEntryPoint();
-//                return "192.168.86.20:3000";
+                String entry = super.supplyEntryPoint();
+//                entry = "192.168.86.20:3000";
+                return entry;
             }
         };
         m_host.setFrontend(frontend);
         setContentView(frontend);
         frontend.loadEntryPoint();
-        int port = m_host.getPort();
-        if (port == 9229) {
-            port = port + 0;
-        }
     }
 
     @Override
