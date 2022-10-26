b802bfc3db0c26792c6021e543992f18a7aba706
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-10-26 07:16:04 -0400

    Note no camera view

--- core/frontend/src/MarginPercent.ts
+++ core/frontend/src/MarginPercent.ts
@@ -12,6 +12,7 @@ import { Geometry } from "@itwin/core-geometry";
  * viewed volume by a percentage of its original size to add extra space around one or more edges.
  * The margin values represent a fraction of the view volume's width (for [[left]] and [[right]]) or height (for [[top]] and [[bottom]]).
  * The values are clamped to the range 0 through 0.25.
+ * @note Margins are only applied in 2d views, or 3d views in which the camera has been turned off.
  * @note The way in which the extra space is computed is somewhat unintuitive and may lead to surprising results.
  * @see [[MarginOptions.marginPercent]].
  * @see [[PaddingPercent]] for a more predictable way of adjusting the viewed volume.
@@ -35,7 +36,8 @@ export class MarginPercent {
  * All properties default to zero if `undefined`.
  * For example, if the original volume has a width of 100 and the padding is specified as `{ left: 1 }`, the width will be doubled to 200, with 100 padding on the left side of the view.
  * If a padding of 0.25 is specified for each of [[left]], [[right]], [[top]], and [[bottom]], then the final volume will be inset such that 25% of the original volume falls outside
- * of the view on each side.
+ * of the view on each side.\
+ * @note Margins are only applied in 2d views, or 3d views in which the camera has been turned off.
  * @see [[MarginOptions.paddingPercent]].
  * @public
  * @extensions
