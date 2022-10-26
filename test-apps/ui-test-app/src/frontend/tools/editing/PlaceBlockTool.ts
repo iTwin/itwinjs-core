7dd76c50c93ce591e66fefe7f21fa14416b481f6
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2022-10-26 09:10:45 -0400

    lint error

--- test-apps/ui-test-app/src/frontend/tools/editing/PlaceBlockTool.ts
+++ test-apps/ui-test-app/src/frontend/tools/editing/PlaceBlockTool.ts
@@ -7,7 +7,7 @@
 import { assert } from "@itwin/core-bentley";
 import { AxisOrder, LinearSweep, Matrix3d, Point3d, Transform, Vector3d, YawPitchRollAngles } from "@itwin/core-geometry";
 import { Code, ColorDef, ElementGeometry, GeometryStreamBuilder, LinePixels, PhysicalElementProps } from "@itwin/core-common";
-import { BasicManipulationCommandIpc, editorBuiltInCmdIds } from "@itwin/editor-common";
+import { editorBuiltInCmdIds } from "@itwin/editor-common";
 import { basicManipulationIpc, CreateElementTool, EditTools } from "@itwin/editor-frontend";
 import {
   AccuDrawHintBuilder, BeButtonEvent, ContextRotationId, CoreTools, DecorateContext, EventHandled, GraphicType, IModelApp, NotifyMessageDetails, OutputMessagePriority, ToolAssistance,
