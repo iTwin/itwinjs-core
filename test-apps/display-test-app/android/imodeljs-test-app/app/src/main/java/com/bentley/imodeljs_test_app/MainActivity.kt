8d6bbafb1ec44332c2427f78a006889e3b916269
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2022-11-07 15:35:29 -0800

    Added AssetsPathHandler implementation

--- test-apps/display-test-app/android/imodeljs-test-app/app/src/main/java/com/bentley/imodeljs_test_app/MainActivity.kt
+++ test-apps/display-test-app/android/imodeljs-test-app/app/src/main/java/com/bentley/imodeljs_test_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bentley.imodeljs_test_app
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.res.AssetManager
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.JavascriptInterface
@@ -15,9 +16,11 @@ import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
 import com.bentley.itwin.IModelJsHost
 import com.bentley.itwin.MobileFrontend
+import java.io.IOException
+import java.io.InputStream
+import java.net.URLConnection
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-
 
 typealias PickUriContractType = ActivityResultContract<Nothing?, Uri?>
 
@@ -48,6 +51,31 @@ class PickUriContract : PickUriContractType() {
     }
 }
 
+// Very similar to WebViewAssetLoader.AssetsPathHandler except it doesn't log an error when a resource isn't found, also doesn't handle SvgZ streams.
+class AssetsPathHandler(context: Context) : WebViewAssetLoader.PathHandler {
+    private val mContext = context
+
+    private fun removeLeadingSlash(path: String): String {
+        return if (path.startsWith('/')) path.substring(1) else path
+    }
+
+    private fun openAsset(path: String): InputStream {
+        return mContext.assets.open(removeLeadingSlash(path), AssetManager.ACCESS_STREAMING)
+    }
+
+    private fun guessMimeType(path: String): String {
+        return URLConnection.guessContentTypeFromName(path) ?: "text/plain"
+    }
+
+    override fun handle(path: String): WebResourceResponse? {
+        return try {
+            WebResourceResponse(guessMimeType(path), null, openAsset(path))
+        } catch (e: IOException) {
+            WebResourceResponse(null, null, null)
+        }
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     private lateinit var host: IModelJsHost
     private var promiseName: String = ""
@@ -61,15 +89,13 @@ class MainActivity : AppCompatActivity() {
         val frontend: MobileFrontend = object : MobileFrontend(host, "&standalone=true") {
             override fun supplyEntryPoint(): String {
                 // If you want to connect to a local dev server instead of the built-in frontend, return something like: "192.168.86.20:3000"
-                return "https://appassets.androidplatform.net/assets/frontend/index.html"
+                return "https://${WebViewAssetLoader.DEFAULT_DOMAIN}/assets/frontend/index.html"
             }
         }
 
         // using a WebViewAssetLoader so that the localization json files load properly
         // the version of i18next-http-backend we're using tries to use the fetch API with file URL's (apparently fixed in version 2.0.1)
-        val assetLoader = WebViewAssetLoader.Builder()
-            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
-            .build()
+        val assetLoader = WebViewAssetLoader.Builder().addPathHandler("/assets/", AssetsPathHandler(this)).build()
 
         frontend.webViewClient = object : WebViewClientCompat() {
             override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest): WebResourceResponse? {
@@ -107,13 +133,13 @@ class MainActivity : AppCompatActivity() {
         frontend.loadEntryPoint()
     }
 
-//    override fun onPause() {
-//        host.onPause()
-//        super.onPause()
-//    }
-//
-//    override fun onResume() {
+    override fun onResume() {
 //        host.onResume()
-//        super.onResume()
-//    }
+        super.onResume()
+    }
+
+    override fun onPause() {
+//        host.onPause()
+        super.onPause()
+    }
 }
