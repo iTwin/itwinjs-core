5639e74113000dc4da96b68d7c51cce5c3558513
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2022-11-17 05:46:50 -0500

    read polyfaces.

--- core/common/src/ElementMesh.ts
+++ core/common/src/ElementMesh.ts
@@ -6,8 +6,8 @@
  * @module Geometry
  */
 
-import { Id64String } from "@itwin/core-bentley";
-import { IndexedPolyface } from "@itwin/core-geometry";
+import { ByteStream, Id64String } from "@itwin/core-bentley";
+import { BentleyGeometryFlatBuffer, IndexedPolyface } from "@itwin/core-geometry";
 
 export interface ElementMeshOptions {
   chordTolerance?: number;
@@ -20,6 +20,45 @@ export interface ElementMeshRequestProps extends ElementMeshOptions {
   source: Id64String;
 }
 
-export function readElementMeshes(_data: Uint8Array): IndexedPolyface[] {
-  return []; // ###TODO
+interface Chunk {
+  type: string;
+  data?: Uint8Array;
+}
+
+function nextChunk(stream: ByteStream): Chunk | undefined {
+  if (stream.remainingLength < 8)
+    return undefined;
+
+  // Type codes are a sequence of four uppercase ASCII letters.
+  const chars = [stream.nextUint8, stream.nextUint8, stream.nextUint8, stream.nextUint8];
+  if (chars.some((c) => c < 65 || c > 90))
+    return undefined;
+
+  const dataLength = stream.nextUint32;
+  const data = dataLength > 0 ? stream.nextBytes(dataLength) : undefined;
+  return {
+    type: String.fromCharCode(...chars),
+    data,
+  };
+}
+
+export function readElementMeshes(data: Uint8Array): IndexedPolyface[] {
+  const polyfaces: IndexedPolyface[] = [];
+
+  const stream = new ByteStream(data);
+  const firstChunk = nextChunk(stream);
+  if (!firstChunk || "LMSH" !== firstChunk.type)
+    return polyfaces;
+
+  while (stream.remainingLength > 0) {
+    const chunk = nextChunk(stream);
+    if (!chunk || chunk.type !== "PLFC" || !chunk.data)
+      continue;
+
+    const geom = BentleyGeometryFlatBuffer.bytesToGeometry(chunk.data);
+    if (geom instanceof IndexedPolyface)
+      polyfaces.push(geom);
+  }
+
+  return polyfaces;
 }
