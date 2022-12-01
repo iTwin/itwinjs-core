b7057626b9f674d47e1ecedd9140d06de723e278
Seamus Kirby <32379572+skirby1996@users.noreply.github.com>
2022-12-01 08:10:00 -0500

    switch to using ubuntu 20.04

--- tools/certa/src/runners/electron/ElectronTestRunner.ts
+++ tools/certa/src/runners/electron/ElectronTestRunner.ts
@@ -31,7 +31,6 @@ export class ElectronTestRunner {
       show: config.debug,
       webPreferences: {
         nodeIntegration: true,
-        sandbox: true,
         enableRemoteModule: true,
       },
     });
