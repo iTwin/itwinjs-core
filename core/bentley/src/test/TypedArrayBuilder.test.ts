c63258e3c318e47ea61890766e93b614d80aeb2d
pmconne <22944042+pmconne@users.noreply.github.com>
2022-12-12 12:41:20 -0500

    remove .only

--- core/bentley/src/test/TypedArrayBuilder.test.ts
+++ core/bentley/src/test/TypedArrayBuilder.test.ts
@@ -5,7 +5,7 @@
 import { expect } from "chai";
 import { UintArrayBuilder } from "../TypedArrayBuilder";
 
-describe.only("UintArrayBuilder", () => {
+describe("UintArrayBuilder", () => {
   class Builder extends UintArrayBuilder {
     public get data() {
       return this._data;
