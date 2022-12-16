5fe19964b6f9b738adbebae6814661e9e326402e
pmconne <22944042+pmconne@users.noreply.github.com>
2022-12-16 06:19:45 -0500

    finish tests

--- core/frontend/src/test/render/RealityMeshParams.test.ts
+++ core/frontend/src/test/render/RealityMeshParams.test.ts
@@ -4,17 +4,49 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { expect } from "chai";
-import { Range3d } from "@itwin/core-geometry";
+import { Point3d, Range3d } from "@itwin/core-geometry";
+import { QPoint3dBuffer } from "@itwin/core-common";
 import { RealityMeshParamsBuilder } from "../../render/RealityMeshParams";
 
-describe.only("RealityMeshParamsBuilder", () => {
-  it("supports 8-bit indices", () => {
-  });
+describe("RealityMeshParamsBuilder", () => {
+  it("supports 8-, 16-, and 32-bit indices", () => {
+    function test(numIndices: number, expectedType: typeof Uint8Array | typeof Uint16Array | typeof Uint32Array): void {
+      const lastIndex = numIndices - 1;
+      const builder = new RealityMeshParamsBuilder({ positionRange: new Range3d(-1, -2, -3, 1, 2, 3) });
+      for (let i = 0; i < lastIndex; i++) {
+        builder.addUnquantizedVertex({ x: -1, y: -2, z: -3 }, { x: 0, y: 0 });
+        builder.addIndices([i]);
+      }
 
-  it("supports 16-bit indices", () => {
-  });
+      builder.addUnquantizedVertex({ x: 1, y: 2, z: 3 }, { x: 1, y: 1 });
+      builder.addIndices([lastIndex]);
 
-  it("supports 32-bit indices", () => {
+      const params = builder.finish();
+      expect(params.indices).instanceof(expectedType);
+      expect(params.indices.length).to.equal(numIndices);
+      expect(params.positions.points.length).to.equal(numIndices * 3);
+
+      const pt = new Point3d();
+      for (let i = 0; i < lastIndex; i++) {
+        expect(params.indices[i]).to.equal(i);
+        QPoint3dBuffer.unquantizePoint(params.positions, i, pt);
+        expect(pt.x).to.equal(-1);
+        expect(pt.y).to.equal(-2);
+        expect(pt.z).to.equal(-3);
+      }
+
+      expect(params.indices[lastIndex]).to.equal(lastIndex);
+      QPoint3dBuffer.unquantizePoint(params.positions, lastIndex, pt);
+        expect(pt.x).to.equal(1);
+        expect(pt.y).to.equal(2);
+        expect(pt.z).to.equal(3);
+    }
+
+    test(3, Uint8Array);
+    test(255, Uint8Array);
+    test(258, Uint16Array);
+    test(65535, Uint16Array);
+    test(65538, Uint32Array);
   });
 
   it("defines initial index buffer type based on number of initial vertices", () => {
