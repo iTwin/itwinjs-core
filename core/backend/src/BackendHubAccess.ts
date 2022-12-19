5c6e20433aea1b2d7021a72edb3452b01f9ee244
nick.tessier <22119573+nick4598@users.noreply.github.com>
2022-12-19 10:18:03 -0500

    remove queryCurrentOrPReceindg

--- core/backend/src/BackendHubAccess.ts
+++ core/backend/src/BackendHubAccess.ts
@@ -228,12 +228,6 @@ export interface BackendHubAccess {
   queryV2Checkpoint: (arg: CheckpointProps) => Promise<V2CheckpointAccessProps | undefined>;
 
   /**
-   * Get the access props for a successful V2 checkpoint that is at the requested changesetId or the most recent checkpoint before it. Returns undefined if no such V2 checkpoint exists.
-   * @internal
-   */
-  queryCurrentOrPrecedingV2Checkpoint?: (arg: CheckpointProps) => Promise<V2CheckpointAccessProps | undefined>;
-
-  /**
    * acquire one or more locks. Throws if unsuccessful. If *any* lock cannot be obtained, no locks are acquired
    * @internal
    */
