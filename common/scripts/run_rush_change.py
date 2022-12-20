78a11ebe369823db1a141a791895c8b734a9aec4
DanRod1999 <DanRod1999@users.noreply.github.com>
2022-12-20 12:04:11 -0500

    only check for imodel02

--- common/scripts/run_rush_change.py
+++ common/scripts/run_rush_change.py
@@ -14,19 +14,17 @@ if buildReason == "PullRequest":
 print ("Current branch: " + srcBranch)
 print ("Target branch: " + targetBranch)
 
-# Uses default head ("origin/master"), if not defined
-branchCmd = []
 # Verifying with rush change requires the branch that is being merged into to be provided.  More details, https://rushjs.io/pages/commands/rush_change/.
 # With release/* being a potential target branch in addition to master, special case those branches.
-if targetBranch == "refs/heads/master":
-    # For builds when target and current branch == refs/heads/master
-    branchCmd = []
-elif "refs/heads/release" in targetBranch:
+if "refs/heads/release" in targetBranch:
     branchCmd = ["-b", targetBranch.replace("refs/heads/", "origin/")]
-elif "release" in targetBranch or targetBranch == srcBranch:
+elif "release" in targetBranch or targetBranch == "imodel02":
     # ADOps uses the branch name (i.e. 'release/2.8.0') for GH PR branch names instead of full refs.
     # or for addon validation when there is a change in native side, but not in itwinjs-core
     branchCmd = ["-b", "origin/" + targetBranch]
+else:
+    # Uses default head ("origin/master"), if not defined
+    branchCmd = []
 
 command = ["node", "common/scripts/install-run-rush.js", "change", "-v"] + branchCmd
 print ("Executing: " + " ".join(command))
