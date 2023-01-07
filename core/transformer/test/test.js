b97e1bd757aef136a4e36221363d9780319d0539
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2023-01-07 13:33:39 -0500

    this is the only thing that worked

--- core/transformer/test/test.js
+++ core/transformer/test/test.js
@@ -1,4 +1,8 @@
 "use strict";
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 exports.__esModule = true;
 var core_backend_1 = require("@itwin/core-backend");
 // import { IModelHost } from "@itwin/core-backend/lib/internal-types/itwin__core-backend/lib/cjs/core-backend";
