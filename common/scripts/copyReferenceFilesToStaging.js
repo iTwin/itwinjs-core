a3d62b9a36be98b5f6c528d730fd5cdb2548b998
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-01-13 17:09:00 -0500

    remove cascading callbacks

--- common/scripts/copyReferenceFilesToStaging.js
+++ common/scripts/copyReferenceFilesToStaging.js
@@ -3,10 +3,12 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 /*
-This script creates a staging-directory for the docs build and copies the folders from itwinjs-core/docs and itwinjs-core/generated-docs.
-The default location of this staging directory is itwinjs-core/staging-docs but can be modified by the user through CLI arguments
+This script does the following:
+1. Creates a staging-directory for the docs build
+2. Copies the folders from itwinjs-core/docs and itwinjs-core/generated-docs.
+3. Provides the ability for users to give an absolute path
+4. Provides the ability to modify the default path (itwinjs-core/staging-docs) through CLI arguments
 For example: node copyReferenceFilesToStaging.js itwinjs-core/core/test-staging-directory.
-The user can also provide an absolute path.
 */
 let fse;
 try {
@@ -17,37 +19,40 @@ try {
 }
 const path = require('path');
 const process = require('process');
-let dest;
+
 const basePath = path.resolve(__dirname, "..", "..", "..");
+let dest;
+
 if (process.argv[2]) {
   dest = process.argv[2];
 } else {
   dest = "itwinjs-core/staging-docs";
 }
-fse.mkdir(path.resolve(basePath, dest), (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log("Staging directory created successfully");
-  fse.mkdir(path.resolve(basePath, dest, "extract"), (err) => {
-    if (err) {
-      return console.error(err);
-    }
-    console.log("Extract folder created successfully");
-    copyDocs();
-  });
-});
+
+try {
+  fse.ensureDirSync(path.resolve(basePath, dest, "extract"));
+} catch (err) {
+  console.log("Could not create staging directory structure");
+  console.error(err);
+}
+
+copyDocs();
+
 function copyDocs() {
+
   const docsPath = path.resolve(basePath, dest);
   const referencePath = path.resolve(basePath, dest, "reference");
   const extractPath = path.resolve(basePath, dest, "extract");
   const folderList = ["core", "domains", "editor", "presentation", "ui"];
+
   try {
     fse.copySync(path.resolve(basePath, "itwinjs-core", "docs"), docsPath);
     fse.copySync(path.resolve(basePath, "itwinjs-core", "generated-docs", "extract"), extractPath);
+
     folderList.forEach(folder => {
       fse.copySync(path.resolve(basePath, "itwinjs-core", "generated-docs", folder), referencePath);
     })
+
     console.log("Copying finished sucessfully");
   } catch (err) {
     console.error(err);
