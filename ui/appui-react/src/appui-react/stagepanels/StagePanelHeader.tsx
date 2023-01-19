47c6887a9fbe794279489fcb668501e21f5d285c
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2023-01-19 14:31:10 -0500

    Add deprecation version to Ui Packages.

--- ui/appui-react/src/appui-react/stagepanels/StagePanelHeader.tsx
+++ ui/appui-react/src/appui-react/stagepanels/StagePanelHeader.tsx
@@ -18,7 +18,7 @@ import { StagePanelState } from "./StagePanelDef";
 // cspell:ignore stagepanelheader
 
 /** Properties of a [[StagePanelHeader]] component
- * @alpha @deprecated
+ * @alpha @deprecated in 3.0.
  */
 export interface StagePanelHeaderProps extends CommonProps {
   /** Describes if the collapse button is visible. */
@@ -32,7 +32,7 @@ export interface StagePanelHeaderProps extends CommonProps {
 }
 
 /** Stage panel header React component.
- * @alpha @deprecated
+ * @alpha @deprecated in 3.0.
  */
 export class StagePanelHeader extends React.PureComponent<StagePanelHeaderProps> {
   public override render() {
