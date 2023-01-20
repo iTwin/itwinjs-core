026f6477bc858471cf64c95a6b3c901591e993c1
Gytis Cepkauskas <GytisCepk@users.noreply.github.com>
2023-01-20 17:39:15 +0200

    Drop support for Electron 15, 16, 17.

--- core/electron/src/backend/ElectronHost.ts
+++ core/electron/src/backend/ElectronHost.ts
@@ -126,7 +126,7 @@ export class ElectronHost {
   }
 
   private static _openWindow(options?: ElectronHostWindowOptions) {
-    const webPreferences: WebPreferences & { nativeWindowOpen: true } = { // nativeWindowOpen was removed starting Electron 18
+    const webPreferences: WebPreferences = {
       ...options?.webPreferences,
 
       // These web preference variables should not be overriden by the ElectronHostWindowOptions
@@ -135,7 +135,6 @@ export class ElectronHost {
       nodeIntegration: false,
       contextIsolation: true,
       sandbox: true,
-      nativeWindowOpen: true,
       nodeIntegrationInWorker: false,
       nodeIntegrationInSubFrames: false,
     };
