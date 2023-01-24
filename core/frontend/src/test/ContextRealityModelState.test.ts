99da7a2b19005ba8fd5f1b9b55bcc73321073abd
pmconne <22944042+pmconne@users.noreply.github.com>
2023-01-24 14:50:52 -0500

    test file

---
+++ core/frontend/src/test/ContextRealityModelState.test.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+
+describe.only("Context reality models", () => {
+  it("is assigned a transient modelId", () => {
+  });
+
+  it("shares compatible tile trees between views", () => {
+  });
+
+  it("does not share compatible tile trees within a single view", () => {
+  });
+
+  it("does not share compatible persistent tile trees", () => {
+  });
+});
