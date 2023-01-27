6a31c8e291e22f9fce92b1db24536e5255a254bb
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-01-27 08:45:00 -0500

    Custom lint rule to require version

---
+++ tools/eslint-plugin/dist/rules/require-version.js
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+// Writing all of these eslint rules in javascript so we can run them before the build step
+
+"use strict";
+
+const { getParserServices } = require("./utils/parser");
+const ts = require("typescript");
+
+module.exports = {
+  create(context) {
+
+    const parserServices = getParserServices(context);
+
+    function checkJsDoc(declaration, node) {
+      let tags = ts.getJSDocTags(declaration);
+
+      if (!tags || tags.length == 0) {
+        return;
+      }
+      for (let tag of tags) {
+        if (tag.tagName.escapedText == "deprecated") {
+          console.log(tag);
+
+          if (tag.comment == undefined) {
+            context.report(node, "Please provide version and info about the depercated API");
+          }
+          else if (!(/\d+\.x/.test(String(tag.comment)) || /\d+\.\d/.test(String(tag.comment)))) {
+            context.report(node, "No version info found");
+          }
+        }
+      }
+    }
+
+    return {
+      ExportNamedDeclaration(node) {
+        const tsCall = parserServices.esTreeNodeToTSNodeMap.get(node);
+        if (!tsCall)
+          return;
+        checkJsDoc(tsCall, node);
+      }
+    }
+
+  }
+}
