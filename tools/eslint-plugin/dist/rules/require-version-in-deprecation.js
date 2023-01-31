fbd8a226f43266bc80edd07007f7771366712a8c
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-01-31 16:38:05 -0500

    use capture groups

--- tools/eslint-plugin/dist/rules/require-version-in-deprecation.js
+++ tools/eslint-plugin/dist/rules/require-version-in-deprecation.js
@@ -11,9 +11,10 @@ module.exports = {
   create(context) {
     return {
       Program(node) {
-        for (let comment of node.comments) {
-          if (/@deprecated/.test(comment.value)) {
-            if (!(/\d+(\.(\d|x))+.{5,}/.test(comment.value))) {
+        let match;
+        for (const comment of node.comments) {
+          if (match = /@deprecated(?<in> in \d+(\.(\d|x))+.{5,})?/.exec(comment.value)) {
+            if (!match?.groups?.in) {
               context.report(comment, "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required");
             }
           }
