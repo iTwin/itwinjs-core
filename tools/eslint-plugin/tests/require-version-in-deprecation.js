80103fae0b5bbbb72f581672996ce16cf032bfd3
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-02-02 11:58:22 -0500

    Add mocharc.js and some tests

---
+++ tools/eslint-plugin/tests/require-version-in-deprecation.js
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+"use strict";
+
+const path = require("path");
+const ESLintTester = require("eslint").RuleTester;
+const BentleyESLintPlugin = require("../dist");
+const RequireVersionInDeprecationESLintRule =
+  BentleyESLintPlugin.rules["require-version-in-deprecation"];
+
+/** allow specifying `only` and `skip` properties for easier debugging */
+function supportSkippedAndOnlyInTests(obj) {
+  const hasOnly =
+    obj.valid.some((test) => Boolean(test.only)) ||
+    obj.invalid.some((test) => Boolean(test.only));
+  const keepTest = (test) => (hasOnly ? test.only : !test.skip);
+  const stripExtraTags = (test) => {
+    delete test.skip;
+    delete test.only;
+    return test;
+  };
+  return {
+    valid: obj.valid.filter(keepTest).map(stripExtraTags),
+    invalid: obj.invalid.filter(keepTest).map(stripExtraTags),
+  };
+}
+
+const ruleTester = new ESLintTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+ruleTester.run(
+  "require-version-in-deprecation",
+  RequireVersionInDeprecationESLintRule,
+  supportSkippedAndOnlyInTests({
+    valid: [
+      {
+        code: `/**
+      * @beta
+      * @deprecated in 3.x. Use XYZ instead, see https://www.google.com/ for more details.
+      */`
+      },
+      {
+        code: `/**
+        * @deprecated in 3.x, use XYZ instead
+        * @beta
+        */`
+      },
+      {
+        code: `// @deprecated in 3.6 Use XYZ instead.`
+      },
+      {
+        code: `/* @deprecated in 2.x. Use xyz instead. */`
+      },
+      {
+        code: `// @deprecated in 2.x, use xyz instead
+        function canWeUseFunctions() {}`
+      },
+      {
+        code: `// @deprecated in 3.6. Use xyz instead.
+        class canWeUseAClass {};`
+      },
+      {
+        code: `/* @deprecated in 2.x Use XYZ */
+        let canWeUseArrowFunction = () => {};`
+      },
+      {
+        code: `/**
+        * @deprecated in 3.x. Please use XYZ.
+        */
+        export interface canWeUseInterface {}`
+      },
+      {
+        code: `/* @deprecated in 2.x. Use xyz instead. */
+        namespace canWeUseNamespaces {}`
+      },
+      {
+        code: `// @deprecated in 3.6 Use XYZ instead.
+        export enum canWeUseEnum {}`
+      },
+
+    ],
+    invalid: [
+      {
+        code: `/**
+        * @beta
+        * @deprecated
+        */`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `// @deprecated`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `// @deprecated in 3.x.`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `/* @deprecated */`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `/* @deprecated in 2.6.*/
+        function shouldFailSinceNoSentence() {}`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `// @deprecated in 3.x
+        let shouldFailSinceNoSentenceGiven = () => {};`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `// @deprecated
+        class shouldFailSinceNoVersionOrSentenceGiven {};`,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+      {
+        code: `/** @deprecated in 3.6.
+        * I am giving the description here.
+        */
+         `,
+        errors: [{ message: "@deprecated in Major.minor format followed by deprecation reason and/or alternative API usage required" }],
+      },
+
+    ],
+  })
+);
