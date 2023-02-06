00b2333037aa50e1c311d06030de2d8daca6020a
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2023-02-06 15:40:57 -0500

    cherry-pick from yash's PR mocharc and dep changes for running tests

---
+++ tools/eslint-plugin/.mocharc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //tslib injects global variables that we can't remove
+  globals: ['__extends', '__assign', '__rest', '__decorate', '__param', '__metadata', '__awaiter',
+    '__generator', '__exportStar', '__createBinding', '__values', '__read', '__spread', '__spreadArrays',
+    '__await', '__asyncGenerator', '__asyncDelegator', '__asyncValues', '__makeTemplateObject', '__importStar',
+    '__importDefault', '__classPrivateFieldGet', '__classPrivateFieldSet']
+}
