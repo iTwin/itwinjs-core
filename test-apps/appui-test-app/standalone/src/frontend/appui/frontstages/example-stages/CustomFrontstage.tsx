f5536d8b383556ed66dc52fa1bda4711a7bfb5a1
AntanasBuk <124153238+AntanasBuk@users.noreply.github.com>
2023-02-15 17:31:07 +0200

    Update Frontstage docs (#5040)
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>
    (cherry picked from commit f314e7651da52100cc7a15a698b40d6d121391b4)

---
+++ test-apps/appui-test-app/standalone/src/frontend/appui/frontstages/example-stages/CustomFrontstage.tsx
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+import {
+  BackstageAppButton, BackstageManager, ConfigurableCreateInfo, ConfigurableUiManager, ContentControl, ContentGroup,
+  ContentToolWidgetComposer, FrontstageConfig, FrontstageProps, FrontstageProvider,
+} from "@itwin/appui-react";
+import { StandardContentLayouts } from "@itwin/appui-abstract";
+
+// __PUBLISH_EXTRACT_START__ Example_Custom_Content_Control
+class CustomContentControl extends ContentControl {
+  constructor(info: ConfigurableCreateInfo, options: any) {
+    super(info, options);
+
+    this.reactNode = (
+      <h1 style={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        Custom content!
+      </h1>
+    );
+  }
+}
+// __PUBLISH_EXTRACT_END__
+
+// __PUBLISH_EXTRACT_START__ Example_Custom_Frontstage_Provider_1
+export class CustomFrontstageProvider extends FrontstageProvider {
+  public override get id(): string {
+    return "example:CustomFrontstage";
+  }
+  // eslint-disable-next-line @typescript-eslint/indent
+// __PUBLISH_EXTRACT_END__
+  public override get frontstage(): React.ReactElement<FrontstageProps> { // eslint-disable-line deprecation/deprecation
+    throw new Error("`frontstageConfig` should be used instead.");
+  }
+  // eslint-disable-next-line @typescript-eslint/indent
+// __PUBLISH_EXTRACT_START__ Example_Custom_Frontstage_Provider_2
+  public override frontstageConfig(): FrontstageConfig {
+    const id = this.id;
+    const contentGroup = new ContentGroup({
+      id: "test-group",
+      layout: StandardContentLayouts.singleView,
+      contents: [{id: "custom-content", classId: CustomContentControl }],
+    });
+    return {
+      id,
+      version: 1,
+      contentGroup,
+      contentManipulation: {
+        id: `${id}-contentManipulationTools`,
+        element: <ContentToolWidgetComposer
+          cornerButton={
+            <BackstageAppButton label="Toggle Backstage" icon="icon-bentley-systems"
+              execute={() => BackstageManager.getBackstageToggleCommand().execute()} />
+          }
+        />,
+      },
+    };
+  }
+}
+// __PUBLISH_EXTRACT_END__
+
+export function registerCustomFrontstage(): void {
+  ConfigurableUiManager.addFrontstageProvider(new CustomFrontstageProvider());
+}
---
+++ test-apps/appui-test-app/standalone/src/frontend/appui/frontstages/example-stages/ViewportFrontstage.tsx
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { StandardContentLayouts } from "@itwin/appui-abstract";
+import { BackstageAppButton, ConfigurableUiManager, ContentGroup, ContentGroupProvider, FrontstageProps, IModelViewportControl, StandardFrontstageProps, StandardFrontstageProvider, UiFramework } from "@itwin/appui-react";
+import React from "react";
+
+// __PUBLISH_EXTRACT_START__ Example_Viewport_Frontstage_Group_Provider_1
+export class ViewportFrontstageGroupProvider extends ContentGroupProvider {
+// __PUBLISH_EXTRACT_END__
+  public override async provideContentGroup(_props: FrontstageProps): Promise<ContentGroup> { // eslint-disable-line deprecation/deprecation
+    throw new Error("Method not implemented.");
+  }
+
+  // eslint-disable-next-line @typescript-eslint/indent
+// __PUBLISH_EXTRACT_START__ Example_Viewport_Frontstage_Group_Provider_2
+  public override async contentGroup(): Promise<ContentGroup> {
+    return new ContentGroup({
+      id: "content-group",
+      layout: StandardContentLayouts.singleView,
+      contents: [
+        {
+          id: "viewport",
+          classId: IModelViewportControl,
+          applicationData: {
+            viewState: UiFramework.getDefaultViewState,
+            iModelConnection: UiFramework.getIModelConnection,
+          },
+        },
+      ],
+    });
+  }
+}
+// __PUBLISH_EXTRACT_END__
+
+// __PUBLISH_EXTRACT_START__ Example_Register_Viewport_Frontstage
+export function registerViewportFrontstage(): void {
+  const stageProps: StandardFrontstageProps = {
+    id: "example:ViewportFrontstage",
+    contentGroupProps: new ViewportFrontstageGroupProvider(),
+    cornerButton: <BackstageAppButton />,
+    usage: "General",
+  };
+  ConfigurableUiManager.addFrontstageProvider(new StandardFrontstageProvider(stageProps));
+}
+// __PUBLISH_EXTRACT_END__
