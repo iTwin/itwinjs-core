13c3584009c0154c308fbe78b9d07c95175f0a1f
pmconne <22944042+pmconne@users.noreply.github.com>
2023-02-22 16:46:18 -0500

    parser

--- core/frontend/src/gltf/GltfParser.ts
+++ core/frontend/src/gltf/GltfParser.ts
@@ -6,3 +6,43 @@
  * @module Rendering
  */
 
+import { Logger } from "@itwin/core-bentley";
+import { FrontendLoggerCategory } from "../FrontendLoggerCategory";
+import * as schema from "./GltfSchema";
+import { Gltf } from "./GltfModel";
+
+export interface ParseGltfLogger {
+  log(message: string, type: "error" | "warning" | "info"): void;
+}
+
+export interface ParseGltfArgs {
+  logger?: ParseGltfLogger;
+  gltf: Uint8Array | schema.GltfDocument;
+  baseUrl?: string;
+  isCanceled?: boolean;
+}
+
+export async function parseGltf(args: ParseGltfArgs): Promise<Gltf.Model | undefined> {
+  const parser = new GltfParser(args);
+  return parser.parse();
+}
+
+class GltfParser {
+  private readonly _logger: ParseGltfLogger;
+  private readonly _isCanceled: () => boolean;
+
+  public constructor(args: ParseGltfArgs) {
+    this._isCanceled = () => args.isCanceled ?? false;
+    this._logger = args.logger ?? {
+      log: (message: string, type: "error" | "warning" | "info") => {
+        const category = `${FrontendLoggerCategory.Package}.gltf`;
+        const fn = type === "error" ? "logError" : (type === "warning" ? "logWarning" : "logInfo");
+        Logger[fn](category, message);
+      },
+    }
+  }
+
+  public async parse(): Promise<Gltf.Model | undefined> {
+    return undefined;
+  }
+}
