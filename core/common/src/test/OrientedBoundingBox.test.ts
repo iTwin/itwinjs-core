19ec698c0f5bc0a2f1ef4142af94a40b553c5898
pmconne <22944042+pmconne@users.noreply.github.com>
2023-02-26 13:49:07 -0500

    obb transform test

--- core/common/src/test/OrientedBoundingBox.test.ts
+++ core/common/src/test/OrientedBoundingBox.test.ts
@@ -7,8 +7,23 @@ import { expect } from "chai";
 import { Matrix3d, Point3d, Transform } from "@itwin/core-geometry";
 import { OrientedBoundingBox } from "../geometry/OrientedBoundingBox";
 
-describe.only("OrientedBoundingBox", () => {
+describe("OrientedBoundingBox", () => {
   it("transforms", () => {
+    const obb = new OrientedBoundingBox(Point3d.fromJSON([-1, 0, 2]), Matrix3d.fromJSON([1, 0, 0, 0, 2, 0, 0, 0, 3]));
+    const expectTransformed = (transform: Transform, cx: number, cy: number, cz: number, axes: number[]) => {
+      const t = obb.transformBy(transform);
+      expect(t.center.x).to.equal(cx);
+      expect(t.center.y).to.equal(cy);
+      expect(t.center.z).to.equal(cz);
+      expect(t.halfAxes.isAlmostEqual(Matrix3d.fromJSON(axes))).to.be.true;
+    };
+
+    expectTransformed(Transform.createIdentity(), -1, 0, 2, [1, 0, 0, 0, 2, 0, 0, 0, 3]);
+    expectTransformed(Transform.createTranslationXYZ(1, 2, 3), 0, 2, 5, [1, 0, 0, 0, 2, 0, 0, 0, 3]);
+    expectTransformed(Transform.createZero(), 0, 0, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    expectTransformed(Transform.createRefs(undefined, Matrix3d.createUniformScale(3)), -3, 0, 6, [3, 0, 0, 0, 6, 0, 0, 0, 9]);
+    expectTransformed(Transform.createRefs(undefined, Matrix3d.createScale(2, 1, 0.5)), -2, 0, 1, [2, 0, 0, 0, 2, 0, 0, 0, 1.5]);
+    expectTransformed(Transform.createRefs(undefined, Matrix3d.createUniformScale(-1)), 1, 0, -2, [-1, 0, 0, 0, -2, 0, 0, 0, -3]);
   });
 
   it("computes distance to closest point", () => {
