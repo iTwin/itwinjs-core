bb517002de8247a1f54d1dd859f328f1fb7abfb3
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-02 16:53:08 -0500

    wip tile

--- extensions/frontend-tiles/src/BatchedTile.ts
+++ extensions/frontend-tiles/src/BatchedTile.ts
@@ -3,3 +3,32 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import {
+  IModelApp, RenderSystem, Tile, TileContent, TileParams, TileRequest, TileRequestChannel,
+} from "@itwin/core-frontend";
+import { BatchedTileTree } from "./BatchedTileTree";
+
+export class BatchedTile extends Tile {
+  public constructor(params: TileParams, tree: BatchedTileTree) {
+    super(params, tree);
+  }
+
+  protected override _loadChildren(resolve: (children: Tile[] | undefined) => void, _reject: (error: Error) => void): void {
+    // ###TODO
+    resolve([]);
+  }
+
+  public override get channel(): TileRequestChannel {
+    return IModelApp.tileAdmin.channels.getForHttp("itwinjs-batched-models");
+  }
+
+  public override requestContent(_isCanceled: () => boolean): Promise<TileRequest.Response> {
+    // ###TODO
+    return Promise.resolve(undefined);
+  }
+
+  public override readContent(_data: TileRequest.ResponseData, _system: RenderSystem, _isCanceled?: () => boolean): Promise<TileContent> {
+    // ###TODO
+    return Promise.resolve({ });
+  }
+}
--- extensions/frontend-tiles/src/BatchedTileTree.ts
+++ extensions/frontend-tiles/src/BatchedTileTree.ts
@@ -3,3 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import { TileTree } from "@itwin/core-frontend";
+
+// ###TODO
+export type BatchedTileTree = TileTree;
