e57f1c04707bb7705162cf5830645259c69f1940
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-02 16:35:21 -0500

    supplier, id

--- extensions/frontend-tiles/src/BatchedTileTreeSupplier.ts
+++ extensions/frontend-tiles/src/BatchedTileTreeSupplier.ts
@@ -3,3 +3,26 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import { assert } from "@itwin/core-bentley";
+import {
+  IModelConnection, TileTree, TileTreeSupplier,
+} from "@itwin/core-frontend";
+
+export type TreeId = "spatial-models";
+
+class BatchedTileTreeSupplier implements TileTreeSupplier {
+  public compareTileTreeIds(lhs: TreeId, rhs: TreeId): number {
+    // Currently each iModel has exactly 1 unique tile tree for all spatial models.
+    assert(lhs === "spatial-models");
+    assert(rhs === "spatial-models");
+    return 0;
+  }
+
+  public createTileTree(id: TreeId, _iModel: IModelConnection): Promise<TileTree | undefined> {
+    // ###TODO
+    assert(id === "spatial-models");
+    return Promise.resolve(undefined);
+  }
+}
+
+export const batchedTileTreeSupplier: TileTreeSupplier = new BatchedTileTreeSupplier();
