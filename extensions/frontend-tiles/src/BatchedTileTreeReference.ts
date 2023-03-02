fa4d11dbacf56d818f6f6de93fc2c095f5108e4e
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-02 16:43:07 -0500

    tree ref

--- extensions/frontend-tiles/src/BatchedTileTreeReference.ts
+++ extensions/frontend-tiles/src/BatchedTileTreeReference.ts
@@ -3,3 +3,23 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import {
+  HitDetail, TileTreeOwner, TileTreeReference,
+} from "@itwin/core-frontend";
+
+class BatchedTileTreeReference extends TileTreeReference {
+  private readonly _treeOwner: TileTreeOwner;
+
+  public constructor(treeOwner: TileTreeOwner) {
+    super();
+    this._treeOwner = treeOwner;
+  }
+
+  public override get treeOwner(): TileTreeOwner {
+    return this._treeOwner;
+  }
+
+  public override async getToolTip(_hit: HitDetail): Promise<HTMLElement | string | undefined> {
+    return "###TODO";
+  }
+}
