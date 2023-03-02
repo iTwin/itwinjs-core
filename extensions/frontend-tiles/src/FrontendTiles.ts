443d43fddb9aedbe90b1094e6cb939dec18369cc
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-02 14:59:37 -0500

    bare minimum impl

--- extensions/frontend-tiles/src/FrontendTiles.ts
+++ extensions/frontend-tiles/src/FrontendTiles.ts
@@ -3,11 +3,31 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { SpatialTileTreeReferences, SpatialViewState } from "@itwin/core-frontend";
+import {
+  ContextRealityModelState, SpatialTileTreeReferences, SpatialViewState, TileTreeReference,
+} from "@itwin/core-frontend";
 
-const impl = SpatialTileTreeReferences.create;
+class TreeRefs implements SpatialTileTreeReferences {
+  private readonly _treeRef: TileTreeReference;
+
+  public constructor(treeRef: TileTreeReference) {
+    this._treeRef = treeRef;
+  }
+
+  public *[Symbol.iterator](): Iterator<TileTreeReference> {
+    yield this._treeRef;
+  }
+
+  public update(): void {
+  }
+
+  public setDeactivated(): void {
+    // This exists chiefly for debugging. Unimplemented here.
+  }
+}
 
 export function createSpatialTileTreeReferences(view: SpatialViewState): SpatialTileTreeReferences {
-  console.log("hi");
-  return impl(view);
+  const tilesetUrl = "http://localhost:8080/tileset.json";
+  const model = new ContextRealityModelState({ tilesetUrl }, view.iModel, view.displayStyle);
+  return new TreeRefs(model.treeRef);
 }
