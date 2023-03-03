80635d6354516029a21a9c76815205a149204ceb
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-03 13:54:51 -0500

    wip reader

--- extensions/frontend-tiles/src/BatchedTilesetReader.ts
+++ extensions/frontend-tiles/src/BatchedTilesetReader.ts
@@ -3,6 +3,9 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import {
+  Matrix3d, Point3d, Range3d, Transform, Vector3d,
+} from "@itwin/core-geometry";
 import { Tileset3dSchema as schema } from "@itwin/core-common";
 import { IModelConnection } from "@itwin/core-frontend";
 
@@ -12,6 +15,41 @@ function isTileset3d(json: unknown): json is schema.Tileset {
 
   const props = json as schema.Tileset;
   return undefined !== props.root && undefined !== props.geometricError && undefined !== props.asset;
+}
+
+function rangeFromBoundingVolume(vol: schema.BoundingVolume): Range3d {
+  if (vol.box) {
+    const center = new Point3d(vol.box[0], vol.box[1], vol.box[2]);
+    const ux = new Vector3d(vol.box[3], vol.box[4], vol.box[5]);
+    const uy = new Vector3d(vol.box[6], vol.box[7], vol.box[8]);
+    const uz = new Vector3d(vol.box[9], vol.box[10], vol.box[11]);
+
+    const range = Range3d.createNull();
+    for (let i = -1; i <= 1; i += 2)
+      for (let j = -1; j <= 1; j += 2)
+        for (let k = -1; k <= 1; k += 2)
+          range.extendPoint(center.plus3Scaled(ux, i, uy, j, uz, k));
+
+    return range;
+  } else if (vol.sphere) {
+    const center = new Point3d(vol.sphere[0], vol.sphere[1], vol.sphere[2]);
+    const radius = vol.sphere[3];
+    return Range3d.createXYZXYZ(center.x - radius, center.y - radius, center.z - radius, center.x + radius, center.y + radius, center.z + radius);
+  }
+
+  // We won't get region bounding volumes in our tiles.
+  throw new Error("region bounding volume unimplemented");
+}
+
+function transformFromJSON(json: schema.Transform): Transform {
+  const translation = new Point3d(json[12], json[13], json[14]);
+  const matrix = Matrix3d.createRowValues(
+    json[0], json[4], json[8],
+    json[1], json[5], json[9],
+    json[2], json[6], json[10]
+  );
+
+  return Transform.createOriginAndMatrix(translation, matrix);
 }
 
 export class BatchedTilesetReader {
