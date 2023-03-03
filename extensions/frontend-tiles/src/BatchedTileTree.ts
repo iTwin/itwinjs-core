7693b49b569e5edcfcfb2688d37427339ba3b378
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-03 04:23:42 -0500

    wip tile tree

--- extensions/frontend-tiles/src/BatchedTileTree.ts
+++ extensions/frontend-tiles/src/BatchedTileTree.ts
@@ -3,7 +3,55 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { TileTree } from "@itwin/core-frontend";
+import { Range3d } from "@itwin/core-geometry";
+import { RenderMode, ViewFlagOverrides } from "@itwin/core-common";
+import { Tile, TileDrawArgs, TileTree, TileTreeParams } from "@itwin/core-frontend";
+import { BatchedTile } from "./BatchedTile";
 
-// ###TODO
-export type BatchedTileTree = TileTree;
+const viewFlagOverrides: ViewFlagOverrides = {
+  renderMode: RenderMode.SmoothShade,
+  visibleEdges: false,
+};
+
+export class BatchedTileTree extends TileTree {
+  private readonly _rootTile: BatchedTile;
+
+  public constructor(params: TileTreeParams) {
+    super(params);
+    this._rootTile = new BatchedTile({
+      contentId: "###TODO",
+      range: new Range3d(), // ###TODO
+      maximumSize: 512, // ###TODO
+    }, this);
+  }
+
+  public override get rootTile(): BatchedTile {
+    return this._rootTile;
+  }
+
+  public override get is3d(): boolean {
+    return true;
+  }
+
+  public override get maxDepth(): number | undefined {
+    // ###TODO?
+    return undefined;
+  }
+
+  public override get viewFlagOverrides(): ViewFlagOverrides {
+    return viewFlagOverrides;
+  }
+
+  public override _selectTiles(_args: TileDrawArgs): Tile[] {
+    // ###TODO
+    return [];
+  }
+
+  public override draw(_args: TileDrawArgs): void {
+    // ###TODO
+  }
+
+  public override prune(): void {
+    // ###TODO
+  }
+}
