ea9550f6dd9e4ea5a083b64d00303a44094041bb
pmconne <22944042+pmconne@users.noreply.github.com>
2023-03-07 15:18:04 -0500

    logger category

--- extensions/frontend-tiles/src/LoggerCategory.ts
+++ extensions/frontend-tiles/src/LoggerCategory.ts
@@ -3,5 +3,5 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-export const loggerCategory = "batched-tiles";
+export const loggerCategory = "frontend-tiles";
 
