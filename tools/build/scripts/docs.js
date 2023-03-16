288905b3f9e6bb36d978ffeacafc0e19264bc71f
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-03-16 11:42:49 -0400

    keep the changelog copying

--- tools/build/scripts/docs.js
+++ tools/build/scripts/docs.js
@@ -86,6 +86,11 @@ spawn(require.resolve(".bin/typedoc"), args).then((code) => {
     fs.renameSync(path.join(outputDir, argv.tsIndexFile), path.join(outputDir, 'index.ts'));
   }
 
+  // Copy CHANGELOG.json to json output folder
+  if (fs.existsSync(path.join(process.cwd(), 'CHANGELOG.json'))) {
+    cpx.copySync(path.join(process.cwd(), 'CHANGELOG.json'), outputDir);
+  }
+
   // Append the directory of the package, version and repository URL to the output
   addPackageMetadata(json, process.cwd());
 
