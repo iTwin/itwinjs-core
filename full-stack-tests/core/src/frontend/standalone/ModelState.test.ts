c944814fd5415d142b4bb052eeb0069500657e69
Matt Jordan <Matthew.Jordan@bentley.com>
2023-03-20 11:21:27 -0400

    Deprecate query, queryRowCount, and restartQuery methods (#5240)
    Co-authored-by: Matthew Jordan <14913576+mattbjordan@users.noreply.github.com>
    Co-authored-by: Paul Connelly <22944042+pmconne@users.noreply.github.com>

--- full-stack-tests/core/src/frontend/standalone/ModelState.test.ts
+++ full-stack-tests/core/src/frontend/standalone/ModelState.test.ts
@@ -102,6 +102,7 @@ describe("ModelState", () => {
     assert.isNotTrue(modelProps[1].isTemplate);
 
     let propsCount = 0;
+    // eslint-disable-next-line deprecation/deprecation
     for await (const props of imodel.models.query({ from: "BisCore:DictionaryModel", wantPrivate: true, wantTemplate: true, limit: 1 })) {
       propsCount++;
       assert.equal(props.classFullName, "BisCore:DictionaryModel");
