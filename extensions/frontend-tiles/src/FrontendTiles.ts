3d12ea709293ea74db2312540d13dd44d5afd808
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2023-03-24 16:49:33 -0400

    Configure max tiles to skip for batched tile trees (#5282)
    (cherry picked from commit d2cffe455ad67eadd3d8533a301485907b95086b)

--- extensions/frontend-tiles/src/FrontendTiles.ts
+++ extensions/frontend-tiles/src/FrontendTiles.ts
@@ -20,14 +20,33 @@ export type ComputeSpatialTilesetBaseUrl = (iModel: IModelConnection) => Promise
 export interface FrontendTilesOptions {
   /** Provide the base URL for the pre-published tileset for a given iModel. */
   computeSpatialTilesetBaseUrl: ComputeSpatialTilesetBaseUrl;
+  /** The maximum number of levels in the tile tree to skip loading if they do not provide the desired level of detail for the current view.
+   * Default: 4.
+   * Reducing this value will load more intermediate tiles, which causes more gradual refinement: low-resolution tiles will display quickly, followed more gradually by
+   * successively higher-resolution ones.
+   * Increasing the value jumps more directly to tiles of the exact level of detail desired, which may load more, smaller tiles up-front, leaving some areas of the view
+   * vacant for longer; and when zooming out some newly-exposed areas of the view may remain vacant for longer because no lower-resolution tiles are initially available to
+   * fill them. However, tiles close to the viewer (and therefore likely of most interest to them) will refine to an appropriate level of detail more quickly.
+   */
+  maxLevelsToSkip?: number;
 }
 
 /** @internal */
 export const createFallbackSpatialTileTreeReferences = SpatialTileTreeReferences.create;
 
+let maxLevelsToSkip = 4;
+
+/** @internal */
+export function getMaxLevelsToSkip(): number {
+  return maxLevelsToSkip;
+}
+
 /** Initialize the frontend-tiles package to obtain tiles for spatial views.
  * @alpha
  */
 export function initializeFrontendTiles(options: FrontendTilesOptions): void {
+  if (undefined !== options.maxLevelsToSkip && options.maxLevelsToSkip >= 0)
+    maxLevelsToSkip = options.maxLevelsToSkip;
+
   SpatialTileTreeReferences.create = (view: SpatialViewState) => createBatchedSpatialTileTreeReferences(view, options.computeSpatialTilesetBaseUrl);
 }
