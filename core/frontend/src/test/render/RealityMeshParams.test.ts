46b1c40cf4736d4f4d94509439d027d3d92338b0
Seamus Kirby <32379572+skirby1996@users.noreply.github.com>
2023-04-03 15:04:57 -0400

    eslint disable

--- core/frontend/src/test/render/RealityMeshParams.test.ts
+++ core/frontend/src/test/render/RealityMeshParams.test.ts
@@ -11,6 +11,7 @@ import { RealityMeshParamsBuilder } from "../../render/RealityMeshParams";
 describe("RealityMeshParamsBuilder", () => {
   it("supports 8-, 16-, and 32-bit indices", () => {
     function test(numIndices: number, expectedType: typeof Uint8Array | typeof Uint16Array | typeof Uint32Array): void {
+      // eslint-disable-next-line no-console
       console.log(`Running test with ${numIndices} indices`);
       const lastIndex = numIndices - 1;
       const builder = new RealityMeshParamsBuilder({ positionRange: new Range3d(-1, -2, -3, 1, 2, 3) });
