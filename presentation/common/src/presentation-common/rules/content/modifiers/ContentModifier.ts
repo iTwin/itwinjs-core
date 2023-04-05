5c48f409c2f7d78ec37c67c648a995e1f59804bd
Vasaris Kaveckas <vasaris.kaveckas@bentley.com>
2023-04-05 15:25:24 +0300

    beta

--- presentation/common/src/presentation-common/rules/content/modifiers/ContentModifier.ts
+++ presentation/common/src/presentation-common/rules/content/modifiers/ContentModifier.ts
@@ -70,6 +70,7 @@ export interface ContentModifier extends RuleBase, ContentModifiersList {
    * Specifies whether [`calculatedProperties`]($docs/presentation/content/CalculatedPropertiesSpecification.md) and
    * [`relatedProperties`]($docs/presentation/content/RelatedPropertiesSpecification.md) specifications should also be applied on
    * [nested content]($docs/presentation/content/Terminology.md#nested-content).
+   * @beta
    */
   applyOnNestedContent?: boolean;
 }
