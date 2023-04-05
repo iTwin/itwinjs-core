1278caaa9eba3fda0fe12285c55e78fe9e54449a
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2023-04-05 07:55:21 -0400

    Remove EC rule 102- abstract base classes

--- core/ecschema-editing/src/Validation/ECRules.ts
+++ core/ecschema-editing/src/Validation/ECRules.ts
@@ -58,7 +58,6 @@ export const DiagnosticCodes = {
   // Class Rule Codes (100-199)
   BaseClassIsSealed: getCode(100),
   BaseClassOfDifferentType: getCode(101),
-  AbstractClassWithNonAbstractBase: getCode(102),
 
   // CA Container Rule Codes (500-599)
   CustomAttributeNotOfConcreteClass: getCode(500),
@@ -110,10 +109,6 @@ export const Diagnostics = {
   /** EC-101: Required message parameters: childClass.FullName, baseClass.FullName, baseClass.schemaItemType */
   BaseClassIsOfDifferentType: createClassDiagnosticClass<[string, string, string]>(DiagnosticCodes.BaseClassOfDifferentType,
     "Class '{0}' cannot derive from base class '{1}' of type '{2}'."),
-
-  /** EC-102: Required message parameters: childClass.FullName, baseClass.FullName */
-  AbstractClassWithNonAbstractBase: createClassDiagnosticClass<[string, string]>(DiagnosticCodes.AbstractClassWithNonAbstractBase,
-    "Abstract Class '{0}' cannot derive from base class '{1}' because it is not an abstract class."),
 
   /** EC-500: Required message parameters: CustomAttribute container name and CustomAttributeClass name. */
   CustomAttributeNotOfConcreteClass: createCustomAttributeContainerDiagnosticClass<[string, string]>(DiagnosticCodes.CustomAttributeNotOfConcreteClass,
@@ -197,7 +192,6 @@ export const ECRuleSet: IRuleSet = {
   classRules: [
     baseClassIsSealed,
     baseClassIsOfDifferentType,
-    abstractClassWithNonAbstractBase,
   ],
   propertyRules: [
     incompatibleValueTypePropertyOverride,
@@ -296,22 +290,6 @@ export async function* baseClassIsOfDifferentType(ecClass: AnyClass): AsyncItera
 
   const itemType = schemaItemTypeToString(baseClass.schemaItemType);
   yield new Diagnostics.BaseClassIsOfDifferentType(ecClass, [ecClass.fullName, baseClass.fullName, itemType]);
-}
-
-/**
- * EC Rule: Abstract class cannot derive from a non-abstract base class.
- * @internal
- */
-export async function* abstractClassWithNonAbstractBase(ecClass: AnyClass): AsyncIterable<ClassDiagnostic<any[]>> {
-  if (ecClass.modifier !== ECClassModifier.Abstract || !ecClass.baseClass)
-    return;
-
-  const baseClass = await ecClass.baseClass;
-  // return if rule passed
-  if (baseClass.modifier === ECClassModifier.Abstract)
-    return;
-
-  yield new Diagnostics.AbstractClassWithNonAbstractBase(ecClass, [ecClass.fullName, baseClass.fullName]);
 }
 
 /**
