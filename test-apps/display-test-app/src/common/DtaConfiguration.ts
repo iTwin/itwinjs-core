01a7445a5e677acd84d79a5e00edfe9ba9da4046
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2023-04-25 12:56:23 -0400

    Address tile content disposal issues (#5440)
    (cherry picked from commit c4ae8a52dd3a6bef71c5a720ec307cf6f156045a)

--- test-apps/display-test-app/src/common/DtaConfiguration.ts
+++ test-apps/display-test-app/src/common/DtaConfiguration.ts
@@ -69,6 +69,7 @@ export interface DtaNumberConfiguration {
 
 export interface DtaOtherConfiguration {
   disabledExtensions?: string[]; // An array of names of WebGL extensions to be disabled
+  gpuMemoryLimit?: string | number; // see GpuMemoryLimit in core-frontend for supported string values
 }
 
 /** Parameters for starting display-test-app with a specified initial configuration */
@@ -157,6 +158,11 @@ export const getConfig = (): DtaConfiguration => {
   configuration.useProjectExtents = undefined === process.env.IMJS_NO_USE_PROJECT_EXTENTS;
   configuration.noElectronAuth = undefined !== process.env.IMJS_NO_ELECTRON_AUTH;
   configuration.useFrontendTiles = undefined !== process.env.IMJS_USE_FRONTEND_TILES;
+  const gpuMemoryLimit = process.env.IMJS_GPU_MEMORY_LIMIT;
+  if (undefined !== gpuMemoryLimit) {
+    const gpuByteLimit = Number.parseInt(gpuMemoryLimit, 10);
+    configuration.gpuMemoryLimit = Number.isNaN(gpuByteLimit) ? gpuMemoryLimit : gpuByteLimit;
+  }
 
   const parseSeconds = (key: string) => {
     const env = process.env[key];
