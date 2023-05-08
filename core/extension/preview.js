71f42a95ff0dc15233ef6169ec41435154c02090
Arun George <aruniverse@users.noreply.github.com>
2023-05-08 12:13:09 -0400

    update gen-exports script to be esm compat

--- core/extension/preview.js
+++ core/extension/preview.js
@@ -8,7 +8,7 @@ const ext = globalThis[globalSymbol].getExtensionApi(import.meta.url);
 
 // BEGIN GENERATED CODE
 export const {
-  // @itwin/core-frontend:
-  // @itwin/core-common:
+// @itwin/core-frontend:
+// @itwin/core-common:
 } = ext.exports;
 // END GENERATED CODE
