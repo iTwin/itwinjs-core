34862122ec621687bebe70bca885b4ec0b99c893
pmconne <22944042+pmconne@users.noreply.github.com>
2023-05-09 14:32:38 -0400

    ImdlModel sans SurfaceParams

---
+++ core/frontend/src/imdl/ImdlModel.ts
@@ -0,0 +1,173 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Tiles
+ */
+
+import { StructuredCloneableObject, UintArray } from "@itwin/core-bentley";
+import { XYAndZ } from "@itwin/core-geometry";
+import {
+  ColorDefProps, FeatureIndexType, LinePixels, PolylineTypeFlags, QParams2dProps, QParams3dProps,
+} from "@itwin/core-common";
+import { EdgeTable } from "../render/primitives/EdgeParams";
+
+/*
+export type ImdlTextureMapping = Omit<TextureMapping, "texture"> & {
+  // The name of the texture as defined in ImdlDocument.namedTextures, or a gradient.
+  texture: string | Gradient.Symb;
+};
+
+export type ImdlRenderMaterial = Omit<RenderMaterial.Params, "textureMapping"> & {
+  textureMapping?: ImdlTextureMapping;
+};
+
+export interface ImdlSurfaceRenderMaterial {
+  isAtlas: false;
+  readonly material: ImdlRenderMaterial;
+}
+
+export type ImdlSurfaceMaterial = ImdlSurfaceRenderMaterial | SurfaceMaterialAtlas;
+
+export type ImdlSurfaceParams = Omit<SurfaceParams, "textureMapping" | "material"> & {
+  material?: ImdlSurfaceMaterial;
+  textureMapping?: {
+    texture: string | Gradient.Symb;
+    alwaysDisplayed: boolean;
+  };
+};
+
+export type ImdlMeshParams = Omit<MeshParams, "surface"> & {
+  surface: ImdlSurfaceParams;
+};
+
+export type ImdlPrimitiveParams = ImdlMeshParams | PointStringParams | PolylineParams;
+*/
+
+export namespace ImdlModel {
+  export interface VertexTable extends StructuredCloneableObject {
+    data: Uint8Array;
+    usesUnquantizedPositions?: boolean;
+    qparams: QParams3dProps;
+    width: number;
+    height: number;
+    hasTranslucency: boolean;
+    uniformColor?: ColorDefProps;
+    featureIndexType: FeatureIndexType;
+    uniformFeatureID?: number;
+    numVertices: number;
+    numRgbaPerVertex: number;
+    uvParams?: QParams2dProps;
+  }
+
+  export interface PointStringParams {
+    vertices: VertexTable;
+    indices: Uint8Array;
+    weight: number;
+  }
+
+  export interface TesselatedPolyline {
+    indices: Uint8Array;
+    prevIndices: Uint8Array;
+    nextIndicesAndParams: Uint8Array;
+  }
+
+  export interface PolylineParams {
+    vertices: VertexTable;
+    polyline: TesselatedPolyline;
+    isPlanar: boolean;
+    type: PolylineTypeFlags;
+    weight: number;
+    linePixels: LinePixels;
+  }
+
+  export interface SegmentEdgeParams {
+    indices: Uint8Array;
+    endPointAndQuadIndices: Uint8Array;
+  }
+
+  export interface SilhouetteParams extends SegmentEdgeParams {
+    normalPairs: Uint8Array;
+  }
+
+  export interface IndexedEdgeParams {
+    indices: Uint8Array;
+    edges: EdgeTable;
+  }
+
+  export interface EdgeParams {
+    weight: number;
+    linePixels: LinePixels;
+    segments?: SegmentEdgeParams;
+    silhouettes?: SilhouetteParams;
+    polylines?: TesselatedPolyline;
+    indexed?: IndexedEdgeParams;
+  }
+
+  export interface MeshParams {
+    vertices: VertexTable;
+    // ###TODO surface: SurfaceParams
+    edges?: EdgeParams;
+    isPlanar: boolean;
+    // ###TODO auxChannels?: AuxChannelTable;
+  }
+
+  export type PrimitiveParams = {
+    params: MeshParams;
+    viOrigin?: XYAndZ;
+    type: "mesh";
+  } | {
+    params: PointStringParams;
+    viOrigin: XYAndZ;
+    type: "point";
+  } | {
+    params: PolylineParams;
+    viOrigin?: XYAndZ;
+    type: "polyline";
+  };
+
+  export interface BasicNode {
+    primitives: PrimitiveParams[];
+    animationNodeId?: never;
+    animationId?: never;
+    layerId?: never;
+  }
+
+  export interface AnimationNode {
+    primitives: PrimitiveParams[];
+    animationNodeId: number;
+    animationId: string;
+    layerId?: never;
+  }
+
+  export interface Layer {
+    primitives: PrimitiveParams[];
+    layerId: string;
+    animationNodeId?: never;
+    animationId?: never;
+  }
+
+  export type Node = BasicNode | AnimationNode | Layer;
+
+  export interface SingleModelFeatureTable {
+    multiModel: false;
+    data: Uint32Array;
+    numFeatures: number;
+    animationNodeIds?: UintArray;
+  }
+
+  export interface MultiModelFeatureTable {
+    multiModel: true;
+    data: Uint32Array;
+    numFeatures: number;
+    numSubCategories: number;
+  }
+
+  export type FeatureTable = SingleModelFeatureTable | MultiModelFeatureTable;
+
+  export interface Document {
+    featureTable: FeatureTable;
+    nodes: Node[];
+  }
+}
