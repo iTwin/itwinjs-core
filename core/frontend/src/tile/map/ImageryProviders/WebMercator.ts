43b3305a810af4eea6347e60f352fe79a12758ca
Michel Dastous <mdastous-bentley@users.noreply.github.com>
2023-05-12 13:06:26 -0400

    removed duplicated web mercator class

--- core/frontend/src/tile/map/ImageryProviders/WebMercator.ts
+++ core/frontend/src/tile/map/ImageryProviders/WebMercator.ts
@@ -7,15 +7,12 @@ import { Angle, Constant } from "@itwin/core-geometry";
 
 /** @internal */
 export class WebMercator {
-  protected static readonly earthRadius = 6378137;
 
-  /** @internal */
   // calculates the longitude in EPSG:4326 (WGS84) from the projected x cartesian coordinate in EPSG:3857
-  public static  getEPSG4326Lon(x3857: number): number {
+  public static getEPSG4326Lon(x3857: number): number {
     return Angle.radiansToDegrees(x3857/Constant.earthRadiusWGS84.equator);
   }
 
-  /** @internal */
   // calculates the latitude in EPSG:4326 (WGS84) from the projected y cartesian coordinate in EPSG:3857
   public static getEPSG4326Lat(y3857: number): number {
     const y = 2 * Math.atan(Math.exp(y3857 / Constant.earthRadiusWGS84.equator)) - (Math.PI/2);
