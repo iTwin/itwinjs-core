595de0d75ccc82fe2ed96023e5447b3ebeb6353d
Gerardas Butkevicius <10091419+GerardasB@users.noreply.github.com>
2023-05-17 18:56:19 +0300

    Revert isLetter removal.

--- ui/appui-abstract/src/appui-abstract.ts
+++ ui/appui-abstract/src/appui-abstract.ts
@@ -53,6 +53,7 @@ export * from "./appui-abstract/statusbar/StatusBarItemsManager";
 
 export * from "./appui-abstract/utils/callbacks";
 export * from "./appui-abstract/utils/misc";
+export * from "./appui-abstract/utils/isLetter";
 export * from "./appui-abstract/utils/IconSpecUtilities";
 export * from "./appui-abstract/utils/PointProps";
 export * from "./appui-abstract/utils/UiError";
