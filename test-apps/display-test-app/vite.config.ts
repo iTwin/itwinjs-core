30ed9768c1c253066fbc86bdc00a778b90654c11
maggiemah <55515768+maggiemah@users.noreply.github.com>
2023-06-02 13:22:35 -0700

    edit vite config and move index.html back

--- test-apps/display-test-app/vite.config.ts
+++ test-apps/display-test-app/vite.config.ts
@@ -2,35 +2,57 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import envCompatible from 'vite-plugin-env-compatible';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { externalGlobalPlugin } from "esbuild-plugin-external-global";
-import { esbuildCommonjs, viteCommonjs } from "@kckst8/vite-plugin-commonjs";
+import { externalGlobalPlugin } from 'esbuild-plugin-external-global';
+import { esbuildCommonjs, viteCommonjs } from '@kckst8/vite-plugin-commonjs';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import ignore from "rollup-plugin-ignore";
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import ignore from 'rollup-plugin-ignore';
+import replace from '@rollup/plugin-replace';
+import rollupNodeModulesPolyfillPlugin from "rollup-plugin-node-polyfills";
 
 const mode = process.env.NODE_ENV === "development" ? "development" : "production";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // This changes the output dir from dist to build
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return {
-      server: {
-          port: 3000,
-          strictPort: true,
+    server: {
+        open: '/public/index.html',
+        port: 3000,
+        strictPort: true,
+        fs: {
+          allow: [
+            searchForWorkspaceRoot(process.cwd()),
+          ],
         },
+    },
     envPrefix: "REACT_APP_",
-    build: {
-      outDir: 'build',
+    build: { 
+      outDir: './lib',
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
+      rollupOptions: {
+        input: 'src/index.ts'
+      }
     },
     plugins: [
       ignore(["electron"]),
       react(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("development")
+      }),
+      viteCommonjs({
+        include: [
+          "@itwin/core-electron",
+        ],
+      }),
       svgrPlugin({
       svgrOptions: {
         icon: true,
@@ -39,11 +61,13 @@ export default defineConfig(({ mode }) => {
       envCompatible(),
       tsconfigPaths(),
     ],
+    resolve: {
+        extensions: ['.js', '.ts'],
+        alias: [
+        ],
+    },
     optimizeDeps: {
       esbuildOptions: {
-        define: {
-          global: "globalThis",
-        },
         plugins: [
           NodeGlobalsPolyfillPlugin({
             process: true,
@@ -54,14 +78,10 @@ export default defineConfig(({ mode }) => {
             electron: "window['electron']",
           }),
           esbuildCommonjs([
-            "@itwin/core-frontend",
-            "@itwin/presentation-frontend",
-            "@itwin/presentation-components",
-            "@itwin/appui-react",
+            "@itwin/core-electron",
           ]),
         ],
       },
-      include: ["@studio/promise-utils"], // see https://github.com/vitejs/vite/issues/5668
     },
   }
 })
