0214efdac184972913b139692e7107bd8b1b09fe
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2023-06-09 14:52:05 -0400

    Improve batching of coordinate conversion requests (#5321)

--- core/frontend/src/Viewport.ts
+++ core/frontend/src/Viewport.ts
@@ -1059,7 +1059,7 @@ export abstract class Viewport implements IDisposable, TileUser {
     const worldPoint = hit.hitPoint.clone();
     const backgroundMapGeometry = hit.viewport.displayStyle.getBackgroundMapGeometry();
     if (undefined !== backgroundMapGeometry) {
-      featureInfo.hitPoint = await backgroundMapGeometry.dbToCartographicFromGcs(worldPoint);
+      featureInfo.hitPoint = (await backgroundMapGeometry.dbToCartographicFromGcs([worldPoint]))[0];
     }
 
     const results = await Promise.all(promises);
