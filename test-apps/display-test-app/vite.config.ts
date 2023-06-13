39c0ee9c173ed235af0d9c633f098987b9799885
Arun George <aruniverse@users.noreply.github.com>
2023-06-13 15:30:22 -0400

    got it working?

--- test-apps/display-test-app/vite.config.ts
+++ test-apps/display-test-app/vite.config.ts
@@ -2,64 +2,90 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { defineConfig, loadEnv, searchForWorkspaceRoot } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import envCompatible from 'vite-plugin-env-compatible';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import browserslistToEsbuild from 'browserslist-to-esbuild';
-import { externalGlobalPlugin } from 'esbuild-plugin-external-global';
-import { esbuildCommonjs, viteCommonjs } from '@kckst8/vite-plugin-commonjs';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import ignore from 'rollup-plugin-ignore';
-import viteInspect from "vite-plugin-inspect";
-import replace from '@rollup/plugin-replace';
-import copy from "rollup-plugin-copy";
-import * as packageJson from "./package.json";
-import fs from 'fs';
+import { defineConfig, loadEnv, searchForWorkspaceRoot } from "vite";
 
-const mode = process.env.NODE_ENV === "development" ? "development" : "production";
+import envCompatible from "vite-plugin-env-compatible";
+import tsconfigPaths from "vite-tsconfig-paths";
+import browserslistToEsbuild from "browserslist-to-esbuild";
+import { esbuildCommonjs } from "@kckst8/vite-plugin-commonjs";
+
+import viteInspect from "vite-plugin-inspect";
+
+
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
+import { externalGlobalPlugin } from "esbuild-plugin-external-global";
+
+
+import replace from "@rollup/plugin-replace";
+import copy from "rollup-plugin-copy";
+import ignore from "rollup-plugin-ignore";
+import rollupVisualizer from "rollup-plugin-visualizer";
+import { webpackStats } from "rollup-plugin-webpack-stats";
+
+import * as packageJson from "./package.json";
+
+const mode = process.env.NODE_ENV === "development" ?? "production";
+
 const iTwinDeps = Object.keys(packageJson.dependencies)
   .filter((pkgName) => pkgName.startsWith("@itwin"))
   .flatMap((pkgName) => {
-    return [`./node_modules/${pkgName}/lib/public/**`, `${pkgName.replace("@itwin/core-", "../../core/")}/src/public/**`]
-  })
-  // use require.resolve for paths
-  console.log(iTwinDeps);
+    return [
+      `./node_modules/${pkgName}/lib/public/**`,
+      `${pkgName.replace("@itwin/core-", "../../core/")}/src/public/**`,
+    ];
+  });
+// use require.resolve for paths
+// console.log(iTwinDeps);
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   // This changes the output dir from dist to build
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
-    debug: mode === "development",
+    debug: true,
     server: {
-        open: false,
-        port: 3000,
-        strictPort: true,
-        fs: {
-          allow: [
-            searchForWorkspaceRoot(process.cwd()),
-          ],
-        },
+      open: false,
+      port: 3000,
+      strictPort: true,
+      fs: {
+        allow: [searchForWorkspaceRoot(process.cwd())],
+      },
     },
-    envPrefix: "REACT_APP_",
+    envPrefix: "IMJS_",
     build: {
-      outDir: './lib',
-      sourcemap: mode === "development",
-      minify: mode !== "development",
+      outDir: "./lib",
+      sourcemap: "inline",
+      minify: false,
       target: browserslistToEsbuild(),
       commonjsOptions: {
-        include: [/core\/electron/, /core\/mobile/, /node_modules/],
+        include: [
+          /core\/common/,
+          /core\/electron/,
+          /core\/frontend/,
+          /core\/mobile/,
+          /node_modules/,
+        ],
       },
       rollupOptions: {
-        input: 'src/index.ts',
-      }
+        input: "src/index.ts",
+        plugins: [
+          rollupVisualizer({
+            open: true,
+            // emitFile: true,
+            filename: "stats.html",
+            template: "network",
+            sourcemap: true,
+          }),
+          webpackStats(), // needs to be the last plugin
+        ],
+      },
     },
     plugins: [
-      ignore(["electron"]),
-      react(),
+      ignore(["electron"]), // equivalent to webpack externals
+      // react(),
+      // copy static assets to public folder
       copy({
         targets: [
           {
@@ -67,24 +93,27 @@ export default defineConfig(() => {
             dest: "public",
             rename: (_name, _extension, fullPath) => {
               const regex = new RegExp("(public(?:\\\\|/))(.*)");
-              return regex.exec(fullPath)[2];
+              return regex.exec(fullPath)![2];
             },
           },
         ],
         verbose: true,
         overwrite: true,
+        copyOnce: true,
       }),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("development"),
-        preventAssignment: true
+      // replace({
+      //   // "process.env.NODE_ENV": JSON.stringify("development"),
+      //   preventAssignment: true,
+      // }),
+      ...(process.env.NODE_ENV === "development" ? [viteInspect({ build: true })] : []),
+      // svgrPlugin({
+      //   svgrOptions: {
+      //     icon: true,
+      //   },
+      // }),
+      envCompatible({
+        prefix: "IMJS_",
       }),
-      ...(mode === "development" ? [viteInspect({ build: true })] : []),
-      svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-      envCompatible(),
       tsconfigPaths(),
     ],
     optimizeDeps: {
@@ -99,17 +128,18 @@ export default defineConfig(() => {
             electron: "window['electron']",
           }),
           esbuildCommonjs([
-            'react-s3'
+            "@itwin/core-electron"
           ]),
         ],
       },
       // force: true, // TODO: remove line
       // overoptimized dependencies in the same monorepo
       include: [
+        "@itwin/core-common",
         "@itwin/core-electron/lib/cjs/ElectronFrontend",
+        "@itwin/core-frontend",
         "@itwin/core-mobile/lib/cjs/MobileFrontend",
-        "@itwin/core-frontend"
-      ]
+      ],
     },
-  }
-})
+  };
+});
