ba74a041bab13ab16a836f732e3b74d07e9d8080
maggiemah <55515768+maggiemah@users.noreply.github.com>
2023-06-15 18:32:13 -0700

    minor clean up

--- test-apps/display-test-app/src/vite-env.d.ts
+++ test-apps/display-test-app/src/vite-env.d.ts
@@ -3,13 +3,3 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-svgr/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    // more env variables...
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
