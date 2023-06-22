5d3c3f4ab6ea6d46452051b2ffa95b0528c1dcc7
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2023-06-22 12:59:23 -0400

    change log. Added tests

--- extensions/map-layers-formats/src/ArcGisFeature/ArcGisFeatureProvider.ts
+++ extensions/map-layers-formats/src/ArcGisFeature/ArcGisFeatureProvider.ts
@@ -100,6 +100,7 @@ export class ArcGisFeatureProvider extends ArcGISImageryProvider {
           for (const layer of this.serviceJson.layers) {
             if (layer.defaultVisibility) {
               this._layerId = layer.id;
+              break;
             }
           }
         } else {
@@ -169,48 +170,23 @@ export class ArcGisFeatureProvider extends ArcGISImageryProvider {
 
     // Read range using full extent from service metadata
     if (this._layerMetadata?.extent) {
-      const readExtent = (extent: any) => {
-        const range3857 = Range2d.createFrom({
-          low: { x: extent.xmin, y: extent.ymin },
-          high: { x: extent.xmax, y: extent.ymax },
-        });
-
-        const west = this.getEPSG4326Lon(range3857.xLow);
-        const south = this.getEPSG4326Lat(range3857.yLow);
-        const east = this.getEPSG4326Lon(range3857.xHigh);
-        const north = this.getEPSG4326Lat(range3857.yHigh);
-        this.cartoRange = MapCartoRectangle.fromDegrees(west, south, east, north);
-      };
-
       const layerExtent = this._layerMetadata?.extent;
       if (layerExtent.spatialReference.latestWkid === 3857 || layerExtent.spatialReference.wkid === 102100) {
-        readExtent(layerExtent);
-      } else {
-        // Extent is not advertised in a coordinate system we support, make a new request to get proper extent
-        try {
-          let extentJson:any;
-          const tmpUrl = new URL(this._settings.url);
-          tmpUrl.pathname = `${tmpUrl.pathname}/${this._layerId}/query`;
-          tmpUrl.searchParams.append("where", "1=1");
-          tmpUrl.searchParams.append("outSR", "3857");
-          tmpUrl.searchParams.append("returnExtentOnly", "true");
-          tmpUrl.searchParams.append("f", "json");
-          const cached = ArcGisFeatureProvider._extentCache.get(tmpUrl.toString());
-          if (cached) {
-            extentJson = cached;
-          } else {
-            const response = await this.fetch(tmpUrl, { method: "GET" });
-            const extentJson = await response.json();
-            ArcGisFeatureProvider._extentCache.set(tmpUrl.toString(), extentJson);
-          }
+        this.setCartoRangeFromExtentJson(layerExtent);
+      }
+    }
 
-          if (extentJson?.extent)
-            readExtent(extentJson?.extent);
-          else
-            Logger.logWarning(loggerCategory, `Could not get features extent, disabling extent filtering`);
-        } catch {
-          Logger.logError(loggerCategory, `Could not get feature extent`);
-        }
+    if (!this.cartoRange) {
+      // Range could not be found (or is not in a coordinate system we support), make a request to compute the extent
+      try {
+        const extentJson = await this.fetchLayerExtent();
+
+        if (extentJson)
+          this.setCartoRangeFromExtentJson(extentJson);
+        else
+          Logger.logWarning(loggerCategory, `Could not get features extent, disabling extent filtering`);
+      } catch {
+        Logger.logError(loggerCategory, `Could not get feature extent`);
       }
     }
 
@@ -225,6 +201,38 @@ export class ArcGisFeatureProvider extends ArcGISImageryProvider {
     this._maxDepthFromLod =  (scales.maxLod ? scales.maxLod : this.defaultMaximumZoomLevel);
 
     this._symbologyRenderer = new ArcGisSymbologyRenderer(this._layerMetadata?.geometryType, this._layerMetadata?.drawingInfo?.renderer);
+  }
+
+  private async fetchLayerExtent() {
+    let extentJson: any;
+    const tmpUrl = new URL(this._settings.url);
+    tmpUrl.pathname = `${tmpUrl.pathname}/${this._layerId}/query`;
+    tmpUrl.searchParams.append("where", "1=1");
+    tmpUrl.searchParams.append("outSR", "3857");
+    tmpUrl.searchParams.append("returnExtentOnly", "true");
+    tmpUrl.searchParams.append("f", "json");
+    const cached = ArcGisFeatureProvider._extentCache.get(tmpUrl.toString());
+    if (cached) {
+      extentJson = cached;
+    } else {
+      const response = await this.fetch(tmpUrl, { method: "GET" });
+      extentJson = await response.json();
+      ArcGisFeatureProvider._extentCache.set(tmpUrl.toString(), extentJson);
+    }
+    return (extentJson ? extentJson.extent : undefined);
+  }
+
+  private setCartoRangeFromExtentJson(extent: any) {
+    const range3857 = Range2d.createFrom({
+      low: { x: extent.xmin, y: extent.ymin },
+      high: { x: extent.xmax, y: extent.ymax },
+    });
+
+    const west = this.getEPSG4326Lon(range3857.xLow);
+    const south = this.getEPSG4326Lat(range3857.yLow);
+    const east = this.getEPSG4326Lon(range3857.xHigh);
+    const north = this.getEPSG4326Lat(range3857.yHigh);
+    this.cartoRange = MapCartoRectangle.fromDegrees(west, south, east, north);
   }
 
   protected async getLayerMetadata(layerId: number) {
