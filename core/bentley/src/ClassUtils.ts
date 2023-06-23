51ecc1beace8420c70fced5cb7e29634b5aedacb
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2023-06-22 20:50:01 -0400

    Fix Entity release tags (#5662)

--- core/bentley/src/ClassUtils.ts
+++ core/bentley/src/ClassUtils.ts
@@ -2,12 +2,16 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Utils
+ */
 
 /** Check if class `subclass` is a different class from `superclass` but extends from `superclass`
- * @internal
  * @param subclass the class that may be a subclass of `superclass`
  * @param superclass the class that may be a base class of `subclass`
+ * @note The constructors of all types must be `public`.
  * @returns whether `subclass` is a proper subclass of `superclass`
+ * @public
  */
 export function isProperSubclassOf<
   SuperClass extends new (..._: any[]) => any,
@@ -18,10 +22,11 @@ export function isProperSubclassOf<
 }
 
 /** Check if class `subclass` is `superclass` or extends from `superclass`
- * @internal
- * @param subclass the class that may be a subclass of `superclass`
- * @param superclass the class that may be a base class of `subclass`
+ * @param subclass the class that may be a subclass of `superclass`.
+ * @param superclass the class that may be a base class of `subclass`.
+ * @note The constructors of all types must be `public`.
  * @returns whether `subclass` is a subclass of `superclass`
+ * @public
  */
 export function isSubclassOf<
   SuperClass extends new (..._: any[]) => any,
