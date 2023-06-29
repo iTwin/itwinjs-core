e4739e4aa1e54ea2c0571592da587f052be9798f
pmconne <22944042+pmconne@users.noreply.github.com>
2023-06-29 09:32:32 -0400

    wip test infrastructure

---
+++ extensions/frontend-tiles/src/test/FrontendTiles.test.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { expect } from "chai";
+
+describe("test", () => {
+  it("tests", () => {
+    expect(true).to.be.false;
+  });
+});
