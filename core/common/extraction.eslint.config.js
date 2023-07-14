14bf2aeca6156fcf8bcd3639a4e4a3558875a46e
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-07-14 12:07:43 -0400

    changes to 2nd config file

--- core/common/extraction.eslint.config.js
+++ core/common/extraction.eslint.config.js
@@ -4,5 +4,17 @@ module.exports = [
   {
     files: ["src/**/*.ts"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
+  },
+  {
+    files: ["src/**/*.ts"],
+    ...iTwinPlugin.configs.extensionExportsConfig,
+  },
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      "@itwin/no-internal-barrel-imports": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "comma-dangle": "off"
+    }
   }
 ];
