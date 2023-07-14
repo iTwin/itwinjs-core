28e89f6936e599b686227858f10494c1d2126c53
Yashasvi Mutteneni <yashincontrol@users.noreply.github.com>
2023-07-14 16:28:12 -0400

    Switch to @itwin/eslint-plugin 4.0.0-dev.44

--- core/frontend/extraction.eslint.config.js
+++ core/frontend/extraction.eslint.config.js
@@ -3,18 +3,6 @@ const iTwinPlugin = require("@itwin/eslint-plugin");
 module.exports = [
   {
     files: ["src/**/*.ts"],
-    ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
-  },
-  {
-    files: ["src/**/*.ts"],
     ...iTwinPlugin.configs.extensionExportsConfig,
-  },
-  {
-    files: ["src/**/*.ts"],
-    rules: {
-      "@itwin/no-internal-barrel-imports": "off",
-      "@typescript-eslint/unbound-method": "off",
-      "comma-dangle": "off"
-    }
   }
 ];
