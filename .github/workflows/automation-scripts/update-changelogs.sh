6c67ab39c9139e920cf3d9217d7cd4280809ad64
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-07-17 10:33:19 -0400

    add comments

--- .github/workflows/automation-scripts/update-changelogs.sh
+++ .github/workflows/automation-scripts/update-changelogs.sh
@@ -1,3 +1,4 @@
+# Using git termanology, current represents the branch targeted for changes, incoming is the branch with new changelogs
 currentPath="./temp-current-changelogs"
 incomingPath="./temp-incoming-changelogs"
 
@@ -9,6 +10,7 @@ if [ -z "$commitId" ]; then
   exit 1
 fi
 
+# copy all changelogs from the incoming branch to a temp folder, the files will be named: package_name_CHANGELOG.json
 git checkout $commitId
 find ./ -type f -name "CHANGELOG.json" -not -path "*/node_modules/*" -exec sh -c 'cp "{}" "./temp-incoming-changelogs/$(echo "{}" | sed "s/^.\///; s/\//_/g")"' \;
 
@@ -17,16 +19,21 @@ if [ -z "$currentBranch" ]; then
   exit 1
 fi
 
+# copy all changelogs from the target branch to a temp folder, the files will be named: package_name_CHANGELOG.json
 git checkout $currentBranch
 find ./ -type f -name "CHANGELOG.json" -not -path "*/node_modules/*" -exec sh -c 'cp "{}" "./temp-current-changelogs/$(echo "{}" | sed "s/^.\///; s/\//_/g")"' \;
 
+# run js script that will add the new changes from incoming to the target changelogs and output new json files into temp-current-changelogs
 node ./.github/workflows/automation-scripts/update-changelogs.js $currentPath $incomingPath
 
+# copy changelogs back to proper file paths and convert names back to: CHANGELOG.json
 find ./temp-current-changelogs/ -type f -name "*CHANGELOG.json" -exec sh -c 'cp "{}" "$(echo "{}" | sed "s|temp-current-changelogs/\(.*\)_|./\1/|; s|_|/|g")"' \;
 
+# delete temps
 rm -r $currentPath
 rm -r $incomingPath
 
+# regen CHANGELOG.md
 rush publish --regenerate-changelogs #updates changelogs
 
 commitMessage=$(git log --format=%B -n 1 $commitid)
