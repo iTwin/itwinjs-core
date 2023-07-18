6be5b22a45b928ad648589be867a71eff35c4f69
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-07-18 10:04:17 -0400

    remove unnecessary Checkout

--- .github/workflows/automation-scripts/update-changelogs.sh
+++ .github/workflows/automation-scripts/update-changelogs.sh
@@ -9,6 +9,7 @@ mkdir $incomingPath
 # find the latest release, and make that the target for the changelogs
 targetBranch=$(git branch -a --list "origin/release/[0-9]*.[0-9]*.x" | tail -n1 | sed 's/  remotes\///')
 currentBranch=$(git branch --show-current)
+commitMessage=$(git log --format=%B -n 1)
 
 if [ "origin/$currentBranch" = "$targetBranch" ]; then
   echo "The current branch is the latest release, so the target will be master branch"
@@ -17,13 +18,6 @@ else
   echo "The current branch is the $currentBranch, so the target will be $targetBranch branch"
 fi
 
-if [ -z "$commitId" ]; then
-  echo "ERROR: the variable commitId was not delcared"
-  exit 1
-fi
-
-# copy all changelogs from the incoming branch to a temp folder, the files will be named: package_name_CHANGELOG.json
-git checkout $commitId
 find ./ -type f -name "CHANGELOG.json" -not -path "*/node_modules/*" -exec sh -c 'cp "{}" "./temp-incoming-changelogs/$(echo "{}" | sed "s/^.\///; s/\//_/g")"' \;
 
 if [ -z "$targetBranch" ]; then
@@ -48,7 +42,6 @@ rm -r $incomingPath
 # regen CHANGELOG.md
 rush publish --regenerate-changelogs #updates changelogs
 
-commitMessage=$(git log --format=%B -n 1 $commitId)
 git add .
 git commit -m "$commitMessage Changelogs"
 
