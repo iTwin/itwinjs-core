a988d6d7c5e56a7d553ad6a1df3b4d12b1dcfc01
Hoang Nam Le <50554904+hl662@users.noreply.github.com>
2023-07-20 15:33:42 -0400

    fix missing import error for icons generic webfont

--- test-apps/display-test-app/src/frontend/App.ts
+++ test-apps/display-test-app/src/frontend/App.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-
+import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
 import { GuidString, ProcessDetector } from "@itwin/core-bentley";
 import { ElectronApp, ElectronAppOpts } from "@itwin/core-electron/lib/cjs/ElectronFrontend";
 import { BrowserAuthorizationCallbackHandler } from "@itwin/browser-authorization";
