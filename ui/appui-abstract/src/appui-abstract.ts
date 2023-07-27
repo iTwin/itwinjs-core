aed824af3cf6c33d1d4b560f7ac53550b1b1f624
Arun George <aruniverse@users.noreply.github.com>
2023-07-27 09:40:13 -0400

    get appui-abstract to build; rm internal Managers

--- ui/appui-abstract/src/appui-abstract.ts
+++ ui/appui-abstract/src/appui-abstract.ts
@@ -1,15 +1,9 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 
-export * from "./appui-abstract/BaseUiItemsProvider";
 export * from "./appui-abstract/UiAdmin";
-export * from "./appui-abstract/UiItemsManager";
-export * from "./appui-abstract/UiItemsProvider";
-
-export * from "./appui-abstract/backstage/BackstageItem";
-export * from "./appui-abstract/backstage/BackstageItemsManager";
 
 export * from "./appui-abstract/common/KeyboardKey";
 
@@ -26,9 +20,7 @@ export * from "./appui-abstract/items/AbstractToolbarProps";
 export * from "./appui-abstract/items/BadgeType";
 export * from "./appui-abstract/items/ConditionalBooleanValue";
 export * from "./appui-abstract/items/ConditionalStringValue";
-export * from "./appui-abstract/items/ProvidedItem";
 export * from "./appui-abstract/items/RelativePosition";
-export * from "./appui-abstract/items/StageUsage";
 
 export * from "./appui-abstract/notification/MessagePresenter";
 export * from "./appui-abstract/notification/MessageSeverity";
@@ -42,14 +34,8 @@ export * from "./appui-abstract/properties/StandardTypeNames";
 export * from "./appui-abstract/properties/Value";
 
 export * from "./appui-abstract/quantity/BaseQuantityDescription";
-export * from "./appui-abstract/statusbar/StatusBarItem";
-export * from "./appui-abstract/statusbar/StatusBarItemsManager";
 
 export * from "./appui-abstract/toolbars/ToolbarItem";
-export * from "./appui-abstract/toolbars/ToolbarItemsManager";
-
-export * from "./appui-abstract/statusbar/StatusBarItem";
-export * from "./appui-abstract/statusbar/StatusBarItemsManager";
 
 export * from "./appui-abstract/utils/callbacks";
 export * from "./appui-abstract/utils/misc";
@@ -62,10 +48,6 @@ export * from "./appui-abstract/utils/UiEvent";
 export * from "./appui-abstract/utils/filter/charCode";
 export * from "./appui-abstract/utils/filter/filters";
 export * from "./appui-abstract/utils/filter/strings";
-
-export * from "./appui-abstract/widget/AbstractWidgetProps";
-export * from "./appui-abstract/widget/StagePanel";
-export * from "./appui-abstract/widget/WidgetState";
 
 /** @docs-package-description
  * The appui-abstract package contains abstractions for UI controls, such as toolbars, buttons and menus.
