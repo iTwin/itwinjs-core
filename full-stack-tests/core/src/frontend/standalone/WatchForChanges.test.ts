633f4ac653fe885d1c0eb0771785a5c210b85a95
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2023-08-16 17:33:30 -0400

    test that it works when viewing through the same connection that's modifying

--- full-stack-tests/core/src/frontend/standalone/WatchForChanges.test.ts
+++ full-stack-tests/core/src/frontend/standalone/WatchForChanges.test.ts
@@ -11,110 +11,112 @@ import { BriefcaseConnection, GeometricModelState, ViewCreator3d } from "@itwin/
 import { coreFullStackTestIpc, initializeEditTools, insertLineStringElement, makeModelCode, transformElements } from "../Editing";
 import { TestUtility } from "../TestUtility";
 
-describe.only("watchForChanges", () => {
-  if (ProcessDetector.isMobileAppFrontend)
-    return;
+for (const watchForChanges of [false, true]) {
+  describe.only(`watchForChanges (${watchForChanges})`, () => {
+    if (ProcessDetector.isMobileAppFrontend)
+      return;
 
-  let rwConn: BriefcaseConnection;
-  let roConn: BriefcaseConnection;
-  let modelId: string;
-  let categoryId: string;
-  let elemId: string;
+    let rwConn: BriefcaseConnection;
+    let roConn: BriefcaseConnection;
+    let modelId: string;
+    let categoryId: string;
+    let elemId: string;
 
-  before(async () => {
-    await TestUtility.startFrontend(undefined, undefined, true);
-    await initializeEditTools();
-  });
-
-  after(async () => TestUtility.shutdownFrontend());
-
-  beforeEach(async () => {
-    const filePath = path.join(process.env.IMODELJS_CORE_DIRNAME!, "core/backend/lib/cjs/test/assets/planprojection.bim");
-
-    // Populate the iModel with some initial geometry.
-    rwConn = await BriefcaseConnection.openStandalone(filePath, OpenMode.ReadWrite);
-    modelId = await coreFullStackTestIpc.createAndInsertPhysicalModel(rwConn.key, (await makeModelCode(rwConn, rwConn.models.repositoryModelId, Guid.createValue())));
-    const dictId = await rwConn.models.getDictionaryModel();
-    categoryId = await coreFullStackTestIpc.createAndInsertSpatialCategory(rwConn.key, dictId, Guid.createValue(), { color: 0 });
-
-    const projCenter = rwConn.projectExtents.center;
-    const point = new Point3d(Math.round(projCenter.x), Math.round(projCenter.y), Math.round(projCenter.z));
-    elemId = await insertLineStringElement(rwConn, { model: modelId, category: categoryId, color: ColorDef.green, points: [point, new Point3d(point.x, point.y + 2, point.z)] });
-    expect(Id64.isValid(elemId)).to.be.true;
-    await rwConn.saveChanges();
-
-    // Open a second, read-only connection that will monitor for changes made via the read-write connection.
-    roConn = await BriefcaseConnection.openFile({
-      fileName: filePath,
-      key: Guid.createValue(),
-      readonly: true,
-      watchForChanges: true,
+    before(async () => {
+      await TestUtility.startFrontend(undefined, undefined, true);
+      await initializeEditTools();
     });
-  });
 
-  afterEach(async () => {
-    await roConn.close();
-    await rwConn.close();
-  });
+    after(async () => TestUtility.shutdownFrontend());
 
-  async function expectModelChanges(func: () => Promise<void>): Promise<void> {
-    const promise = new Promise<void>((resolve) => {
-      roConn.onBufferedModelChanges.addOnce((modelIds) => {
-        expect(modelIds.size).to.equal(1);
-        expect(modelIds.has(modelId)).to.be.true;
-        resolve();
+    beforeEach(async () => {
+      const filePath = path.join(process.env.IMODELJS_CORE_DIRNAME!, "core/backend/lib/cjs/test/assets/planprojection.bim");
+
+      // Populate the iModel with some initial geometry.
+      rwConn = await BriefcaseConnection.openStandalone(filePath, OpenMode.ReadWrite);
+      modelId = await coreFullStackTestIpc.createAndInsertPhysicalModel(rwConn.key, (await makeModelCode(rwConn, rwConn.models.repositoryModelId, Guid.createValue())));
+      const dictId = await rwConn.models.getDictionaryModel();
+      categoryId = await coreFullStackTestIpc.createAndInsertSpatialCategory(rwConn.key, dictId, Guid.createValue(), { color: 0 });
+
+      const projCenter = rwConn.projectExtents.center;
+      const point = new Point3d(Math.round(projCenter.x), Math.round(projCenter.y), Math.round(projCenter.z));
+      elemId = await insertLineStringElement(rwConn, { model: modelId, category: categoryId, color: ColorDef.green, points: [point, new Point3d(point.x, point.y + 2, point.z)] });
+      expect(Id64.isValid(elemId)).to.be.true;
+      await rwConn.saveChanges();
+
+      // Open a second, read-only connection that will monitor for changes made via the read-write connection.
+      roConn = watchForChanges ? (await BriefcaseConnection.openFile({
+        fileName: filePath,
+        key: Guid.createValue(),
+        readonly: true,
+        watchForChanges: true,
+      })) : rwConn;
+    });
+
+    afterEach(async () => {
+      await roConn.close();
+      await rwConn.close();
+    });
+
+    async function expectModelChanges(func: () => Promise<void>): Promise<void> {
+      const promise = new Promise<void>((resolve) => {
+        roConn.onBufferedModelChanges.addOnce((modelIds) => {
+          expect(modelIds.size).to.equal(1);
+          expect(modelIds.has(modelId)).to.be.true;
+          resolve();
+        });
       });
+
+      await func();
+      return promise;
+    }
+
+    let xTranslation = 0;
+    async function moveElement(): Promise<void> {
+      const transform = Transform.createTranslationXYZ(++xTranslation, 0, 0);
+      await transformElements(rwConn, [elemId], transform);
+      await rwConn.saveChanges();
+    }
+
+    async function getModel(iModel: BriefcaseConnection): Promise<GeometricModelState> {
+      await iModel.models.load(modelId);
+      const model = iModel.models.getLoaded(modelId) as GeometricModelState;
+      expect(model).instanceof(GeometricModelState);
+      return model;
+    }
+
+    it("updates ModelState.geometryGuid when model geometry changes", async () => {
+      const model = await getModel(roConn);
+      const prevGuid = model.geometryGuid;
+      expect(prevGuid).not.to.be.undefined;
+
+      await expectModelChanges(async () => moveElement());
+      expect(roConn.models.getLoaded(model.id)).to.equal(model);
+      expect(model.geometryGuid).not.to.be.undefined;
+      expect(model.geometryGuid).not.to.equal(prevGuid);
+
+      const rwModel = await getModel(rwConn);
+      expect(rwModel.geometryGuid).to.equal(model.geometryGuid);
     });
 
-    await func();
-    return promise;
-  }
+    it("purges and recreates tile trees when model geometry changes", async () => {
+      const viewCreator = new ViewCreator3d(roConn);
+      const view = await viewCreator.createDefaultView(undefined, [modelId]);
 
-  let xTranslation = 0;
-  async function moveElement(): Promise<void> {
-    const transform = Transform.createTranslationXYZ(++xTranslation, 0, 0);
-    await transformElements(rwConn, [elemId], transform);
-    await rwConn.saveChanges();
-  }
+      const model = await getModel(roConn);
+      const ref = model.createTileTreeReference(view);
+      const prevTree = (await ref.treeOwner.loadTree())!;
+      expect(prevTree).not.to.be.undefined;
 
-  async function getModel(iModel: BriefcaseConnection): Promise<GeometricModelState> {
-    await iModel.models.load(modelId);
-    const model = iModel.models.getLoaded(modelId) as GeometricModelState;
-    expect(model).instanceof(GeometricModelState);
-    return model;
-  }
+      await expectModelChanges(async () => moveElement());
+      const newTree = (await ref.treeOwner.loadTree())!;
+      expect(newTree).not.to.be.undefined;
+      expect(newTree).not.to.equal(prevTree);
+      expect(newTree.isDisposed).to.be.false;
+      expect(prevTree.isDisposed).to.be.true;
 
-  it("updates ModelState.geometryGuid when model geometry changes", async () => {
-    const model = await getModel(roConn);
-    const prevGuid = model.geometryGuid;
-    expect(prevGuid).not.to.be.undefined;
-
-    await expectModelChanges(async () => moveElement());
-    expect(roConn.models.getLoaded(model.id)).to.equal(model);
-    expect(model.geometryGuid).not.to.be.undefined;
-    expect(model.geometryGuid).not.to.equal(prevGuid);
-
-    const rwModel = await getModel(rwConn);
-    expect(rwModel.geometryGuid).to.equal(model.geometryGuid);
+      console.log(JSON.stringify(prevTree.iModelTransform.toJSON()));
+      console.log(JSON.stringify(newTree.iModelTransform.toJSON()));
+    });
   });
-
-  it("purges and recreates tile trees when model geometry changes", async () => {
-    const viewCreator = new ViewCreator3d(roConn);
-    const view = await viewCreator.createDefaultView(undefined, [modelId]);
-
-    const model = await getModel(roConn);
-    const ref = model.createTileTreeReference(view);
-    const prevTree = (await ref.treeOwner.loadTree())!;
-    expect(prevTree).not.to.be.undefined;
-
-    await expectModelChanges(async () => moveElement());
-    const newTree = (await ref.treeOwner.loadTree())!;
-    expect(newTree).not.to.be.undefined;
-    expect(newTree).not.to.equal(prevTree);
-    expect(newTree.isDisposed).to.be.false;
-    expect(prevTree.isDisposed).to.be.true;
-
-    console.log(JSON.stringify(prevTree.iModelTransform.toJSON()));
-    console.log(JSON.stringify(newTree.iModelTransform.toJSON()));
-  });
-});
+}
