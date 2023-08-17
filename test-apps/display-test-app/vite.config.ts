76562e27974beda825463ed61ddc19e9f272fe4f
Michael Belousov <MichaelBelousov@users.noreply.github.com>
2023-08-17 14:13:25 -0400

    cherry-pick VITE_CI part of #5870 to prevent OOM

--- test-apps/display-test-app/vite.config.ts
+++ test-apps/display-test-app/vite.config.ts
@@ -47,9 +47,10 @@ export default defineConfig(() => {
     },
     envPrefix: "IMJS_",
     publicDir: ".static-assets",
+    logLevel: process.env.VITE_CI ? "error" : "warn",
     build: {
       outDir: "./lib",
-      sourcemap: process.env.CI ? false : "inline", // append to the resulting output file if not running in CI.
+      sourcemap: !!process.env.VITE_CI, // append to the resulting output file if not running in CI.
       minify: false, // disable compaction of source code
       target: browserslistToEsbuild(), // for browserslist in package.json
       commonjsOptions: {
