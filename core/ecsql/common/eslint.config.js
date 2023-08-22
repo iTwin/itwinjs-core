e293f9a4bb39222f27ea13b2c21464b7847cb53d
Affan Khan <khanaffan@users.noreply.github.com>
2023-08-22 09:32:52 -0400

    fix review comments.

--- core/ecsql/common/eslint.config.js
+++ core/ecsql/common/eslint.config.js
@@ -6,20 +6,5 @@ module.exports = [
     files: ["**/*.ts"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
   },
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "@itwin/public-extension-exports": [
-        "error",
-        {
-          "releaseTags": [
-            "public",
-            "preview"
-          ],
-          "outputApiFile": false
-        }
-      ]
-    }
-  },
   ...eslintBaseConfig,
 ];
