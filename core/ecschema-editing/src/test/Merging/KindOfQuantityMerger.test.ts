74949e8184b68d400438a9199afc25ca76d39157
Julio Alva <julioalva@users.noreply.github.com>
2023-08-25 11:17:04 -0400

    descriobe kind of quantity test

---
+++ core/ecschema-editing/src/test/Merging/KindOfQuantityMerger.test.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import { Enumeration, Schema, SchemaContext } from "@itwin/ecschema-metadata";
+import { SchemaMerger } from "../../Merging/SchemaMerger";
+import { expect } from "chai";
+
+/* eslint-disable @typescript-eslint/naming-convention */
+
+describe("KindOfQuantity Merger test", ()=>{
+    
+})
