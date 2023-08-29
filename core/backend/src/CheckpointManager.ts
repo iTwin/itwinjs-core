be97c56ed80cd7f6a942adec28b73a5dfb51f1a3
Ben.Pusey <Ben-Pusey-Bentley@users.noreply.github.com>
2023-08-29 08:17:59 -0400

    Adds logging for applying changesets.

--- core/backend/src/CheckpointManager.ts
+++ core/backend/src/CheckpointManager.ts
@@ -349,7 +349,7 @@ export class CheckpointManager {
           const accessToken = checkpoint.accessToken;
           const toIndex = checkpoint.changeset.index ??
             (await IModelHost.hubAccess.getChangesetFromVersion({ accessToken, iModelId: checkpoint.iModelId, version: IModelVersion.asOfChangeSet(checkpoint.changeset.id) })).index;
-          const stopwatch = new StopWatch(`[${checkpoint.changeset.id}]`);
+          const stopwatch = new StopWatch(`[${checkpoint.changeset.id}]`, true);
           Logger.logInfo(loggerCategory, `Starting application of changeset with id ${stopwatch.description}`);
           await BriefcaseManager.pullAndApplyChangesets(db, { accessToken, toIndex });
           Logger.logInfo(loggerCategory, `Applied changeset with id ${stopwatch.description} (${stopwatch.elapsedSeconds} seconds)`);
