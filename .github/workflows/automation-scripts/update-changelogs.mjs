69d99e4c2052eb2536aea273c14d7ab27301e993
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-08-30 12:52:10 -0400

    origin

--- .github/workflows/automation-scripts/update-changelogs.mjs
+++ .github/workflows/automation-scripts/update-changelogs.mjs
@@ -67,7 +67,7 @@ await $`git commit -m "${commitMessage} Changelogs"`;
 await $`rush change --bulk --message "" --bump-type none`;
 await $`git add .`;
 await $`git commit --amend --no-edit`;
-await $`git push`
+await $`git push origin HEAD:${targetBranch}`;
 
 // Read all files in the directory
 function getFilePaths(directoryPath) {
