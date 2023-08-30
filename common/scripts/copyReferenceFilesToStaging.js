81d7dddc860382e4d908db4a848e0e1f63b4232f
Ben Polinsky <78756012+ben-polinsky@users.noreply.github.com>
2023-08-30 17:20:37 -0400

    docs - upgrade bemetalsmith to 5.1.x (#5913)
    Co-authored-by: Ben Polinsky <ben-polinsky@users.noreply.github.com>
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>
    (cherry picked from commit 129700dba82e6915ddaf7d167021abcc80756740)

--- common/scripts/copyReferenceFilesToStaging.js
+++ common/scripts/copyReferenceFilesToStaging.js
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 
 /*
 1. Creates a staging-directory for the docs build
@@ -10,59 +10,47 @@
 4. Provides the ability to modify the default path (itwinjs-core/staging-docs) through CLI arguments
 Usage: node copyReferenceFilesToStaging.js staging-docs-directory.
 */
-const path = require('path');
-const process = require('process');
+const fs = require("fs/promises");
+const path = require("path");
+const process = require("process");
 const childProcess = require("child_process");
 const options = {
-  encoding: "utf8"
+  encoding: "utf8",
 };
 
-let fse;
-try {
-  fse = require("fs-extra");
-} catch (err) {
-  console.log("Please check that fs-extra is installed");
-  return console.error(err);
-}
+async function copyFilesToStaging() {
+  let basePath = childProcess.execSync(
+    "git rev-parse --show-toplevel",
+    options
+  );
 
-let basePath;
-try {
-  basePath = childProcess.execSync("git rev-parse --show-toplevel", options);
   basePath = basePath.split("\n").join("");
-} catch (err) {
-  console.log("Could not get the root directory");
-  return console.error(err);
-}
 
-let dest;
-if (process.argv[2]) {
-  dest = path.resolve(basePath, process.argv[2]);
-} else {
-  dest = path.resolve(basePath, "staging-docs");
-}
+  const dest = path.resolve(basePath, process.argv[2] ?? "staging-docs");
+  await fs.mkdir(path.resolve(dest, "extract"), { recursive: true }); // avoid rejection on existing dir
 
-try {
-  fse.ensureDirSync(path.resolve(dest, "extract"));
-} catch (err) {
-  console.log("Could not create staging directory structure");
-  console.error(err);
-}
-
-//copy docs
-try {
   const referencePath = path.resolve(dest, "reference");
   const extractPath = path.resolve(dest, "extract");
   const folderList = ["core", "domains", "editor", "presentation", "ui"];
 
-  fse.copySync(path.resolve(basePath, "docs"), dest);
-  fse.copySync(path.resolve(basePath, "generated-docs", "extract"), extractPath);
+  await fs.cp(path.resolve(basePath, "docs"), dest, { recursive: true });
+  await fs.cp(
+    path.resolve(basePath, "generated-docs", "extract"),
+    extractPath,
+    { recursive: true }
+  );
 
-  folderList.forEach(folder => {
-    fse.copySync(path.resolve(basePath, "generated-docs", folder), referencePath);
-  });
+  for (const folder of folderList) {
+    await fs.cp(
+      path.resolve(basePath, "generated-docs", folder),
+      referencePath,
+      { recursive: true }
+    );
+  }
 
   console.log("Copying finished successfully");
-} catch (err) {
-  console.log("Error while copying reference files to staging");
-  console.error(err);
 }
+
+copyFilesToStaging().then(() => {
+  console.log("Copy finished successfully");
+});
