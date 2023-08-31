701bc95cd6f87799f6d0f0eb7f1243e88e72c414
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-08-31 11:20:24 -0400

    use replace to remove newline

--- .github/workflows/automation-scripts/update-changelogs.mjs
+++ .github/workflows/automation-scripts/update-changelogs.mjs
@@ -28,9 +28,9 @@ let currentBranch = await $`git branch --show-current`;
 let commitMessage = await $`git log --format=%B -n 1`;
 
 // remove extra null and new line characters from git cmds
-targetBranch = String(targetBranch).slice(0, -1);
-currentBranch = String(currentBranch).slice(0, -1);
-commitMessage = String(commitMessage).slice(0, -2);
+targetBranch = String(targetBranch).replace('\n', '');
+currentBranch = String(currentBranch).replace('\n', '');
+commitMessage = String(commitMessage).replace('\n', '');
 
 if (targetBranch === `origin/${currentBranch}`) {
   console.log("The current branch is the latest release, so the target will be master branch")
@@ -69,7 +69,7 @@ await $`git commit -m "${commitMessage} Changelogs"`;
 await $`rush change --bulk --message "" --bump-type none`;
 await $`git add .`;
 await $`git commit --amend --no-edit`;
-await $`git push HEAD:${targetBranch}`;
+// await $`git push origin HEAD:${targetBranch}`;
 
 // Read all files in the directory
 function getFilePaths(directoryPath) {
