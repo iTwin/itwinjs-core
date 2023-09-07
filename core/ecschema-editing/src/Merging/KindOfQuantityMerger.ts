f9cb49af23ade6de997ed0cf13e454ba713c4d88
Julio Alva <julioalva@users.noreply.github.com>
2023-09-07 08:43:09 -0400

    clean up

--- core/ecschema-editing/src/Merging/KindOfQuantityMerger.ts
+++ core/ecschema-editing/src/Merging/KindOfQuantityMerger.ts
@@ -1,21 +1,14 @@
 
 import { KindOfQuantity } from "@itwin/ecschema-metadata";
-import { ChangeType, KindOfQuantityChanges } from "../ecschema-editing";
+import { KindOfQuantityChanges } from "../ecschema-editing";
 import { MutableKindOfQuantity } from "../Editing/Mutable/MutableKindOfQuantity";
 import { mergeSchemaItemProperties } from "./SchemaItemMerger";
 
 export default async function mergeKindOfQuantity(target: KindOfQuantity, _source: KindOfQuantity, changes: KindOfQuantityChanges) {
   const targetMutableKindOfQuantity = target as MutableKindOfQuantity;
-  console.log(changes);
 
-  // If item missing, copy over to target schema
-  if(changes.schemaItemMissing?.changeType === ChangeType.Missing){
-    
-  }
-
-  await mergeSchemaItemProperties(targetMutableKindOfQuantity, changes.propertyValueChanges, (item, propertyName, propertyValue) => {
+  await mergeSchemaItemProperties(targetMutableKindOfQuantity, changes.propertyValueChanges, (_item, propertyName, _propertyValue) => {
     switch (propertyName) {
-      case "displayLabel": return item.setDisplayLabel(propertyValue);
     }
   });
 }
