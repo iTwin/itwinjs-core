aa761d55c435af3014b4266d7592821feaf32667
dassaf4 <68340676+dassaf4@users.noreply.github.com>
2023-09-08 11:14:08 -0500

    undo breaking change

--- core/geometry/src/bspline/BSplineSurface.ts
+++ core/geometry/src/bspline/BSplineSurface.ts
@@ -1040,6 +1040,7 @@ export class BSplineSurface3dH extends BSpline2dNd implements BSplineSurface3dQu
     const result = {
       points: Point3dArray.unpackNumbersToNestedArraysIJK(this.coffs, 4, this.numPolesUV(0)),
       numCartesianDimensions: 3,
+      weighStyle: WeightStyle.WeightsAlreadyAppliedToCoordinates, // @deprecated in 4.x. Use weightStyle instead.
       weightStyle: WeightStyle.WeightsAlreadyAppliedToCoordinates,
     };
     return result;
