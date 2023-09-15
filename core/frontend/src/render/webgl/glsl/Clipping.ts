558f13074a0d6c67f8848f6e4f83ddd64ea8c9ce
AJMigliore <AJMigliore@users.noreply.github.com>
2023-09-15 13:47:47 -0600

    Adding math to find nearest point on clip plane, and to convert from eye space to window coords

--- core/frontend/src/render/webgl/glsl/Clipping.ts
+++ core/frontend/src/render/webgl/glsl/Clipping.ts
@@ -10,7 +10,8 @@ import { assert } from "@itwin/core-bentley";
 import { TextureUnit } from "../RenderFlags";
 import { FragmentShaderComponent, ProgramBuilder, VariablePrecision, VariableType } from "../ShaderBuilder";
 import { addEyeSpace } from "./Common";
-import { addModelViewMatrix } from "./Vertex";
+import { addModelViewMatrix, addProjectionMatrix } from "./Vertex";
+import { addViewport } from "./Viewport";
 
 const getClipPlaneFloat = `
 vec4 getClipPlane(int index) {
@@ -66,6 +67,43 @@ const applyClipPlanesPostlude = `
     } else if (!clippedByCurrentPlaneSet && calcClipPlaneDist(v_eyeSpace, plane) < 0.0) {
       clippedByCurrentPlaneSet = true;
     }
+
+    //avoiding near and far clip planes
+    if (i <= u_clipParams[1] - 2) {
+
+      /* 
+      * The closest point on a plane from a point, p,  will be: p minus the distance between p and the plane, in the direction of the plane's normal vector.
+      * We have normal as plane.xyz, and our point as v_eyeSpace.
+      * We can find the distance from the plane using calcClipPlaneDist, then multiply that by the plane's normal.
+      * Subtract the result from the original point to obtain the location of the closest point on the clip plane to v_eyeSpace.
+      * Finally, convert the point on the plane to window coords, and take the distance between that and gl_FragCoord,
+      * to determine whether or not to highlight the current fragment. 
+      */
+
+      float d = calcClipPlaneDist(v_eyeSpace, plane);
+      vec4 pointOnPlane = vec4((v_eyeSpace - (d * plane.xyz)), 1.0);
+
+      //Converting original point to window coords
+      //Only doing this now to ensure the conversion is correct by later comparing with gl_FragCoord
+      //In final implementation, pointOnPlane will be converted, not v_eyeSpace
+
+      vec4 convertedEyeSpace = vec4(v_eyeSpace, 1.0);
+      convertedEyeSpace = convertedEyeSpace * u_proj;
+      convertedEyeSpace.xyz /= convertedEyeSpace.w;                               //Should now be in NDC
+
+      //This will convert from NDC to window coords
+      //commented out for now to check previous conversion
+      //convertedEyeSpace.x = ((convertedEyeSpace.x + 1.0) * 0.5 * u_viewport.x);
+      //convertedEyeSpace.y = ((convertedEyeSpace.y + 1.0) * 0.5 * u_viewport.y);   //Should now be in window coords
+
+      //This check will show if it is correctly in NDC
+      //since NDC is in range -1 to 1, we should expect every point to be red
+      // while being darkest at the center, getting brighter as absolute value of x increases
+      if ((abs(convertedEyeSpace.x) <= 1.0) && (abs(convertedEyeSpace.y) <= 1.0) && (!clippedByCurrentPlaneSet)) {
+        g_clipColor = vec3(abs(convertedEyeSpace.x), 0.0, 0.0);
+        return true;
+      }
+    }
   }
 
   numSetsClippedBy += int(clippedByCurrentPlaneSet);
@@ -94,6 +132,17 @@ export function addClipping(prog: ProgramBuilder) {
   const vert = prog.vert;
 
   addEyeSpace(prog);
+  //addProjectionMatrix(vert); 
+
+  //getting access to u_proj, I could be doing this incorrectly
+  frag.addUniform("u_proj", VariableType.Mat4, (prog) => {
+    prog.addProgramUniform("u_proj", (uniform, params) => {
+      params.bindProjectionMatrix(uniform);
+    });
+  });
+
+  addModelViewMatrix(vert);
+  //addViewport(frag);
 
   // [0] = index of first plane
   // [1] = index of last plane (one past the end)
@@ -124,8 +173,6 @@ export function addClipping(prog: ProgramBuilder) {
     });
   });
 
-  addModelViewMatrix(vert);
-
   frag.addFunction(getClipPlaneFloat);
 
   frag.addFunction(calcClipPlaneDist);
@@ -140,3 +187,5 @@ export function addClipping(prog: ProgramBuilder) {
 
   frag.set(FragmentShaderComponent.ApplyClipping, applyClipPlanes);
 }
+
+
