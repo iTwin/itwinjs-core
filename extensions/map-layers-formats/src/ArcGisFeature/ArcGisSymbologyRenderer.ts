f0b5766424be2d1011ff7cb0b26a802f02ef4bb8
Michel D'Astous <mdastous-bentley@users.noreply.github.com>
2023-09-20 16:45:09 -0400

    edit to trigger builds

--- extensions/map-layers-formats/src/ArcGisFeature/ArcGisSymbologyRenderer.ts
+++ extensions/map-layers-formats/src/ArcGisFeature/ArcGisSymbologyRenderer.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
+* See LICENSE.md in the project root for license terms and full copyright notice. 
 *--------------------------------------------------------------------------------------------*/
 
 import { Logger } from "@itwin/core-bentley";
