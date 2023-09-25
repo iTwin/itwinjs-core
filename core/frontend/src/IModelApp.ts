04b4ee51e0b28f5b91971574adb0a5337f3340e2
Arun George <aruniverse@users.noreply.github.com>
2023-09-25 14:31:45 -0400

    explicitly mark as string

--- core/frontend/src/IModelApp.ts
+++ core/frontend/src/IModelApp.ts
@@ -11,7 +11,7 @@
 import { version } from "../../package.json";
 /** @public */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export const ITWINJS_CORE_VERSION = version;
+export const ITWINJS_CORE_VERSION = version as string;
 const COPYRIGHT_NOTICE = 'Copyright © 2017-2023 <a href="https://www.bentley.com" target="_blank" rel="noopener noreferrer">Bentley Systems, Inc.</a>';
 
 import { UiAdmin } from "@itwin/appui-abstract";
