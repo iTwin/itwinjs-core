58109255fcef2a68a3c89572dc3efd809707b0f2
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2023-10-27 15:55:19 -0400

    fix typo in getBestBWContrastColor

--- ui/core-react/src/core-react/utils/getBestBWContrastColor.ts
+++ ui/core-react/src/core-react/utils/getBestBWContrastColor.ts
@@ -12,7 +12,7 @@
  * @internal
  */
 export function getBestBWContrastColor(hexColor: string) {
-  const r = parseInt(hexColor.substring(1, 2), 16);
+  const r = parseInt(hexColor.substring(1, 3), 16);
   const g = parseInt(hexColor.substring(3, 5), 16);
   const b = parseInt(hexColor.substring(5, 7), 16);
   const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
