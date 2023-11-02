0643097ad25b3429e372e103115985f017041dc3
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2023-11-02 10:32:51 -0700

    Run iOS backend unit tests on simulator

--- tools/internal/ios/core-test-runner/core-test-runner/ViewController.swift
+++ tools/internal/ios/core-test-runner/core-test-runner/ViewController.swift
@@ -8,11 +8,21 @@ import os
 
 import IModelJsNative
 
+/// Convenience type alias for a dictionary intended for interop via JSON.
+typealias JSON = [String: Any]
+
 class ViewController: ObservableObject {
     @Published var testStatus = "Tests not started."
     @Published var numFailed = -1
+    private var configData: JSON = [:]
+    private var exitAfterCompletion = false
 
-    func copyAssetsToTemp() throws {
+    init() {
+        parseArguments()
+        exitAfterCompletion = configData["IMJS_EXIT_AFTER_COMPLETION"] as? String ?? "" == "1"
+    }
+
+    private func copyAssetsToTemp() throws {
         let srcPath = Bundle.main.bundlePath.appending("/Assets/assets")
         let dstPath = NSString(string: NSTemporaryDirectory()).appendingPathComponent("assets")
         let fm = FileManager.default
@@ -22,7 +32,7 @@ class ViewController: ObservableObject {
         try fm.copyItem(atPath: srcPath, toPath: dstPath)
     }
 
-    func trimResults(_ documentsDir: String, _ maxResults: Int = 10) {
+    private func trimResults(_ documentsDir: String, _ maxResults: Int = 10) {
         let fm = FileManager.default
         do {
             let files = try fm.contentsOfDirectory(atPath: documentsDir).filter { $0.hasPrefix("mocha_test_results-") }
@@ -36,19 +46,39 @@ class ViewController: ObservableObject {
         } catch {}
     }
 
+    private func parseArguments() {
+        // args can come from Xcode or when running the simulator (xcrun simctl launch), useful for automation
+        ProcessInfo.processInfo.arguments[1...].forEach { arg in
+            if arg.hasPrefix("IMJS_") {
+                let split = arg.split(separator: "=", maxSplits: 1)
+                if split.count == 2 {
+                    configData[String(split[0])] = String(split[1])
+                }
+            }
+        }
+    }
+
+    private func log(_ message: String) {
+        if exitAfterCompletion {
+            print(message)
+        } else {
+            NSLog(message)
+        }
+    }
+
     func runTests() {
         testStatus = "Starting tests..."
         do {
             try copyAssetsToTemp()
         } catch {
-            NSLog("Error copying assets to tmp: \(error)")
+            log("Error copying assets to tmp: \(error)")
             self.testStatus = "Tests initialization error."
             return
         }
         // Environment variable is read in configureMocha.js and passed to BentleyMochaReporter.
         let documentsDirs = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         guard documentsDirs.count >= 1 else {
-            NSLog("Error determining documents dir")
+            log("Error determining documents dir")
             self.testStatus = "Tests initialization error."
             return
         }
@@ -63,21 +93,30 @@ class ViewController: ObservableObject {
         let bundlePath = Bundle.main.bundlePath
         let mainPath = bundlePath.appending("/Assets/main.js")
         let main = URL(fileURLWithPath: mainPath)
-        NSLog("(ios): Running tests.")
+        log("(ios): Running tests.")
         host.loadBackend(main, withAuthClient: nil, withInspect: true) { [self] (numFailed: UInt32) in
             do {
                 let text = try String(contentsOf: testResultsUrl, encoding: .utf8)
                 for line in text.components(separatedBy: .newlines) {
-                    NSLog("[Mocha_Result_XML]: \(line)")
+                    log("[Mocha_Result_XML]: \(line)")
                 }
             } catch {
-                NSLog("(ios): Failed to read mocha test results.")
+                log("(ios): Failed to read mocha test results.")
             }
 
             // Indicate via UI that the tests have finished.
             self.testStatus = "Tests finished."
             self.numFailed = Int(numFailed)
-            NSLog("(ios): Tests finished. \(numFailed) tests failed.")
+            log("(ios): Tests finished. \(numFailed) tests failed.")
+            if exitAfterCompletion {
+                exit(EXIT_SUCCESS)
+            }
+        }
+        if exitAfterCompletion {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 60 * 20) {
+                self.log("(ios): Test timed out!")
+                exit(EXIT_FAILURE)
+            }
         }
     }
 }
