12a32123f5a3fa7cdcd38a4a2603292e559dfc81
DanRod1999 <DanRod1999@users.noreply.github.com>
2023-11-29 14:48:40 -0500

    import zx/globals, fix comment blocks, use fs-extra

--- .github/workflows/automation-scripts/update-changelogs.mjs
+++ .github/workflows/automation-scripts/update-changelogs.mjs
@@ -2,8 +2,8 @@
 
 "use strict";
 
-const fs = require('fs');
-const path = require('path');
+import 'zx/globals';
+// const fs = require('fs-extra');
 
 /****************************************************************
 * To run manually:
@@ -14,6 +14,46 @@ const path = require('path');
 * run this file using `zx .github/workflows/automation-scripts/update-changelogs.mjs`
 * open PR into target branch
 *****************************************************************/
+
+// function loadJsonFiles(filePath) {
+//   // Load each JSON file
+//   const data = fs.readFileSync(filePath);
+//   const jsonData = JSON.parse(data);
+//   return jsonData;
+// }
+
+function sortByVersion(entries) {
+  return entries.sort((a, b) => {
+    const versionA = a.version.split('.').map(Number);
+    const versionB = b.version.split('.').map(Number);
+
+    for (let i = 0; i < 3; i++) {
+      if (versionA[i] < versionB[i]) return 1;
+      if (versionA[i] > versionB[i]) return -1;
+    }
+
+    return 0;
+  });
+}
+
+function fixChangeLogs(files) {
+  const numFiles = files.length;
+  for (let i = 0; i < numFiles; i++) {
+    const currentJson = fs.readJsonSync(`temp-target-changelogs/${files[i]}`);
+    const incomingJson = fs.readJsonSync(`temp-incoming-changelogs/${files[i]}`);
+    // .map creates an array of [version num, entry obj], which is passed into Map and creates a key value pair of the two elements.
+    // Map objects do not allow duplicate keys, so this will remove duplicate versions
+    let completeEntries = new Map([...currentJson.entries, ...incomingJson.entries].map((obj) => [obj['version'], obj]));
+    // convert entries back into an array and sort by version number
+    completeEntries = sortByVersion(Array.from(completeEntries.values()));
+    currentJson.entries = completeEntries;
+
+    let jsonString = JSON.stringify(currentJson, null, 2);
+    jsonString = jsonString + '\n';
+    // clean up fs
+    fs.writeJsonSync(`temp-target-changelogs/${files[i]}`, jsonString);
+  }
+}
 
 const targetPath = "./temp-target-changelogs"
 const incomingPath = "./temp-incoming-changelogs"
@@ -32,8 +72,9 @@ targetBranch = String(targetBranch).replace(/\n/g, '');
 currentBranch = String(currentBranch).replace(/\n/g, '');
 commitMessage = String(commitMessage).replace(/\n/g, '');
 const substring = " Changelogs";
-if (commitMessage.includes(substring))
+if (commitMessage.includes(substring)) {
   commitMessage = commitMessage.replace(substring, '');
+}
 
 console.log(`target branch: ${targetBranch}`);
 console.log(`current branch: ${currentBranch}`);
@@ -74,12 +115,12 @@ await $`rm -r ${targetPath}`;
 await $`rm -r ${incomingPath}`;
 // # regen CHANGELOG.md
 await $`rush publish --regenerate-changelogs`;
-/*********************************************************************/
-// Uncomment For Manual runs and fix branch name to appropriate version
-// the version should match your incoming branch
+/*********************************************************************
+* Uncomment For Manual runs and fix branch name to appropriate version
+* the version should match your incoming branch
+*********************************************************************/
 // await $`git checkout -b finalize-release-X.X.X`;
 // targetBranch = "finalize-release-X.X.X"
-/*********************************************************************/
 await $`git add .`;
 await $`git commit -m "${commitMessage} Changelogs"`;
 await $`rush change --bulk --message "" --bump-type none`;
@@ -87,41 +128,3 @@ await $`git add .`;
 await $`git commit --amend --no-edit`;
 await $`git push origin HEAD:${targetBranch}`;
 
-function loadJsonFiles(filePath) {
-  // Load each JSON file
-  const data = fs.readFileSync(filePath);
-  const jsonData = JSON.parse(data);
-  return jsonData;
-}
-
-function sortByVersion(entries) {
-  return entries.sort((a, b) => {
-    const versionA = a.version.split('.').map(Number);
-    const versionB = b.version.split('.').map(Number);
-
-    for (let i = 0; i < 3; i++) {
-      if (versionA[i] < versionB[i]) return 1;
-      if (versionA[i] > versionB[i]) return -1;
-    }
-
-    return 0;
-  });
-}
-
-function fixChangeLogs(files) {
-  const numFiles = files.length;
-  for (let i = 0; i < numFiles; i++) {
-    const currentJson = loadJsonFiles(`temp-target-changelogs/${files[i]}`);
-    const incomingJson = loadJsonFiles(`temp-incoming-changelogs/${files[i]}`);
-    let completeEntries = new Map([...currentJson.entries, ...incomingJson.entries].map((obj) => [obj['version'], obj]));
-    completeEntries = sortByVersion(Array.from(completeEntries.values()));
-    currentJson.entries = completeEntries;
-
-    let jsonString = JSON.stringify(currentJson, null, 2);
-    jsonString = jsonString + '\n';
-    fs.writeFileSync(`temp-target-changelogs/${files[i]}`, jsonString, (err) => {
-      if (err)
-        console.error("Error Writing JSON file");
-    });
-  }
-}
