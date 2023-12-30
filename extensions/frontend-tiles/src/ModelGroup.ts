a956d1f3a46eb2d5881d6c96f31ca98eb16671b6
pmconne <22944042+pmconne@users.noreply.github.com>
2023-12-29 19:50:50 -0500

    Remove dependency on SpatialViewState

--- extensions/frontend-tiles/src/ModelGroup.ts
+++ extensions/frontend-tiles/src/ModelGroup.ts
@@ -5,7 +5,7 @@
 
 import { assert, Id64Set, Id64String } from "@itwin/core-bentley";
 import { PlanProjectionSettings } from "@itwin/core-common";
-import { ModelDisplayTransform, RenderClipVolume, SpatialViewState } from "@itwin/core-frontend";
+import { ModelDisplayTransform, ModelDisplayTransformProvider, RenderClipVolume, } from "@itwin/core-frontend";
 
 export interface ModelGroupInfo {
   displayTransform?: ModelDisplayTransform;
@@ -18,10 +18,20 @@ export interface ModelGroup extends ModelGroupInfo {
   animationTransformNodeIds?: ReadonlySet<number>;
 }
 
-function createModelGroupInfo(view: SpatialViewState, modelId: Id64String): ModelGroupInfo {
-  const displayTransform = view.modelDisplayTransformProvider?.getModelDisplayTransform(modelId);
-  const clip = view.getModelClip(modelId);
-  const planProjectionSettings = view.displayStyle.settings.getPlanProjectionSettings(modelId);
+export interface ModelGroupingContext {
+  modelDisplayTransformProvider?: ModelDisplayTransformProvider;
+  getModelClip(modelId: Id64String): RenderClipVolume | undefined;
+  displayStyle: {
+    settings: {
+      getPlanProjectionSettings(modelId: Id64String): PlanProjectionSettings | undefined;
+    };
+  };
+}
+
+function createModelGroupInfo(context: ModelGroupingContext, modelId: Id64String): ModelGroupInfo {
+  const displayTransform = context.modelDisplayTransformProvider?.getModelDisplayTransform(modelId);
+  const clip = context.getModelClip(modelId);
+  const planProjectionSettings = context.displayStyle.settings.getPlanProjectionSettings(modelId);
   return { displayTransform, clip, planProjectionSettings };
 }
 
@@ -52,10 +62,10 @@ function equalModelGroupInfo(a: ModelGroupInfo, b: ModelGroupInfo): boolean {
   return true;
 }
 
-export function groupModels(view: SpatialViewState, modelIds: Id64Set): ModelGroup[] {
+export function groupModels(context: ModelGroupingContext, modelIds: Id64Set): ModelGroup[] {
   const groups: ModelGroup[] = [];
   for (const modelId of modelIds) {
-    const info = createModelGroupInfo(view, modelId);
+    const info = createModelGroupInfo(context, modelId);
     let group = groups.find((x) => equalModelGroupInfo(x, info));
     if (!group)
       groups.push(group = { ...info, modelIds: new Set<string>() });
