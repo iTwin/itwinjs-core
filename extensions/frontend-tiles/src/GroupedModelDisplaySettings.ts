a070071f4309a23681a7c9b5f7b596cd276b7229
pmconne <22944042+pmconne@users.noreply.github.com>
2024-01-05 08:51:08 -0500

    Make sure settings up to date even if groupings don't change, and add docs.

--- extensions/frontend-tiles/src/GroupedModelDisplaySettings.ts
+++ extensions/frontend-tiles/src/GroupedModelDisplaySettings.ts
@@ -3,27 +3,82 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { ModelGroupDisplayTransforms } from "./ModelGroupDisplayTransforms";
-import { ModelGroupingContext } from "./ModelGroup";
+import { groupModels, ModelGroup, ModelGroupingContext } from "./ModelGroup";
 import { ModelDisplayTransformProvider, RenderClipVolume } from "@itwin/core-frontend";
-import { Id64Set, Id64String } from "@itwin/core-bentley";
+import { CompressedId64Set, Id64Set, Id64String } from "@itwin/core-bentley";
 import { PlanProjectionSettings } from "@itwin/core-common";
 
+/** Arguments supplied to the constructor of [[GroupedModelDisplaySettings]].
+ * @internal
+ */
 export interface GroupedModelDisplaySettingsArgs {
+  /** The set of all models to be grouped. Its contents should not change during the lifetime of the GroupedModelDisplaySettings object. */
   includedModelIds: Id64Set;
   modelDisplayTransformProvider?: ModelDisplayTransformProvider;
   getModelClip(modelId: Id64String): RenderClipVolume | undefined;
   getPlanProjectionSettings(modelId: Id64String): PlanProjectionSettings | undefined;
 }
+
+/** Groups models that have equivalent display settings.
+ * @internal
+ */
 export class GroupedModelDisplaySettings implements ModelGroupingContext {
-  private readonly _modelIds: Id64Set;
+  private _guid: string = "";
+  private _transformsValid = false;
+  private _groupsValid = false;
+  private readonly _args: GroupedModelDisplaySettingsArgs;
+  private _groups: ModelGroup[] = [];
   public modelGroupDisplayTransforms: ModelGroupDisplayTransforms;
-  public getModelClip: (modelId: Id64String) => RenderClipVolume | undefined;
-  public getPlanProjectionSettings: (modelId: Id64String) => PlanProjectionSettings | undefined;
 
   public constructor(args: GroupedModelDisplaySettingsArgs) {
-    this._modelIds = args.includedModelIds;
-    this.modelGroupDisplayTransforms = new ModelGroupDisplayTransforms(this._modelIds, args.modelDisplayTransformProvider);
-    this.getPlanProjectionSettings = (id) => args.getPlanProjectionSettings(id);
-    this.getModelClip = (id) => args.getModelClip(id);
+    this._args = args;
+    this.modelGroupDisplayTransforms = new ModelGroupDisplayTransforms(args.includedModelIds, args.modelDisplayTransformProvider);
+
+    this.update();
+  }
+
+  public getModelClip(modelId: Id64String): RenderClipVolume | undefined {
+    return this._args.getModelClip(modelId);
+  }
+
+  public getPlanProjectionSettings(modelId: Id64String): PlanProjectionSettings | undefined {
+    return this._args.getPlanProjectionSettings(modelId);
+  }
+
+  /** A string uniquely identifying the current model groupings. This changes when (and only when) the groupings change. */
+  public get guid(): string { return this._guid; }
+
+  /** The current groups with their display settings. */
+  public get groups(): ReadonlyArray<ModelGroup> { return this._groups; }
+
+  /** Call this when the display transforms might have changed - e.g., if the ModelDisplayTransformProvider changed. */
+  public invalidateTransforms() { 
+    this._transformsValid = false;
+  }
+
+  /** Call this when the groupings might have changed - e.g., if the model clip groups or plan projection settings changed. */
+  public invalidateGroups() {
+    this._groupsValid = false;
+  }
+
+  /** Call this to ensure the groupings and their display settings are up to date - e.g., before drawing.
+   * Returns true if the groupings changed as a result.
+   */
+  public update(): boolean {
+    if (!this._transformsValid && this.modelGroupDisplayTransforms.update(this._args.modelDisplayTransformProvider))
+      this._groupsValid = false;
+
+    this._transformsValid = true;
+    if (this._groupsValid)
+      return false;
+
+    this._groupsValid = true;
+    this._groups = groupModels(this, this._args.includedModelIds);
+    const guid = this._groups.map((x) => CompressedId64Set.compressSet(x.modelIds)).join("_");
+    if (guid === this._guid)
+      return false;
+
+    this._guid = guid;
+    return true;
   }
 }
