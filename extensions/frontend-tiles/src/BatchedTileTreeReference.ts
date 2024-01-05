570e0aa5f2652ae834ffc60daf185e4e005906e4
pmconne <22944042+pmconne@users.noreply.github.com>
2024-01-05 09:59:15 -0500

    WIP ModelGroupTileTreeReference

--- extensions/frontend-tiles/src/BatchedTileTreeReference.ts
+++ extensions/frontend-tiles/src/BatchedTileTreeReference.ts
@@ -3,6 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+import { assert } from "@itwin/core-bentley";
 import { Range3d, Transform } from "@itwin/core-geometry";
 import {
   BatchType, FeatureAppearance, FeatureAppearanceProvider, FeatureAppearanceSource, GeometryClass, RenderSchedule,
@@ -11,6 +12,7 @@ import {
   AnimationNodeId, formatAnimationBranchId, SceneContext, TileDrawArgs, TileTree, TileTreeOwner, TileTreeReference,
 } from "@itwin/core-frontend";
 import { BatchedModels } from "./BatchedModels";
+import { ModelGroup, ModelGroupInfo } from "./ModelGroup";
 
 /** @internal */
 export abstract class BatchedTileTreeReference extends TileTreeReference {
@@ -109,5 +111,78 @@ export class AnimatedBatchedTileTreeReference extends BatchedTileTreeReference {
     const args = super.createDrawArgs(context);
     // ###TODO args.boundingRange = args.tree.getTransformNodeRange(this._animationTransformNodeId);
     return args;
+  }
+}
+
+export interface ModelGroupTileTreeReferenceArgs {
+  readonly models: BatchedModels;
+  readonly groups: ModelGroup[];
+  readonly treeOwner: TileTreeOwner;
+}
+export class ModelGroupTileTreeReference extends TileTreeReference implements FeatureAppearanceProvider {
+  private readonly _args: ModelGroupTileTreeReferenceArgs;
+  private readonly _groupIndex: number;
+
+  public constructor(args: ModelGroupTileTreeReferenceArgs, groupIndex: number) {
+    super();
+    this._args = args;
+    this._groupIndex = groupIndex;
+  }
+
+  private get _groupInfo(): ModelGroupInfo {
+    assert(this._groupIndex < this._args.groups.length);
+    return this._args.groups[this._groupIndex];
+  }
+
+  public override get treeOwner(): TileTreeOwner {
+    return this._args.treeOwner;
+  }
+
+  public override getAppearanceProvider(): FeatureAppearanceProvider {
+    return this;
+  }
+
+  public getFeatureAppearance(
+    source: FeatureAppearanceSource,
+    elemLo: number, elemHi: number,
+    subcatLo: number, subcatHi: number,
+    geomClass: GeometryClass,
+    modelLo: number, modelHi: number,
+    type: BatchType,
+    animationNodeId: number,
+  ): FeatureAppearance | undefined {
+    if (!this._args.models.isViewed(modelLo, modelHi))
+      return undefined;
+
+    return source.getAppearance(elemLo, elemHi, subcatLo, subcatHi, geomClass, modelLo, modelHi, type, animationNodeId);
+  }
+
+  public override unionFitRange(range: Range3d): void {
+    this._args.models.unionRange(range);
+  }
+
+  protected override computeTransform(tree: TileTree): Transform {
+    // ###TODO apply display/elevation transforms
+    // ###TODO apply schedule script transform
+    return super.computeTransform(tree);
+  }
+
+  public override getAnimationTransformNodeId() {
+    // ###TODO
+    return AnimationNodeId.Untransformed;
+  }
+
+  public override createDrawArgs(context: SceneContext): TileDrawArgs | undefined {
+    // ###TODO apply plan projection overlay settings
+    // ###TODO all this
+    // const animBranch = context.viewport.target.animationBranches?.branchStates.get(this._branchId);
+    // if (animBranch && animBranch.omit)
+    //   return undefined;
+
+    // const args = super.createDrawArgs(context);
+    // // ###TODO args.boundingRange = args.tree.getTransformNodeRange(this._animationTransformNodeId);
+    // return args;
+
+    return super.createDrawArgs(context);
   }
 }
