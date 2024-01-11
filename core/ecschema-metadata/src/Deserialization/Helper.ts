87c23387de9dba69998d20322623a3bec8b862c7
Julio Alva <141143406+julioalvahash@users.noreply.github.com>
2024-01-11 15:54:37 -0500

    added error handle in loadCustomAttribute

--- core/ecschema-metadata/src/Deserialization/Helper.ts
+++ core/ecschema-metadata/src/Deserialization/Helper.ts
@@ -1010,11 +1010,18 @@ export class SchemaReadHelper<T = unknown> {
   private async loadCustomAttributes(container: AnyCAContainer, caProviders: Iterable<CAProviderTuple>): Promise<void> {
     for (const providerTuple of caProviders) {
       // First tuple entry is the CA class name.
-      const caClass = await this.findSchemaItem(providerTuple[0]) as CustomAttributeClass;
+      const caClass = await this.findSchemaItem(providerTuple[0]);
+
+      // If custom attribute exist within context and is referenced, validate the reference is defined in the container's schema
+      if (caClass && caClass.key.schemaName !== container.schema.name &&
+        !container.schema.references.find((reference) => reference.name === caClass.key.schemaName)) {
+        throw new ECObjectsError(ECObjectsStatus.InvalidECJson, `Unable to load custom attribute ${caClass.fullName}, schema ${caClass.key.schemaName} reference not defined in ${container.schema.name}`);
+      }
 
       // Second tuple entry ia a function that provides the CA instance.
       const provider = providerTuple[1];
-      const customAttribute = provider(caClass);
+      const customAttribute = provider(caClass as CustomAttributeClass);
+
       (container as AnyMutableCAContainer).addCustomAttribute(customAttribute);
     }
   }
