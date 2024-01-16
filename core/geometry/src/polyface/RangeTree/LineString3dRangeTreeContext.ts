1cd9d99d94882dbe1b68ffbfc3a622b7db4c841d
dassaf4 <68340676+dassaf4@users.noreply.github.com>
2024-01-16 17:32:34 -0600

    comment

--- core/geometry/src/polyface/RangeTree/LineString3dRangeTreeContext.ts
+++ core/geometry/src/polyface/RangeTree/LineString3dRangeTreeContext.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 /*---------------------------------------------------------------------------------------------
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
