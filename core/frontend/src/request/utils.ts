ef2ed2de4f9d6dba11f3506b605c8ec0f225736c
Michel Dastous <mdastous-bentley@users.noreply.github.com>
2024-01-19 15:52:24 -0500

    fixed typo 'setBasicAuthorization' and improved doc.

--- core/frontend/src/request/utils.ts
+++ core/frontend/src/request/utils.ts
@@ -10,6 +10,7 @@ import { RequestBasicCredentials } from "./Request";
  * Check whether or not one of the requested authentication method is listed in the HTTP 'WWW-Authenticate' response header
  * @param headers Headers object
  * @param query List of authentication method to lookup
+ * @note For CORS requests, the 'Access-Control-Expose-Headers' header from the server must make the 'WWW-Authenticate' available to the browser, otherwise this won't work.
  * @internal
  */
 export function headersIncludeAuthMethod(headers: Headers, query: string[]): boolean {
@@ -45,7 +46,7 @@ export function setBasicAuthorization(headers: Headers, userOrCreds: string|Requ
     pwd  = userOrCreds.password;
   }
 
-  if (username !== undefined && pwd !== password)
-    headers.set("Authorization", `Basic ${Base64EncodedString.encode(`${username}:${password}`)}`);
+  if (username !== undefined && pwd !== undefined)
+    headers.set("Authorization", `Basic ${Base64EncodedString.encode(`${username}:${pwd}`)}`);
 }
 
