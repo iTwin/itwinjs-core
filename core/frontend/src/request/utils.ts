42bcfeea1d7d692bfb497b5196955176c110de78
Michel Dastous <mdastous-bentley@users.noreply.github.com>
2024-01-19 15:26:10 -0500

    wip

---
+++ core/frontend/src/request/utils.ts
@@ -0,0 +1,51 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { Base64EncodedString } from "@itwin/core-common";
+import { RequestBasicCredentials } from "./Request";
+
+/**
+ * Check whether or not one of the requested authentication method is listed in the HTTP 'WWW-Authenticate' response header
+ * @param headers Headers object
+ * @param query List of authentication method to lookup
+ * @internal
+ */
+export function headersIncludeAuthMethod(headers: Headers, query: string[]): boolean {
+  const headersArray = Array.from(headers);
+  const foundAuth = headersArray.filter((pair)=> pair[0].toLowerCase() === "www-authenticate");
+  if (foundAuth.length > 0) {
+    const authMethods = foundAuth.map((pair)=>pair[1].toLowerCase().split(",").map(((value)=>value.trim()))).flat();
+    for (const queryValue of query) {
+      if (authMethods.includes(queryValue))
+        return true;
+    }
+  }
+
+  return false;
+}
+/**
+ * Set the value of the 'Authorization' header with Basic authentication value
+ * scheme: 'Authorization: Basic <credentials>'
+ * @internal
+ */
+export function setBasicAuthorization(headers: Headers, credentials: RequestBasicCredentials): void;
+/** @internal */
+export function setBasicAuthorization(headers: Headers, user: string, password: string): void;
+/** @internal */
+export function setBasicAuthorization(headers: Headers, userOrCreds: string|RequestBasicCredentials, password?: string): void {
+  let username: string|undefined;
+  let pwd: string|undefined;
+  if (typeof userOrCreds === "string") {
+    username = userOrCreds;
+    pwd  = password;
+  } else {
+    username = userOrCreds.user;
+    pwd  = userOrCreds.password;
+  }
+
+  if (username !== undefined && pwd !== password)
+    headers.set("Authorization", `Basic ${Base64EncodedString.encode(`${username}:${password}`)}`);
+}
+
