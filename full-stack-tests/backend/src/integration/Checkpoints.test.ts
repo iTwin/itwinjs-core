abf825a6c444df7d47220447c48de52e3788bcff
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2024-02-05 12:25:56 -0500

    remove .only

--- full-stack-tests/backend/src/integration/Checkpoints.test.ts
+++ full-stack-tests/backend/src/integration/Checkpoints.test.ts
@@ -24,7 +24,7 @@ async function queryBisModelCount(imodel: IModelDb): Promise<number> {
   return -1;
 }
 
-describe.only("Checkpoints", () => {
+describe("Checkpoints", () => {
   let daemon: ChildProcess;
   let cacheProps: CloudSqlite.CacheProps;
   let daemonProps: NativeCloudSqlite.DaemonProps;
