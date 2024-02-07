47090d0c987de54d522727d66f6b6ee84f80a84d
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2024-02-07 09:42:31 -0500

    Update core/common/src/TerrainSettings.ts
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>

--- core/common/src/TerrainSettings.ts
+++ core/common/src/TerrainSettings.ts
@@ -105,7 +105,7 @@ export class TerrainSettings {
     return this._nonLocatable;
   }
 
-  /** @deprecated in 4.x. Use the overload that takes [[TerrainProps]]. */
+  /** @deprecated in 4.5.x. Use the overload that takes [[TerrainProps]]. */
   constructor(providerName?: string, exaggeration?: number, applyLighting?: boolean, heightOrigin?: number, heightOriginMode?: TerrainHeightOriginMode);
 
   constructor(props?: TerrainProps);
