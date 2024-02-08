5db645a2b0077f14d5d167b7bd4f89892eceff0d
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2024-02-08 12:55:02 -0500

    invalidate PRs workflow

---
+++ .github/workflows/automation-scripts/invalidate-status-checks.mjs
@@ -0,0 +1,31 @@
+import { Octokit, App } from "octokit"
+
+const octokit = new Octokit;
+
+let pull_requests = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+  owner: 'iTwin',
+  repo: 'itwinjs-core',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+
+for (let i = 0; i < pull_requests.data.length; i++) {
+  if (pull_requests.data[i].head.ref === 'dan/automate-addon') {
+    console.log(pull_requests.data[i].head.ref)
+    console.log(pull_requests.data[i].head.sha)
+    let pr_sha = pull_requests.data[i].head.sha;
+    await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
+      owner: 'iTwin',
+      repo: 'itwinjs-core',
+      sha: `${pr_sha}`,
+      state: 'success',
+      target_url: 'https://github.com/iTwin/itwinjs-core',
+      description: 'imodeljs-native version is out of date',
+      context: '@bentley/imodeljs-native version check',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+  }
+}
