482960df60f5ce15a2c913a1fdd5140d64ee7b21
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-02-12 13:57:33 +0200

    Add prettier to presentation-full-stack-tests

--- full-stack-tests/presentation/eslint.config.js
+++ full-stack-tests/presentation/eslint.config.js
@@ -1,8 +1,16 @@
 const iTwinPlugin = require("@itwin/eslint-plugin");
+const prettierConfig = require("eslint-config-prettier");
 
 module.exports = [
   {
     files: ["**/*.ts"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
-  }
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      ...prettierConfig.rules,
+      curly: ["error", "all"],
+    },
+  },
 ];
