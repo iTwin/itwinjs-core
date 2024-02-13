0bb787f4abb742700220f236b5e1bf59638f026f
pmconne <22944042+pmconne@users.noreply.github.com>
2024-02-13 08:54:19 -0500

    SceneRealityModel, WIP SpatialViewSceneObject

--- core/frontend/src/ViewportScene.ts
+++ core/frontend/src/ViewportScene.ts
@@ -6,21 +6,23 @@
  * @module Views
  */
 
-import { Id64String } from "@itwin/core-bentley";
+import { Id64String, assert } from "@itwin/core-bentley";
 import { Viewport, ViewportDecorator } from "./Viewport";
 import { HitDetail } from "./HitDetail";
 import { SceneContext } from "./ViewContext";
-import { AmbientOcclusion, Environment, SolarShadowSettings } from "@itwin/core-common";
+import { AmbientOcclusion, ContextRealityModelProps, Environment, FeatureAppearance, PlanarClipMaskSettings, RealityDataSourceKey, RealityModelDisplaySettings, SolarShadowSettings, SpatialClassifiers, ViewFlags } from "@itwin/core-common";
+import { IModelConnection } from "./IModelConnection";
 
 // Describes the common interface for all SceneObjects.
 // For documentation and type-checking purposes only - SceneObject is a union type.
 export interface ISceneObject extends ViewportDecorator {
   readonly scene: ViewportScene;
   readonly isLoadingComplete: boolean;
+  readonly isGlobal: boolean;
 
   getToolTip(hit: HitDetail): Promise<HTMLElement | string | undefined>;
 
-  addToScene(context: SceneContext): void;
+  draw(context: SceneContext): void;
 
   // need higher-level abstraction?
   // collectStatistics(stats: RenderMemory.Statistics): void;
@@ -28,6 +30,24 @@ export interface ISceneObject extends ViewportDecorator {
 
 export interface SceneRealityModel extends ISceneObject {
   readonly type: "realityModel";
+  readonly rdSourceKey: RealityDataSourceKey;
+  readonly name: string;
+  readonly description: string;
+  readonly realityDataId?: string;
+  // SpatialClassifiers is implemented on top of a JSON container, it's weird.
+  readonly classifiers?: SpatialClassifiers;
+  planarClipMaskSettings?: PlanarClipMaskSettings;
+  appearanceOverrides?: FeatureAppearance;
+  displaySettings: RealityModelDisplaySettings;
+  // Is this actually needed, or just confusing?
+  // readonly modelId: Id64String;
+}
+
+export namespace SceneRealityModel {
+  export function fromContextRealityModel(props: ContextRealityModelProps, scene: ViewportScene): SceneRealityModel {
+    assert(!props && !scene);
+    throw new Error("TODO");
+  }
 }
 
 export interface SceneRealityModels extends Iterable<SceneRealityModel> {
@@ -40,10 +60,12 @@ export interface SceneMap extends ISceneObject {
 
 export interface ScenePresentation extends ISceneObject {
   readonly type: "environment";
+  viewFlags: ViewFlags;
   environment: Environment;
   toggleSkyBox(display?: boolean): void;
   toggleAtmosphere(display?: boolean): void;
-  // Would anyone use this, and what would it do?
+  // We would need to know in what coordinate reference frame to draw the ground plane.
+  // Perhaps each iModel can individually toggle+draw its own ground plane.
   // toggleGroundPlane(display?: boolean): void;
 
   ambientOcclusion: AmbientOcclusion.Settings;
@@ -59,8 +81,19 @@ export interface CustomSceneObjects extends Iterable<CustomSceneObject> {
 
 }
 
+export interface IIModelSceneObject extends ISceneObject {
+  readonly type: "imodel";
+  readonly iModel: IModelConnection;
+}
+
+export interface SpatialViewSceneObject extends IIModelSceneObject {
+  readonly viewType: "spatial";
+}
+
+export type IModelSceneObject = SpatialViewSceneObject;
+
 // This union type is open for future expansion - the `type` field should be treated as non-exhaustive.
-export type SceneObject = SceneRealityModel | SceneMap | ScenePresentation | CustomSceneObject;
+export type SceneObject = SceneRealityModel | SceneMap | ScenePresentation | CustomSceneObject | IModelSceneObject;
 
 // Exists for documentation + type-checking only.
 export interface IViewportScene {
