b617d1ac8ace080af93a010b41c1e990f300bd80
pmconne <22944042+pmconne@users.noreply.github.com>
2024-02-13 13:41:39 -0500

    Replace prev impl

--- core/frontend/src/scene/ViewportScene.ts
+++ core/frontend/src/scene/ViewportScene.ts
@@ -6,12 +6,12 @@
  * @module Views
  */
 
-import { Id64String, assert } from "@itwin/core-bentley";
-import { Viewport, ViewportDecorator } from "../Viewport";
+import { ViewportDecorator } from "../Viewport";
 import { HitDetail } from "../HitDetail";
 import { SceneContext } from "../ViewContext";
-import { AmbientOcclusion, ColorDef, ContextRealityModelProps, Environment, FeatureAppearance, PlanarClipMaskSettings, RealityDataSourceKey, RealityModelDisplaySettings, SolarShadowSettings, SpatialClassifiers, ViewFlags } from "@itwin/core-common";
-import { SpatialView } from "./View";
+import { AmbientOcclusion, ColorDef, Environment, FeatureAppearance, PlanarClipMaskSettings, RealityDataSourceKey, RealityModelDisplaySettings, SolarShadowSettings, SpatialClassifiers, ViewFlags } from "@itwin/core-common";
+import { SpatialView, View, View2d } from "./View";
+import { TiledGraphicsProvider } from "../core-frontend";
 
 // Describes the common interface for all SceneObjects.
 // For documentation and type-checking purposes only - SceneObject is a union type.
@@ -28,7 +28,16 @@ export interface ISceneObject extends ViewportDecorator {
   // collectStatistics(stats: RenderMemory.Statistics): void;
 }
 
-export interface SceneRealityModel extends ISceneObject {
+export interface BaseSceneObject {
+  customType?: never;
+  view?: never;
+  presentation?: never;
+  realityModel?: never;
+  graphicsProvider?: never;
+  map?: never;
+}
+
+export interface RealityModel {
   readonly rdSourceKey: RealityDataSourceKey;
   readonly name: string;
   readonly description: string;
@@ -42,93 +51,84 @@ export interface SceneRealityModel extends ISceneObject {
   // readonly modelId: Id64String;
 }
 
-export namespace SceneRealityModel {
-  export function fromContextRealityModel(props: ContextRealityModelProps, scene: ViewportScene): SceneRealityModel {
-    assert(!props && !scene);
-    throw new Error("TODO");
-  }
+export interface RealityModelSceneObject extends ISceneObject, Omit<BaseSceneObject, "realityModel"> {
+  readonly scene: SpatialScene;
+  readonly realityModel: RealityModel;
 }
 
-export interface RealityModelSceneObject extends ISceneObject {
-  readonly type: "realityModel";
-  readonly model: SceneRealityModel;
-}
-export interface SceneRealityModels extends Iterable<RealityModelSceneObject> {
+export interface RealityModelSceneObjects extends Iterable<RealityModelSceneObject> {
   
 }
 
-// ###TODO
-export interface SceneMap extends ISceneObject {
-  readonly type: "map";
+export interface ViewSceneObject extends ISceneObject, Omit<BaseSceneObject, "view"> {
+  readonly view: View;
 }
 
-export interface IScenePresentation extends ISceneObject {
-  readonly type: "presentation";
-  readonly backgroundColor: ColorDef;
-  readonly viewFlags: ViewFlags;
+export interface ViewSceneObjects<T extends View> extends Iterable<T> {
+  
 }
 
-export interface ScenePresentation2d extends IScenePresentation {
-  readonly is3d: false;
+export interface ScenePresentation2d {
+  is2d: true;
+  is3d?: false;
+  viewFlags: ViewFlags;
+  backgroundColor: ColorDef;
 }
 
-export interface ScenePresentation3d extends IScenePresentation {
-  readonly is3d: true;
+export interface ScenePresentation3d {
+  is3d: true;
+  is2d?: false;
+  viewFlags: ViewFlags;
+  backgroundColor:  ColorDef;
   environment: Environment;
   toggleSkyBox(display?: boolean): void;
   toggleAtmosphere(display?: boolean): void;
-  // We would need to know in what coordinate reference frame to draw the ground plane.
-  // Perhaps each iModel can individually toggle+draw its own ground plane.
-  // toggleGroundPlane(display?: boolean): void;
-
   ambientOcclusion: AmbientOcclusion.Settings;
   solarShadows: SolarShadowSettings;
 }
 
-export type ScenePresentation = ScenePresentation2d | ScenePresentation3d;
+export interface PresentationSceneObject extends ISceneObject, Omit<BaseSceneObject, "presentation"> {
+  readonly presentation: ScenePresentation2d | ScenePresentation3d;
+}
 
-export interface CustomSceneObject extends ISceneObject {
-  readonly type: "custom";
-  readonly customType: string;
+export interface CustomSceneObject extends ISceneObject, Omit<BaseSceneObject, "customType"> {
+  customType?: string;
 }
 
 export interface CustomSceneObjects extends Iterable<CustomSceneObject> {
-
+  
 }
 
-export interface IViewSceneObject extends ISceneObject {
-  readonly type: "view";
+export interface MapSceneObject extends ISceneObject, Omit<BaseSceneObject, "map"> {
+  readonly map: any; // ###TODO
 }
 
-export interface SpatialViewSceneObject extends IViewSceneObject {
-  readonly viewType: "spatial";
-  readonly view: SpatialView;
+export interface TiledGraphicsSceneObject extends ISceneObject, Omit<BaseSceneObject, "graphicsProvider"> {
+  readonly graphicsProvider: TiledGraphicsProvider;
 }
 
-export type ViewSceneObject = SpatialViewSceneObject;
-
-// This union type is open for future expansion - the `type` field should be treated as non-exhaustive.
-export type SceneObject = RealityModelSceneObject| SceneMap | ScenePresentation | CustomSceneObject | ViewSceneObject;
-
-// Exists for documentation + type-checking only.
-export interface IViewportScene {
-  viewport: Viewport;
+export interface TiledGraphicsSceneObjects extends Iterable<TiledGraphicsSceneObject> {
+  
 }
 
-export interface SpatialViewportScene extends IViewportScene, Iterable<SceneObject>, ViewportDecorator {
-  readonly type: "spatial";
-  readonly realityModels: SceneRealityModels;
-  // Will we need/want individual SceneObjects for each map layer, and the globe, and whatever?
-  readonly maps: SceneMap;
+export type SceneObject = RealityModelSceneObject | ViewSceneObject | PresentationSceneObject | CustomSceneObject | MapSceneObject | TiledGraphicsSceneObject;
+
+export interface SpatialScene {
+  readonly isSpatial: true;
+  readonly realityModels: RealityModelSceneObjects;
+  readonly maps: MapSceneObject;
+  readonly views: ViewSceneObjects<SpatialView>;
+  readonly custom: CustomSceneObjects;
   readonly presentation: ScenePresentation3d;
-  readonly customObjects: CustomSceneObjects;
-
-  // Something for computing/providing a coordinate reference frame for nav cube and standard orientations.
+  readonly tiledGraphicsProviders: TiledGraphicsSceneObjects;
 }
 
-export type ViewportScene = SpatialViewportScene;
-
-export interface SceneEntity {
-  readonly id: Id64String;
-  readonly object: SceneObject;
+export interface TestScene2d {
+  isSpatial?: false;
+  readonly views: ViewSceneObjects<View2d>;
+  readonly presentation: ScenePresentation2d;
+  readonly tiledGraphicsProviders: TiledGraphicsSceneObjects;
+  readonly custom: CustomSceneObjects;
 }
+
+export type ViewportScene = SpatialScene | TestScene2d;
