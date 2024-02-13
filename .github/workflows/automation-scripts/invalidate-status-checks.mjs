18a110efbb80b2ad2133dbe85dc0d5724cd1f8a1
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2024-02-13 10:40:01 -0500

    collect target url

--- .github/workflows/automation-scripts/invalidate-status-checks.mjs
+++ .github/workflows/automation-scripts/invalidate-status-checks.mjs
@@ -20,13 +20,32 @@ for (let i = 0; i < pull_requests.data.length; i++) {
     console.log(pull_requests.data[i].head.ref)
     console.log(pull_requests.data[i].head.sha)
     let pr_sha = pull_requests.data[i].head.sha;
+
+    // Get statuses of current PR
+    let statuses = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/statuses', {
+      owner: 'iTwin',
+      repo: 'itwinjs-core',
+      ref: pull_requests.data[i].head.ref,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    let itwinjs_target_url = '';
+    for (let j = 0; j < statuses.length; j++) {
+      if (statuses[j].context === 'iTwin.js') {
+        itwinjs_target_url = statuses[j].target_url
+      }
+    }
+
     await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
       owner: 'iTwin',
       repo: 'itwinjs-core',
       sha: `${pr_sha}`,
       state: 'success',
+      target_url: itwinjs_target_url,
       description: 'imodeljs-native version is out of date',
-      context: '@bentley/imodeljs-native version check',
+      context: 'iTwin.js',
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
