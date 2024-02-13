04f27d10744149c70c10ce408934cee139900b5f
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2024-02-13 15:04:02 -0500

    invalidate all open PRs

--- .github/workflows/automation-scripts/invalidate-status-checks.mjs
+++ .github/workflows/automation-scripts/invalidate-status-checks.mjs
@@ -1,3 +1,7 @@
+// Invalidate all iTwin.js build status checks when master has a change to core/backend/package.json
+// This is specifically to catch changes to @bentley/imodeljs-native
+// This will also invalidate PRs if there's a new nightly build, however our 3 hour rule should also invalidate the same PRs
+
 import { Octokit, App } from "octokit"
 import dotenv from 'dotenv'
 
@@ -16,21 +20,18 @@ let pull_requests = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
 })
 
 for (let i = 0; i < pull_requests.data.length; i++) {
-  if (pull_requests.data[i].head.ref === 'dan/automate-addon') {
-    console.log(pull_requests.data[i].head.ref)
-    console.log(pull_requests.data[i].head.sha)
-    let pr_sha = pull_requests.data[i].head.sha;
+  console.log(pull_requests.data[i].head.ref)
+  let pr_sha = pull_requests.data[i].head.sha;
 
-    await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
-      owner: 'iTwin',
-      repo: 'itwinjs-core',
-      sha: `${pr_sha}`,
-      state: 'failure',
-      description: 'imodeljs-native version is out of date',
-      context: 'iTwin.js',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
-  }
+  await octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
+    owner: 'iTwin',
+    repo: 'itwinjs-core',
+    sha: `${pr_sha}`,
+    state: 'failure',
+    description: '@bentley/imodeljs-native may be out of date with master, please merge',
+    context: 'iTwin.js',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 }
