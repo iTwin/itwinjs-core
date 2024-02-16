ea0ae0495c33a13cab947b173af1b2b50ecef40b
pmconne <22944042+pmconne@users.noreply.github.com>
2024-02-16 09:46:48 -0500

    abstract classes => interfaces

--- core/frontend/src/scene/View.ts
+++ core/frontend/src/scene/View.ts
@@ -7,8 +7,8 @@
  */
 
 import { Id64, Id64Arg, Id64String } from "@itwin/core-bentley";
-import { View3dStyle, ViewStyle, ViewStyleFlags } from "./ViewStyle";
-import { FeatureAppearance, ModelClipGroups, SubCategoryOverride } from "@itwin/core-common";
+import { View2dStyle, View3dStyle, ViewStyle, ViewStyleFlags } from "./ViewStyle";
+import { AxisAlignedBox3d, FeatureAppearance, ModelClipGroups, SubCategoryOverride } from "@itwin/core-common";
 import { IModelConnection } from "../IModelConnection";
 import { GeometricModelState } from "../ModelState";
 import { Range3d, Transform } from "@itwin/core-geometry";
@@ -18,279 +18,129 @@ import { DecorateContext, SceneContext } from "../ViewContext";
 import { ComputeDisplayTransformArgs, ModelDisplayTransformProvider } from "../ViewState";
 import { Viewport } from "../Viewport";
 import { RenderClipVolume } from "../render/RenderClipVolume";
+import { ComputeSpatialViewFitRangeOptions } from "../SpatialViewState";
 
-export abstract class ViewCategorySelector {
- /** @internal */
- protected constructor() { }
- 
- abstract categories: Set<string>;
- abstract equalState(other: ViewCategorySelector): boolean;
+export interface ViewCategorySelector {
+  categories: Set<string>;
+  equalState(other: ViewCategorySelector): boolean;
 
- has(id: Id64String): boolean {
-  return this.categories.has(id);
- }
-
- isCategoryViewed(categoryId: Id64String): boolean {
-  return this.has(categoryId);
- }
-
- addCategories(arg: Id64Arg): void {
-  for (const id of Id64.iterable(arg))
-   this.categories.add(id);
- }
-
- dropCategories(arg: Id64Arg): void {
-  for (const id of Id64.iterable(arg))
-   this.categories.delete(id);
- }
-
- changeCategoryDisplay(arg: Id64Arg, add: boolean): void {
-  add ? this.addCategories(arg) : this.dropCategories(arg);
- }
+  addCategories(arg: Id64Arg): void;
+  dropCategories(arg: Id64Arg): void;
+  changeCategoryDisplay(arg: Id64Arg, add: boolean): void;
 }
 
-export abstract class View {
- /** @internal */
- protected constructor() { }
+export interface IView {
+  readonly iModel: IModelConnection;
 
- abstract get iModel(): IModelConnection;
+  categorySelector: ViewCategorySelector;
+  displayStyle: ViewStyle;
+  viewFlags: ViewStyleFlags;
 
- abstract categorySelector: ViewCategorySelector;
+ isSpatial(): this is SpatialView;
+ isDrawing(): this is DrawingView;
+ isSheet(): this is SheetView;
 
- abstract displayStyle: ViewStyle;
- 
- get viewFlags(): ViewStyleFlags {
-  return this.displayStyle.viewFlags;
- }
+  // ###TODO scheduleScript, scheduleScriptReference
+  // ###TODO analysisStyle
 
- set viewFlags(flags: ViewStyleFlags) {
-  this.displayStyle.viewFlags = flags;
- }
+  // ###TODO is this needed?
+  equals(other: this): boolean;
 
- // ###TODO scheduleScript, scheduleScriptReference
- // ###TODO analysisStyle
+  readonly areAllTileTreesLoaded: boolean;
 
- // ###TODO is this needed?
- abstract equals(other: this): boolean;
+  isSubCategoryVisible(id: Id64String): boolean;
+  enableAllLoadedSubCategories(categoryIds: Id64Arg): boolean;
+  setSubCategoryVisible(subCategoryId: Id64String, visible: boolean): boolean;
 
- abstract get areAllTileTreesLoaded(): boolean;
+  computeFitRange(): Range3d;
 
- getSubCategoryOverride(id: Id64String): SubCategoryOverride | undefined {
-  return this.displayStyle.getSubCategoryOverride(id);
- }
+  viewsModel(modelId: Id64String): boolean;
 
- isSubCategoryVisible(id: Id64String): boolean {
-  const app = this.iModel.subcategories.getSubCategoryAppearance(id);
-  if (!app)
-   return false;
+  forEachModel(func: (model: GeometricModelState) => void): void;
 
-  const ovr = this.getSubCategoryOverride(id);
-  if (undefined === ovr?.invisible)
-   return !app.invisible;
+  /** @internal */
+  forEachModelTreeRef(func: (treeRef: TileTreeReference) => void): void;
 
-  return !ovr.invisible;
- }
+  forEachTileTreeRef(func: (treeRef: TileTreeReference) => void): void;
 
- getModelAppearanceOverride(id: Id64String): Readonly<FeatureAppearance> | undefined {
-  return this.displayStyle.getModelAppearanceOverride(id);
- }
+  /** @internal */
+  discloseTileTrees(trees: DisclosedTileTreeSet): void;
 
- get details(): View {
-  return this;
- }
- 
- abstract is3d(): this is View3d;
- abstract isSpatialView(): this is SpatialView;
- abstract isDrawingView(): this is DrawingView;
- abstract isSheetView(): this is SheetView;
+  /** @internal */
+  collectStatistics(stats: RenderMemory.Statistics): void;
 
- is2d(): this is View2d {
-  return !this.is3d();
- }
+  /** @internal */
+  collectNonTileTreeStatistics(_stats: RenderMemory.Statistics): void;
 
- abstract computeFitRange(): Range3d;
+  /** @internal */
+  createScene(context: SceneContext): void;
 
- abstract viewsModel(modelId: Id64String): boolean;
+  /** @internal */
+  decorate(context: DecorateContext): void;
 
- abstract forEachModel(func: (model: GeometricModelState) => void): void;
+  // get/setViewClip in addition to scene clip?
 
- /** @internal */
- abstract forEachModelTreeRef(func: (treeRef: TileTreeReference) => void): void;
- 
- forEachTileTreeRef(func: (treeRef: TileTreeReference) => void): void {
-  this.forEachModelTreeRef(func);
-  this.displayStyle.forEachTileTreeRef(func);
- }
-
- /** @internal */
- discloseTileTrees(trees: DisclosedTileTreeSet): void {
-  this.forEachTileTreeRef((ref) => trees.disclose(ref));
- }
-
- /** @internal */
- public collectStatistics(stats: RenderMemory.Statistics): void {
-  const trees = new DisclosedTileTreeSet();
-   this.discloseTileTrees(trees);
-   for (const tree of trees)
-     tree.collectStatistics(stats);
-
-   this.collectNonTileTreeStatistics(stats);
- }
-
- /** @internal */
- collectNonTileTreeStatistics(_stats: RenderMemory.Statistics): void {
-  // derived classes will override if necessary.
- }
-
- /** @internal */
- createScene(context: SceneContext): void {
-  this.forEachTileTreeRef((ref) => ref.addToScene(context));
- }
-
- /** @internal */
- abstract decorate(context: DecorateContext): void;
-
- setDisplayStyle(style: ViewStyle): void {
-  this.displayStyle = style;
- }
-
- setCategorySelector(selector: ViewCategorySelector): void {
-  this.categorySelector = selector;
- }
-
- viewsCategory(id: Id64String): boolean {
-  return this.categorySelector.isCategoryViewed(id);
- }
-
- // get/setViewClip in addition to scene clip?
-
- /** @internal */
- refreshForModifiedModels(modelIds: Id64Arg | undefined): boolean {
-   let refreshed = false;
-   this.forEachModelTreeRef((ref) => {
-     const tree = ref.treeOwner.tileTree;
-     if (undefined !== tree && (undefined === modelIds || Id64.has(modelIds, tree.modelId))) {
-       ref.treeOwner.dispose();
-       refreshed = true;
-     }
-   });
-
-   return refreshed;
-  }
+  /** @internal */
+  refreshForModifiedModels(modelIds: Id64Arg | undefined): boolean;
 
   /** Determine whether this ViewState has the same coordinate system as another one.
    * They must be from the same iModel, and view a model in common.
    */
-  public hasSameCoordinates(other: View): boolean {
-    if (this.iModel !== other.iModel)
-      return false;
+  hasSameCoordinates(other: this): boolean;
 
-    // Spatial views view any number of spatial models all sharing one coordinate system.
-    if (this.isSpatialView() && other.isSpatialView())
-      return true;
+  /** @internal strictly for plan projection models. */
+  getModelElevation(modelId: Id64String): number;
 
-    // People sometimes mistakenly stick 2d models into spatial views' model selectors.
-    if (this.isSpatialView() != other.isSpatialView())
-      return false;
+  modelDisplayTransformProvider: ModelDisplayTransformProvider | undefined;
 
-    // Non-spatial views view exactly one model. If they view the same model, they share a coordinate system.
-    let allowView = false;
-    this.forEachModel((model) => {
-      allowView ||= other.viewsModel(model.id);
-    });
+  computeDisplayTransform(args: ComputeDisplayTransformArgs): Transform | undefined;
 
-    return allowView;
-  }
+  /** Returns an iterator over additional Viewports used to construct this view's scene. e.g., those used for ViewAttachments and section drawings.
+   * This exists chiefly for display-performance-test-app to determine when all tiles required for the view have been loaded.
+   * @internal
+   */
+  readonly secondaryViewports: Iterable<Viewport>;
 
- /** @internal strictly for plan projection models. */
- getModelElevation(_modelId: Id64String): number {
-  return 0;
- }
+  /** Find the viewport that renders the contents of the view attachment with the specified element Id into this view.
+   * @internal
+   */
+  getAttachmentViewport(_id: Id64String): Viewport | undefined;
 
- abstract modelDisplayTransformProvider: ModelDisplayTransformProvider | undefined;
-
- computeDisplayTransform(args: ComputeDisplayTransformArgs): Transform | undefined {
-   const elevation = this.getModelElevation(args.modelId);
-   const modelTransform = this.modelDisplayTransformProvider?.getModelDisplayTransform(args.modelId);
-
-   // NB: A ModelTimeline can apply a transform to all elements in the model, but no code exists which actually applies that at display time.
-   // So for now we continue to only consider the ElementTimeline transform.
-   let scriptTransform;
-   /* ###TODO
-   if (this.scheduleScript && args.elementId) {
-     const idPair = Id64.getUint32Pair(args.elementId);
-     const modelTimeline = this.scheduleScript.find(args.modelId);
-     const elementTimeline = modelTimeline?.getTimelineForElement(idPair.lower, idPair.upper);
-     scriptTransform = elementTimeline?.getAnimationTransform(args.timePoint ?? this.displayStyle.settings.timePoint ?? 0);
-   }
-  */
-
-   if (0 === elevation && !modelTransform && !scriptTransform)
-     return undefined;
-
-   const transform = Transform.createTranslationXYZ(0, 0, elevation);
-   if (modelTransform?.premultiply)
-     modelTransform.transform.multiplyTransformTransform(transform, transform);
-
-   if (modelTransform && !modelTransform.premultiply)
-     transform.multiplyTransformTransform(modelTransform.transform, transform);
-
-   if (scriptTransform)
-     transform.multiplyTransformTransform(scriptTransform as Transform, transform);
-
-   return transform;
- }
-
- /** Returns an iterator over additional Viewports used to construct this view's scene. e.g., those used for ViewAttachments and section drawings.
-  * This exists chiefly for display-performance-test-app to determine when all tiles required for the view have been loaded.
-  * @internal
-  */
- public get secondaryViewports(): Iterable<Viewport> {
-   return [];
- }
-
- /** Find the viewport that renders the contents of the view attachment with the specified element Id into this view.
-  * @internal
-  */
- public getAttachmentViewport(_id: Id64String): Viewport | undefined {
-   return undefined;
- }
-
- abstract modelClipGroups: ModelClipGroups;
+  modelClipGroups: ModelClipGroups;
 }
 
-export abstract class View3d extends View {
- /** @internal */
- protected constructor() { super(); }
+export interface View3d extends IView {
+  readonly is3d: true;
+  readonly is2d?: never;
 
- override is3d(): this is View3d { return true; }
- override isDrawingView(): this is DrawingView { return false; }
+  displayStyle: View3dStyle;
 
- abstract getDisplayStyle3d(): View3dStyle;
- 
- override get displayStyle(): View3dStyle {
-  return this.getDisplayStyle3d();
- }
- 
- /** @internal */
- abstract getModelClip(modelId: Id64String): RenderClipVolume | undefined;
+  /** @internal */
+  getModelClip(modelId: Id64String): RenderClipVolume | undefined;
+}
+
+export interface View2d extends IView {
+  readonly is2d: true;
+  readonly is3d: never;
+}
+
+export interface ViewModelSelector {
+ models: Set<Id64String>;
+ equalState(other: ViewModelSelector): boolean;
+ addModels(models: Id64Arg): void;
+ dropModels(models: Id64Arg): void;
+}
+
+export interface SpatialView extends View3d {
+ computeSpatialFitRange(options?: ComputeSpatialViewFitRangeOptions): AxisAlignedBox3d;
+}
+
+export interface DrawingView extends View2d {
  
 }
 
-export abstract class View2d extends View {
-
-}
-
-export abstract class SpatialView extends View3d {
- override isSpatialView(): this is SpatialView {
-  return true;
- }
-}
-
-export abstract class DrawingView extends View2d {
+export interface SheetView extends View2d {
  
 }
 
-export abstract class SheetView extends View2d {
- 
-}
+export type View = DrawingView | SheetView | SpatialView | View3d | View2d;
--- core/frontend/src/scene/ViewStyle.ts
+++ core/frontend/src/scene/ViewStyle.ts
@@ -15,63 +15,54 @@ import { TileTreeReference } from "../tile/internal";
  */
 export type ViewStyleFlags = ViewFlagsProperties; // Optional<ViewFlagsProperties, "acsTriad" | "grid" | "backgroundMap" | "ambientOcclusion">;
 
-export abstract class ViewStyle {
-  /** @internal */
-  protected constructor() { }
+export interface IViewStyle {
+  viewFlags: ViewStyleFlags;
+  readonly iModel: IModelConnection;
 
-  abstract viewFlags: ViewStyleFlags;
-  abstract get iModel(): IModelConnection;
-
-  abstract planarClipMasks: Map<Id64String, PlanarClipMaskSettings>;
+  planarClipMasks: Map<Id64String, PlanarClipMaskSettings>;
 
   // ###TODO renderTimeline Id and scheduleScriptProps
 
-  abstract overrideSubCategory(id: Id64String, ovr: SubCategoryOverride): void;  
-  abstract dropSubCategoryOverride(id: Id64String): void;  
-  abstract get subCategoryOverrides(): ReadonlyMap<Id64String, Readonly<SubCategoryOverride>>;  
-  abstract getSubCategoryOverride(id: Id64String): Readonly<SubCategoryOverride> | undefined;  
-  abstract hasSubCategoryOverride(): boolean;
+  overrideSubCategory(id: Id64String, ovr: SubCategoryOverride): void;  
+  dropSubCategoryOverride(id: Id64String): void;  
+  readonly subCategoryOverrides: ReadonlyMap<Id64String, Readonly<SubCategoryOverride>>;  
+  getSubCategoryOverride(id: Id64String): Readonly<SubCategoryOverride> | undefined;  
+  hasSubCategoryOverride(): boolean;
 
-  abstract overrideModelAppearance(id: Id64String, ovr: FeatureAppearance): void;  
-  abstract dropModelAppearanceOverride(id: Id64String): void;  
-  abstract get modelAppearanceOverrides(): ReadonlyMap<Id64String, Readonly<FeatureAppearance>>;  
-  abstract getModelAppearanceOverride(id: Id64String): Readonly<FeatureAppearance> | undefined;  
-  abstract hasModelAppearanceOverride(): boolean;
+  overrideModelAppearance(id: Id64String, ovr: FeatureAppearance): void;  
+  dropModelAppearanceOverride(id: Id64String): void;  
+  readonly modelAppearanceOverrides: ReadonlyMap<Id64String, Readonly<FeatureAppearance>>;  
+  getModelAppearanceOverride(id: Id64String): Readonly<FeatureAppearance> | undefined;  
+  hasModelAppearanceOverride(): boolean;
 
-  abstract getRealityModelDisplaySettings(modelId: Id64String): RealityModelDisplaySettings | undefined;
-  abstract setRealityModelDisplaySettings(modelId: Id64String, settings: RealityModelDisplaySettings | undefined): void;
+  // For persistent reality models only.
+  getRealityModelDisplaySettings(modelId: Id64String): RealityModelDisplaySettings | undefined;
+  setRealityModelDisplaySettings(modelId: Id64String, settings: RealityModelDisplaySettings | undefined): void;
 
-  abstract get excludedElementIds(): OrderedId64Iterable;
-  abstract addExcludedElements(id: Id64String | Iterable<Id64String>): void;
-  abstract dropExcludedElement(id: Id64String): void;
-  abstract dropExcludedElements(id: Id64String | Iterable<Id64String>): void;
-  abstract clearExcludedElements(): void;
+  readonly excludedElementIds: OrderedId64Iterable;
+  addExcludedElements(id: Id64String | Iterable<Id64String>): void;
+  dropExcludedElement(id: Id64String): void;
+  dropExcludedElements(id: Id64String | Iterable<Id64String>): void;
+  clearExcludedElements(): void;
 
   // ###TODO yuck
-  abstract forEachTileTreeRef(func: (ref: TileTreeReference) => void): void;
-  
-  // ###TODO ClipStyle to override/replace Scene's style?
-
-  get settings(): ViewStyle {
-    return this;
-  }
-
-  is3d(): this is View3dStyle {
-    return false;
-  }
+  forEachTileTreeRef(func: (ref: TileTreeReference) => void): void;
 }
 
-export abstract class View3dStyle extends ViewStyle {
-  /** @internal */
-  protected constructor() { super(); }
-
-  override is3d(): this is View3dStyle {
-    return true;
-  }
-
+export interface View3dStyle extends IViewStyle {
+  readonly is3d: true;
+  is2d?: never;
+  
   // ###TODO permit Scene's HiddenLine.Settings to be overridden/replaced?
 
-  abstract getPlanProjectionSettings(modelId: Id64String): PlanProjectionSettings | undefined;
-  abstract setPlanProjectionSettings(modelId: Id64String, settings: PlanProjectionSettings | undefined): void;
-  abstract get planProjectionSettings(): Iterable<[Id64String, PlanProjectionSettings]> | undefined;
+  getPlanProjectionSettings(modelId: Id64String): PlanProjectionSettings | undefined;
+  setPlanProjectionSettings(modelId: Id64String, settings: PlanProjectionSettings | undefined): void;
+  readonly planProjectionSettings: Iterable<[Id64String, PlanProjectionSettings]> | undefined;
 }
+
+export interface View2dStyle extends IViewStyle {
+  readonly is2d: true;
+  is3d?: never;
+}
+
+export type ViewStyle = View2dStyle | View3dStyle;
