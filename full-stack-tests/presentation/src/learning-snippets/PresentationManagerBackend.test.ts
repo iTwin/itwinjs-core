b07ed040c85a73f8afc8ac3fae9b07ac8b760db5
grigas.petraitis <35135765+grigasp@users.noreply.github.com>
2024-03-06 09:07:55 +0200

    Small improvement to `getElementProperties` learning snippet

--- full-stack-tests/presentation/src/learning-snippets/PresentationManagerBackend.test.ts
+++ full-stack-tests/presentation/src/learning-snippets/PresentationManagerBackend.test.ts
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
-import { IContentVisitor, ProcessMergedValueProps, ProcessPrimitiveValueProps, traverseContentItem, Value } from "@itwin/presentation-common";
+import { DisplayValue, IContentVisitor, ProcessPrimitiveValueProps, traverseContentItem, Value } from "@itwin/presentation-common";
 import { initialize, terminate } from "../IntegrationTests";
 import { IModelDb, SnapshotDb } from "@itwin/core-backend";
 import { Presentation } from "@itwin/presentation-backend";
@@ -25,11 +25,16 @@ describe("Learning Snippets", () => {
     describe("getElementProperties", () => {
       it("retrieves element properties with custom content parser", async () => {
         // __PUBLISH_EXTRACT_START__ Presentation.Backend.GetElementProperties.WithCustomContentParser
-        interface RawPrimitivePropertiesSet {
-          [propertyLabel: string]: Value;
+        interface PrimitivePropertiesSet {
+          [propertyLabel: string]:
+            | {
+                rawValue: string | number | boolean;
+                displayValue?: string;
+              }
+            | undefined;
         }
-        class RawPrimitivePropertiesBuilder implements IContentVisitor {
-          private _curr: RawPrimitivePropertiesSet = {};
+        class PrimitivePropertiesBuilder implements IContentVisitor {
+          private _curr: PrimitivePropertiesSet = {};
 
           public getResult() {
             return this._curr;
@@ -49,7 +54,8 @@ describe("Learning Snippets", () => {
           public finishItem() {}
 
           public startCategory() {
-            // TODO: handle properties' categorization here
+            // TODO: handle properties' categorization here.
+            // Example: https://github.com/iTwin/itwinjs-core/blob/ee850d3d62fc2fd6e263236205c866e3be21f063/presentation/backend/src/presentation-backend/ElementPropertiesHelper.ts#L296
             return true;
           }
           public finishCategory() {}
@@ -60,40 +66,61 @@ describe("Learning Snippets", () => {
           public finishField() {}
 
           public startStruct() {
-            // TODO: handle struct properties here. Returning `false` omits them from result.
+            // TODO: handle struct properties here.
+            // Example: https://github.com/iTwin/itwinjs-core/blob/ee850d3d62fc2fd6e263236205c866e3be21f063/presentation/backend/src/presentation-backend/ElementPropertiesHelper.ts#L309
+            // Returning `false` omits them from result.
             return false;
           }
           public finishStruct() {}
 
           public startArray() {
-            // TODO: handle array properties here. Returning `false` omits them from result.
+            // TODO: handle array properties here.
+            // Example: https://github.com/iTwin/itwinjs-core/blob/ee850d3d62fc2fd6e263236205c866e3be21f063/presentation/backend/src/presentation-backend/ElementPropertiesHelper.ts#L317
+            // Returning `false` omits them from result.
             return false;
           }
           public finishArray() {}
 
-          public processMergedValue(props: ProcessMergedValueProps) {
-            this._curr[props.mergedField.label] = undefined;
+          public processMergedValue() {
+            // `getElementProperties` doesn't create merged values, so no need to handle them here
           }
           public processPrimitiveValue(props: ProcessPrimitiveValueProps) {
+            this._curr[props.field.label] = this.createPrimitiveValueEntry(props);
+          }
+          private createPrimitiveValueEntry(props: ProcessPrimitiveValueProps) {
             let rawValue: string | number | boolean | undefined;
             if (Value.isNavigationValue(props.rawValue)) {
               rawValue = props.rawValue.id;
             } else if (Value.isPrimitive(props.rawValue)) {
               rawValue = props.rawValue;
             }
-            this._curr[props.field.label] = rawValue;
+
+            if (rawValue === undefined) {
+              return undefined;
+            }
+
+            if (props.displayValue === rawValue) {
+              return { rawValue };
+            }
+
+            let displayValue: string | undefined;
+            if (DisplayValue.isPrimitive(props.displayValue)) {
+              displayValue = props.displayValue;
+            }
+            return { rawValue, displayValue };
           }
         }
         const result = await Presentation.getManager().getElementProperties({
           imodel,
           contentParser(descriptor, item) {
-            const builder = new RawPrimitivePropertiesBuilder();
+            const builder = new PrimitivePropertiesBuilder();
             traverseContentItem(builder, descriptor, item);
             return builder.getResult();
           },
           elementClasses: ["BisCore:PhysicalPartition"],
         });
         // __PUBLISH_EXTRACT_END__
+
         const batches = await fromAsync(result.iterator());
         expect(batches).to.have.lengthOf(1);
         const firstBatch = batches[0];
@@ -101,8 +128,13 @@ describe("Learning Snippets", () => {
           .to.have.lengthOf(1)
           .and.to.containSubset([
             {
-              ["Model"]: "0x1",
-              ["Code"]: "Properties_60InstancesWithUrl2",
+              ["Model"]: {
+                rawValue: "0x1",
+                displayValue: "DgnV8Bridge",
+              },
+              ["Code"]: {
+                rawValue: "Properties_60InstancesWithUrl2",
+              },
               ["User Label"]: undefined,
               ["Description"]: undefined,
             },
