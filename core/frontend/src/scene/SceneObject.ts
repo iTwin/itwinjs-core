c6e506334d3f071d03f49e277db4f8e85feac48f
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-11 16:45:59 -0400

    scene object collections, decorator scene objects

--- core/frontend/src/scene/SceneObject.ts
+++ core/frontend/src/scene/SceneObject.ts
@@ -10,12 +10,14 @@ import { GuidString } from "@itwin/core-bentley";
 import { ViewportDecorator } from "../Viewport";
 import { HitDetail } from "../HitDetail";
 import { SceneContext } from "../ViewContext";
-import { IModelView } from "./IModelView";
+import { IModelSpatialView, IModelView, IModelView2d } from "./IModelView";
 import { TiledGraphicsProvider } from "../tile/internal";
 import { SceneRealityModel } from "./SceneRealityModel";
 import { ScenePresentation, ScenePresentation2d, ScenePresentation3d } from "./ScenePresentation";
 import { SceneMap } from "./SceneMap";
 import { ViewportScene } from "./Scene";
+import { IModelConnection } from "../IModelConnection";
+import { Decorator } from "../ViewManager";
 
 export interface SceneObjectPayload {
   readonly customType?: string;
@@ -24,6 +26,7 @@ export interface SceneObjectPayload {
   readonly realityModel?: SceneRealityModel;
   readonly graphicsProvider?: TiledGraphicsProvider;
   readonly map?: SceneMap;
+  readonly decorator?: Decorator;
 }
 
 export interface SceneObject extends ViewportDecorator, SceneObjectPayload {
@@ -42,7 +45,43 @@ export interface SceneObject extends ViewportDecorator, SceneObjectPayload {
 }
 
 export type RealityModelSceneObject = SceneObject & { readonly realityModel: SceneRealityModel; };
+
 export type PresentationSceneObject = SceneObject & { readonly presentation: ScenePresentation; }
 export type Presentation2dSceneObject = SceneObject & { readonly presentation: ScenePresentation2d; }
 export type Presentation3dSceneObject = SceneObject & { readonly presentation: ScenePresentation3d; }
+
 export type CustomSceneObject = SceneObject & { readonly customType: string; }
+export type TiledGraphicsSceneObject = SceneObject & { readonly graphicsProvider: TiledGraphicsProvider; }
+export type MapSceneObject = SceneObject & { readonly map: SceneMap; }
+export type DecoratorSceneObject = SceneObject & { readonly decorator: Decorator; }
+
+export type IModelViewSceneObject = SceneObject & { readonly view: IModelView; };
+export type SpatialViewSceneObject = SceneObject & { readonly view: IModelSpatialView; }
+export type TestIModelView2dSceneObject = SceneObject & { readonly view: IModelView2d; };
+
+export interface SpatialViewSceneObjects extends Iterable<SpatialViewSceneObject> {
+  add(view: IModelSpatialView): SpatialViewSceneObject;
+  delete(view: SpatialViewSceneObject): void; // ###TODO return type?
+  clear(): void;
+  findFirstForIModel(iModel: IModelConnection): SpatialViewSceneObject | undefined;
+}
+
+export type SceneRealityModelProps = Pick<SceneRealityModel, "sourceKey" | "name" | "description" | "realityDataId">;
+
+export interface RealityModelSceneObjects extends Iterable<RealityModelSceneObject> {
+  add(props: SceneRealityModelProps): RealityModelSceneObject;
+  delete(model: RealityModelSceneObject): void; // ###TODO return type?
+  clear(): void;
+}
+
+export interface TiledGraphicsSceneObjects extends Iterable<TiledGraphicsSceneObject> {
+  add(provider: TiledGraphicsProvider): TiledGraphicsSceneObject;
+  delete(object: TiledGraphicsSceneObject): void; // ###TODO return type?
+  clear(): void;
+}
+
+// For Decorators registered with ViewManager.
+// Add/remove is handled by ViewManager API, not here - we react to it by updating this collection though.
+export interface DecoratorSceneObjects extends Iterable<DecoratorSceneObject> {
+  find(decorator: Decorator): DecoratorSceneObject | undefined;
+}
