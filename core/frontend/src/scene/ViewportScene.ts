f9ad47eb6a9098a14726033919494e4448d448bb
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-12 14:28:08 -0400

    create spatial scene from view state - don't populate

--- core/frontend/src/scene/ViewportScene.ts
+++ core/frontend/src/scene/ViewportScene.ts
@@ -6,17 +6,28 @@
  * @module Views
  */
 
+import { GuidString } from "@itwin/core-bentley";
 import { DecoratorSceneObjects, IModelViewSceneObject, MapSceneObject, RealityModelSceneObjects, SceneObject, SpatialViewSceneObjects, IModelView2dSceneObject, TiledGraphicsSceneObjects } from "./SceneObject";
 import { ScenePresentation, ScenePresentation2d, ScenePresentation3d } from "./ScenePresentation";
 import { SceneVolume, SceneVolume3d } from "./SceneVolume";
+import { SpatialViewState } from "../SpatialViewState";
+import { ViewState } from "../ViewState";
+import { createSpatialScene, createViewportScene } from "./impl/ViewportSceneImpl";
 
 export interface ViewportScene extends Iterable<SceneObject> {
+  /** An IModelView created from the ViewState supplied to the constructor.
+   * this.presentation and this.volume are created from the same ViewState.
+   * This is used as a shim for deprecated Viewport methods+properties like Viewport.iModel, Viewport.perModelCategoryVisibility, etc.
+   * @internal
+   */
+ readonly backingView: ViewState;
+
  isSpatial(): this is SpatialScene;
  is2dModel(): this is Model2dScene;
 
  readonly volume: SceneVolume;
  readonly presentation: ScenePresentation;
- readonly iModels: Iterable<IModelViewSceneObject>;
+ // readonly iModels: Iterable<IModelViewSceneObject>; // ###TODO needed, or just confusing for 2d?
  readonly tiledGraphicsProviders: TiledGraphicsSceneObjects;
  readonly decorators: DecoratorSceneObjects;
 
@@ -35,8 +46,24 @@ export interface Model2dScene extends ViewportScene {
  readonly volume: SceneVolume;
  readonly presentation: ScenePresentation2d;
  readonly view: IModelView2dSceneObject;
- readonly iModels: Iterable<IModelView2dSceneObject>;
+ // readonly iModels: Iterable<IModelView2dSceneObject>;
 }
 
 // ###TODO export interface DrawingModelScene extends Model2dScene
 // ###TODO export interface SheetModelScene extends Model2dScene
+
+export interface CreateSpatialSceneArgs {
+ view: SpatialViewState;
+ mapGuid?: GuidString;
+ presentationGuid?: GuidString;
+}
+
+export namespace ViewportScene {
+ export function createSpatial(args: CreateSpatialSceneArgs): SpatialScene {
+  return createSpatialScene(args);
+ }
+
+ export function fromViewState(view: ViewState): ViewportScene {
+  return createViewportScene(view);
+ }
+}
--- core/frontend/src/scene/impl/ViewportSceneImpl.ts
+++ core/frontend/src/scene/impl/ViewportSceneImpl.ts
@@ -6,7 +6,7 @@
 import { assert, Guid } from "@itwin/core-bentley";
 import { ViewState } from "../../ViewState";
 import { Viewport } from "../../Viewport";
-import { Model2dScene, SpatialScene, ViewportScene } from "../ViewportScene";
+import { CreateSpatialSceneArgs, Model2dScene, SpatialScene, ViewportScene } from "../ViewportScene";
 import { BaseIModelViewImpl, createIModelView, IModelSpatialViewImpl, SpatialViewSceneObjectImpl } from "./IModelViewImpl";
 import { ScenePresentation3dImpl, BaseScenePresentationImpl, ScenePresentationImpl, PresentationSceneObjectImpl } from "./ScenePresentationImpl";
 import { SceneVolume3dImpl, SceneVolumeImpl } from "./SceneVolumeImpl";
@@ -15,20 +15,12 @@ import { SubCategoriesCache } from "../../SubCategoriesCache";
 import { IModelViewSceneObject, PresentationSceneObject, SceneObject } from "../SceneObject";
 
 export abstract class ViewportSceneImpl implements ViewportScene {
-  private _backingView: BaseIModelViewImpl;
+  readonly backingView: ViewState;
   private readonly _subcategories = new SubCategoriesCache.Queue();
 
   readonly tiledGraphicsProviders = [] as any; // ###TODO
   readonly decorators = [] as any; // ###TODO
   
-
-  /** An IModelView created from the ViewState supplied to the constructor.
-   * this.presentation and this.volume are created from the same ViewState.
-   * Unless the view refers to a blank iModel, this view is also added to this.iModels.
-   * This is used as a shim for deprecated Viewport methods+properties like Viewport.iModel, Viewport.perModelCategoryVisibility, etc.
-   */
-  get backingView(): BaseIModelViewImpl { return this._backingView; }
-
   abstract isSpatial(): this is SpatialScene;
   abstract is2dModel(): this is Model2dScene;
   
@@ -38,7 +30,7 @@ export abstract class ViewportSceneImpl implements ViewportScene {
   abstract get iModels(): Iterable<IModelViewSceneObject>;
   
   protected constructor(view: ViewState) {
-    this._backingView = createIModelView(view);
+    this.backingView = view;
   }
 
   dispose(): void {
@@ -77,20 +69,15 @@ export class SpatialSceneImpl extends ViewportSceneImpl implements SpatialScene 
   readonly iModels = [] as any; // ###TODO
   readonly map?: any; // ###TODO
 
-  constructor(view: SpatialViewState) {
-    super(view);
+  constructor(args: CreateSpatialSceneArgs) {
+    super(args.view);
 
-    this.volume = new SceneVolume3dImpl(view);
-    const presentation = new ScenePresentation3dImpl(view);
-    const presentationGuid = Guid.createValue(); // ###TODO round-trip GUIDs
+    this.volume = new SceneVolume3dImpl(args.view);
+    const presentation = new ScenePresentation3dImpl(args.view);
+    const presentationGuid = args.presentationGuid ?? Guid.createValue();
     this.presentationObject = new PresentationSceneObjectImpl<ScenePresentation3dImpl, SpatialScene>(presentation, presentationGuid, this);
 
-    // ###TODO move this to post-constructor initialization.
-    if (!view.iModel.isBlank) {
-      const viewGuid = Guid.createValue(); // ###TODO round-trip GUIDs
-      const spatialView = new IModelSpatialViewImpl(view);
-      this.iModels.add(spatialView, { guid: viewGuid });
-    }
+    // ###TODO initialize this.map
   }
 
   *[Symbol.iterator]() {
@@ -104,9 +91,15 @@ export class SpatialSceneImpl extends ViewportSceneImpl implements SpatialScene 
   }
 }
 
-export function createViewportScene(view: ViewState): ViewportScene {
-  if (view.isSpatialView())
-    return new SpatialSceneImpl(view);
+export function createSpatialScene(args: CreateSpatialSceneArgs): SpatialSceneImpl {
+  return new SpatialSceneImpl(args);
+}
 
-  throw new Error("###TODO non-spatial scenes");
+export function createViewportScene(view: ViewState): ViewportScene {
+  if (!view.isSpatialView())
+    throw new Error("###TODO non-spatial scenes");
+
+  const scene = createSpatialScene({ view });
+  // ###TODO populate it from ViewState
+  return scene;
 }
