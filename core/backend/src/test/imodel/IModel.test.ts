664d12ccc1285310fc6c62277c25475764a43204
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-20 09:44:44 -0400

    update test

--- core/backend/src/test/imodel/IModel.test.ts
+++ core/backend/src/test/imodel/IModel.test.ts
@@ -52,7 +52,7 @@ function expectIModelError(expectedErrorNumber: IModelStatus | DbResult, error: 
   expect(error!.errorNumber).to.equal(expectedErrorNumber);
 }
 
-describe.only("iModel", () => {
+describe("iModel", () => {
   let imodel1: SnapshotDb;
   let imodel2: SnapshotDb;
   let imodel3: SnapshotDb;
@@ -2853,16 +2853,15 @@ describe.only("iModel", () => {
     imodel.close();
   });
 
-  it.only("throws NotFound when attempting to access element props after closing the iModel", () => {
+  it("throws NotFound when attempting to access element props after closing the iModel", () => {
     const imodelPath = IModelTestUtils.prepareOutputFile("IModel", "accessAfterClose.bim");
     const imodel = SnapshotDb.createEmpty(imodelPath, { rootSubject: { name: "accessAfterClose" } });
 
-    let props = imodel.elements.getElementProps(IModel.rootSubjectId) as SubjectProps;
-    expect(props.id).to.equal(IModel.rootSubjectId);
+    const elem = imodel.elements.getElement<Subject>(IModel.rootSubjectId);
+    expect(elem.id).to.equal(IModel.rootSubjectId);
     
     imodel.close();
     
-    props = imodel.elements.getElementProps(IModel.rootSubjectId) as SubjectProps;
-    expect(props.id).to.equal(IModel.rootSubjectId);
+    expect(() => imodel.elements.getElement<Subject>(IModel.rootSubjectId)).to.throw(IModelError, "Element=0x1", "Not Found");
   });
 });
