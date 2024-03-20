6421e0163fb283e521bb07c0411c4f90de9f1d43
RohitPtnkr1996 <111407262+RohitPtnkr1996@users.noreply.github.com>
2024-03-20 15:26:15 +0530

    Added test to perform profile/domain schema upgrade on an imodel set up with schema sync

--- full-stack-tests/backend/src/integration/SchemaSync.test.ts
+++ full-stack-tests/backend/src/integration/SchemaSync.test.ts
@@ -3,12 +3,14 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { Suite } from "mocha";
-import { BriefcaseDb, BriefcaseManager, CloudSqlite, HubMock, IModelDb, IModelHost, SchemaSync, SnapshotDb } from "@itwin/core-backend";
+import { BriefcaseDb, BriefcaseManager, CloudSqlite, HubMock, IModelDb, IModelHost, SchemaSync, SnapshotDb, SqliteStatement } from "@itwin/core-backend";
 import { AzuriteTest } from "./AzuriteTest";
 import { IModelTestUtils, KnownTestLocations } from "@itwin/core-backend/lib/cjs/test";
-import { AccessToken, Guid, OpenMode } from "@itwin/core-bentley";
+import { AccessToken, DbResult, Guid, OpenMode } from "@itwin/core-bentley";
+import * as path from "path";
+
 const storageType = "azure" as const;
 
 async function initializeContainer(containerProps: { containerId: string, isPublic?: boolean, baseUri: string }) {
@@ -224,5 +226,110 @@ describe("Schema synchronization", function (this: Suite) {
 
     HubMock.shutdown();
   });
-});
 
+  it("test schema sync with profile and domain schema upgrade", async () => {
+    const containerProps = await initializeContainer({ baseUri: AzuriteTest.baseUri, containerId: "imodel-sync-itwin-1" });
+
+    const iTwinId = Guid.createValue();
+    const user1AccessToken = "token 1";
+    const user2AccessToken = "token 2";
+    const user3AccessToken = "token 3";
+
+    HubMock.startup("test", KnownTestLocations.outputDir);
+
+    // Setup seed file from exising 4.0.0.3 imodel
+    const testFile = SnapshotDb.openDgnDb({ path: path.join(process.env.IMODELJS_CORE_DIRNAME!, "core/backend/lib/cjs/test/assets/test_ec_4003.bim")}, OpenMode.ReadWrite);
+    const version0 = testFile.getFilePath();
+    testFile.closeFile();
+
+    const iModelId = await HubMock.createNewIModel({ accessToken: user1AccessToken, iTwinId, version0, iModelName: "schemaSync" });
+
+    const openNewBriefcase = async (accessToken: AccessToken) => {
+      const bcProps = await BriefcaseManager.downloadBriefcase({ iModelId, iTwinId, accessToken });
+      return BriefcaseDb.open(bcProps);
+    };
+
+    const b1Props = await BriefcaseManager.downloadBriefcase({ iModelId, iTwinId, accessToken: user1AccessToken });
+    let b1 = await BriefcaseDb.open(b1Props);
+    const b2 = await openNewBriefcase(user2AccessToken);
+    const b3 = await openNewBriefcase(user3AccessToken);
+
+    SchemaSync.setTestCache(b1, "briefcase1");
+    SchemaSync.setTestCache(b2, "briefcase2");
+    SchemaSync.setTestCache(b3, "briefcase3");
+
+    await SchemaSync.initializeForIModel({ iModel: b1, containerProps });
+    await b1.pushChanges({ accessToken: user1AccessToken, description: "enable shared schema channel" });
+    assert.isTrue(b1.nativeDb.schemaSyncEnabled());
+
+    const getProfileVersion = async (db: BriefcaseDb): Promise<string> => {
+      return db.withPreparedSqliteStatement("SELECT StrData FROM be_Prop WHERE Namespace='ec_Db' and Name='SchemaVersion'", (stmt: SqliteStatement) => {
+        if (stmt.step() === DbResult.BE_SQLITE_ROW)
+          return stmt.getValue(0).getString();
+        return "";
+      });
+    };
+
+    // Make sure all briefcases are on the same profile version 4.0.0.3
+    const initialProfileVersion = JSON.parse(`{"major":4,"minor":0,"sub1":0,"sub2":3}`);
+    let b1ProfileVersion = JSON.parse(await getProfileVersion(b1));
+    expect(b1ProfileVersion.major === initialProfileVersion.major).to.be.true;
+    expect(b1ProfileVersion.minor === initialProfileVersion.minor).to.be.true;
+    expect(b1ProfileVersion.sub1 === initialProfileVersion.sub1).to.be.true;
+    expect(b1ProfileVersion.sub2 === initialProfileVersion.sub2).to.be.true;
+
+    let b2ProfileVersion = JSON.parse(await getProfileVersion(b2));
+    expect(b2ProfileVersion.major === initialProfileVersion.major).to.be.true;
+    expect(b2ProfileVersion.minor === initialProfileVersion.minor).to.be.true;
+    expect(b2ProfileVersion.sub1 === initialProfileVersion.sub1).to.be.true;
+    expect(b2ProfileVersion.sub2 === initialProfileVersion.sub2).to.be.true;
+
+    let b3ProfileVersion = JSON.parse(await getProfileVersion(b3));
+    expect(b3ProfileVersion.major === initialProfileVersion.major).to.be.true;
+    expect(b3ProfileVersion.minor === initialProfileVersion.minor).to.be.true;
+    expect(b3ProfileVersion.sub1 === initialProfileVersion.sub1).to.be.true;
+    expect(b3ProfileVersion.sub2 === initialProfileVersion.sub2).to.be.true;
+
+    b1.close();
+
+    // Perform a profile and domain schema upgrade on one of the briefcases
+    await BriefcaseDb.upgradeSchemas(b1Props);
+    b1 = await BriefcaseDb.open(b1Props);
+
+    // Push the profile upgrade changes
+    await b1.pushChanges({ accessToken: user2AccessToken, description: "Push profile and domain schema upgrade changes" });
+    assert.isTrue(b1.nativeDb.schemaSyncEnabled());
+
+    // Pull the new changes into the other briefcases
+    await b2.pullChanges({ accessToken: user1AccessToken });
+    assert.isTrue(b2.nativeDb.schemaSyncEnabled());
+    await b3.pullChanges({ accessToken: user3AccessToken });
+    assert.isTrue(b3.nativeDb.schemaSyncEnabled());
+
+    // Test all 3 briefcases for the upgraded profile version 4.0.0.X (where X is at least 4)
+    const updatedProfileVersion = JSON.parse(`{"major":4,"minor":0,"sub1":0,"sub2":4}`);
+    b1ProfileVersion = JSON.parse(await getProfileVersion(b1));
+    expect(b1ProfileVersion.major).to.be.equal(updatedProfileVersion.major, "Profile version major should be 4");
+    expect(b1ProfileVersion.minor).to.be.equal(updatedProfileVersion.minor, "Profile version minor should be 0");
+    expect(b1ProfileVersion.sub1).to.be.equal(updatedProfileVersion.sub1, "Profile version sub1 should be 0");
+    expect(b1ProfileVersion.sub2).to.be.greaterThanOrEqual(updatedProfileVersion.sub2, "Profile version sub2 should be at least 4");
+
+    b2ProfileVersion = JSON.parse(await getProfileVersion(b2));
+    expect(b2ProfileVersion.major).to.be.equal(updatedProfileVersion.major, "Profile version major should be 4");
+    expect(b2ProfileVersion.minor).to.be.equal(updatedProfileVersion.minor, "Profile version minor should be 0");
+    expect(b2ProfileVersion.sub1).to.be.equal(updatedProfileVersion.sub1, "Profile version sub1 should be 0");
+    expect(b2ProfileVersion.sub2).to.be.greaterThanOrEqual(updatedProfileVersion.sub2, "Profile version sub2 should be at least 4");
+
+    b3ProfileVersion = JSON.parse(await getProfileVersion(b3));
+    expect(b3ProfileVersion.major).to.be.equal(updatedProfileVersion.major, "Profile version major should be 4");
+    expect(b3ProfileVersion.minor).to.be.equal(updatedProfileVersion.minor, "Profile version minor should be 0");
+    expect(b3ProfileVersion.sub1).to.be.equal(updatedProfileVersion.sub1, "Profile version sub1 should be 0");
+    expect(b3ProfileVersion.sub2).to.be.greaterThanOrEqual(updatedProfileVersion.sub2, "Profile version sub2 should be at least 4");
+
+    b1.close();
+    b2.close();
+    b3.close();
+
+    HubMock.shutdown();
+  });
+});
