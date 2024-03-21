1de523b92ac729b2d1fd022a40eb16808f7b2104
pmconne <22944042+pmconne@users.noreply.github.com>
2024-03-21 07:13:27 -0400

    render test

--- core/frontend/src/test/render/webgl/RenderGraphicTileTree.test.ts
+++ core/frontend/src/test/render/webgl/RenderGraphicTileTree.test.ts
@@ -5,9 +5,9 @@
 
 import { expect } from "chai";
 import { IModelApp } from "../../../IModelApp";
-import { GraphicType, TileTreeReference } from "../../../core-frontend";
-import { testBlankViewportAsync } from "../../openBlankViewport";
-import { ColorDef } from "@itwin/core-common";
+import { GraphicType, TileTreeReference, TiledGraphicsProvider } from "../../../core-frontend";
+import { readUniquePixelData, testBlankViewportAsync } from "../../openBlankViewport";
+import { ColorDef, Feature } from "@itwin/core-common";
 import { Point3d } from "@itwin/core-geometry";
 
 describe.only("TileTreeReference.createFromRenderGraphic", () => {
@@ -17,22 +17,17 @@ describe.only("TileTreeReference.createFromRenderGraphic", () => {
 
   it("loads asynchronously", async () => {
     testBlankViewportAsync(async (vp) => {
-      vp.displayStyle.backgroundColor = ColorDef.blue;
-    
       const builder = IModelApp.renderSystem.createGraphic({
         computeChordTolerance: () => 0,
         type: GraphicType.ViewOverlay,
       });
 
-      builder.setSymbology(ColorDef.red, ColorDef.red, 1);
       builder.addPointString([new Point3d(1, 1, 1)]);
 
-      const graphic = builder.finish();
-      const modelId = vp.iModel.transientIds.getNext();
       const ref = TileTreeReference.createFromRenderGraphic({
         iModel: vp.iModel,
-        graphic,
-        modelId,
+        graphic: builder.finish(),
+        modelId: vp.iModel.transientIds.getNext(),
       });
 
       expect(ref.isLoadingComplete).to.be.false;
@@ -47,16 +42,44 @@ describe.only("TileTreeReference.createFromRenderGraphic", () => {
     });
   });
 
-  it("renders to screen", () => {
-    
-  });
+  it("renders to screen", async () => {
+    testBlankViewportAsync(async (vp) => {
+      vp.displayStyle.backgroundColor = ColorDef.black;
+  
+      const modelId = vp.iModel.transientIds.getNext();
+      const point1Id = vp.iModel.transientIds.getNext();
+      const point2Id = vp.iModel.transientIds.getNext();
+  
+      const builder = IModelApp.renderSystem.createGraphic({
+        computeChordTolerance: () => 0,
+        type: GraphicType.ViewOverlay,
+      });
 
-  it("is contains a single pickable feature by default", () => {
-    
-  });
+      builder.setSymbology(ColorDef.red, ColorDef.red, 1);
+      builder.activateFeature(new Feature(point1Id));
+      builder.addPointString([new Point3d(1, 1, 1)]);
+      builder.setSymbology(ColorDef.blue, ColorDef.blue, 1);
+      builder.activateFeature(new Feature(point2Id));
+      builder.addPointString([new Point3d(2, 2, 2)]);
 
-  it("can contain multiple pickable features", () => {
-    
+
+      const ref = TileTreeReference.createFromRenderGraphic({
+        iModel: vp.iModel,
+        graphic: builder.finish(),
+        modelId,
+      });
+
+      vp.addTiledGraphicsProvider({
+        forEachTileTreeRef: (_, func) => func(ref),
+      });
+
+      await vp.waitForSceneCompletion();
+
+      const pixels = readUniquePixelData(vp);
+      expect(pixels.length).to.equal(3);
+      expect(pixels.containsElement(point1Id)).to.be.true;
+      expect(pixels.containsElement(point2Id)).to.be.true;
+    });
   });
 
   it("can supply a tooltip", () => {
