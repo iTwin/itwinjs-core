9d9b67380d0dfd8fdf486ea5bb13832b9dc0760a
StefanApfel <71712332+StefanApfel-Bentley@users.noreply.github.com>
2024-03-28 13:36:34 +0100

    Adopt latest SchemaItemType changes in SchemaComparer

--- core/ecschema-editing/src/Validation/SchemaComparer.ts
+++ core/ecschema-editing/src/Validation/SchemaComparer.ts
@@ -10,7 +10,7 @@ import {
   AnyClass, AnyEnumerator, AnyProperty, classModifierToString, Constant, containerTypeToString, CustomAttributeClass,
   CustomAttributeContainerProps, EntityClass, Enumeration, Format, InvertedUnit, KindOfQuantity, Mixin, Phenomenon,
   primitiveTypeToString, PropertyCategory, propertyTypeToString, RelationshipClass, RelationshipConstraint, Schema,
-  SchemaItem, SchemaItemKey, schemaItemTypeToString, strengthDirectionToString, strengthToString, StructProperty, Unit,
+  SchemaItem, SchemaItemKey, strengthDirectionToString, strengthToString, StructProperty, Unit,
 } from "@itwin/ecschema-metadata";
 import { formatTraitsToArray, formatTypeToString, scientificTypeToString, showSignOptionToString } from "@itwin/core-quantity";
 import { ISchemaCompareReporter } from "./SchemaCompareReporter";
@@ -143,7 +143,7 @@ export class SchemaComparer {
       promises.push(this._reporter.reportSchemaItemMissing(schemaItemA, this._compareDirection));
       promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "description", schemaItemA.description, undefined, this._compareDirection));
       promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "label", schemaItemA.label, undefined, this._compareDirection));
-      promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "schemaItemType", schemaItemTypeToString(schemaItemA.schemaItemType), undefined, this._compareDirection));
+      promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "schemaItemType", schemaItemA.schemaItemType, undefined, this._compareDirection));
       await Promise.all(promises);
       return;
     }
@@ -158,8 +158,8 @@ export class SchemaComparer {
       promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "label", schemaItemA.label, schemaItemB.label, this._compareDirection));
 
     if (schemaItemA.schemaItemType !== schemaItemB.schemaItemType) {
-      const aType = schemaItemTypeToString(schemaItemA.schemaItemType);
-      const bType = schemaItemTypeToString(schemaItemB.schemaItemType);
+      const aType = schemaItemA.schemaItemType;
+      const bType = schemaItemB.schemaItemType;
       promises.push(this._reporter.reportSchemaItemDelta(schemaItemA, "schemaItemType", aType, bType, this._compareDirection));
     }
 
