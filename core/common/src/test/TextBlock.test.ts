d5a0d1d054f56f80f60e4163fecd89ffe0027f1d
pmconne <22944042+pmconne@users.noreply.github.com>
2024-04-10 08:36:22 -0400

    tests

--- core/common/src/test/TextBlock.test.ts
+++ core/common/src/test/TextBlock.test.ts
@@ -3,7 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
-import { FractionRunProps, ParagraphProps, RunProps, TextBlock, TextBlockProps, TextRunProps, TextStyleSettingsProps } from "../core-common";
+import { FractionRunProps, Paragraph, ParagraphProps, RunProps, TextBlock, TextBlockProps, TextRun, TextRunProps, TextStyleSettingsProps } from "../core-common";
 
 function makeTextRun(content?: string, styleName = "", styleOverrides?: TextStyleSettingsProps): TextRunProps {
   return {
@@ -34,8 +34,47 @@ function makeParagraph(runs?: RunProps[], styleName = "", styleOverrides?: TextS
 
 describe.only("TextBlockComponent", () => {
   describe("applyStyle", () => {
-    it("clears overrides and propagates to subcomponents by default", () => {
+    let block: TextBlock;
+    let paragraph: Paragraph;
+    let run: TextRun;
 
+    beforeEach(() => {
+      block = TextBlock.create({ styleName: "block", styleOverrides: { widthFactor: 1234 }});
+      paragraph = Paragraph.create({ styleName: "paragraph", styleOverrides: { lineHeight: 42 }});
+      run = TextRun.create({ styleName: "run", styleOverrides: { fontName: "Consolas" } });
+      paragraph.runs.push(run);
+      block.paragraphs.push(paragraph);
+    });
+
+    it("clears overrides and propagates to subcomponents by default", () => {
+      block.applyStyle("new");
+      for (const component of [run, block, paragraph]) {
+        expect(component.styleName).to.equal("new");
+        expect(component.styleOverrides).to.deep.equal({});
+      }
+    });
+
+    it("preserves overrides if specified", () => {
+      block.applyStyle("new", { preserveOverrides: true });
+      for (const component of [run, block, paragraph]) {
+        expect(component.styleName).to.equal("new");
+      }
+
+      expect(block.styleOverrides).to.deep.equal({ widthFactor: 1234 });
+      expect(paragraph.styleOverrides).to.deep.equal({ lineHeight: 42 });
+      expect(run.styleOverrides).to.deep.equal({ fontName: "Consolas" });
+    });
+
+    it("prevents propagation if specified", () => {
+      block.applyStyle("new", { preventPropagation: true });
+      expect(block.styleName).to.equal("new");
+      expect(block.styleOverrides).to.deep.equal({});
+      
+      expect(paragraph.styleName).to.equal("paragraph");
+      expect(paragraph.styleOverrides).to.deep.equal({ lineHeight: 42 });
+
+      expect(run.styleName).to.equal("run");
+      expect(run.styleOverrides).to.deep.equal({ fontName: "Consolas" });
     });
   });
 
@@ -62,6 +101,7 @@ describe.only("TextBlockComponent", () => {
     };
 
     const tb = TextBlock.create(props);
+    expect(tb.stringify()).to.equal("abc 1/π def   ghi j k l     / mno   ")
     const paragraphBreak = "P";
     const lineBreak = "L";
     const fractionSeparator = "F";
