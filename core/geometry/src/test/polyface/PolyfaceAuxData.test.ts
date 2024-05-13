40f536575b74e86a18a3f8367a388a9aab332689
dassaf4 <68340676+dassaf4@users.noreply.github.com>
2024-05-13 18:05:11 -0500

    test with scientific viz mesh auxData

--- core/geometry/src/test/polyface/PolyfaceAuxData.test.ts
+++ core/geometry/src/test/polyface/PolyfaceAuxData.test.ts
@@ -10,6 +10,7 @@ import { LineString3d } from "../../curve/LineString3d";
 import { Path } from "../../curve/Path";
 import { StrokeOptions } from "../../curve/StrokeOptions";
 import { Geometry } from "../../Geometry";
+import { Angle } from "../../geometry3d/Angle";
 import { Point3d, Vector3d } from "../../geometry3d/Point3dVector3d";
 import { NumberArray, Point3dArray } from "../../geometry3d/PointHelpers";
 import { Transform } from "../../geometry3d/Transform";
@@ -210,6 +211,118 @@ describe("PolyfaceAuxData", () => {
     }
 
     GeometryCoreTestIO.saveGeometry(allGeometry, "PolyfaceAuxData", "Compress");
+    expect(ck.getNumErrors()).equals(0);
+  });
+
+  // This test generates a mesh with auxData, inspired by the sandbox https://www.itwinjs.org/sandboxes/iTwinPlatform/Scientific%20Visualization
+  it("FlatMeshWithWaves", () => {
+    const ck = new Checker();
+
+    const createFlatMeshWithWaves = (): IndexedPolyface => {
+      const options = StrokeOptions.createForFacets();
+      options.shouldTriangulate = true;
+      const builder = PolyfaceBuilder.create(options);
+      const nDimensions = 4;
+      const spacing = 1.0;
+
+      // Create a simple flat mesh with nDimensions x nDimensions points
+      for (let iRow = 0; iRow < nDimensions - 1; iRow++) {
+        for (let iColumn = 0; iColumn < nDimensions - 1; iColumn++) {
+          const quad = [
+            Point3d.create(iRow * spacing, iColumn * spacing, 0.0),
+            Point3d.create((iRow + 1) * spacing, iColumn * spacing, 0.0),
+            Point3d.create((iRow + 1) * spacing, (iColumn + 1) * spacing, 0.0),
+            Point3d.create(iRow * spacing, (iColumn + 1) * spacing),
+          ];
+          builder.addQuadFacet(quad);
+        }
+      }
+
+      const polyface = builder.claimPolyface();
+      const zeroScalarData = [], zeroDisplacementData = [], radialHeightData = [], radialSlopeData = [], radialDisplacementData = [];
+      const radius = nDimensions * spacing / 2.0;
+      const center = new Point3d(radius, radius, 0.0);
+      const maxHeight = radius / 4.0;
+      const auxChannels = [];
+
+      // Create a radial wave - start and return to zero
+      for (let i = 0; i < polyface.data.point.length; i++) {
+        const angle = Angle.pi2Radians * polyface.data.point.distanceIndexToPoint(i, center)! / radius;
+        const height = maxHeight * Math.sin(angle);
+        const slope = Math.abs(Math.cos(angle));
+
+        zeroScalarData.push(0.0);
+        zeroDisplacementData.push(0.0);
+        zeroDisplacementData.push(0.0);
+        zeroDisplacementData.push(0.0);
+
+        radialHeightData.push(height);
+        radialSlopeData.push(slope);
+        radialDisplacementData.push(0.0);
+        radialDisplacementData.push(0.0);
+        radialDisplacementData.push(height);
+      }
+
+      // Static Channels
+      auxChannels.push(new AuxChannel([new AuxChannelData(0.0, radialDisplacementData)], AuxChannelDataType.Vector, "Static Radial Displacement", "Radial: Static"));
+      auxChannels.push(new AuxChannel([new AuxChannelData(1.0, radialHeightData)], AuxChannelDataType.Distance, "Static Radial Height", "Radial: Static"));
+      auxChannels.push(new AuxChannel([new AuxChannelData(1.0, radialSlopeData)], AuxChannelDataType.Scalar, "Static Radial Slope", "Radial: Static"));
+
+      // Animated Channels
+      const radialDisplacementDataVector = [new AuxChannelData(0.0, zeroDisplacementData), new AuxChannelData(1.0, radialDisplacementData), new AuxChannelData(2.0, zeroDisplacementData)];
+      const radialHeightDataVector = [new AuxChannelData(0.0, zeroScalarData), new AuxChannelData(1.0, radialHeightData), new AuxChannelData(2.0, zeroScalarData)];
+      const radialSlopeDataVector = [new AuxChannelData(0.0, zeroScalarData), new AuxChannelData(1.0, radialSlopeData), new AuxChannelData(2.0, zeroScalarData)];
+
+      auxChannels.push(new AuxChannel(radialDisplacementDataVector, AuxChannelDataType.Vector, "Animated Radial Displacement", "Radial: Time"));
+      auxChannels.push(new AuxChannel(radialHeightDataVector, AuxChannelDataType.Distance, "Animated Radial Height", "Radial: Time"));
+      auxChannels.push(new AuxChannel(radialSlopeDataVector, AuxChannelDataType.Scalar, "Animated Radial Slope", "Radial: Time"));
+
+      // Create linear waves -- 10 separate frames
+      const waveHeight = radius / 20.0;
+      const waveLength = radius / 2.0;
+      const frameCount = 10;
+      const linearDisplacementDataVector = [], linearHeightDataVector = [], linearSlopeDataVector = [];
+
+      for (let i = 0; i < frameCount; i++) {
+        const fraction = i / (frameCount - 1);
+        const waveCenter = waveLength * fraction;
+        const linearHeightData = [], linearSlopeData = [], linearDisplacementData = [];
+
+        for (let j = 0; j < polyface.data.point.length; j++) {
+          const point = polyface.data.point.getPoint3dAtUncheckedPointIndex(j);
+          const theta = Angle.pi2Radians * (point.x - waveCenter) / waveLength;
+          const height = waveHeight * Math.sin(theta);
+          const slope = Math.abs(Math.cos(theta));
+
+          linearHeightData.push(height);
+          linearSlopeData.push(slope);
+          linearDisplacementData.push(0.0);
+          linearDisplacementData.push(0.0);
+          linearDisplacementData.push(height);
+        }
+        linearDisplacementDataVector.push(new AuxChannelData(i, linearDisplacementData));
+        linearHeightDataVector.push(new AuxChannelData(i, linearHeightData));
+        linearSlopeDataVector.push(new AuxChannelData(i, linearSlopeData));
+      }
+      auxChannels.push(new AuxChannel(linearDisplacementDataVector, AuxChannelDataType.Vector, "Linear Displacement", "Linear: Time"));
+      auxChannels.push(new AuxChannel(linearHeightDataVector, AuxChannelDataType.Distance, "Linear Height", "Linear: Time"));
+      auxChannels.push(new AuxChannel(linearSlopeDataVector, AuxChannelDataType.Scalar, "Linear Slope", "Linear: Time"));
+
+      polyface.data.auxData = new PolyfaceAuxData(auxChannels, polyface.data.pointIndex);
+      return polyface;
+    };
+
+    const dir = "PolyfaceAuxData";
+    const filename = "indexedMesh-auxData2-new";
+    const mesh = createFlatMeshWithWaves();
+    if (ck.testDefined(mesh, "created mesh with auxData")) {
+      GeometryCoreTestIO.saveGeometry(mesh, dir, filename);   // write .imjs file
+      const bytes = BentleyGeometryFlatBuffer.geometryToBytes(mesh, true);
+      if (ck.testDefined(bytes, "exported mesh to fb bytes")) {
+        const pathname = GeometryCoreTestIO.makeOutputDir(dir).concat(`/${filename}`).concat(".fb");
+        GeometryCoreTestIO.writeBytesToFile(bytes, pathname); // write .fb file
+      }
+    }
     expect(ck.getNumErrors()).equals(0);
   });
 });
