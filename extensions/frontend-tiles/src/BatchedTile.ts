b6fee40f876ed66e91d31ecce8451d1e01f271d4
= <101671244+andremig-bentley@users.noreply.github.com>
2024-05-13 08:17:44 -0600

    local commit

--- extensions/frontend-tiles/src/BatchedTile.ts
+++ extensions/frontend-tiles/src/BatchedTile.ts
@@ -127,7 +127,6 @@ export class BatchedTile extends Tile {
 
   public override get channel(): IDBTileRequestChannel {
     if (!channel) {
-      // comment 2
       channel = new IDBTileRequestChannel("itwinjs-batched-models", 20, "MX-IDB");
       IModelApp.tileAdmin.channels.add(channel);
     }
