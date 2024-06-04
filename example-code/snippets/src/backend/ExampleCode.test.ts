d5e8ec4daae1d7f67f01ecc828a48ae330ce772e
pmconne <22944042+pmconne@users.noreply.github.com>
2024-06-04 06:44:14 -0400

    Extract workspace example code snippets to separate file

--- example-code/snippets/src/backend/ExampleCode.test.ts
+++ example-code/snippets/src/backend/ExampleCode.test.ts
@@ -3,10 +3,10 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { assert, expect } from "chai";
+import { assert } from "chai";
 import { AccessToken, Guid, Id64, Id64String } from "@itwin/core-bentley";
 import { Range3d } from "@itwin/core-geometry";
-import { BisCoreSchema, BriefcaseDb, ClassRegistry, CodeService, Element, IModelHost, PhysicalModel, SettingsContainer, StandaloneDb, Subject, SettingsPriority } from "@itwin/core-backend";
+import { BisCoreSchema, BriefcaseDb, ClassRegistry, CodeService, Element, PhysicalModel, StandaloneDb, Subject } from "@itwin/core-backend";
 import { Code, CodeScopeSpec, CodeSpec, CodeSpecProperties, IModel } from "@itwin/core-common";
 import { IModelTestUtils } from "./IModelTestUtils";
 
@@ -94,77 +94,6 @@ describe("Example Code", () => {
     assert.notDeepEqual(codeSpec2Id, codeSpecId);
     // __PUBLISH_EXTRACT_END__
 
-  });
-
-  it("Settings", async () => {
-    // __PUBLISH_EXTRACT_START__ Settings.addDictionaryDefine
-    interface TemplateRsc {
-      template: {
-        name: string;
-        loadByDefault?: boolean;
-      };
-    }
-
-    const templates: TemplateRsc[] = [
-      {
-        template: {
-          name: "vertical 1",
-          loadByDefault: false,
-        },
-      },
-      {
-        template: {
-          name: "horizontal 4",
-        },
-      },
-    ];
-
-    const defaultsDict: SettingsContainer = {};
-    defaultsDict["itwin/core/default-tool"] = "select";
-    defaultsDict["itwin/samples/start/leftPane"] = true;
-    defaultsDict["myApp/tree/label"] = "distribution of work";
-    defaultsDict["myApp/tree/indent"] = 4;
-    defaultsDict["myApp/categories"] = ["category1", "lowest", "upper"];
-    defaultsDict["myApp/list/clickMode"] = "doubleClick";
-    defaultsDict["myApp/templateResources"] = templates;
-
-    // __PUBLISH_EXTRACT_END__
-
-    // __PUBLISH_EXTRACT_START__ Settings.addDictionary
-    let workspace = IModelHost.appWorkspace;
-    let settings = workspace.settings;
-    settings.addDictionary({ name: "initial values", priority: SettingsPriority.defaults }, defaultsDict);
-    let defaultTool = settings.getString("itwin/core/default-tool"); // returns "select"
-    const leftPane = settings.getBoolean("itwin/samples/start/leftPane"); // returns true
-    const categories = settings.getArray<string>("myApp/categories"); // returns ["category1", "lowest", "upper"]
-    const t1 = settings.getArray<TemplateRsc>("myApp/templateResources"); // returns copy of `templates`
-    // __PUBLISH_EXTRACT_END__
-
-    expect(defaultTool).eq(defaultsDict["itwin/core/default-tool"]);
-    expect(leftPane).eq(defaultsDict["itwin/samples/start/leftPane"]);
-    expect(categories).deep.equal(defaultsDict["myApp/categories"]);
-    expect(t1).deep.equal(templates);
-
-    // __PUBLISH_EXTRACT_START__ Settings.addITwinDictionary
-    const iTwin555: SettingsContainer = {};
-    iTwin555["itwin/core/default-tool"] = "measure";
-    iTwin555["app5/markerName"] = "arrows";
-    iTwin555["app5/markerIcon"] = "arrows.ico";
-
-    workspace = iModel.workspace;
-    settings = workspace.settings;
-    settings.addDictionary({ name: "for iTwin 555", priority: SettingsPriority.iTwin }, iTwin555);
-    defaultTool = settings.getString("itwin/core/default-tool"); // returns "measure"
-    // __PUBLISH_EXTRACT_END__
-    expect(defaultTool).eq(iTwin555["itwin/core/default-tool"]);
-
-    // __PUBLISH_EXTRACT_START__ Settings.dropITwinDictionary
-    workspace = iModel.workspace;
-    settings = workspace.settings;
-    settings.dropDictionary({ name: "for iTwin 555" });
-    defaultTool = settings.getString("itwin/core/default-tool"); // returns "select" again
-    // __PUBLISH_EXTRACT_END__
-    expect(defaultTool).eq(defaultsDict["itwin/core/default-tool"]);
   });
 
   it("CodeService", async () => {
