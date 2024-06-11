760541986c57832d54ef4f27bb04e650b1a8e5a4
Alfonso-Martello <113918602+Alfonso-Martello@users.noreply.github.com>
2024-06-11 11:21:59 -0500

    Improving descriptions with Paul's suggestions
    Co-authored-by: Paul Connelly <22944042+pmconne@users.noreply.github.com>

--- core/common/src/annotation/TextBlockLayoutResult.ts
+++ core/common/src/annotation/TextBlockLayoutResult.ts
@@ -29,11 +29,11 @@ export interface RunLayoutResult {
   characterCount: number;
   /** The offset of the RunLayoutResult from the top and left of the [[LineLayoutResult]] */
   offsetFromLine: XAndY;
-  /** The range containing the contents of the RunLayoutResult. */
+  /** Bounding box enclosing this RunLayoutResult's content. */
   range: Range2dProps;
   /** The [[TextStyleSettings]] for the run. */
   textStyle: TextStyleSettingsProps;
-  /** The justification range of the RunLayoutResult. */
+  /** Bounding box used when justifying the run. This may be smaller than [[range]]. */
   justificationRange?: Range2dProps;
   /** The range containing the contents of the [[FractionRun]]'s numerator. */
   numeratorRange?: Range2dProps;
@@ -52,7 +52,7 @@ export interface LineLayoutResult {
   runs: RunLayoutResult[];
   /** The range containing the contents of the line. */
   range: Range2dProps;
-  /** The justification range of the line. */
+  /** Bounding box used when justifying the line. This may be smaller than [[range]]. */
   justificationRange: Range2dProps;
   /** The offset of the line from the top and left of the [[TextBlock]]. */
   offsetFromDocument: XAndY;
@@ -60,6 +60,7 @@ export interface LineLayoutResult {
 
 /**
  * Represents the result of laying out a [[TextBlock]]'s contents into a series of lines containing runs.
+ * @see [computeLayoutTextBlockResult]($backend) to lay out a `TextBlock`.
  * @beta
  */
 export interface TextBlockLayoutResult {
