e008b0e6547f7e37ee57f2316896546356567c77
christophermlawson <32881725+christophermlawson@users.noreply.github.com>
2024-06-11 12:37:17 -0400

    extract api again

--- core/ecschema-editing/src/test/Editing/SchemaItems.test.ts
+++ core/ecschema-editing/src/test/Editing/SchemaItems.test.ts
@@ -1,4 +1,4 @@
-import { ECClassModifier, EntityClass, Enumeration, EnumerationProperty, PrimitiveArrayProperty, PrimitiveProperty, PrimitiveType, PropertyCategory, Schema, SchemaContext, SchemaItemKey, SchemaKey, StructClass, UnitSystem } from "@itwin/ecschema-metadata";
+import { SchemaContext, SchemaItemKey, SchemaKey, UnitSystem } from "@itwin/ecschema-metadata";
 import { expect } from "chai";
 import { SchemaContextEditor } from "../../ecschema-editing";
 import { ECEditingStatus } from "../../Editing/Exception";
@@ -6,20 +6,13 @@ import { ECEditingStatus } from "../../Editing/Exception";
 describe("Properties editing tests", () => {
   // Uses an entity class to create properties.
   let testEditor: SchemaContextEditor;
-  let testSchema: Schema;
   let testKey: SchemaKey;
   let context: SchemaContext;
-  let entityKey: SchemaItemKey;
-  let structKey: SchemaItemKey;
-  let entity: EntityClass | undefined;
 
   beforeEach(async () => {
     context = new SchemaContext();
     testEditor = new SchemaContextEditor(context);
     testKey = await testEditor.createSchema("TestSchema", "test", 1, 0, 0);
-    entityKey = await testEditor.entities.create(testKey, "testEntity", ECClassModifier.None);
-    entity = await testEditor.schemaContext.getSchemaItem(entityKey);
-    structKey = await testEditor.structs.create(testKey, "testStruct");
   });
 
   it("should successfully set SchemaItem description", async () => {
