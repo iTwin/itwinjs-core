cdd6ca1d03e5d4b4ff31e8742e5f471f0a0c13b7
pmconne <22944042+pmconne@users.noreply.github.com>
2024-06-11 18:10:27 -0400

    yield manager

--- core/bentley/src/test/YieldManager.test.ts
+++ core/bentley/src/test/YieldManager.test.ts
@@ -9,14 +9,15 @@ describe("YieldManager", () => {
   it("allowYield yields once per 'iterationsBeforeYield' iterations", async () => {
     class CountingYieldManager extends YieldManager {
       public actualYieldCount = 0;
-      protected override async actualYield() { this.actualYieldCount++; }
     }
 
     const expectedYieldTimes = 5;
     const yieldManager = new CountingYieldManager();
+    (yieldManager as any).actualYield = async () => { yieldManager.actualYieldCount++ };
     for (let i = 0; i < expectedYieldTimes * yieldManager.options.iterationsBeforeYield; ++i) {
       await yieldManager.allowYield();
     }
+
     expect(yieldManager.actualYieldCount).to.equal(expectedYieldTimes);
   });
 });
