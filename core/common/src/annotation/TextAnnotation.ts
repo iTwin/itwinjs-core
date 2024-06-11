74dc14a5fe3e96ba2208b403ba0364e69fedd22b
pmconne <22944042+pmconne@users.noreply.github.com>
2024-06-11 12:29:45 -0400

    fix doc link

--- core/common/src/annotation/TextAnnotation.ts
+++ core/common/src/annotation/TextAnnotation.ts
@@ -146,7 +146,7 @@ export class TextAnnotation {
    * The anchor point is computed as specified by this annotation's [[anchor]] setting. For example, if the text block is anchored
    * at the bottom left, then the transform will be relative to the bottom-left corner of `textBlockExtents`.
    * The text block will be rotated around the fixed anchor point according to [[orientation]], then the anchor point will be translated by [[offset]].
-   * @param textBlockDimensions The width and height of the bounding box containing the text block. You can compute this using [computeTextBlockExtents]($backend).
+   * @param boundingBox A box fully containing the [[textBlock]]. You can compute this using [computeTextBlockBoundingBox]($backend).
    * @see [[computeAnchorPoint]] to compute the transform's anchor point.
    */
   public computeTransform(boundingBox: Range2d): Transform {
