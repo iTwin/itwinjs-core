7b48798798edd967a2a38cd935abf37aa2b29864
pmconne <22944042+pmconne@users.noreply.github.com>
2024-06-12 20:33:20 -0400

    add packageDocumentation tag that should not be necessary.

--- core/backend/src/linebreak.d.ts
+++ core/backend/src/linebreak.d.ts
@@ -2,6 +2,10 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module ElementGeometry
+ */
+
 declare module "linebreak" {
   export = LineBreaker;
 
