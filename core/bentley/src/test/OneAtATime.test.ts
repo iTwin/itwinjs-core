db78ec1613fd6727bf2f8261626c5434d0e07ed4
Arun George <aruniverse@users.noreply.github.com>
2024-06-21 16:25:39 -0400

    need to look at replacing chai-as-promised

--- core/bentley/src/test/OneAtATime.test.ts
+++ core/bentley/src/test/OneAtATime.test.ts
@@ -3,17 +3,18 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
+// import * as chai from "chai";
+// import * as chaiAsPromised from "chai-as-promised";
+import { assert, describe, expect, it } from "vitest";
 import { AbandonedError, OneAtATimeAction } from "../OneAtATimeAction";
 import { BeDuration } from "../Time";
 
 /* eslint-disable @typescript-eslint/no-floating-promises */
 
-const assert = chai.assert;
-const expect = chai.expect;
+// const assert = chai.assert;
+// const expect = chai.expect;
 
-chai.use(chaiAsPromised);
+// chai.use(chaiAsPromised);
 describe("OneAtATime test", () => {
 
   it("OneAtATime", async () => {
@@ -28,16 +29,19 @@ describe("OneAtATime test", () => {
       return ++calls;
     }, "testAbandon");
 
-    expect(operation.request(200, "hello")).to.be.eventually.fulfilled; // is started immediately
-    expect(operation.request(200, "hello")).to.be.rejectedWith(AbandonedError); // becomes pending, doesn't abort previous because its already started
-    expect(operation.request(200, "hello")).to.be.rejectedWith(AbandonedError); // aborts previous, becomes pending
+    // expect(operation.request(200, "hello")).to.be.eventually.fulfilled; // is started immediately
+    // expect(operation.request(200, "hello")). ; // is started immediately
+
+    expect(operation.request(200, "hello")).rejects.with.toEqual(AbandonedError); // becomes pending, doesn't abort previous because its already started
+
+    expect(operation.request(200, "hello")).rejects.with.toEqual(AbandonedError); // aborts previous, becomes pending
     let count = await operation.request(200, "hello"); // aborts previous, becomes pending, eventually is run
     assert.equal(count, 2); // only the first and last complete
 
-    // then, just try the whole thing again
-    expect(operation.request(10, "hello")).to.be.rejectedWith(AbandonedError, "cancelled"); // try calling a function that throws
-    expect(operation.request(200, "hello")).to.be.rejectedWith(AbandonedError, "testAbandon"); // becomes pending, doesn't abort previous because its already started
-    expect(operation.request(200, "hello")).to.be.rejectedWith(AbandonedError, "testAbandon"); // aborts previous, becomes pending
+    // // then, just try the whole thing again
+    // expect(operation.request(10, "hello")).rejects.with.toEqual(AbandonedError, "cancelled"); // try calling a function that throws
+    // expect(operation.request(200, "hello")).rejects.with.toEqual(AbandonedError, "testAbandon"); // becomes pending, doesn't abort previous because its already started
+    // expect(operation.request(200, "hello")).rejects.with.toEqual(AbandonedError, "testAbandon"); // aborts previous, becomes pending
     count = await operation.request(200, "hello");
     assert.equal(count, 3);
   });
