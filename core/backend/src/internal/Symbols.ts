1f51a7a087b3cbd73d8d641a15a3d7b40995a2eb
pmconne <22944042+pmconne@users.noreply.github.com>
2024-06-27 12:30:26 -0400

    incorporate package name into symbol description

--- core/backend/src/internal/Symbols.ts
+++ core/backend/src/internal/Symbols.ts
@@ -8,7 +8,11 @@
 
 export const _implementationProhibited = Symbol.for("Implementations of this interface can only be obtained from the containing package");
 
-export const _close = Symbol.for("close_INTERNAL_ONLY_DO_NOT_USE");
-export const _elementWasCreated = Symbol.for("elementWasCreated_INTERNAL_ONLY_DO_NOT_USE");
-export const _releaseAllLocks = Symbol.for("releaseAllLocks_INTERNAL_ONLY_DO_NOT_USE");
-export const _verifyChannel = Symbol.for("verifyChannel_INTERNAL_ONLY_DO_NOT_USE");
+function sym(name: string): string {
+  return `${name}_core-backend_INTERNAL_ONLY_DO_NOT_USE`;
+}
+
+export const _close = Symbol.for(sym("close"));
+export const _elementWasCreated = Symbol.for(sym("elementWasCreated_INTERNAL_ONLY_DO_NOT_USE"));
+export const _releaseAllLocks = Symbol.for(sym("releaseAllLocks_INTERNAL_ONLY_DO_NOT_USE"));
+export const _verifyChannel = Symbol.for(sym("verifyChannel_INTERNAL_ONLY_DO_NOT_USE"));
