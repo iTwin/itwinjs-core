744e8befacac4b89cb10266dbd6e10527efea3d7
Mike Nguyen <MikeNBentley@users.noreply.github.com>
2024-06-28 11:10:31 -0400

    feat: get latest commit 5

--- .github/workflows/automation-scripts/update-changelogs.mjs
+++ .github/workflows/automation-scripts/update-changelogs.mjs
@@ -74,7 +74,7 @@ let currentBranch = await $`git branch --show-current`;
 // const versionMatch = tag.match(/release\/(\d+\.\d+\.\d+)/);
 // let commitMessage = versionMatch ? versionMatch[1] : null;
 
-const result = await $`git log --grep="^[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\b[^-]*$" -n 1 --pretty=format:%s`;
+const result = await $`git log --grep="^[0-9]+\\.[0-9]+\\.[0-9]+($|\\s.*$)" -n 1 --pretty=format:%s`;
 let commitMessage = result.stdout.trim();
 
 // remove extra null and new line characters from git cmds
