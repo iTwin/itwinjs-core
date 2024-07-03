572dca517495d1e1b7f7ccf5a9956dde8dea7297
pmconne <22944042+pmconne@users.noreply.github.com>
2024-07-03 08:55:03 -0400

    make vite happy

--- core/common/src/internal/cross-package.ts
+++ core/common/src/internal/cross-package.ts
@@ -3,5 +3,5 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-export { DecorationGeometryProps, SnapRequestProps, SnapResponseProps } from "./Snapping";
+export type { DecorationGeometryProps, SnapRequestProps, SnapResponseProps } from "./Snapping";
 
