3ee9c6e14c212d5fe263816a3cb0754653ca4105
Daniel Iborra <107669762+danieliborra@users.noreply.github.com>
2024-07-08 18:31:09 +0200

    new graphics-provider

--- extensions/frontend-tiles/src/frontend-tiles.ts
+++ extensions/frontend-tiles/src/frontend-tiles.ts
@@ -6,4 +6,10 @@
 export * from "./FrontendTiles";
 export * from "./GraphicsProvider/GraphicRepresentationProvider";
 export * from "./GraphicsProvider/GraphicsProvider";
+export * from "./graphics-provider/tileset-creators/IModelTileset";
+export * from "./graphics-provider/tileset-creators/GeoscienceTileset";
+export * from "./graphics-provider/tileset-creators/RealityModelTileset";
+export * from "./graphics-provider/url-providers/GraphicUrlProvider";
+export * from "./graphics-provider/url-providers/GeoscienceUrlProvider";
+export * from "./graphics-provider/url-providers/IModelUrlProvider";
 
