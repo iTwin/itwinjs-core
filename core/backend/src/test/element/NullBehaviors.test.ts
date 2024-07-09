e410337ff6c96b6dcc677e1c9a7e8e8db0cc33fc
Sarunas.Griskus <86420873+6ar8nas@users.noreply.github.com>
2024-07-09 15:27:25 +0300

    Update the test fixture to be more multi-use applicable

--- core/backend/src/test/element/NullBehaviors.test.ts
+++ core/backend/src/test/element/NullBehaviors.test.ts
@@ -4,21 +4,24 @@
 *--------------------------------------------------------------------------------------------*/
 import { assert, expect } from "chai";
 import { Id64, Id64String } from "@itwin/core-bentley";
-import {
-  BriefcaseIdValue, Code,  ColorDef,  GeometricElementProps, IModel,
-  SubCategoryAppearance,
-} from "@itwin/core-common";
-import {   _nativeDb, IModelDb, IModelJsFs, SnapshotDb, SpatialCategory } from "../../core-backend";
+import { Code, ColorDef, GeometricElementProps, IModel, SubCategoryAppearance } from "@itwin/core-common";
+import { IModelDb, IModelJsFs, SnapshotDb, SpatialCategory } from "../../core-backend";
 import { IModelTestUtils } from "../IModelTestUtils";
 
+interface TestSchemaLocation {
+  city: string;
+  zip: number;
+}
+
 interface TestElement extends GeometricElementProps {
-  addresses: [null, {city: "Pune", zip: 28}];
+  addresses: TestSchemaLocation[];
+  favoriteNumbers: number[];
 }
 
 function initElemProps( _iModelName: IModelDb, modId: Id64String, catId: Id64String, autoHandledProp: any): GeometricElementProps {
   // Create props
   const elementProps: GeometricElementProps = {
-    classFullName: "Test:Foo",
+    classFullName: "Test:TestClass",
     model: modId,
     category: catId,
     code: Code.createEmpty(),
@@ -28,36 +31,33 @@ function initElemProps( _iModelName: IModelDb, modId: Id64String, catId: Id64Str
   return elementProps;
 }
 
-describe("Insert Null elements in Struct Array, and ensure they are returned while querying rows", () => {
+describe("Various ECProperties null behavior handling cases test fixture", () => {
   const testSchema = `<?xml version="1.0" encoding="UTF-8"?>
-  <ECSchema schemaName="Test" alias="test" version="01.00.00" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.1">
-  <ECSchemaReference name="BisCore" version="01.00.04" alias="bis"/>
-  <ECStructClass typeName="Location" modifier="Sealed">
-    <ECProperty propertyName="City" typeName="string"/>
-    <ECProperty propertyName="Zip" typeName="int"/>
-  </ECStructClass>
-  <ECEntityClass typeName="Foo" modifier="Sealed">
-  <BaseClass>bis:PhysicalElement</BaseClass>
-    <ECArrayProperty propertyName="I_Array" typeName="int"/>
-    <ECArrayProperty propertyName="Dt_Array" typeName="dateTime"/>
-    <ECStructArrayProperty propertyName="Addresses" typeName="Location"/>
-  </ECEntityClass>
+  <ECSchema schemaName="TestSchema" alias="test" version="01.00.00" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.1">
+    <ECSchemaReference name="BisCore" version="01.00.04" alias="bis"/>
+    <ECStructClass typeName="Location" modifier="None">
+      <ECProperty propertyName="City" typeName="string"/>
+      <ECProperty propertyName="Zip" typeName="int"/>
+    </ECStructClass>
+    <ECEntityClass typeName="TestClass" modifier="None">
+      <BaseClass>bis:PhysicalElement</BaseClass>
+      <ECArrayProperty propertyName="FavoriteNumbers" typeName="int"/>
+      <ECStructArrayProperty propertyName="Addresses" typeName="Location"/>
+    </ECEntityClass>
   </ECSchema>`;
 
-  const schemaFileName = "NullStructElementTest.01.00.00.xml";
-  const iModelFileName = "NullStructElementTest.bim";
-  const categoryName = "NullStructElement";
-  const subDirName = "NullStructElement";
+  const schemaFileName = "NullBehaviorsTest.01.00.00.xml";
+  const iModelFileName = "NullBehaviorsTest.bim";
+  const subDirName = "NullBehaviors";
   const iModelPath = IModelTestUtils.prepareOutputFile(subDirName, iModelFileName);
+  const categoryName = "NullBehaviorsCategory";
 
   before(async () => {
-    // write schema to disk as we do not have api to import xml directly
     const testSchemaPath = IModelTestUtils.prepareOutputFile(subDirName, schemaFileName);
     IModelJsFs.writeFileSync(testSchemaPath, testSchema);
 
-    const imodel = SnapshotDb.createEmpty(iModelPath, { rootSubject: { name: "InsertNullStructArrayTest" } });
+    const imodel = SnapshotDb.createEmpty(iModelPath, { rootSubject: { name: "NullBehaviorsTest" } });
     await imodel.importSchemas([testSchemaPath]);
-    imodel[_nativeDb].resetBriefcaseId(BriefcaseIdValue.Unassigned);
     IModelTestUtils.createAndInsertPhysicalPartitionAndModel(imodel, Code.createEmpty(), true);
 
     let spatialCategoryId = SpatialCategory.queryCategoryIdByName(imodel, IModel.dictionaryId, categoryName);
@@ -69,29 +69,30 @@ describe("Insert Null elements in Struct Array, and ensure they are returned whi
     imodel.close();
   });
 
-  it("Test for struct array to contain null structs", async () => {
-    const testFileName = IModelTestUtils.prepareOutputFile(subDirName, "roundtrip_correct_data.bim");
+  it("validates arrays to contain null values", async () => {
+    const testFileName = IModelTestUtils.prepareOutputFile(subDirName, "struct_array_contain_nulls.bim");
     const imodel = IModelTestUtils.createSnapshotFromSeed(testFileName, iModelPath);
-    const spatialCategoryId = SpatialCategory.queryCategoryIdByName(imodel, IModel.dictionaryId, categoryName);
+    const spatialCategoryId = SpatialCategory.queryCategoryIdByName(imodel, IModel.dictionaryId, categoryName)!;
     const [, newModelId] = IModelTestUtils.createAndInsertPhysicalPartitionAndModel(imodel, Code.createEmpty(), true);
 
-    // create element with auto handled properties
-    const expectedValue = initElemProps( imodel, newModelId, spatialCategoryId!, {
-      addresses: [null, {city: "Pune", zip: 28}],
+    const expectedProps = initElemProps(imodel, newModelId, spatialCategoryId, {
+      addresses: [null, { city: "Pune", zip: 28 }],
+      favoriteNumbers: [1, 44, 31, null, 81, 19],
     }) as TestElement;
 
-    // insert a element
-    const geomElement = imodel.elements.createElement(expectedValue);
+    // Insert an element containing a struct array with at least one null value
+    const geomElement = imodel.elements.createElement(expectedProps);
     const id = imodel.elements.insertElement(geomElement.toJSON());
     assert.isTrue(Id64.isValidId64(id), "insert worked");
     imodel.saveChanges();
 
-    // verify inserted element properties
+    // Verify the properties of the inserted element
     const actualValue = imodel.elements.getElementProps<TestElement>(id);
     expect(actualValue.addresses.length).to.equal(2);
-    expect(actualValue.addresses[0]).to.be.empty;
+    expect(actualValue.addresses).to.equal([undefined, { city: "Pune", zip: 28 }]);
+    expect(actualValue.favoriteNumbers.length).to.equal(6);
+    expect(actualValue.favoriteNumbers).to.equal([1, 44, 31, undefined, 81, 19]);
 
     imodel.close();
   });
-
 });
