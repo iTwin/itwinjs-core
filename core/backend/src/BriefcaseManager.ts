ce8a8de76e9a6f4f79b3da538ae31f0db15b5d07
nick.tessier <22119573+nick4598@users.noreply.github.com>
2024-07-10 09:57:38 -0400

    Log the error from downloadCheckpoint just incase releaseBriefcase also thows an error.

--- core/backend/src/BriefcaseManager.ts
+++ core/backend/src/BriefcaseManager.ts
@@ -219,10 +219,15 @@ export class BriefcaseManager {
     try {
       await CheckpointManager.downloadCheckpoint({ localFile: fileName, checkpoint, onProgress: arg.onProgress });
     } catch (error: unknown) {
-      if (arg.accessToken && arg.briefcaseId === undefined)
+      if (arg.accessToken && arg.briefcaseId === undefined) {
+        Logger.logInfo(loggerCategory, `Failed to download briefcase to ${fileName}, errorMessage: ${(error as Error).message}, releasing the briefcaseId...`);
         await this.releaseBriefcase(arg.accessToken, { briefcaseId, iModelId: arg.iModelId });
+      }
       if (IModelJsFs.existsSync(fileName)) {
-        Logger.logInfo(loggerCategory, `Failed to download briefcase to ${fileName}, errorMessage: ${(error as Error).message}, deleting the file...`);
+        if (arg.accessToken && arg.briefcaseId === undefined)
+          Logger.logTrace(loggerCategory, `Deleting the file: ${fileName}...`);
+        else
+          Logger.logInfo(loggerCategory, `Failed to download briefcase to ${fileName}, errorMessage: ${(error as Error).message}, deleting the file...`);
         try {
           IModelJsFs.unlinkSync(fileName);
           Logger.logInfo(loggerCategory, `Deleted ${fileName}`);
