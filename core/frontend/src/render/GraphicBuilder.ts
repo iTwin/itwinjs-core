3cb05470ca025dc45183cebbc43093dbd0d14bb0
pmconne <22944042+pmconne@users.noreply.github.com>
2024-07-11 16:38:48 -0400

    GraphicBuilder impls GraphicAssembler

--- core/frontend/src/render/GraphicBuilder.ts
+++ core/frontend/src/render/GraphicBuilder.ts
@@ -17,6 +17,7 @@ import { RenderGraphic } from "./RenderGraphic";
 import { GraphicType } from "../common/render/GraphicType";
 import type { PickableGraphicOptions} from "../common/render/BatchOptions";
 import { GraphicPrimitive } from "../common/render/GraphicPrimitive";
+import { GraphicAssembler } from "../common/render/GraphicAssembler";
 
 /** Options for creating a [[GraphicBuilder]] used by functions like [[DecorateContext.createGraphic]] and [[RenderSystem.createGraphic]].
  * @see [[ViewportGraphicBuilderOptions]] to create a graphic builder for a [[Viewport]].
@@ -132,7 +133,7 @@ export interface CustomGraphicBuilderOptions extends GraphicBuilderOptions {
  * @public
  * @extensions
  */
-export abstract class GraphicBuilder {
+export abstract class GraphicBuilder implements GraphicAssembler {
   /** The local coordinate system transform applied to this builder's geometry.
    * @see [[GraphicBuilderOptions.placement]].
    */
