ca1abfaaa698539643799c13d2f20c22c7524e7b
pmconne <22944042+pmconne@users.noreply.github.com>
2024-07-15 07:44:41 -0400

    WIP GraphicDescriptionBuilder.finish

--- core/frontend/src/common/internal/render/GraphicDescriptionBuilderImpl.ts
+++ core/frontend/src/common/internal/render/GraphicDescriptionBuilderImpl.ts
@@ -6,16 +6,21 @@
  * @module Rendering
  */
 
-import { Transform, TransformProps } from "@itwin/core-geometry";
+import { Point3d, Range3d, Range3dProps, Transform, TransformProps } from "@itwin/core-geometry";
 import { ImdlModel, addPrimitiveTransferables } from "../../imdl/ImdlModel";
-import { ComputeGraphicDescriptionChordToleranceArgs, GraphicDescription, GraphicDescriptionBuilder, GraphicDescriptionBuilderOptions } from "../../render/GraphicDescriptionBuilder";
+import { ComputeGraphicDescriptionChordToleranceArgs, FinishGraphicDescriptionArgs, GraphicDescription, GraphicDescriptionBuilder, GraphicDescriptionBuilderOptions } from "../../render/GraphicDescriptionBuilder";
 import { GraphicType } from "../../render/GraphicType";
 import { GraphicAssembler } from "../../render/GraphicAssembler";
+import { PackedFeatureTable } from "@itwin/core-common";
 
 export interface GraphicDescriptionImpl extends GraphicDescription {
   type: GraphicType;
   primitives: ImdlModel.Primitive[];
   transform?: TransformProps;
+  batch?: {
+    featureTable: ImdlModel.FeatureTable;
+    range: Range3dProps;
+  };
 }
 
 export class GraphicDescriptionBuilderImpl extends GraphicAssembler implements GraphicDescriptionBuilder {
@@ -33,9 +38,42 @@ export class GraphicDescriptionBuilderImpl extends GraphicAssembler implements G
     this._computeChordTolerance = options.computeChordTolerance;
   }
 
-  public finish(): GraphicDescription {
-    // ###TODO
-    return { primitives: [] };
+  public finish(_args: FinishGraphicDescriptionArgs): GraphicDescriptionImpl {
+    const primitives: ImdlModel.Primitive[] = [];
+    const description: GraphicDescriptionImpl = { type: this.type, primitives };
+    if (this.accum.isEmpty) {
+      return description;
+    }
+
+    let transformOrigin: Point3d | undefined;
+
+    const tolerance = this._computeChordTolerance({ builder: this, computeRange: () => this.accum.geometries.computeRange() });
+    const meshes = this.accum.toMeshes(this, tolerance, this.pickable);
+    if (meshes.length === 0) {
+      return description;
+    }
+
+    let featureTable = this.pickable && meshes.features?.anyDefined ? meshes.features : undefined;
+    if (featureTable) {
+      const features = PackedFeatureTable.pack(featureTable);
+      const range = meshes.range ?? new Range3d();
+      description.batch = {
+        range: range.toJSON(),
+        featureTable: {
+          multiModel: false,
+          data: features.data,
+          numFeatures: features.numFeatures,
+          animationNodeIds: features.animationNodeIds,
+        },
+      };
+    }
+
+    this.accum.clear();
+    return {
+      type: this.type,
+      primitives,
+      transform: transformOrigin ? Transform.createTranslation(transformOrigin).toJSON() : undefined,
+    };
   }
 
   protected override resolveGradient() {
