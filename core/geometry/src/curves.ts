1ea72ab4ea122ab113a8104783c3ac5c6f9a61ab
pmconne <22944042+pmconne@users.noreply.github.com>
2024-07-18 22:51:47 -0400

    explain curves.ts

--- core/geometry/src/curves.ts
+++ core/geometry/src/curves.ts
@@ -7,6 +7,13 @@
  * @module Utility
  */
 
+/** The ./curve/ directory contains tall class hierarchies and many circular file dependencies (A.ts imports from B.ts and B.ts also imports from A.ts, possibly indirectly).
+ * This can result in run-time errors when the file defining a derived class is processed before the file containing its superclass.
+ * To prevent that, curves.ts exports the contents of files in order based on the class hierarchies they contain: base classes before derived classes.
+ * For it to work, every file in core-geometry must import other types within ./curve/ only via curves.ts - never directly.
+ * A simple explanation: https://medium.com/visual-development/how-to-fix-nasty-circular-dependency-issues-once-and-for-all-in-javascript-typescript-a04c987cf0de
+ */
+ 
 export * from "./crv/CurveExtendMode";
 export * from "./crv/CurveProcessor";
 export * from "./crv/GeometryQuery";
@@ -20,14 +27,10 @@ export * from "./crv/CurveCollection";
 export * from "./crv/Loop";
 export * from "./crv/Path";
 
-
-
 export * from "./crv/CurveTypes";
 export * from "./crv/ConstructCurveBetweenCurves";
 export * from "./crv/CoordinateXYZ";
 export * from "./crv/CurveChainWithDistanceIndex";
-
-
 
 export * from "./crv/CurveCurve";
 export * from "./crv/CurveLocationDetail";
