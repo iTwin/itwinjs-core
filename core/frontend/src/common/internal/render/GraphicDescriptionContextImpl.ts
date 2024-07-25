8401a0048b238295b20978bc1fdfa92a715912c3
pmconne <22944042+pmconne@users.noreply.github.com>
2024-07-25 06:55:15 -0400

    WorkerTexture

--- core/frontend/src/common/internal/render/GraphicDescriptionContextImpl.ts
+++ core/frontend/src/common/internal/render/GraphicDescriptionContextImpl.ts
@@ -10,7 +10,7 @@ import { TransientIdSequence, TransientIdSequenceProps, assert } from "@itwin/co
 import { _implementationProhibited } from "../Symbols";
 import { GraphicDescriptionContextProps, WorkerGraphicDescriptionContext, WorkerGraphicDescriptionContextProps, WorkerTextureParams } from "../../render/GraphicDescriptionContext";
 import { MaterialParams } from "../../render/MaterialParams";
-import { Gradient, RenderMaterial, RenderTexture } from "@itwin/core-common";
+import { Gradient, ImageBufferFormat, ImageSource, ImageSourceFormat, RenderMaterial, RenderTexture, TextureTransparency } from "@itwin/core-common";
 
 /** As part of a [[WorkerGraphicDescriptionContext]], describes constraints imposed by the [[RenderSystem]] that a [[GraphicDescriptionBuilder]] needs to know about
  * when creating a [[GraphicDescription]].
@@ -37,6 +37,80 @@ export interface GraphicDescriptionContextPropsImpl extends GraphicDescriptionCo
   resolved?: boolean;
 }
 
+export interface WorkerTextureProps {
+  key: string | Gradient.SymbProps;
+  type: RenderTexture.Type;
+  transparency?: TextureTransparency;
+  source: {
+    type: "URL";
+    url: string;
+    format?: never;
+    width?: never;
+  } | {
+    type: "ImageBuffer";
+    data: Uint8Array;
+    format: ImageBufferFormat;
+    width: number;
+  } | {
+    type: "ImageSource";
+    data: Uint8Array | string;
+    format: ImageSourceFormat;
+    width?: never;
+    url?: never;
+  }
+}
+
+class WorkerTexture extends RenderTexture {
+  public readonly key: string | Gradient.Symb;
+  public readonly params: WorkerTextureParams;
+
+  public constructor(key: string | Gradient.Symb, params: WorkerTextureParams) {
+    super(params.type ?? RenderTexture.Type.Normal);
+    this.key = typeof key === "string" ? key : key.clone();
+    this.params = {
+      type: params.type,
+      source: params.source,
+      transparency: params.transparency,
+    };
+  }
+
+  public override dispose() { }
+  public override get bytesUsed() { return 0; } // doesn't matter, nobody's calling this.
+
+  public toProps(xfer: Set<Transferable>): WorkerTextureProps {
+    const key = typeof this.key === "string" ? this.key : this.key.toJSON();
+    let source;
+    if (this.params.source instanceof URL) {
+      source = { type: "URL" as const, url: this.params.source.toString() };
+    } else if (this.params.source instanceof ImageSource) {
+      if (typeof this.params.source.data !== "string") {
+        xfer.add(this.params.source.data.buffer);
+      }
+      
+      source = {
+        type: "ImageSource" as const,
+        data: this.params.source.data,
+        format: this.params.source.format,
+      };
+    } else {
+      xfer.add(this.params.source.data);
+      source = {
+        type: "ImageBuffer" as const,
+        data: this.params.source.data,
+        format: this.params.source.format,
+        width: this.params.source.width,
+      };
+    }
+
+    return {
+      type: this.type,
+      key,
+      source,
+      transparency: this.params.transparency,
+    }
+  }
+}
+
 export class WorkerGraphicDescriptionContextImpl implements WorkerGraphicDescriptionContext {
   public readonly [_implementationProhibited] = undefined;
   public readonly constraints: GraphicDescriptionConstraints;
@@ -56,10 +130,12 @@ export class WorkerGraphicDescriptionContextImpl implements WorkerGraphicDescrip
     assert(undefined !== key && undefined !== params);
     throw new Error("###TODO");
   }
+
   public createTexture(key: string, params: WorkerTextureParams): RenderTexture {
-    assert(undefined !== key && undefined !== params);
-    throw new Error("###TODO");
+    // ###TODO cache it after verifying a texture with same key does not already exist.
+    return new WorkerTexture(key, params);
   }
+
   public createGradientTexture(gradient: Gradient.Symb): RenderTexture {
     assert(undefined !== gradient);
     throw new Error("###TODO");
