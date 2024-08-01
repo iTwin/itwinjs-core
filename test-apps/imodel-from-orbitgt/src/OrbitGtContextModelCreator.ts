2a91e309e936bac05e280d3c1a9ce02133e9326f
MarcBedard8 <31048177+MarcBedard8@users.noreply.github.com>
2024-08-01 14:10:42 -0400

    Update test-apps/imodel-from-orbitgt/src/OrbitGtContextModelCreator.ts
    Co-authored-by: Paul Connelly <22944042+pmconne@users.noreply.github.com>

--- test-apps/imodel-from-orbitgt/src/OrbitGtContextModelCreator.ts
+++ test-apps/imodel-from-orbitgt/src/OrbitGtContextModelCreator.ts
@@ -66,7 +66,6 @@ export class OrbitGtContextIModelCreator {
       const bounds = fileReader.getFileBounds();
       let worldRange = Range3d.createXYZXYZ(bounds.getMinX(), bounds.getMinY(), bounds.getMinZ(), bounds.getMaxX(), bounds.getMaxY(), bounds.getMaxZ());
       let geoLocated = false;
-      // Fix https://dev.azure.com/bentleycs/SEG/_workitems/edit/1491612
       // the CRS 9300 is not defined in the CRS registry database, so we cannot use CRSManager
       // Check to isGeographicCRS and isProjectedCRS are both going to return false in that case and we will fallback to
       // use un-georeferenced code path.
