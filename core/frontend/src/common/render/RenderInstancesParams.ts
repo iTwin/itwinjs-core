b6c922fcc09f9388ce31d3d660b77eb3df698dc0
pmconne <22944042+pmconne@users.noreply.github.com>
2024-08-08 18:29:52 -0400

    wip builder

--- core/frontend/src/common/render/RenderInstancesParams.ts
+++ core/frontend/src/common/render/RenderInstancesParams.ts
@@ -52,8 +52,41 @@ export interface Instance {
   symbology?: InstanceSymbology;
 }
 
+export interface CreateRenderInstancesParamsBuilderArgs {
+  modelId?: Id64String;
+}
+
 export interface RenderInstancesParamsBuilder {
   add(instance: Instance): void;
   finish(): RenderInstancesParams;
 }
 
+export namespace RenderInstancesParamsBuilder {
+  export function create(args: CreateRenderInstancesParamsBuilderArgs): RenderInstancesParamsBuilder {
+    return new Builder(args.modelId);
+  }
+}
+
+class Builder implements RenderInstancesParamsBuilder {
+  private readonly _opaque: Instance[] = []
+  private readonly _translucent: Instance[] = []
+  private readonly _modelId?: Id64String;
+  
+  public constructor(modelId?: Id64String) {
+    this._modelId = modelId;
+  }
+
+  public add(instance: Instance): void {
+    if (undefined !== instance.feature && undefined === this._modelId) {
+      throw new Error("Instanced features require a model Id to be supplied when creating the RenderInstancesParamsBuilder");
+    }
+
+    // If symbology.transparency is defined and non-zero, the instance goes in the translucent bucket.
+    const list = instance.symbology?.transparency ? this._translucent : this._opaque;
+    list.push(instance);
+  }
+
+  public finish(): RenderInstancesParams {
+    return {} as any;
+  }
+}
