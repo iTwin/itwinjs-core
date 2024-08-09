a610a7879d9c74fca6cd863548425107f9dd28ae
pmconne <22944042+pmconne@users.noreply.github.com>
2024-08-09 13:31:43 -0400

    rm old code

--- test-apps/display-test-app/src/frontend/TiledGraphics.ts
+++ test-apps/display-test-app/src/frontend/TiledGraphics.ts
@@ -3,102 +3,10 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { QueryRowFormat } from "@itwin/core-common";
 import {
-  FeatureSymbology, IModelApp, IModelConnection, SceneContext, SnapshotConnection, SpatialModelState, TiledGraphicsProvider, TileTreeReference, Tool, ViewCreator3d, Viewport, ViewState,
+  FeatureSymbology, IModelApp, IModelConnection, SceneContext, SnapshotConnection, TiledGraphicsProvider, TileTreeReference, Tool, ViewCreator3d, Viewport, ViewState,
 } from "@itwin/core-frontend";
 import { DisplayTestApp } from "./App";
-
-/*
-// A reference to a TileTree originating from a different IModelConnection than the one the user opened.
-class ExternalTreeRef extends TileTreeReference {
-  private readonly _ref: TileTreeReference;
-  private readonly _ovrs: FeatureSymbology.Overrides;
-
-  public constructor(ref: TileTreeReference, ovrs: FeatureSymbology.Overrides) {
-    super();
-    this._ref = ref;
-    this._ovrs = ovrs;
-  }
-
-  public override get castsShadows() {
-    return this._ref.castsShadows;
-  }
-
-  public get treeOwner() { return this._ref.treeOwner; }
-
-  public override addToScene(context: SceneContext): void {
-    const tree = this.treeOwner.load();
-    if (undefined === tree)
-      return;
-
-    // ###TODO transform
-    const args = this.createDrawArgs(context);
-    if (undefined === args)
-      return;
-
-    tree.draw(args);
-
-    args.graphics.symbologyOverrides = this._ovrs;
-    const branch = context.createBranch(args.graphics, args.location);
-    context.outputGraphic(branch);
-  }
-}
-
-class Provider implements TiledGraphicsProvider {
-  private readonly _refs: TileTreeReference[] = [];
-  public readonly iModel: IModelConnection;
-
-  private constructor(vp: Viewport, iModel: IModelConnection, ovrs: FeatureSymbology.Overrides) {
-    this.iModel = iModel;
-    for (const model of iModel.models) {
-      const spatial = model.asSpatialModel;
-      if (undefined !== spatial) {
-        const ref = spatial.createTileTreeReference(vp.view);
-        this._refs.push(new ExternalTreeRef(ref, ovrs));
-      }
-    }
-  }
-
-  public static async create(vp: Viewport, iModel: IModelConnection): Promise<Provider> {
-    const query = { from: SpatialModelState.classFullName, wantPrivate: false };
-    const props = await iModel.models.queryProps(query);
-
-    const modelIds = [];
-    for (const prop of props)
-      if (undefined !== prop.id)
-        modelIds.push(prop.id);
-
-    await iModel.models.load(modelIds);
-
-    // Enable all categories (and subcategories thereof)
-    const ecsql = "SELECT DISTINCT Category.Id as CategoryId from BisCore.GeometricElement3d WHERE Category.Id IN (SELECT ECInstanceId from BisCore.SpatialCategory)";
-    const catIds: string[] = [];
-    for await (const catId of iModel.createQueryReader(ecsql, undefined, { rowFormat: QueryRowFormat.UseJsPropertyNames }))
-      catIds.push(catId.categoryId);
-
-    const subcatsRequest = iModel.subcategories.load(catIds);
-    if (undefined !== subcatsRequest)
-      await subcatsRequest.promise;
-
-    // Ignore the symbology overrides defined on the viewport - instead, set up our own to draw our iModel's categories.
-    const ovrs = new FeatureSymbology.Overrides();
-    for (const catId of catIds) {
-      const subcats = iModel.subcategories.getSubCategories(catId);
-      if (undefined !== subcats)
-        for (const subcat of subcats)
-          ovrs.setVisibleSubCategory(subcat);
-    }
-
-    return new Provider(vp, iModel, ovrs);
-  }
-
-  public forEachTileTreeRef(_vp: Viewport, func: (ref: TileTreeReference) => void): void {
-    for (const ref of this._refs)
-      func(ref);
-  }
-}
-*/
 
 class Provider implements TiledGraphicsProvider {
   private readonly _view: ViewState;
