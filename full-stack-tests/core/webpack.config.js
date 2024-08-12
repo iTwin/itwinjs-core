b29ca93b5dfea23c82b6956934d31edf25ea8543
Hoang Nam Le <50554904+hl662@users.noreply.github.com>
2024-08-12 10:56:24 -0400

    address missing buffer on imports

--- full-stack-tests/core/webpack.config.js
+++ full-stack-tests/core/webpack.config.js
@@ -49,6 +49,9 @@ function createConfig(shouldInstrument) {
         zlib: require.resolve("browserify-zlib"),
       },
       alias: {
+        "@azure/storage-blob$": "@azure/storage-blob/dist-esm/storage-blob/src/index.browser.js",
+        "@azure/core-http$": "@azure/core-http/dist-esm/src/index.js",
+        "@azure/logger$": "@azure/logger/dist-esm/src/index.js",
         "supports-color$": "supports-color/browser.js"
       }
     },
