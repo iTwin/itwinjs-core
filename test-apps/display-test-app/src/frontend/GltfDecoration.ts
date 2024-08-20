d27a48c28f455c0a213273a752bead9809d35449
pmconne <22944042+pmconne@users.noreply.github.com>
2024-08-20 09:39:11 -0400

    WIP dta gltf takes number of instances.

--- test-apps/display-test-app/src/frontend/GltfDecoration.ts
+++ test-apps/display-test-app/src/frontend/GltfDecoration.ts
@@ -3,11 +3,12 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { Range3d, Transform } from "@itwin/core-geometry";
+import { Point3d, Range3d, Transform } from "@itwin/core-geometry";
 import {
-  DecorateContext, GraphicBranch, GraphicType, IModelApp, readGltfGraphics, RenderGraphic, Tool,
+  DecorateContext, GraphicBranch, GraphicType, IModelApp, IModelConnection, readGltfGraphics, readGltfTemplate, RenderGraphic, RenderInstances, RenderInstancesParamsBuilder, Tool,
 } from "@itwin/core-frontend";
 import { parseArgs } from "@itwin/frontend-devtools";
+import { Id64String } from "@itwin/core-bentley";
 
 class GltfDecoration {
   private readonly _graphic: RenderGraphic;
@@ -36,6 +37,42 @@ class GltfDecoration {
   }
 }
 
+function createInstances(numInstances: number, templateRange: Range3d, iModel: IModelConnection, modelId: Id64String): RenderInstances | undefined {
+  if (numInstances <= 1) {
+    return undefined;
+  }
+
+  const maxExtents = iModel.projectExtents.clone();
+  const projectCenter = iModel.projectExtents.center;
+  maxExtents.low.subtractInPlace(projectCenter);
+  maxExtents.high.subtractInPlace(projectCenter);
+
+  function applyRandomOffset(pos: Point3d, coord: "x" | "y" | "z"): void {
+    const r = Math.random() * 2 *maxExtents.high[coord] - projectCenter[coord];
+    pos[coord] += r;
+  }
+
+  function computeRandomPosition(): Point3d {
+    const pos = templateRange.center;
+    applyRandomOffset(pos, "x");
+    applyRandomOffset(pos, "y");
+    applyRandomOffset(pos, "z");
+    return pos;
+  }
+
+  const builder = RenderInstancesParamsBuilder.create({ modelId });
+  for (let i = 0; i < numInstances; i++) {
+    const origin = computeRandomPosition();
+    builder.add({
+      transform: Transform.createTranslation(origin),
+      feature: iModel.transientIds.getNext(),
+    });
+  }
+
+  const params = builder.finish();
+  return IModelApp.renderSystem.createRenderInstances(params);
+}
+
 /** Opens a file picker from which the user can select a glTF or glb file. Creates a decoration graphic from the glTF and
  * installs a decorator to display it at the center of the active viewport's iModel's project extents.
  */
@@ -45,10 +82,12 @@ export class GltfDecorationTool extends Tool {
   public static override get maxArgs() { return 1; }
 
   private _url?: string;
+  private _numInstances = 1;
 
   public override async parseAndRun(...inArgs: string[]) {
     const args = parseArgs(inArgs);
     this._url = args.get("u");
+    this._numInstances = args.getInteger("i") ?? 1;
     return this.run();
   }
 
@@ -86,20 +125,24 @@ export class GltfDecorationTool extends Tool {
 
       // Convert the glTF into a RenderGraphic.
       const id = iModel.transientIds.getNext();
-      let graphic = await readGltfGraphics({
+      // The modelId must be different from the pickable Id for the decoration to be selectable and hilite-able.
+      const modelId = iModel.transientIds.getNext();
+      let gltfTemplate = await readGltfTemplate({
         gltf: new Uint8Array(buffer),
         iModel,
         baseUrl: url ? new URL(url) : undefined,
         pickableOptions: {
           id,
-          // The modelId must be different from the pickable Id for the decoration to be selectable and hilite-able.
-          modelId: iModel.transientIds.getNext(),
+          modelId,
         },
       });
 
-      if (!graphic)
+      if (!gltfTemplate?.template)
         return false;
 
+      const instances = createInstances(this._numInstances, gltfTemplate.boundingBox, vp.iModel, modelId);
+      let graphic = IModelApp.renderSystem.createGraphicFromTemplate({ template: gltfTemplate.template, instances });
+      
       // Transform the graphic to the center of the project extents.
       const branch = new GraphicBranch();
       branch.add(graphic);
