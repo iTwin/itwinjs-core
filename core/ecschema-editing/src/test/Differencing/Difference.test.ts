0346bdc1c1c2de36a901c175ed47058dad22dd8e
StefanApfel <71712332+StefanApfel-Bentley@users.noreply.github.com>
2024-08-22 13:00:27 +0200

    Lint fixes & change message

--- core/ecschema-editing/src/test/Differencing/Difference.test.ts
+++ core/ecschema-editing/src/test/Differencing/Difference.test.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { Schema, SchemaContext, SchemaItemType } from "@itwin/ecschema-metadata";
+import { Schema, SchemaContext } from "@itwin/ecschema-metadata";
 import { AnySchemaDifference, getSchemaDifferences, SchemaDifferenceResult, SchemaOtherTypes } from "../../Differencing/SchemaDifference";
 import { expect } from "chai";
 
