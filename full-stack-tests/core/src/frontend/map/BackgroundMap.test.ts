b763ab73a7a197a0cfcc4397f4a41d30420d5b40
pmconne <22944042+pmconne@users.noreply.github.com>
2024-09-10 07:58:37 -0400

    initial test, works

--- full-stack-tests/core/src/frontend/map/BackgroundMap.test.ts
+++ full-stack-tests/core/src/frontend/map/BackgroundMap.test.ts
@@ -4,7 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 import { assert, expect } from "chai";
 import { Id64 } from "@itwin/core-bentley";
-import { BackgroundMapProps, BackgroundMapSettings, ColorDef } from "@itwin/core-common";
+import { BackgroundMapProps, BackgroundMapSettings, ColorDef, PlanarClipMaskMode, PlanarClipMaskPriority } from "@itwin/core-common";
 import { IModelConnection, Pixel, SnapshotConnection } from "@itwin/core-frontend";
 import { TestUtility } from "../TestUtility";
 import { testOnScreenViewport, TestViewport } from "../TestViewport";
@@ -115,6 +115,35 @@ describe("Background map (#integration)", () => {
 
       for (const test of tests)
         await expectPixelTypes(vp, test[0], test[1], test[2], test[3], test[4]);
+    });
+  });
+
+  it.only("is masked by design model", async () => {
+    await testOnScreenViewport("0x24", imodel, 100, 100, async (vp) => {
+      vp.viewFlags = vp.viewFlags.with("lighting", false).with("backgroundMap", true);
+      vp.backgroundMapSettings = BackgroundMapSettings.fromJSON({
+        planarClipMask: {
+          mode: PlanarClipMaskMode.Priority,
+          priority: PlanarClipMaskPriority.BackgroundMap,
+        },
+      });
+
+      await vp.waitForAllTilesToRender();
+      vp.invalidateRenderPlan();
+      await vp.waitForAllTilesToRender();
+
+      const cx = Math.floor(vp.viewRect.width / 2);
+      const cy = Math.floor(vp.viewRect.height / 2);
+
+      // Test what's rendered to the screen.
+      const expectColor = (x: number, y: number, expectedColor: "bg" | "map") => {
+        const actualColor = vp.readColor(x, y);
+        console.log(JSON.stringify(actualColor));
+        expect(actualColor.equalsColorDef(vp.view.backgroundColor)).to.equal("bg" === expectedColor);
+      };
+      
+      expectColor(1, 1, "map");
+      expectColor(cx, cy, "bg");
     });
   });
 });
