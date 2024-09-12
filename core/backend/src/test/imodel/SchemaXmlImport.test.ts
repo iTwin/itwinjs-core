cbef0b48e8b62234d1de8d686b0596ebe78d3070
RohitPtnkr1996 <111407262+RohitPtnkr1996@users.noreply.github.com>
2024-09-12 16:13:57 +0530

    Added tests for schema import

--- core/backend/src/test/imodel/SchemaXmlImport.test.ts
+++ core/backend/src/test/imodel/SchemaXmlImport.test.ts
@@ -41,4 +41,42 @@ describe("Schema XML Import Tests", () => {
     assert.equal(testDomainClass.baseClasses[0], PhysicalElement.classFullName);
   });
 
+  it("Schema import for newer ECXml Versions", async () => {
+    const helperFunction = async (imodelDb: SnapshotDb, xmlSchema: string[], importSchema: boolean) => {
+      try {
+        // ECObjects is expected to throw for schemas that fail to import
+        if (importSchema)
+          await imodelDb.importSchemaStrings(xmlSchema);
+        else
+          imodelDb.getSchemaProps(xmlSchema[0]);
+      } catch (error: any) {
+        return false;
+      }
+      return true;
+    };
+
+    // Incrementing major ECXml version is not supported
+    for (let testCase of [`4.1`, `5.10`]) {
+      assert(!(await helperFunction(imodel, [`<ECSchema schemaName="TestSchema" alias="ts" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.${testCase}"/>`], true)), `Schema ${testCase} import should not have succeeded.`);
+      assert(!(await helperFunction(imodel, [`TestSchema`], false)), `Schema ${testCase} test should not have succeeded.`);
+    }
+
+    // Importing a set of schemas should all fail if any one of them fails
+    {
+      const schemaXmls = [`<ECSchema schemaName="TestSchema1" alias="ts1" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.2"/>`,
+        `<ECSchema schemaName="TestSchema2" alias="ts2" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.3.8"/>`,
+        `<ECSchema schemaName="TestSchema3" alias="ts3" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.4.5"/>`];
+
+      assert(!(await helperFunction(imodel, schemaXmls, true)), `Schema import should not have succeeded.`);
+      assert(!(await helperFunction(imodel, [`TestSchema1`], false)), `Schema TestSchema1 import should not have succeeded.`);
+      assert(!(await helperFunction(imodel, [`TestSchema2`], false)), `Schema TestSchema2 import should not have succeeded.`);
+      assert(!(await helperFunction(imodel, [`TestSchema3`], false)), `Schema TestSchema3 import should not have succeeded.`);
+    }
+
+    // Schema should be imported successfully
+    for (let testCase of [`3.2`, `3.5`]) {
+      assert(await helperFunction(imodel, [`<ECSchema schemaName="TestSchema" alias="ts" version="1.0.0" xmlns="http://www.bentley.com/schemas/Bentley.ECXML.${testCase}"/>`], true), `Schema ${testCase} import should have succeeded.`);
+      assert(await helperFunction(imodel, [`TestSchema`], false), `Schema ${testCase} test should have succeeded.`);
+    }
+  });
 });
