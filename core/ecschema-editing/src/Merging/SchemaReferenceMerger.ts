0d1de030bd601d1434bb9a0a4837683f308f5450
StefanApfel <71712332+StefanApfel-Bentley@users.noreply.github.com>
2024-09-16 14:20:55 +0200

    Fixed skipped schemareference test

--- core/ecschema-editing/src/Merging/SchemaReferenceMerger.ts
+++ core/ecschema-editing/src/Merging/SchemaReferenceMerger.ts
@@ -21,21 +21,20 @@ export async function addSchemaReferences(context: SchemaMergeContext, change: S
  * @internal
  */
 export async function modifySchemaReferences(context: SchemaMergeContext, change: SchemaReferenceDifference) {
-  const referencedSchema = await locateSchema(context.editor, change.difference.name, change.difference.version);
-  const existingSchema  = (await context.targetSchema.getReference(referencedSchema.name))!;
-
-  const [older, latest] = sortSchemas(existingSchema, referencedSchema);
-  if(!latest.schemaKey.matches(older.schemaKey, SchemaMatchType.LatestWriteCompatible)) {
-    throw new Error(`Schemas references of ${referencedSchema.name} have incompatible versions: ${older.schemaKey.version} and ${latest.schemaKey.version}`);
+  const existingSchema  = await context.targetSchema.getReference(change.difference.name) as Schema;
+  const [older, latest] = sortSchemas(existingSchema.schemaKey, new SchemaKey(change.difference.name, ECVersion.fromString(change.difference.version)));
+  if(latest === existingSchema.schemaKey) {
+    return;
   }
 
-  if(latest === existingSchema) {
-    return;
+  if(!latest.matches(older, SchemaMatchType.LatestWriteCompatible)) {
+    throw new Error(`Schemas references of ${change.difference.name} have incompatible versions: ${older.version} and ${latest.version}`);
   }
 
   const index = context.targetSchema.references.findIndex((reference) => reference === existingSchema);
   context.targetSchema.references.splice(index, 1);
 
+  const referencedSchema = await locateSchema(context.editor, change.difference.name, change.difference.version);
   await context.editor.addSchemaReference(context.targetSchema.schemaKey, referencedSchema);
 }
 
@@ -61,12 +60,12 @@ async function locateSchema(editor: SchemaContextEditor, schemaName?: string, ve
 
 /**
  * Sorts the schemas and return them in order of their versions [older, latest].
- * @param left    The first schema to be added.
- * @param right   The second schema to be added.
+ * @param left    The first schema key to be added.
+ * @param right   The second schema key to be added.
  * @returns       The schemas in order.
  */
-function sortSchemas(left: Schema, right: Schema): [Schema, Schema] {
-  return left.schemaKey.compareByVersion(right.schemaKey) < 0
+function sortSchemas(left: SchemaKey, right: SchemaKey): [SchemaKey, SchemaKey] {
+  return left.compareByVersion(right) < 0
     ? [left, right]
     : [right, left];
 }
