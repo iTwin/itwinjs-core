54ca0a860327f062469693b8d3cfcf38ed361dc6
Stefan Apfel <71712332+StefanApfel-Bentley@users.noreply.github.com>
2024-09-17 16:41:20 +0200

    Schema-Editing Merger refactoring (#7162)

--- core/ecschema-editing/src/Merging/KindOfQuantityMerger.ts
+++ core/ecschema-editing/src/Merging/KindOfQuantityMerger.ts
@@ -2,59 +2,64 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { type KindOfQuantityDifference } from "../Differencing/SchemaDifference";
-import { type SchemaItemMergerHandler, updateSchemaItemFullName } from "./SchemaItemMerger";
-import { type MutableKindOfQuantity } from "../Editing/Mutable/MutableKindOfQuantity";
-import { SchemaMergeContext } from "./SchemaMerger";
+import type { KindOfQuantityDifference } from "../Differencing/SchemaDifference";
+import type { MutableKindOfQuantity } from "../Editing/Mutable/MutableKindOfQuantity";
+import type { SchemaMergeContext } from "./SchemaMerger";
+import type { SchemaItemKey } from "@itwin/ecschema-metadata";
+import { updateSchemaItemFullName } from "./Utils";
 
 /**
- * Defines a merge handler to merge KindOfQuantity schema items.
+ * Merges a new KindOfQuantity into the target schema.
  * @internal
  */
-export const kindOfQuantityMerger: SchemaItemMergerHandler<KindOfQuantityDifference> = {
-  async add(context, change) {
-    if (change.difference.persistenceUnit === undefined) {
-      throw new Error("KindOfQuantity must define persistenceUnit");
-    }
-    if (change.difference.relativeError === undefined) {
-      throw new Error("KindOfQuantity must define relativeError");
-    }
-    change.difference.persistenceUnit = await updateSchemaItemFullName(context, change.difference.persistenceUnit);
-    if(change.difference.presentationUnits) {
-      if(Array.isArray(change.difference.presentationUnits)) {
-        for(let index = 0; index < change.difference.presentationUnits.length; index++) {
-          change.difference.presentationUnits[index] = await updateOverrideFormat(context, change.difference.presentationUnits[index]);
-        }
-      } else {
-        change.difference.presentationUnits = await updateOverrideFormat(context, change.difference.presentationUnits);
+export async function addKindOfQuantity(context: SchemaMergeContext, change: KindOfQuantityDifference) {
+  if (change.difference.persistenceUnit === undefined) {
+    throw new Error("KindOfQuantity must define persistenceUnit");
+  }
+  if (change.difference.relativeError === undefined) {
+    throw new Error("KindOfQuantity must define relativeError");
+  }
+  change.difference.persistenceUnit = await updateSchemaItemFullName(context, change.difference.persistenceUnit);
+  if(change.difference.presentationUnits) {
+    if(Array.isArray(change.difference.presentationUnits)) {
+      for(let index = 0; index < change.difference.presentationUnits.length; index++) {
+        change.difference.presentationUnits[index] = await updateOverrideFormat(context, change.difference.presentationUnits[index]);
       }
+    } else {
+      change.difference.presentationUnits = await updateOverrideFormat(context, change.difference.presentationUnits);
     }
+  }
 
-    return context.editor.kindOfQuantities.createFromProps(context.targetSchemaKey, {
-      ...change.difference,
-      name: change.itemName,
-      schemaItemType: change.schemaType,
-      persistenceUnit: change.difference.persistenceUnit,
-      presentationUnits: change.difference.presentationUnits,
-      relativeError: change.difference.relativeError,
-    });
-  },
-  async modify(_context, change, itemKey, item: MutableKindOfQuantity) {
-    if(change.difference.label !== undefined) {
-      item.setDisplayLabel(change.difference.label);
-    }
-    if(change.difference.description !== undefined) {
-      item.setDescription(change.difference.description);
-    }
-    if(change.difference.relativeError !== undefined) {
-      item.setRelativeError(change.difference.relativeError);
-    }
-    if(change.difference.persistenceUnit !== undefined) {
-      // TODO: It should be checked if the unit is the same, but referring to the source schema.
-      throw new Error(`Changing the kind of quantity '${itemKey.name}' persistenceUnit is not supported.`);
-    }
-  },
-};
+  await context.editor.kindOfQuantities.createFromProps(context.targetSchemaKey, {
+    ...change.difference,
+    name: change.itemName,
+    schemaItemType: change.schemaType,
+    persistenceUnit: change.difference.persistenceUnit,
+    presentationUnits: change.difference.presentationUnits,
+    relativeError: change.difference.relativeError,
+  });
+}
+
+/**
+ * Merges differences to an existing KindOfQuantity in the target schema.
+ * @internal
+ */
+export async function modifyKindOfQuantity(context: SchemaMergeContext, change: KindOfQuantityDifference, itemKey: SchemaItemKey) {
+  const kindOfQuantity = await context.targetSchema.lookupItem(itemKey) as MutableKindOfQuantity;
+  if(change.difference.label !== undefined) {
+    await context.editor.kindOfQuantities.setDisplayLabel(itemKey, change.difference.label);
+  }
+  if(change.difference.description !== undefined) {
+    await context.editor.kindOfQuantities.setDescription(itemKey, change.difference.description);
+  }
+  if(change.difference.relativeError !== undefined) {
+    kindOfQuantity.setRelativeError(change.difference.relativeError);
+  }
+  if(change.difference.persistenceUnit !== undefined) {
+    // TODO: It should be checked if the unit is the same, but referring to the source schema.
+    throw new Error(`Changing the kind of quantity '${itemKey.name}' persistenceUnit is not supported.`);
+  }
+}
 
 async function updateOverrideFormat(context: SchemaMergeContext, formatString: string) {
   // https://www.itwinjs.org/v1/bis/ec/kindofquantity/#format-string
