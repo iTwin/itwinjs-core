b935cac71a3d7a4df6a322cddc807fc776810a46
pmconne <22944042+pmconne@users.noreply.github.com>
2024-09-17 21:09:17 -0400

    Revert pickable surface test, it's in a separate PR

--- core/frontend/src/test/render/webgl/PickableDecorations.test.ts
+++ core/frontend/src/test/render/webgl/PickableDecorations.test.ts
@@ -12,19 +12,15 @@ import { readUniquePixelData, testBlankViewport } from "../../openBlankViewport"
 import { GraphicType } from "../../../common/render/GraphicType";
 
 describe("Pickable decorations", () => {
-  type DecorationType = "square" | "point";
-
   class Decorator {
     private _type = GraphicType.Scene;
     private _curId = "SetMeBeforeTesting";
     private _x = 1;
     private _y = 1;
-    private _decType: DecorationType = "point";
 
-    public test(vp: Viewport, type: GraphicType, decType: DecorationType, expectPickable = true): void {
+    public test(vp: Viewport, type: GraphicType, expectPickable = true): void {
       this._type = type;
       this._curId = vp.iModel.transientIds.getNext();
-      this._decType = decType;
       this._x++;
       this._y++;
 
@@ -41,19 +37,7 @@ describe("Pickable decorations", () => {
         context.viewport.viewToWorld(pt, pt);
 
       const builder = context.createGraphicBuilder(this._type, undefined, this._curId);
-      if (this._decType === "point") {
-        builder.addPointString([pt]);
-      } else {
-        const pts = [
-          pt,
-          new Point3d(pt.x + 2, pt.y, 0),
-          new Point3d(pt.x + 2, pt.y + 2, 0),
-          new Point3d(pt.x, pt.y + 2, 0),
-          pt.clone(),
-        ];
-        builder.addShape(pts);
-      }
-
+      builder.addPointString([pt]);
       context.addDecorationFromBuilder(builder);
     }
   }
@@ -69,21 +53,12 @@ describe("Pickable decorations", () => {
     await IModelApp.shutdown();
   });
 
-  it("world and overlay point decorations are pickable", () => {
+  it("world and overlay decorations are pickable", () => {
     testBlankViewport((vp) => {
-      decorator.test(vp, GraphicType.Scene, "point");
-      decorator.test(vp, GraphicType.WorldDecoration, "point");
-      decorator.test(vp, GraphicType.WorldOverlay, "point");
-      decorator.test(vp, GraphicType.ViewOverlay, "point");
-    });
-  });
-
-  it("world and overlay surface decorations are pickable", () => {
-    testBlankViewport((vp) => {
-      decorator.test(vp, GraphicType.Scene, "square");
-      decorator.test(vp, GraphicType.WorldDecoration, "square");
-      decorator.test(vp, GraphicType.WorldOverlay, "square");
-      decorator.test(vp, GraphicType.ViewOverlay, "square");
+      decorator.test(vp, GraphicType.Scene);
+      decorator.test(vp, GraphicType.WorldDecoration);
+      decorator.test(vp, GraphicType.WorldOverlay);
+      decorator.test(vp, GraphicType.ViewOverlay);
     });
   });
 
@@ -91,8 +66,7 @@ describe("Pickable decorations", () => {
   // Until we have some use case for making it pickable, we won't complicate our display code to support that.
   it("view background is not pickable", () => {
     testBlankViewport((vp) => {
-      decorator.test(vp, GraphicType.ViewBackground, "point", false);
-      decorator.test(vp, GraphicType.ViewBackground, "square", false);
+      decorator.test(vp, GraphicType.ViewBackground, false);
     });
   });
 });
