b529fa7cda59ef9cc9d26995637b4c175b422af8
pmconne <22944042+pmconne@users.noreply.github.com>
2024-10-01 08:39:46 -0400

    create proxy tree refs so we can apply a transform.

--- test-apps/display-test-app/src/frontend/TiledGraphics.ts
+++ test-apps/display-test-app/src/frontend/TiledGraphics.ts
@@ -5,47 +5,56 @@
 
 import {
   BriefcaseConnection,
-  FeatureSymbology, IModelApp, IModelConnection, SceneContext, TiledGraphicsProvider, TileTreeReference, Tool, ViewCreator3d, Viewport, ViewState,
+  FeatureSymbology, HitDetail, IModelApp, IModelConnection, TiledGraphicsProvider, TileTreeReference, Tool, ViewCreator3d, Viewport, ViewState,
 } from "@itwin/core-frontend";
 import { DisplayTestApp } from "./App";
+import { Transform } from "@itwin/core-geometry";
+
+class Reference extends TileTreeReference {
+  private readonly _ref: TileTreeReference;
+  private readonly _provider: Provider;
+
+  public constructor(ref: TileTreeReference, provider: Provider) {
+    super();
+    this._ref = ref;
+    this._provider = provider;
+  }
+
+  public override get castsShadows() { return this._ref.castsShadows; }
+  public override get treeOwner() { return this._ref.treeOwner; }
+  public override async getToolTip(hit: HitDetail) { return this._ref.getToolTip(hit); }
+  public override canSupplyToolTip(hit: HitDetail) { return this._ref.canSupplyToolTip(hit); }
+  public override getToolTipPromise(hit: HitDetail) { return this._ref.getToolTipPromise(hit); }
+
+  protected override getSymbologyOverrides() { return this._provider.ovrs; }
+  protected override computeTransform(): Transform {
+    // ###TODO
+    return Transform.createIdentity();
+  }
+}
 
 class Provider implements TiledGraphicsProvider {
-  private readonly _view: ViewState;
-  private readonly _ovrs: FeatureSymbology.Overrides;
-
-  public get iModel() { return this._view.iModel; }
+  public readonly iModel: IModelConnection;
+  public readonly ovrs: FeatureSymbology.Overrides;
+  private readonly _refs: Reference[];
 
   private constructor(view: ViewState) {
-    this._view = view;
+    this.iModel = view.iModel;
 
     // These overrides ensure that all of the categories and subcategories in the secondary iModel are displayed.
     // Any symbology overrides applied to the viewport are ignored.
-    this._ovrs = new FeatureSymbology.Overrides(view);
+    this.ovrs = new FeatureSymbology.Overrides(view);
+
+    this._refs = [];
+    view.forEachModelTreeRef((ref) => {
+      this._refs.push(new Reference(ref, this));
+    });
   }
 
   public forEachTileTreeRef(_vp: Viewport, func: (ref: TileTreeReference) => void): void {
-    this._view.forEachTileTreeRef(func);
-  }
-
-  public addToScene(context: SceneContext): void {
-    // We only need to override this so that we can apply our symbology overrides.
-    this._view.forEachTileTreeRef((ref) => {
-      const tree = ref.treeOwner.load();
-      if (!tree) {
-        return;
-      }
-
-      const args = ref.createDrawArgs(context);
-      if (!args) {
-        return;
-      }
-
-      tree.draw(args);
-
-      args.graphics.symbologyOverrides = this._ovrs;
-      const branch = context.createBranch(args.graphics, args.location);
-      context.outputGraphic(branch);
-    });
+    for (const ref of this._refs) {
+      func(ref);
+    }
   }
 
   public static async create(iModel: IModelConnection): Promise<Provider> {
