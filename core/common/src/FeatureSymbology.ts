ecaf07afb73fa4eaaff55415984b688834836fd9
pmconne <22944042+pmconne@users.noreply.github.com>
2024-10-08 16:59:34 -0400

    add line transparency+rgb to FeatureAppearance.

--- core/common/src/FeatureSymbology.ts
+++ core/common/src/FeatureSymbology.ts
@@ -33,10 +33,14 @@ function copyIdSetToUint32Set(dst: Id64.Uint32Set, src: Iterable<string>): void 
 export interface FeatureAppearanceProps {
   /** See [[FeatureAppearance.rgb]]. */
   rgb?: RgbColorProps;
+  /** See [[FeatureAppearance.lineRgb]]. */
+  lineRgb?: RgbColorProps | false;
   /** See [[FeatureAppearance.weight]]. */
   weight?: number;
   /** See [[FeatureAppearance.transparency]]. */
   transparency?: number;
+  /** See [[FeatureAppearance.lineTransparency]]. */
+  lineTransparency?: number | false;
   /** See [[FeatureAppearance.viewDependentTransparency]]. */
   viewDependentTransparency?: true;
   /** See [[FeatureAppearance.linePixels]]. */
@@ -49,6 +53,75 @@ export interface FeatureAppearanceProps {
   emphasized?: true;
 }
 
+function propsMatchDefaults(props: FeatureAppearanceProps): boolean {
+  return !props.rgb && !props.lineRgb
+    && undefined === props.weight && undefined === props.linePixels
+    && undefined === props.transparency && undefined === props.lineTransparency
+    && !props.ignoresMaterial && !props.nonLocatable && !props.emphasized;
+}
+
+function equalRgb(a: RgbColor | undefined, b: RgbColor | undefined): boolean {
+  if (a === b) {
+    return true;
+  } else if (!a || !b) {
+    return false;
+  } else {
+    return a.equals(b);
+  }
+}
+
+function equalLineRgb(a: RgbColor | false | undefined, b: RgbColor | false | undefined): boolean {
+  if (a === b) {
+    return true;
+  } else if (a instanceof RgbColor && b instanceof RgbColor) {
+    return equalRgb(a, b);
+  } else {
+    assert(a === undefined || a === false);
+    assert(b === undefined || b === false);
+    return false;
+  }
+}
+
+function equalTransparency(a: number | undefined, b: number | undefined): boolean {
+  if (a === b) {
+    return true;
+  } else if (undefined === a || undefined === b) {
+    return false;
+  } else {
+    return Math.floor(a * 0xff) === Math.floor(b * 0xff);
+  }
+}
+
+function equalLineTransparency(a: number | false | undefined, b: number | false | undefined): boolean {
+  if (a === b) {
+    return true;
+  } else if (typeof a === "number" && typeof b === "number") {
+    return equalTransparency(a, b);
+  } else {
+    assert(a === undefined || a === false);
+    assert(b === undefined || b === false);
+    return false;
+  }
+}
+
+function transparencyFromJSON(transp: number | undefined): number | undefined {
+  if (undefined === transp) {
+    return undefined;
+  }
+
+  transp = Math.max(0, Math.min(transp, 1));
+  
+  // Fix up rounding errors...
+  const smallDelta = 0.0001;
+  if (1.0 - transp < smallDelta) {
+    transp = 1.0;
+  } else if (transp < smallDelta) {
+    transp = 0.0;
+  }
+
+  return transp;
+}
+
 /** Defines overrides for selected aspects of a [[Feature]]'s symbology.
  * Any member defined in the appearance overrides that aspect of symbology for all [[Feature]]s to which the appearance is applied.
  * @see [[FeatureOverrides]] to customize the appearance of multiple features.
@@ -57,12 +130,14 @@ export interface FeatureAppearanceProps {
 export class FeatureAppearance {
   /** Overrides the feature's color. */
   public readonly rgb?: RgbColor;
+  public readonly lineRgb?: RgbColor | false;
   /** The width of lines and edges in pixels as an integer in [1, 31]. */
   public readonly weight?: number;
   /** The transparency in the range [0, 1] where 0 indicates fully opaque and 1 indicates fully transparent.
    * @see [[viewDependentTransparency]] for details on how this override interacts with the [DisplayStyle]($backend).
    */
   public readonly transparency?: number;
+  public readonly lineTransparency?: number | false;
   /** The pixel pattern applied to lines and edges. */
   public readonly linePixels?: LinePixels;
   /** If true, don't apply the [[RenderMaterial]] to the feature's surfaces. */
@@ -71,9 +146,9 @@ export class FeatureAppearance {
   public readonly nonLocatable?: true;
   /** If true, the feature will be rendered using the [[Hilite.Settings]] defined by [Viewport.emphasisSettings]($frontend) to make it stand out. */
   public readonly emphasized?: true;
-  /** If true, then [[transparency]] will only be applied if [[ViewFlags.transparency]] is enabled and the current [[RenderMode]] supports transparency.
+  /** If true, then [[transparency]] and/or [[lineTransparency]] will only be applied if [[ViewFlags.transparency]] is enabled and the current [[RenderMode]] supports transparency.
    * Default: false, meaning the transparency will be applied regardless of view flags or render mode.
-   * This property has no effect if [[transparency]] is `undefined`.
+   * This property has no effect if this appearance does not override transparency.
    */
   public readonly viewDependentTransparency?: true;
 
@@ -81,24 +156,25 @@ export class FeatureAppearance {
   public static readonly defaults = new FeatureAppearance({});
 
   public static fromJSON(props?: FeatureAppearanceProps) {
-    if (undefined === props || (undefined === props.rgb && undefined === props.weight && undefined === props.transparency && undefined === props.linePixels && !props.ignoresMaterial && !props.nonLocatable && !props.emphasized))
+    if (!props || propsMatchDefaults(props)) {
       return this.defaults;
-    else
-      return new FeatureAppearance(props);
+    }
+
+    return new FeatureAppearance(props);
   }
 
   /** Create a FeatureAppearance that overrides only the RGB color.
    * @note The transparency component of the ColorDef is ignored.
    */
   public static fromRgb(color: ColorDef): FeatureAppearance {
-    return this.fromJSON({ rgb: RgbColor.fromColorDef(color) });
+    return new FeatureAppearance({ rgb: RgbColor.fromColorDef(color) });
   }
 
   /** Create a FeatureAppearance that overrides the RGB and transparency.
    * The appearance's transparency is derived from the transparency component of the ColorDef.
    */
   public static fromRgba(color: ColorDef, viewDependentTransparency = false): FeatureAppearance {
-    return this.fromJSON({
+    return new FeatureAppearance({
       rgb: RgbColor.fromColorDef(color),
       transparency: color.colors.t / 255,
       viewDependentTransparency: viewDependentTransparency ? true : undefined,
@@ -106,7 +182,7 @@ export class FeatureAppearance {
   }
   /** Create a FeatureAppearance that overrides only the transparency */
   public static fromTransparency(transparencyValue: number, viewDependent = false): FeatureAppearance {
-    return this.fromJSON({
+    return new FeatureAppearance({
       transparency: transparencyValue,
       viewDependentTransparency: viewDependent ? true : undefined,
     });
@@ -132,12 +208,29 @@ export class FeatureAppearance {
   public get overridesTransparency(): boolean { return undefined !== this.transparency; }
   public get overridesLinePixels(): boolean { return undefined !== this.linePixels; }
   public get overridesWeight(): boolean { return undefined !== this.weight; }
+
+  public getLineRgb(): RgbColor | undefined {
+    return false !== this.lineRgb ? (this.lineRgb ?? this.rgb) : undefined;
+  }
+
+  public getLineTransparency(): number | undefined {
+    return false !== this.lineTransparency ? (this.lineTransparency ?? this.transparency) : undefined;
+  }
+
   public get overridesSymbology(): boolean {
     return this.overridesRgb || this.overridesTransparency || this.overridesWeight || this.overridesLinePixels || !!this.ignoresMaterial
-      || this.emphasized || this.overridesNonLocatable;
+      || this.emphasized || this.overridesNonLocatable
+      || undefined !== this.getLineRgb() || undefined !== this.getLineTransparency();
   }
+
   public get overridesNonLocatable(): boolean { return undefined !== this.nonLocatable; }
-  public get isFullyTransparent(): boolean { return undefined !== this.transparency && this.transparency >= 1.0; }
+
+  public get isFullyTransparent(): boolean {
+    const surf = this.transparency ?? 0;
+    const line = this.getLineTransparency() ?? 0;
+    return surf >= 1 && line >= 1;
+  }
+  
   /** Returns true if any aspect of the appearance is overridden (i.e., if any member is not undefined). */
   public get anyOverridden(): boolean { return this.overridesSymbology || this.overridesNonLocatable; }
 
@@ -145,14 +238,16 @@ export class FeatureAppearance {
     if (this === other)
       return true;
 
-    return this.rgbIsEqual(other.rgb)
+    return equalRgb(this.rgb, other.rgb)
       && this.weight === other.weight
-      && this.transparencyIsEqual(other.transparency)
+      && equalTransparency(this.transparency, other.transparency)
       && this.linePixels === other.linePixels
       && this.ignoresMaterial === other.ignoresMaterial
       && this.nonLocatable === other.nonLocatable
       && this.emphasized === other.emphasized
-      && this.viewDependentTransparency === other.viewDependentTransparency;
+      && this.viewDependentTransparency === other.viewDependentTransparency
+      && equalLineTransparency(this.lineTransparency, other.lineTransparency)
+      && equalLineRgb(this.lineRgb, other.lineRgb);
   }
 
   public toJSON(): FeatureAppearanceProps {
@@ -180,6 +275,15 @@ export class FeatureAppearance {
 
     if (true === this.emphasized)
       props.emphasized = true;
+
+    if (undefined !== this.lineTransparency)
+      props.lineTransparency = this.lineTransparency;
+
+    if (this.lineRgb) {
+      props.lineRgb = this.lineRgb;
+      if (this.viewDependentTransparency)
+        props.viewDependentTransparency = true;
+    }
 
     return props;
   }
@@ -231,8 +335,12 @@ export class FeatureAppearance {
       props.nonLocatable = true;
     if (undefined === props.emphasized && this.emphasized)
       props.emphasized = true;
+    if (!props.lineRgb)
+      props.lineRgb = this.lineRgb;
+    if (undefined === props.lineTransparency)
+      props.lineTransparency = this.lineTransparency;
 
-    if (undefined !== props.transparency && this.viewDependentTransparency)
+    if (this.viewDependentTransparency && (undefined !== props.transparency || undefined !== props.lineTransparency))
       props.viewDependentTransparency = true;
 
     return FeatureAppearance.fromJSON(props);
@@ -240,47 +348,25 @@ export class FeatureAppearance {
 
   protected constructor(props: FeatureAppearanceProps) {
     this.rgb = undefined !== props.rgb ? RgbColor.fromJSON(props.rgb) : undefined;
+    this.lineRgb = typeof props.lineRgb === "object" ? RgbColor.fromJSON(props.lineRgb) : (false === props.lineRgb ? false : undefined);
+
+    this.transparency = transparencyFromJSON(props.transparency);
+    this.lineTransparency = typeof props.lineTransparency === "number" ? transparencyFromJSON(props.lineTransparency) : (false === props.lineTransparency ? false : undefined);
+
     this.weight = props.weight;
-    this.transparency = props.transparency;
     this.linePixels = props.linePixels;
+
     this.ignoresMaterial = props.ignoresMaterial;
     this.nonLocatable = props.nonLocatable;
     this.emphasized = props.emphasized;
 
-    if (undefined !== this.weight)
+    if (undefined !== this.weight) {
       this.weight = Math.max(1, Math.min(this.weight, 32));
-
-    if (undefined !== this.transparency) {
-      if (props.viewDependentTransparency)
-        this.viewDependentTransparency = true;
-
-      this.transparency = Math.max(0, Math.min(this.transparency, 1));
-
-      // Fix up rounding errors...
-      const smallDelta = 0.0001;
-      if (1.0 - this.transparency < smallDelta)
-        this.transparency = 1.0;
-      else if (this.transparency < smallDelta)
-        this.transparency = 0.0;
     }
-  }
 
-  private rgbIsEqual(rgb?: RgbColor): boolean {
-    if (undefined === this.rgb)
-      return undefined === rgb;
-    else if (undefined === rgb)
-      return false;
-    else
-      return this.rgb.equals(rgb);
-  }
-
-  private transparencyIsEqual(transp?: number): boolean {
-    if (undefined === this.transparency)
-      return undefined === transp;
-    else if (undefined === transp)
-      return false;
-    else
-      return Math.floor(this.transparency * 0xff) === Math.floor(transp * 0xff);
+    if (props.viewDependentTransparency && (undefined !== this.transparency || undefined !== this.getLineTransparency())) {
+      this.viewDependentTransparency = true;
+    }
   }
 }
 
