5d31734e5728c201ced9f4372d4666e713278cd5
Saeed Torabi <114527638+saeeedtorabi@users.noreply.github.com>
2024-11-01 15:28:25 -0400

    Minor document changes for EllipticalArcApproximation (#7313)

--- core/geometry/src/curve/internalContexts/CurveCurveCloseApproachXY.ts
+++ core/geometry/src/curve/internalContexts/CurveCurveCloseApproachXY.ts
@@ -645,9 +645,9 @@ export class CurveCurveCloseApproachXY extends RecurseToCurvesGeometryHandler {
   public allPerpendicularsArcArcBounded(arc0: Arc3d, arc1: Arc3d, reversed: boolean = false): void {
     const newtonEvaluator = new CurveCurveCloseApproachXYRRtoRRD(arc0, arc1);
     // HEURISTIC: 2 ellipses have up to 8 perpendiculars and up to 4 intersections
-    const seedDelta = 1 / 10;  // denominator 9 fails the unit test
+    const seedDelta = 1 / 10; // denominator 9 fails the unit test
     const seedStart = seedDelta / 2;
-    const newtonSearcher = new Newton2dUnboundedWithDerivative(newtonEvaluator, 100);  // observed convergence to 1.0e-11 in 49 iters
+    const newtonSearcher = new Newton2dUnboundedWithDerivative(newtonEvaluator, 100); // observed convergence to 1.0e-11 in 49 iters
     for (let seedU = seedStart; seedU < 1; seedU += seedDelta) {
       for (let seedV = seedStart; seedV < 1; seedV += seedDelta) {
         newtonSearcher.setUV(seedU, seedV);
