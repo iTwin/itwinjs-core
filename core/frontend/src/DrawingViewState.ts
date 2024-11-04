acda03353f29e2e8c53a832da44c6982f70bdeaf
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2024-11-04 10:26:56 -0500

    Trivial core-frontend API promotions. (#7318)

--- core/frontend/src/DrawingViewState.ts
+++ core/frontend/src/DrawingViewState.ts
@@ -370,7 +370,6 @@ export class DrawingViewState extends ViewState2d {
     this._attachment = dispose(this._attachment);
   }
 
-  /** @internal */
   public override async changeViewedModel(modelId: Id64String): Promise<void> {
     await super.changeViewedModel(modelId);
     const props = await this.querySectionDrawingProps();
@@ -457,7 +456,6 @@ export class DrawingViewState extends ViewState2d {
     };
   }
 
-  /** @internal */
   public override isDrawingView(): this is DrawingViewState { return true; }
 
   /** See [[ViewState.getOrigin]]. */
@@ -494,7 +492,6 @@ export class DrawingViewState extends ViewState2d {
       this._attachment.addToScene(context);
   }
 
-  /** @internal */
   public override get areAllTileTreesLoaded(): boolean {
     return super.areAllTileTreesLoaded && (!this._attachment || this._attachment.view.areAllTileTreesLoaded);
   }
