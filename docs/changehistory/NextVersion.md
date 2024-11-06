8ef5002f14d11163049bf2ff93eb12d5e2774748
Gytis Čepkauskas <98940208+GytisCepk@users.noreply.github.com>
2024-11-06 10:56:06 +0200

    Make `SnapshotConnection.openFile` IPC only (#7171)

--- docs/changehistory/NextVersion.md
+++ docs/changehistory/NextVersion.md
@@ -1,5 +1,14 @@
 ---
 publish: false
 ---
+
 # NextVersion
 
+- [Breaking Changes](#breaking-changes)
+  - [Opening connection to local snapshot requires IPC](#opening-connection-to-local-snapshot-requires-ipc)
+
+## Breaking Changes
+
+### Opening connection to local snapshot requires IPC
+
+[SnapshotConnection.openFile]($frontend) now requires applications to have set up a valid IPC communication. If you're using this API in an Electron or Mobile application, no additional action is needed as long as you call `ElectronHost.startup` or `MobileHost.startup` respectively. This API shouldn't be used in Web applications, so it has no replacement there.
--- full-stack-tests/presentation/src/rpc/PresentationRpcInterface.test.ts
+++ full-stack-tests/presentation/src/rpc/PresentationRpcInterface.test.ts
@@ -4,9 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 import { expect } from "chai";
 import { RpcManager } from "@itwin/core-common";
-import { IModelConnection, SnapshotConnection } from "@itwin/core-frontend";
+import { IModelConnection } from "@itwin/core-frontend";
 import { PresentationRpcInterface } from "@itwin/presentation-common";
 import { initialize, terminate } from "../IntegrationTests";
+import { TestIModelConnection } from "../IModelSetupUtils";
 
 describe("PresentationRpcInterface", () => {
   let imodel: IModelConnection;
@@ -14,7 +15,7 @@ describe("PresentationRpcInterface", () => {
   before(async () => {
     await initialize();
     const testIModelName: string = "assets/datasets/Properties_60InstancesWithUrl2.ibim";
-    imodel = await SnapshotConnection.openFile(testIModelName);
+    imodel = TestIModelConnection.openFile(testIModelName);
     expect(imodel).is.not.null;
   });
 
