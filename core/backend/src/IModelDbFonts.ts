618acdbb8ea906697cee3802136882842b5ff706
pmconne <22944042+pmconne@users.noreply.github.com>
2024-12-18 17:26:08 -0500

    backend API docs.

--- core/backend/src/IModelDbFonts.ts
+++ core/backend/src/IModelDbFonts.ts
@@ -10,29 +10,56 @@ import { FontFamilyDescriptor, FontId, FontProps } from "@itwin/core-common";
 import { _implementationProhibited } from "./internal/Symbols";
 import { FontFile } from "./FontFile";
 
+/** Arguments supplied to [[IModelDbFonts.embedFontFile]].
+ * @beta
+ */
 export interface EmbedFontFileArgs {
+  /** The file to embed. */
   file: FontFile;
+  /** Unless true, [[IModelDbFonts.acquireId]] will be used to ensure that every font family in [[file]] is assigned a [FontId]($common). */
   dontAllocateFontIds?: boolean;
 }
 
+/** Arguments supplied to [[IModelDbFonts.queryMappedFamilies]].
+ * @beta
+ */
 export interface QueryMappedFamiliesArgs {
+  /** If true, include families that have been assigned a [FontId]($common) but for which no [FontFace]($common)s have been embedded. */
   includeNonEmbedded?: boolean;
 }
 
+/** Provides read-write access to the [font-related information]($docs/learning/backend/Fonts.md) stored in an [[IModelDb]].
+ * @see [[IModelDb.fonts]] to access the fonts for a specific iModel.
+ * @beta
+ */
 export interface IModelDbFonts {
   /** @internal */
   readonly [_implementationProhibited]: unknown;
 
+  /** Obtain the collection of font families that have been assigned a [FontId]($common).
+   * By default, font families with no corresponding embedded font faces are omitted.
+   */
   queryMappedFamilies(args?: QueryMappedFamiliesArgs): Iterable<FontProps>;
 
-  // Note that you can embed returned FontFiles into a different iModel as long as you keep the source iModel open for the duration.
+  /** Obtain the collection of [[FontFile]]s embedded in the iModel.
+   * A FontFile obtained from one iModel can be embedded into another iModel using [[embedFontFile]], but the source iModel must remain open
+   */
   queryEmbeddedFontFiles(): Iterable<FontFile>;
 
+  /** Store a [[FontFile]] in the iModel. By default, this method also ensures a [FontId]($common) is assigned to each font family in the file. */
   embedFontFile(args: EmbedFontFileArgs): Promise<void>;
 
+  /** Returns the Id assigned to the specified font family, or `undefined` if no Id has been assigned.
+   * @see [[acquireId]] to assign an Id.
+   * @see [[findDescriptor]] to perform a reverse lookup.
+   */
   findId(descriptor: FontFamilyDescriptor): FontId | undefined;
 
+  /** Returns the font family associated with the specified Id, or `undefined` if no such mapping exists.
+   * @see [[findId]] to perform a reverse lookup.
+   */
   findDescriptor(id: FontId): FontFamilyDescriptor | undefined;
 
+  /** Look up the Id assigned to the specified font family, allocating a new Id if one has not already been assigned. */
   acquireId(descriptor: FontFamilyDescriptor): Promise<FontId>;
 }
