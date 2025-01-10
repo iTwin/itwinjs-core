08c5092bcc062e9194bcc154a11b7af35924dd35
Arun George <11051042+aruniverse@users.noreply.github.com>
2025-01-09 22:41:33 -0500

    Remove re-export of core-bentley apis in core-common  (#7531)
    Co-authored-by: Arun George <aruniverse@users.noreply.github.com>

--- core/frontend/src/BriefcaseTxns.ts
+++ core/frontend/src/BriefcaseTxns.ts
@@ -6,9 +6,9 @@
  * @module IModelConnection
  */
 
-import { BeEvent } from "@itwin/core-bentley";
+import { BeEvent, IModelStatus } from "@itwin/core-bentley";
 import {
-  ChangesetIndexAndId, EcefLocation, EcefLocationProps, GeographicCRS, GeographicCRSProps, IModelStatus, ipcAppChannels,
+  ChangesetIndexAndId, EcefLocation, EcefLocationProps, GeographicCRS, GeographicCRSProps, ipcAppChannels,
   ModelIdAndGeometryGuid, NotifyEntitiesChangedArgs, RemoveFunction, RootSubjectProps, TxnNotifications,
 } from "@itwin/core-common";
 import { Point3d, Range3d, Range3dProps, XYZProps } from "@itwin/core-geometry";
