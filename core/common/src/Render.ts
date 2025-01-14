53d865cc0102876074000c6d14451d4176578096
pmconne <22944042+pmconne@users.noreply.github.com>
2025-01-14 15:30:46 -0500

    Revert "RenderMesh.ts"
    This reverts commit c95a55eb410b2aeb73c5aee2b0cc68c022be4709.

--- core/common/src/Render.ts
+++ core/common/src/Render.ts
@@ -6,9 +6,46 @@
  * @module Rendering
  */
 
-import { OctEncodedNormalPair } from "../OctEncodedNormal";
-import { PolylineIndices } from "../RenderPolyline";
+import { OctEncodedNormalPair } from "./OctEncodedNormal";
 
+// cSpell:ignore vals
+
+/** Describes the semantics of a [PolylineArgs]($frontend).
+ * @public
+ */
+export enum PolylineTypeFlags {
+  /** Just an ordinary polyline with no special semantics. */
+  Normal = 0,
+  /** A polyline used to define the edges of a planar region. */
+  Edge = 1 << 0,
+  /** Like [[Edge]], but the edges are only displayed in [[RenderMode.Wireframe]] when the surface's fill is not displayed.
+   * [[FillFlags]] controls whether the fill is displayed.
+   */
+  Outline = 1 << 1,
+}
+
+/** Flags describing a [PolylineArgs]($frontend).
+ * @public
+ */
+export interface PolylineFlags {
+  /** If `true`, the polylines are to be drawn as individual disconnected point strings instead of as connected line strings. */
+  isDisjoint?: boolean;
+  /** If `true`, the polylines' positions are all coplanar. */
+  isPlanar?: boolean;
+  /** If `true`, the polylines' positions all have the same z coordinate. */
+  is2d?: boolean;
+  /** Default: Normal. */
+  type?: PolylineTypeFlags;
+}
+
+/** Describes the vertex indices of a single line within a [PolylineArgs]($frontend).
+ * The indices represent either a line string as a connected series of points, or a point string as a set of disconnected points, depending
+ * on the [[PolylineFlags.isDisjoint]] value of [PolylineArgs.flags]($frontend).
+ * @public
+ */
+export type PolylineIndices = number[];
+
+/** @internal */
 export class MeshPolyline {
   public readonly indices: PolylineIndices;
 
@@ -25,8 +62,14 @@ export class MeshPolyline {
   public clear() { this.indices.length = 0; }
 }
 
-export type MeshPolylineList = MeshPolyline[];
+/** @internal */
+export class MeshPolylineList extends Array<MeshPolyline> {
+  constructor(...args: MeshPolyline[]) {
+    super(...args);
+  }
+}
 
+/** @internal */
 export class MeshEdge {
   public indices = [0, 0];
 
@@ -51,14 +94,16 @@ export class MeshEdge {
   }
 }
 
+/** @internal */
 export class MeshEdges {
   public visible: MeshEdge[] = [];
   public silhouette: MeshEdge[] = [];
-  public polylines: MeshPolylineList = [];
+  public polylines: MeshPolylineList = new MeshPolylineList();
   public silhouetteNormals: OctEncodedNormalPair[] = [];
   public constructor() { }
 }
 
+/** @internal */
 export class EdgeArgs {
   public edges?: MeshEdge[];
 
@@ -75,6 +120,7 @@ export class EdgeArgs {
   public get numEdges() { return undefined !== this.edges ? this.edges.length : 0; }
 }
 
+/** @internal */
 export class SilhouetteEdgeArgs extends EdgeArgs {
   public normals?: OctEncodedNormalPair[];
 
@@ -94,6 +140,7 @@ export class SilhouetteEdgeArgs extends EdgeArgs {
   }
 }
 
+/** @internal */
 export class PolylineEdgeArgs {
   public lines?: PolylineIndices[];
 
