9e1fe6debebcf3ebaedc39a876fa76f7e46838d1
pmconne <22944042+pmconne@users.noreply.github.com>
2025-01-14 13:46:03 -0500

    Cross-package

--- core/common/src/internal/cross-package.ts
+++ core/common/src/internal/cross-package.ts
@@ -4,4 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 
 export type { DecorationGeometryProps, SnapRequestProps, SnapResponseProps } from "./Snapping";
+export {
+  EdgeArgs, MeshEdge, MeshEdges, MeshPolyline, MeshPolylineList, PolylineEdgeArgs, SilhouetteEdgeArgs
+} from "./RenderMesh";
 
