dd827490c690b72a92be4b9ed16afd7cdcce02ed
William Zhao <williamzqr1@users.noreply.github.com>
2025-02-04 15:50:52 -0600

    fixed linting issues

--- core/frontend/src/internal/scheduleScriptReference.ts
+++ core/frontend/src/internal/scheduleScriptReference.ts
@@ -1,6 +1,5 @@
 /** @internal */
 import { RenderSchedule } from "@itwin/core-common";
-import { BeEvent } from "@itwin/core-bentley";
 
 /** Type definition for internal schedule script reference */
 export type InternalScriptReference = RenderSchedule.ScriptReference | undefined;
