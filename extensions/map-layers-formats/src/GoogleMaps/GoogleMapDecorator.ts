5897c018c15b3ee170a3d19ff0c7e08f3af9a1c7
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2025-02-05 11:05:13 -0500

    Cleanup decorator code

--- extensions/map-layers-formats/src/GoogleMaps/GoogleMapDecorator.ts
+++ extensions/map-layers-formats/src/GoogleMaps/GoogleMapDecorator.ts
@@ -1,15 +1,45 @@
-import { CanvasDecoration, DecorateContext, Decorator, IconSprites, IModelApp, ScreenViewport, Sprite } from "@itwin/core-frontend";
+import { CanvasDecoration, DecorateContext, Decorator, IconSprites, IModelApp, Sprite } from "@itwin/core-frontend";
 import { Point3d } from "@itwin/core-geometry";
 import { MapTypesType } from "./GoogleMaps";
 
-// Similar to 'SprintLocation' but uses a viewport pixel position instead of world position
-class ImagePixelLocationDecoration implements CanvasDecoration {
-  private _viewport?: ScreenViewport;
+
+/** A simple decorator that show logo at the a given screen position.
+ * @internal
+ */
+class LogoDecoration implements CanvasDecoration {
   private _sprite?: Sprite;
-  private _alpha?: number;
+
+  /** The current position of the logo in view coordinates. */
   public readonly position = new Point3d();
-  // public get isActive(): boolean { return this._viewport !== undefined; }
-  private _isSpriteLoaded = false;
+
+  private _offset: Point3d|undefined;
+
+  public set offset(offset: Point3d|undefined) {
+      this._offset = offset;
+    }
+
+  /** The logo offset in view coordinates.*/
+  public get offset() {
+    return this._offset;
+  }
+
+  /** Move the logo to the lower left corner of the screen. */
+  public moveToLowerLeftCorner(context: DecorateContext) : boolean{
+    if (!this._sprite || !this._sprite.isLoaded)
+      return false;
+
+    this.position.x = this._offset?.x ?? 0;
+    this.position.y =  context.viewport.parentDiv.clientHeight - this._sprite.size.y;
+    if (this._offset?.y)
+      this.position.y -= this._offset.y;
+    return true;
+  }
+
+  /* TODO: Add other move methods as needed */
+
+  /** Indicate if the logo is loaded and ready to be drawn. */
+  public get isLoaded() { return this._sprite?.isLoaded ?? false; }
+
   public async activate(sprite: Sprite): Promise<boolean> {
     this._sprite = sprite;
     return new Promise<boolean>((resolve, _reject) => {
@@ -19,50 +49,47 @@ class ImagePixelLocationDecoration implements CanvasDecoration {
         resolve (false);
       });
     });
-
-
-  }
-
-  public deactivate() {
-
   }
 
   /** Draw this sprite onto the supplied canvas.
    * @see [[CanvasDecoration.drawDecoration]]
    */
   public drawDecoration(ctx: CanvasRenderingContext2D): void {
-    const sprite = this._sprite!;
-    if (undefined === sprite.image)
-      return;
-
-    if (undefined !== this._alpha)
-      ctx.globalAlpha = this._alpha;
-
-    ctx.drawImage(sprite.image, -sprite.offset.x, -sprite.offset.y);
+    if (this.isLoaded) {
+    // Draw image with an origin at the top left corner
+    ctx.drawImage(this._sprite!.image!, 0, 0);
+    }
   }
 
-  /** If this SpriteLocation is active and the supplied DecorateContext is for its Viewport, add the Sprite to decorations. */
   public decorate(context: DecorateContext) {
-    this._viewport = context.viewport;
-    const vpHeight = context.viewport.parentDiv.clientHeight;
-    this.position.setFrom({x: 120, y: vpHeight - 25, z: 0});
     context.addCanvasDecoration(this);
   }
 }
 
+/** A decorator that adds the Google Maps logo to the lower left corner of the screen.
+ * @internal
+*/
 export class GoogleMapsDecorator implements Decorator {
-  public readonly logo = new ImagePixelLocationDecoration();
-  private _sprite: Sprite|undefined;
-  public constructor() {
-  }
+  public readonly logo = new LogoDecoration();
 
+  /** Activate the logo with the given map type. */
   public async activate(mapType: MapTypesType): Promise<boolean> {
-    const imageName = mapType === "roadmap" ? "google_on_white_hdpi" : "google_on_non_white_hdpi";
-    this._sprite = IconSprites.getSpriteFromUrl(`${IModelApp.publicPath}images/${imageName}.png`);
-    return this.logo.activate(this._sprite);
+    // Pick the logo that is the most visible on the background map
+    const imageName = mapType === "roadmap" ?
+    "google_on_white_hdpi" :
+    "google_on_non_white_hdpi";
+
+    // We need to move the logo right after the 'i.js' button
+    this.logo.offset = new Point3d(45, 5);
+
+    return this.logo.activate(IconSprites.getSpriteFromUrl(`${IModelApp.publicPath}images/${imageName}.png`));
   };
 
+  /** Decorate implementation */
   public decorate = (context: DecorateContext) => {
+    if (!this.logo.isLoaded)
+      return;
+    this.logo.moveToLowerLeftCorner(context);
     this.logo.decorate(context);
   };
 }
