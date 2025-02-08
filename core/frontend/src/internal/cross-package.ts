66cd3abae5cff75cddc560952542e771a3bca5c3
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2025-02-07 19:25:08 -0500

    Remove internal tile APIs from core-frontend package exports (#7673)

--- core/frontend/src/internal/cross-package.ts
+++ core/frontend/src/internal/cross-package.ts
@@ -14,3 +14,38 @@ export { PerformanceMetrics } from "./render/webgl/PerformanceMetrics";
 export { type GLTimerResult, RenderDiagnostics, type RenderSystemDebugControl } from "./render/RenderSystemDebugControl";
 export { formatAnimationBranchId } from "./render/AnimationBranchState";
 export { PrimitiveVisibility, type RenderTargetDebugControl } from "./render/RenderTargetDebugControl";
+
+// Used by frontend-tiles, map-layers-formats, frontend-dev-tools
+export {
+ acquireImdlDecoder,
+ appendQueryParams,
+ ArcGisErrorCode,
+ ArcGisGeometryReaderJSON,
+ type ArcGisGetServiceJsonArgs,
+ ArcGISImageryProvider,
+ type ArcGISServiceMetadata,
+ ArcGisUtilities,
+ type ArcGisValidateSourceArgs,
+ createSpatialTileTreeReferences,
+ deflateCoordinates,
+ type FeatureAttributeDrivenSymbology,
+ FeatureGeometryBaseRenderer,
+ type FeatureGeometryRenderer,
+ FeatureGraphicsRenderer,
+ type FeatureSymbolizedRenderer,
+ type FeatureSymbologyRenderer,
+ GltfReader,
+ type GltfReaderArgs,
+ type GltfReaderResult,
+ type GraphicsGeometryRenderer,
+ ImageryMapTileTree,
+ type ImdlDecoder,
+ ImdlReader,
+ type MapLayerInfoFromTileTree,
+ MapTileTreeReference,
+ RealityModelTileUtils,
+ RealityTileLoader,
+ SpatialTileTreeReferences,
+ type WGS84Extent,
+ WmsUtilities,
+} from "../tile/internal";
