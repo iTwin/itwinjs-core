7f1d325ad0bd97e2decaf97753de37d718a95249
Gytis Cepkauskas <Gytis.Cepkauskas@bentley.com>
2025-02-13 13:08:50 +0200

    Remove unnecessary changes

--- core/common/src/BentleyError.ts
+++
@@ -1,4 +0,0 @@
-// Somewhere in Viewer dep hierarchy there is import of these types from core-common
-
-import { BentleyError, BentleyStatus } from "@itwin/core-bentley";
-export { BentleyError, BentleyStatus };
