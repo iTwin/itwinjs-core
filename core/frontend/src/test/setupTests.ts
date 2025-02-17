5ca94722285c437095873befebcffa1b2a338225
Nam Le <50554904+hl662@users.noreply.github.com>
2025-02-17 11:47:54 -0500

    Use vitest v3, Resolve GHSA-9crc-q9x8-ghqq  (#7340)
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>
    (cherry picked from commit ffa6c0f45cf618bd410b27b53e1b368f20466cbe)
    
    # Conflicts:
    #	common/config/rush/pnpm-lock.yaml
    #	core/bentley/package.json
    #	core/common/package.json
    #	core/frontend/src/test/setupTests.ts
    #	core/geometry/package.json
    #	core/quantity/package.json

--- core/frontend/src/test/setupTests.ts
+++ core/frontend/src/test/setupTests.ts
@@ -1,4 +1,10 @@
 // By importing a barrel file within a setup file, we would be disabling vitest's ability to mock modules. But it's the easiest way to avoid circular import runtime errors within vitest.
 // Link to a section covering this issue: https://vitest.dev/guide/common-errors.html#cannot-mock-mocked-file-js-because-it-is-already-loaded
+<<<<<<< HEAD
 // Not importing all of core frontend, as it's not needed. Only MockRender namespace is causing circular import runtime errors.
 import "../render/MockRender";
+=======
+import "../core-frontend";
+// Import custom matchers
+import "./setupCustomMatchers";
+>>>>>>> ffa6c0f45c (Use vitest v3, Resolve GHSA-9crc-q9x8-ghqq  (#7340))
