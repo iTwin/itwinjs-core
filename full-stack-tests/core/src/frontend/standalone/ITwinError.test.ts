793378ffca601207ac233f63c7434800c839d231
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2025-03-19 19:50:36 -0400

    verify with and without logFn

--- full-stack-tests/core/src/frontend/standalone/ITwinError.test.ts
+++ full-stack-tests/core/src/frontend/standalone/ITwinError.test.ts
@@ -20,61 +20,51 @@ if (ProcessDetector.isElectronAppFrontend) {
       const metadata = { category: "test", severity: "error" };
       const testMsg = "test message";
       const errorNumber = IModelStatus.AlreadyLoaded;
-      let caughtError = false;
-      const verify = (err: any) => {
-        caughtError = true;
-        expect(err instanceof BackendError).true;
-        expect(err.stack?.includes("core") && err.stack?.includes("backend")).true;
-        expect(err.message).equal(testMsg);
-        expect(err.errorNumber).equal(IModelStatus.AlreadyLoaded);
-        expect(err.name).equal("Already Loaded");
-        expect(err.getMetaData()).deep.equal(metadata);
+      const verify = async (logFn: boolean) => {
+        let caughtError = false;
+
+        try {
+          await coreFullStackTestIpc.throwLegacyError(errorNumber, testMsg, metadata, logFn);
+        } catch (err: any) {
+          caughtError = true;
+          expect(err instanceof BackendError).true;
+          expect(err.stack?.includes("core") && err.stack?.includes("backend")).true;
+          expect(err.message).equal(testMsg);
+          expect(err.errorNumber).equal(IModelStatus.AlreadyLoaded);
+          expect(err.name).equal("Already Loaded");
+          expect(err.getMetaData()).deep.equal(metadata);
+        }
+        expect(caughtError).true;
       }
-      try {
-        await coreFullStackTestIpc.throwLegacyError(errorNumber, testMsg, metadata, false);
-      } catch (err: any) {
-        verify(err);
-      }
-      expect(caughtError).true;
-      caughtError = false;
-      try {
-        await coreFullStackTestIpc.throwLegacyError(errorNumber, testMsg, metadata, true);
-      } catch (err: any) {
-        verify(err);
-      }
-      expect(caughtError).true;
+      await verify(false);
+      await verify(true);
     });
 
-    it("should receive ChannelError on the frontend", async () => {
+    it("should receive ChannelError", async () => {
       const sentErr = {
         message: "test message",
         channelKey: "123",
         metadata: { category: "test", severity: "error" },
       }
 
-      let caughtError = false;
-      const verify = (err: any) => {
-        caughtError = true;
-        expect(ChannelError.isError(err, "may-not-nest")).true;
-        // Even though we're on the frontend we should make sure our stack trace includes backend code.
-        expect(err.stack?.includes("core") && err.stack?.includes("backend")).true;
-        expect(err.message).equal(sentErr.message);
-        expect(err.channelKey).equal(sentErr.channelKey);
-        expect(getITwinErrorMetaData(err)).deep.equal(sentErr.metadata);
+      const verify = async (logFn: boolean) => {
+        let caughtError = false;
+
+        try {
+          await coreFullStackTestIpc.throwChannelError("may-not-nest", sentErr.message, sentErr.channelKey, sentErr.metadata, logFn);
+        } catch (err: any) {
+          caughtError = true;
+          expect(ChannelError.isError(err, "may-not-nest")).true;
+          // Even though we're on the frontend we should make sure our stack trace includes backend code.
+          expect(err.stack?.includes("core") && err.stack?.includes("backend")).true;
+          expect(err.message).equal(sentErr.message);
+          expect(err.channelKey).equal(sentErr.channelKey);
+          expect(getITwinErrorMetaData(err)).deep.equal(sentErr.metadata);
+        }
+        expect(caughtError).true;
       }
-      try {
-        await coreFullStackTestIpc.throwChannelError("may-not-nest", sentErr.message, sentErr.channelKey, sentErr.metadata, false);
-      } catch (err: any) {
-        verify(err);
-      }
-      expect(caughtError).true;
-      caughtError = false;
-      try {
-        await coreFullStackTestIpc.throwChannelError("may-not-nest", sentErr.message, sentErr.channelKey, sentErr.metadata, true);
-      } catch (err: any) {
-        verify(err);
-      }
-      expect(caughtError).true;
+      await verify(false);
+      await verify(true);
     });
 
   });
