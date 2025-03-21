585c7565ca11f19fffb0321df532abafe0ac86c4
Arun George <aruniverse@users.noreply.github.com>
2025-03-20 21:31:40 -0400

    remove deprecated apis from 3.x

--- core/backend/src/BackendHubAccess.ts
+++ core/backend/src/BackendHubAccess.ts
@@ -171,12 +171,6 @@ export interface DownloadChangesetRangeArg extends ChangesetRangeArg, DownloadPr
 }
 
 /**
- * @deprecated in 3.x. Use [[DownloadRequest]].
- * @internal
- */
-export type CheckpointArg = DownloadRequest;
-
-/**
  * Arguments to create a new iModel in iModelHub
  *  @public
  */
@@ -224,7 +218,7 @@ export interface BackendHubAccess {
    * @deprecated in 3.x. V1 checkpoints are deprecated. Download V2 checkpoint using [[V2CheckpointManager.downloadCheckpoint]].
    * @internal
    */
-  downloadV1Checkpoint: (arg: CheckpointArg) => Promise<ChangesetIndexAndId>; // eslint-disable-line @typescript-eslint/no-deprecated
+  downloadV1Checkpoint: (arg: DownloadRequest) => Promise<ChangesetIndexAndId>;
 
   /** Get the access props for a V2 checkpoint. Returns undefined if no V2 checkpoint exists. */
   queryV2Checkpoint: (arg: CheckpointProps) => Promise<V2CheckpointAccessProps | undefined>;
