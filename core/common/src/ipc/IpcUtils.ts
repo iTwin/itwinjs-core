90adc7e555280036ba97603d3b4bfcf8ea46cbf8
Augustas Grigas <Augustas.Grigas@bentley.com>
2025-03-24 13:26:32 +0200

    remove unused helper function

--- core/common/src/ipc/IpcUtils.ts
+++ core/common/src/ipc/IpcUtils.ts
@@ -9,19 +9,3 @@
 export type IpcAdapted<T> = {
   [K in keyof T]: T[K] extends (...args: infer Args) => infer R ? (key: string, ...args: Args) => R : T[K];
 };
-
-/**
- * Creates an async iterable from an IPC function that returns next iterable based on identifier
- * @param queryId Identifier of the query from which to return the next value
- * @param nextFn Function that returns the next value in the iterable based on the identifier
- * @beta
- */
-export function getIpcIterable<T>(
-  queryId: string,
-  nextFn: (queryId: string) => Promise<IteratorResult<T, void>>,
-): AsyncIterable<T> {
-  const next = async () => nextFn(queryId);
-  return {
-    [Symbol.asyncIterator]: () => ({ next })
-  };
-}
