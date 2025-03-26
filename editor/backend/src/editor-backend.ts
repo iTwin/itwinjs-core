93a4d407f9c952f5d6389844eba68db2a0281a3b
Pankhur Dhaila <pankhur94@users.noreply.github.com>
2025-03-26 16:07:16 -0600

    editor-backend esm

--- editor/backend/src/editor-backend.ts
+++ editor/backend/src/editor-backend.ts
@@ -2,8 +2,8 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-export * from "./EditCommand";
-export * from "./EditBuiltInCommand";
+export * from "./EditCommand.js";
+export * from "./EditBuiltInCommand.js";
 
 /** @docs-package-description
  * The editor-backend package contains backend functionality for editing iModels.
