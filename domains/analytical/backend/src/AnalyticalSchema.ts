c98c0925c420657e27756700bc468f5dd358ef43
Arun George <aruniverse@users.noreply.github.com>
2025-04-03 12:40:17 -0400

    try to fix domains, mocharc not complete

--- domains/analytical/backend/src/AnalyticalSchema.ts
+++ domains/analytical/backend/src/AnalyticalSchema.ts
@@ -6,7 +6,7 @@
  * @module Analytical
  */
 
-import * as path from "path";
+import * as path from "node:path";
 import { ClassRegistry, KnownLocations, Schema, Schemas } from "@itwin/core-backend";
 import * as elementsModule from "./AnalyticalElements.js";
 import * as modelsModule from "./AnalyticalModels.js";
@@ -17,6 +17,10 @@ import * as relationshipsModule from "./AnalyticalRelationships.js";
  */
 export class AnalyticalSchema extends Schema {
   public static override get schemaName(): string { return "Analytical"; }
+  /**
+   * TODO: Currently the schema file comes from a hardcoded path assuming its delivered with imodeljs-native
+   * Look to add a dependency on @bentley/analytical-schema and using require.resolve to obtain the path
+   */
   public static get schemaFilePath(): string { return path.join(KnownLocations.nativeAssetsDir, "ECSchemas", "Domain", "Analytical.ecschema.xml"); }
   public static registerSchema() {
     if (this !== Schemas.getRegisteredSchema(this.schemaName)) {
