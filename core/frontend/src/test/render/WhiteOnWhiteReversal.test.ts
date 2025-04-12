249ab5bf939b2d40cc9d549f7713fababed07a87
pmconne <22944042+pmconne@users.noreply.github.com>
2025-04-12 09:18:26 -0400

    more tests

--- core/frontend/src/test/render/WhiteOnWhiteReversal.test.ts
+++ core/frontend/src/test/render/WhiteOnWhiteReversal.test.ts
@@ -12,6 +12,7 @@ import { Viewport } from "../../Viewport";
 import { Point3d } from "@itwin/core-geometry";
 import { GraphicBuilder } from "../../render/GraphicBuilder";
 import { readUniqueColors, testBlankViewport } from "../openBlankViewport";
+import { ViewRect } from "../../common";
 
 describe("White-on-white reversal", () => {
   class TestDecorator {
@@ -45,7 +46,7 @@ describe("White-on-white reversal", () => {
     await IModelApp.shutdown();
   });
 
-  function addTriangleDecoration(context: DecorateContext, color: ColorDef, material?: RenderMaterial, fillFlags: FillFlags = FillFlags.None): void {
+  function addSquareDecoration(context: DecorateContext, color: ColorDef, material?: RenderMaterial, fillFlags: FillFlags = FillFlags.None): void {
     const builder = context.createSceneGraphicBuilder();
     const params = new GraphicParams;
     params.lineColor = params.fillColor = color;
@@ -53,7 +54,7 @@ describe("White-on-white reversal", () => {
     params.fillFlags = fillFlags;
     builder.activateGraphicParams(params);
 
-    const pts = [new Point3d(0, 0, 0), new Point3d(10, 0, 0), new Point3d(0, 10, 0), new Point3d(0, 0, 0)];
+    const pts = [new Point3d(0, 0, 0), new Point3d(10, 0, 0), new Point3d(10, 10, 0), new Point3d(0, 10, 0), new Point3d(0, 0, 0)];
     context.viewport.viewToWorldArray(pts);
     builder.addShape(pts);
     context.addDecorationFromBuilder(builder);
@@ -64,13 +65,17 @@ describe("White-on-white reversal", () => {
     return colors.sort((a, b) => a.r - b.r || a.g - b.g || a.b - b.b);
   }
 
-  function expectColors(vp: Viewport, expected: ColorDef[], bgColor: ColorDef, decorate: (context: DecorateContext) => void): void {
+  interface Options {
+    rect?: ViewRect;
+  }
+
+  function expectColors(vp: Viewport, expected: ColorDef[], bgColor: ColorDef, decorate: (context: DecorateContext) => void, options?: Options): void {
     TestDecorator.register(decorate);
     vp.displayStyle.backgroundColor = bgColor;
     vp.invalidateDecorations();
     vp.renderFrame();
 
-    const actualColors = sortColors(readUniqueColors(vp).array.map((x) => { return { r: x.r, g: x.g, b: x.b } }));
+    const actualColors = sortColors(readUniqueColors(vp, options?.rect).array.map((x) => { return { r: x.r, g: x.g, b: x.b } }));
     const expectedColors = sortColors(expected.map((x) => RgbColor.fromColorDef(x)));
     expect(actualColors).to.deep.equal(expectedColors);
 
@@ -80,11 +85,14 @@ describe("White-on-white reversal", () => {
   it("applies to white scene decorations IFF view background is white", () => {
     testBlankViewport((vp) => {
       vp.viewFlags = vp.viewFlags.copy({ renderMode: RenderMode.SmoothShade, lighting: false });
-      expectColors(vp, [ColorDef.red, ColorDef.white], ColorDef.red, (ctx) => addTriangleDecoration(ctx, ColorDef.white));
-      expectColors(vp, [ColorDef.blue, ColorDef.white], ColorDef.white, (ctx) => addTriangleDecoration(ctx, ColorDef.blue));
+      expectColors(vp, [ColorDef.red, ColorDef.white], ColorDef.red, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      expectColors(vp, [ColorDef.blue, ColorDef.white], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.blue));
 
-      expectColors(vp, [ColorDef.black, ColorDef.white], ColorDef.white, (ctx) => addTriangleDecoration(ctx, ColorDef.white));
-      expectColors(vp, [ColorDef.black], ColorDef.black, (ctx) => addTriangleDecoration(ctx, ColorDef.black));
+      expectColors(vp, [ColorDef.black, ColorDef.white], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      expectColors(vp, [ColorDef.black], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white), { rect: new ViewRect(2, 2, 8, 8) });
+
+      expectColors(vp, [ColorDef.black, ColorDef.white], ColorDef.black, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      expectColors(vp, [ColorDef.white], ColorDef.black, (ctx) => addSquareDecoration(ctx, ColorDef.white), { rect: new ViewRect(2, 2, 8, 8) });
     });
   });
 
@@ -96,7 +104,16 @@ describe("White-on-white reversal", () => {
     
   });
 
-  it("doesn't apply to surfaces if edges are displayed", () => {
+  it("applies to edges but not surfaces if edges are displayed", () => {
+    testBlankViewport((vp) => {
+      vp.viewFlags = vp.viewFlags.copy({ renderMode: RenderMode.SmoothShade, lighting: false, visibleEdges: true });
+      expectColors(vp, [ColorDef.white, ColorDef.black], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      expectColors(vp, [ColorDef.white], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white), { rect: new ViewRect(2, 2, 8, 8) });
+
+      vp.viewFlags = vp.viewFlags.copy({ renderMode: RenderMode.SmoothShade, lighting: false, visibleEdges: false });
+      expectColors(vp, [ColorDef.white, ColorDef.black], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      expectColors(vp, [ColorDef.black], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white), { rect: new ViewRect(2, 2, 8, 8) });
+    });
     
   });
 
@@ -105,6 +122,12 @@ describe("White-on-white reversal", () => {
   });
 
   it("doesn't apply to lit surfaces", () => {
-    
+    testBlankViewport((vp) => {
+      vp.viewFlags = vp.viewFlags.copy({ renderMode: RenderMode.SmoothShade, lighting: true });
+      const litWhite = ColorDef.from(177, 177, 177);
+      expectColors(vp, [ColorDef.white, litWhite], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+      vp.viewFlags = vp.viewFlags.copy({ renderMode: RenderMode.SmoothShade, lighting: false });
+      expectColors(vp, [ColorDef.black, ColorDef.white], ColorDef.white, (ctx) => addSquareDecoration(ctx, ColorDef.white));
+    });
   });
 });
