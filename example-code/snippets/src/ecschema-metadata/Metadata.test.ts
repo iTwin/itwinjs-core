628cc1b904e1e996a5540fb31f93701fa38d5c28
Robert Schili <rschili@users.noreply.github.com>
2025-05-06 11:47:43 +0200

    move metadata example from app to snippets

---
+++ example-code/snippets/src/ecschema-metadata/Metadata.test.ts
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import * as path from "path";
+import { BisCoreSchema, ClassRegistry, IModelDb, IModelHost, Schema, Schemas, SnapshotDb, SpatialCategory } from "@itwin/core-backend";
+import { IModelStatus } from "@itwin/core-bentley";
+import { ColorByName, IModelError, SubCategoryAppearance } from "@itwin/core-common";
+import { EntityClass } from "@itwin/ecschema-metadata";
+import { assert } from "chai";
+import { DbResult, Id64Set, Id64String } from "@itwin/core-bentley";
+import { ECSqlStatement, Element, IModelDb, PhysicalPartition, SnapshotDb, Subject } from "@itwin/core-backend";
+import { IModelTestUtils } from "../backend/IModelTestUtils";
+
+/** Common usage of ecschema-metadata */
+describe("Metadata examples", () => {
+  let iModel: SnapshotDb;
+
+  before(async () => {
+    iModel = IModelTestUtils.openSnapshotFromSeed("test.bim", { copyFilename: "metadata.bim" });
+  });
+
+  after(() => {
+    iModel.close();
+  });
+
+  it("iterate entities on IModelDb", async () => {
+    // __PUBLISH_EXTRACT_START__ Metadata.entitiesFromIModelDb
+    // For the sake of this example, we will assume an open IModelDb is provided. See backend docs for details on how to work with imodels.
+    // Get the schema context from the IModelDb
+    const schemaContext = iModel.schemaContext;
+
+    const bisCore = await schemaContext.getSchema(BisCoreSchema.schemaKey);
+    if (bisCore === undefined) {
+      throw new IModelError(IModelStatus.NotFound, "BisCore schema not found");
+    }
+
+    for (const entity of bisCore.getItems(EntityClass)) {
+      // console.log(`Entity: ${entity.name}`);
+
+      // Get the properties of the entity
+      const properties = await entity.getProperties();
+      // Iterate through each property
+      for (const property of properties) {
+        // Check if the property is a primitive type
+        if (property.isPrimitive()) {
+          // Do something with the primitive property
+          //console.log(`    Property: ${property.name}, Primitive Type: ${property.primitiveType}`);
+        }
+      }
+    }
+    // __PUBLISH_EXTRACT_END__
+  });
+});
+
+
+
+
+/* eslint-disable @typescript-eslint/naming-convention */
+
