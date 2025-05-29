91b48cb6f579ed6b1c79a0ce79d41a1686136d99
Liutauras Skamarakas <Liutauras.Skamarakas@bentley.com>
2025-05-29 17:31:02 +0300

    Cleaned up tests

--- full-stack-tests/core/src/frontend/standalone/CodeSpecs.test.ts
+++ full-stack-tests/core/src/frontend/standalone/CodeSpecs.test.ts
@@ -1,170 +1,167 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import { assert, expect } from "chai";
-import * as sinon from "sinon";
-import { BisCodeSpec, CodeScopeSpec, CodeSpec, CodeSpecProperties, ECSqlReader, QueryRowProxy } from "@itwin/core-common";
-import { IModelConnection, SnapshotConnection } from "@itwin/core-frontend";
+import {
+  BisCodeSpec,
+  CodeScopeSpec,
+  CodeSpec,
+  IModelError,
+} from "@itwin/core-common";
+import { IModelConnection } from "@itwin/core-frontend";
 import { TestUtility } from "../TestUtility";
+import { IModelStatus } from "@itwin/core-bentley";
+import { TestSnapshotConnection } from "../TestSnapshotConnection";
 
 describe("CodeSpecs", async () => {
   let iModel: IModelConnection;
 
   before(async () => {
     await TestUtility.startFrontend();
-    iModel = await SnapshotConnection.openFile("test.bim");
+    iModel = await TestSnapshotConnection.openFile("test.bim");
   });
 
   after(async () => {
-    if (iModel)
+    if (iModel) {
       await iModel.close();
+    }
     await TestUtility.shutdownFrontend();
   });
 
-  it("should load CodeSpecs", async () => {
-    const nullCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(BisCodeSpec.nullCodeSpec);
-    assert.equal(nullCodeSpec.scopeType, CodeScopeSpec.Type.Repository);
-    assert.equal(nullCodeSpec.scopeReq, CodeScopeSpec.ScopeRequirement.ElementId);
+  const existing = {
+    name: BisCodeSpec.subject,
+    id: "0x1f",
+  };
 
-    const subCategoryCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(BisCodeSpec.subCategory);
-    assert.equal(subCategoryCodeSpec.scopeType, CodeScopeSpec.Type.ParentElement);
-    assert.equal(subCategoryCodeSpec.scopeReq, CodeScopeSpec.ScopeRequirement.ElementId);
+  /** verify that given codespec has expected properties */
+  function verifyCodeSpec(
+    codeSpec: CodeSpec,
+    requestedIModel: IModelConnection,
+    requestedName: string,
+    requestedId: string
+  ) {
+    expect(codeSpec).to.exist;
 
-    const viewDefinitionCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(BisCodeSpec.viewDefinition);
-    assert.equal(viewDefinitionCodeSpec.scopeType, CodeScopeSpec.Type.Model);
-    assert.equal(viewDefinitionCodeSpec.scopeReq, CodeScopeSpec.ScopeRequirement.ElementId);
-  });
-});
+    expect(codeSpec.id).to.exist;
+    expect(codeSpec.id).to.equal(requestedId);
 
-describe("CodeSpecs._isCodeSpecProperties()", async () => {
-  let iModelStub: sinon.SinonStubbedInstance<IModelConnection>;
-  let ecsqlReaderStub: sinon.SinonStubbedInstance<ECSqlReader>;
+    expect(codeSpec.name).to.exist;
+    expect(codeSpec.name).to.equal(requestedName);
 
-  before(async () => {
-    await TestUtility.startFrontend();
-  });
-
-  beforeEach(() => {
-    iModelStub = sinon.createStubInstance(IModelConnection);
-    ecsqlReaderStub = sinon.createStubInstance(ECSqlReader);
-  });
-
-  after(async () => {
-    await TestUtility.shutdownFrontend();
-  });
-
-  afterEach(() => {
-    sinon.restore();
-  });
-
-  it("should validate valid CodeSpecProperties", async () => {
-    const fakeCodeSpecProperties: CodeSpecProperties = {
-      scopeSpec: { type: CodeScopeSpec.Type.Repository, fGuidRequired: true, relationship: "valid" },
-      spec: { isManagedWithDgnDb: false },
-      version: "1.0.0",
-    };
-    const fakeRow = {
-      toArray: () => ["0x123", "TestCodeSpec", JSON.stringify(fakeCodeSpecProperties)],
-    } as QueryRowProxy;
-
-    ecsqlReaderStub.next.resolves({ done: false, value: fakeRow });
-    iModelStub.createQueryReader.returns(ecsqlReaderStub);
-
-    const codeSpecs = new IModelConnection.CodeSpecs(iModelStub);
-    const codeSpec = await codeSpecs.getByName("TestCodeSpec");
-
-    assert.equal(codeSpec.name, "TestCodeSpec");
-    assert.equal(codeSpec.id, "0x123");
-    assert.deepEqual(codeSpec.properties, fakeCodeSpecProperties);
-  });
-
-  async function expectInvalidCodeSpecPropertiesToBeRejected(codeSpecProperties: any) {
-    const fakeRow = {
-      toArray: () => ["0x123", "TestCodeSpec", JSON.stringify(codeSpecProperties)],
-    } as QueryRowProxy;
-
-    ecsqlReaderStub.next.resolves({ done: false, value: fakeRow });
-    iModelStub.createQueryReader.returns(ecsqlReaderStub);
-
-    const codeSpecs = new IModelConnection.CodeSpecs(iModelStub);
-    await expect(codeSpecs.getByName("TestCodeSpec")).to.be.rejectedWith(
-      Error,
-      "Invalid CodeSpecProperties returned in the CodeSpec",
-    );
+    expect(codeSpec.iModel).to.exist;
+    expect(codeSpec.iModel.name).to.equal(requestedIModel.name);
   }
 
-  const invalidCodeSpecPropertiesWithWrongTypes = [
-    {
-      reason: "invalid scopeSpec.type type",
-      codeSpecProperties: {
-        scopeSpec: { type: "invalid", fGuidRequired: true, relationship: "valid" },
-        spec: { isManagedWithDgnDb: false },
-        version: "1.0.0",
-      },
-    },
-    {
-      reason: "invalid scopeSpec.fGuidRequired type",
-      codeSpecProperties: {
-        scopeSpec: { type: CodeScopeSpec.Type.Repository, fGuidRequired: "invalid", relationship: "valid" },
-        spec: { isManagedWithDgnDb: false },
-        version: "1.0.0",
-      },
-    },
-    {
-      reason: "invalid scopeSpec.relationship type",
-      codeSpecProperties: {
-        scopeSpec: { type: CodeScopeSpec.Type.Repository, fGuidRequired: true, relationship: false },
-        spec: { isManagedWithDgnDb: false },
-        version: "1.0.0",
-      },
-    },
-    {
-      reason: "invalid spec.isManagedWithDgnDb type",
-      codeSpecProperties: {
-        scopeSpec: { type: CodeScopeSpec.Type.Repository, fGuidRequired: true, relationship: "valid" },
-        spec: { isManagedWithDgnDb: "invalid" },
-        version: "1.0.0",
-      },
-    },
-    {
-      reason: "invalid version type",
-      codeSpecProperties: {
-        scopeSpec: { type: CodeScopeSpec.Type.Repository, fGuidRequired: true, relationship: "valid" },
-        spec: { isManagedWithDgnDb: false },
-        version: false,
-      },
-    },
-  ];
+  it("should load CodeSpecs", async () => {
+    const nullCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(
+      BisCodeSpec.nullCodeSpec
+    );
+    assert.equal(nullCodeSpec.scopeType, CodeScopeSpec.Type.Repository);
+    assert.equal(
+      nullCodeSpec.scopeReq,
+      CodeScopeSpec.ScopeRequirement.ElementId
+    );
 
-  for (const { codeSpecProperties, reason } of invalidCodeSpecPropertiesWithWrongTypes) {
-    it(`should not validate invalid CodeSpecProperties with ${reason}`, async () => {
-      await expectInvalidCodeSpecPropertiesToBeRejected(codeSpecProperties);
-    });
-  };
+    const subCategoryCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(
+      BisCodeSpec.subCategory
+    );
+    assert.equal(
+      subCategoryCodeSpec.scopeType,
+      CodeScopeSpec.Type.ParentElement
+    );
+    assert.equal(
+      subCategoryCodeSpec.scopeReq,
+      CodeScopeSpec.ScopeRequirement.ElementId
+    );
 
-  const invalidCodeSpecPropertiesWithMissingProperties = [
-    {
-      reason: "codeSpecProperties.scopeSpec are undefined",
-      codeSpecProperties: {
-        scopeSpec: undefined,
-        spec: { isManagedWithDgnDb: false },
-        version: "1.0.0",
-      },
-    },
-    {
-      reason: "codeSpecProperties.scopeSpec.type are undefined",
-      codeSpecProperties: {
-        scopeSpec: { type: undefined, fGuidRequired: true, relationship: "valid" },
-        spec: { isManagedWithDgnDb: false },
-        version: "1.0.0",
-      },
-    },
-  ];
+    const viewDefinitionCodeSpec: CodeSpec = await iModel.codeSpecs.getByName(
+      BisCodeSpec.viewDefinition
+    );
+    assert.equal(viewDefinitionCodeSpec.scopeType, CodeScopeSpec.Type.Model);
+    assert.equal(
+      viewDefinitionCodeSpec.scopeReq,
+      CodeScopeSpec.ScopeRequirement.ElementId
+    );
+  });
 
-  for (const { codeSpecProperties, reason } of invalidCodeSpecPropertiesWithMissingProperties) {
-    it(`should not validate invalid CodeSpecProperties when ${reason}`, async () => {
-      await expectInvalidCodeSpecPropertiesToBeRejected(codeSpecProperties);
-    });
-  };
+  it("should return code spec by name", async () => {
+    const codeSpecName = existing.name;
+    const codeSpecId = existing.id;
+
+    const codeSpec = await iModel.codeSpecs.getByName(codeSpecName);
+
+    verifyCodeSpec(codeSpec, iModel, codeSpecName, codeSpecId);
+  });
+
+  it("should return code spec by id", async () => {
+    const codeSpecName = existing.name;
+    const codeSpecId = existing.id;
+
+    const codeSpec = await iModel.codeSpecs.getById(codeSpecId);
+
+    verifyCodeSpec(codeSpec, iModel, codeSpecName, codeSpecId);
+  });
+
+  it("should fail because empty id", async () => {
+    const codeSpecId = "";
+
+    try {
+      await iModel.codeSpecs.getById(codeSpecId);
+    } catch (error: any) {
+      expect(error).to.be.instanceOf(IModelError);
+      expect(error.errorNumber).to.equal(IModelStatus.NotFound);
+      expect(error.message).to.equal("CodeSpec not found");
+    }
+  });
+
+  it("should fail because empty name", async () => {
+    const codeSpecName = "";
+
+    try {
+      await iModel.codeSpecs.getByName(codeSpecName);
+    } catch (error: any) {
+      expect(error).to.be.instanceOf(IModelError);
+      expect(error.errorNumber).to.equal(IModelStatus.NotFound);
+      expect(error.message).to.equal("CodeSpec not found");
+    }
+  });
+
+  it("should fail because invalid id", async () => {
+    const codeSpecId = "0";
+
+    try {
+      await iModel.codeSpecs.getById(codeSpecId);
+    } catch (error: any) {
+      expect(error).to.be.instanceOf(IModelError);
+      expect(error.errorNumber).to.equal(IModelStatus.InvalidId);
+      expect(error.message).to.equal("Invalid codeSpecId");
+      expect(error.getMetaData).to.be.a("function");
+      expect(error.getMetaData()).to.deep.equal({ codeSpecId });
+    }
+  });
+
+  it("should fail because non existent id", async () => {
+    const codeSpecId = "0xff";
+
+    try {
+      await iModel.codeSpecs.getById(codeSpecId);
+    } catch (error: any) {
+      expect(error).to.be.instanceOf(IModelError);
+      expect(error.errorNumber).to.equal(IModelStatus.NotFound);
+      expect(error.message).to.equal("CodeSpec not found");
+    }
+  });
+
+  it("should fail because non existent name", async () => {
+    const codeSpecName = "non-existent-name";
+
+    try {
+      await iModel.codeSpecs.getByName(codeSpecName);
+    } catch (error: any) {
+      expect(error).to.be.instanceOf(IModelError);
+      expect(error.errorNumber).to.equal(IModelStatus.NotFound);
+      expect(error.message).to.equal("CodeSpec not found");
+    }
+  });
 });
