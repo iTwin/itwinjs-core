813ca34b73f528a83ce0fdbdff3c66dc47a11cf2
Robert Schili <rschili@users.noreply.github.com>
2025-06-25 12:56:31 +0200

    make it so explicitly flagged indirect changes do not need a lock

--- full-stack-tests/backend/src/integration/IndirectChanges.test.ts
+++ full-stack-tests/backend/src/integration/IndirectChanges.test.ts
@@ -12,6 +12,19 @@ import { describe } from "mocha";
 import { AzuriteTest } from "./AzuriteTest";
 import { IModel } from "@itwin/core-common";
 
+
+async function assertThrowsAsync<T>(test: () => Promise<T>, contains?: string) {
+  try {
+    await test();
+  } catch (e) {
+    if (e instanceof Error && contains) {
+      assert.isTrue(e.message.includes(contains));
+    }
+    return;
+  }
+  throw new Error(`Failed to throw error with message: "${contains}"`);
+}
+
 describe.only("Indirect changes flag on elements", () => {
   const iTwinId = Guid.createValue();
   const user1AccessToken = "token 1";
@@ -36,20 +49,35 @@ describe.only("Indirect changes flag on elements", () => {
     IModelHost.authorizationClient = undefined;
   });
 
-  it.only("multi user indirect changes workflow", async () => {
-    const openNewBriefcase = async (accessToken: AccessToken) => {
-      const bcProps = await BriefcaseManager.downloadBriefcase({ iModelId, iTwinId, accessToken });
-      return BriefcaseDb.open(bcProps);
-    };
+  async function openNewBriefcase(accessToken: AccessToken) {
+    const bcProps = await BriefcaseManager.downloadBriefcase({ iModelId, iTwinId, accessToken });
+    return BriefcaseDb.open(bcProps);
+  }
 
+  function makeSubject(db: IModelDb, name: string, description: string, indirect?: boolean): Id64String {
+    const subject = Subject.create(db, IModel.rootSubjectId, name, description);
+    return db.elements.insertElement(subject.toJSON(), { indirect });
+  };
+
+  it.only("indirect changes should not require a lock", async () => {
+    const b1 = await openNewBriefcase(user1AccessToken);
+    await assertThrowsAsync(async () => {
+      makeSubject(b1, "Subject 1", "Description 1", false);
+    }, "shared lock not held on model for insert");
+
+    await assertThrowsAsync(async () => {
+      makeSubject(b1, "Subject 2", "Description 2");
+    }, "shared lock not held on model for insert");
+
+    const b1s3 = makeSubject(b1, "Subject 3", "Description 3", true);
+    assert.isDefined(b1s3, "Subject 3 should be created in b1 as an indirect change");
+  });
+
+  it.only("multi user indirect changes workflow", async () => {
     const b1 = await openNewBriefcase(user1AccessToken);
     const b2 = await openNewBriefcase(user2AccessToken);
     const _b3 = await openNewBriefcase(user3AccessToken);
 
-    const makeSubject = (db: IModelDb, name: string, description: string, indirect?: boolean): Id64String => {
-      const subject = Subject.create(db, IModel.rootSubjectId, name, description);
-      return db.elements.insertElement(subject.toJSON(), { indirect });
-    }
 
     await b1.locks.acquireLocks({ shared: IModel.rootSubjectId });
     const b1s1 = makeSubject(b1, "Subject 1", "Description 1", false);
