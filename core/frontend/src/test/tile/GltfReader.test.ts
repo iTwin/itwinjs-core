406f5144cb80cba5e872f87788161dae4ab0d614
pmconne <22944042+pmconne@users.noreply.github.com>
2025-07-10 10:31:10 -0400

    lint

--- core/frontend/src/test/tile/GltfReader.test.ts
+++ core/frontend/src/test/tile/GltfReader.test.ts
@@ -13,7 +13,7 @@ import { createBlankConnection } from "../createBlankConnection";
 import { BatchedTileIdMap } from "../../tile/internal";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-// eslint-disable @typescript-eslint/naming-convention
+/* eslint-disable @typescript-eslint/naming-convention */
 
 const minimalBin = new Uint8Array([12, 34, 0xfe, 0xdc]);
 const minimalJson = { asset: { version: "02.00" }, meshes: [] };
