95a420838ca0af30499f56140cff9d72df580625
pmconne <22944042+pmconne@users.noreply.github.com>
2025-07-23 12:40:18 -0400

    TextAnnotation elements implement ITextAnnotation.

--- core/backend/src/annotations/TextAnnotationElement.ts
+++ core/backend/src/annotations/TextAnnotationElement.ts
@@ -9,9 +9,10 @@
 import { ElementGeometry, ElementGeometryBuilderParams, PlacementProps, TextAnnotation, TextAnnotation2dProps, TextAnnotation3dProps, TextAnnotationProps } from "@itwin/core-common";
 import { IModelDb } from "../IModelDb";
 import { AnnotationElement2d, GraphicalElement3d } from "../Element";
-import { Id64String } from "@itwin/core-bentley";
+import { assert, Id64String } from "@itwin/core-bentley";
 import { layoutTextBlock } from "./TextBlockLayout";
 import { appendTextAnnotationGeometry } from "./TextAnnotationGeometry";
+import { ITextAnnotation, TextBlockAndId } from "./ElementDrivesTextAnnotation";
 
 function getElementGeometryBuilderParams(iModel: IModelDb, _placementProps: PlacementProps, annotationProps: TextAnnotationProps, categoryId: Id64String, _subCategory?: Id64String): ElementGeometryBuilderParams {
   const textBlock = TextAnnotation.fromJSON(annotationProps).textBlock;
@@ -27,7 +28,7 @@ function getElementGeometryBuilderParams(iModel: IModelDb, _placementProps: Plac
  * @see [[setAnnotation]] to change the textual content.
  * @public @preview
  */
-export class TextAnnotation2d extends AnnotationElement2d {
+export class TextAnnotation2d extends AnnotationElement2d implements ITextAnnotation {
   /** @internal */
   public static override get className(): string { return "TextAnnotation2d"; }
   protected constructor(props: TextAnnotation2dProps, iModel: IModelDb) { super(props, iModel); }
@@ -58,6 +59,16 @@ export class TextAnnotation2d extends AnnotationElement2d {
   public setAnnotation(annotation: TextAnnotation) {
     this.jsonProperties.annotation = annotation.toJSON();
   }
+
+  /** @internal */
+  public getTextBlocks(): Iterable<TextBlockAndId> {
+    return getTextBlocks(this);
+  }
+
+  /** @internal */
+  public updateTextBlocks(textBlocks: TextBlockAndId[]): void {
+    return updateTextBlocks(this, textBlocks);
+  }
 }
 
 /** An element that displays textual content within a 3d model.
@@ -65,7 +76,7 @@ export class TextAnnotation2d extends AnnotationElement2d {
  * @see [[setAnnotation]] to change the textual content.
  * @public @preview
  */
-export class TextAnnotation3d extends GraphicalElement3d {
+export class TextAnnotation3d extends GraphicalElement3d implements ITextAnnotation {
   /** @internal */
   public static override get className(): string { return "TextAnnotation3d"; }
   protected constructor(props: TextAnnotation3dProps, iModel: IModelDb) { super(props, iModel); }
@@ -96,4 +107,33 @@ export class TextAnnotation3d extends GraphicalElement3d {
   public setAnnotation(annotation: TextAnnotation) {
     this.jsonProperties.annotation = annotation.toJSON();
   }
+
+  /** @internal */
+  public getTextBlocks(): Iterable<TextBlockAndId> {
+    return getTextBlocks(this);
+  }
+
+  /** @internal */
+  public updateTextBlocks(textBlocks: TextBlockAndId[]): void {
+    return updateTextBlocks(this, textBlocks);
+  }
+}
+
+function getTextBlocks(elem: TextAnnotation2d | TextAnnotation3d): Iterable<TextBlockAndId> {
+  const annotation = elem.getAnnotation();
+  return annotation ? [{ textBlock: annotation.textBlock, id: undefined }] : [];
+}
+
+function updateTextBlocks(elem: TextAnnotation2d | TextAnnotation3d, textBlocks: TextBlockAndId[]): void {
+  assert(textBlocks.length === 1);
+  assert(textBlocks[0].id === undefined);
+
+  let annotation = elem.getAnnotation();
+  if (!annotation) {
+    annotation = TextAnnotation.create({ textBlock: textBlocks[0].textBlock });
+  } else {
+    annotation.textBlock = textBlocks[0].textBlock;
+  }
+
+  elem.update();
 }
