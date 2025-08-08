48a55e0c8c4f0b42e2aeea7a646b094281768d22
Erin Ingram <47707444+eringram@users.noreply.github.com>
2025-08-08 16:26:53 -0400

    WIP move to utility function

--- core/frontend/src/tile/GltfReader.ts
+++ core/frontend/src/tile/GltfReader.ts
@@ -7,7 +7,7 @@
  */
 
 import {
-  assert, ByteStream, compareBooleans, compareNumbers, compareStrings, Dictionary, JsonUtils, Logger, utf8ToString,
+  addAllToArray, assert, ByteStream, compareBooleans, compareNumbers, compareStrings, Dictionary, JsonUtils, Logger, utf8ToString
 } from "@itwin/core-bentley";
 import {
   Angle, IndexedPolyface, Matrix3d, Point2d, Point3d, Point4d, Range2d, Range3d, Transform, Vector3d,
@@ -445,14 +445,17 @@ export function getMeshPrimitives(mesh: GltfMesh | undefined): GltfMeshPrimitive
   // Start with a copy of mesh.primitives. For each group, replace the first primitive in the group with a primitive representing the entire group,
   // and set the rest of the primitives in the group to `undefined`.
   // This allows us to identify which remaining primitives do not use primitive restart, and any errors involving a primitive appearing in more than one group.
-  let primitives: Array<GltfMeshPrimitive | undefined> = [];
-  if (meshPrimitives.length > 50000) {
-    // This method is slower for smaller array sizes, but when the size of meshPrimitives gets larger its performance is ok.
-    primitives = primitives.concat(meshPrimitives);
-  } else {
-    // This method runs faster, but gets a stack overflow when the size of meshPrimitives is too large.
-    primitives = [...meshPrimitives];
-  }
+
+  // let primitives: Array<GltfMeshPrimitive | undefined> = [];
+  // if (meshPrimitives.length > 50000) {
+  //   // This method is slower for smaller array sizes, but when the size of meshPrimitives gets larger its performance is ok.
+  //   primitives = primitives.concat(meshPrimitives);
+  // } else {
+  //   // This method runs faster, but gets a stack overflow when the size of meshPrimitives is too large.
+  //   primitives = [...meshPrimitives];
+  // }
+
+  const primitives = addAllToArray([], meshPrimitives);
 
   for (const group of ext.primitiveGroups) {
     // Spec: the group must not be empty and all indices must be valid array indices into mesh.primitives.
