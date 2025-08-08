0b84c7d2986c6ade7add3fe8749ffb1cfa2ddecb
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2025-08-08 10:13:52 -0600

    Remove unused import

--- core/common/src/rpc/web/BentleyCloudRpcProtocol.ts
+++ core/common/src/rpc/web/BentleyCloudRpcProtocol.ts
@@ -6,7 +6,7 @@
  * @module RpcInterface
  */
 
-import { BentleyStatus, expectDefined } from "@itwin/core-bentley";
+import { BentleyStatus } from "@itwin/core-bentley";
 import { IModelRpcProps } from "../../IModel";
 import { IModelError } from "../../IModelError";
 import { RpcConfiguration } from "../core/RpcConfiguration";
