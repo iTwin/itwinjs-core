1aec0a1fc9c92e79aa73d318eabfd74c0f311708
Erin Ingram <47707444+eringram@users.noreply.github.com>
2025-08-26 18:15:43 -0400

    WIP saved view to cesium camera

---
+++ core/frontend/src/internal/render/CesiumView.ts
@@ -0,0 +1,74 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { Cartographic, EcefLocation, ViewDefinition3dProps } from "@itwin/core-common";
+import { Point3d, Range3d, Vector3d, YawPitchRollAngles } from "@itwin/core-geometry";
+
+interface CesiumFrustum {
+  fov?: number;
+  near: number;
+  far: number;
+}
+
+interface CesiumCamera {
+  frustum: CesiumFrustum;
+  up: Vector3d;
+  direction: Vector3d;
+  position: Point3d;
+}
+
+// Returns position, orientation (direction, up), and frustum needed to create/modify a Cesium camera
+export function createCesiumCamera(viewDefinition: ViewDefinition3dProps, ecefLoc?: EcefLocation, modelExtents?: Range3d): CesiumCamera {
+  const defaultOrigin = Cartographic.fromDegrees({ longitude: 0, latitude: 0, height: 0 });
+  let ecefLocation;
+  if (ecefLoc) {
+    ecefLocation = ecefLoc;
+  } else if (modelExtents) {
+    ecefLocation = EcefLocation.createFromCartographicOrigin(defaultOrigin, modelExtents.center);
+  } else {
+    throw new Error("Either ecefLocation or modelExtents must be defined to create Cesium camera.");
+  }
+
+  const angles = new YawPitchRollAngles();
+  angles.setFromJSON(viewDefinition.angles);
+
+  const rotation = angles.toMatrix3d();
+  const up = rotation.rowY();
+  const direction = rotation.rowZ().scale(-1);
+
+  const viewExtents = new Vector3d();
+  viewExtents.setFromJSON(viewDefinition.extents);
+
+  let fov;
+  let position = new Point3d();
+  if (viewDefinition.cameraOn) {
+    position = Point3d.fromJSON(viewDefinition.camera.eye);
+    fov = 2.0 * Math.atan2(viewExtents.x / 2.0, viewDefinition.camera.focusDist);
+  } else {
+    position = Point3d.fromJSON(viewDefinition.origin);
+    rotation.multiplyVectorInPlace(position);
+    position.addScaledInPlace(viewExtents, 0.5);
+    position = rotation.multiplyInverseXYZAsPoint3d(position.x, position.y, position.z) ?? position;
+    position.addScaledInPlace(direction, -viewExtents.z);
+  }
+
+  const transformedPosition = ecefLocation.getTransform().multiplyPoint3d(position);
+  const transformedUp = ecefLocation.getTransform().multiplyVector(up);
+  const transformedDirection = ecefLocation.getTransform().multiplyVector(direction);
+
+  const frustum: CesiumFrustum = {
+    near: 0.01,
+    far: 1000000,
+    fov
+  };
+  const cesiumCamera: CesiumCamera = {
+    position: transformedPosition,
+    up: transformedUp,
+    direction: transformedDirection,
+    frustum
+  }
+
+  return cesiumCamera;
+}
---
+++ core/frontend/src/test/render/CesiumView.test.ts
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { describe, expect, it } from "vitest";
+import { Code, EcefLocation, IModel } from "@itwin/core-common";
+import { createCesiumCamera } from "../../internal/render/CesiumView";
+
+// Saved view data and ecef location are required to create the Cesium camera
+// These values come from saved views created in DTA for Metrostation.bim
+
+const cameraOnView = {
+  cameraOn: true,
+  origin: [-50.20252266797269, 56.989460084120665, -93.48021229168089],
+  extents: [224.2601166976935, 165.04873366794442, 249.514861628184],
+  angles: {
+    pitch: -26.15946129868821,
+    roll: -43.25863504612565,
+    yaw: 25.103938995163002,
+  },
+  camera: {
+    lens: 45.95389015950363,
+    focusDist: 264.45767738020345,
+    eye: [-37.863420740019635, -118.27234989806642, 132.40005835408053],
+  },
+  code: Code.createEmpty(),
+  model: IModel.dictionaryId,
+  classFullName: "test",
+  categorySelectorId: "@1",
+  displayStyleId: "@1",
+};
+
+const cameraOffView = {
+  cameraOn: false,
+  origin: [
+    -23.625709412040386,
+    -80.7768748077716,
+    -105.35651978750751
+  ],
+  extents: [
+    224.26011669769352,
+    211.51108266624928,
+    249.51486162818406
+  ],
+  angles: {
+    pitch: 19.609147443371107,
+    roll: -58.40009741588226,
+    yaw: -11.665541819368947
+  },
+  camera: {
+    lens: 45.95389015950363,
+    focusDist: 264.45767738020345,
+    eye: [
+      -37.86342074001966,
+      -118.27234989806631,
+      132.40005835408056
+    ]
+  },
+  code: Code.createEmpty(),
+  model: IModel.dictionaryId,
+  classFullName: "test",
+  categorySelectorId: "@1",
+  displayStyleId: "@1",
+}
+
+const ecefLocProps = {
+  origin: [
+    1255641.5519893507,
+    -4732698.684827632,
+    4073546.2460685894
+  ],
+  orientation: {
+    pitch: -49.005021293968355,
+    roll: -11.823580111180991,
+    yaw: -90.642664633961
+  },
+  transform: [
+    [
+      -0.007357864592832313,
+      0.9804561979367872,
+      0.19659986204464436,
+      1255641.5519893507
+    ],
+    [
+      -0.6559516195525271,
+      0.14366280316126617,
+      -0.7410050416793829,
+      -4732698.684827632
+    ],
+    [
+      -0.75476707309941,
+      -0.13441221267127085,
+      0.6420747794842614,
+      4073546.2460685894
+    ]
+  ],
+  cartographicOrigin: {
+    latitude: 0.6972007432483922,
+    longitude: -1.311456937133241,
+    height: 4.102413240985213
+  }
+};
+const ecefLocation = new EcefLocation(ecefLocProps);
+
+describe("createCesiumCamera", () => {
+  it("creates a CesiumCamera object from SpatialViewDefinitionProps with the camera on (perspective projection)", () => {
+    const cesiumCamera = createCesiumCamera(cameraOnView, ecefLocation);
+
+    expect(cesiumCamera.position.x).toBeCloseTo(1255551.8995579786);
+    expect(cesiumCamera.position.y).toBeCloseTo(-4732788.948703558);
+    expect(cesiumCamera.position.z).toBeCloseTo(4073675.732118358);
+
+    expect(cesiumCamera.direction.x).toBeCloseTo(0.4713215948502468);
+    expect(cesiumCamera.direction.y).toBeCloseTo(0.28355077006441065);
+    expect(cesiumCamera.direction.z).toBeCloseTo(-0.8351376623190333);
+
+    expect(cesiumCamera.up.x).toBeCloseTo(0.6668920140307033);
+    expect(cesiumCamera.up.y).toBeCloseTo(-0.7342387351881554);
+    expect(cesiumCamera.up.z).toBeCloseTo(0.12707683255168906);
+
+    expect(cesiumCamera.frustum.fov).toBeCloseTo(0.8020466873831604);
+    expect(cesiumCamera.frustum.near).to.equal(0.01);
+    expect(cesiumCamera.frustum.far).to.equal(1000000);
+  });
+
+  it("creates a CesiumCamera object from SpatialViewDefinitionProps with the camera off (orthographic projection)", () => {
+    const cesiumCamera = createCesiumCamera(cameraOffView, ecefLocation);
+
+    // WIP
+    expect(cesiumCamera).toBeDefined;
+    console.log(cesiumCamera);
+  });
+});
