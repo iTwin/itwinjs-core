4383b03da4cd2cf9b73df15c78466d502e662e60
Erin Ingram <47707444+eringram@users.noreply.github.com>
2025-09-16 12:09:04 -0400

    lint

--- extensions/cesium-renderer/src/CesiumCamera.ts
+++ extensions/cesium-renderer/src/CesiumCamera.ts
@@ -3,8 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { Cartographic, EcefLocation } from "@itwin/core-common";
-import type { ViewDefinition3dProps } from "@itwin/core-common";
+import { Cartographic, EcefLocation, type ViewDefinition3dProps } from "@itwin/core-common";
 import { Point3d, Range3d, Vector3d, YawPitchRollAngles } from "@itwin/core-geometry";
 
 /** Properties that define a Cesium frustum.
