f6df2042bd0d2473d56bc6290dcca0fef9c30d65
Michael Swiger <108895074+MichaelSwigerAtBentley@users.noreply.github.com>
2025-09-17 14:58:19 -0500

    Pass deviceName to acquireNewBriefcaseId

--- core/backend/src/BriefcaseManager.ts
+++ core/backend/src/BriefcaseManager.ts
@@ -8,7 +8,8 @@
 
 // cspell:ignore cset csets ecchanges
 
-import * as path from "path";
+import * as path from "node:path";
+import * as os from "node:os";
 import {
   AccessToken, BeDuration, ChangeSetStatus, DbResult, GuidString, IModelHubStatus, IModelStatus, Logger, OpenMode, Optional, StopWatch
 } from "@itwin/core-bentley";
@@ -229,7 +230,7 @@ export class BriefcaseManager {
    * @note *It is invalid to edit briefcases on a shared network drive* and that is a sure way to corrupt your briefcase (see https://www.sqlite.org/howtocorrupt.html)
    */
   public static async downloadBriefcase(arg: RequestNewBriefcaseArg): Promise<LocalBriefcaseProps> {
-    const briefcaseId = arg.briefcaseId ?? await this.acquireNewBriefcaseId(arg);
+    const briefcaseId = arg.briefcaseId ?? await this.acquireNewBriefcaseId({...arg, deviceName: os.hostname()});
     const fileName = arg.fileName ?? this.getFileName({ ...arg, briefcaseId });
 
     if (IModelJsFs.existsSync(fileName))
