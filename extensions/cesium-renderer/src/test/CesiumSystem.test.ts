8b8b0d99c649342654ad50d7c3832ff316f90933
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2025-09-26 12:48:00 -0400

    fix CesiumSystem.test.ts

--- extensions/cesium-renderer/src/test/CesiumSystem.test.ts
+++ extensions/cesium-renderer/src/test/CesiumSystem.test.ts
@@ -28,8 +28,9 @@ describe("CesiumSystem", () => {
 
   it("should use a CesiumSystem object to create valid targets", () => {
     const canvas = document.createElement("canvas")
-    canvas.style.height = '32px';
-    canvas.style.width = '64px';
+    canvas.width = 64;
+    canvas.height = 32;
+    document.body.appendChild(canvas);
     const onScreenTarget = cesiumSystem?.createTarget(canvas);
     expect(onScreenTarget).not.to.be.undefined;
     expect(onScreenTarget).to.be.instanceOf(CesiumOnScreenTarget);
@@ -37,6 +38,7 @@ describe("CesiumSystem", () => {
     expect(onScreenTarget?.viewRect?.top).to.equal(0);
     expect(onScreenTarget?.viewRect?.right).to.equal(64);
     expect(onScreenTarget?.viewRect?.bottom).to.equal(32);
+    document.body.removeChild(canvas);
 
     const offScreenTarget = cesiumSystem?.createOffscreenTarget(new ViewRect(0, 0, 64, 32));
     expect(offScreenTarget).not.to.be.undefined;
