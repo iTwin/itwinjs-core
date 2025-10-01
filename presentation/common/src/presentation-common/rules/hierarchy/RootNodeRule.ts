9ba188a622644b4e3c5b24130a51aa05682061e5
Daniel Rodriguez <44824788+DanRod1999@users.noreply.github.com>
2025-10-01 15:50:56 -0400

    Revert "Apply deprecation date rule" (#8591)
    Co-authored-by: Daniel Rodriguez <DanRod1999@users.noreply.github.com>

--- presentation/common/src/presentation-common/rules/hierarchy/RootNodeRule.ts
+++ presentation/common/src/presentation-common/rules/hierarchy/RootNodeRule.ts
@@ -14,7 +14,7 @@ import { NavigationRuleBase } from "./NavigationRule.js";
  *
  * @see [Root node rule reference documentation page]($docs/presentation/hierarchies/RootNodeRule.md)
  * @public
- * @deprecated in 5.2 - will not be removed until after 2026-10-01. Use the new [@itwin/presentation-hierarchies](https://github.com/iTwin/presentation/blob/master/packages/hierarchies/README.md)
+ * @deprecated in 5.2. Use the new [@itwin/presentation-hierarchies](https://github.com/iTwin/presentation/blob/master/packages/hierarchies/README.md)
  * package for creating hierarchies.
  */
 export interface RootNodeRule extends NavigationRuleBase {
