4cf9deaee80eb2a4b4785c609f1cefdacdd70f7b
dassaf4 <68340676+dassaf4@users.noreply.github.com>
2025-10-01 17:07:24 -0500

    reproduce Sample geometry with simpler code

--- core/frontend/src/test/render/primitives/Primitives.test.ts
+++ core/frontend/src/test/render/primitives/Primitives.test.ts
@@ -4,7 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 import { describe, expect, it } from "vitest";
 import { GraphicParams } from "@itwin/core-common";
-import { Geometry as Geom, IndexedPolyface, LineString3d, Loop, Point3d, Range3d, Transform, Vector3d } from "@itwin/core-geometry";
+import { BSplineSurface3d, IndexedPolyface, LineString3d, Loop, Point3d, PolyfaceBuilder, Range3d, StrokeOptions, Transform, Vector3d } from "@itwin/core-geometry";
 import { DisplayParams } from "../../../common/internal/render/DisplayParams";
 import { GeometryList } from "../../../common/internal/render/GeometryList";
 import { Geometry } from "../../../common/internal/render/GeometryPrimitives";
@@ -15,62 +15,21 @@ function verifyGeometryQueries(g: Geometry, doDecimate: boolean = false, doVerte
   expect(g.doVertexCluster()).toBe(doVertexCluster);
   expect(g.part() !== undefined).toBe(hasPart);
 }
-function createUnitCircle(numPoints: number): Point3d[] {
-  const points: Point3d[] = [];
-  const dTheta = Geom.safeDivideFraction(Math.PI * 2, numPoints - 1, 0.0);
-  for (let i = 0; i + 1 < numPoints; i++) {
-    const theta = i * dTheta;
-    points.push(Point3d.create(Math.cos(theta), Math.sin(theta), 0.0));
-  }
-  points.push(points[0].clone());
-  return points;
-}
-function createTriangularUnitGridPolyface(
-  origin: Point3d, vectorX: Vector3d, vectorY: Vector3d,
-  numXVertices: number, numYVertices: number, createParams: boolean = false, createNormals: boolean = false,
-): IndexedPolyface {
-  const mesh = IndexedPolyface.create(createNormals, createParams);
-  const normal = vectorX.crossProduct(vectorY);
-  if (createNormals) {
-    normal.normalizeInPlace();
-    mesh.addNormalXYZ(normal.x, normal.y, normal.z); // use XYZ to help coverage count
-  }
-  // push to coordinate arrays
-  for (let j = 0; j < numYVertices; j++) {
-    for (let i = 0; i < numXVertices; i++) {
-      mesh.addPoint(origin.plus2Scaled(vectorX, i, vectorY, j));
-      if (createParams)
-        mesh.addParamUV(i, j);
-    }
-  }
-  for (let j = 0; j + 1 < numYVertices; j++) {
-    for (let i = 0; i + 1 < numXVertices; i++) {
-      const vertex00 = numXVertices * j + i;
-      const vertex10 = vertex00 + 1;
-      const vertex01 = vertex00 + numXVertices;
-      const vertex11 = vertex01 + 1;
-      // push quad
-      mesh.addPointIndex(vertex00, true);
-      mesh.addPointIndex(vertex10, true);
-      mesh.addPointIndex(vertex11, true);
-      mesh.addPointIndex(vertex01, true);
-      // param indexing matches points
-      if (createParams) {
-        mesh.addParamIndex(vertex00);
-        mesh.addParamIndex(vertex10);
-        mesh.addParamIndex(vertex11);
-        mesh.addParamIndex(vertex01);
-      }
-      if (createNormals) {
-        mesh.addNormalIndex(0);
-        mesh.addNormalIndex(0);
-        mesh.addNormalIndex(0);
-        mesh.addNormalIndex(0);
-      }
-      mesh.terminateFacet(false);
-    }
-  }
-  return mesh;
+
+function createExampleMesh(origin: Point3d, vectorX: Vector3d, vectorY: Vector3d, numXVertices: number, numYVertices: number, createParams: boolean, createNormals: boolean, triangulate: boolean): IndexedPolyface {
+  const numXEdges = numXVertices - 1;
+  const numYEdges = numYVertices - 1;
+  const points = new Array(numYVertices).fill(0).map((_row, i) => new Array(numXVertices).fill(0).map((_pt, j) => origin.plus2Scaled(vectorX, j / numXEdges, vectorY, i / numYEdges).toArray()));
+  const linearPatch = BSplineSurface3d.createGrid(points, 2, undefined, 2, undefined);
+  expect(linearPatch).toBeDefined();
+  const options = new StrokeOptions();
+  options.shouldTriangulate = triangulate;
+  options.needParams = createParams;
+  options.needNormals = createNormals;
+  const builder = PolyfaceBuilder.create(options);
+  if (linearPatch)
+    builder.addUVGridBody(linearPatch, numXEdges, numYEdges);
+  return builder.claimPolyface();
 }
 
 describe("ToleranceRatio", () => {
@@ -136,8 +95,7 @@ describe("GeometryList", () => {
     const gp = new GraphicParams();
     const dp = DisplayParams.createForLinear(gp);
     const origin = Point3d.create(1, 2, 3);
-    const polyface = createTriangularUnitGridPolyface(origin,
-      Vector3d.create(1, 0, 0), Vector3d.create(0, 2, 0), 4, 5, true, true);
+    const polyface = createExampleMesh(origin, Vector3d.create(3, 0, 0), Vector3d.create(0, 8, 0), 4, 5, true, true, true);
     const polyfaceG0 = Geometry.createFromPolyface(polyface, Transform.createIdentity(), polyface.range(), dp, undefined);
     glist0.push(polyfaceG0);
     verifyGeometryQueries(polyfaceG0, false, true, false); // maybe this has to change someday?
@@ -151,7 +109,7 @@ describe("GeometryList", () => {
     expect(glist0.isEmpty).toBe(true);
     const gp = new GraphicParams();
     const dp = DisplayParams.createForLinear(gp);
-    const loop = Loop.create(LineString3d.create(createUnitCircle(5)));
+    const loop = Loop.create(LineString3d.createRegularPolygonXY(Point3d.createZero(), 4, 1, true));
     const loopG0 = Geometry.createFromLoop(loop, Transform.createIdentity(), loop.range(), dp, false, undefined);
     glist0.push(loopG0);
     verifyGeometryQueries(loopG0, false, true, false); // maybe this has to change someday?
