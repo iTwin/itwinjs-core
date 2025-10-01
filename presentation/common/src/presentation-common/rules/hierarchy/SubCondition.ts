7617f4341b78c3e33a190ae51be73cbb6a0498f0
imodeljs-admin <imodeljs-admin@users.noreply.github.com>
2025-10-01 14:53:41 +0000

    Apply deprecation date rule

--- presentation/common/src/presentation-common/rules/hierarchy/SubCondition.ts
+++ presentation/common/src/presentation-common/rules/hierarchy/SubCondition.ts
@@ -18,7 +18,7 @@ import { ChildNodeSpecification } from "./ChildNodeSpecification.js";
  *
  * @see [Sub-conditions reference documentation section]($docs/presentation/hierarchies/ChildNodeRule.md#attribute-subconditions)
  * @public
- * @deprecated in 5.2. Use the new [@itwin/presentation-hierarchies](https://github.com/iTwin/presentation/blob/master/packages/hierarchies/README.md)
+ * @deprecated in 5.2 - will not be removed until after 2026-10-01. Use the new [@itwin/presentation-hierarchies](https://github.com/iTwin/presentation/blob/master/packages/hierarchies/README.md)
  * package for creating hierarchies.
  */
 export interface SubCondition {
