252abc1365c58a644ab8729eff4235a762b85b05
andremig-bentley <andremig-bentley@users.noreply.github.com>
2025-10-15 10:10:40 -0600

    cleanup

--- extensions/frontend-tiles/src/frontend-tiles.ts
+++ extensions/frontend-tiles/src/frontend-tiles.ts
@@ -8,15 +8,3 @@ export * from "./GraphicsProvider/GraphicRepresentationProvider.js";
 export * from "./GraphicsProvider/GraphicsProvider.js";
 export * from "./GraphicsProvider/tileset-creators/GeoscienceTileset.js";
 export * from "./GraphicsProvider/url-providers/GeoscienceUrlProvider.js";
-
-/** @docs-package-description
- * Package that provides/does?
- */
-/**
- * @docs-group-description Batched Tiles
- * Set of classes used to?
- */
-/**
- * @docs-group-description something else?
- * Set of classes used to?
- */
