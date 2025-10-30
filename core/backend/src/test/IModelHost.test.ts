139e2af92124a59fac5ff1813e54d862f3768e3f
andremig-bentley <andremig-bentley@users.noreply.github.com>
2025-10-30 11:22:39 -0600

    backend test fixes

--- core/backend/src/test/IModelHost.test.ts
+++ core/backend/src/test/IModelHost.test.ts
@@ -18,7 +18,7 @@ import { IModelTestUtils } from "./IModelTestUtils";
 import { Logger, LogLevel } from "@itwin/core-bentley";
 import { overrideSyncNativeLogLevels } from "../internal/NativePlatform";
 
-describe("IModelHost", () => {
+describe.only("IModelHost", () => {
   const opts = { cacheDir: TestUtils.getCacheDir() };
   beforeEach(async () => {
     await TestUtils.shutdownBackend();
