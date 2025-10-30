0fb0e1fcd53bba41bc202b0f43b2545bf7d4b8bd
andremig-bentley <andremig-bentley@users.noreply.github.com>
2025-10-29 20:53:13 -0600

    test fix

--- full-stack-tests/backend/src/integration/IModelOpen.test.ts
+++ full-stack-tests/backend/src/integration/IModelOpen.test.ts
@@ -3,8 +3,8 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import { AccessToken, GuidString } from "@itwin/core-bentley";
-import { IModelVersion } from "@itwin/core-common";
+import { AccessToken, GuidString, RepositoryStatus } from "@itwin/core-bentley";
+import { IModelError, IModelVersion } from "@itwin/core-common";
 import { TestUsers, TestUtility } from "@itwin/oidc-signin-tool";
 import { assert, expect } from "chai";
 import { BriefcaseManager, IModelHost, SnapshotDb } from "@itwin/core-backend";
@@ -33,10 +33,7 @@ describe("IModelOpen", () => {
   it("Unauthorized requests should cause an obvious error", async () => {
     // Try the bad request context
     await expect(HubWrappers.downloadAndOpenCheckpoint({ accessToken: "bad", iTwinId: testITwinId, iModelId: testIModelId }))
-      .to.be.rejectedWith(Error)
-      .to.eventually.have.property("originalError")
-      .that.has.property("iTwinErrorId")
-      .that.has.property("key", "InvalidiModelsRequest");
+      .to.be.rejectedWith(IModelError).to.eventually.have.property("errorNumber", RepositoryStatus.InvalidRequest);
   });
 
   it("should be able to handle simultaneous open calls", async () => {
