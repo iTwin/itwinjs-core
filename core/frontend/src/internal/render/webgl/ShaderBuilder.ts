d7bd8582da48780e6bf7db657add7c7ca007f01d
Mark Schlosser <47000437+markschlosseratbentley@users.noreply.github.com>
2025-11-10 14:43:37 -0500

    Avoid Intel Ultra 7 graphical glitches during vertex discard (#8730)
    Co-authored-by: pmconne <22944042+pmconne@users.noreply.github.com>

--- core/frontend/src/internal/render/webgl/ShaderBuilder.ts
+++ core/frontend/src/internal/render/webgl/ShaderBuilder.ts
@@ -819,13 +819,19 @@ export class VertexShaderBuilder extends ShaderBuilder {
       main.addline("  v_color = baseColor;");
     }
 
+    /*
+    There is currently a driver glitch which is known to affect much of the Intel Ultra 7 family of chipsets when using Intel driver from some point after driver version 32.0.101.6078 (9/13/2024).
+
+    The graphics driver will glitch when discarding triangles using the vertex shader (setting all vertices to the same value) if gl_Position was not initialized to a valid position beforehand.
+
+    The workaround for this bug involves ensuring that gl_Position is initialized to a valid position before attempting a discard using a degenerate triangle.
+    */
+    main.addline("  gl_Position = computePosition(rawPosition);");
     const checkForDiscard = this.get(VertexShaderComponent.CheckForDiscard);
     if (undefined !== checkForDiscard) {
       prelude.addFunction("bool checkForDiscard()", checkForDiscard);
       main.add(vertexDiscard);
     }
-
-    main.addline("  gl_Position = computePosition(rawPosition);");
 
     const finalizePos = this.get(VertexShaderComponent.FinalizePosition);
     if (undefined !== finalizePos) {
