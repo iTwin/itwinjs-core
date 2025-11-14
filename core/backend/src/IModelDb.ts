e791a0a4f5102dbc27b39286eca8c012aa683bf0
Michael Swiger <108895074+MichaelSwigerAtBentley@users.noreply.github.com>
2025-11-14 09:59:24 -0600

    Swap out clearCache

--- core/backend/src/IModelDb.ts
+++ core/backend/src/IModelDb.ts
@@ -861,7 +861,9 @@ export abstract class IModelDb extends IModel {
    * @note This will not delete Txns that have already been saved, even if they have not yet been pushed.
   */
   public abandonChanges(): void {
-    this.clearCaches();
+    // Clears instanceKey caches only, not the full caches, since the changes are not saved yet
+    this.elements[_instanceKeyCache].clear();
+    this.models[_instanceKeyCache].clear();
     this[_nativeDb].abandonChanges();
   }
 
