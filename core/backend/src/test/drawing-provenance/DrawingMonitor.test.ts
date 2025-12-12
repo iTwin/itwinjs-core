d69c74a17015e4177ee09a8ed8c9f051101e4fe8
pmconne <22944042+pmconne@users.noreply.github.com>
2025-12-12 17:01:17 -0500

    Stub out DrawingMonitor tests.

---
+++ core/backend/src/test/drawing-provenance/DrawingMonitor.test.ts
@@ -0,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+
+describe.only("DrawingMonitor", () => {
+  it("updates drawings when the geometry of a viewed spatial model is modified", async () => {
+
+  });
+
+  it("updates drawings when a viewed spatial model is deleted", async () => {
+
+  });
+
+  it("waits a specified delay before updating drawings", async () => {
+
+  });
+
+  it("only returns the most up-to-date results if multiple changes occur while delayed", async () => {
+
+  });
+
+  it("cancels delay if updates are requested while delayed", async () => {
+
+  });
+
+  it("updates drawings if their provenance is out of date at initialization", async () => {
+
+  });
+
+  it("updates drawings if their provenance is missing at initialization", async () => {
+
+  });
+
+  it("only updates drawings affected by a particular set of changes", async () => {
+
+  });
+
+  it("throws when attempting to access updates after termination", async () => {
+
+  });
+});
