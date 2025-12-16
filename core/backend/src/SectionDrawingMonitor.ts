89e0b41beb14c0d43a9446b7b0c77f0fe35acfde
pmconne <22944042+pmconne@users.noreply.github.com>
2025-12-16 08:05:14 -0500

    doc group

--- core/backend/src/SectionDrawingMonitor.ts
+++ core/backend/src/SectionDrawingMonitor.ts
@@ -2,6 +2,9 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+/** @packageDocumentation
+ * @module Elements
+ */
 
 import { Id64String } from "@itwin/core-bentley";
 import { createSectionDrawingMonitor } from "./internal/SectionDrawingMonitorImpl";
