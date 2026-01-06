9a0b671a320c5fc7fac1a052e7451c982a1b7d28
Erin Ingram <47707444+eringram@users.noreply.github.com>
2026-01-06 15:12:28 -0500

    WIP

--- core/frontend/src/internal/render/webgl/System.ts
+++ core/frontend/src/internal/render/webgl/System.ts
@@ -686,8 +686,8 @@ export class System extends RenderSystem implements RenderSystemDebugControl, Re
         textureWeight: args.textureMapping.weight,
         worldMapping: args.textureMapping.worldMapping,
         // args.textureMapping is a TextureMapping, but is expected to be a MaterialTextureMappingProps here
-        useConstantLod: (args.textureMapping as TextureMapping)?.params.useConstantLod,
-        constantLodProps: (args.textureMapping as TextureMapping)?.params.constantLodParams,
+        useConstantLod: (args.textureMapping as TextureMapping)?.params?.useConstantLod,
+        constantLodProps: (args.textureMapping as TextureMapping)?.params?.constantLodParams,
       }));
       params.textureMapping.normalMapParams = args.textureMapping.normalMapParams;
     }
