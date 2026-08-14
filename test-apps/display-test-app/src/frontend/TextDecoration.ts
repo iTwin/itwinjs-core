61ee8bb1137d385350cb307ee3f8c79b441eb666
Claudia Barnes <claudiaareneee@users.noreply.github.com>
2026-08-14 14:04:45 -0700

    removing emphasize elements

--- test-apps/display-test-app/src/frontend/TextDecoration.ts
+++ test-apps/display-test-app/src/frontend/TextDecoration.ts
@@ -36,7 +36,7 @@ import {
   TextRun,
   TextStyleSettingsProps,
 } from "@itwin/core-common";
-import { DecorateContext, Decorator, EmphasizeElements, GraphicType, IModelApp, IModelConnection, NotifyMessageDetails, OutputMessagePriority,  readElementGraphics, RenderGraphicOwner, Tool } from "@itwin/core-frontend";
+import { DecorateContext, Decorator, GraphicType, IModelApp, IModelConnection, NotifyMessageDetails, OutputMessagePriority,  readElementGraphics, RenderGraphicOwner, Tool } from "@itwin/core-frontend";
 import { DtaRpcInterface } from "../common/DtaRpcInterface";
 import { assert, Id64, Id64String } from "@itwin/core-bentley";
 import { Angle, Point3d, Vector3d, YawPitchRollAngles } from "@itwin/core-geometry";
@@ -54,7 +54,6 @@ class TextEditor implements Decorator {
   public categoryId: Id64String = Id64.invalid;
   public modelId: Id64String = Id64.invalid;
   public defaultTextStyleId: Id64String = Id64.invalid;
-  public emphasizeElements = new EmphasizeElements();
 
   // TextAnnotation properties
   public origin: Point3d = new Point3d(0, 0, 0);
@@ -142,9 +141,6 @@ class TextEditor implements Decorator {
 
   public clear(): void {
     IModelApp.viewManager.dropDecorator(this);
-
-    const vp = IModelApp.viewManager.selectedView;
-    if (vp) this.emphasizeElements.clearHiddenElements(vp);
 
     this._iModel = undefined;
     this._graphic?.disposeGraphic();
