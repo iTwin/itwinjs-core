e0d367b11fa47abd26f11383e97d5e91ce6f09e9
pmconne <22944042+pmconne@users.noreply.github.com>
2026-08-24 13:31:42 -0400

    ugh

--- core/frontend/src/internal/IModelDisplaySettingsImpl.ts
+++ core/frontend/src/internal/IModelDisplaySettingsImpl.ts
@@ -6,4 +6,18 @@
  * @module Views
  */
 
+/*
+import { IModelDisplaySettings, IModelDisplaySettings3d } from "../IModelDisplaySettings";
+import { ViewState } from "../ViewState";
+import { _implementationProhibited } from "./cross-package";
 
+abstract class IModelDisplaySettingsImpl implements IModelDisplaySettings {
+  public readonly [_implementationProhibited] = undefined;
+
+  protected abstract get _view(): ViewState;
+
+  public abstract is3d(): this is IModelDisplaySettings3d;
+
+  public 
+}
+*/
