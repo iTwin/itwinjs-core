a6d2101212e31e10dfebc3774956cf089113cd27
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2026-08-28 12:20:25 -0400

    Apply suggestion from @pmconne

--- core/bentley/src/test/ObservableSet.test.ts
+++ core/bentley/src/test/ObservableSet.test.ts
@@ -172,8 +172,6 @@ describe("ObservableSet", () => {
     expect(set.size).to.equal(1);
     expect(set.has("b")).to.be.true;
   });
-
-
   it("subclasses can override `add`", () => {
     class MySet extends ObservableSet<string> {
       public myAddWasCalled = false;
