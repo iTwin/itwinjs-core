24975ad2aaf5589bd0a7abd8858493e71f287ca9
dassaf4 <68340676+dassaf4@users.noreply.github.com>
2026-09-04 17:03:45 -0500

    react to clone return type tightening

--- core/geometry/src/curve/CoordinateXYZ.ts
+++ core/geometry/src/curve/CoordinateXYZ.ts
@@ -56,13 +56,13 @@ export class CoordinateXYZ extends GeometryQuery {
     return true;
   }
   /** Return a transformed clone. */
-  public cloneTransformed(transform: Transform): GeometryQuery | undefined {
+  public cloneTransformed(transform: Transform): GeometryQuery {
     const result = new CoordinateXYZ(this._xyz.clone());
     result.tryTransformInPlace(transform);
     return result;
   }
   /** Return a clone. */
-  public clone(): GeometryQuery | undefined {
+  public clone(): GeometryQuery {
     return new CoordinateXYZ(this._xyz.clone());
   }
   /** Test if (other instanceof Coordinate).  */
