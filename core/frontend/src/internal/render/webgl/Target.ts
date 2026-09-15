4ec791d35afaf5678aeb63da046bc83219710121
Erin Ingram <47707444+eringram@users.noreply.github.com>
2026-09-15 10:15:51 -0400

    Fix

--- core/frontend/src/internal/render/webgl/Target.ts
+++ core/frontend/src/internal/render/webgl/Target.ts
@@ -186,6 +186,7 @@ export abstract class Target extends RenderTarget implements RenderTargetDebugCo
   }
 
   public get compositor() { return this._compositor; }
+  public get outputColorTexture(): TextureHandle | undefined { return this._fbo?.getColor(0); }
   public get isReadPixelsInProgress(): boolean { return this._isReadPixelsInProgress; }
   public get readPixelsSelector(): Pixel.Selector { return this._readPixelsSelector; }
   public get drawNonLocatable(): boolean { return this._drawNonLocatable; }
@@ -788,6 +789,10 @@ export abstract class Target extends RenderTarget implements RenderTargetDebugCo
     }
 
     let result: Pixel.Buffer | undefined;
+
+    // this.renderSystem.frameBufferStack.execute(expectDefined(this._fbo), false, false, () => {
+    //   this.compositor.preDraw();
+    // });
 
     this.renderSystem.frameBufferStack.execute(resources.fbo, true, false, () => {
       let updatedExclusions = false;
